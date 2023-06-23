a8ef3a69100d1607ee6d84b3c5f58cd5d56fc82d
Simone Camito <zibo.camito@gmail.com>
2023-06-23 17:08:50 +0200

    docker db + event table draft

---
+++ db/init.sql
@@ -0,0 +1,15 @@
+create table event ( 
+    sequence_num bigserial NOT NULL,
+
+    stream_id uuid NOT NULL,
+    version bigint NOT NULL,
+
+    type text NOT NULL,
+    data jsonb NOT NULL,
+    meta jsonb NOT NULL,
+
+    log_date timestamptz NOT NULL DEFAULT now(),
+
+    PRIMARY KEY (sequence_num),
+    UNIQUE (stream_id, version)
+);
---
+++ scripts/db-start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose -f ./db/local-db.yml up -d
