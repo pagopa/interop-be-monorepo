80971b661e580f89db2816c1ad1988ee0d23e50a
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-06-27 17:43:23 +0200

    Update models, types and errors definitions

--- src/model/domain/errors.ts
+++ src/model/domain/errors.ts
@@ -1,4 +1,23 @@
-interface IGenericError {
-  readonly message: string;
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+export enum ErrorCode {
+  DuplicateEserviceName = "0010",
+  ContentTypeParsingError = "0001",
+  UnexpectedError = "9999", // TODO: arbitrary error code retrieve it
 }
-export default IGenericError;
+
+export type DuplicatedEServiceNameError = CatalogProcessError & {
+  readonly code: ErrorCode.DuplicateEserviceName;
+};
+
+export type ContentTypeParsingError = CatalogProcessError & {
+  readonly code: ErrorCode.ContentTypeParsingError;
+};
+
+export class CatalogProcessError extends Error {
+  public readonly code: ErrorCode;
+
+  constructor(message: string, code: ErrorCode) {
+    super(message);
+    this.code = code;
+  }
+}
