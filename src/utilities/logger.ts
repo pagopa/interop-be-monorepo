39290a3c83de2c99a8b6287e722c60ecc709c934
Simone Camito <zibo.camito@gmail.com>
2023-06-27 18:12:56 +0200

    removed logs on file

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -4,16 +4,7 @@ import { config } from "./config.js";
 
 export const logger = winston.createLogger({
   level: config.logLevel,
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.DailyRotateFile({
-      filename: "%DATE%.log",
-      dirname: "./logs",
-      datePattern: "YYYY-MM-DD-HH",
-      maxSize: "20m",
-      maxFiles: "365d",
-    }),
-  ],
+  transports: [new winston.transports.Console()],
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.json()
