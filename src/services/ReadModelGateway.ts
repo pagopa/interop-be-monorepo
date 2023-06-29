507ee25e250b59efb4aab7c0d7409dc1bb051ed7
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-06-29 15:34:52 +0200

    Minor fix eservice models

--- src/services/ReadModelGateway.ts
+++ src/services/ReadModelGateway.ts
@@ -5,12 +5,13 @@
 import { EService } from "../model/domain/models.js";
 
 export interface IReadModelGateway {
-  readonly getEServiceByName: (name: string) => Promise<EService>;
+  readonly getEServiceByName: (name: string) => Promise<EService | undefined>;
   readonly getOrganizationID: () => Promise<string>;
 }
 
 export const ReadModelGateway: IReadModelGateway = {
-  getEServiceByName: async (_name: string): Promise<EService> => undefined,
+  getEServiceByName: async (_name: string): Promise<EService | undefined> =>
+    undefined,
   getOrganizationID: async (): Promise<string> =>
-    "6A568A80-1B05-48EA-A74A-9A4C1B825CFB", // read organizaiotn id from context instead
+    "6A568A80-1B05-48EA-A74A-9A4C1B825CFB", // read organization id from context instead
 };
