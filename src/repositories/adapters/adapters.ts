e6f046886e0f1900609429dcd17e53a005065f14
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-06-29 15:05:35 +0200

    User uuid instead uuidv4

--- src/repositories/adapters/adapters.ts
+++ src/repositories/adapters/adapters.ts
@@ -1,11 +1,11 @@
-import { uuid } from "uuidv4";
+import { v4 as uuidv4 } from "uuid";
 import { EServiceSeed } from "../../model/domain/models.js";
 import { CreateEvent } from "../events.js";
 
 export const eserviceSeedToCreateEvent = (
   eserviceSeed: EServiceSeed
 ): CreateEvent<EServiceSeed> => ({
-  streamId: uuid(),
+  streamId: uuidv4(),
   version: 0,
   type: "EServiceCreated",
   data: eserviceSeed,
