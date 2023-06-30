510a4c1d05246add2c90049ae43b73826025429a
Gabriele Petronella <gabriele@buildo.io>
2023-06-30 16:11:11 +0200

    Refactor using EffectTS

--- src/routers/catalog.ts
+++ src/routers/catalog.ts
@@ -1,37 +1,59 @@
+import * as Effect from "@effect/io/Effect";
+import { pipe } from "@effect/data/Function";
 import { zodiosRouter } from "@zodios/express";
+import { Response } from "express";
 import { api } from "../model/generated/api.js";
-import { ApiError, mapCatalogServiceErrorToApiError } from "../model/types.js";
+import {
+  makeGenericErrorProblem,
+  mapCatalogServiceErrorToApiError,
+} from "../model/types.js";
 import { catalogService } from "../services/CatalogService.js";
+import { db, DB } from "../repositories/db.js";
+import { CatalogProcessError } from "../model/domain/errors.js";
 
 const eservicesRouter = zodiosRouter(api.api);
 
+function runWithDb<A>(
+  program: Effect.Effect<DB, CatalogProcessError, A>,
+  res: Response,
+  successStatusCode: number
+): Promise<void> {
+  return Effect.runPromise(
+    pipe(
+      program,
+      Effect.provideService(DB, db),
+      Effect.catchAllDefect(() =>
+        Effect.sync(() => {
+          const errorRes = makeGenericErrorProblem();
+          res.status(errorRes.status).json(errorRes).end();
+        })
+      ),
+      Effect.match(
+        (error) => {
+          const errorRes = mapCatalogServiceErrorToApiError(error);
+          res.status(errorRes.status).json(errorRes).end();
+        },
+        () => {
+          res.status(successStatusCode).send();
+        }
+      )
+    )
+  );
+}
+
 eservicesRouter
-  .post("/eservices", async (req, res) => {
-    try {
-      await catalogService.createEService(req.body, req.authData);
-      return res.status(201).end();
-    } catch (error) {
-      const errorRes: ApiError = mapCatalogServiceErrorToApiError(error);
-      return res.status(errorRes.status).json(errorRes).end();
-    }
-  })
-  .put("/eservices/:eServiceId", async (req, res) => {
-    try {
-      await catalogService.updateEService(req.params.eServiceId, req.body);
-      return res.status(200).end();
-    } catch (error) {
-      const errorRes: ApiError = mapCatalogServiceErrorToApiError(error);
-      return res.status(errorRes.status).json(errorRes).end();
-    }
-  })
-  .delete("/eservices/:eServiceId", async (req, res) => {
-    try {
-      await catalogService.deleteEService(req.params.eServiceId);
-      return res.status(204).end();
-    } catch (error) {
-      const errorRes: ApiError = mapCatalogServiceErrorToApiError(error);
-      return res.status(errorRes.status).json(errorRes).end();
-    }
-  });
+  .post("/eservices", async (req, res) =>
+    runWithDb(catalogService.createEService(req.body, req.authData), res, 201)
+  )
+  .put("/eservices/:eServiceId", async (req, res) =>
+    runWithDb(
+      catalogService.updateEService(req.params.eServiceId, req.body),
+      res,
+      200
+    )
+  )
+  .delete("/eservices/:eServiceId", async (req, res) =>
+    runWithDb(catalogService.deleteEService(req.params.eServiceId), res, 204)
+  );
 
 export default eservicesRouter;
--- src/services/ReadModelGateway.ts
+++ src/services/ReadModelGateway.ts
@@ -2,16 +2,22 @@
   IMPORTANT: This service mocks all operations performed throught read models
 ===========================================================================  */
 
+import * as Effect from "@effect/io/Effect";
+import * as Option from "@effect/data/Option";
 import { EService } from "../model/domain/models.js";
 
 export const readModelGateway = {
-  async getEServiceById(_id: string): Promise<EService | undefined> {
-    return undefined;
+  getEServiceById(
+    _id: string
+  ): Effect.Effect<never, never, Option.Option<EService>> {
+    return Effect.succeed(Option.none());
   },
-  async getEServiceByName(_name: string): Promise<EService | undefined> {
-    return undefined;
+  getEServiceByName(
+    _name: string
+  ): Effect.Effect<never, never, Option.Option<EService>> {
+    return Effect.succeed(Option.none());
   },
-  async getOrganizationID(): Promise<string> {
-    return "6A568A80-1B05-48EA-A74A-9A4C1B825CFB"; // read organization id from context instead
+  getOrganizationID(): Effect.Effect<never, never, string> {
+    return Effect.succeed("6A568A80-1B05-48EA-A74A-9A4C1B825CFB"); // read organization id from context instead
   },
 };
