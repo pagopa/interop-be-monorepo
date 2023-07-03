a86a422e873a38f6626377a61d59923885f00ffb
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-07-03 18:39:51 +0200

    Minor fix

--- src/auth/authData.ts
+++ src/auth/authData.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
-export const authJWTToken = z.object({
+export const AuthJWTToken = z.object({
   organizationId: z.string().uuid(),
 });
-export type AuthJWTToken = z.infer<typeof authJWTToken>;
+export type AuthJWTToken = z.infer<typeof AuthJWTToken>;
 
-export const authData = z.object({
+export const AuthData = z.object({
   organizationId: z.string().uuid(),
 });
-export type AuthData = z.infer<typeof authData>;
+export type AuthData = z.infer<typeof AuthData>;
--- src/authMiddleware.ts
+++ src/authMiddleware.ts
@@ -12,25 +12,20 @@ export const authMiddleware: ZodiosRouterContextRequestHandler<
     const authorization = req.headers.authorization;
     return match(authorization)
       .with(P.string, (auth: string) => {
-        const bearer = auth.replace("Bearer ", "");
-        if (!bearer) {
+        const authContent = auth.split(" ");
+
+        if (authContent.length !== 2 && authContent[0] !== "Bearer") {
           throw new CatalogProcessError(
             `Bearer token has not been passed`,
             ErrorCode.MissingBearer
           );
         }
 
-        const authData = readClaimsFromJwtToken(bearer);
+        const jwtToken = authContent[1];
+        const authData = readClaimsFromJwtToken(jwtToken);
         if (!authData) {
           throw new CatalogProcessError(
             `Invalid claims: token parsing error`,
-            ErrorCode.MissingClaim
-          );
-        }
-
-        if (!authData.organizationId) {
-          throw new CatalogProcessError(
-            `Claim organizationId has not been passed`,
             ErrorCode.MissingClaim
           );
         }
