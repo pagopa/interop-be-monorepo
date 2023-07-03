91d0f23c829346bc0f8ded89ec53e2a914da5ea3
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-07-03 13:30:42 +0200

    Add jwt decoding utils

---
+++ src/auth/jwt.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { logger } from "../utilities/logger.js";
+import { AuthData, authJWTToken } from "./authData.js";
+
+export const JWTClaim = {
+  ORGANIZATION_ID_CLAIM: "organizationId",
+};
+
+export const readClaimsFromJwtToken = (jwtToken: string): AuthData | null => {
+  const decoded = jwt.decode(jwtToken, { json: true });
+  const token = authJWTToken.safeParse(decoded);
+
+  if (!token.success) {
+    logger.error(`Error parsing token: ${JSON.stringify(token.error)}`);
+    return null;
+  } else {
+    return {
+      organizationId: token.data.organizationId,
+    };
+  }
+};
