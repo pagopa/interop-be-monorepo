9d7ea7c9b472268449c4a1ee80cc9655339e17c8
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-07-13 15:50:12 +0200

    Update authmiddleware and JWT

--- packages/catalog-process/src/authMiddleware.ts
+++ packages/catalog-process/src/authMiddleware.ts
@@ -1,77 +1,87 @@
-/* eslint-disable functional/immutable-data */
 import { ZodiosRouterContextRequestHandler } from "@zodios/express";
 import { match, P } from "ts-pattern";
+import { z } from "zod";
 import { ExpressContext } from "./app.js";
 import { ApiError, makeApiError } from "./model/types.js";
 import { AuthData } from "./auth/authData.js";
 import { readAuthDataFromJwtToken } from "./auth/jwt.js";
-import { CatalogProcessError, ErrorType } from "./model/domain/errors.js";
+import {
+  CatalogProcessError,
+  ErrorTypes,
+  missingHeader,
+} from "./model/domain/errors.js";
 import { logger } from "./utilities/logger.js";
+
+const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+
+const Headers = z.object({
+  Authorization: z.string(),
+  "X-Correlation-Id": z.string(),
+  "X-Forwarded-For": z.string().ip().optional(),
+});
+
+type Headers = z.infer<typeof Headers>;
 
 export const authMiddleware: ZodiosRouterContextRequestHandler<
   ExpressContext
 > = (req, res, next) => {
+  const handleJwtToken = (headers: Headers) => {
+    const authorizationHeader = headers.Authorization.split(" ");
+    if (
+      authorizationHeader.length !== 2 ||
+      authorizationHeader[0] !== "Bearer"
+    ) {
+      logger.warn(
+        `No authentication has been provided for this call ${req.method} ${req.url}`
+      );
+      throw new CatalogProcessError(
+        "Authorization Illegal header key.",
+        ErrorTypes.MissingBearer
+      );
+    }
+
+    const jwtToken = authorizationHeader[1];
+    const authData = readAuthDataFromJwtToken(jwtToken);
+
+    match(authData)
+      .with(
+        P.shape({
+          organizationId: P.string,
+          userId: P.string,
+          sub: P.string,
+        }),
+        (claimsRes: AuthData) => {
+          req.ctx.authData = claimsRes;
+          req.ctx.correlationId = headers["X-Correlation-Id"];
+          req.ctx.ip = headers["X-Forwarded-For"];
+          next();
+        }
+      )
+      .with(P.instanceOf(Error), (err) => {
+        logger.warn(`Invalid authentication provided: ${err.message}`);
+        throw new CatalogProcessError(
+          `Invalid claims: ${err.message}`,
+          ErrorTypes.MissingClaim
+        );
+      });
+  };
+
   try {
-    const correlationId = req.headers["X-Correlation-Id"];
-    const ip = req.headers["X-Forwarded-For"];
-    const authBearerToken = req.headers.authorization;
-
-    const headersData = {
-      authBearerToken,
-      correlationId,
-      ip,
-    };
-
-    return match(headersData)
+    const headers = Headers.parse(req.headers);
+    return match(headers)
       .with(
         {
-          authBearerToken: P.string,
-          correlationId: P.string,
-          ip: P.string.or(P.nullish),
+          Authorization: P.string,
+          "X-Correlation-Id": P.string,
+          "X-Forwarded-For": P.string.regex(ipRegex).or(P.nullish),
         },
-        (headers) => {
-          const authContent = headers.authBearerToken.split(" ");
-          if (authContent.length !== 2 || authContent[0] !== "Bearer") {
-            logger.warn(
-              `No authentication has been provided for this call ${req.method} ${req.url}`
-            );
-            throw new CatalogProcessError(
-              "Authorization Illegal header key.",
-              ErrorCode.MissingBearer
-            );
-          }
-
-          const jwtToken = authContent[1];
-          const authData = readAuthDataFromJwtToken(jwtToken);
-
-          match(authData)
-            .with(
-              P.shape({
-                organizationId: P.string,
-                userId: P.string,
-                sub: P.string,
-              }),
-              (claimsRes: AuthData) => {
-                req.ctx.authData = claimsRes;
-                req.ctx.correlationId = headers.correlationId;
-                req.ctx.ip = headers.ip;
-                next();
-              }
-            )
-            .with(P.instanceOf(Error), (err) => {
-              logger.warn(`Invalid authentication provided: ${err.message}`);
-              throw new CatalogProcessError(
-                `Invalid claims: ${err.message}`,
-                ErrorCode.MissingClaim
-              );
-            });
-        }
+        (headers) => handleJwtToken(headers)
       )
       .with(
         {
-          authBearerToken: P.nullish,
-          correlationId: P._,
-          ip: P._,
+          Authorization: P.nullish,
+          "X-Correlation-Id": P._,
+          "X-Forwarded-For": P._,
         },
         () => {
           logger.warn(
@@ -80,37 +90,21 @@ export const authMiddleware: ZodiosRouterContextRequestHandler<
 
           throw new CatalogProcessError(
             `Bearer token has not been passed`,
-            ErrorCode.MissingBearer
+            ErrorTypes.MissingBearer
           );
         }
       )
       .with(
         {
-          authBearerToken: P._,
-          correlationId: P.nullish,
-          ip: P._,
+          Authorization: P.string,
+          "X-Correlation-Id": P.nullish,
+          "X-Forwarded-For": P._,
         },
-        () => {
-          throw new CatalogProcessError(
-            `Invalid claims: token parsing error`,
-            ErrorType.MissingClaim
-          );
-        }
-
-        // eslint-disable-next-line functional/immutable-data
-        req.authData = authData;
-        next();
-      })
-      .with(P.nullish, () => {
-        throw new CatalogProcessError(
-            `Header X-Correlation-Id not existing in this request`,
-            ErrorCode.MissingHeader
-          );
-        }
+        () => missingHeader("X-Correlation-Id")
       )
       .otherwise(() => {
         throw new CatalogProcessError(
-          `Header authorization not existing in this request`,
+          ErrorTypes.MissingHeader.title,
           ErrorTypes.MissingHeader
         );
       });
