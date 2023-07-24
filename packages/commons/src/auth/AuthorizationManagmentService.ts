47c5a31095242e6a10ed49d59783f2e396c00f12
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-07-24 17:23:04 +0200

    Add publish descriptor API

---
+++ packages/commons/src/auth/AuthorizationManagmentService.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* 
+  This service is a mock for the AuthorizationManagementService it is used as entrypoint for authorization permission updates.
+*/
+
+export const authorizationManagementServiceMock = {
+  async updateStateOnClients(): Promise<void> {
+    // TODO : [https://buildo.atlassian.net/browse/PPA-25] handle the update of the state on the clients
+    return Promise.resolve();
+  },
+};
