e2d7258cf39cad8b2ae44a06dbfe5945ad495e6d
Simone Camito <zibo.camito@gmail.com>
2023-08-31 15:43:50 +0200

    add create descriptor test

--- packages/catalog-process/src/utilities/versionGenerator.ts
+++ packages/catalog-process/src/utilities/versionGenerator.ts
@@ -4,7 +4,9 @@ import { CatalogProcessError, ErrorTypes } from "../model/domain/errors.js";
 
 export const nextDescriptorVersion = (eservice: EService): string => {
   const currentVersion = eservice.descriptors.reduce((max, descriptor) => {
-    const currentVersionNumber = z.number().safeParse(descriptor.version);
+    const currentVersionNumber = z.coerce
+      .number()
+      .safeParse(descriptor.version);
     if (!currentVersionNumber.success) {
       throw new CatalogProcessError(
         `${descriptor.version} is not a valid descriptor version`,
