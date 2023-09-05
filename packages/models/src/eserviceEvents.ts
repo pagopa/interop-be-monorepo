d4bd8cefbd18cfe04d8adba79d514adb41627300
Simone Camito <zibo.camito@gmail.com>
2023-09-05 17:33:14 +0200

    write binary data on db

--- packages/models/src/eserviceEvents.ts
+++ packages/models/src/eserviceEvents.ts
@@ -1,9 +1,10 @@
 // import z from "zod";
 // import { EService, Descriptor, Document } from "./eservice.js";
 
+import { match } from "ts-pattern";
 import {
   ClonedEServiceAddedV1,
-  EServiceV1AddedV1,
+  EServiceAddedV1,
   EServiceUpdatedV1,
   EServiceWithDescriptorsDeletedV1,
   EServiceDocumentUpdatedV1,
@@ -15,83 +16,46 @@ import {
   MovedAttributesFromEserviceToDescriptorsV1,
 } from "./gen/v1/events.js";
 
-// export const EServiceAdded = z.object({
-//   eService: EService,
-// });
-// export type EServiceAdded = z.infer<typeof EServiceAdded>;
-
-// export const ClonedEServiceAdded = z.object({
-//   eService: EService,
-// });
-// export type ClonedEServiceAdded = z.infer<typeof ClonedEServiceAdded>;
-
-// export const EServiceUpdated = z.object({
-//   eService: EService,
-// });
-// export type EServiceUpdated = z.infer<typeof EServiceUpdated>;
-
-// // This event leads just the deletion of the descriptor. The old name has been kept to maintain data retrocompatibility
-// export const EServiceWithDescriptorsDeleted = z.object({
-//   eService: EService,
-//   descriptorId: z.string().uuid(),
-// });
-// export type EServiceWithDescriptorsDeleted = z.infer<
-//   typeof EServiceWithDescriptorsDeleted
-// >;
-
-// export const EServiceDocumentUpdated = z.object({
-//   eServiceId: z.string().uuid(),
-//   descriptorId: z.string().uuid(),
-//   documentId: z.string().uuid(),
-//   updatedDocument: Document,
-//   serverUrls: z.array(z.string()),
-// });
-// export type EServiceDocumentUpdated = z.infer<typeof EServiceDocumentUpdated>;
-
-// export const EServiceDeleted = z.object({
-//   eServiceId: z.string().uuid(),
-// });
-// export type EServiceDeleted = z.infer<typeof EServiceDeleted>;
-
-// export const EServiceDocumentAdded = z.object({
-//   eServiceId: z.string().uuid(),
-//   descriptorId: z.string().uuid(),
-//   document: Document,
-//   isInterface: z.boolean(),
-//   serverUrls: z.array(z.string()),
-// });
-// export type EServiceDocumentAdded = z.infer<typeof EServiceDocumentAdded>;
-
-// export const EServiceDocumentDeleted = z.object({
-//   eServiceId: z.string().uuid(),
-//   descriptorId: z.string().uuid(),
-//   documentId: z.string().uuid(),
-// });
-// export type EServiceDocumentDeleted = z.infer<typeof EServiceDocumentDeleted>;
-
-// export const EServiceDescriptorAdded = z.object({
-//   eServiceId: z.string().uuid(),
-//   eServiceDescriptor: Descriptor,
-// });
-// export type EServiceDescriptorAdded = z.infer<typeof EServiceDescriptorAdded>;
-
-// export const EServiceDescriptorUpdated = z.object({
-//   eServiceId: z.string().uuid(),
-//   eServiceDescriptor: Descriptor,
-// });
-// export type EServiceDescriptorUpdated = z.infer<
-//   typeof EServiceDescriptorUpdated
-// >;
-
-// export const MovedAttributesFromEserviceToDescriptors = z.object({
-//   eService: EService,
-// });
-// export type MovedAttributesFromEserviceToDescriptors = z.infer<
-//   typeof MovedAttributesFromEserviceToDescriptors
-// >;
+export function toBinaryData(event: EServiceEvent): Uint8Array {
+  return match(event)
+    .with({ type: "EServiceAdded" }, ({ data }) =>
+      EServiceAddedV1.toBinary(data)
+    )
+    .with({ type: "ClonedEServiceAdded" }, ({ data }) =>
+      ClonedEServiceAddedV1.toBinary(data)
+    )
+    .with({ type: "EServiceUpdated" }, ({ data }) =>
+      EServiceUpdatedV1.toBinary(data)
+    )
+    .with({ type: "EServiceWithDescriptorsDeleted" }, ({ data }) =>
+      EServiceWithDescriptorsDeletedV1.toBinary(data)
+    )
+    .with({ type: "EServiceDocumentUpdated" }, ({ data }) =>
+      EServiceDocumentUpdatedV1.toBinary(data)
+    )
+    .with({ type: "EServiceDeleted" }, ({ data }) =>
+      EServiceDeletedV1.toBinary(data)
+    )
+    .with({ type: "EServiceDocumentAdded" }, ({ data }) =>
+      EServiceDocumentAddedV1.toBinary(data)
+    )
+    .with({ type: "EServiceDocumentDeleted" }, ({ data }) =>
+      EServiceDocumentDeletedV1.toBinary(data)
+    )
+    .with({ type: "EServiceDescriptorAdded" }, ({ data }) =>
+      EServiceDescriptorAddedV1.toBinary(data)
+    )
+    .with({ type: "EServiceDescriptorUpdated" }, ({ data }) =>
+      EServiceDescriptorUpdatedV1.toBinary(data)
+    )
+    .with({ type: "MovedAttributesFromEserviceToDescriptors" }, ({ data }) =>
+      MovedAttributesFromEserviceToDescriptorsV1.toBinary(data)
+    )
+    .exhaustive();
+}
 
 export type EServiceEvent =
-  | { type: "EServiceAdded"; data: EServiceV1AddedV1 }
+  | { type: "EServiceAdded"; data: EServiceAddedV1 }
   | { type: "ClonedEServiceAdded"; data: ClonedEServiceAddedV1 }
   | { type: "EServiceUpdated"; data: EServiceUpdatedV1 }
   | {
