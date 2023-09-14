113dc2cf3b4e625c8812b48ea284a0b31c950263
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-09-14 19:18:08 +0200

    EventConsumer read aws role credentials for kafkajs config

--- packages/event-consumer/src/index.ts
+++ packages/event-consumer/src/index.ts
@@ -2,13 +2,45 @@ import "dotenv-flow/config.js";
 
 import { Kafka, KafkaMessage } from "kafkajs";
 import { logger } from "pagopa-interop-commons";
+import AWS from "aws-sdk";
 import { decodeKafkaMessage } from "./model/models.js";
 import { handleMessage } from "./consumerService.js";
 import { config } from "./utilities/config.js";
 
+const sts = new AWS.STS();
+
+const assumeRoleResponse = await sts
+  .assumeRole({
+    RoleArn: "arn:aws:iam::505630707203:role/interop-buildo-developers-dev",
+    RoleSessionName: "catalog-consumer-session",
+  })
+  .promise();
+
+if (!assumeRoleResponse.Credentials || !assumeRoleResponse.AssumedRoleUser) {
+  exitGracefully();
+}
+
+const accessKey = assumeRoleResponse.Credentials
+  ? assumeRoleResponse.Credentials.AccessKeyId
+  : "";
+const secretKey = assumeRoleResponse.Credentials
+  ? assumeRoleResponse.Credentials.SecretAccessKey
+  : "";
+
+const roleId = assumeRoleResponse.AssumedRoleUser
+  ? assumeRoleResponse.AssumedRoleUser.AssumedRoleId
+  : "";
+
 const kafka = new Kafka({
   clientId: config.kafkaClientId,
   brokers: [config.kafkaBrokers],
+  ssl: true,
+  sasl: {
+    mechanism: "aws",
+    authorizationIdentity: roleId,
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+  },
 });
 
 const consumer = kafka.consumer({ groupId: config.kafkaGroupId });
