1c9bd762d92d3616005694b42c15286470da98b7
Simone Camito <zibo.camito@gmail.com>
2023-09-19 15:53:32 +0200

    add jwt token verification

--- packages/commons/src/config/index.ts
+++ packages/commons/src/config/index.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
+import { APIEndpoint } from "./../model/apiEndpoint.js";
 
 const Config = z
   .object({
     LOG_LEVEL: z.enum(["debug", "info", "warn", "error"]),
+    WELL_KNOWN_URLS: z
+      .string()
+      .transform((s) => s.split(",").map((s) => APIEndpoint.parse(s))),
   })
   .transform((c) => ({
     logLevel: c.LOG_LEVEL,
+    wellKnownUrls: c.WELL_KNOWN_URLS,
   }));
 
 export type Config = z.infer<typeof Config>;
