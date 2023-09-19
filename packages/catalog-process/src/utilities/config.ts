4ce397fa16ca05da67c18a56fc3e622ea8ec2754
Simone Camito <zibo.camito@gmail.com>
2023-09-19 16:27:54 +0200

    move apiendpoint definition in common package

--- packages/catalog-process/src/utilities/config.ts
+++ packages/catalog-process/src/utilities/config.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 import {
   config as commonsConfig,
   Config as CommonConfig,
+  APIEndpoint,
 } from "pagopa-interop-commons";
-import { APIEndpoint } from "../model/apiEndpoint.js";
 
 const FileManagerConfig = z
   .discriminatedUnion("MOCK_FILE_MANAGER", [
@@ -30,16 +30,16 @@ const FileManagerConfig = z
   .transform((c) =>
     c.MOCK_FILE_MANAGER === "false"
       ? {
-          mockFileManager: false as const,
-          s3AccessKeyId: c.S3_ACCESS_KEY_ID,
-          s3SecretAccessKey: c.S3_SECRET_ACCESS_KEY,
-          s3Region: c.S3_REGION,
-          s3BucketName: c.S3_BUCKET_NAME,
-          eserviceDocsPath: c.ESERVICE_DOCS_PATH,
-        }
+        mockFileManager: false as const,
+        s3AccessKeyId: c.S3_ACCESS_KEY_ID,
+        s3SecretAccessKey: c.S3_SECRET_ACCESS_KEY,
+        s3Region: c.S3_REGION,
+        s3BucketName: c.S3_BUCKET_NAME,
+        eserviceDocsPath: c.ESERVICE_DOCS_PATH,
+      }
       : {
-          mockFileManager: true as const,
-        }
+        mockFileManager: true as const,
+      }
   );
 
 export type FileManagerConfig = z.infer<typeof FileManagerConfig>;
