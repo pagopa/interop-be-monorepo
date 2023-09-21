6b4e0c0fc973e36b3ef8ee44b56f9f0770492810
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-09-21 15:55:56 +0200

    config KAFKA_DISABLE_AWS_IAM_AUTH allow only lowercase

--- packages/event-consumer/src/utilities/config.ts
+++ packages/event-consumer/src/utilities/config.ts
@@ -22,17 +22,13 @@ const KafkaConfig = z
     KAFKA_BROKERS: z.string(),
     KAFKA_CLIENT_ID: z.string(),
     KAFKA_GROUP_ID: z.string(),
-    KAFKA_DISABLE_AWS_IAM_AUTH: z
-      .enum(["true", "false", "TRUE", "FALSE"])
-      .optional(),
+    KAFKA_DISABLE_AWS_IAM_AUTH: z.literal("true").optional(),
   })
   .transform((c) => ({
     kafkaBrokers: c.KAFKA_BROKERS,
     kafkaClientId: c.KAFKA_CLIENT_ID,
     kafkaGroupId: c.KAFKA_GROUP_ID,
-    kafkaDisableAwsIamAuth:
-      c.KAFKA_DISABLE_AWS_IAM_AUTH === "TRUE" ||
-      c.KAFKA_DISABLE_AWS_IAM_AUTH === "true",
+    kafkaDisableAwsIamAuth: c.KAFKA_DISABLE_AWS_IAM_AUTH === "true",
   }));
 export type KafkaConfig = z.infer<typeof KafkaConfig>;
 
