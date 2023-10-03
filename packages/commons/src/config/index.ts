d2dce6a4fabfc1ba446fdacf3cce562d71209e5a
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-10-03 16:15:01 +0200

    Fix JWT configuration parsing

--- packages/commons/src/config/index.ts
+++ packages/commons/src/config/index.ts
@@ -37,16 +37,12 @@ type JWTConfig = z.infer<typeof JWTConfig>;
 const RequiredConfig = z
   .object({
     LOG_LEVEL: z.enum(["debug", "info", "warn", "error"]),
-    WELL_KNOWN_URLS: z
-      .string()
-      .transform((s) => s.split(",").map((s) => APIEndpoint.parse(s))),
   })
   .transform((c) => ({
     logLevel: c.LOG_LEVEL,
-    wellKnownUrls: c.WELL_KNOWN_URLS,
   }));
 
-const Config = RequiredConfig.and(JWTConfig.optional());
+const Config = RequiredConfig.and(JWTConfig);
 
 export type Config = z.infer<typeof Config>;
 export const config = Config.parse(process.env);
