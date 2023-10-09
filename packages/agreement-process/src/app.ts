54b4731a977512cc2536d62ad43be26dabf98c33
Simone Camito <zibo.camito@gmail.com>
2023-10-09 12:42:32 +0200

    bootstrap health route

---
+++ packages/agreement-process/src/app.ts
@@ -0,0 +1,35 @@
+import { zodiosContext } from "@zodios/express";
+import * as expressWinston from "express-winston";
+import { logger } from "pagopa-interop-commons";
+import healthRouter from "./routers/HealthRouter.js";
+import { config } from "./utilities/config.js";
+
+const zodiosCtx = zodiosContext();
+const app = zodiosCtx.app();
+
+export type ZodiosContext = NonNullable<typeof zodiosCtx>;
+export type ExpressContext = NonNullable<typeof zodiosCtx.context>;
+
+// Disable the "X-Powered-By: Express" HTTP header for security reasons.
+// See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
+app.disable("x-powered-by");
+
+app.use(
+    expressWinston.logger({
+        winstonInstance: logger,
+        metaField: null,
+        requestWhitelist:
+            config.logLevel === "debug" ? ["body", "headers", "query"] : [],
+        ignoredRoutes: ["/status"],
+        responseWhitelist:
+            config.logLevel === "debug"
+                ? ["body", "statusCode", "statusMessage"]
+                : [],
+    })
+);
+
+// NOTE(gabro): the order is relevant, authMiddleware must come *after* the routes
+// we want to be unauthenticated.
+app.use(healthRouter);
+
+export default app;
---
+++ packages/agreement-process/src/utilities/config.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import {
+    config as commonsConfig,
+    Config as CommonConfig,
+    APIEndpoint,
+} from "pagopa-interop-commons";
+
+const Config = z
+    .object({
+        HOST: APIEndpoint,
+        PORT: z.coerce.number().min(1001),
+    })
+    .transform((c) => ({
+        host: c.HOST,
+        port: c.PORT,
+    }));
+
+export type Config = z.infer<typeof Config>;
+
+export const config: Config & CommonConfig = {
+    ...commonsConfig,
+    ...Config.parse(process.env),
+};
