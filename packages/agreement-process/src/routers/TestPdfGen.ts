7c723d2dc1f5e53d0094627a197e28066225c835
Simone Camito <zibo.camito@gmail.com>
2023-10-09 18:22:37 +0200

    generate pdf from template

--- packages/agreement-process/src/routers/TestPdfGen.ts
+++ packages/agreement-process/src/routers/TestPdfGen.ts
@@ -11,12 +11,29 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 testPdfGenRouter.get("/generatePDF", async (_, res) => {
-  const localFilePath = "file://" + path.resolve(dirname, "template3.html");
+  const outHtmlPath = path.resolve(dirname, "..", "templates", "test.html");
+  const outPdfPath = path.resolve(dirname, "test.pdf");
+  const templateFilePath = path.resolve(
+    dirname,
+    "..",
+    "templates",
+    "template.html"
+  );
+  const cssFilePath = path.resolve(dirname, "..", "templates", "style.css");
+
+  const template = fs.readFileSync(templateFilePath, "utf-8");
+  const css = fs.readFileSync(cssFilePath, "utf-8");
+
+  const content = template.replace(
+    '<link rel="stylesheet" href="style.css" />',
+    `<style>${css}</style><script src="paged.polyfill.js"></script>`
+  );
+  fs.writeFileSync(outHtmlPath, content);
 
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  await page.goto(localFilePath, {
+  await page.goto(`file://${outHtmlPath}`, {
     waitUntil: "networkidle0", // Wait until network is idle
   });
 
@@ -33,7 +50,7 @@ testPdfGenRouter.get("/generatePDF", async (_, res) => {
 
   await browser.close();
 
-  fs.writeFileSync("test.pdf", buffer);
+  fs.writeFileSync(outPdfPath, buffer);
 
   res.status(200).end();
 });
