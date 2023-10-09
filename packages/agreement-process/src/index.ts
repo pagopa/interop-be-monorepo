81ed0bdda1ee6fa3b80ed82188fd99afd0ef98df
Simone Camito <zibo.camito@gmail.com>
2023-10-09 16:22:50 +0200

    fix linter

--- packages/agreement-process/src/index.ts
+++ packages/agreement-process/src/index.ts
@@ -3,5 +3,5 @@ import { config } from "./utilities/config.js";
 import app from "./app.js";
 
 app.listen(config.port, config.host, () => {
-    logger.info(`listening on ${config.host}:${config.port}`);
+  logger.info(`listening on ${config.host}:${config.port}`);
 });
