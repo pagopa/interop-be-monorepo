1286ac828d5904c062283bb9a08415e8746d5ed7
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-10-10 18:17:16 +0200

    Add user roles

--- packages/commons/src/auth/authData.ts
+++ packages/commons/src/auth/authData.ts
@@ -1,6 +1,21 @@
 import { JwtPayload } from "jsonwebtoken";
 import { z } from "zod";
 
+export const userRoles = {
+  ADMIN_ROLE: "admin",
+  SECURITY_ROLE: "security",
+  API_ROLE: "api",
+  M2M_ROLE: "m2m",
+  INTERNAL_ROLE: "internal",
+  SUPPORT_ROLE: "support",
+} as const;
+
+export const UserRoles = z.enum([
+  Object.values(userRoles)[0],
+  ...Object.values(userRoles).slice(1),
+]);
+export type UserRoles = z.infer<typeof UserRoles>;
+
 export const AuthJWTToken = z.object({
   organizationId: z.string().uuid(),
   "user-roles": z
