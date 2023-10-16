4da67ee1b284335d1ccec70ba01373ea1873b37e
Simone Camito <zibo.camito@gmail.com>
2023-10-16 10:44:56 +0200

    change from 200 to 501

--- packages/agreement-process/src/routers/AgreementRouter.ts
+++ packages/agreement-process/src/routers/AgreementRouter.ts
@@ -9,100 +9,100 @@ const agreementRouter = (
   const agreementRouter = ctx.router(api.api);
 
   agreementRouter.post("/agreements/:agreementId/submit", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.post(
     "/agreements/:agreementId/activate",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.post(
     "/agreements/:agreementId/consumer-documents",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.get(
     "/agreements/:agreementId/consumer-documents/:documentId",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.delete(
     "/agreements/:agreementId/consumer-documents/:documentId",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.post(
     "/agreements/:agreementId/suspend",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.post("/agreements/:agreementId/reject", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.post(
     "/agreements/:agreementId/archive",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.post("/agreements", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.get("/agreements", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.get("/producers", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.get("/consumers", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.get("/agreements/:agreementId", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.delete("/agreements/:agreementId", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.post("/agreements/:agreementId/update", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.post(
     "/agreements/:agreementId/upgrade",
     async (_req, res) => {
-      res.status(200).send();
+      res.status(501).send();
     }
   );
 
   agreementRouter.post("/agreements/:agreementId/clone", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.post("/compute/agreementsState", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   agreementRouter.get("/agreements/filter/eservices", async (_req, res) => {
-    res.status(200).send();
+    res.status(501).send();
   });
 
   return agreementRouter;
