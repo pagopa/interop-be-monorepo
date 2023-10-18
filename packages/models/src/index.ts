3199c04663be4da17144965626e0cf7d4bf77b33
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-10-18 17:52:12 +0200

    Add agreement protobuf schemas

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -4,8 +4,14 @@ export * from "./eservice.js";
 export * from "./eserviceEvents.js";
 export * from "./gen/v1/eservice/eservice.js";
 export * from "./gen/v1/eservice/events.js";
+<<<<<<< HEAD
 export * from "./gen/v1/agreement/events.js";
 export * from "./gen/v1/agreement/agreement.js";
 export * from "./gen/v1/agreement/state.js";
 export * from "./agreementEvents.js";
+=======
+export * from "./gen/v1/agreement/agreement.js";
+export * from "./gen/v1/agreement/events.js";
+export * from "./gen/v1/agreement/state.js";
+>>>>>>> 54493af (Add agreement protobuf schemas)
 export * from "./readModels/readModels.js";
