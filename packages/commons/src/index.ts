2b229c8c5af933bf618c7dcc0f04dce2e876e622
Simone Camito <zibo.camito@gmail.com>
2023-10-18 14:59:19 +0200

    moved eventRepository inside the commons package

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./auth/index.js";
+export * from "./repositories/db.js";
+export * from "./repositories/EventRepository.js";
 export * from "./config/index.js";
 export * from "./context/index.js";
 export * from "./logging/index.js";
