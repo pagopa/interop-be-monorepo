1b81ab2430afbb7caf7985d85bc5eb618dece375
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-10-19 14:36:05 +0200

    Implements add agreement consuming

---
+++ packages/agreement-consumer/src/agreementConsumerService.ts
@@ -0,0 +1,46 @@
+import { match } from "ts-pattern";
+import { Collection, MongoClient } from "mongodb";
+import { logger, consumerConfig } from "pagopa-interop-commons";
+import { AgreementV1 } from "pagopa-interop-models";
+import { EventEnvelope } from "./model/models.js";
+
+const {
+  readModelDbUsername: username,
+  readModelDbPassword: password,
+  readModelDbHost: host,
+  readModelDbPort: port,
+  readModelDbName: database,
+} = consumerConfig;
+
+const mongoDBConectionURI = `mongodb://${username}:${password}@${host}:${port}`;
+const client = new MongoClient(mongoDBConectionURI, {
+  retryWrites: false,
+});
+
+const db = client.db(database);
+const eservices: Collection<{
+  data: AgreementV1 | undefined;
+  metadata: { version: number };
+}> = db.collection("eservices", { ignoreUndefined: true });
+
+export async function handleMessage(message: EventEnvelope): Promise<void> {
+  logger.info(message);
+  await match(message)
+    .with({ type: "AgreementAdded" }, async (msg) => {
+      await eservices.updateOne(
+        {
+          "data.id": msg.stream_id,
+        },
+        {
+          $setOnInsert: {
+            data: msg.data.agreement,
+            metadata: {
+              version: msg.version,
+            },
+          },
+        },
+        { upsert: true }
+      );
+    })
+    .exhaustive();
+}
---
+++ packages/agreement-consumer/src/index.ts
@@ -0,0 +1,52 @@
+import { Kafka, KafkaMessage } from "kafkajs";
+import { consumerConfig, logger } from "pagopa-interop-commons";
+import { createMechanism } from "@jm18457/kafkajs-msk-iam-authentication-mechanism";
+
+import { decodeKafkaMessage } from "./model/models.js";
+import { handleMessage } from "./agreementConsumerService.js";
+
+const kafkaConfig = consumerConfig.kafkaDisableAwsIamAuth
+  ? {
+      clientId: consumerConfig.kafkaClientId,
+      brokers: [consumerConfig.kafkaBrokers],
+      ssl: false,
+    }
+  : {
+      clientId: consumerConfig.kafkaClientId,
+      brokers: [consumerConfig.kafkaBrokers],
+      ssl: true,
+      sasl: createMechanism({ region: consumerConfig.awsRegion }),
+    };
+
+const kafka = new Kafka(kafkaConfig);
+const consumer = kafka.consumer({ groupId: consumerConfig.kafkaGroupId });
+await consumer.connect();
+
+function exitGracefully(): void {
+  consumer.disconnect().finally(() => {
+    logger.info("Consumer disconnected");
+    process.exit(0);
+  });
+}
+
+process.on("SIGINT", exitGracefully);
+process.on("SIGTERM", exitGracefully);
+
+await consumer.subscribe({
+  topics: ["event-store.agreement.events"],
+  fromBeginning: true,
+});
+
+async function processMessage(message: KafkaMessage): Promise<void> {
+  try {
+    await handleMessage(decodeKafkaMessage(message));
+
+    logger.info("Read model was updated");
+  } catch (e) {
+    logger.error(`Error during message handling ${e}`);
+  }
+}
+
+await consumer.run({
+  eachMessage: ({ message }) => processMessage(message),
+});
