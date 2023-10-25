bcbf5e3562dd4e28c001218dbf03d5752d6eb5cc
AsterITA <fabrizio.musella@gmail.com>
2023-10-25 12:25:15 +0200

    updated config based on refactor

--- packages/attribute-registry-process/src/utilities/config.ts
+++ packages/attribute-registry-process/src/utilities/config.ts
@@ -1,30 +1,15 @@
-import { z } from "zod";
 import {
-  config as commonsConfig,
-  Config as CommonConfig,
-  HTTPServerConfig,
+  CommonConfig,
+  ReadModelDbConfig,
+  EventStoreConfig,
 } from "pagopa-interop-commons";
+import { z } from "zod";
 
-const RequiredConfig = z
-  .object({
-    READMODEL_DB_HOST: z.string(),
-    READMODEL_DB_NAME: z.string(),
-    READMODEL_DB_USERNAME: z.string(),
-    READMODEL_DB_PASSWORD: z.string(),
-    READMODEL_DB_PORT: z.coerce.number().min(1001),
-  })
-  .transform((c) => ({
-    readModelDbHost: c.READMODEL_DB_HOST,
-    readModelDbName: c.READMODEL_DB_NAME,
-    readModelDbUsername: c.READMODEL_DB_USERNAME,
-    readModelDbPassword: c.READMODEL_DB_PASSWORD,
-    readModelDbPort: c.READMODEL_DB_PORT,
-  }));
+const CataloProcessConfig =
+  CommonConfig.and(ReadModelDbConfig).and(EventStoreConfig);
 
-export const Config = RequiredConfig.and(HTTPServerConfig);
-export type Config = z.infer<typeof Config>;
+export type CatalogProcessConfig = z.infer<typeof CataloProcessConfig>;
 
-export const config: Config & CommonConfig = {
-  ...commonsConfig,
-  ...Config.parse(process.env),
+export const config: CatalogProcessConfig = {
+  ...CataloProcessConfig.parse(process.env),
 };
