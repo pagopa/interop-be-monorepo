f16035e076b32787aa618eece5031d606002bec0
Simone Camito <32327779+MalpenZibo@users.noreply.github.com>
2023-10-25 17:46:31 +0200

    IMN 75 -  BKE ReadModel Client (#88)

--- packages/catalog-consumer/src/consumerService.ts
+++ packages/catalog-consumer/src/consumerService.ts
@@ -1,7 +1,9 @@
 import { match } from "ts-pattern";
-import { Collection, MongoClient } from "mongodb";
-import { logger, consumerConfig } from "pagopa-interop-commons";
-import { EService } from "pagopa-interop-models";
+import {
+  logger,
+  consumerConfig,
+  ReadModelRepository,
+} from "pagopa-interop-commons";
 import { EventEnvelope } from "./model/models.js";
 import {
   fromDescriptorV1,
@@ -9,24 +11,7 @@ import {
   fromEServiceV1,
 } from "./model/converter.js";
 
-const {
-  readModelDbUsername: username,
-  readModelDbPassword: password,
-  readModelDbHost: host,
-  readModelDbPort: port,
-  readModelDbName: database,
-} = consumerConfig();
-
-const mongoDBConectionURI = `mongodb://${username}:${password}@${host}:${port}`;
-const client = new MongoClient(mongoDBConectionURI, {
-  retryWrites: false,
-});
-
-const db = client.db(database);
-const eservices: Collection<{
-  data: EService | undefined;
-  metadata: { version: number };
-}> = db.collection("eservices", { ignoreUndefined: true });
+const { eservices } = ReadModelRepository.init(consumerConfig());
 
 export async function handleMessage(message: EventEnvelope): Promise<void> {
   logger.info(message);
---
+++ packages/commons/src/repositories/ReadModelRepository.ts
@@ -0,0 +1,47 @@
+import { EService, PersistentAgreement } from "pagopa-interop-models";
+import { Collection, Db, MongoClient } from "mongodb";
+import { ReadModelDbConfig } from "../index.js";
+
+export class ReadModelRepository {
+  private static instance: ReadModelRepository;
+
+  public eservices: Collection<{
+    data: EService | undefined;
+    metadata: { version: number };
+  }>;
+
+  public agreements: Collection<{
+    data: PersistentAgreement;
+    metadata: { version: number };
+  }>;
+
+  private client: MongoClient;
+  private db: Db;
+
+  private constructor({
+    readModelDbHost: host,
+    readModelDbPort: port,
+    readModelDbUsername: username,
+    readModelDbPassword: password,
+    readModelDbName: database,
+  }: ReadModelDbConfig) {
+    const mongoDBConnectionURI = `mongodb://${username}:${password}@${host}:${port}`;
+    this.client = new MongoClient(mongoDBConnectionURI, {
+      retryWrites: true,
+    });
+    this.db = this.client.db(database);
+    this.eservices = this.db.collection("eservices", { ignoreUndefined: true });
+    this.agreements = this.db.collection("agreements", {
+      ignoreUndefined: true,
+    });
+  }
+
+  public static init(config: ReadModelDbConfig): ReadModelRepository {
+    if (!ReadModelRepository.instance) {
+      // eslint-disable-next-line functional/immutable-data
+      ReadModelRepository.instance = new ReadModelRepository(config);
+    }
+
+    return ReadModelRepository.instance;
+  }
+}
