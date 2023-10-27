f11b2eadc3ee52e60db5e0e1e47ab28893aab66d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-10-27 17:51:58 +0200

    Implement tests

--- packages/attribute-registry-process/test/attributeRegistryService.test.ts
+++ packages/attribute-registry-process/test/attributeRegistryService.test.ts
@@ -1,7 +1,60 @@
 import { describe, expect, it } from "vitest";
+import { generateMock } from "@anatine/zod-mock";
 
-describe("TODO", () => {
-  it("TODO", () => {
-    expect(true).toBe(true);
-  });
+import {
+  AttributeKind,
+  AttributeTmp,
+  attributeDuplicate
+} from "pagopa-interop-models"
+
+import {
+  createDeclaredAttributeLogic
+} from "../src/services/attributeRegistryService.js"
+import * as api from "../src/model/generated/api.js";
+import { toCreateEventDeclaredAttributeAdded } from "../src/model/domain/toEvent.js";
+import {
+  toDeclaredAttributeV1
+} from "../src/model/domain/toEvent.js";
+
+const mockAttribute: AttributeTmp = generateMock(AttributeTmp)
+const mockDeclaredAttributeSeed = generateMock(api.schemas.AttributeSeed)
+describe("AttributeResistryService", () => {
+  describe("create a declared attribute", () => {
+    it("creates the attribute", async () => {
+      const attribute = {
+        ...mockAttribute,
+        kind: AttributeKind.Enum.Declared
+      }
+
+      const declaredAttributeLogic = createDeclaredAttributeLogic({
+        attributes: { results: [], totalCount: 0 },
+        apiDeclaredAttributeSeed: mockDeclaredAttributeSeed
+      });
+
+      expect(declaredAttributeLogic.kind).toBe(attribute.kind)
+
+      const event = toCreateEventDeclaredAttributeAdded(declaredAttributeLogic)
+      expect(event.event.data).toMatchObject({
+        attribute: {
+          ...toDeclaredAttributeV1(attribute),
+          id: event.streamId,
+          name: mockDeclaredAttributeSeed.name,
+          description: mockDeclaredAttributeSeed.description,
+          creationTime: (
+            event.event.data as unknown as { attribute: { creationTime: Date } }
+          ).attribute.creationTime,
+          code: undefined,
+          origin: undefined
+        }
+      })
+    })
+    it("returns an error if the attributes list is not empty", async () => {
+      expect(() =>
+        createDeclaredAttributeLogic({
+          attributes: { results: [mockAttribute], totalCount: 1 },
+          apiDeclaredAttributeSeed: mockDeclaredAttributeSeed
+        })
+      ).toThrowError(attributeDuplicate(mockDeclaredAttributeSeed.name));
+    });
+  })
 });
