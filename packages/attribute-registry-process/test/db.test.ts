52026cc3b655dbfe26a6a4291d5deb5e471ac960
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-07 16:19:00 +0100

    Try to add testcontainers

---
+++ packages/attribute-registry-process/test/db.test.ts
@@ -0,0 +1,6 @@
+import { describe, it } from "vitest";
+
+describe("database test", () => {
+  it("tests the connection - mongodb", async () => {});
+  it("tests the connection - postgresql", async () => {});
+});
--- packages/attribute-registry-process/vitest.config.ts
+++ packages/attribute-registry-process/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    setupFiles: ["dotenv/config"],
+    setupFiles: ["dotenv/config", "./setuptest"],
+    testTimeout: 60000,
   },
 });
