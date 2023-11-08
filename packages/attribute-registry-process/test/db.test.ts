d9e129b98722f9bcd79c845f7c3e7db84999b5cd
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-08 10:50:59 +0100

    Restructure and add sample tests

--- packages/attribute-registry-process/test/db.test.ts
+++ packages/attribute-registry-process/test/db.test.ts
@@ -1,6 +1,80 @@
-import { describe, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
+import { GenericContainer } from "testcontainers";
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
+import { config } from "../src/utilities/config.js";
+import { attributeRegistryService } from "../src/services/attributeRegistryService.js";
+import { readModelService } from "../src/services/readModelService.js";
+import { ReadModelRepository } from "pagopa-interop-commons";
+import { v4 as uuidv4 } from "uuid";
 
 describe("database test", () => {
-  it("tests the connection - mongodb", async () => {});
-  it("tests the connection - postgresql", async () => {});
+  beforeAll(async () => {
+    await new PostgreSqlContainer("postgres:14")
+      .withUsername(config.eventStoreDbUsername)
+      .withPassword(config.eventStoreDbPassword)
+      .withDatabase(config.eventStoreDbName)
+      .withCopyFilesToContainer([
+        {
+          source: "../../docker/event-store-init.sql",
+          target: "/docker-entrypoint-initdb.d/01-init.sql",
+        },
+      ])
+      .withExposedPorts({ container: 5432, host: config.eventStoreDbPort })
+      .start();
+
+    await new GenericContainer("mongo:6.0.7")
+      .withEnvironment({
+        MONGO_INITDB_DATABASE: "readmodel",
+        MONGO_INITDB_ROOT_USERNAME: "root",
+        MONGO_INITDB_ROOT_PASSWORD: "example",
+      })
+      .withExposedPorts({ container: 27017, host: 27017 })
+      .start();
+  });
+
+  it("should write on event-store", async () => {
+    const id = await attributeRegistryService.createDeclaredAttribute(
+      {
+        name: "name",
+        description: "description",
+      },
+      {
+        organizationId: "organization-id",
+        externalId: {
+          origin: "IPA",
+          value: "123456",
+        },
+        userId: uuidv4(),
+        userRoles: [],
+      }
+    );
+    expect(id).toBeDefined();
+  });
+
+  it("should write and read on readmodel", async () => {
+    const id = uuidv4();
+    await addOneAttribute(id);
+    const res = await readModelService.getAttributeById(id);
+    expect(res?.data.name).toBe("name");
+    expect(res?.data.description).toBe("description");
+  });
 });
+
+const addOneAttribute = (id: string) => {
+  const { attributes } = ReadModelRepository.init(config);
+
+  return attributes.insertOne({
+    data: {
+      id: id,
+      name: "name",
+      kind: "Certified",
+      description: "description",
+      creationTime: new Date(),
+      code: undefined,
+      origin: undefined,
+    },
+    metadata: {
+      version: 0,
+    },
+  });
+};
--- packages/attribute-registry-process/vitest.config.ts
+++ packages/attribute-registry-process/vitest.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    setupFiles: ["dotenv/config", "./setuptest"],
+    setupFiles: ["dotenv/config"],
     testTimeout: 60000,
+    hookTimeout: 60000,
   },
 });
