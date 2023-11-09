ce42ef42fc220b8f1e203083f4d4685016712398
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-09 16:01:06 +0100

    Add testcontainers in agreement-process

---
+++ packages/agreement-process/test/db.test.ts
@@ -0,0 +1,54 @@
+import { afterEach, beforeAll, describe, expect, it } from "vitest";
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
+import { config } from "../src/utilities/config.js";
+import { GenericContainer } from "testcontainers";
+import { ReadModelRepository, initDB } from "pagopa-interop-commons";
+
+describe("database test", async () => {
+  const postgresDB = initDB({
+    username: config.eventStoreDbUsername,
+    password: config.eventStoreDbPassword,
+    host: config.eventStoreDbHost,
+    port: config.eventStoreDbPort,
+    database: config.eventStoreDbName,
+    schema: config.eventStoreDbSchema,
+    useSSL: config.eventStoreDbUseSSL,
+  });
+
+  beforeAll(async () => {
+    await new PostgreSqlContainer("postgres:14")
+      .withUsername(config.eventStoreDbUsername)
+      .withPassword(config.eventStoreDbPassword)
+      .withDatabase(config.eventStoreDbName)
+      .withCopyFilesToContainer([
+        {
+          source: "../../docker/event-store-init.sql",
+          target: "/docker-entrypoint-initdb.d/01-init.sql",
+        },
+      ])
+      .withExposedPorts({ container: 5432, host: config.eventStoreDbPort })
+      .start();
+
+    await new GenericContainer("mongo:6.0.7")
+      .withEnvironment({
+        MONGO_INITDB_DATABASE: "readmodel",
+        MONGO_INITDB_ROOT_USERNAME: "root",
+        MONGO_INITDB_ROOT_PASSWORD: "example",
+      })
+      .withExposedPorts({ container: 27017, host: 27017 })
+      .start();
+  });
+
+  afterEach(async () => {
+    const { agreements } = ReadModelRepository.init(config);
+    await agreements.deleteMany({});
+
+    postgresDB.none("TRUNCATE TABLE agreement.events RESTART IDENTITY");
+  });
+
+  describe("TO DO", () => {
+    it("TO DO", () => {
+      expect(1).toBe(1);
+    });
+  });
+});
