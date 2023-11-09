511325ab30675a844a84fbdebacf0eede88d0696
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-09 17:49:46 +0100

    Test without the postgres container

--- packages/catalog-process/test/db.test.ts
+++ packages/catalog-process/test/db.test.ts
@@ -1,34 +1,34 @@
 import { afterEach, beforeAll, describe, expect, it } from "vitest";
-import { PostgreSqlContainer } from "@testcontainers/postgresql";
+// import { PostgreSqlContainer } from "@testcontainers/postgresql";
 import { GenericContainer } from "testcontainers";
-import { ReadModelRepository, initDB } from "pagopa-interop-commons";
+import { ReadModelRepository } from "pagopa-interop-commons";
 import { config } from "../src/utilities/config.js";
 
 describe("database test", async () => {
-  const postgresDB = initDB({
-    username: config.eventStoreDbUsername,
-    password: config.eventStoreDbPassword,
-    host: config.eventStoreDbHost,
-    port: config.eventStoreDbPort,
-    database: config.eventStoreDbName,
-    schema: config.eventStoreDbSchema,
-    useSSL: config.eventStoreDbUseSSL,
-  });
+  // const postgresDB = initDB({
+  //   username: config.eventStoreDbUsername,
+  //   password: config.eventStoreDbPassword,
+  //   host: config.eventStoreDbHost,
+  //   port: config.eventStoreDbPort,
+  //   database: config.eventStoreDbName,
+  //   schema: config.eventStoreDbSchema,
+  //   useSSL: config.eventStoreDbUseSSL,
+  // });
 
   beforeAll(async () => {
-    await new PostgreSqlContainer("postgres:14")
-      .withUsername(config.eventStoreDbUsername)
-      .withPassword(config.eventStoreDbPassword)
-      .withDatabase(config.eventStoreDbName)
-      .withCopyFilesToContainer([
-        {
-          source: "../../docker/event-store-init.sql",
-          target: "/docker-entrypoint-initdb.d/01-init.sql",
-        },
-      ])
-      .withExposedPorts({ container: 5432, host: config.eventStoreDbPort })
-      .withReuse()
-      .start();
+    // await new PostgreSqlContainer("postgres:14")
+    //   .withUsername(config.eventStoreDbUsername)
+    //   .withPassword(config.eventStoreDbPassword)
+    //   .withDatabase(config.eventStoreDbName)
+    //   .withCopyFilesToContainer([
+    //     {
+    //       source: "../../docker/event-store-init.sql",
+    //       target: "/docker-entrypoint-initdb.d/01-init.sql",
+    //     },
+    //   ])
+    //   .withExposedPorts({ container: 5432, host: config.eventStoreDbPort })
+    //   .withReuse()
+    //   .start();
 
     await new GenericContainer("mongo:6.0.7")
       .withEnvironment({
@@ -45,7 +45,7 @@ describe("database test", async () => {
     const { eservices } = ReadModelRepository.init(config);
     await eservices.deleteMany({});
 
-    await postgresDB.none("TRUNCATE TABLE catalog.events RESTART IDENTITY");
+    // await postgresDB.none("TRUNCATE TABLE catalog.events RESTART IDENTITY");
   });
 
   describe("TO DO", () => {
