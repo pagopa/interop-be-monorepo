19c418a2de111a72710ea88b7c4bd9486b97ab32
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-13 10:59:39 +0100

    Add postgres container and refactor

--- packages/catalog-process/test/db.test.ts
+++ packages/catalog-process/test/db.test.ts
@@ -1,63 +1,49 @@
-import { afterEach, beforeAll, afterAll, describe, expect, it } from "vitest";
-// import { PostgreSqlContainer } from "@testcontainers/postgresql";
-import { config } from "../src/utilities/config.js";
+import { afterEach, beforeAll, describe, expect, it } from "vitest";
 import {
   ReadModelRepository,
   getMongodbContainer,
+  getPostgreSqlContainer,
+  initDB,
 } from "pagopa-interop-commons";
-import { StartedTestContainer } from "testcontainers";
+import { config } from "../src/utilities/config.js";
 
 describe("database test", async () => {
-  // const postgresDB = initDB({
-  //   username: config.eventStoreDbUsername,
-  //   password: config.eventStoreDbPassword,
-  //   host: config.eventStoreDbHost,
-  //   port: config.eventStoreDbPort,
-  //   database: config.eventStoreDbName,
-  //   schema: config.eventStoreDbSchema,
-  //   useSSL: config.eventStoreDbUseSSL,
-  // });
-
-  let mongodbContainer: StartedTestContainer | undefined;
+  const postgresDB = initDB({
+    username: config.eventStoreDbUsername,
+    password: config.eventStoreDbPassword,
+    host: config.eventStoreDbHost,
+    port: config.eventStoreDbPort,
+    database: config.eventStoreDbName,
+    schema: config.eventStoreDbSchema,
+    useSSL: config.eventStoreDbUseSSL,
+  });
 
   beforeAll(async () => {
-    // await new PostgreSqlContainer("postgres:14")
-    //   .withUsername(config.eventStoreDbUsername)
-    //   .withPassword(config.eventStoreDbPassword)
-    //   .withDatabase(config.eventStoreDbName)
-    //   .withCopyFilesToContainer([
-    //     {
-    //       source: "../../docker/event-store-init.sql",
-    //       target: "/docker-entrypoint-initdb.d/01-init.sql",
-    //     },
-    //   ])
-    //   .withExposedPorts({ container: 5432, host: config.eventStoreDbPort })
-    //   .withReuse()
-    //   .start();
+    await getPostgreSqlContainer({
+      dbName: config.eventStoreDbName,
+      username: config.eventStoreDbUsername,
+      password: config.eventStoreDbPassword,
+      port: config.eventStoreDbPort,
+    }).start();
 
-    mongodbContainer = await getMongodbContainer()
-      .start()
-      .catch(() => undefined);
+    await getMongodbContainer({
+      dbName: config.readModelDbName,
+      username: config.readModelDbUsername,
+      password: config.readModelDbPassword,
+      port: config.readModelDbPort,
+    }).start();
   });
 
   afterEach(async () => {
     const { eservices } = ReadModelRepository.init(config);
     await eservices.deleteMany({});
 
-    // await postgresDB.none("TRUNCATE TABLE catalog.events RESTART IDENTITY");
+    await postgresDB.none("TRUNCATE TABLE catalog.events RESTART IDENTITY");
   });
 
   describe("TO DO", () => {
     it("TO DO", () => {
       expect(1).toBe(1);
     });
-  });
-
-  afterAll(async () => {
-    if (mongodbContainer !== undefined) {
-      await mongodbContainer.stop({
-        remove: true,
-      });
-    }
   });
 });
--- packages/commons/src/test/utils.ts
+++ packages/commons/src/test/utils.ts
@@ -1,11 +1,44 @@
 import { GenericContainer } from "testcontainers";
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
 
-export const getMongodbContainer = () =>
+export const getMongodbContainer = ({
+  dbName,
+  username,
+  password,
+  port,
+}: {
+  username: string;
+  password: string;
+  dbName: string;
+  port: number;
+}): GenericContainer =>
   new GenericContainer("mongo:6.0.7")
     .withEnvironment({
-      MONGO_INITDB_DATABASE: "readmodel",
-      MONGO_INITDB_ROOT_USERNAME: "root",
-      MONGO_INITDB_ROOT_PASSWORD: "example",
+      MONGO_INITDB_DATABASE: dbName,
+      MONGO_INITDB_ROOT_USERNAME: username,
+      MONGO_INITDB_ROOT_PASSWORD: password,
     })
-    .withExposedPorts({ container: 27017, host: 27017 })
-    .withReuse();
+    .withExposedPorts({ container: 27017, host: port });
+
+export const getPostgreSqlContainer = ({
+  dbName,
+  username,
+  password,
+  port,
+}: {
+  dbName: string;
+  username: string;
+  password: string;
+  port: number;
+}): PostgreSqlContainer =>
+  new PostgreSqlContainer("postgres:14")
+    .withDatabase(dbName)
+    .withUsername(username)
+    .withPassword(password)
+    .withCopyFilesToContainer([
+      {
+        source: "../../docker/event-store-init.sql",
+        target: "/docker-entrypoint-initdb.d/01-init.sql",
+      },
+    ])
+    .withExposedPorts({ container: 5432, host: port });
