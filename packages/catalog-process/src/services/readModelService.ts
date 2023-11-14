41406e99fc2220d9fad3da24a40309e1ed374f94
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-11-14 11:26:35 +0100

    Reformat catalog-process readModelService to implement `ReadModelFilter`

--- packages/catalog-process/src/services/readModelService.ts
+++ packages/catalog-process/src/services/readModelService.ts
@@ -3,6 +3,7 @@ import {
   logger,
   ReadModelRepository,
   readmodelDbConfig,
+  ReadModelFilter,
 } from "pagopa-interop-commons";
 import {
   DescriptorState,
@@ -23,11 +24,11 @@ import { Consumer, consumer } from "../model/domain/models.js";
 
 const { eservices, agreements } = ReadModelRepository.init(readmodelDbConfig);
 
-function arrayToFilter<T, F extends object>(
-  array: T[],
-  f: (array: T[]) => F
-): F | undefined {
-  return array.length > 0 ? f(array) : undefined;
+function getConditionalFilter<T>(
+  condition: boolean,
+  filter: ReadModelFilter<T>
+): ReadModelFilter<T> {
+  return condition ? filter : {};
 }
 
 export const readModelService = {
@@ -66,7 +67,7 @@ export const readModelService = {
       return emptyListResult;
     }
 
-    const nameFilter = name
+    const nameFilter: ReadModelFilter<EService> = name
       ? {
           "data.name": {
             $regex: name.exactMatch ? `^${name.value}$$` : name.value,
@@ -79,14 +80,16 @@ export const readModelService = {
       {
         $match: {
           ...nameFilter,
-          ...arrayToFilter(states, (states) => ({
+          ...getConditionalFilter(states.length > 0, {
             "data.descriptors": { $elemMatch: { state: { $in: states } } },
-          })),
-          ...arrayToFilter(ids, (ids) => ({ "data.id": { $in: ids } })),
-          ...arrayToFilter(producersIds, (producersIds) => ({
+          }),
+          ...getConditionalFilter(ids.length > 0, {
+            "data.id": { $in: ids },
+          }),
+          ...getConditionalFilter(producersIds.length > 0, {
             "data.producerId": { $in: producersIds },
-          })),
-        },
+          }),
+        } satisfies ReadModelFilter<EService>,
       },
       {
         $project: {
@@ -126,7 +129,7 @@ export const readModelService = {
     id: string
   ): Promise<WithMetadata<EService> | undefined> {
     const data = await eservices.findOne(
-      { "data.id": id },
+      { "data.id": id } satisfies ReadModelFilter<EService>,
       { projection: { data: true, metadata: true } }
     );
 
@@ -176,7 +179,7 @@ export const readModelService = {
               },
             },
           },
-        },
+        } satisfies ReadModelFilter<EService>,
       },
       {
         $lookup: {
@@ -283,19 +286,19 @@ export const readModelService = {
     const aggregationPipeline = [
       {
         $match: {
-          ...arrayToFilter(eservicesIds, (eservicesIds) => ({
+          ...getConditionalFilter(eservicesIds.length > 0, {
             "data.eserviceId": { $in: eservicesIds },
-          })),
-          ...arrayToFilter(consumersIds, (consumersIds) => ({
+          }),
+          ...getConditionalFilter(consumersIds.length > 0, {
             "data.consumerId": { $in: consumersIds },
-          })),
-          ...arrayToFilter(producersIds, (producersIds) => ({
+          }),
+          ...getConditionalFilter(producersIds.length > 0, {
             "data.producerId": { $in: producersIds },
-          })),
-          ...arrayToFilter(states, (states) => ({
+          }),
+          ...getConditionalFilter(states.length > 0, {
             "data.state": { $elemMatch: { state: { $in: states } } },
-          })),
-        },
+          }),
+        } satisfies ReadModelFilter<Agreement>,
       },
       {
         $project: {
