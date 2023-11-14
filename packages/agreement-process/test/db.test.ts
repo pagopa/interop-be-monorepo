61e3c16a0076933248a55bde4f6f0883a371e48c
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-14 11:50:53 +0100

    Update db tests

--- packages/agreement-process/test/db.test.ts
+++ packages/agreement-process/test/db.test.ts
@@ -1,45 +1,71 @@
+/* eslint-disable functional/no-let */
+/* eslint-disable functional/immutable-data */
 import { afterEach, beforeAll, describe, expect, it } from "vitest";
 import {
+  AgreementCollection,
+  EServiceCollection,
   ReadModelRepository,
+  TenantCollection,
   getMongodbContainer,
   getPostgreSqlContainer,
   initDB,
 } from "pagopa-interop-commons";
-
+import { IDatabase } from "pg-promise";
 import { config } from "../src/utilities/config.js";
+// import { ReadModelService } from "../src/services/readModelService.js";
+// import { AgreementService } from "../src/services/agreementService.js";
 
 describe("database test", async () => {
-  const postgresDB = initDB({
-    username: config.eventStoreDbUsername,
-    password: config.eventStoreDbPassword,
-    host: config.eventStoreDbHost,
-    port: config.eventStoreDbPort,
-    database: config.eventStoreDbName,
-    schema: config.eventStoreDbSchema,
-    useSSL: config.eventStoreDbUseSSL,
-  });
+  let agreements: AgreementCollection;
+  let eservices: EServiceCollection;
+  let tenants: TenantCollection;
+  // let readModelService: ReadModelService;
+  // let agreementService: AgreementService;
+  let postgresDB: IDatabase<unknown>;
 
   beforeAll(async () => {
-    await getPostgreSqlContainer({
+    const postgreSqlContainer = await getPostgreSqlContainer({
       dbName: config.eventStoreDbName,
       username: config.eventStoreDbUsername,
       password: config.eventStoreDbPassword,
-      port: config.eventStoreDbPort,
     }).start();
 
-    await getMongodbContainer({
+    const mongodbContainer = await getMongodbContainer({
       dbName: config.readModelDbName,
       username: config.readModelDbUsername,
       password: config.readModelDbPassword,
-      port: config.readModelDbPort,
     }).start();
+
+    config.readModelDbPort = mongodbContainer.getMappedPort(27017);
+    agreements = ReadModelRepository.init(config).agreements;
+    eservices = ReadModelRepository.init(config).eservices;
+    tenants = ReadModelRepository.init(config).tenants;
+
+    // readModelService = new ReadModelService(agreements, eservices, tenants);
+    // agreementService = new AgreementService(
+    //   readModelService,
+    //   postgreSqlContainer.getMappedPort(5432)
+    // );
+
+    postgresDB = initDB({
+      username: config.eventStoreDbUsername,
+      password: config.eventStoreDbPassword,
+      host: config.eventStoreDbHost,
+      port: postgreSqlContainer.getMappedPort(5432),
+      database: config.eventStoreDbName,
+      schema: config.eventStoreDbSchema,
+      useSSL: config.eventStoreDbUseSSL,
+    });
   });
 
   afterEach(async () => {
-    const { agreements } = ReadModelRepository.init(config);
     await agreements.deleteMany({});
+    await eservices.deleteMany({});
+    await tenants.deleteMany({});
 
     await postgresDB.none("TRUNCATE TABLE agreement.events RESTART IDENTITY");
+    await postgresDB.none("TRUNCATE TABLE catalog.events RESTART IDENTITY");
+    // await postgresDB.none("TRUNCATE TABLE tenant.events RESTART IDENTITY");
   });
 
   describe("TO DO", () => {
--- packages/catalog-process/test/db.test.ts
+++ packages/catalog-process/test/db.test.ts
@@ -1,44 +1,64 @@
+/* eslint-disable functional/no-let */
 import { afterEach, beforeAll, describe, expect, it } from "vitest";
 import {
+  AgreementCollection,
+  EServiceCollection,
   ReadModelRepository,
   getMongodbContainer,
   getPostgreSqlContainer,
   initDB,
 } from "pagopa-interop-commons";
+import { IDatabase } from "pg-promise";
 import { config } from "../src/utilities/config.js";
+// import { ReadModelService } from "../src/services/readModelService.js";
+// import { CatalogService } from "../src/services/catalogService.js";
 
 describe("database test", async () => {
-  const postgresDB = initDB({
-    username: config.eventStoreDbUsername,
-    password: config.eventStoreDbPassword,
-    host: config.eventStoreDbHost,
-    port: config.eventStoreDbPort,
-    database: config.eventStoreDbName,
-    schema: config.eventStoreDbSchema,
-    useSSL: config.eventStoreDbUseSSL,
-  });
+  let eservices: EServiceCollection;
+  let agreements: AgreementCollection;
+  // let readModelService: ReadModelService;
+  // let catalogService: CatalogService;
+  let postgresDB: IDatabase<unknown>;
 
   beforeAll(async () => {
-    await getPostgreSqlContainer({
+    const postgreSqlContainer = await getPostgreSqlContainer({
       dbName: config.eventStoreDbName,
       username: config.eventStoreDbUsername,
       password: config.eventStoreDbPassword,
-      port: config.eventStoreDbPort,
     }).start();
 
-    await getMongodbContainer({
+    const mongodbContainer = await getMongodbContainer({
       dbName: config.readModelDbName,
       username: config.readModelDbUsername,
       password: config.readModelDbPassword,
-      port: config.readModelDbPort,
     }).start();
+
+    config.readModelDbPort = mongodbContainer.getMappedPort(27017);
+    eservices = ReadModelRepository.init(config).eservices;
+    agreements = ReadModelRepository.init(config).agreements;
+    // readModelService = new ReadModelService(eservices, agreements);
+    // catalogService = new CatalogService(
+    //   readModelService,
+    //   postgreSqlContainer.getMappedPort(5432)
+    // );
+
+    postgresDB = initDB({
+      username: config.eventStoreDbUsername,
+      password: config.eventStoreDbPassword,
+      host: config.eventStoreDbHost,
+      port: postgreSqlContainer.getMappedPort(5432),
+      database: config.eventStoreDbName,
+      schema: config.eventStoreDbSchema,
+      useSSL: config.eventStoreDbUseSSL,
+    });
   });
 
   afterEach(async () => {
-    const { eservices } = ReadModelRepository.init(config);
     await eservices.deleteMany({});
+    await agreements.deleteMany({});
 
     await postgresDB.none("TRUNCATE TABLE catalog.events RESTART IDENTITY");
+    await postgresDB.none("TRUNCATE TABLE agreement.events RESTART IDENTITY");
   });
 
   describe("TO DO", () => {
