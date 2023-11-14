2e4d5102bc324acb300e4761bd4d15ab6202dee2
Roberto Gregnanin <gregnaninroberto@gmail.com>
2023-11-14 17:42:31 +0100

    Added get helper and refactor getTenantKind

--- packages/tenant-process/src/services/readModelService.ts
+++ packages/tenant-process/src/services/readModelService.ts
@@ -4,13 +4,60 @@ import {
   WithMetadata,
   Tenant,
   TenantAttribute,
+  Attribute,
+  ExternalId,
+  AttributeNotFound,
 } from "pagopa-interop-models";
 import { z } from "zod";
 import { config } from "../utilities/config.js";
 
 const { tenants, attributes } = ReadModelRepository.init(config);
 
+async function getAttributeByExternalCode(
+  code: string,
+  origin: string
+): Promise<WithMetadata<Attribute>> {
+  const data = await attributes.findOne(
+    { "data.code": code, "data.origin": origin },
+    { projection: { data: true, metadata: true } }
+  );
+
+  if (data) {
+    const result = z
+      .object({
+        metadata: z.object({ version: z.number() }),
+        data: Attribute,
+      })
+      .safeParse(data);
+
+    if (!result.success) {
+      logger.error(
+        `Unable to parse tenant item: result ${JSON.stringify(
+          result
+        )} - data ${JSON.stringify(data)} `
+      );
+
+      throw ErrorTypes.GenericError;
+    }
+
+    return {
+      data: result.data.data,
+      metadata: { version: result.data.metadata.version },
+    };
+  } else {
+    throw AttributeNotFound(`${origin}/${code}`);
+  }
+}
 export const readModelService = {
+  async getAttributes(
+    attributes: ExternalId[]
+  ): Promise<Array<WithMetadata<Attribute>>> {
+    const prova = attributes.map((attribute) =>
+      getAttributeByExternalCode(attribute.origin, attribute.value)
+    );
+    return Promise.all(prova);
+  },
+
   async getTenantById(
     tenantId: string
   ): Promise<WithMetadata<Tenant> | undefined> {
@@ -44,6 +91,7 @@ export const readModelService = {
     }
     return undefined;
   },
+
   async getAttributeById(
     attributeId: string
   ): Promise<WithMetadata<TenantAttribute> | undefined> {
--- packages/tenant-process/src/services/validators.ts
+++ packages/tenant-process/src/services/validators.ts
@@ -1,10 +1,13 @@
 // import { AuthData, userRoles } from "pagopa-interop-commons";
 import {
+  ExternalId,
   Tenant,
+  TenantKind,
   WithMetadata,
   //   operationForbidden,
   tenantIdNotFound,
 } from "pagopa-interop-models";
+import { match } from "ts-pattern";
 
 export function assertTenantExist(
   tenantId: string,
@@ -13,6 +16,44 @@ export function assertTenantExist(
   if (tenant === undefined) {
     throw tenantIdNotFound(tenantId);
   }
+}
+
+const PUBLIC_ADMINISTRATIONS_IDENTIFIER = "IPA";
+const CONTRACT_AUTHORITY_PUBLIC_SERVICES_MANAGERS = "SAG";
+const PUBLIC_SERVICES_MANAGERS = "L37";
+
+export function getTenantKind(
+  attributes: ExternalId[],
+  externalId: ExternalId
+): TenantKind {
+  if (
+    externalId.origin === PUBLIC_ADMINISTRATIONS_IDENTIFIER &&
+    attributes.some(
+      (attr) =>
+        attr.origin === PUBLIC_ADMINISTRATIONS_IDENTIFIER &&
+        (attr.value === PUBLIC_SERVICES_MANAGERS ||
+          attr.value === CONTRACT_AUTHORITY_PUBLIC_SERVICES_MANAGERS)
+    )
+  ) {
+    return TenantKind.enum.GSP;
+  } else {
+    return TenantKind.enum.PRIVATE;
+  }
+
+  // return match(externalId.origin)
+  //   .with(
+  //     PUBLIC_ADMINISTRATIONS_IDENTIFIER,
+  //     () => {
+  //       attributes.some(
+  //         (attr) =>
+  //           attr.origin === PUBLIC_ADMINISTRATIONS_IDENTIFIER &&
+  //           (attr.value === PUBLIC_SERVICES_MANAGERS ||
+  //             attr.value === CONTRACT_AUTHORITY_PUBLIC_SERVICES_MANAGERS)
+  //       );
+  //     },
+  //     () => TenantKind.enum.GSP
+  //   )
+  //   .otherwise(() => TenantKind.enum.PRIVATE);
 }
 
 // async function assertRequesterAllowed(
