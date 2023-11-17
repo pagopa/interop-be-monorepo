f302bfc9c142dba070b3c57a6a948bd72edc543c
Roberto Gregnanin <gregnaninroberto@gmail.com>
2023-11-17 16:16:32 +0100

    Used getTotalCount from ReadModelRepository

--- packages/tenant-process/src/services/readModelService.ts
+++ packages/tenant-process/src/services/readModelService.ts
@@ -1,33 +1,10 @@
 import { z } from "zod";
 import { logger, ReadModelRepository } from "pagopa-interop-commons";
-import {
-  Document,
-  ErrorTypes,
-  ListResult,
-  Tenant,
-} from "pagopa-interop-models";
-import { AggregationCursor, Filter } from "mongodb";
+import { ErrorTypes, ListResult, Tenant } from "pagopa-interop-models";
+import { Filter } from "mongodb";
 import { config } from "../utilities/config.js";
 
 const { tenants } = ReadModelRepository.init(config);
-
-async function getTotalCount(
-  query: AggregationCursor<Document>
-): Promise<number> {
-  const data = await query.toArray();
-  const result = z.array(z.object({ count: z.number() })).safeParse(data);
-
-  if (result.success) {
-    return result.data.length > 0 ? result.data[0].count : 0;
-  }
-
-  logger.error(
-    `Unable to get total count from aggregation pipeline: result ${JSON.stringify(
-      result
-    )} - data ${JSON.stringify(data)} `
-  );
-  throw ErrorTypes.GenericError;
-}
 
 function listTenantsFilters(
   name: string | undefined
@@ -82,8 +59,9 @@ export const readModelService = {
 
     return {
       results: result.data,
-      totalCount: await getTotalCount(
-        tenants.aggregate([...aggregationPipeline, { $count: "count" }])
+      totalCount: await ReadModelRepository.getTotalCount(
+        tenants,
+        aggregationPipeline
       ),
     };
   },
