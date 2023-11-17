223289f9c5369b948b57f7340f863e8272f234f3
Roberto Gregnanin <gregnaninroberto@gmail.com>
2023-11-17 12:53:59 +0100

    Added assertVerifiedAttributeOperationAllowed

--- packages/tenant-process/src/services/readModelService.ts
+++ packages/tenant-process/src/services/readModelService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { logger, ReadModelRepository } from "pagopa-interop-commons";
 import {
   ErrorTypes,
@@ -7,11 +8,107 @@ import {
   Attribute,
   ExternalId,
   AttributeNotFound,
+  AgreementState,
+  Agreement,
+  ListResult,
+  EService,
 } from "pagopa-interop-models";
 import { z } from "zod";
 import { config } from "../utilities/config.js";
 
-const { tenants, attributes } = ReadModelRepository.init(config);
+const { tenants, attributes, agreements, eservices } =
+  ReadModelRepository.init(config);
+
+async function listAgreements({
+  consumerId,
+  producerId,
+  states,
+}: {
+  consumerId: string;
+  producerId: string;
+  states: AgreementState[];
+}): Promise<ListResult<Agreement>> {
+  const aggregationPipeline = [
+    {
+      "data.consumerId": consumerId,
+    },
+    {
+      "data.producerId": producerId,
+    },
+    {
+      ...(states.length > 0 && {
+        "data.state": {
+          $in: states,
+        },
+      }),
+    },
+  ];
+
+  const data = await agreements.aggregate([...aggregationPipeline]).toArray();
+
+  const result = z.array(Agreement).safeParse(data.map((d) => d.data));
+  if (!result.success) {
+    logger.error(
+      `Unable to parse agreements items: result ${JSON.stringify(
+        result
+      )} - data ${JSON.stringify(data)} `
+    );
+
+    throw ErrorTypes.GenericError;
+  }
+
+  return {
+    results: result.data,
+    totalCount: await ReadModelRepository.getTotalCount(
+      eservices,
+      aggregationPipeline
+    ),
+  };
+}
+
+async function getAgreements(filters: {
+  consumerId: string;
+  producerId: string;
+  states: AgreementState[];
+}): Promise<ListResult<Agreement>> {
+  logger.info("Retrieving agreements in tenant process");
+  return await listAgreements(filters);
+}
+
+async function getEServiceById(
+  id: string
+): Promise<WithMetadata<EService> | undefined> {
+  const data = await eservices.findOne(
+    { "data.id": id },
+    { projection: { data: true, metadata: true } }
+  );
+
+  if (data) {
+    const result = z
+      .object({
+        metadata: z.object({ version: z.number() }),
+        data: EService,
+      })
+      .safeParse(data);
+
+    if (!result.success) {
+      logger.error(
+        `Unable to parse eservices item: result ${JSON.stringify(
+          result
+        )} - data ${JSON.stringify(data)} `
+      );
+
+      throw ErrorTypes.GenericError;
+    }
+
+    return {
+      data: result.data.data,
+      metadata: { version: result.data.metadata.version },
+    };
+  }
+
+  return undefined;
+}
 
 async function getAttributeByExternalCode(
   code: string,
@@ -47,15 +144,58 @@ async function getAttributeByExternalCode(
   } else {
     throw AttributeNotFound(`${origin}/${code}`);
   }
+
+  async function assertVerifiedAttributeOperationAllowed(
+    producerId: string,
+    consumerId: string,
+    attributeId: string,
+    states: AgreementState[]
+  ): Promise<void> {
+    const agreements = await getAgreements({ producerId, consumerId, states });
+    const descriptorIds = agreements.results.map(
+      (agreement) => agreement.descriptorId
+    );
+    const eServices = await Promise.all(
+      agreements.results.map((agreement) => {
+        const eService = getEServiceById(agreement.eserviceId);
+        if (eService === undefined) {
+          throw ErrorTypes.GenericError;
+        } else {
+          return eService;
+        }
+      })
+    );
+
+    const attributeIds = new Set<string>(
+      eServices.flatMap((eService) => {
+        if (eService === undefined) {
+          throw ErrorTypes.GenericError;
+        } else {
+          return eService.data.descriptors
+            .filter((descriptor) => descriptorIds.includes(descriptor.id))
+            .flatMap((descriptor) => descriptor.attributes.verified)
+            .flatMap((attributes) =>
+              attributes.map((attribute) => attribute.id)
+            );
+        }
+      })
+    );
+
+    if (!attributeIds.has(attributeId)) {
+      throw ErrorTypes.GenericError;
+    } else {
+      return Promise.resolve();
+    }
+  }
 }
 export const readModelService = {
   async getAttributes(
     attributes: ExternalId[]
   ): Promise<Array<WithMetadata<Attribute>>> {
-    const prova = attributes.map((attribute) =>
+    const promiseAttribute = attributes.map((attribute) =>
       getAttributeByExternalCode(attribute.origin, attribute.value)
     );
-    return Promise.all(prova);
+    return Promise.all(promiseAttribute);
   },
 
   async getTenantById(
