da3fec35245dc9148e2f6e277804dbbae058e102
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-11-17 15:53:43 +0100

    Updated tenant-consumer initialization

--- packages/tenant-consumer/src/index.ts
+++ packages/tenant-consumer/src/index.ts
@@ -1,53 +1,19 @@
-import { Kafka, KafkaMessage } from "kafkajs";
+import { KafkaMessage } from "kafkajs";
 import { logger, consumerConfig } from "pagopa-interop-commons";
-import { createMechanism } from "@jm18457/kafkajs-msk-iam-authentication-mechanism";
+import { runConsumer } from "kafka-iam-auth";
 import { decodeKafkaMessage } from "./model/models.js";
 import { handleMessage } from "./consumerService.js";
-
-const config = consumerConfig();
-
-const kafkaConfig = config.kafkaDisableAwsIamAuth
-  ? {
-      clientId: config.kafkaClientId,
-      brokers: [config.kafkaBrokers],
-      ssl: false,
-    }
-  : {
-      clientId: config.kafkaClientId,
-      brokers: [config.kafkaBrokers],
-      ssl: true,
-      sasl: createMechanism({ region: config.awsRegion }),
-    };
-
-const kafka = new Kafka(kafkaConfig);
-const consumer = kafka.consumer({ groupId: config.kafkaGroupId });
-await consumer.connect();
-
-function exitGracefully(): void {
-  consumer.disconnect().finally(() => {
-    logger.info("Consumer disconnected");
-    process.exit(0);
-  });
-}
-
-process.on("SIGINT", exitGracefully);
-process.on("SIGTERM", exitGracefully);
-
-await consumer.subscribe({
-  topics: ["event-store.tenant.events"],
-  fromBeginning: true,
-});
 
 async function processMessage(message: KafkaMessage): Promise<void> {
   try {
     await handleMessage(decodeKafkaMessage(message));
-
     logger.info("Read model was updated");
   } catch (e) {
     logger.error(`Error during message handling ${e}`);
   }
 }
 
-await consumer.run({
-  eachMessage: ({ message }) => processMessage(message),
-});
+const config = consumerConfig();
+await runConsumer(config, ["event-store.tenant.events"], processMessage).catch(
+  logger.error
+);
