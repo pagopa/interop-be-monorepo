e18e84219c48ecaade91fdbb9ba7b28bff383488
AsterITA <fabrizio.musella@gmail.com>
2023-11-21 12:34:57 +0100

    fix updateTenantAttribute

--- packages/tenant-process/src/services/readModelService.ts
+++ packages/tenant-process/src/services/readModelService.ts
@@ -262,40 +262,6 @@ export const readModelService = {
     return undefined;
   },
 
-  async getTenantAttributeById(
-    attributeId: string
-  ): Promise<WithMetadata<TenantAttribute> | undefined> {
-    const data = await tenants.findOne(
-      { "data.id": attributeId },
-      { projection: { data: true, metadata: true } }
-    );
-
-    if (data) {
-      const result = z
-        .object({
-          metadata: z.object({ version: z.number() }),
-          data: TenantAttribute,
-        })
-        .safeParse(data);
-
-      if (!result.success) {
-        logger.error(
-          `Unable to parse attribute item: result ${JSON.stringify(
-            result
-          )} - data ${JSON.stringify(data)} `
-        );
-
-        throw ErrorTypes.GenericError;
-      }
-
-      return {
-        data: result.data.data,
-        metadata: { version: result.data.metadata.version },
-      };
-    }
-    return undefined;
-  },
-
   async getTenantByName(
     name: string
   ): Promise<WithMetadata<Tenant> | undefined> {
