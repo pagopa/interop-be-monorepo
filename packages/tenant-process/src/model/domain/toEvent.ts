ed2a18f943626fbc75da9034cb54e0d165674bde
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-11-29 14:38:54 +0100

    IMN-94 - Added `tenant-process` scaffold (#98)
    Co-authored-by: Roberto Gregnanin <gregnaninroberto@gmail.com>
    Co-authored-by: AsterITA <fabrizio.musella@gmail.com>

---
+++ packages/tenant-process/src/model/domain/toEvent.ts
@@ -0,0 +1,131 @@
+import {
+  TenantMail,
+  TenantMailKind,
+  Tenant,
+  TenantAttribute,
+  TenantAttributeV1,
+  TenantFeature,
+  TenantFeatureV1,
+  TenantKind,
+  TenantKindV1,
+  TenantMailKindV1,
+  TenantMailV1,
+  TenantRevoker,
+  TenantRevokerV1,
+  TenantV1,
+  TenantVerifier,
+  TenantVerifierV1,
+  tenantMailKind,
+  tenantKind,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+
+export function toFeatureV1(feature: TenantFeature): TenantFeatureV1 {
+  return match<TenantFeature, TenantFeatureV1>(feature)
+    .with({ type: "Certifier" }, (feature) => ({
+      sealedValue: {
+        oneofKind: "certifier",
+        certifier: {
+          certifierId: feature.certifierId,
+        },
+      },
+    }))
+    .exhaustive();
+}
+
+export function toTenantVerifierV1(verifier: TenantVerifier): TenantVerifierV1 {
+  return {
+    id: verifier.id,
+    verificationDate: BigInt(verifier.verificationDate.getTime()),
+    expirationDate: verifier.expirationDate
+      ? BigInt(verifier.expirationDate?.getTime())
+      : undefined,
+    extensionDate: verifier.extensionDate
+      ? BigInt(verifier.extensionDate?.getTime())
+      : undefined,
+  };
+}
+
+export function toTenantRevokerV1(revoker: TenantRevoker): TenantRevokerV1 {
+  return {
+    id: revoker.id,
+    verificationDate: BigInt(revoker.verificationDate.getTime()),
+    expirationDate: revoker.expirationDate
+      ? BigInt(revoker.expirationDate?.getTime())
+      : undefined,
+    extensionDate: revoker.extensionDate
+      ? BigInt(revoker.extensionDate?.getTime())
+      : undefined,
+    revocationDate: BigInt(revoker.revocationDate.getTime()),
+  };
+}
+
+export function toAttributeV1(input: TenantAttribute): TenantAttributeV1 {
+  return match<TenantAttribute, TenantAttributeV1>(input)
+    .with({ type: "certified" }, (attribute) => ({
+      sealedValue: {
+        oneofKind: "certifiedAttribute",
+        certifiedAttribute: {
+          id: attribute.id,
+          assignmentTimestamp: BigInt(attribute.assignmentTimestamp.getTime()),
+          revocationTimestamp: attribute.revocationTimestamp
+            ? BigInt(attribute.revocationTimestamp?.getTime())
+            : undefined,
+        },
+      },
+    }))
+    .with({ type: "verified" }, (attribute) => ({
+      sealedValue: {
+        oneofKind: "verifiedAttribute",
+        verifiedAttribute: {
+          id: attribute.id,
+          assignmentTimestamp: BigInt(attribute.assignmentTimestamp.getTime()),
+          verifiedBy: attribute.verifiedBy.map(toTenantVerifierV1),
+          revokedBy: attribute.revokedBy.map(toTenantRevokerV1),
+        },
+      },
+    }))
+    .with({ type: "declared" }, (attribute) => ({
+      sealedValue: {
+        oneofKind: "declaredAttribute",
+        declaredAttribute: {
+          id: attribute.id,
+          assignmentTimestamp: BigInt(attribute.assignmentTimestamp.getTime()),
+        },
+      },
+    }))
+    .exhaustive();
+}
+
+export function toTenantMailV1(mail: TenantMail): TenantMailV1 {
+  return {
+    kind: toTenantMailKindV1(mail.kind),
+    address: mail.address,
+    createdAt: BigInt(mail.createdAt.getTime()),
+    description: mail.description ?? undefined,
+  };
+}
+
+export function toTenantMailKindV1(kind: TenantMailKind): TenantMailKindV1 {
+  return match(kind)
+    .with(tenantMailKind.ContactEmail, () => TenantMailKindV1.CONTACT_EMAIL)
+    .exhaustive();
+}
+
+export function toTenantKindV1(input: TenantKind): TenantKindV1 {
+  return match<TenantKind, TenantKindV1>(input)
+    .with(tenantKind.GSP, () => TenantKindV1.GSP)
+    .with(tenantKind.PA, () => TenantKindV1.PA)
+    .with(tenantKind.PRIVATE, () => TenantKindV1.PRIVATE)
+    .exhaustive();
+}
+
+export const toTenantV1 = (tenant: Tenant): TenantV1 => ({
+  ...tenant,
+  features: tenant.features.map(toFeatureV1),
+  attributes: tenant.attributes.map(toAttributeV1),
+  createdAt: BigInt(tenant.createdAt.getTime()),
+  updatedAt: tenant.updatedAt ? BigInt(tenant.updatedAt.getTime()) : undefined,
+  mails: tenant.mails.map(toTenantMailV1),
+  kind: tenant.kind ? toTenantKindV1(tenant.kind) : undefined,
+});
---
+++ packages/tenant-process/src/routers/TenantRouter.ts
@@ -0,0 +1,161 @@
+import { ZodiosEndpointDefinitions } from "@zodios/core";
+import { ZodiosRouter } from "@zodios/express";
+import {
+  ExpressContext,
+  userRoles,
+  ZodiosContext,
+  authorizationMiddleware,
+} from "pagopa-interop-commons";
+import { api } from "../model/generated/api.js";
+
+const tenantsRouter = (
+  ctx: ZodiosContext
+): ZodiosRouter<ZodiosEndpointDefinitions, ExpressContext> => {
+  const tenantsRouter = ctx.router(api.api);
+  const {
+    ADMIN_ROLE,
+    SECURITY_ROLE,
+    API_ROLE,
+    M2M_ROLE,
+    INTERNAL_ROLE,
+    SUPPORT_ROLE,
+  } = userRoles;
+  tenantsRouter
+    .get(
+      "/consumers",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        SECURITY_ROLE,
+        SUPPORT_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .get(
+      "/producers",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        SECURITY_ROLE,
+        SUPPORT_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .get(
+      "/tenants",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        SECURITY_ROLE,
+        SUPPORT_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .get(
+      "/tenants/:id",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        M2M_ROLE,
+        SECURITY_ROLE,
+        SUPPORT_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .get(
+      "/tenants/origin/:origin/code/:code",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        M2M_ROLE,
+        SECURITY_ROLE,
+        SUPPORT_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .get(
+      "/tenants/selfcare/:selfcareId",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        M2M_ROLE,
+        SECURITY_ROLE,
+        INTERNAL_ROLE,
+        SUPPORT_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/internal/tenants",
+      authorizationMiddleware([INTERNAL_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/tenants/:id",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/internal/origin/:tOrigin/externalId/:tExternalId/attributes/origin/:aOrigin/externalId/:aExternalId",
+      authorizationMiddleware([INTERNAL_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/m2m/tenants",
+      authorizationMiddleware([M2M_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/selfcare/tenants",
+      authorizationMiddleware([
+        ADMIN_ROLE,
+        API_ROLE,
+        SECURITY_ROLE,
+        INTERNAL_ROLE,
+      ]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/tenants/:tenantId/attributes/verified",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/tenants/:tenantId/attributes/verified/:attributeId",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/tenants/:tenantId/attributes/verified/:attributeId/verifier/:verifierId",
+      authorizationMiddleware([INTERNAL_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .post(
+      "/tenants/attributes/declared",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .delete(
+      "/internal/origin/:tOrigin/externalId/:tExternalId/attributes/origin/:aOrigin/externalId/:aExternalId",
+      authorizationMiddleware([INTERNAL_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .delete(
+      "/m2m/origin/:origin/externalId/:externalId/attributes/:code",
+      authorizationMiddleware([M2M_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .delete(
+      "/tenants/:tenantId/attributes/verified/:attributeId",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (_req, res) => res.status(501).send()
+    )
+    .delete(
+      "/tenants/attributes/declared/:attributeId",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (_req, res) => res.status(501).send()
+    );
+
+  return tenantsRouter;
+};
+export default tenantsRouter;
