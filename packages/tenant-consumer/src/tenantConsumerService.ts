b966e12f6fe937f618f793b6140560b5dff95c90
AsterITA <fabrizio.musella@gmail.com>
2023-11-30 12:30:39 +0100

    removed commented code

--- packages/tenant-consumer/src/tenantConsumerService.ts
+++ packages/tenant-consumer/src/tenantConsumerService.ts
@@ -1,12 +1,6 @@
 import { match } from "ts-pattern";
-import {
-  logger,
-  // consumerConfig,
-  // ReadModelRepository,
-} from "pagopa-interop-commons";
+import { logger } from "pagopa-interop-commons";
 import { EventEnvelope } from "./model/models.js";
-
-// const { tenants } = ReadModelRepository.init(consumerConfig());
 
 export async function handleMessage(message: EventEnvelope): Promise<void> {
   logger.info(message);
