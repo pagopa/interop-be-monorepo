4af35a3e13a7eccf16df1a9dfb7473759bc296a3
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-11-30 17:59:03 +0100

    IMN-76 Add post attribute routes in attribute-registry-process (#93)

--- packages/attribute-registry-process/src/services/attributeRegistryService.ts
+++ packages/attribute-registry-process/src/services/attributeRegistryService.ts
@@ -1,1 +1,123 @@
-// TODO
+import {
+  AuthData,
+  CreateEvent,
+  eventRepository,
+  initDB,
+} from "pagopa-interop-commons";
+import {
+  AttributeEvent,
+  Attribute,
+  WithMetadata,
+  attributeEventToBinaryData,
+  attributeKind,
+} from "pagopa-interop-models";
+import { v4 as uuidv4 } from "uuid";
+import { config } from "../utilities/config.js";
+import {
+  ApiDeclaredAttributeSeed,
+  ApiVerifiedAttributeSeed,
+} from "../model/types.js";
+import { toCreateEventAttributeAdded } from "../model/domain/toEvent.js";
+import {
+  attributeDuplicate,
+  originNotCompliant,
+} from "../model/domain/errors.js";
+import { readModelService } from "./readModelService.js";
+
+const repository = eventRepository(
+  initDB({
+    username: config.eventStoreDbUsername,
+    password: config.eventStoreDbPassword,
+    host: config.eventStoreDbHost,
+    port: config.eventStoreDbPort,
+    database: config.eventStoreDbName,
+    schema: config.eventStoreDbSchema,
+    useSSL: config.eventStoreDbUseSSL,
+  }),
+  attributeEventToBinaryData
+);
+
+export const attributeRegistryService = {
+  async createDeclaredAttribute(
+    apiDeclaredAttributeSeed: ApiDeclaredAttributeSeed,
+    authData: AuthData
+  ): Promise<string> {
+    if (authData.externalId.origin !== "IPA") {
+      throw originNotCompliant("IPA");
+    }
+
+    return repository.createEvent(
+      createDeclaredAttributeLogic({
+        attribute: await readModelService.getAttributeByName(
+          apiDeclaredAttributeSeed.name
+        ),
+        apiDeclaredAttributeSeed,
+      })
+    );
+  },
+  async createVerifiedAttribute(
+    apiVerifiedAttributeSeed: ApiVerifiedAttributeSeed,
+    authData: AuthData
+  ): Promise<string> {
+    if (authData.externalId.origin !== "IPA") {
+      throw originNotCompliant("IPA");
+    }
+
+    return repository.createEvent(
+      createVerifiedAttributeLogic({
+        attribute: await readModelService.getAttributeByName(
+          apiVerifiedAttributeSeed.name
+        ),
+        apiVerifiedAttributeSeed,
+      })
+    );
+  },
+};
+
+export function createDeclaredAttributeLogic({
+  attribute,
+  apiDeclaredAttributeSeed,
+}: {
+  attribute: WithMetadata<Attribute> | undefined;
+  apiDeclaredAttributeSeed: ApiDeclaredAttributeSeed;
+}): CreateEvent<AttributeEvent> {
+  if (attribute) {
+    throw attributeDuplicate(apiDeclaredAttributeSeed.name);
+  }
+
+  const newDeclaredAttribute: Attribute = {
+    id: uuidv4(),
+    kind: attributeKind.declared,
+    name: apiDeclaredAttributeSeed.name,
+    description: apiDeclaredAttributeSeed.description,
+    creationTime: new Date(),
+    code: undefined,
+    origin: undefined,
+  };
+
+  return toCreateEventAttributeAdded(newDeclaredAttribute);
+}
+
+export function createVerifiedAttributeLogic({
+  attribute,
+  apiVerifiedAttributeSeed,
+}: {
+  attribute: WithMetadata<Attribute> | undefined;
+  apiVerifiedAttributeSeed: ApiVerifiedAttributeSeed;
+}): CreateEvent<AttributeEvent> {
+  if (attribute) {
+    throw attributeDuplicate(apiVerifiedAttributeSeed.name);
+  }
+
+  const newVerifiedAttribute: Attribute = {
+    id: uuidv4(),
+    kind: attributeKind.verified,
+    name: apiVerifiedAttributeSeed.name,
+    description: apiVerifiedAttributeSeed.description,
+    creationTime: new Date(),
+    code: undefined,
+    origin: undefined,
+  };
+
+  return toCreateEventAttributeAdded(newVerifiedAttribute);
+}
