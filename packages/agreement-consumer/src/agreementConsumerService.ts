78dcebbfd2462e4f1baeca48b3b1144d48b81024
Simone Camito <zibo.camito@gmail.com>
2023-12-14 11:54:24 +0100

    add consumer Doc add event consumer

--- packages/agreement-consumer/src/agreementConsumerService.ts
+++ packages/agreement-consumer/src/agreementConsumerService.ts
@@ -5,7 +5,7 @@ import {
   ReadModelRepository,
 } from "pagopa-interop-commons";
 import { EventEnvelope } from "./model/models.js";
-import { fromAgreementV1 } from "./model/converter.js";
+import { fromAgreementV1, fromDocumentV1 } from "./model/converter.js";
 
 const { agreements } = ReadModelRepository.init(consumerConfig());
 
@@ -47,6 +47,26 @@ export async function handleMessage(message: EventEnvelope): Promise<void> {
             data: msg.data.agreement
               ? fromAgreementV1(msg.data.agreement)
               : undefined,
+            metadata: {
+              version: msg.version,
+            },
+          },
+        }
+      );
+    })
+    .with({ type: "AgreementConsumerDocumentAdded" }, async (msg) => {
+      await agreements.updateOne(
+        {
+          "data.id": msg.stream_id,
+          "metadata.version": { $lt: msg.version },
+        },
+        {
+          $push: {
+            "data.consumerDocuments": msg.data.document
+              ? fromDocumentV1(msg.data.document)
+              : undefined,
+          },
+          $set: {
             metadata: {
               version: msg.version,
             },
