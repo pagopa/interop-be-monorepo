319dc026d96d2638c0ecfabe5b13164d97af0039
Vittorio Caprio <vittorio.caprio@buildo.io>
2023-12-14 16:24:31 +0100

    Add HTTP status constants to errorMappers

--- packages/tenant-process/src/utilities/errorMappers.ts
+++ packages/tenant-process/src/utilities/errorMappers.ts
@@ -1,25 +1,31 @@
 /* eslint-disable sonarjs/no-identical-functions */
+import { constants } from "http2";
 import { ApiError, CommonErrorCodes } from "pagopa-interop-models";
 import { match } from "ts-pattern";
 import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
 
 type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
 
+const {
+  HTTP_STATUS_INTERNAL_SERVER_ERROR, // 500
+  HTTP_STATUS_NOT_FOUND, // 404
+} = constants;
+
 export const getTenantByIdErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
-    .with("tenantNotFound", () => 404)
-    .otherwise(() => 500);
+    .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const getTenantByExternalIdErrorMapper = (
   error: ApiError<ErrorCodes>
 ): number =>
   match(error.code)
-    .with("tenantNotFound", () => 404)
-    .otherwise(() => 500);
+    .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const getTenantBySelfcareIdErrorMapper = (
   error: ApiError<ErrorCodes>
 ): number =>
   match(error.code)
-    .with("tenantBySelfcateIdNotFound", () => 404)
-    .otherwise(() => 500);
+    .with("tenantBySelfcateIdNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
