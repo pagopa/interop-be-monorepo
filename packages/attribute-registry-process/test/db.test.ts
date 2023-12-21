27516ca227e7fbdcf39e47b60eaff32ba64a482d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2023-12-21 14:35:29 +0100

    Minor change

--- packages/attribute-registry-process/test/db.test.ts
+++ packages/attribute-registry-process/test/db.test.ts
@@ -324,12 +324,11 @@ describe("database test", () => {
       });
       describe("get an attribute by name", () => {
         it("should get the attribute if it exists", async () => {
-          const expectedAttribute = mockAttribute;
-          await addOneAttribute(expectedAttribute);
+          await addOneAttribute(mockAttribute);
           const attribute = await readModelService.getAttributeByName(
-            expectedAttribute.name
+            mockAttribute.name
           );
-          expect(attribute?.data).toEqual(expectedAttribute);
+          expect(attribute?.data).toEqual(mockAttribute);
         });
         it("should not get the attribute if it doesn't exist", async () => {
           const attribute = await readModelService.getAttributeByName("name");
