beb479d95c8645be97a187e225e755898209142e
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-01-02 11:39:31 +0100

    Remove comments http status code values

--- packages/catalog-process/src/utilities/errorMappers.ts
+++ packages/catalog-process/src/utilities/errorMappers.ts
@@ -7,11 +7,11 @@ import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
 type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
 
 const {
-  HTTP_STATUS_INTERNAL_SERVER_ERROR, // 500
-  HTTP_STATUS_BAD_REQUEST, // 400
-  HTTP_STATUS_FORBIDDEN, // 403
-  HTTP_STATUS_NOT_FOUND, // 404
-  HTTP_STATUS_CONFLICT, // 409
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_FORBIDDEN,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_CONFLICT,
 } = constants;
 
 export const createEServiceErrorMapper = (
--- packages/tenant-process/src/utilities/errorMappers.ts
+++ packages/tenant-process/src/utilities/errorMappers.ts
@@ -6,10 +6,7 @@ import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
 
 type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
 
-const {
-  HTTP_STATUS_INTERNAL_SERVER_ERROR, // 500
-  HTTP_STATUS_NOT_FOUND, // 404
-} = constants;
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = constants;
 
 export const getTenantByIdErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
