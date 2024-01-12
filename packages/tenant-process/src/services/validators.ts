4aa6dd0e94c33d9786cd0dcf097af67195cae2cc
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-01-12 17:40:03 +0100

    Removed functions

--- packages/tenant-process/src/services/validators.ts
+++ packages/tenant-process/src/services/validators.ts
@@ -1,25 +1,18 @@
 import { AuthData, userRoles } from "pagopa-interop-commons";
-import { match } from "ts-pattern";
 import {
-  Attribute,
-  ExternalId,
   Tenant,
   TenantAttribute,
-  TenantKind,
   TenantVerifier,
   WithMetadata,
   operationForbidden,
   tenantAttributeType,
-  tenantKind,
 } from "pagopa-interop-models";
 import {
   organizationNotFoundInVerifiers,
   verifiedAttributeNotFoundInTenant,
-  attributeNotFound,
   tenantNotFound,
   expirationDateNotFoundInVerifier,
 } from "../model/domain/errors.js";
-import { ReadModelService } from "./readModelService.js";
 
 export function assertTenantExists(
   tenantId: string,
@@ -64,40 +57,6 @@ export function assertExpirationDateExist(
   }
 }
 
-export function assertAttributeExists(
-  attributeId: string,
-  attributes: TenantAttribute[]
-): asserts attributes is NonNullable<TenantAttribute[]> {
-  if (!attributes.some((attr) => attr.id === attributeId)) {
-    throw attributeNotFound(attributeId);
-  }
-}
-
-const PUBLIC_ADMINISTRATIONS_IDENTIFIER = "IPA";
-const CONTRACT_AUTHORITY_PUBLIC_SERVICES_MANAGERS = "SAG";
-const PUBLIC_SERVICES_MANAGERS = "L37";
-
-export function getTenantKind(
-  attributes: ExternalId[],
-  externalId: ExternalId
-): TenantKind {
-  return match(externalId.origin)
-    .with(
-      PUBLIC_ADMINISTRATIONS_IDENTIFIER,
-      // condition to be satisfied
-      (origin) =>
-        attributes.some(
-          (attr) =>
-            attr.origin === origin &&
-            (attr.value === PUBLIC_SERVICES_MANAGERS ||
-              attr.value === CONTRACT_AUTHORITY_PUBLIC_SERVICES_MANAGERS)
-        ),
-      () => tenantKind.GSP
-    )
-    .with(PUBLIC_ADMINISTRATIONS_IDENTIFIER, () => tenantKind.PA)
-    .otherwise(() => tenantKind.PRIVATE);
-}
-
 async function assertRequesterAllowed(
   resourceId: string,
   requesterId: string
@@ -119,35 +78,4 @@ export async function assertResourceAllowed(
   if (!roles.includes(userRoles.INTERNAL_ROLE)) {
     throw operationForbidden;
   }
-}
-
-export async function getTenantKindLoadingCertifiedAttributes(
-  readModelService: ReadModelService,
-  attributes: TenantAttribute[],
-  externalId: ExternalId
-): Promise<TenantKind> {
-  function getCertifiedAttributesIds(attributes: TenantAttribute[]): string[] {
-    return attributes.flatMap((attr) =>
-      attr.type === "certified" ? attr.id : []
-    );
-  }
-
-  const convertAttributes = (
-    attributes: Array<WithMetadata<Attribute>>
-  ): ExternalId[] =>
-    attributes.flatMap((attr) => {
-      const origin = attr.data.origin;
-      const code = attr.data.code;
-
-      if (origin !== undefined && code !== undefined) {
-        return { origin, value: code } as ExternalId;
-      } else {
-        return [];
-      }
-    });
-
-  const attributesIds = getCertifiedAttributesIds(attributes);
-  const attrs = await readModelService.getAttributesById(attributesIds);
-  const extIds = convertAttributes(attrs);
-  return getTenantKind(extIds, externalId);
 }
