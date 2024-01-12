56bc610b28ccf164ba06d27f66ea2798508d39c7
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-01-12 17:46:50 +0100

    WIP

--- packages/tenant-consumer/src/consumerService.ts
+++ packages/tenant-consumer/src/consumerService.ts
@@ -1,6 +1,12 @@
 import { match } from "ts-pattern";
-import { logger } from "pagopa-interop-commons";
+import {
+  logger,
+  // consumerConfig,
+  // ReadModelRepository,
+} from "pagopa-interop-commons";
 import { EventEnvelope } from "./model/models.js";
+
+// const { tenants } = ReadModelRepository.init(consumerConfig());
 
 export async function handleMessage(message: EventEnvelope): Promise<void> {
   logger.info(message);
