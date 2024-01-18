d4f7cedfd589a699c354fe41067a5b5de655e55f
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-01-18 11:02:45 +0100

    Add missing error

--- packages/catalog-process/src/utilities/errorMappers.ts
+++ packages/catalog-process/src/utilities/errorMappers.ts
@@ -107,6 +107,7 @@ export const publishDescriptorErrorMapper = (
       "eServiceDescriptorNotFound",
       () => HTTP_STATUS_NOT_FOUND
     )
+    .with("notValidDescriptor", () => HTTP_STATUS_BAD_REQUEST)
     .with("eServiceDescriptorWithoutInterface", () => HTTP_STATUS_BAD_REQUEST)
     .with("operationForbidden", () => HTTP_STATUS_FORBIDDEN)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
