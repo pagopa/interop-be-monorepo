986ff04e5c08e68a569a2e049e5e0b6611ee8075
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-01-18 11:12:05 +0100

    Remove error

--- packages/catalog-process/src/utilities/errorMappers.ts
+++ packages/catalog-process/src/utilities/errorMappers.ts
@@ -159,6 +159,5 @@ export const archiveDescriptorErrorMapper = (
       "eServiceDescriptorNotFound",
       () => HTTP_STATUS_NOT_FOUND
     )
-    .with("notValidDescriptor", () => HTTP_STATUS_BAD_REQUEST)
     .with("operationForbidden", () => HTTP_STATUS_FORBIDDEN)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
