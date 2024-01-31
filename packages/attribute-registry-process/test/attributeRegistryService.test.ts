2fc97cdfc1927de214439ea7cbba13985c48699d
AsterITA <fabrizio.musella@gmail.com>
2024-01-31 11:14:29 +0100

    removed unit tests, renamed integration tests

--- packages/attribute-registry-process/test/attributeRegistryService.test.ts
+++ packages/attribute-registry-process/test/attributeRegistryService.test.ts
@@ -1,182 +1,175 @@
-import { describe, expect, it } from "vitest";
-import { generateMock } from "@anatine/zod-mock";
-
-import { AttributeKind, Attribute, WithMetadata } from "pagopa-interop-models";
-
+/* eslint-disable functional/no-let */
+import { beforeAll, afterEach, describe, expect, it } from "vitest";
+import { GenericContainer } from "testcontainers";
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
 import {
-  createCertifiedAttributeLogic,
-  createDeclaredAttributeLogic,
-  createInternalCertifiedAttributeLogic,
-  createVerifiedAttributeLogic,
+  AttributeCollection,
+  ReadModelRepository,
+  initDB,
+} from "pagopa-interop-commons";
+import { v4 as uuidv4 } from "uuid";
+import { Document } from "mongodb";
+import { IDatabase } from "pg-promise";
+import { config } from "../src/utilities/config.js";
+import {
+  AttributeRegistryService,
+  attributeRegistryServiceBuilder,
 } from "../src/services/attributeRegistryService.js";
-import * as api from "../src/model/generated/api.js";
 import {
-  toAttributeKindV1,
-  toAttributeV1,
-} from "../src/model/domain/toEvent.js";
-import { attributeDuplicate } from "../src/model/domain/errors.js";
+  ReadModelService,
+  readModelServiceBuilder,
+} from "../src/services/readModelService.js";
 
-const mockAttribute: Attribute = generateMock(Attribute);
-const addMetadata = (attribute: Attribute): WithMetadata<Attribute> => ({
-  data: attribute,
-  metadata: { version: 0 },
-});
-
-const mockAttributeSeed = generateMock(api.schemas.AttributeSeed);
-const mockCertifiedAttributeSeed = generateMock(
-  api.schemas.CertifiedAttributeSeed
-);
-const mockInternalCertifiedAttributeSeed = generateMock(
-  api.schemas.InternalCertifiedAttributeSeed
-);
-
-describe("AttributeResistryService", () => {
-  describe("create a declared attribute", () => {
-    it("creates the attribute", async () => {
-      const attribute = {
-        ...mockAttribute,
-        kind: AttributeKind.Enum.Declared,
-      };
-
-      const event = createDeclaredAttributeLogic({
-        attribute: undefined,
-        apiDeclaredAttributeSeed: mockAttributeSeed,
-      });
-      expect(event.event.type).toBe("AttributeAdded");
-      expect(event.event.data).toMatchObject({
-        attribute: {
-          ...toAttributeV1(attribute),
-          id: event.streamId,
-          name: mockAttributeSeed.name,
-          description: mockAttributeSeed.description,
-          kind: toAttributeKindV1(AttributeKind.Enum.Declared),
-          creationTime: (
-            event.event.data as unknown as { attribute: { creationTime: Date } }
-          ).attribute.creationTime,
-          code: undefined,
-          origin: undefined,
+describe("database test", () => {
+  let attributes: AttributeCollection;
+  let readModelService: ReadModelService;
+  let attributeRegistryService: AttributeRegistryService;
+  let postgresDB: IDatabase<unknown>;
+  beforeAll(async () => {
+    const postgreSqlContainer = await new PostgreSqlContainer("postgres:14")
+      .withUsername(config.eventStoreDbUsername)
+      .withPassword(config.eventStoreDbPassword)
+      .withDatabase(config.eventStoreDbName)
+      .withCopyFilesToContainer([
+        {
+          source: "../../docker/event-store-init.sql",
+          target: "/docker-entrypoint-initdb.d/01-init.sql",
         },
-      });
-    });
-    it("returns an error if the attributes list is not empty", async () => {
-      expect(() =>
-        createDeclaredAttributeLogic({
-          attribute: addMetadata(mockAttribute),
-          apiDeclaredAttributeSeed: mockAttributeSeed,
-        })
-      ).toThrowError(attributeDuplicate(mockAttributeSeed.name));
+      ])
+      .withExposedPorts(5432)
+      .start();
+
+    const mongodbContainer = await new GenericContainer("mongo:4.0.0")
+      .withEnvironment({
+        MONGO_INITDB_DATABASE: config.readModelDbName,
+        MONGO_INITDB_ROOT_USERNAME: config.readModelDbUsername,
+        MONGO_INITDB_ROOT_PASSWORD: config.readModelDbPassword,
+      })
+      .withExposedPorts(27017)
+      .start();
+
+    config.eventStoreDbPort = postgreSqlContainer.getMappedPort(5432);
+    config.readModelDbPort = mongodbContainer.getMappedPort(27017);
+    attributes = ReadModelRepository.init(config).attributes;
+    readModelService = readModelServiceBuilder(config);
+    attributeRegistryService = attributeRegistryServiceBuilder(
+      config,
+      readModelService
+    );
+
+    postgresDB = initDB({
+      username: config.eventStoreDbUsername,
+      password: config.eventStoreDbPassword,
+      host: config.eventStoreDbHost,
+      port: postgreSqlContainer.getMappedPort(5432),
+      database: config.eventStoreDbName,
+      schema: config.eventStoreDbSchema,
+      useSSL: config.eventStoreDbUseSSL,
     });
   });
-  describe("create a verified attribute", () => {
-    it("creates the attribute", async () => {
-      const attribute = {
-        ...mockAttribute,
-        kind: AttributeKind.Enum.Verified,
-      };
 
-      const event = createVerifiedAttributeLogic({
-        attribute: undefined,
-        apiVerifiedAttributeSeed: mockAttributeSeed,
-      });
-      expect(event.event.type).toBe("AttributeAdded");
-      expect(event.event.data).toMatchObject({
-        attribute: {
-          ...toAttributeV1(attribute),
-          id: event.streamId,
-          name: mockAttributeSeed.name,
-          description: mockAttributeSeed.description,
-          kind: toAttributeKindV1(AttributeKind.Enum.Verified),
-          creationTime: (
-            event.event.data as unknown as { attribute: { creationTime: Date } }
-          ).attribute.creationTime,
-          code: undefined,
-          origin: undefined,
-        },
-      });
-    });
-    it("returns an error if the attributes list is not empty", async () => {
-      expect(() =>
-        createVerifiedAttributeLogic({
-          attribute: addMetadata(mockAttribute),
-          apiVerifiedAttributeSeed: mockAttributeSeed,
-        })
-      ).toThrowError(attributeDuplicate(mockAttributeSeed.name));
-    });
+  afterEach(async () => {
+    await attributes.deleteMany({});
+    await postgresDB.none("TRUNCATE TABLE attribute.events RESTART IDENTITY");
   });
-  describe("create a certified attribute", () => {
-    it("creates the attribute", async () => {
-      const attribute = {
-        ...mockAttribute,
-        kind: AttributeKind.Enum.Certified,
-      };
 
-      const event = createCertifiedAttributeLogic({
-        attribute: undefined,
-        apiCertifiedAttributeSeed: mockCertifiedAttributeSeed,
-        certifier: "certifier",
-      });
-      expect(event.event.type).toBe("AttributeAdded");
-      expect(event.event.data).toMatchObject({
-        attribute: {
-          ...toAttributeV1(attribute),
-          id: event.streamId,
-          name: mockCertifiedAttributeSeed.name,
-          description: mockCertifiedAttributeSeed.description,
-          kind: toAttributeKindV1(AttributeKind.Enum.Certified),
-          creationTime: (
-            event.event.data as unknown as { attribute: { creationTime: Date } }
-          ).attribute.creationTime,
-          code: mockCertifiedAttributeSeed.code,
-          origin: "certifier",
+  describe("attribute creation", () => {
+    it("should write on event-store for the creation of a declared attribute", async () => {
+      const id = await attributeRegistryService.createDeclaredAttribute(
+        {
+          name: "name",
+          description: "description",
         },
-      });
-    });
-    it("returns an error if the attributes list is not empty", async () => {
-      expect(() =>
-        createCertifiedAttributeLogic({
-          attribute: addMetadata(mockAttribute),
-          apiCertifiedAttributeSeed: mockCertifiedAttributeSeed,
-          certifier: "certifier",
-        })
-      ).toThrowError(attributeDuplicate(mockCertifiedAttributeSeed.name));
-    });
-  });
-  describe("create an internal certified attribute", () => {
-    it("creates the attribute", async () => {
-      const attribute = {
-        ...mockAttribute,
-        kind: AttributeKind.Enum.Certified,
-      };
-
-      const event = createInternalCertifiedAttributeLogic({
-        attribute: undefined,
-        apiInternalCertifiedAttributeSeed: mockInternalCertifiedAttributeSeed,
-      });
-      expect(event.event.type).toBe("AttributeAdded");
-      expect(event.event.data).toMatchObject({
-        attribute: {
-          ...toAttributeV1(attribute),
-          id: event.streamId,
-          name: mockInternalCertifiedAttributeSeed.name,
-          description: mockInternalCertifiedAttributeSeed.description,
-          kind: toAttributeKindV1(AttributeKind.Enum.Certified),
-          creationTime: (
-            event.event.data as unknown as { attribute: { creationTime: Date } }
-          ).attribute.creationTime,
-          code: mockInternalCertifiedAttributeSeed.code,
-          origin: mockInternalCertifiedAttributeSeed.origin,
-        },
-      });
-    });
-    it("returns an error if the attributes list is not empty", async () => {
-      expect(() =>
-        createInternalCertifiedAttributeLogic({
-          attribute: addMetadata(mockAttribute),
-          apiInternalCertifiedAttributeSeed: mockInternalCertifiedAttributeSeed,
-        })
-      ).toThrowError(
-        attributeDuplicate(mockInternalCertifiedAttributeSeed.name)
+        {
+          organizationId: "organization-id",
+          externalId: {
+            origin: "IPA",
+            value: "123456",
+          },
+          userId: uuidv4(),
+          userRoles: [],
+        }
       );
+      expect(id).toBeDefined();
+
+      const writtenEvent = await postgresDB.one(
+        "SELECT * FROM attribute.events WHERE stream_id = $1",
+        [id]
+      );
+      expect(writtenEvent.stream_id).toBe(id);
+      expect(writtenEvent.version).toBe("0");
+      expect(writtenEvent.type).toBe("AttributeAdded");
+    });
+    it("should write on event-store for the creation of a verified attribute", async () => {
+      const id = await attributeRegistryService.createVerifiedAttribute(
+        {
+          name: "name",
+          description: "description",
+        },
+        {
+          organizationId: "organization-id",
+          externalId: {
+            origin: "IPA",
+            value: "123456",
+          },
+          userId: uuidv4(),
+          userRoles: [],
+        }
+      );
+      expect(id).toBeDefined();
+
+      const writtenEvent = await postgresDB.one(
+        "SELECT * FROM attribute.events WHERE stream_id = $1",
+        [id]
+      );
+      expect(writtenEvent.stream_id).toBe(id);
+      expect(writtenEvent.version).toBe("0");
+      expect(writtenEvent.type).toBe("AttributeAdded");
     });
   });
+
+  describe("get an attribute by id", () => {
+    it("should get the attribute if it exists", async () => {
+      const id = uuidv4();
+      await addOneAttribute(id);
+      const attribute = await readModelService.getAttributeById(id);
+      expect(attribute?.data.name).toBe("name");
+      expect(attribute?.data.description).toBe("description");
+    });
+    it("should not get the attribute if it doesn't exist", async () => {
+      const id = uuidv4();
+      const attribute = await readModelService.getAttributeById(id);
+      expect(attribute).toBeUndefined();
+    });
+  });
+
+  describe("get an attribute by name", () => {
+    it("should get the attribute if it exists", async () => {
+      const id = uuidv4();
+      await addOneAttribute(id);
+      const attribute = await readModelService.getAttributeByName("name");
+      expect(attribute?.data.name).toBe("name");
+      expect(attribute?.data.description).toBe("description");
+    });
+    it("should not get the attribute if it doesn't exist", async () => {
+      const attribute = await readModelService.getAttributeByName("name");
+      expect(attribute).toBeUndefined();
+    });
+  });
+
+  const addOneAttribute = (id: string): Promise<Document> =>
+    attributes.insertOne({
+      data: {
+        id,
+        name: "name",
+        kind: "Certified",
+        description: "description",
+        creationTime: new Date(),
+        code: undefined,
+        origin: undefined,
+      },
+      metadata: {
+        version: 0,
+      },
+    });
 });
