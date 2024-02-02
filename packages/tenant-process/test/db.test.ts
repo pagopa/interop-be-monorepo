bf8741debd04f5e32074949cf46c8cdf1fdccffa
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-02-02 12:57:48 +0100

    Refactor and fixed problem

--- packages/tenant-process/test/db.test.ts
+++ packages/tenant-process/test/db.test.ts
@@ -846,26 +846,6 @@ describe("database test", async () => {
       });
     });
     describe("getTenantById", () => {
-      const consumerId1 = uuidv4();
-      const consumerId2 = uuidv4();
-      const consumerId3 = uuidv4();
-      const mockTenant = getMockTenant();
-
-      const tenant1: Tenant = {
-        ...mockTenant,
-        id: consumerId1,
-        name: "A tenant1",
-      };
-      const tenant2: Tenant = {
-        ...mockTenant,
-        id: consumerId2,
-        name: "A tenant2",
-      };
-      const tenant3: Tenant = {
-        ...mockTenant,
-        id: consumerId3,
-        name: "A tenant3",
-      };
       it("should get the tenant by ID", async () => {
         await addOneTenant(tenant1, postgresDB, tenants);
         await addOneTenant(tenant2, postgresDB, tenants);
@@ -883,12 +863,16 @@ describe("database test", async () => {
         await addOneTenant(tenant1, postgresDB, tenants);
         await addOneTenant(tenant2, postgresDB, tenants);
         await addOneTenant(tenant3, postgresDB, tenants);
+        const selfcareId =
+          tenant1.selfcareId !== undefined ? tenant1.selfcareId : uuidv4();
         const tenantBySelfcareId = await readModelService.getTenantBySelfcareId(
           selfcareId
         );
         expect(tenantBySelfcareId?.data).toEqual(tenant1);
       });
       it("should not get the tenant by selfcareId if it isn't in DB", async () => {
+        const selfcareId =
+          tenant1.selfcareId !== undefined ? tenant1.selfcareId : uuidv4();
         const tenantBySelfcareId = await readModelService.getTenantBySelfcareId(
           selfcareId
         );
