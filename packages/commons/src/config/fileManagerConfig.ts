8a68292a1015ef6b8f5f5cd670ee2301517a018b
Eric Camellini <eric.camellini@gmail.com>
2024-02-07 13:04:15 +0100

    Implementing S3 File Manager with tests + using it in deleteDocument
    Adding docker-compose service for local minio server
    
    Adding file manager test container
    
    Committing wip
    
    Committing WIP
    
    Committing WIP
    
    Improving filemanager tests
    
    Adding test for delete document
    
    Fixing also Agreement compilation
    
    Implementing also storeBytes
    
    Trying to stop container after tests to fix CI
    
    Fixes after rebase
    
    Moving file manager tests to commons-test
    
    Removing unrelated PR change
    
    Adding comment
    
    Removing dependencies not needed in commons
    
    Update packages/commons/src/logging/loggerMiddleware.ts
    
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- packages/commons/src/config/fileManagerConfig.ts
+++ packages/commons/src/config/fileManagerConfig.ts
@@ -1,34 +1,39 @@
 import { z } from "zod";
 
-export const FileManagerConfig = z
-  .discriminatedUnion("MOCK_FILE_MANAGER", [
+const LocalMinioConfig = z
+  .discriminatedUnion("S3_LOCAL_MINIO", [
     z.object({
-      MOCK_FILE_MANAGER: z.literal("true"),
+      S3_LOCAL_MINIO: z.literal("true"),
+      S3_LOCAL_MINIO_HOST: z.string(),
+      S3_LOCAL_MINIO_PORT: z.coerce.number().min(1001),
     }),
     z.object({
-      MOCK_FILE_MANAGER: z.literal("false"),
-      S3_ACCESS_KEY_ID: z.string(),
-      S3_SECRET_ACCESS_KEY: z.string(),
-      S3_REGION: z.string(),
-    }),
-    z.object({
-      MOCK_FILE_MANAGER: z.undefined(),
-      S3_ACCESS_KEY_ID: z.string(),
-      S3_SECRET_ACCESS_KEY: z.string(),
-      S3_REGION: z.string(),
+      S3_LOCAL_MINIO: z.literal("false"),
     }),
   ])
   .transform((c) =>
-    c.MOCK_FILE_MANAGER === "false"
+    c.S3_LOCAL_MINIO === "true"
       ? {
-          mockFileManager: false as const,
-          s3AccessKeyId: c.S3_ACCESS_KEY_ID,
-          s3SecretAccessKey: c.S3_SECRET_ACCESS_KEY,
-          s3Region: c.S3_REGION,
+          s3LocalMinio: true as const,
+          s3LocalMinioHost: c.S3_LOCAL_MINIO_HOST,
+          s3LocalMinioPort: c.S3_LOCAL_MINIO_PORT,
         }
       : {
-          mockFileManager: true as const,
+          s3LocalMinio: false as const,
         }
   );
 
+const S3Config = z
+  .object({
+    S3_ACCESS_KEY_ID: z.string(),
+    S3_SECRET_ACCESS_KEY: z.string(),
+    S3_REGION: z.string(),
+  })
+  .transform((c) => ({
+    s3AccessKeyId: c.S3_ACCESS_KEY_ID,
+    s3SecretAccessKey: c.S3_SECRET_ACCESS_KEY,
+    s3Region: c.S3_REGION,
+  }));
+
+export const FileManagerConfig = z.intersection(LocalMinioConfig, S3Config);
 export type FileManagerConfig = z.infer<typeof FileManagerConfig>;
