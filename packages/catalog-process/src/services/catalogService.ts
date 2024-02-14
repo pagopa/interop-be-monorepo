69c01710cb786b914157f3b3b39ec0e5854a58cd
Eric Camellini <eric.camellini@gmail.com>
2024-02-14 11:50:57 +0100

    Implementing attributesIds filter in getEServices

--- packages/catalog-process/src/services/catalogService.ts
+++ packages/catalog-process/src/services/catalogService.ts
@@ -24,6 +24,7 @@ import {
   operationForbidden,
   unsafeBrandId,
   ListResult,
+  AttributeId,
 } from "pagopa-interop-models";
 import { match } from "ts-pattern";
 import {
@@ -801,7 +802,7 @@ export async function createDescriptorLogic({
   eserviceDescriptorSeed: EServiceDescriptorSeed;
   authData: AuthData;
   eService: WithMetadata<EService> | undefined;
-  getAttributesByIds: (attributesIds: string[]) => Promise<Attribute[]>;
+  getAttributesByIds: (attributesIds: AttributeId[]) => Promise<Attribute[]>;
 }): Promise<CreateEvent<EServiceEvent>> {
   assertEServiceExist(eserviceId, eService);
   assertRequesterAllowed(eService.data.producerId, authData.organizationId);
@@ -820,7 +821,9 @@ export async function createDescriptorLogic({
   ];
 
   if (attributesSeeds.length > 0) {
-    const attributesSeedsIds = attributesSeeds.map((attr) => attr.id);
+    const attributesSeedsIds: AttributeId[] = attributesSeeds.map((attr) =>
+      unsafeBrandId(attr.id)
+    );
     const attributes = await getAttributesByIds(attributesSeedsIds);
     const attributesIds = attributes.map((attr) => attr.id);
     for (const attributeSeedId of attributesSeedsIds) {
