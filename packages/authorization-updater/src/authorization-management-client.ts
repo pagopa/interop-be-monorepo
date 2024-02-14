ffc05ec811052382070484a8599193ecacc9a7ec
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-02-14 10:46:10 +0100

    Add AuthManagment client builder

--- packages/authorization-updater/src/authorization-management-client.ts
+++ packages/authorization-updater/src/authorization-management-client.ts
@@ -1,13 +1,11 @@
 import { ZodiosInstance } from "@zodios/core";
-import { createApiClient } from "./model/generated/api.js";
-import { api } from "./model/generated/api.js";
+import { authServiceConfig } from "pagopa-interop-commons";
+import { api, createApiClient } from "./model/generated/api.js";
 
 type Api = typeof api.api;
 
-export function getAuthMgmtClient(options: {
-  url: string;
-  apiKey: string;
-}): ZodiosInstance<Api> {
-  const { url } = options;
+export function buidAuthMgmtClient(): ZodiosInstance<Api> {
+  const { url } = authServiceConfig();
+
   return createApiClient(url);
 }
