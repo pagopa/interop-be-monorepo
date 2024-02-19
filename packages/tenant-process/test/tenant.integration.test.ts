132f68607af8dd08ba6bee4456f24a61ecea8930
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-02-19 17:12:44 +0100

    Removed unused import

--- packages/tenant-process/test/tenant.integration.test.ts
+++ packages/tenant-process/test/tenant.integration.test.ts
@@ -50,7 +50,6 @@ import {
   getMockCertifiedTenantAttribute,
   getMockDescriptor,
   getMockEService,
-  getMockRevokedBy,
   getMockTenant,
   getMockVerifiedBy,
   getMockVerifiedTenantAttribute,
