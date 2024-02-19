e9cf3d20b55c225f3f3c1cfd0a349be6b1fbee36
Eric Camellini <eric.camellini@gmail.com>
2024-02-19 16:46:45 +0100

    Using AWS CLI internals and env var to override local config without custom logic

--- packages/commons/src/config/fileManagerConfig.ts
+++ packages/commons/src/config/fileManagerConfig.ts
@@ -6,8 +6,6 @@ const S3CustomServerConfig = z
       S3_CUSTOM_SERVER: z.literal("true"),
       S3_SERVER_HOST: z.string(),
       S3_SERVER_PORT: z.coerce.number().min(1001),
-      S3_ACCESS_KEY_ID: z.string(),
-      S3_SECRET_ACCESS_KEY: z.string(),
     }),
     z.object({
       S3_CUSTOM_SERVER: z.literal("false"),
@@ -19,21 +17,11 @@ const S3CustomServerConfig = z
           s3CustomServer: true as const,
           s3ServerHost: c.S3_SERVER_HOST,
           s3ServerPort: c.S3_SERVER_PORT,
-          s3AccessKeyId: c.S3_ACCESS_KEY_ID,
-          s3SecretAccessKey: c.S3_SECRET_ACCESS_KEY,
         }
       : {
           s3CustomServer: false as const,
         }
   );
 
-const S3Config = z
-  .object({
-    AWS_REGION: z.string(),
-  })
-  .transform((c) => ({
-    s3Region: c.AWS_REGION,
-  }));
-
-export const FileManagerConfig = z.intersection(S3CustomServerConfig, S3Config);
+export const FileManagerConfig = S3CustomServerConfig;
 export type FileManagerConfig = z.infer<typeof FileManagerConfig>;
