be6c7b097a5d440148f731683c051783bb12f53b
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-02-21 12:52:02 +0100

    Add Keyholder service for KID

---
+++ packages/commons/src/config/signerConfig.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { KID } from "../auth/keys/keys.js";
+
+export const SignerConfig = z
+  .object({
+    KMS_MAX_CONCURRENCY: z.number(),
+    KMS_MAX_ACQUISITION_TIMEOUT_SECONDS: z.number(),
+    EC_KEYS_IDENTIFIERS: z
+      .string()
+      .transform((val) => val?.split(","))
+      .pipe(z.array(KID)),
+    RSA_KEYS_IDENTIFIERS: z
+      .string()
+      .transform((val) => val?.split(","))
+      .pipe(z.array(KID)),
+  })
+  .transform((c) => ({
+    maxConcurrency: c.KMS_MAX_CONCURRENCY,
+    maxAcquisitionTimeoutSeconds: c.KMS_MAX_ACQUISITION_TIMEOUT_SECONDS,
+    ecKeysIdentifiers: c.EC_KEYS_IDENTIFIERS,
+    rsaKeysIdentifiers: c.RSA_KEYS_IDENTIFIERS,
+  }));
+
+export type SignerConfig = z.infer<typeof SignerConfig>;
+
+export const signerConfig: () => SignerConfig = () =>
+  SignerConfig.parse(process.env);
