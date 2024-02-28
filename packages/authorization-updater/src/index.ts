66a74c67b43eb0f566101f0d24d88f3af69556c3
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-02-28 15:44:50 +0100

    Implements Authorization service logic

--- packages/authorization-updater/src/index.ts
+++ packages/authorization-updater/src/index.ts
@@ -1,10 +1,45 @@
 import { runConsumer } from "kafka-iam-auth";
+import { match } from "ts-pattern";
 import { EachMessagePayload } from "kafkajs";
 import {
   messageDecoderSupplier,
   kafkaConsumerConfig,
   logger,
 } from "pagopa-interop-commons";
+import {
+  Descriptor,
+  EServiceV2,
+  fromEServiceV2,
+  missingMessageDataError,
+  EServiceId,
+} from "pagopa-interop-models";
+import { authorizationServiceBuilder } from "./authorization-service.js";
+
+const authService = authorizationServiceBuilder();
+const config = kafkaConsumerConfig();
+
+const getDescriptorFromMsg = (msg: {
+  data: {
+    descriptorId: string;
+    eservice?: EServiceV2;
+  };
+}): {
+  eserviceId: EServiceId;
+  descriptor: Descriptor;
+} => {
+  if (!msg.data.eservice) {
+    throw missingMessageDataError("eservice", "EServiceDescriptorPublished");
+  }
+
+  const eservice = fromEServiceV2(msg.data.eservice);
+  const descriptor = eservice.descriptors.find(() => msg.data.descriptorId);
+
+  if (!descriptor) {
+    throw missingMessageDataError("descriptor", "EServiceDescriptorPublished");
+  }
+
+  return { eserviceId: eservice.id, descriptor };
+};
 
 async function processMessage({
   topic,
@@ -13,13 +48,49 @@ async function processMessage({
 }: EachMessagePayload): Promise<void> {
   try {
     const messageDecoder = messageDecoderSupplier(topic);
-    const decodedMessage = messageDecoder(message);
+    const decodedMsg = messageDecoder(message);
 
-    // TODO : update authorization to AuthjorizationManagement service
+    match(decodedMsg)
+      .with(
+        {
+          event_version: 2,
+          type: "EServiceDescriptorPublished",
+        },
+        {
+          event_version: 2,
+          type: "EServiceDescriptorActivated",
+        },
+        async (msg) => {
+          const data = getDescriptorFromMsg(msg);
+          await authService.updateEServiceState(
+            "ACTIVE",
+            data.descriptor.id,
+            data.eserviceId,
+            data.descriptor.audience,
+            data.descriptor.voucherLifespan
+          );
+        }
+      )
+      .with(
+        {
+          event_version: 2,
+          type: "EServiceDescriptorSuspended",
+        },
+        async (msg) => {
+          const data = getDescriptorFromMsg(msg);
+          await authService.updateEServiceState(
+            "INACTIVE",
+            data.descriptor.id,
+            data.eserviceId,
+            data.descriptor.audience,
+            data.descriptor.voucherLifespan
+          );
+        }
+      );
 
     logger.info(
       `Authorization updated after ${JSON.stringify(
-        decodedMessage.type
+        decodedMsg.type
       )} event - Partition number: ${partition} - Offset: ${message.offset}`
     );
   } catch (e) {
@@ -29,5 +100,4 @@ async function processMessage({
   }
 }
 
-const config = kafkaConsumerConfig();
 await runConsumer(config, processMessage).catch(logger.error);
