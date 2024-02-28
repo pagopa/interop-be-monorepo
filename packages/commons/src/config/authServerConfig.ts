1b9648de8d60638821e2ba627176b98d195e4918
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-02-28 10:36:46 +0100

    rename env var for authorization management service url

--- packages/commons/src/config/authServerConfig.ts
+++ packages/commons/src/config/authServerConfig.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 export const AuthServiceConfig = z
   .object({
-    AUTH_SERVICE_URL: z.string(),
+    AUTH_MNGT_SERVICE_URL: z.string(),
   })
   .transform((c) => ({
-    url: c.AUTH_SERVICE_URL,
+    url: c.AUTH_MNGT_SERVICE_URL,
   }));
 
 export type AuthServiceConfig = z.infer<typeof AuthServiceConfig>;
