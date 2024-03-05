c0358ba9e4447801116f8b4929a9789d1251059c
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-03-05 14:07:56 +0100

    Add producerAllowedOrigins

--- packages/attribute-registry-process/src/utilities/config.ts
+++ packages/attribute-registry-process/src/utilities/config.ts
@@ -5,8 +5,13 @@ import {
 } from "pagopa-interop-commons";
 import { z } from "zod";
 
-const AttributeRegistryConfig =
-  CommonConfig.and(ReadModelDbConfig).and(EventStoreConfig);
+const AttributeRegistryConfig = CommonConfig.and(ReadModelDbConfig)
+  .and(EventStoreConfig)
+  .and(
+    z.object({ PRODUCER_ALLOWED_ORIGINS: z.string() }).transform((c) => ({
+      producerAllowedOrigins: c.PRODUCER_ALLOWED_ORIGINS.split(","),
+    }))
+  );
 
 export type AttributeRegistryConfig = z.infer<typeof AttributeRegistryConfig>;
 
