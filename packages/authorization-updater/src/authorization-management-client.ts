987b8a1989cbdf89536288c38d9fb6a43fbb8e30
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-03-07 14:27:58 +0100

    Rename env var to AUTHORIZATION_MANAGEMENT_URL

--- packages/authorization-updater/src/authorization-management-client.ts
+++
@@ -1,8 +0,0 @@
-import { ZodiosInstance } from "@zodios/core";
-import { authServiceConfig } from "pagopa-interop-commons";
-import { api, createApiClient } from "./model/generated/api.js";
-
-type Api = typeof api.api;
-
-export const buildAuthMgmtClient = (): ZodiosInstance<Api> =>
-  createApiClient(authServiceConfig().url);
