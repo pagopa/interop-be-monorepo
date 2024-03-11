52eac3255d0adee2ea15155a977a5fb91875524d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-03-11 09:52:12 +0100

    IMN-314 Assign attributes during updateDraftDescriptor in catalog-process (#262)

--- packages/catalog-process/src/routers/EServiceRouter.ts
+++ packages/catalog-process/src/routers/EServiceRouter.ts
@@ -44,6 +44,7 @@ import {
   publishDescriptorErrorMapper,
   suspendDescriptorErrorMapper,
   updateDescriptorErrorMapper,
+  updateDraftDescriptorErrorMapper,
   updateEServiceErrorMapper,
 } from "../utilities/errorMappers.js";
 
@@ -400,7 +401,10 @@ const eservicesRouter = (
             .json(eServiceToApiEService(updatedEService))
             .end();
         } catch (error) {
-          const errorRes = makeApiProblem(error, updateDescriptorErrorMapper);
+          const errorRes = makeApiProblem(
+            error,
+            updateDraftDescriptorErrorMapper
+          );
           return res.status(errorRes.status).json(errorRes).end();
         }
       }
