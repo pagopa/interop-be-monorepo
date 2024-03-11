25862b49b5744861d7dce11ec792a38b227cd984
Eric Camellini <eric.camellini@gmail.com>
2024-03-11 17:53:55 +0100

    IMN-252 - Risk Analysis Validation (#238)

---
+++ packages/commons/src/risk-analysis/models.ts
@@ -0,0 +1,60 @@
+import { DataType } from "./rules/riskAnalysisFormRules.js";
+import { RiskAnalysisValidationIssue } from "./riskAnalysisErrors.js";
+
+export type RiskAnalysisValidationInvalid = {
+  type: "invalid";
+  issues: RiskAnalysisValidationIssue[];
+};
+
+export type RiskAnalysisValidationValid<T> = {
+  type: "valid";
+  value: T;
+};
+
+export type RiskAnalysisValidationResult<T> =
+  | RiskAnalysisValidationValid<T>
+  | RiskAnalysisValidationInvalid;
+
+export type RiskAnalysisFormToValidate = {
+  version: string;
+  answers: Record<string, string[]>;
+};
+
+export type RiskAnalysisValidatedMultiAnswer = {
+  key: string;
+  values: string[];
+};
+
+export type RiskAnalysisValidatedSingleAnswer = {
+  key: string;
+  value?: string;
+};
+
+export type RiskAnalysisValidatedSingleOrMultiAnswer =
+  | {
+      type: "single";
+      answer: RiskAnalysisValidatedSingleAnswer;
+    }
+  | {
+      type: "multi";
+      answer: RiskAnalysisValidatedMultiAnswer;
+    };
+
+export type RiskAnalysisValidatedForm = {
+  version: string;
+  singleAnswers: RiskAnalysisValidatedSingleAnswer[];
+  multiAnswers: RiskAnalysisValidatedMultiAnswer[];
+};
+
+export type ValidationRuleDependency = {
+  fieldName: string;
+  fieldValue: string;
+};
+
+export type ValidationRule = {
+  fieldName: string;
+  dataType: DataType;
+  required: boolean;
+  dependencies: ValidationRuleDependency[];
+  allowedValues: Set<string> | undefined;
+};
