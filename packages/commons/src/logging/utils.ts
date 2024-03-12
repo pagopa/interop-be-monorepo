64c25d950a0cfb19de32a6803e7c256dc0d84fb8
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-03-12 17:54:54 +0100

    changed bigIntReplacer function

--- packages/commons/src/logging/utils.ts
+++ packages/commons/src/logging/utils.ts
@@ -1,6 +1,7 @@
 export function bigIntReplacer(_key: string, value: unknown): unknown {
   if (typeof value === "bigint") {
-    return value.toString() + "n";
+    const date = new Date(Number(value));
+    return date.toISOString();
   }
   return value;
 }
