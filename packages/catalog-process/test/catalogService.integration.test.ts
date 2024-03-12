aa653042a9255b4221d32f532bbe6de9a24a5ab2
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-03-12 12:20:31 +0100

    Minor fix

--- packages/catalog-process/test/catalogService.integration.test.ts
+++ packages/catalog-process/test/catalogService.integration.test.ts
@@ -1549,7 +1549,7 @@ describe("database test", async () => {
             getMockAuthData(eservice.producerId)
           )
         ).rejects.toThrowError(
-          notValidDescriptor(mockDescriptor.id, descriptorState.published)
+          notValidDescriptor(descriptor.id, descriptorState.published)
         );
       });
       it("should throw notValidDescriptor if the eservice is in deprecated state", async () => {
@@ -1573,7 +1573,7 @@ describe("database test", async () => {
             getMockAuthData(eservice.producerId)
           )
         ).rejects.toThrowError(
-          notValidDescriptor(mockDescriptor.id, descriptorState.deprecated)
+          notValidDescriptor(descriptor.id, descriptorState.deprecated)
         );
       });
       it("should throw notValidDescriptor if the eservice is in suspended state", async () => {
@@ -1597,7 +1597,7 @@ describe("database test", async () => {
             getMockAuthData(eservice.producerId)
           )
         ).rejects.toThrowError(
-          notValidDescriptor(mockDescriptor.id, descriptorState.suspended)
+          notValidDescriptor(descriptor.id, descriptorState.suspended)
         );
       });
       it("should throw notValidDescriptor if the eservice is in archived state", async () => {
@@ -1621,7 +1621,7 @@ describe("database test", async () => {
             getMockAuthData(eservice.producerId)
           )
         ).rejects.toThrowError(
-          notValidDescriptor(mockDescriptor.id, descriptorState.archived)
+          notValidDescriptor(descriptor.id, descriptorState.archived)
         );
       });
     });
