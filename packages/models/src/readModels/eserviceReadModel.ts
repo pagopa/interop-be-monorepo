9f3b0379ecdf7802cb7d0680ab2b280e490eb7d3
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-03-14 12:49:03 +0100

    IMN-315 Readmodel date to iso string (#260)

---
+++ packages/models/src/readModels/eserviceReadModel.ts
@@ -0,0 +1,41 @@
+/* 
+ --- Technical NOTE ---
+  ISSUE https://pagopa.atlassian.net/browse/IMN-315
+  This model is used to guarantee compatibility with the old read model version,
+  the old service read it using ISO string for all date fields, 
+  while the new event version will use the Date type.
+  After the complete services migration, this definition should be removed.
+  Refer to the zod extend method for more implementation details https://zod.dev/?id=extend
+*/
+
+import { z } from "zod";
+import { Descriptor, Document, EService } from "../eservice/eservice.js";
+import { RiskAnalysis } from "../risk-analysis/riskAnalysis.js";
+
+export const DocumentReadModel = Document.extend({
+  uploadDate: z.string().datetime(),
+});
+export type DocumentReadModel = z.infer<typeof DocumentReadModel>;
+
+export const RiskAnalysisReadModel = RiskAnalysis.extend({
+  createdAt: z.string().datetime(),
+});
+export type RiskAnalysisReadModel = z.infer<typeof RiskAnalysisReadModel>;
+
+export const DescriptorReadModel = Descriptor.extend({
+  interface: DocumentReadModel.optional(),
+  docs: z.array(DocumentReadModel),
+  createdAt: z.string().datetime(),
+  publishedAt: z.string().datetime().optional(),
+  suspendedAt: z.string().datetime().optional(),
+  deprecatedAt: z.string().datetime().optional(),
+  archivedAt: z.string().datetime().optional(),
+});
+export type DescriptorReadModel = z.infer<typeof DescriptorReadModel>;
+
+export const EServiceReadModel = EService.extend({
+  descriptors: z.array(DescriptorReadModel),
+  createdAt: z.string().datetime(),
+  riskAnalysis: z.array(RiskAnalysisReadModel),
+});
+export type EServiceReadModel = z.infer<typeof EServiceReadModel>;
