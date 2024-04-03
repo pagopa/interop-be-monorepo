9a1e2c77edc34e7b1ed4582483d74c42ce9c1eca
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-03 12:38:07 +0200

    Update index.ts

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -25,7 +25,8 @@ export * from "./risk-analysis/riskAnalysis.js";
 
 export * from "./tenant/tenant.js";
 export * from "./tenant/tenantEvents.js";
-export * from "./tenant/protobufConverter.js";
+export * from "./tenant/protobufConverterFromV1.js";
+export * from "./tenant/protobufConverterFromV2.js";
 
 export * from "./user/user.js";
 
@@ -51,5 +52,7 @@ export * from "./gen/v1/agreement/events.js";
 export * from "./gen/v1/agreement/state.js";
 export * from "./gen/v1/tenant/tenant.js";
 export * from "./gen/v1/tenant/events.js";
+export * from "./gen/v2/tenant/tenant.js";
+export * from "./gen/v2/tenant/events.js";
 export * from "./gen/v1/attribute/attribute.js";
 export * from "./gen/v1/attribute/events.js";
