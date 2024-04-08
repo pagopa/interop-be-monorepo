2ed63dd51a2dbd753c9f29f7cbc3537143a85f42
Eric Camellini <eric.camellini@gmail.com>
2024-04-08 17:42:04 +0200

    Using readfile instead of require

--- packages/commons/src/risk-analysis/rules/riskAnalysisFormRulesProvider.ts
+++ packages/commons/src/risk-analysis/rules/riskAnalysisFormRulesProvider.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 import { TenantKind } from "pagopa-interop-models";
 import { match } from "ts-pattern";
 import { RiskAnalysisFormRules } from "./riskAnalysisFormRules.js";
@@ -5,24 +8,30 @@ import { RiskAnalysisFormRules } from "./riskAnalysisFormRules.js";
 /*  We avoid using import for JSON files because they would
 require import assertions, an experimental feature that results
 in experimental warnings that trigger our alarm systems */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const pa1 = require("./PA/1.0.json");
-const pa2 = require("./PA/2.0.json");
-const pa3 = require("./PA/3.0.json");
-const private1 = require("./PRIVATE/1.0.json");
-const private2 = require("./PRIVATE/2.0.json");
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+// ^^^ workaround to get __dirname equivalent in ES modules
+const pa1 = path.resolve(dirname, "./PA/1.0.json");
+const pa2 = path.resolve(dirname, "./PA/2.0.json");
+const pa3 = path.resolve(dirname, "./PA/3.0.json");
+const private1 = path.resolve(dirname, "./PRIVATE/1.0.json");
+const private2 = path.resolve(dirname, "./PRIVATE/2.0.json");
 
 function getFormRules(
   ruleset: "pa1" | "pa2" | "pa3" | "private1" | "private2"
 ): RiskAnalysisFormRules {
   return RiskAnalysisFormRules.parse(
-    match(ruleset)
-      .with("pa1", () => pa1)
-      .with("pa2", () => pa2)
-      .with("pa3", () => pa3)
-      .with("private1", () => private1)
-      .with("private2", () => private2)
-      .exhaustive()
+    JSON.parse(
+      fs.readFileSync(
+        match(ruleset)
+          .with("pa1", () => pa1)
+          .with("pa2", () => pa2)
+          .with("pa3", () => pa3)
+          .with("private1", () => private1)
+          .with("private2", () => private2)
+          .exhaustive(),
+        "utf-8"
+      )
+    )
   );
 }
 
