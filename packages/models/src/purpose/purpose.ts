c4895e650d9c26cd7f9463628f277668d2d620c3
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-08 12:25:44 +0200

    Continue scaffold

---
+++ packages/models/src/purpose/purpose.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+import {
+  EServiceId,
+  PurposeId,
+  PurposeVersionDocumentId,
+  PurposeVersionId,
+  TenantId,
+} from "../brandedIds.js";
+import { RiskAnalysisForm } from "../risk-analysis/riskAnalysis.js";
+
+export const purposeVersionState = {
+  draft: "Draft",
+  active: "Active",
+  suspended: "Suspended",
+  archived: "Archived",
+  waitingForApproval: "WaitingForApproval",
+  rejected: "Rejected",
+} as const;
+export const PurposeVersionState = z.enum([
+  Object.values(purposeVersionState)[0],
+  ...Object.values(purposeVersionState).slice(1),
+]);
+export type PurposeVersionState = z.infer<typeof PurposeVersionState>;
+
+export const PurposeVersionDocument = z.object({
+  id: PurposeVersionDocumentId,
+  contentType: z.string(),
+  path: z.string(),
+  createdAt: z.coerce.date(),
+});
+export type PurposeVersionDocument = z.infer<typeof PurposeVersionDocument>;
+
+export const PurposeVersion = z.object({
+  id: PurposeVersionId,
+  state: PurposeVersionState,
+  expectedApprovalDate: z.coerce.date().optional(),
+  riskAnalysys: PurposeVersionDocument.optional(),
+  dailyCalls: z.number(),
+  rejectionReason: z.string().optional(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date().optional(),
+  firstActivationAt: z.coerce.date().optional(),
+  suspendedAt: z.coerce.date().optional(),
+});
+export type PurposeVersion = z.infer<typeof PurposeVersion>;
+
+export const Purpose = z.object({
+  id: PurposeId,
+  eserviceId: EServiceId,
+  consumerId: TenantId,
+  versions: z.array(PurposeVersion),
+  suspendedByCondumer: z.boolean().optional(),
+  suspendedByProducer: z.boolean().optional(),
+  title: z.string(),
+  description: z.string(),
+  riskAnalysisForm: RiskAnalysisForm.optional(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+  isFreeOfCharge: z.boolean(),
+  freeOfChargeReason: z.string().optional(),
+});
+export type Purpose = z.infer<typeof Purpose>;
