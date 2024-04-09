dcf31529651537373c0de678eba278b5d2ea4bb6
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-04-09 12:48:38 +0200

    Added purpose v2 messages handling

--- packages/purpose-readmodel-writer/src/purposeConsumerServiceV2.ts
+++ packages/purpose-readmodel-writer/src/purposeConsumerServiceV2.ts
@@ -1,8 +1,54 @@
 import { PurposeCollection } from "pagopa-interop-commons";
-import { PurposeEventEnvelopeV2 } from "pagopa-interop-models";
+import { PurposeEventEnvelopeV2, fromPurposeV2 } from "pagopa-interop-models";
+import { match } from "ts-pattern";
 
 export async function handleMessageV2(
-  _message: PurposeEventEnvelopeV2,
-  _purposes: PurposeCollection
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-): Promise<void> {}
+  message: PurposeEventEnvelopeV2,
+  purposes: PurposeCollection
+): Promise<void> {
+  const purpose = message.data.purpose;
+
+  await match(message)
+    .with({ type: "DraftPurposeDeleted" }, async (message) => {
+      await purposes.deleteOne({
+        "data.id": message.stream_id,
+        "metadata.version": { $lt: message.version },
+      });
+    })
+    .with(
+      { type: "PurposeAdded" },
+      { type: "DraftPurposeUpdated" },
+      { type: "NewPurposeVersionActivated" },
+      { type: "NewPurposeVersionWaitingForApproval" },
+      { type: "PurposeActivated" },
+      { type: "PurposeArchived" },
+      { type: "PurposeVersionOverQuotaUnsuspended" },
+      { type: "PurposeVersionRejected" },
+      { type: "PurposeVersionSuspendedByConsumer" },
+      { type: "PurposeVersionSuspendedByProducer" },
+      { type: "PurposeVersionUnsuspendedByConsumer" },
+      { type: "PurposeVersionUnsuspendedByProducer" },
+      { type: "PurposeWaitingForApproval" },
+      { type: "WaitingForApprovalPurposeDeleted" },
+      { type: "WaitingForApprovalPurposeVersionDeleted" },
+      { type: "PurposeVersionActivated" },
+      async (message) =>
+        await purposes.updateOne(
+          {
+            "data.id": message.stream_id,
+            "metadata.version": { $lt: message.version },
+          },
+          {
+            $set: {
+              data: purpose ? fromPurposeV2(purpose) : undefined,
+              metadata: {
+                version: message.version,
+              },
+            },
+          },
+          { upsert: true }
+        )
+    )
+
+    .exhaustive();
+}
