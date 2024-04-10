16d690beb3ee43f381c034abf393e7f75dad2ef0
Simone Camito <zibo.camito@gmail.com>
2024-04-10 17:41:51 +0200

    fixes

--- packages/agreement-process/test/archiveAgreement.integration.test.ts
+++ packages/agreement-process/test/archiveAgreement.integration.test.ts
@@ -1,140 +1,114 @@
 import { fail } from "assert";
 import { v4 as uuidv4 } from "uuid";
 import {
+  Agreement,
   AgreementId,
   AgreementUpdatedV1,
   AgreementV1,
-  AttributeId,
-  DescriptorId,
   EServiceId,
   TenantId,
   agreementState,
   generateId,
   protobufDecoder,
-  unsafeBrandId,
 } from "pagopa-interop-models";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import {
   StoredEvent,
   getMockAgreement,
-  getMockDescriptorPublished,
-  getMockEService,
-  getMockEServiceAttribute,
-  getMockTenant,
   getRandomAuthData,
+  randomArrayItem,
 } from "pagopa-interop-commons-test/index.js";
-import { toAgreementStateV1 } from "../src/model/domain/toEvent.js";
+import { toAgreementV1 } from "../src/model/domain/toEvent.js";
 import {
   agreementNotFound,
   agreementNotInExpectedState,
   operationNotAllowed,
 } from "../src/model/domain/errors.js";
-import {
-  addOneAgreement,
-  addOneEService,
-  addOneTenant,
-  readLastAgreementEvent,
-} from "./utils.js";
+import { agreementArchivableStates } from "../src/model/domain/validators.js";
+import { addOneAgreement, readLastAgreementEvent } from "./utils.js";
 import {
   agreementService,
   agreements,
-  eservices,
   postgresDB,
-  tenants,
 } from "./agreementService.test.setup.js";
 
-const expectedAgreementArchived = async (
-  agreementId: AgreementId | undefined
-): Promise<AgreementV1> => {
-  expect(agreementId).toBeDefined();
-  if (!agreementId) {
-    fail("Unhandled error: returned agreementId is undefined");
-  }
-
-  const actualAgreementData: StoredEvent | undefined =
-    await readLastAgreementEvent(agreementId, postgresDB);
-
-  if (!actualAgreementData) {
-    fail("Creation fails: agreement not found in event-store");
-  }
-
-  expect(actualAgreementData).toMatchObject({
-    type: "AgreementUpdated",
-    event_version: 1,
-    version: "0",
-    stream_id: agreementId,
-  });
-
-  const actualAgreement: AgreementV1 | undefined = protobufDecoder(
-    AgreementUpdatedV1
-  ).parse(actualAgreementData.data)?.agreement;
-
-  if (!actualAgreement) {
-    fail("impossible to decode AgreementAddedV1 data");
-  }
-
-  expect(actualAgreement.state).toBe(
-    toAgreementStateV1(agreementState.archived)
-  );
-  return actualAgreement;
-};
-
 describe("archive agreement", () => {
-  it("should succeed when the requester is the consumer and the agreement is in a archivable state", async () => {
+  it("should succeed when the requester is the consumer and the agreement is in an archivable state", async () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date());
+
     const authData = getRandomAuthData();
     const eserviceId = generateId<EServiceId>();
-    const descriptorId = generateId<DescriptorId>();
-    const attributeId = generateId<AttributeId>();
 
-    const descriptor = getMockDescriptorPublished(descriptorId, [
-      [getMockEServiceAttribute(attributeId)],
-    ]);
-    const eservice = getMockEService(eserviceId, authData.organizationId, [
-      descriptor,
-    ]);
-    const tenant = getMockTenant(authData.organizationId);
     const agreement = getMockAgreement(
       eserviceId,
-      tenant.id,
-      agreementState.active
+      authData.organizationId,
+      randomArrayItem(agreementArchivableStates)
     );
 
-    await addOneEService(eservice, eservices);
-    await addOneTenant(tenant, tenants);
     await addOneAgreement(agreement, postgresDB, agreements);
 
-    const archivedAgreementId = await agreementService.archiveAgreement(
+    const agreementId = await agreementService.archiveAgreement(
       agreement.id,
       authData,
       uuidv4()
     );
 
-    await expectedAgreementArchived(
-      unsafeBrandId<AgreementId>(archivedAgreementId)
+    expect(agreementId).toBeDefined();
+    if (!agreementId) {
+      fail("Unhandled error: returned agreementId is undefined");
+    }
+
+    const actualAgreementData: StoredEvent | undefined =
+      await readLastAgreementEvent(agreementId, postgresDB);
+
+    if (!actualAgreementData) {
+      fail("Creation fails: agreement not found in event-store");
+    }
+
+    expect(actualAgreementData).toMatchObject({
+      type: "AgreementUpdated",
+      event_version: 1,
+      version: "0",
+      stream_id: agreementId,
+    });
+
+    const actualAgreement: AgreementV1 | undefined = protobufDecoder(
+      AgreementUpdatedV1
+    ).parse(actualAgreementData.data)?.agreement;
+
+    if (!actualAgreement) {
+      fail("impossible to decode AgreementAddedV1 data");
+    }
+
+    const expectedAgreemenentArchived: Agreement = {
+      ...agreement,
+      state: agreementState.archived,
+      stamps: {
+        ...agreement.stamps,
+        archiving: {
+          who: authData.userId,
+          when: new Date(),
+        },
+      },
+    };
+    expect(actualAgreement).toMatchObject(
+      toAgreementV1(expectedAgreemenentArchived)
     );
+
+    vi.useRealTimers();
   });
 
   it("should throw a agreementNotFound error when the Agreement doesn't exist", async () => {
     const authData = getRandomAuthData();
     const eserviceId = generateId<EServiceId>();
-    const descriptorId = generateId<DescriptorId>();
-    const attributeId = generateId<AttributeId>();
 
-    const descriptor = getMockDescriptorPublished(descriptorId, [
-      [getMockEServiceAttribute(attributeId)],
-    ]);
-    const eservice = getMockEService(eserviceId, authData.organizationId, [
-      descriptor,
-    ]);
-    const tenant = getMockTenant(authData.organizationId);
     const agreement = getMockAgreement(
       eserviceId,
-      tenant.id,
-      agreementState.active
+      authData.organizationId,
+      randomArrayItem(agreementArchivableStates)
     );
 
-    await addOneEService(eservice, eservices);
-    await addOneTenant(tenant, tenants);
     await addOneAgreement(agreement, postgresDB, agreements);
 
     const agreementToArchiveId = generateId<AgreementId>();
@@ -151,24 +125,13 @@ describe("archive agreement", () => {
   it("should throw a operationNotAllowed error when the requester is not the Agreement consumer", async () => {
     const authData = getRandomAuthData();
     const eserviceId = generateId<EServiceId>();
-    const descriptorId = generateId<DescriptorId>();
-    const attributeId = generateId<AttributeId>();
 
-    const descriptor = getMockDescriptorPublished(descriptorId, [
-      [getMockEServiceAttribute(attributeId)],
-    ]);
-    const eservice = getMockEService(eserviceId, generateId<TenantId>(), [
-      descriptor,
-    ]);
-    const tenant = getMockTenant(generateId<TenantId>());
     const agreement = getMockAgreement(
       eserviceId,
-      tenant.id,
-      agreementState.active
+      generateId<TenantId>(),
+      randomArrayItem(agreementArchivableStates)
     );
 
-    await addOneEService(eservice, eservices);
-    await addOneTenant(tenant, tenants);
     await addOneAgreement(agreement, postgresDB, agreements);
 
     await expect(
@@ -179,30 +142,22 @@ describe("archive agreement", () => {
   it("should throw a agreementNotInExpectedState error when the Agreement is not in a archivable states", async () => {
     const authData = getRandomAuthData();
     const eserviceId = generateId<EServiceId>();
-    const descriptorId = generateId<DescriptorId>();
-    const attributeId = generateId<AttributeId>();
 
-    const descriptor = getMockDescriptorPublished(descriptorId, [
-      [getMockEServiceAttribute(attributeId)],
-    ]);
-    const eservice = getMockEService(eserviceId, authData.organizationId, [
-      descriptor,
-    ]);
-    const tenant = getMockTenant(authData.organizationId);
+    const state = randomArrayItem(
+      Object.values(agreementState).filter(
+        (s) => !agreementArchivableStates.includes(s)
+      )
+    );
     const agreement = getMockAgreement(
       eserviceId,
-      tenant.id,
-      agreementState.draft
+      authData.organizationId,
+      state
     );
 
-    await addOneEService(eservice, eservices);
-    await addOneTenant(tenant, tenants);
     await addOneAgreement(agreement, postgresDB, agreements);
 
     await expect(
       agreementService.archiveAgreement(agreement.id, authData, uuidv4())
-    ).rejects.toThrowError(
-      agreementNotInExpectedState(agreement.id, agreementState.draft)
-    );
+    ).rejects.toThrowError(agreementNotInExpectedState(agreement.id, state));
   });
 });
