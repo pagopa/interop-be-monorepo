f4d6dd9c0db5f90ac49151c980b97f45c75fc16d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 11:25:47 +0200

    Add purposeService with getPurposeById

---
+++ packages/purpose-process/src/services/purposeService.ts
@@ -0,0 +1,152 @@
+import {
+  AuthData,
+  DB,
+  eventRepository,
+  logger,
+  riskAnalysisFormToRiskAnalysisFormToValidate,
+  validateRiskAnalysis,
+} from "pagopa-interop-commons";
+import {
+  EService,
+  EServiceId,
+  TenantId,
+  WithMetadata,
+  Tenant,
+  Purpose,
+  PurposeId,
+  purposeEventToBinaryData,
+  TenantKind,
+  purposeVersionState,
+  RiskAnalysisForm,
+} from "pagopa-interop-models";
+import {
+  eserviceNotFound,
+  purposeNotFound,
+  tenantNotFound,
+} from "../model/domain/errors.js";
+import { ReadModelService } from "./readModelService.js";
+
+const retrievePurpose = async (
+  purposeId: PurposeId,
+  readModelService: ReadModelService
+): Promise<WithMetadata<Purpose>> => {
+  const purpose = await readModelService.getPurposeById(purposeId);
+  if (purpose === undefined) {
+    throw purposeNotFound(purposeId);
+  }
+  return purpose;
+};
+
+const retrieveEService = async (
+  eserviceId: EServiceId,
+  readModelService: ReadModelService
+): Promise<EService> => {
+  const eservice = await readModelService.getEServiceById(eserviceId);
+  if (eservice === undefined) {
+    throw eserviceNotFound(eserviceId);
+  }
+  return eservice;
+};
+
+const retrieveTenant = async (
+  tenantId: TenantId,
+  readModelService: ReadModelService
+): Promise<Tenant> => {
+  const tenant = await readModelService.getTenantById(tenantId);
+  if (tenant === undefined) {
+    throw tenantNotFound(tenantId);
+  }
+  return tenant;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function purposeServiceBuilder(
+  dbInstance: DB,
+  readModelService: ReadModelService
+) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const repository = eventRepository(dbInstance, purposeEventToBinaryData);
+
+  return {
+    async getPurposeById(
+      purposeId: PurposeId,
+      authData: AuthData
+    ): Promise<{ purpose: Purpose; isRiskAnalysisValid: boolean }> {
+      logger.info(`Retrieving Purpose ${purposeId}`);
+
+      const purpose = await retrievePurpose(purposeId, readModelService);
+      const eservice = await retrieveEService(
+        purpose.data.eserviceId,
+        readModelService
+      );
+      const tenant = await retrieveTenant(
+        authData.organizationId,
+        readModelService
+      );
+
+      if (tenant.kind === undefined) {
+        throw new Error();
+      }
+
+      return authorizeRiskAnalysisForm({
+        purpose: purpose.data,
+        producerId: eservice.producerId,
+        organizationId: authData.organizationId,
+        tenantKind: tenant.kind,
+      });
+    },
+  };
+}
+
+export type PurposeService = ReturnType<typeof purposeServiceBuilder>;
+
+const authorizeRiskAnalysisForm = ({
+  purpose,
+  producerId,
+  organizationId,
+  tenantKind,
+}: {
+  purpose: Purpose;
+  producerId: TenantId;
+  organizationId: TenantId;
+  tenantKind: TenantKind;
+}): { purpose: Purpose; isRiskAnalysisValid: boolean } => {
+  if (organizationId === purpose.consumerId || organizationId === producerId) {
+    if (purposeIsDraft(purpose)) {
+      const isRiskAnalysisValid = isRiskAnalysisFormValid(
+        purpose.riskAnalysisForm,
+        false,
+        tenantKind
+      );
+      return { purpose, isRiskAnalysisValid };
+    } else {
+      return { purpose, isRiskAnalysisValid: true };
+    }
+  } else {
+    return {
+      purpose: { ...purpose, riskAnalysisForm: undefined },
+      isRiskAnalysisValid: false,
+    };
+  }
+};
+
+const isRiskAnalysisFormValid = (
+  riskAnalysisForm: RiskAnalysisForm | undefined,
+  schemaOnlyValidation: boolean,
+  tenantKind: TenantKind
+): boolean => {
+  if (riskAnalysisForm === undefined) {
+    return false;
+  } else {
+    return (
+      validateRiskAnalysis(
+        riskAnalysisFormToRiskAnalysisFormToValidate(riskAnalysisForm),
+        schemaOnlyValidation,
+        tenantKind
+      ).type === "valid"
+    );
+  }
+};
+
+const purposeIsDraft = (purpose: Purpose): boolean =>
+  !purpose.versions.some((v) => v.state !== purposeVersionState.draft);
