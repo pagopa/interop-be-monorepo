2ce47359bbd9a2b79fe5c9e948eaa176402a27dc
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 15:48:35 +0200

    Add error mapper

--- packages/purpose-process/src/utilities/errorMappers.ts
+++ packages/purpose-process/src/utilities/errorMappers.ts
@@ -5,11 +5,24 @@ import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
 
 type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
 
-const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = constants;
+const {
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_FORBIDDEN,
+} = constants;
 
 export const getPurposeErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
     .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("eserviceNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+
+export const getRiskAnalysisDocumentErrorMapper = (
+  error: ApiError<ErrorCodes>
+): number =>
+  match(error.code)
+    .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("eserviceNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("organizationNotAllowed", () => HTTP_STATUS_FORBIDDEN)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
