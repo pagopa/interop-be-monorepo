b9a4e0e6ba4f59a4487068f94796583aafe742d7
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 15:48:29 +0200

    Add errors

--- packages/purpose-process/src/model/domain/errors.ts
+++ packages/purpose-process/src/model/domain/errors.ts
@@ -2,6 +2,8 @@ import {
   ApiError,
   EServiceId,
   PurposeId,
+  PurposeVersionDocumentId,
+  PurposeVersionId,
   TenantId,
   makeApiProblemBuilder,
 } from "pagopa-interop-models";
@@ -12,6 +14,9 @@ export const errorCodes = {
   eserviceNotFound: "0002",
   tenantNotFound: "0003",
   tenantKindNotFound: "0004",
+  purposeVersionNotFound: "0005",
+  purposeVersionDocumentNotFound: "0006",
+  organizationNotAllowed: "0007",
 };
 
 export type ErrorCodes = keyof typeof errorCodes;
@@ -47,5 +52,38 @@ export function tenantKindNotFound(tenantId: TenantId): ApiError<ErrorCodes> {
     detail: `Tenant kind for tenant ${tenantId} not found`,
     code: "tenantKindNotFound",
     title: "Tenant kind not found",
+  });
+}
+
+export function purposeVersionNotFound(
+  purposeId: PurposeId,
+  versionId: PurposeVersionId
+): ApiError<ErrorCodes> {
+  return new ApiError({
+    detail: `Version ${versionId} not found for purpose ${purposeId}`,
+    code: "purposeVersionNotFound",
+    title: "Purpose version not found",
+  });
+}
+
+export function purposeVersionDocumentNotFound(
+  purposeId: PurposeId,
+  versionId: PurposeVersionId,
+  documentId: PurposeVersionDocumentId
+): ApiError<ErrorCodes> {
+  return new ApiError({
+    detail: `Document ${documentId} not found for version ${versionId} of purpose ${purposeId}`,
+    code: "purposeVersionDocumentNotFound",
+    title: "Purpose version document not found",
+  });
+}
+
+export function organizationNotAllowed(
+  organizationId: TenantId
+): ApiError<ErrorCodes> {
+  return new ApiError({
+    detail: `Organization ${organizationId} is not allowed to perform the operation`,
+    code: "organizationNotAllowed",
+    title: "Organization not allowed",
   });
 }
