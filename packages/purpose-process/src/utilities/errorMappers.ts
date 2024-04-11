958582d8043845940ce557bc5b69ed9648d2dd74
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 11:36:01 +0200

    Add error mapper

---
+++ packages/purpose-process/src/utilities/errorMappers.ts
@@ -0,0 +1,15 @@
+import { constants } from "http2";
+import { ApiError, CommonErrorCodes } from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
+
+type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
+
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = constants;
+
+export const getPurposeErrorMapper = (error: ApiError<ErrorCodes>): number =>
+  match(error.code)
+    .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("eserviceNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
