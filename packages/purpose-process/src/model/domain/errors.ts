37dbbe5364e9eab7e94fe70e854bb9551c286d1d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 09:58:12 +0200

    Add scaffold for errors

---
+++ packages/purpose-process/src/model/domain/errors.ts
@@ -0,0 +1,44 @@
+import {
+  ApiError,
+  EServiceId,
+  PurposeId,
+  TenantId,
+  makeApiProblemBuilder,
+} from "pagopa-interop-models";
+import { logger } from "pagopa-interop-commons";
+
+export const errorCodes = {
+  purposeNotFound: "0001",
+  eserviceNotFound: "0002",
+  tenantNotFound: "0003",
+};
+
+export type ErrorCodes = keyof typeof errorCodes;
+
+export const makeApiProblem = makeApiProblemBuilder(logger, errorCodes);
+
+export function purposeNotFoundFound(
+  purposeId: PurposeId
+): ApiError<ErrorCodes> {
+  return new ApiError({
+    detail: `Purpose ${purposeId} not found`,
+    code: "purposeNotFound",
+    title: "Purpose not found",
+  });
+}
+
+export function eserviceNotFound(eserviceId: EServiceId): ApiError<ErrorCodes> {
+  return new ApiError({
+    detail: `EService ${eserviceId} not found`,
+    code: "eserviceNotFound",
+    title: "EService not found",
+  });
+}
+
+export function tenantNotFound(tenantId: TenantId): ApiError<ErrorCodes> {
+  return new ApiError({
+    detail: `Tenant ${tenantId} not found`,
+    code: "tenantNotFound",
+    title: "Tenant not found",
+  });
+}
