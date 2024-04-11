fd6ddf4109d1479263012c839d37fc3967c63192
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 17:49:06 +0200

    Add error mapper

--- packages/purpose-process/src/utilities/errorMappers.ts
+++ packages/purpose-process/src/utilities/errorMappers.ts
@@ -9,6 +9,7 @@ const {
   HTTP_STATUS_INTERNAL_SERVER_ERROR,
   HTTP_STATUS_NOT_FOUND,
   HTTP_STATUS_FORBIDDEN,
+  HTTP_STATUS_CONFLICT,
 } = constants;
 
 export const getPurposeErrorMapper = (error: ApiError<ErrorCodes>): number =>
@@ -25,4 +26,14 @@ export const getRiskAnalysisDocumentErrorMapper = (
     .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("eserviceNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("organizationNotAllowed", () => HTTP_STATUS_FORBIDDEN)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+
+export const deletePurposeVersionErrorMapper = (
+  error: ApiError<ErrorCodes>
+): number =>
+  match(error.code)
+    .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("purposeVersionNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("organizationIsNotTheConsumer", () => HTTP_STATUS_FORBIDDEN)
+    .with("purposeVersionCannotBeDeleted", () => HTTP_STATUS_CONFLICT)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
