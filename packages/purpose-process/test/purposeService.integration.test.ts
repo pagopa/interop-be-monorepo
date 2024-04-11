8675eceecd1c71618ba511062c1c1974024eaf9e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-11 12:27:52 +0200

    Implement tests

--- packages/purpose-process/test/purposeService.integration.test.ts
+++ packages/purpose-process/test/purposeService.integration.test.ts
@@ -5,8 +5,10 @@
 
 import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
 import {
+  EServiceCollection,
   PurposeCollection,
   ReadModelRepository,
+  TenantCollection,
   initDB,
 } from "pagopa-interop-commons";
 import { IDatabase } from "pg-promise";
@@ -14,10 +16,23 @@ import { IDatabase } from "pg-promise";
 import {
   TEST_MONGO_DB_PORT,
   TEST_POSTGRES_DB_PORT,
+  getMockAuthData,
+  getMockPurpose,
+  getMockTenant,
   mongoDBContainer,
   postgreSQLContainer,
+  writeInReadmodel,
 } from "pagopa-interop-commons-test";
 import { StartedTestContainer } from "testcontainers";
+import {
+  EService,
+  Purpose,
+  PurposeId,
+  TenantKind,
+  generateId,
+  tenantKind,
+  toReadModelEService,
+} from "pagopa-interop-models";
 import { config } from "../src/utilities/config.js";
 import {
   PurposeService,
@@ -27,9 +42,13 @@ import {
   ReadModelService,
   readModelServiceBuilder,
 } from "../src/services/readModelService.js";
+import { purposeNotFound } from "../src/model/domain/errors.js";
+import { addOnePurpose, getMockEService } from "./utils.js";
 
 describe("database test", async () => {
   let purposes: PurposeCollection;
+  let eservices: EServiceCollection;
+  let tenants: TenantCollection;
   let readModelService: ReadModelService;
   let purposeService: PurposeService;
   let postgresDB: IDatabase<unknown>;
@@ -48,6 +67,8 @@ describe("database test", async () => {
 
     const readModelRepository = ReadModelRepository.init(config);
     purposes = readModelRepository.purposes;
+    eservices = readModelRepository.eservices;
+    tenants = readModelRepository.tenants;
     readModelService = readModelServiceBuilder(readModelRepository);
     postgresDB = initDB({
       username: config.eventStoreDbUsername,
@@ -73,8 +94,53 @@ describe("database test", async () => {
   });
 
   describe("Purpose service", () => {
+    const mockPurpose = getMockPurpose();
     describe("getPurposeById", () => {
-      it("TO DO", () => {
+      it("Should get the purpose if it exists", async () => {
+        const mockEService = getMockEService();
+        const mockTenant = {
+          ...getMockTenant(),
+          kind: tenantKind.PA,
+        };
+
+        const mockPurpose1: Purpose = {
+          ...mockPurpose,
+          eserviceId: mockEService.id,
+        };
+        const mockPurpose2: Purpose = {
+          ...getMockPurpose(),
+          id: generateId(),
+          title: "another purpose",
+        };
+        await addOnePurpose(mockPurpose1, postgresDB, purposes);
+        await addOnePurpose(mockPurpose2, postgresDB, purposes);
+        await writeInReadmodel(toReadModelEService(mockEService), eservices);
+        await writeInReadmodel(mockTenant, tenants);
+
+        const result = await purposeService.getPurposeById(
+          mockPurpose1.id,
+          getMockAuthData(mockTenant.id)
+        );
+        expect(result).toMatchObject({
+          purpose: mockPurpose1,
+          isRiskAnalysisValid: false,
+        });
+      });
+      it("Should throw purposeNotFound if the purpose doesn't exist", async () => {
+        const notExistingId: PurposeId = generateId();
+        await addOnePurpose(mockPurpose, postgresDB, purposes);
+
+        expect(
+          purposeService.getPurposeById(notExistingId, getMockAuthData())
+        ).rejects.toThrowError(purposeNotFound(notExistingId));
+      });
+      it("Should throw eserviceNotFound if the eservice doesn't exist", () => {
+        expect(1).toBe(1);
+      });
+      it("Should throw tenantNotFound if the tenant doesn't exist", () => {
+        expect(1).toBe(1);
+      });
+      it("Should throw tenantKindNotFound if the tenant doesn't exist", () => {
         expect(1).toBe(1);
       });
     });
