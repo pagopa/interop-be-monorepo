c426f131a113d59983419f7c230985c26ffb527c
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-04-16 14:43:40 +0200

    TODO

--- packages/purpose-process/src/routers/PurposeRouter.ts
+++ packages/purpose-process/src/routers/PurposeRouter.ts
@@ -170,7 +170,9 @@ const purposeRouter = (
     .post(
       "/purposes/:purposeId/versions",
       authorizationMiddleware([ADMIN_ROLE]),
-      (_req, res) => res.status(501).send()
+      (_req, res) =>
+        // TODO
+        res.status(501).send()
     )
     .delete(
       "/purposes/:purposeId/versions/:versionId",
