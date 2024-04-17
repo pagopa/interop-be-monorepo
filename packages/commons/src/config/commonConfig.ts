e26f9992a2c7a693d47b749efe6f18cfe45f4358
Eric Camellini <eric.camellini@gmail.com>
2024-04-17 11:22:49 +0200

    Validating aud in JWT

--- packages/commons/src/config/commonConfig.ts
+++ packages/commons/src/config/commonConfig.ts
@@ -7,9 +7,15 @@ export const JWTConfig = z
       .string()
       .transform((s) => s.split(","))
       .pipe(z.array(APIEndpoint)),
+
+    ACCEPTED_AUDIENCES: z
+      .string()
+      .transform((s) => s.split(","))
+      .pipe(z.array(z.string())),
   })
   .transform((c) => ({
     wellKnownUrls: c.WELL_KNOWN_URLS,
+    acceptedAudiences: c.ACCEPTED_AUDIENCES,
   }));
 export type JWTConfig = z.infer<typeof JWTConfig>;
 
