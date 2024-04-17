25d2d72ae6eaf49a1719ed636472477d6ecfd3a0
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-17 10:00:00 +0200

    Adjust to latest changes

--- packages/purpose-process/src/services/purposeService.ts
+++ packages/purpose-process/src/services/purposeService.ts
@@ -383,6 +383,7 @@ export function purposeServiceBuilder(
 
       const event = toCreateEventPurposeArchived({
         purpose: updatedPurpose,
+        purposeVersionId: archivedVersion.id,
         version: purpose.metadata.version,
         correlationId,
       });
