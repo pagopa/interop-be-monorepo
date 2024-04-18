1b2e124d5b0c55804ccb5017c886a53e0a22488c
AsterITA <fabrizio.musella@gmail.com>
2024-04-18 16:24:35 +0200

    fix test

--- packages/commons-test/test/jwt.unit.test.ts
+++ packages/commons-test/test/jwt.unit.test.ts
@@ -240,6 +240,7 @@ describe("JWT tests", () => {
           value: "5N2TR557",
         },
         organizationId: "69e2865e-65ab-4e48-a638-2037a9ee2ee7",
+        selfcareId: "1962d21c-c701-4805-93f6-53a877898756",
         userId: "f07ddb8f-17f9-47d4-b31e-35d1ac10e521",
         userRoles: ["support"],
       });
