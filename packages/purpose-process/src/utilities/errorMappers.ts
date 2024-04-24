4d6cabc275faebc50c051b6efebc0efa479766cd
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-04-24 09:46:51 +0200

    Group handlers in error mapper

--- packages/purpose-process/src/utilities/errorMappers.ts
+++ packages/purpose-process/src/utilities/errorMappers.ts
@@ -75,8 +75,11 @@ export const archivePurposeVersionErrorMapper = (
   error: ApiError<ErrorCodes>
 ): number =>
   match(error.code)
-    .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
-    .with("purposeVersionNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with(
+      "purposeNotFound",
+      "purposeVersionNotFound",
+      () => HTTP_STATUS_NOT_FOUND
+    )
     .with("organizationIsNotTheConsumer", () => HTTP_STATUS_FORBIDDEN)
     .with("notValidVersionState", () => HTTP_STATUS_BAD_REQUEST)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
