839e56c62b83cd252b60745e6c4b9f9e445b65e2
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-07 17:09:39 +0200

    Draft

--- packages/commons/src/kafka/kafka.ts
+++ packages/commons/src/kafka/kafka.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { z } from "zod";
 import { EachMessagePayload, KafkaMessage } from "kafkajs";
-import { EServiceEvent, Message } from "pagopa-interop-models";
+import { EServiceEvent, Message, PurposeEventV2 } from "pagopa-interop-models";
 import { P, match } from "ts-pattern";
 import { KafkaTopicConfig } from "../config/kafkaTopicConfig.js";
 
@@ -46,6 +46,11 @@ export const messageDecoderSupplier = (
       { catalogTopic: P.string },
       () => (message: KafkaMessage) =>
         decodeKafkaMessage(message, EServiceEvent)
+    )
+    .with(
+      { purposeTopic: P.string },
+      () => (message: KafkaMessage) =>
+        decodeKafkaMessage(message, PurposeEventV2)
     )
     .otherwise(() => {
       throw new Error(`Topic decoder not found for provided topic : ${topic}`);
