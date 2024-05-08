adef8b2a8bc1c562475e0988cfb444f75dd180b4
Eric Camellini <eric.camellini@gmail.com>
2024-05-08 16:48:03 +0200

    More fixes after rebase

--- packages/eservice-descriptors-archiver/test/utils.ts
+++ packages/eservice-descriptors-archiver/test/utils.ts
@@ -6,6 +6,7 @@ import { writeInReadmodel } from "pagopa-interop-commons-test/index.js";
 import {
   Agreement,
   EService,
+  toReadModelAgreement,
   toReadModelEService,
 } from "pagopa-interop-models";
 
@@ -13,7 +14,7 @@ export const addOneAgreement = async (
   agreement: Agreement,
   agreements: AgreementCollection
 ): Promise<void> => {
-  await writeInReadmodel(agreement, agreements);
+  await writeInReadmodel(toReadModelAgreement(agreement), agreements);
 };
 
 export const addOneEService = async (
