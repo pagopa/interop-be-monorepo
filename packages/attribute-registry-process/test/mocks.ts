36edfbd826357439ea0346c832df0b30dbf1919a
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-05-09 11:26:28 +0200

    Fix type problem

--- packages/attribute-registry-process/test/mocks.ts
+++ packages/attribute-registry-process/test/mocks.ts
@@ -1,6 +1,5 @@
 import { Tenant, TenantId, generateId } from "pagopa-interop-models";
 import { AuthData } from "pagopa-interop-commons";
-import { v4 as uuidv4 } from "uuid";
 
 export const getMockTenant = (): Tenant => ({
   name: "tenant_Name",
@@ -17,10 +16,11 @@ export const getMockTenant = (): Tenant => ({
 
 export const getMockAuthData = (organizationId?: TenantId): AuthData => ({
   organizationId: organizationId || generateId(),
-  userId: uuidv4(),
+  userId: generateId(),
   userRoles: [],
   externalId: {
     value: "123456",
     origin: "IPA",
   },
+  selfcareId: generateId(),
 });
