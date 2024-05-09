014b58664d1991ed640d796550868329589a4f1d
AsterITA <fabrizio.musella@gmail.com>
2024-05-09 16:14:26 +0200

    IMN 439 + 441: updatePurpose and reverseUpdatePurpose in purpose-process (#405)

--- packages/purpose-process/src/utilities/errorMappers.ts
+++ packages/purpose-process/src/utilities/errorMappers.ts
@@ -56,3 +56,22 @@ export const rejectPurposeVersionErrorMapper = (
     .with("organizationIsNotTheProducer", () => HTTP_STATUS_FORBIDDEN)
     .with("notValidVersionState", () => HTTP_STATUS_BAD_REQUEST)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+
+export const updatePurposeErrorMapper = (error: ApiError<ErrorCodes>): number =>
+  match(error.code)
+    .with(
+      "eServiceModeNotAllowed",
+      "missingFreeOfChargeReason",
+      "riskAnalysisValidationFailed",
+      () => HTTP_STATUS_BAD_REQUEST
+    )
+    .with(
+      "organizationIsNotTheConsumer",
+      "purposeNotInDraftState",
+      () => HTTP_STATUS_FORBIDDEN
+    )
+    .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("duplicatedPurposeTitle", () => HTTP_STATUS_CONFLICT)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+
+export const updateReversePurposeErrorMapper = updatePurposeErrorMapper;
