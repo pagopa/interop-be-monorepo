6e9759f6536da98f9f13cbb823f20d0679553ab5
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-05-14 15:54:30 +0200

    fix

--- packages/catalog-process/test/createEService.test.ts
+++ packages/catalog-process/test/createEService.test.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { describe, expect, it } from "vitest";
-import { decodeProtobufPayload } from "pagopa-interop-commons-test";
-import { EService, EServiceAddedV2, toEServiceV2 } from "pagopa-interop-models";
 import { genericLogger } from "pagopa-interop-commons";
+import { decodeProtobufPayload } from "pagopa-interop-commons-test/index.js";
+import { EServiceAddedV2, EService, toEServiceV2 } from "pagopa-interop-models";
+import { expect, describe, it } from "vitest";
 import {
   eServiceDuplicate,
   originNotCompliant,
@@ -15,11 +15,51 @@ import {
   readLastEserviceEvent,
 } from "./utils.js";
 
-export const testCreateEService = (): ReturnType<typeof describe> =>
-  describe("create eservice", () => {
-    const mockEService = getMockEService();
-    it("should write on event-store for the creation of an eservice", async () => {
-      const eservice = await catalogService.createEService(
+describe("create eservice", () => {
+  const mockEService = getMockEService();
+  it("should write on event-store for the creation of an eservice", async () => {
+    const eservice = await catalogService.createEService(
+      {
+        name: mockEService.name,
+        description: mockEService.description,
+        technology: "REST",
+        mode: "DELIVER",
+      },
+      {
+        authData: getMockAuthData(mockEService.producerId),
+        correlationId: "",
+        serviceName: "",
+        logger: genericLogger,
+      }
+    );
+
+    expect(eservice).toBeDefined();
+    const writtenEvent = await readLastEserviceEvent(eservice.id);
+    expect(writtenEvent).toMatchObject({
+      stream_id: eservice.id,
+      version: "0",
+      type: "EServiceAdded",
+      event_version: 2,
+    });
+    const writtenPayload = decodeProtobufPayload({
+      messageType: EServiceAddedV2,
+      payload: writtenEvent.data,
+    });
+
+    const expectedEservice: EService = {
+      ...mockEService,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      createdAt: new Date(Number(writtenPayload.eservice!.createdAt)),
+      id: eservice.id,
+    };
+
+    expect(writtenPayload.eservice).toEqual(toEServiceV2(expectedEservice));
+  });
+
+  it("should throw eServiceDuplicate if an eservice with the same name already exists", async () => {
+    await addOneEService(mockEService);
+    expect(
+      catalogService.createEService(
         {
           name: mockEService.name,
           description: mockEService.description,
@@ -32,73 +72,32 @@ export const testCreateEService = (): ReturnType<typeof describe> =>
           serviceName: "",
           logger: genericLogger,
         }
-      );
-
-      expect(eservice).toBeDefined();
-      const writtenEvent = await readLastEserviceEvent(eservice.id);
-      expect(writtenEvent).toMatchObject({
-        stream_id: eservice.id,
-        version: "0",
-        type: "EServiceAdded",
-        event_version: 2,
-      });
-      const writtenPayload = decodeProtobufPayload({
-        messageType: EServiceAddedV2,
-        payload: writtenEvent.data,
-      });
-
-      const expectedEservice: EService = {
-        ...mockEService,
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        createdAt: new Date(Number(writtenPayload.eservice!.createdAt)),
-        id: eservice.id,
-      };
-
-      expect(writtenPayload.eservice).toEqual(toEServiceV2(expectedEservice));
-    });
-
-    it("should throw eServiceDuplicate if an eservice with the same name already exists", async () => {
-      await addOneEService(mockEService);
-      expect(
-        catalogService.createEService(
-          {
-            name: mockEService.name,
-            description: mockEService.description,
-            technology: "REST",
-            mode: "DELIVER",
-          },
-          {
-            authData: getMockAuthData(mockEService.producerId),
-            correlationId: "",
-            serviceName: "",
-            logger: genericLogger,
-          }
-        )
-      ).rejects.toThrowError(eServiceDuplicate(mockEService.name));
-    });
-
-    it("should throw originNotCompliant if the requester externalId origin is not allowed", async () => {
-      expect(
-        catalogService.createEService(
-          {
-            name: mockEService.name,
-            description: mockEService.description,
-            technology: "REST",
-            mode: "DELIVER",
-          },
-          {
-            authData: {
-              ...getMockAuthData(mockEService.producerId),
-              externalId: {
-                value: "123456",
-                origin: "not-allowed-origin",
-              },
-            },
-            correlationId: "",
-            serviceName: "",
-            logger: genericLogger,
-          }
-        )
-      ).rejects.toThrowError(originNotCompliant("not-allowed-origin"));
-    });
+      )
+    ).rejects.toThrowError(eServiceDuplicate(mockEService.name));
   });
+
+  it("should throw originNotCompliant if the requester externalId origin is not allowed", async () => {
+    expect(
+      catalogService.createEService(
+        {
+          name: mockEService.name,
+          description: mockEService.description,
+          technology: "REST",
+          mode: "DELIVER",
+        },
+        {
+          authData: {
+            ...getMockAuthData(mockEService.producerId),
+            externalId: {
+              value: "123456",
+              origin: "not-allowed-origin",
+            },
+          },
+          correlationId: "",
+          serviceName: "",
+          logger: genericLogger,
+        }
+      )
+    ).rejects.toThrowError(originNotCompliant("not-allowed-origin"));
+  });
+});
