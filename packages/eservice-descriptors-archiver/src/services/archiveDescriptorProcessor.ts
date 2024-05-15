750401139450d5a841191530e97b41b64c65f360
Eric Camellini <eric.camellini@gmail.com>
2024-05-15 12:35:45 +0200

    IMN-162 - descriptors archiver consumer job (#465)

---
+++ packages/eservice-descriptors-archiver/src/services/archiveDescriptorProcessor.ts
@@ -0,0 +1,109 @@
+import { Logger, RefreshableInteropToken } from "pagopa-interop-commons";
+import {
+  Agreement,
+  descriptorState,
+  genericInternalError,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import { ReadModelService } from "./readModelService.js";
+import { CatalogProcessClient } from "./catalogProcessClient.js";
+
+// eslint-disable-next-line max-params
+export async function archiveDescriptorForArchivedAgreement(
+  archivedAgreement: Agreement,
+  refreshableToken: RefreshableInteropToken,
+  readModelService: ReadModelService,
+  catalogProcessClient: CatalogProcessClient,
+  logger: Logger,
+  correlationId: string
+): Promise<void> {
+  const relatingNonArchivedAgreements = (
+    await readModelService.getNonArchivedAgreementsByEserviceAndDescriptorId(
+      archivedAgreement.eserviceId,
+      archivedAgreement.descriptorId
+    )
+  ).filter((a) => a.id !== archivedAgreement.id);
+
+  const allArchived = relatingNonArchivedAgreements.length === 0;
+
+  if (!allArchived) {
+    logger.info(
+      `Skipping descriptors archiviation - not all agreements are archived for Descriptor ${archivedAgreement.descriptorId} of EService ${archivedAgreement.eserviceId}`
+    );
+    return undefined;
+  }
+
+  const eservice = await readModelService.getEServiceById(
+    archivedAgreement.eserviceId
+  );
+
+  if (!eservice) {
+    throw genericInternalError(
+      `EService not found for agreement ${archivedAgreement.id}`
+    );
+  }
+
+  const descriptor = eservice.descriptors.find(
+    (d) => d.id === archivedAgreement.descriptorId
+  );
+
+  if (!descriptor) {
+    throw genericInternalError(
+      `Descriptor not found for agreement ${archivedAgreement.id}`
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const getHeaders = (correlationId: string, token: string) => ({
+    "X-Correlation-Id": correlationId,
+    Authorization: `Bearer ${token}`,
+  });
+
+  return await match(descriptor)
+    .with({ state: descriptorState.deprecated }, async () => {
+      const token = (await refreshableToken.get()).serialized;
+      const headers = getHeaders(correlationId, token);
+      await catalogProcessClient.archiveDescriptor(undefined, {
+        params: {
+          eServiceId: archivedAgreement.eserviceId,
+          descriptorId: archivedAgreement.descriptorId,
+        },
+        headers,
+      });
+
+      logger.info(
+        `Descriptor archived for archived Agreement ${archivedAgreement.id} - Descriptor ${archivedAgreement.descriptorId} - EService ${archivedAgreement.eserviceId}`
+      );
+    })
+    .with({ state: descriptorState.suspended }, async () => {
+      const newerDescriptorExists = eservice.descriptors.some(
+        (d) =>
+          (d.state === descriptorState.published ||
+            d.state === descriptorState.suspended) &&
+          Number(d.version) > Number(descriptor.version)
+      );
+      if (newerDescriptorExists) {
+        const token = (await refreshableToken.get()).serialized;
+        const headers = getHeaders(correlationId, token);
+        await catalogProcessClient.archiveDescriptor(undefined, {
+          params: {
+            eServiceId: archivedAgreement.eserviceId,
+            descriptorId: archivedAgreement.descriptorId,
+          },
+          headers,
+        });
+        logger.info(
+          `Descriptor archived for archived Agreement ${archivedAgreement.id} - Descriptor ${archivedAgreement.descriptorId} - EService ${archivedAgreement.eserviceId}`
+        );
+      } else {
+        logger.info(
+          `Skipping descriptor archiviation for Descriptor ${archivedAgreement.descriptorId} of EService ${archivedAgreement.eserviceId} - Descriptor suspended but no newer Descriptor found`
+        );
+      }
+    })
+    .otherwise(() => {
+      logger.info(
+        `Skipping descriptor archiviation for Descriptor ${archivedAgreement.descriptorId} of EService ${archivedAgreement.eserviceId} - Descriptor state is not Deprecated or Suspended (state: ${descriptor.state})`
+      );
+    });
+}
