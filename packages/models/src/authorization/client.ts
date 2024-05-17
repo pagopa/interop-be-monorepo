6ad052d49662750b26c8c3a580063d902969e770
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-17 14:01:13 +0200

    Update model

--- packages/models/src/authorization/client.ts
+++ packages/models/src/authorization/client.ts
@@ -1,5 +1,12 @@
 import { z } from "zod";
-import { PurposeId, TenantId } from "../brandedIds.js";
+import {
+  AgreementId,
+  DescriptorId,
+  EServiceId,
+  PurposeId,
+  PurposeVersionId,
+  TenantId,
+} from "../brandedIds.js";
 
 export const clientKind = {
   consumer: "Consumer",
@@ -11,11 +18,53 @@ export const ClientKind = z.enum([
 ]);
 export type ClientKind = z.infer<typeof ClientKind>;
 
+export const clientComponentState = {
+  active: "Active",
+  inactive: "Inactive",
+} as const;
+export const ClientComponentState = z.enum([
+  Object.values(clientComponentState)[0],
+  ...Object.values(clientComponentState).slice(1),
+]);
+export type ClientComponentState = z.infer<typeof ClientComponentState>;
+
+export const ClientEserviceDetails = z.object({
+  eserviceId: EServiceId,
+  descriptorId: DescriptorId,
+  state: ClientComponentState,
+  audience: z.array(z.string()),
+  voucherLifespan: z.number(),
+});
+export type ClientEserviceDetails = z.infer<typeof ClientEserviceDetails>;
+
+export const ClientAgreementDetails = z.object({
+  eserviceId: EServiceId,
+  consumerId: TenantId,
+  agreementId: AgreementId,
+  state: ClientComponentState,
+});
+export type ClientAgreementDetails = z.infer<typeof ClientAgreementDetails>;
+
+export const ClientPurposeDetails = z.object({
+  purposeId: PurposeId,
+  versionId: PurposeVersionId,
+  state: ClientComponentState,
+});
+export type ClientPurposeDetails = z.infer<typeof ClientPurposeDetails>;
+
+export const ClientStatesChain = z.object({
+  id: z.string(),
+  eservice: ClientEserviceDetails,
+  agreement: ClientAgreementDetails,
+  purpose: ClientPurposeDetails,
+});
+export type ClientStatesChain = z.infer<typeof ClientStatesChain>;
+
 export const Client = z.object({
   id: z.string(),
   consumerId: TenantId,
   name: z.string(),
-  purposes: z.array(PurposeId),
+  purposes: z.array(ClientStatesChain),
   description: z.string().optional(),
   relationships: z.array(z.string()),
   kind: ClientKind,
