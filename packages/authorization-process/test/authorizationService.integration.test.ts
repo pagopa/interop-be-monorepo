447c6cfb540b081b7bd833d387ac659e501a057e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-17 10:33:57 +0200

    Scaffold

---
+++ packages/authorization-process/test/authorizationService.integration.test.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { describe, expect, it } from "vitest";
+
+describe("sample test", () => {
+  it("sample", () => {
+    expect(1).toBe(1);
+  });
+});
