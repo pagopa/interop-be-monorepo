2c7267d50be86d136b01f9ee8ac527ba8f4b3537
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-17 14:01:20 +0200

    Add converters

--- packages/models/src/authorization/protobufConverterFromV1.ts
+++ packages/models/src/authorization/protobufConverterFromV1.ts
@@ -1,4 +1,26 @@
+import { unsafeBrandId } from "../brandedIds.js";
+import {
+  ClientAgreementDetailsV1,
+  ClientComponentStateV1,
+  ClientEServiceDetailsV1,
+  ClientKindV1,
+  ClientPurposeDetailsV1,
+  ClientStatesChainV1,
+  ClientV1,
+} from "../gen/v1/authorization/client.js";
 import { KeyUseV1, KeyV1 } from "../gen/v1/authorization/key.js";
+import { bigIntToDate } from "../utils.js";
+import {
+  Client,
+  ClientAgreementDetails,
+  ClientComponentState,
+  ClientEserviceDetails,
+  ClientKind,
+  ClientPurposeDetails,
+  ClientStatesChain,
+  clientComponentState,
+  clientKind,
+} from "./client.js";
 import { Key, KeyUse, keyUse } from "./key.js";
 
 const fromKeyUseV1 = (input: KeyUseV1): KeyUse => {
@@ -17,4 +39,79 @@ export const fromKeyV1 = (input: KeyV1): Key => ({
   ...input,
   use: fromKeyUseV1(input.use),
   createdAt: new Date(input.createdAt),
+});
+
+export const fromClientKindV1 = (input: ClientKindV1): ClientKind => {
+  switch (input) {
+    case ClientKindV1.API:
+      return clientKind.api;
+    case ClientKindV1.CONSUMER:
+      return clientKind.consumer;
+    case ClientKindV1.UNSPECIFIED$: {
+      throw new Error("Unspecified client kind");
+    }
+  }
+};
+
+export const fromClientComponentStateV1 = (
+  input: ClientComponentStateV1
+): ClientComponentState => {
+  switch (input) {
+    case ClientComponentStateV1.ACTIVE:
+      return clientComponentState.active;
+    case ClientComponentStateV1.INACTIVE:
+      return clientComponentState.inactive;
+    case ClientComponentStateV1.UNSPECIFIED$: {
+      throw new Error("Unspecified client component state");
+    }
+  }
+};
+
+export const fromClientEserviceDetailsV1 = (
+  input: ClientEServiceDetailsV1
+): ClientEserviceDetails => ({
+  eserviceId: unsafeBrandId(input.eServiceId),
+  descriptorId: unsafeBrandId(input.descriptorId),
+  state: fromClientComponentStateV1(input.state),
+  audience: input.audience,
+  voucherLifespan: input.voucherLifespan,
+});
+
+export const fromClientAgreementDetailsV1 = (
+  input: ClientAgreementDetailsV1
+): ClientAgreementDetails => ({
+  eserviceId: unsafeBrandId(input.eServiceId),
+  consumerId: unsafeBrandId(input.consumerId),
+  agreementId: unsafeBrandId(input.agreementId),
+  state: fromClientComponentStateV1(input.state),
+});
+
+export const fromClientPurposeDetailsV1 = (
+  input: ClientPurposeDetailsV1
+): ClientPurposeDetails => ({
+  purposeId: unsafeBrandId(input.purposeId),
+  versionId: unsafeBrandId(input.versionId),
+  state: fromClientComponentStateV1(input.state),
+});
+
+export const fromClientStatesChainV1 = (
+  input: ClientStatesChainV1
+): ClientStatesChain => ({
+  id: input.id,
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  eservice: fromClientEserviceDetailsV1(input.eService!),
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  agreement: fromClientAgreementDetailsV1(input.agreement!),
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  purpose: fromClientPurposeDetailsV1(input.purpose!),
+});
+
+export const fromClientV1 = (input: ClientV1): Client => ({
+  ...input,
+  consumerId: unsafeBrandId(input.consumerId),
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  purposes: input.purposes.map((item) => fromClientStatesChainV1(item.states!)),
+  kind: fromClientKindV1(input.kind),
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  createdAt: bigIntToDate(input.createdAt!),
 });
--- packages/models/src/authorization/protobufConverterToV1.ts
+++ packages/models/src/authorization/protobufConverterToV1.ts
@@ -1,6 +1,27 @@
 import { match } from "ts-pattern";
 import { KeyUseV1, KeyV1 } from "../gen/v1/authorization/key.js";
+import {
+  ClientAgreementDetailsV1,
+  ClientComponentStateV1,
+  ClientEServiceDetailsV1,
+  ClientKindV1,
+  ClientPurposeDetailsV1,
+  ClientStatesChainV1,
+  ClientV1,
+} from "../gen/v1/authorization/client.js";
+import { dateToBigInt } from "../utils.js";
 import { Key, KeyUse, keyUse } from "./key.js";
+import {
+  Client,
+  ClientAgreementDetails,
+  ClientComponentState,
+  ClientEserviceDetails,
+  ClientKind,
+  ClientPurposeDetails,
+  ClientStatesChain,
+  clientComponentState,
+  clientKind,
+} from "./client.js";
 
 const toKeyUseV1 = (input: KeyUse): KeyUseV1 =>
   match(input)
@@ -12,4 +33,64 @@ export const toKeyV1 = (input: Key): KeyV1 => ({
   ...input,
   use: toKeyUseV1(input.use),
   createdAt: input.createdAt.toString(),
+});
+
+export const toClientKindV1 = (input: ClientKind): ClientKindV1 =>
+  match(input)
+    .with(clientKind.api, () => ClientKindV1.API)
+    .with(clientKind.consumer, () => ClientKindV1.CONSUMER)
+    .exhaustive();
+
+export const toClientComponentStateV1 = (
+  input: ClientComponentState
+): ClientComponentStateV1 =>
+  match(input)
+    .with(clientComponentState.active, () => ClientComponentStateV1.ACTIVE)
+    .with(clientComponentState.inactive, () => ClientComponentStateV1.INACTIVE)
+    .exhaustive();
+
+export const toClientEserviceDetailsV1 = (
+  input: ClientEserviceDetails
+): ClientEServiceDetailsV1 => ({
+  eServiceId: input.eserviceId,
+  descriptorId: input.descriptorId,
+  state: toClientComponentStateV1(input.state),
+  audience: input.audience,
+  voucherLifespan: input.voucherLifespan,
+});
+
+export const toClientAgreementDetailsV1 = (
+  input: ClientAgreementDetails
+): ClientAgreementDetailsV1 => ({
+  eServiceId: input.eserviceId,
+  consumerId: input.consumerId,
+  agreementId: input.agreementId,
+  state: toClientComponentStateV1(input.state),
+});
+
+export const toClientPurposeDetailsV1 = (
+  input: ClientPurposeDetails
+): ClientPurposeDetailsV1 => ({
+  purposeId: input.purposeId,
+  versionId: input.versionId,
+  state: toClientComponentStateV1(input.state),
+});
+
+export const toClientStatesChainV1 = (
+  input: ClientStatesChain
+): ClientStatesChainV1 => ({
+  id: input.id,
+  eService: toClientEserviceDetailsV1(input.eservice),
+  agreement: toClientAgreementDetailsV1(input.agreement),
+  purpose: toClientPurposeDetailsV1(input.purpose),
+});
+
+export const toClientV1 = (input: Client): ClientV1 => ({
+  ...input,
+  consumerId: input.consumerId,
+  purposes: input.purposes.map((item) => ({
+    states: toClientStatesChainV1(item),
+  })),
+  kind: toClientKindV1(input.kind),
+  createdAt: dateToBigInt(input.createdAt),
 });
