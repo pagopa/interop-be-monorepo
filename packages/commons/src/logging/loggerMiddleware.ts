4c97ebcbf64dd2ed55192e604147050bc90334c5
Simone Camito <zibo.camito@gmail.com>
2024-05-21 14:51:16 +0200

    fix middleware types

--- packages/commons/src/logging/loggerMiddleware.ts
+++ packages/commons/src/logging/loggerMiddleware.ts
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 import * as express from "express";
+import { AppContext } from "../context/context.js";
 import { LoggerMetadata, logger } from "./index.js";
 
-export function loggerMiddleware(serviceName: string) {
-  return (req: any, res: any, next: express.NextFunction): void => {
+export function loggerMiddleware(serviceName: string): express.RequestHandler {
+  return (req, res, next): void => {
+    const context = (req as express.Request & { ctx?: AppContext }).ctx;
+
     const loggerMetadata: LoggerMetadata = {
       serviceName,
-      userId: req.ctx?.authData?.userId,
-      organizationId: req.ctx?.authData?.organizationId,
-      correlationId: req.ctx?.correlationId,
+      userId: context?.authData?.userId,
+      organizationId: context?.authData?.organizationId,
+      correlationId: context?.correlationId,
     };
 
     const loggerInstance = logger(loggerMetadata);
