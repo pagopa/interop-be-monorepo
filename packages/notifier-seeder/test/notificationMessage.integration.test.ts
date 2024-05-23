4cdfedbcb4cc37138e0b3280afa071b736d44dbf
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-23 10:25:52 +0200

    Update test

--- packages/notifier-seeder/test/notificationMessage.integration.test.ts
+++ packages/notifier-seeder/test/notificationMessage.integration.test.ts
@@ -251,7 +251,7 @@ describe("Notification tests", async () => {
       });
       expect(receivedAuthorizationMessage.payload).toEqual({
         clientId: mockClient.id,
-        keys: [{ ...mockClient.keys[0], createdAt: new Date() }],
+        keys: [{ ...mockClient.keys[0], createdAt: new Date().toISOString() }],
       });
 
       vi.useRealTimers();
