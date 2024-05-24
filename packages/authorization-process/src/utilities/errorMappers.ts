b975dc75447c4fc605a29b7eb147bd60afd5ab86
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-24 12:00:28 +0200

    Fix

--- packages/authorization-process/src/utilities/errorMappers.ts
+++ packages/authorization-process/src/utilities/errorMappers.ts
@@ -31,4 +31,5 @@ export const deleteClientErrorMapper = (error: ApiError<ErrorCodes>): number =>
 export const removeUserErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
     .with("clientNotFound", "userIdNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("organizationNotAllowedOnClient", () => HTTP_STATUS_FORBIDDEN)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
