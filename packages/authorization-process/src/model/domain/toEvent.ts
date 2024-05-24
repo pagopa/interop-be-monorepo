d6a38f4957d4b0ddaae975aa64fbe0701b71085d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-24 11:16:33 +0200

    Add event converter

--- packages/authorization-process/src/model/domain/toEvent.ts
+++ packages/authorization-process/src/model/domain/toEvent.ts
@@ -22,3 +22,22 @@ export function toCreateEventClientAdded(
     correlationId,
   };
 }
+
+export function toCreateEventClientDeleted(
+  client: Client,
+  correlationId: string
+): CreateEvent<AuthorizationEventV2> {
+  return {
+    streamId: client.id,
+    version: 0,
+    event: {
+      type: "ClientDeleted",
+      event_version: 2,
+      data: {
+        client: toClientV2(client),
+        clientId: client.id,
+      },
+    },
+    correlationId,
+  };
+}
