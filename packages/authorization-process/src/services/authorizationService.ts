348cc8ccc5f778d29eccc77b33c216f8e0c93621
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-05-27 10:44:35 +0200

    added getClients

--- packages/authorization-process/src/services/authorizationService.ts
+++ packages/authorization-process/src/services/authorizationService.ts
@@ -1,6 +1,7 @@
 import {
   Client,
   ClientId,
+  ListResult,
   TenantId,
   UserId,
   WithMetadata,
@@ -9,11 +10,11 @@ import {
   generateId,
   unsafeBrandId,
 } from "pagopa-interop-models";
-import { DB, Logger, eventRepository } from "pagopa-interop-commons";
+import { AuthData, DB, Logger, eventRepository } from "pagopa-interop-commons";
 import { clientNotFound } from "../model/domain/errors.js";
 import { ApiClientSeed } from "../model/domain/models.js";
 import { toCreateEventClientAdded } from "../model/domain/toEvent.js";
-import { ReadModelService } from "./readModelService.js";
+import { GetClientsFilters, ReadModelService } from "./readModelService.js";
 
 const retrieveClient = async (
   clientId: ClientId,
@@ -111,6 +112,27 @@ export function authorizationServiceBuilder(
         client,
         showUsers: client.consumerId === organizationId,
       };
+    },
+    async getClients(
+      filters: GetClientsFilters,
+      { offset, limit }: { offset: number; limit: number },
+      authData: AuthData,
+      logger: Logger
+    ): Promise<ListResult<Client>> {
+      logger.info(
+        `Retrieving clients by name ${filters.name} , userIds ${filters.userIds}`
+      );
+      const userIds = authData.userRoles.includes("security")
+        ? [authData.userId]
+        : filters.userIds.map(unsafeBrandId<UserId>);
+
+      return await readModelService.getClients(
+        { ...filters, userIds },
+        {
+          offset,
+          limit,
+        }
+      );
     },
   };
 }
