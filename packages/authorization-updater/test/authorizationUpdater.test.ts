e538444b43367583950bea08a913615979860efe
Eric Camellini <eric.camellini@gmail.com>
2024-05-28 11:39:55 +0200

    Improving tests

--- packages/authorization-updater/test/authorizationUpdater.test.ts
+++ packages/authorization-updater/test/authorizationUpdater.test.ts
@@ -77,135 +77,141 @@ describe("Authorization Updater processMessage", () => {
     vi.clearAllMocks();
   });
 
-  it("should correctly process catalog messages and call updateEServiceState", async () => {
-    const descriptor: Descriptor = {
-      ...getMockDescriptorPublished(),
-      version: "1",
-    };
-    const eservice: EService = {
-      ...getMockEService(),
-      descriptors: [descriptor],
-    };
-    const message: EServiceEventEnvelopeV2 = {
-      sequence_num: 1,
-      stream_id: eservice.id,
-      version: 1,
-      type: randomArrayItem([
-        "EServiceDescriptorPublished",
-        "EServiceDescriptorArchived",
-        "EServiceDescriptorSuspended",
-        "EServiceDescriptorActivated",
-      ]),
-      event_version: 2,
-      data: {
-        eservice: toEServiceV2(eservice),
+  it.each([
+    "EServiceDescriptorPublished",
+    "EServiceDescriptorArchived",
+    "EServiceDescriptorSuspended",
+    "EServiceDescriptorActivated",
+  ])(
+    "should correctly process a catalog message with type %t and call updateEServiceState",
+    async (eventType) => {
+      const descriptor: Descriptor = {
+        ...getMockDescriptorPublished(),
+        version: "1",
+      };
+      const eservice: EService = {
+        ...getMockEService(),
+        descriptors: [descriptor],
+      };
+      const message: EServiceEventEnvelopeV2 = {
+        sequence_num: 1,
+        stream_id: eservice.id,
+        version: 1,
+        type: eventType,
+        event_version: 2,
+        data: {
+          eservice: toEServiceV2(eservice),
+          descriptorId: descriptor.id,
+        },
+        log_date: new Date(),
+      } as EServiceEventEnvelopeV2;
+
+      await sendAuthUpdate(
+        message,
+        readModelService,
+        authorizationService,
+        genericLogger,
+        testCorrelationId
+      );
+
+      const expectedUpdateEServiceStatePayload = {
+        state: match(message)
+          .with(
+            { type: "EServiceDescriptorPublished" },
+            { type: "EServiceDescriptorActivated" },
+            () => ApiClientComponent.Values.ACTIVE
+          )
+          .otherwise(() => ApiClientComponent.Values.INACTIVE),
         descriptorId: descriptor.id,
-      },
-      log_date: new Date(),
-    } as EServiceEventEnvelopeV2;
+        audience: descriptor.audience,
+        voucherLifespan: descriptor.voucherLifespan,
+      };
 
-    await sendAuthUpdate(
-      message,
-      readModelService,
-      authorizationService,
-      genericLogger,
-      testCorrelationId
-    );
+      expect(
+        authorizationManagementClient.updateEServiceState
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        authorizationManagementClient.updateEServiceState
+      ).toHaveBeenCalledWith(expectedUpdateEServiceStatePayload, {
+        params: { eserviceId: eservice.id },
+        withCredentials: true,
+        headers: testHeaders,
+      });
 
-    const expectedUpdateEServiceStatePayload = {
-      state: match(message)
-        .with(
-          { type: "EServiceDescriptorPublished" },
-          { type: "EServiceDescriptorActivated" },
-          () => ApiClientComponent.Values.ACTIVE
-        )
-        .otherwise(() => ApiClientComponent.Values.INACTIVE),
-      descriptorId: descriptor.id,
-      audience: descriptor.audience,
-      voucherLifespan: descriptor.voucherLifespan,
-    };
+      expect(
+        authorizationManagementClient.updateAgreementState
+      ).not.toHaveBeenCalled();
+      expect(
+        authorizationManagementClient.updateAgreementAndEServiceStates
+      ).not.toHaveBeenCalled();
+    }
+  );
 
-    expect(
-      authorizationManagementClient.updateEServiceState
-    ).toHaveBeenCalledTimes(1);
-    expect(
-      authorizationManagementClient.updateEServiceState
-    ).toHaveBeenCalledWith(expectedUpdateEServiceStatePayload, {
-      params: { eserviceId: eservice.id },
-      withCredentials: true,
-      headers: testHeaders,
-    });
+  it.each([
+    "AgreementSubmitted",
+    "AgreementActivated",
+    "AgreementSuspendedByPlatform",
+    "AgreementSuspendedByConsumer",
+    "AgreementSuspendedByProducer",
+    "AgreementUnsuspendedByPlatform",
+    "AgreementUnsuspendedByConsumer",
+    "AgreementUnsuspendedByProducer",
+    "AgreementArchivedByConsumer",
+    "AgreementArchivedByUpgrade",
+  ])(
+    "Should correctly process an agreement messages with type %t and call updateAgreementState",
+    async (eventType) => {
+      const agreement: Agreement = getMockAgreement();
 
-    expect(
-      authorizationManagementClient.updateAgreementState
-    ).not.toHaveBeenCalled();
-    expect(
-      authorizationManagementClient.updateAgreementAndEServiceStates
-    ).not.toHaveBeenCalled();
-  });
+      const message: AgreementEventEnvelopeV2 = {
+        sequence_num: 1,
+        stream_id: agreement.id,
+        version: 1,
+        type: eventType,
+        event_version: 2,
+        data: {
+          agreement: toAgreementV2(agreement),
+        },
+        log_date: new Date(),
+      } as AgreementEventEnvelopeV2;
 
-  it("Should correctly process agreement messages and call updateAgreementState", async () => {
-    const agreement: Agreement = getMockAgreement();
+      await sendAuthUpdate(
+        message,
+        readModelService,
+        authorizationService,
+        genericLogger,
+        testCorrelationId
+      );
 
-    const message: AgreementEventEnvelopeV2 = {
-      sequence_num: 1,
-      stream_id: agreement.id,
-      version: 1,
-      type: randomArrayItem([
-        "AgreementSubmitted",
-        "AgreementActivated",
-        "AgreementSuspendedByPlatform",
-        "AgreementSuspendedByConsumer",
-        "AgreementSuspendedByProducer",
-        "AgreementUnsuspendedByPlatform",
-        "AgreementUnsuspendedByConsumer",
-        "AgreementUnsuspendedByProducer",
-        "AgreementArchivedByConsumer",
-        "AgreementArchivedByUpgrade",
-      ]),
-      event_version: 2,
-      data: {
-        agreement: toAgreementV2(agreement),
-      },
-      log_date: new Date(),
-    } as AgreementEventEnvelopeV2;
+      const expectedUpdateAgreementStatePayload = {
+        state: agreementStateToClientState(agreement),
+        agreementId: agreement.id,
+      };
 
-    await sendAuthUpdate(
-      message,
-      readModelService,
-      authorizationService,
-      genericLogger,
-      testCorrelationId
-    );
+      expect(
+        authorizationManagementClient.updateAgreementState
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        authorizationManagementClient.updateAgreementState
+      ).toHaveBeenCalledWith(expectedUpdateAgreementStatePayload, {
+        params: {
+          eserviceId: agreement.eserviceId,
+          consumerId: agreement.consumerId,
+        },
+        withCredentials: true,
+        headers: testHeaders,
+      });
 
-    const expectedUpdateAgreementStatePayload = {
-      state: agreementStateToClientState(agreement),
-      agreementId: agreement.id,
-    };
+      expect(
+        authorizationManagementClient.updateEServiceState
+      ).not.toHaveBeenCalled();
+      expect(
+        authorizationManagementClient.updateAgreementAndEServiceStates
+      ).not.toHaveBeenCalled();
+    }
+  );
 
-    expect(
-      authorizationManagementClient.updateAgreementState
-    ).toHaveBeenCalledTimes(1);
-    expect(
-      authorizationManagementClient.updateAgreementState
-    ).toHaveBeenCalledWith(expectedUpdateAgreementStatePayload, {
-      params: {
-        eserviceId: agreement.eserviceId,
-        consumerId: agreement.consumerId,
-      },
-      withCredentials: true,
-      headers: testHeaders,
-    });
-
-    expect(
-      authorizationManagementClient.updateEServiceState
-    ).not.toHaveBeenCalled();
-    expect(
-      authorizationManagementClient.updateAgreementAndEServiceStates
-    ).not.toHaveBeenCalled();
-  });
-
-  it("Should correctly process AgreementUpgraded messages and call updateAgreementAndEServiceStates", async () => {
+  it("Should correctly process an agreement message with type AgreementUpgraded and call updateAgreementAndEServiceStates", async () => {
     const descriptor: Descriptor = {
       ...getMockDescriptorPublished(),
       version: "1",
