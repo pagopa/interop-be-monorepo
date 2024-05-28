d6d97dbc530617a918bec1bc62f67b7a681e2ee3
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-28 10:01:38 +0200

    Add tests

---
+++ packages/authorization-process/test/removeClientKetById.test.ts
@@ -0,0 +1,144 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { describe, expect, it } from "vitest";
+import {
+  decodeProtobufPayload,
+  getMockKey,
+  getMockTenant,
+  writeInReadmodel,
+} from "pagopa-interop-commons-test";
+import {
+  Client,
+  ClientKeyDeletedV2,
+  generateId,
+  toClientV2,
+} from "pagopa-interop-models";
+import { genericLogger } from "pagopa-interop-commons";
+import {
+  clientNotFound,
+  keyNotFound,
+  organizationNotAllowedOnClient,
+} from "../src/model/domain/errors.js";
+import {
+  addOneClient,
+  authorizationService,
+  getMockClient,
+  readLastAuthorizationEvent,
+  tenants,
+} from "./utils.js";
+
+describe("remove client key", () => {
+  it("should write on event-store for removing a key from a client", async () => {
+    const mockConsumer = getMockTenant();
+    const keyToRemove = getMockKey();
+    const keyToNotRemove = getMockKey();
+
+    const mockClient: Client = {
+      ...getMockClient(),
+      consumerId: mockConsumer.id,
+      keys: [keyToRemove, keyToNotRemove],
+    };
+
+    await addOneClient(mockClient);
+    await writeInReadmodel(mockConsumer, tenants);
+
+    await authorizationService.deleteClientKeyById({
+      clientId: mockClient.id,
+      keyIdToRemove: keyToRemove.kid,
+      organizationId: mockConsumer.id,
+      correlationId: generateId(),
+      logger: genericLogger,
+    });
+
+    const writtenEvent = await readLastAuthorizationEvent(mockClient.id);
+
+    expect(writtenEvent).toMatchObject({
+      stream_id: mockClient.id,
+      version: "1",
+      type: "ClientKeyDeleted",
+      event_version: 2,
+    });
+
+    const writtenPayload = decodeProtobufPayload({
+      messageType: ClientKeyDeletedV2,
+      payload: writtenEvent.data,
+    });
+
+    expect(writtenPayload).toEqual({
+      kid: keyToRemove.kid,
+      client: toClientV2({ ...mockClient, keys: [keyToNotRemove] }),
+    });
+  });
+  it("should throw clientNotFound if the client doesn't exist", async () => {
+    const mockConsumer = getMockTenant();
+    const keyToRemove = getMockKey();
+
+    const mockClient: Client = {
+      ...getMockClient(),
+      consumerId: mockConsumer.id,
+      keys: [keyToRemove],
+    };
+
+    await addOneClient(getMockClient());
+    await writeInReadmodel(mockConsumer, tenants);
+
+    expect(
+      authorizationService.deleteClientKeyById({
+        clientId: mockClient.id,
+        keyIdToRemove: keyToRemove.kid,
+        organizationId: mockConsumer.id,
+        correlationId: generateId(),
+        logger: genericLogger,
+      })
+    ).rejects.toThrowError(clientNotFound(mockClient.id));
+  });
+  it("should throw keyNotFound if the key doesn't exist in that client", async () => {
+    const mockConsumer = getMockTenant();
+    const notExistingKeyId = generateId();
+    const keyToNotRemove = getMockKey();
+
+    const mockClient: Client = {
+      ...getMockClient(),
+      consumerId: mockConsumer.id,
+      keys: [keyToNotRemove],
+    };
+
+    await addOneClient(mockClient);
+    await writeInReadmodel(mockConsumer, tenants);
+
+    expect(
+      authorizationService.deleteClientKeyById({
+        clientId: mockClient.id,
+        keyIdToRemove: notExistingKeyId,
+        organizationId: mockConsumer.id,
+        correlationId: generateId(),
+        logger: genericLogger,
+      })
+    ).rejects.toThrowError(keyNotFound(notExistingKeyId, mockClient.id));
+  });
+  it("should throw organizationNotAllowedOnClient if the requester is not the consumer", async () => {
+    const mockConsumer1 = getMockTenant();
+    const mockConsumer2 = getMockTenant();
+    const keyToRemove = getMockKey();
+    const mockClient: Client = {
+      ...getMockClient(),
+      consumerId: mockConsumer1.id,
+      keys: [keyToRemove],
+    };
+
+    await addOneClient(mockClient);
+    await writeInReadmodel(mockConsumer1, tenants);
+    await writeInReadmodel(mockConsumer2, tenants);
+
+    expect(
+      authorizationService.deleteClientKeyById({
+        clientId: mockClient.id,
+        keyIdToRemove: keyToRemove.kid,
+        organizationId: mockConsumer2.id,
+        correlationId: generateId(),
+        logger: genericLogger,
+      })
+    ).rejects.toThrowError(
+      organizationNotAllowedOnClient(mockConsumer2.id, mockClient.id)
+    );
+  });
+});
