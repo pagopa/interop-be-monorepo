2992e6a8d05721335a2d28e4d87d9f9c11f9a1e0
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-05-30 15:53:22 +0200

    Adjust test setup

---
+++ packages/client-readmodel-writer/test/utils.ts
@@ -0,0 +1,12 @@
+import { setupTestContainersVitest } from "pagopa-interop-commons-test";
+import { inject, afterEach } from "vitest";
+
+export const { cleanup, readModelRepository } = setupTestContainersVitest(
+  inject("readModelConfig")
+);
+
+afterEach(cleanup);
+
+export const clients = readModelRepository.clients;
+
+// export const readModelService = readModelServiceBuilder(readModelRepository);
