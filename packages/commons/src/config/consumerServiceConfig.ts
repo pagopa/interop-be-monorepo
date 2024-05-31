899418a0e44b86dc57c4da880a37e23af3c69aca
Sandro Tajè <sandro.taje@gmail.com>
2024-05-31 15:18:48 +0200

    Add EmailManager

--- packages/commons/src/config/consumerServiceConfig.ts
+++ packages/commons/src/config/consumerServiceConfig.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { AWSConfig } from "./awsConfig.js";
 import { KafkaConfig } from "./kafkaConfig.js";
 import { ReadModelDbConfig } from "./readmodelDbConfig.js";
+import { EmailManagerConfig } from "./emailManagerConfig.js";
 
 export const KafkaConsumerConfig = KafkaConfig.and(AWSConfig);
 export type KafkaConsumerConfig = z.infer<typeof KafkaConsumerConfig>;
@@ -12,3 +13,6 @@ export const ReadModelWriterConfig = KafkaConsumerConfig.and(ReadModelDbConfig);
 export type ReadModelWriterConfig = z.infer<typeof ReadModelWriterConfig>;
 export const readModelWriterConfig: () => ReadModelWriterConfig = () =>
   ReadModelWriterConfig.parse(process.env);
+
+export const emailManagerConfig: () => EmailManagerConfig = () =>
+  EmailManagerConfig.parse(process.env);
---
+++ packages/commons/src/email-manager/emailManager.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+import { EmailManagerConfig } from "../config/emailManagerConfig.js";
+
+export type EmailManager = {
+  send: (
+    from: string,
+    to: string[],
+    subject: string,
+    body: string
+  ) => Promise<void>;
+};
+
+export function initEmailManager(config: EmailManagerConfig): EmailManager {
+  return {
+    send: async (
+      from: string,
+      to: string[],
+      subject: string,
+      body: string
+    ): Promise<void> => {
+      const transporter = nodemailer.createTransport({
+        host: config.emailManagerHost,
+        port: config.emailManagerPort,
+        secure: config.emailManagerPort === 465, // Use `true` for port 465, `false` for all other ports
+      });
+      await transporter.sendMail({
+        from,
+        to,
+        subject,
+        html: body, // html body
+      });
+    },
+  };
+}
