623720cbebbf1bc82033e21405f2063d9d93628c
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-03 15:27:01 +0200

    Implement consumerServiceV1

--- packages/key-readmodel-writer/src/keyConsumerServiceV1.ts
+++ packages/key-readmodel-writer/src/keyConsumerServiceV1.ts
@@ -1,15 +1,61 @@
 import { KeyCollection } from "pagopa-interop-commons";
-import { AuthorizationEventEnvelopeV1 } from "pagopa-interop-models";
+import {
+  AuthorizationEventEnvelopeV1,
+  fromKeyV1,
+  toReadModelKey,
+} from "pagopa-interop-models";
 import { match } from "ts-pattern";
 
 export async function handleMessageV1(
   message: AuthorizationEventEnvelopeV1,
-  _keys: KeyCollection
+  keys: KeyCollection
 ): Promise<void> {
-  match(message)
+  await match(message)
+    .with({ type: "KeysAdded" }, async (message) => {
+      const keysToAdd = message.data.keys.map((keyEntry) => keyEntry.value);
+
+      for (const key of keysToAdd) {
+        if (key) {
+          const version =
+            (
+              await keys.findOne({
+                "data.kid": key.kid,
+              })
+            )?.metadata.version || 0;
+
+          await keys.updateOne(
+            {
+              "data.kid": key.kid,
+              "metadata.version": { $lt: version },
+            },
+            {
+              $set: {
+                data: toReadModelKey(fromKeyV1(key)),
+                metadata: {
+                  version,
+                },
+              },
+            },
+            { upsert: true }
+          );
+        }
+        await Promise.resolve();
+      }
+    })
+    .with({ type: "KeyDeleted" }, async (message) => {
+      const version =
+        (
+          await keys.findOne({
+            "data.kid": message.data.keyId,
+          })
+        )?.metadata.version || 0;
+
+      await keys.deleteOne({
+        "data.kid": message.data.keyId,
+        "metadata.version": { $lt: version + 1 },
+      });
+    })
     .with(
-      { type: "KeysAdded" },
-      { type: "KeyDeleted" },
       { type: "KeyRelationshipToUserMigrated" },
       { type: "ClientAdded" },
       { type: "ClientDeleted" },
