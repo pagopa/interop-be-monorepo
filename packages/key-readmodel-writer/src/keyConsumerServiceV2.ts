8db40e50f6ea9e38d3eddd7f3755b773979aae58
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-03 15:12:36 +0200

    Fix

--- packages/key-readmodel-writer/src/keyConsumerServiceV2.ts
+++ packages/key-readmodel-writer/src/keyConsumerServiceV2.ts
@@ -52,7 +52,7 @@ export async function handleMessageV2(
       const version =
         (
           await keys.findOne({
-            "data.id": message.data.kid,
+            "data.kid": message.data.kid,
           })
         )?.metadata.version || 0;
 
