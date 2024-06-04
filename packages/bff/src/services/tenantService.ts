cdcc481c77d548b9b29fb7ef37b22e52b155ded5
Simone Camito <zibo.camito@gmail.com>
2024-06-04 18:26:13 +0200

    init

---
+++ packages/bff/src/services/tenantService.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function bffServiceBuilder() {
+  return {};
+}
+export type BffService = ReturnType<typeof bffServiceBuilder>;
