a8efae4a5d96765cc28133ddfaf1ee8f1131b3eb
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-05 12:02:28 +0200

    Fix tests

--- packages/commons-test/src/testUtils.ts
+++ packages/commons-test/src/testUtils.ts
@@ -161,6 +161,7 @@ export const getMockPurpose = (): Purpose => ({
   description: "Test purpose - description",
   createdAt: new Date(),
   isFreeOfCharge: true,
+  freeOfChargeReason: "Sono una Pubblica Amministrazione",
 });
 
 export const getMockPurposeVersion = (
