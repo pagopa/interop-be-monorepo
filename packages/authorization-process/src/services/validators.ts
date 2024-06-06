19ab493474e0d0a9209feed5a286b597553be373
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-06-06 17:50:02 +0200

    fix as suggest

--- packages/authorization-process/src/services/validators.ts
+++ packages/authorization-process/src/services/validators.ts
@@ -1,6 +1,27 @@
-import { TenantId } from "pagopa-interop-models";
+import { userRoles } from "pagopa-interop-commons";
+import { TenantId, UserId } from "pagopa-interop-models";
+import { selfcareV2Client } from "pagopa-interop-selfcare-v2-client";
+import { securityUserNotFound } from "../model/domain/errors.js";
 
 export const isClientConsumer = (
   consumerId: TenantId,
   organizationId: string
 ): boolean => (consumerId === organizationId ? true : false);
+
+export const assertSelfcareUser = async (
+  selfcareId: string,
+  requesterUserId: UserId,
+  userId: UserId
+): Promise<void> => {
+  const users = await selfcareV2Client.getInstitutionProductUsersUsingGET({
+    params: { institutionId: selfcareId },
+    queries: {
+      userIdForAuth: requesterUserId,
+      userId,
+      productRoles: [userRoles.SECURITY_ROLE, userRoles.ADMIN_ROLE],
+    },
+  });
+  if (users.length === 0) {
+    throw securityUserNotFound(requesterUserId, userId);
+  }
+};
