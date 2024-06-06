b37c7bd06e94baa45f77df3657d1e754bcb67fa3
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-06-06 10:52:51 +0200

    Fix the BUG

--- packages/agreement-process/test/agrementUpgradeBug.test.ts
+++ packages/agreement-process/test/agrementUpgradeBug.test.ts
@@ -23,8 +23,10 @@ import {
   VerifiedTenantAttribute,
   agreementState,
   attributeKind,
+  descriptorState,
   generateId,
   toReadModelAgreement,
+  toReadModelEService,
 } from "pagopa-interop-models";
 import { UserResponse } from "pagopa-interop-selfcare-v2-client";
 import { beforeEach, describe, expect, it, vi } from "vitest";
@@ -34,7 +36,9 @@ import {
   addOneTenant,
   agreementService,
   agreements,
+  eservices,
   selfcareV2ClientMock,
+  tenants,
 } from "./utils.js";
 
 describe("Upgrade suspended agreement activation", () => {
@@ -90,27 +94,12 @@ describe("Upgrade suspended agreement activation", () => {
       validDeclaredTenantAttribute.id
     );
 
-    const validVerifiedTenantAttribute: VerifiedTenantAttribute = {
-      ...getMockVerifiedTenantAttribute(),
-      verifiedBy: [
-        {
-          id: producer.id,
-          verificationDate: new Date(new Date().getFullYear() - 1),
-          expirationDate: new Date(new Date().getFullYear() + 1),
-          extensionDate: undefined,
-        },
-      ],
-    };
-    const validVerifiedEserviceAttribute = getMockEServiceAttribute(
-      validVerifiedTenantAttribute.id
-    );
-
     const consumer: Tenant = {
       ...getMockTenant(),
       attributes: [
         validCertifiedTenantAttribute,
         validDeclaredTenantAttribute,
-        validVerifiedTenantAttribute,
+        // validVerifiedTenantAttribute,
       ],
       selfcareId: generateId(),
       mails: [
@@ -128,8 +117,8 @@ describe("Upgrade suspended agreement activation", () => {
       ...getMockDescriptorPublished(
         descriptorId,
         [[validCertifiedEserviceAttribute]],
-        [[validDeclaredEserviceAttribute]],
-        [[validVerifiedEserviceAttribute]]
+        [[validDeclaredEserviceAttribute]]
+        // No verified attributes required in V1
       ),
       version: "1",
       agreementApprovalPolicy: "Automatic",
@@ -157,14 +146,10 @@ describe("Upgrade suspended agreement activation", () => {
       name: "A declared attribute name",
       creationTime: new Date(new Date().getFullYear() - 1),
     });
-    await addOneAttribute({
-      id: validVerifiedEserviceAttribute.id,
-      kind: attributeKind.verified,
-      description: "A verified attribute",
-      name: "A verified attribute name",
-      creationTime: new Date(new Date().getFullYear() - 1),
-    });
-    // 1) Consumer creates an Agreement
+
+    /* ================================= 
+        1) Consumer creates an Agreement
+    ================================= */
     const consumerAuthData = getRandomAuthData(consumer.id);
     const createdAgreement = await agreementService.createAgreement(
       {
@@ -182,7 +167,9 @@ describe("Upgrade suspended agreement activation", () => {
     expect(createdAgreement.state).toEqual(agreementState.draft);
     await writeInReadmodel(toReadModelAgreement(createdAgreement), agreements);
 
-    // 2) Consumer submits the agreement (making it Active)
+    /* ================================= 
+       2) Consumer submits the agreement (making it Active)
+    ================================= */
     const submittedAgreement = await agreementService.submitAgreement(
       createdAgreement.id,
       {
@@ -199,7 +186,9 @@ describe("Upgrade suspended agreement activation", () => {
     expect(submittedAgreement.state).toEqual(agreementState.active);
     await updateAgreementInReadModel(submittedAgreement, 0);
 
-    // 3) Consumer suspends agreement
+    /* ================================= 
+        3) Consumer suspends agreement
+    ================================= */
     const suspendedAgreement = await agreementService.suspendAgreement(
       submittedAgreement.id,
       {
@@ -216,16 +205,158 @@ describe("Upgrade suspended agreement activation", () => {
     expect(suspendedAgreement.suspendedByPlatform).toEqual(false);
     await updateAgreementInReadModel(suspendedAgreement, 1);
 
-    // 4) creazione nuovo descriptor V2
-    // 5) upgrade da parte del fruitore
-    // 6) attivazione da parte dell'erogatore
-    // const producerAuthData = getRandomAuthData(producer.id);
+    /* ================================= 
+        4) creazione nuovo descriptor V2
+    ================================= */
 
-    // ACTUAL
-    // L'agreement viene attivato
+    const validVerifiedEserviceAttribute = getMockEServiceAttribute();
 
-    // EXPECTED
-    // L'agreement dovrebbe essere sospeso dato che
-    // era stato sospeso dal fruitore, e ad attivarlo è l'erogatore
+    const descriptorV2: Descriptor = {
+      ...descriptorV1,
+      id: generateId(),
+      version: "2",
+      attributes: {
+        certified: descriptorV1.attributes.certified,
+        declared: descriptorV1.attributes.declared,
+        verified: [[validVerifiedEserviceAttribute]],
+      },
+    };
+
+    const updatedEservice: EService = {
+      ...eservice,
+      descriptors: [
+        {
+          ...descriptorV1,
+          state: descriptorState.suspended,
+        },
+        descriptorV2,
+      ],
+    };
+
+    await eservices.updateOne(
+      {
+        "data.id": updatedEservice.id,
+        "metadata.version": 0,
+      },
+      {
+        $set: {
+          data: toReadModelEService(updatedEservice),
+          metadata: {
+            version: 1,
+          },
+        },
+      }
+    );
+
+    /* ================================= 
+        5) upgrade da parte del fruitore
+    ================================= */
+
+    const upgradedAgreement = await agreementService.upgradeAgreement(
+      suspendedAgreement.id,
+      {
+        authData: consumerAuthData,
+        serviceName: "",
+        correlationId: "",
+        logger: genericLogger,
+      }
+    );
+
+    expect(upgradedAgreement.state).toEqual(agreementState.draft);
+    expect(upgradedAgreement.suspendedByConsumer).toEqual(true);
+    expect(upgradedAgreement.suspendedByProducer).toEqual(undefined);
+    expect(upgradedAgreement.suspendedByPlatform).toEqual(false);
+    await writeInReadmodel(toReadModelAgreement(upgradedAgreement), agreements);
+
+    /* ================================= 
+      6) Submit da parte del fruitore per far andare l'agreement in stato di PENDING
+       (valid att CERTIFIED and DECLARED)
+    ================================= */
+
+    const submittedUpgradedAgreement = await agreementService.submitAgreement(
+      upgradedAgreement.id,
+      {
+        consumerNotes:
+          "This upgrade is for transit agreement state to PENDING!",
+      },
+      {
+        authData: consumerAuthData,
+        serviceName: "",
+        correlationId: "",
+        logger: genericLogger,
+      }
+    );
+
+    expect(submittedUpgradedAgreement.state).toEqual(agreementState.pending);
+    await updateAgreementInReadModel(submittedUpgradedAgreement, 0);
+
+    /* ================================= 
+      7) Verifica attributi da parte dell'erogatore
+    ================================= */
+
+    const validVerifiedTenantAttribute: VerifiedTenantAttribute = {
+      ...getMockVerifiedTenantAttribute(validVerifiedEserviceAttribute.id),
+      verifiedBy: [
+        {
+          id: producer.id,
+          verificationDate: new Date(new Date().getFullYear() - 1),
+          expirationDate: new Date(new Date().getFullYear() + 1),
+          extensionDate: undefined,
+        },
+      ],
+    };
+
+    const updatedConsumer = {
+      ...consumer,
+      attributes: [...consumer.attributes, validVerifiedTenantAttribute],
+    };
+
+    await tenants.updateOne(
+      {
+        "data.id": updatedConsumer.id,
+        "metadata.version": 0,
+      },
+      {
+        $set: {
+          data: updatedConsumer,
+          metadata: {
+            version: 1,
+          },
+        },
+      }
+    );
+
+    await addOneAttribute({
+      id: validVerifiedTenantAttribute.id,
+      kind: attributeKind.verified,
+      description: "A verified attribute",
+      name: "A verified attribute name",
+      creationTime: new Date(new Date().getFullYear() - 1),
+    });
+
+    /* ================================= 
+      7) Attivazione dell'Agreement da parte dell'erogatore
+    ================================= */
+
+    const producerAuthData = getRandomAuthData(producer.id);
+
+    const activatedAgreement = await agreementService.activateAgreement(
+      submittedUpgradedAgreement.id,
+      {
+        authData: producerAuthData,
+        serviceName: "",
+        correlationId: "",
+        logger: genericLogger,
+      }
+    );
+
+    await updateAgreementInReadModel(activatedAgreement, 1);
+
+    // Expected
+    // Perché era stato sospeso dal fruitore, e ad attivarlo è stato l'erogatore
+    expect(activatedAgreement.state).toEqual(agreementState.suspended);
+
+    // Actual
+    // expect(activatedAgreement.state).toEqual(agreementState.active);
   });
 });
