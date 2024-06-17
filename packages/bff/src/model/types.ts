d61e5744fcffb55032d6db5d7945a52ac2081746
Simone Camito <zibo.camito@gmail.com>
2024-06-17 16:01:08 +0200

    add reverse purposes creation

--- packages/bff/src/model/types.ts
+++ packages/bff/src/model/types.ts
@@ -3,6 +3,12 @@ import { api as purposeApi } from "./generated/purpose-process/api.js";
 
 type PurposeApi = typeof purposeApi.api;
 
+export type ApiEServicePurposeSeedPayload = ZodiosBodyByPath<
+  PurposeApi,
+  "post",
+  "/reverse/purposes"
+>;
+
 export type ApiUpdateReversePurposePayload = ZodiosBodyByPath<
   PurposeApi,
   "post",
