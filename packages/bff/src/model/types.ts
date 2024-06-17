794345023ec999efdeb04be3260d6da1264e4967
paologaleotti <paologaleotti2002@gmail.com>
2024-06-17 14:34:40 +0200

    feat: added get attribute routes in BFF

--- packages/bff/src/model/types.ts
+++ packages/bff/src/model/types.ts
@@ -7,5 +7,10 @@ export type ApiSelfcareInstitution = z.infer<
 >;
 export type ApiSelfcareProduct = z.infer<typeof schemas.SelfcareProduct>;
 export type ApiSelfcareUser = z.infer<typeof schemas.User>;
+export type ApiCompactAttribute = z.infer<typeof schemas.CompactAttribute>;
 
 export type ProcessApiAttribute = z.infer<typeof attributeSchemas.Attribute>;
+export type ProcessApiAttributes = z.infer<typeof attributeSchemas.Attributes>;
+export type ProcessApiAttributeKind = z.infer<
+  typeof attributeSchemas.AttributeKind
+>;
--- packages/bff/src/services/attributeService.ts
+++ packages/bff/src/services/attributeService.ts
@@ -1,4 +1,8 @@
-import { ProcessApiAttribute } from "../model/types.js";
+import {
+  ProcessApiAttribute,
+  ProcessApiAttributeKind,
+  ProcessApiAttributes,
+} from "../model/types.js";
 import { PagoPaClients, Headers } from "../providers/clientProvider.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -12,6 +16,38 @@ export function attributeServiceBuilder(
     ): Promise<ProcessApiAttribute> {
       return attributeClient.getAttributeById({
         params: { attributeId },
+        headers: { ...requestHeaders },
+      });
+    },
+
+    async getAttributeByOriginAndCode(
+      origin: string,
+      code: string,
+      requestHeaders: Headers
+    ): Promise<ProcessApiAttribute> {
+      return attributeClient.getAttributeByOriginAndCode({
+        params: { origin, code },
+        headers: { ...requestHeaders },
+      });
+    },
+
+    async getAttributes({
+      offset,
+      limit,
+      kinds,
+      requestHeaders,
+      name,
+      origin,
+    }: {
+      offset: number;
+      limit: number;
+      kinds: ProcessApiAttributeKind[];
+      requestHeaders: Headers;
+      name?: string;
+      origin?: string;
+    }): Promise<ProcessApiAttributes> {
+      return attributeClient.getAttributes({
+        queries: { offset, limit, kinds, name, origin },
         headers: { ...requestHeaders },
       });
     },
