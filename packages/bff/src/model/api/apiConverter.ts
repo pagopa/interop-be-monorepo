02dbb7f47f47c3a0f900d001a945b3715a33b447
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-06-19 17:29:53 +0200

    Implement get Catalog route

---
+++ packages/bff/src/model/api/apiConverter.ts
@@ -0,0 +1,97 @@
+/* eslint-disable max-params */
+import { IncomingHttpHeaders } from "http";
+import { match, P } from "ts-pattern";
+import { v4 as uuidv4 } from "uuid";
+import {
+  AgreementProcessApiAgreement,
+  agreementApiState,
+} from "./agreementTypes.js";
+import {
+  BffCatalogApiEServiceResponse,
+  BffGetCatalogApiHeaders,
+} from "./bffTypes.js";
+import {
+  EServiceCatalogProcessApi,
+  EServiceCatalogProcessApiDescriptor,
+  descriptorApiState,
+} from "./catalogTypes.js";
+import { TenantProcessApiResponse } from "./tenantTypes.js";
+
+export function parseHeaders(
+  headers: IncomingHttpHeaders
+): BffGetCatalogApiHeaders {
+  return match(headers)
+    .with({ "X-Correlation-Id": P.string }, (headers) => headers)
+    .otherwise(() => ({
+      /* 
+        Otherwise case should never happen anyway this implements gurantee 
+        the same behavior of contextMiddleware: 
+        https://github.com/pagopa/interop-be-monorepo/blob/7dcbbae8f0e7cb385c5977a0304101bd1281cefb/packages/commons/src/context/context.ts#L28
+      */
+      ...headers,
+      "X-Correlation-Id": uuidv4(),
+    }));
+}
+
+export function toBffCatalogApiEServiceResponse(
+  eservice: EServiceCatalogProcessApi,
+  producerTenant: TenantProcessApiResponse,
+  hasCertifiedAttributes: boolean,
+  isRequesterEqProducer: boolean,
+  activeDescriptor?: EServiceCatalogProcessApiDescriptor,
+  agreement?: AgreementProcessApiAgreement
+): BffCatalogApiEServiceResponse {
+  const isUpgradable = (agreement: AgreementProcessApiAgreement): boolean => {
+    const eserviceDescriptor = eservice.descriptors.find(
+      (e) => e.id === agreement.descriptorId
+    );
+
+    return (
+      eserviceDescriptor !== undefined &&
+      eservice.descriptors
+        .filter((d) => Number(d.version) > Number(eserviceDescriptor.version))
+        .find(
+          (d) =>
+            (d.state === descriptorApiState.PUBLISHED ||
+              d.state === descriptorApiState.SUSPENDED) &&
+            (agreement.state === agreementApiState.ACTIVE ||
+              agreement.state === agreementApiState.SUSPENDED)
+        ) !== undefined
+    );
+  };
+
+  const partialEnhancedEservice = {
+    id: eservice.id,
+    name: eservice.name,
+    description: eservice.description,
+    producer: {
+      id: eservice.producerId,
+      name: producerTenant.name,
+    },
+    isMine: isRequesterEqProducer,
+    hasCertifiedAttributes,
+  };
+
+  return {
+    ...partialEnhancedEservice,
+    ...(activeDescriptor
+      ? {
+          activeDescriptor: {
+            id: activeDescriptor.id,
+            version: activeDescriptor.version,
+            audience: activeDescriptor.audience,
+            state: activeDescriptor.state,
+          },
+        }
+      : {}),
+    ...(agreement
+      ? {
+          agreement: {
+            id: agreement.id,
+            state: agreement.state,
+            canBeUpgraded: isUpgradable(agreement),
+          },
+        }
+      : {}),
+  };
+}
--- packages/bff/src/services/catalogService.ts
+++ packages/bff/src/services/catalogService.ts
@@ -1,30 +1,29 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { AppContext, WithLogger } from "pagopa-interop-commons";
+import { toBffCatalogApiEServiceResponse } from "../model/api/apiConverter.js";
 import {
-  AgreementProcessApiResponse,
-  AgreementProcessApiAgreement,
-  agreementApiState,
-} from "../model/api/agreementTypes.js";
-import {
-  BffCatalogApiResponse,
   BffCatalogApiEServiceResponse,
+  BffGetCatalogApiResponse,
+  BffGetCatalogApiHeaders,
 } from "../model/api/bffTypes.js";
 
 import {
+  CatalogProcessApiQueryParam,
   EServiceCatalogProcessApi,
+  EServiceCatalogProcessApiDescriptor,
   EServicesCatalogProcessApiResponse,
   descriptorApiState,
-  EServiceCatalogProcessApiDescriptor,
-  CatalogProcessApiQueryParam,
 } from "../model/api/catalogTypes.js";
 
 import { TenantProcessApiResponse } from "../model/api/tenantTypes.js";
 
+import { certifiedAttributesSatisfied } from "../model/validators.js";
 import {
   AgreementProcessClient,
   CatalogProcessClient,
   TenantProcessClient,
 } from "../providers/clientProvider.js";
-import { certifiedAttributesSatisfied } from "../model/validators.js";
+import { getLatestAgreement } from "./agreementService.js";
 
 const ACTIVE_DESCRIPTOR_STATES_FILTER = [
   descriptorApiState.DRAFT,
@@ -34,70 +33,11 @@ const ACTIVE_DESCRIPTOR_STATES_FILTER = [
 
 export type CatalogService = ReturnType<typeof catalogServiceBuilder>;
 
-const getAllAgreements = async (
-  agreementProcessClient: AgreementProcessClient,
-  consumerId: string,
-  eserviceId: string,
-  headers: { "X-Correlation-Id": string }
-): Promise<AgreementProcessApiAgreement[]> => {
-  const getAgreementsFrom = async (
-    start: number
-  ): Promise<AgreementProcessApiResponse> =>
-    await agreementProcessClient.getAgreements({
-      headers,
-      queries: {
-        consumersIds: [consumerId],
-        eservicesIds: [eserviceId],
-        offset: start,
-        limit: 50,
-      },
-    });
-
-  // Fetched all agreements in a recursive way
-  const getAgreements = async (
-    start: number
-  ): Promise<AgreementProcessApiAgreement[]> => {
-    const agreements = (await getAgreementsFrom(start)).results;
-
-    if (agreements.length >= 50) {
-      return agreements.concat(await getAgreements(start + 50));
-    }
-    return agreements;
-  };
-
-  return await getAgreements(0);
-};
-
-const isUpgradable = (
-  eservice: EServiceCatalogProcessApi,
-  agreement: AgreementProcessApiAgreement
-): boolean => {
-  const eserviceDescriptor = eservice.descriptors.find(
-    (e) => e.id === agreement.descriptorId
-  );
-
-  return (
-    eserviceDescriptor !== undefined &&
-    eservice.descriptors
-      .filter(
-        (d) =>
-          parseInt(d.version, 10) > parseInt(eserviceDescriptor.version, 10)
-      )
-      .find(
-        (d) =>
-          (d.state === descriptorApiState.PUBLISHED ||
-            d.state === descriptorApiState.SUSPENDED) &&
-          (agreement.state === agreementApiState.ACTIVE ||
-            agreement.state === agreementApiState.SUSPENDED)
-      ) !== undefined
-  );
-};
-
 const enhanceCatalogEService =
   (
     tenantProcessClient: TenantProcessClient,
     agreementProcessClient: AgreementProcessClient,
-    correlationId: string,
+    headers: BffGetCatalogApiHeaders,
     requesterId: string
   ): ((
     eservice: EServiceCatalogProcessApi
@@ -105,7 +45,6 @@ const enhanceCatalogEService =
   async (
     eservice: EServiceCatalogProcessApi
   ): Promise<BffCatalogApiEServiceResponse> => {
-    const headers = { "X-Correlation-Id": correlationId };
     const producerTenant = await tenantProcessClient.getTenant({
       headers,
       params: {
@@ -123,55 +62,33 @@ const enhanceCatalogEService =
           })
         : producerTenant;
 
-    const activeDescriptor: EServiceCatalogProcessApiDescriptor | undefined =
-      eservice.descriptors
-        .filter((d) => ACTIVE_DESCRIPTOR_STATES_FILTER.includes(d.state))
-        .sort((a, b) => parseInt(a.version, 10) - parseInt(b.version, 10))
-        .at(-1);
+    const latestActiveDescriptor:
+      | EServiceCatalogProcessApiDescriptor
+      | undefined = eservice.descriptors
+      .filter((d) => ACTIVE_DESCRIPTOR_STATES_FILTER.includes(d.state))
+      .sort((a, b) => Number(a.version) - Number(b.version))
+      .at(-1);
 
-    const agreements = await getAllAgreements(
+    const latestAgreement = await getLatestAgreement(
       agreementProcessClient,
       requesterId,
-      eservice.id,
+      eservice,
       headers
     );
-    const latestAgreement = agreements.length > 0 ? agreements[0] : null;
 
-    if (!latestAgreement) {
-      // TODO : improve error handling
-      throw new Error("No agreement found");
-    }
-
-    const canBeUpgraded = isUpgradable(eservice, latestAgreement);
-
+    const isRequesterEqProducer = requesterId === eservice.producerId;
     const hasCertifiedAttributes =
-      activeDescriptor !== undefined &&
-      certifiedAttributesSatisfied(activeDescriptor, requesterTenant);
+      latestActiveDescriptor !== undefined &&
+      certifiedAttributesSatisfied(latestActiveDescriptor, requesterTenant);
 
-    return {
-      id: eservice.id,
-      name: eservice.name,
-      description: eservice.description,
-      producer: {
-        id: eservice.producerId,
-        name: producerTenant.name,
-      },
-      agreement: {
-        id: latestAgreement.id,
-        state: latestAgreement.state,
-        canBeUpgraded,
-      },
-      isMine: requesterId === eservice.producerId,
+    return toBffCatalogApiEServiceResponse(
+      eservice,
+      requesterTenant,
       hasCertifiedAttributes,
-      activeDescriptor: activeDescriptor
-        ? {
-            id: activeDescriptor.id,
-            version: activeDescriptor.version,
-            audience: activeDescriptor.audience,
-            state: activeDescriptor.state,
-          }
-        : undefined,
-    };
+      isRequesterEqProducer,
+      latestActiveDescriptor,
+      latestAgreement
+    );
   };
 
 export function catalogServiceBuilder(
@@ -181,17 +98,16 @@ export function catalogServiceBuilder(
 ) {
   return {
     getCatalog: async (
-      correlationId: string,
-      requesterId: string,
-      queries: CatalogProcessApiQueryParam
-    ): Promise<BffCatalogApiResponse> => {
+      context: WithLogger<AppContext>,
+      queries: CatalogProcessApiQueryParam,
+      headers: BffGetCatalogApiHeaders
+    ): Promise<BffGetCatalogApiResponse> => {
+      const requesterId = context.authData.organizationId;
       const { offset, limit } = queries;
 
       const eservicesResponse: EServicesCatalogProcessApiResponse =
         await catalogProcessClient.getEServices({
-          headers: {
-            "X-Correlation-Id": correlationId,
-          },
+          headers,
           queries,
         });
 
@@ -200,12 +116,12 @@ export function catalogServiceBuilder(
           enhanceCatalogEService(
             tenantProcessClient,
             agreementProcessClient,
-            correlationId,
+            headers,
             requesterId
           )
         )
       );
-      const response: BffCatalogApiResponse = {
+      const response: BffGetCatalogApiResponse = {
         results,
         pagination: {
           offset,
