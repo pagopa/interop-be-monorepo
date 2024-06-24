66c97615a3c341e76680e8a23f6df0d054e8c0fe
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-06-24 18:06:04 +0200

    rename ErrorCodes class

--- packages/bff/src/utilities/errorMappers.ts
+++ packages/bff/src/utilities/errorMappers.ts
@@ -1,17 +1,19 @@
 import { constants } from "http2";
 import { ApiError, CommonErrorCodes } from "pagopa-interop-models";
 import { match } from "ts-pattern";
-import { BFFErrorCodes } from "../model/domain/errors.js";
+import { ErrorCodes } from "../model/domain/errors.js";
 
-type ErrorCodes = BFFErrorCodes | CommonErrorCodes;
+type BffErrorCodes = ErrorCodes | CommonErrorCodes;
 
 const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = constants;
 
-export const bffGetCatalogErrorMapper = (error: ApiError<ErrorCodes>): number =>
+export const bffGetCatalogErrorMapper = (
+  error: ApiError<BffErrorCodes>
+): number =>
   match(error.code).otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const reversePurposeUpdateErrorMapper = (
-  error: ApiError<ErrorCodes>
+  error: ApiError<BffErrorCodes>
 ): number =>
   match(error.code)
     .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
