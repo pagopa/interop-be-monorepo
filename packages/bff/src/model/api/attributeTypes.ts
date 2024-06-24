6e530f0ddb080c9ddd506d6bcaf76bd012da411e
paologaleotti <paologaleotti2002@gmail.com>
2024-06-24 14:37:23 +0200

    fix: consistency and small refactor

--- packages/bff/src/model/api/attributeTypes.ts
+++ packages/bff/src/model/api/attributeTypes.ts
@@ -2,12 +2,19 @@ import { z } from "zod";
 import { schemas } from "../generated/api.js";
 import { schemas as attributeSchemas } from "../generated/attribute-process/api.js";
 
-export type ApiCompactAttribute = z.infer<typeof schemas.CompactAttribute>;
-export type ApiAttributeSeed = z.infer<typeof schemas.AttributeSeed>;
-export type ApiAttribute = z.infer<typeof schemas.Attribute>;
+export type BffApiCompactAttribute = z.infer<typeof schemas.CompactAttribute>;
+export type BffApiAttributeSeed = z.infer<typeof schemas.AttributeSeed>;
+export type BffApiAttribute = z.infer<typeof schemas.Attribute>;
 
-export type ProcessApiAttribute = z.infer<typeof attributeSchemas.Attribute>;
-export type ProcessApiAttributes = z.infer<typeof attributeSchemas.Attributes>;
-export type ProcessApiAttributeKind = z.infer<
+export type AttributeProcessApiAttribute = z.infer<
+  typeof attributeSchemas.Attribute
+>;
+export type AttributeProcessApiAttributes = z.infer<
+  typeof attributeSchemas.Attributes
+>;
+export type AttributeProcessApiAttributeKind = z.infer<
   typeof attributeSchemas.AttributeKind
+>;
+export type AttributeProcessApiAttributeSeed = z.infer<
+  typeof attributeSchemas.CertifiedAttributeSeed
 >;
--- packages/bff/src/model/api/selfcareTypes.ts
+++ packages/bff/src/model/api/selfcareTypes.ts
@@ -4,5 +4,5 @@ import { schemas } from "../generated/api.js";
 export type ApiSelfcareInstitution = z.infer<
   typeof schemas.SelfcareInstitution
 >;
-export type ApiSelfcareProduct = z.infer<typeof schemas.SelfcareProduct>;
-export type ApiSelfcareUser = z.infer<typeof schemas.User>;
+export type BffApiSelfcareProduct = z.infer<typeof schemas.SelfcareProduct>;
+export type BffApiSelfcareUser = z.infer<typeof schemas.User>;
