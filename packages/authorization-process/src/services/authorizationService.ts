96ddf04732993ceace18cb9787dcf8ee2c1e6462
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2024-06-24 12:50:04 +0200

    IMN-545 getClientById in auth-process (#545)

--- packages/authorization-process/src/services/authorizationService.ts
+++ packages/authorization-process/src/services/authorizationService.ts
@@ -1,16 +1,51 @@
-import { DB } from "pagopa-interop-commons";
+import {
+  Client,
+  ClientId,
+  TenantId,
+  WithMetadata,
+} from "pagopa-interop-models";
+import { DB, Logger } from "pagopa-interop-commons";
+import { clientNotFound } from "../model/domain/errors.js";
 import { ReadModelService } from "./readModelService.js";
+import { isClientConsumer } from "./validators.js";
+
+const retrieveClient = async (
+  clientId: ClientId,
+  readModelService: ReadModelService
+): Promise<WithMetadata<Client>> => {
+  const client = await readModelService.getClientById(clientId);
+  if (!client) {
+    throw clientNotFound(clientId);
+  }
+  return client;
+};
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function authorizationServiceBuilder(
   _dbInstance: DB,
-  _readModelService: ReadModelService
+  readModelService: ReadModelService
 ) {
-  // const repository = eventRepository(dbInstance, authorizationEventToBinaryData);
+  // const repository = eventRepository(
+  //   dbInstance,
+  //   authorizationEventToBinaryData
+  // );
 
   return {
-    sample(): string {
-      return "sample";
+    async getClientById({
+      clientId,
+      organizationId,
+      logger,
+    }: {
+      clientId: ClientId;
+      organizationId: TenantId;
+      logger: Logger;
+    }): Promise<{ client: Client; showUsers: boolean }> {
+      logger.info(`Retrieving Client ${clientId}`);
+      const client = await retrieveClient(clientId, readModelService);
+      return {
+        client: client.data,
+        showUsers: isClientConsumer(client.data.consumerId, organizationId),
+      };
     },
   };
 }
