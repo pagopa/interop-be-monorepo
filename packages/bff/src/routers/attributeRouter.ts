687dec15ad643e9abdbf51b383ffe2196a31886a
paologaleotti <paologaleotti2002@gmail.com>
2024-06-25 14:51:33 +0200

    refactor: new headers handling in bff attribute

--- packages/bff/src/routers/attributeRouter.ts
+++ packages/bff/src/routers/attributeRouter.ts
@@ -3,7 +3,6 @@ import { ZodiosRouter } from "@zodios/express";
 import {
   ExpressContext,
   ZodiosContext,
-  fromAppContext,
   zodiosValidationErrorToApiProblem,
 } from "pagopa-interop-commons";
 import { api } from "../model/generated/api.js";
@@ -11,6 +10,7 @@ import { PagoPAInteropBeClients } from "../providers/clientProvider.js";
 import { attributeServiceBuilder } from "../services/attributeService.js";
 import { makeApiProblem } from "../model/domain/errors.js";
 import { attributeEmptyErrorMapper } from "../utilities/errorMappers.js";
+import { fromBffAppContext } from "../utilities/context.js";
 
 const attributeRouter = (
   ctx: ZodiosContext,
@@ -24,17 +24,12 @@ const attributeRouter = (
 
   attributeRouter
     .post("/certifiedAttributes", async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
       try {
-        const headers = {
-          "X-Correlation-Id": ctx.correlationId,
-          Authorization: req.headers.authorization as string,
-        };
         const result = await attributeService.createCertifiedAttribute(
           req.body,
-          headers,
-          ctx.logger
+          ctx
         );
 
         return res.status(200).json(result).end();
@@ -49,17 +44,12 @@ const attributeRouter = (
     })
 
     .post("/verifiedAttributes", async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
       try {
-        const headers = {
-          "X-Correlation-Id": ctx.correlationId,
-          Authorization: req.headers.authorization as string,
-        };
         const result = await attributeService.createVerifiedAttribute(
           req.body,
-          headers,
-          ctx.logger
+          ctx
         );
 
         return res.status(200).json(result).end();
@@ -74,18 +64,12 @@ const attributeRouter = (
     })
 
     .post("/declaredAttributes", async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
       try {
-        const headers = {
-          "X-Correlation-Id": ctx.correlationId,
-          Authorization: req.headers.authorization as string,
-        };
-
         const result = await attributeService.createDeclaredAttribute(
           req.body,
-          headers,
-          ctx.logger
+          ctx
         );
 
         return res.status(200).json(result).end();
@@ -100,13 +84,9 @@ const attributeRouter = (
     })
 
     .get("/attributes", async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
       try {
-        const headers = {
-          "X-Correlation-Id": ctx.correlationId,
-          Authorization: req.headers.authorization as string,
-        };
         const { q, offset, limit, kinds, origin } = req.query;
 
         const attributes = await attributeService.getAttributes({
@@ -115,8 +95,7 @@ const attributeRouter = (
           limit,
           kinds,
           origin,
-          headers,
-          logger: ctx.logger,
+          ctx,
         });
 
         return res
@@ -136,17 +115,12 @@ const attributeRouter = (
     })
 
     .get("/attributes/:attributeId", async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
       try {
-        const headers = {
-          "X-Correlation-Id": ctx.correlationId,
-          Authorization: req.headers.authorization as string,
-        };
         const result = await attributeService.getAttributeById(
           req.params.attributeId,
-          headers,
-          ctx.logger
+          ctx
         );
 
         return res.status(200).json(result).end();
@@ -161,18 +135,13 @@ const attributeRouter = (
     })
 
     .get("/attributes/origin/:origin/code/:code", async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
       try {
-        const headers = {
-          "X-Correlation-Id": ctx.correlationId,
-          Authorization: req.headers.authorization as string,
-        };
         const result = await attributeService.getAttributeByOriginAndCode(
           req.params.origin,
           req.params.code,
-          headers,
-          ctx.logger
+          ctx
         );
 
         return res.status(200).json(result).end();
