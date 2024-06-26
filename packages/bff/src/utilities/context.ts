286a2c960c494d0ba40909be4e44ff3fe120ec0e
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-06-26 18:28:20 +0200

    BffContext building a new correlationID instead using provided one with header's request

--- packages/bff/src/utilities/context.ts
+++ packages/bff/src/utilities/context.ts
@@ -1,5 +1,6 @@
 import { IncomingHttpHeaders } from "http";
 import { AppContext, WithLogger, logger } from "pagopa-interop-commons";
+import { v4 as uuidv4 } from "uuid";
 
 export type Headers = {
   "X-Correlation-Id": string;
@@ -12,10 +13,12 @@ export function fromBffAppContext(
   ctx: AppContext,
   headers: IncomingHttpHeaders
 ): WithLogger<BffAppContext> {
+  const correlationId = uuidv4();
   return {
     ...ctx,
+    correlationId, // override eventual correlationId provided in the BFF headers
     headers: {
-      "X-Correlation-Id": ctx.correlationId,
+      "X-Correlation-Id": correlationId,
       Authorization: headers.authorization,
     },
     logger: logger({ ...ctx }),
