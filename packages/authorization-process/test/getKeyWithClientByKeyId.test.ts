fb6245d0753e64231e4d846283af0c50ba3cb02a
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-07-02 14:01:51 +0200

    added tests

---
+++ packages/authorization-process/test/getKeyWithClientByKeyId.test.ts
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import crypto, { JsonWebKey } from "crypto";
+import {
+  createJWK,
+  decodeBase64ToPem,
+  genericLogger,
+} from "pagopa-interop-commons";
+import { Client } from "pagopa-interop-models";
+import { describe, it, expect } from "vitest";
+import { getMockClient, getMockKey } from "pagopa-interop-commons-test";
+import { JWKKey } from "../src/model/domain/models.js";
+import { clientNotFound, keyNotFound } from "../src/model/domain/errors.js";
+import { addOneClient, authorizationService } from "./utils.js";
+
+describe("getKeyWithClientByKeyId", async () => {
+  it("should get the jwkKey with client by kid if it exists", async () => {
+    const key = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+    }).publicKey;
+
+    const pemKey = Buffer.from(
+      key.export({ type: "pkcs1", format: "pem" })
+    ).toString("base64");
+
+    const mockKey1 = { ...getMockKey(), encodedPem: pemKey };
+
+    const jwk: JsonWebKey = createJWK(decodeBase64ToPem(pemKey));
+    const JwkKey: JWKKey = {
+      ...jwk,
+      kty: jwk.kty!,
+      keyOps: [],
+      use: mockKey1.use,
+      alg: mockKey1.algorithm,
+      kid: mockKey1.kid,
+      x5u: "",
+      x5t: "",
+      x5tS256: [],
+      x5c: [],
+      oth: [],
+    };
+    const mockKey2 = getMockKey();
+    const mockClient: Client = {
+      ...getMockClient(),
+      keys: [mockKey1, mockKey2],
+    };
+    await addOneClient(mockClient);
+
+    const { JWKKey, client } =
+      await authorizationService.getKeyWithClientByKeyId({
+        clientId: mockClient.id,
+        kid: mockKey1.kid,
+        logger: genericLogger,
+      });
+    expect(JWKKey).toEqual(JwkKey);
+    expect(client).toEqual(mockClient);
+  });
+
+  it("should throw clientNotFound if the client doesn't exist", async () => {
+    const mockKey = getMockKey();
+    const mockClient: Client = {
+      ...getMockClient(),
+    };
+
+    expect(
+      authorizationService.getKeyWithClientByKeyId({
+        clientId: mockClient.id,
+        kid: mockKey.kid,
+        logger: genericLogger,
+      })
+    ).rejects.toThrowError(clientNotFound(mockClient.id));
+  });
+  it("should throw keyNotFound if the key doesn't exist", async () => {
+    const mockKey = getMockKey();
+    const mockClient: Client = {
+      ...getMockClient(),
+      keys: [getMockKey()],
+    };
+    await addOneClient(mockClient);
+
+    expect(
+      authorizationService.getKeyWithClientByKeyId({
+        clientId: mockClient.id,
+        kid: mockKey.kid,
+        logger: genericLogger,
+      })
+    ).rejects.toThrowError(keyNotFound(mockKey.kid, mockClient.id));
+  });
+});
