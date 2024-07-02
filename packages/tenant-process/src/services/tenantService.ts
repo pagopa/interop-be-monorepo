910c337755999997c9879913320e3be5e3d2672e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-07-02 12:48:59 +0200

    Refactor

--- packages/tenant-process/src/services/tenantService.ts
+++ packages/tenant-process/src/services/tenantService.ts
@@ -67,7 +67,6 @@ import {
   getTenantKindLoadingCertifiedAttributes,
   assertOrganizationVerifierExist,
   assertExpirationDateExist,
-  assertTenantExists,
   getTenantCertifierId,
   assertVerifiedAttributeOperationAllowed,
 } from "./validators.js";
@@ -657,8 +656,7 @@ export function tenantServiceBuilder(
       offset: number;
       limit: number;
     }): Promise<ListResult<CertifiedAttributeQueryResult>> {
-      const tenant = await readModelService.getTenantById(organizationId);
-      assertTenantExists(organizationId, tenant);
+      const tenant = await retrieveTenant(organizationId, readModelService);
 
       const certifierId = getTenantCertifierId(tenant.data);
 
