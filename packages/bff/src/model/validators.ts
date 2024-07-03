b6d50d0649738311c3dba2fd4af4279f93f38b56
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-07-03 14:38:53 +0200

    Bff adopt validator from Lifecycle package

--- packages/bff/src/model/validators.ts
+++ packages/bff/src/model/validators.ts
@@ -1,10 +1,16 @@
-import { certifiedAttributesSatisfied } from "pagopa-interop-commons";
-import { toDescriptor, toTenant } from "./api/apiConverter.js";
+import { certifiedAttributesSatisfied } from "pagopa-interop-lifecycle";
 import { CatalogProcessApiEServiceDescriptor } from "./api/catalogTypes.js";
 import { TenantProcessApiTenant } from "./api/tenantTypes.js";
+import {
+  toDescriptorWithOnlyAttributes,
+  toTenantWithOnlyAttributes,
+} from "./api/apiConverter.js";
 
 export const catalogProcessApiEServiceDescriptorCertifiedAttributesSatisfied = (
   descriptor: CatalogProcessApiEServiceDescriptor,
   tenant: TenantProcessApiTenant
 ): boolean =>
-  certifiedAttributesSatisfied(toDescriptor(descriptor), toTenant(tenant));
+  certifiedAttributesSatisfied(
+    toDescriptorWithOnlyAttributes(descriptor),
+    toTenantWithOnlyAttributes(tenant)
+  );
