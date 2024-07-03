dcb782386d6761ca5b66de815a84adc5b2786588
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-07-03 17:29:16 +0200

    Remove not needed parameter

--- packages/tenant-process/src/services/tenantService.ts
+++ packages/tenant-process/src/services/tenantService.ts
@@ -780,7 +780,6 @@ export function tenantServiceBuilder(
       logger,
     }: {
       organizationId: TenantId;
-      tenantId: TenantId;
       tenantOrigin: string;
       tenantExternalId: string;
       attributeOrigin: string;
