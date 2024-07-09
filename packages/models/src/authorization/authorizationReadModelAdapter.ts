19182580e0d407771987ce166b8206221336a395
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-07-09 17:23:30 +0200

    Fix circular dep in models for authorization (#723)

--- packages/models/src/authorization/authorizationReadModelAdapter.ts
+++ packages/models/src/authorization/authorizationReadModelAdapter.ts
@@ -2,8 +2,7 @@ import {
   ClientReadModel,
   KeyReadModel,
 } from "../read-models/authorizationReadModel.js";
-import { Client } from "./client.js";
-import { Key } from "./key.js";
+import { Client, Key } from "./client.js";
 
 export const toReadModelKey = (key: Key): KeyReadModel => ({
   ...key,
