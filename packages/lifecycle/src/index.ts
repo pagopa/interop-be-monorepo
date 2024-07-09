9d87b25656ddf81cfd0bb8961764d6e9908ef973
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-07-09 11:35:25 +0200

    Remove lifecycle package

--- packages/lifecycle/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./validator/attributesValidator.js";
-export * from "./filters/attributesFilter.js";
-export * from "./models/models.js";
--- packages/lifecycle/src/models/models.ts
+++
@@ -1,4 +0,0 @@
-import { Descriptor, Tenant } from "pagopa-interop-models";
-
-export type DescriptorWithOnlyAttributes = Pick<Descriptor, "attributes">;
-export type TenantWithOnlyAttributes = Pick<Tenant, "attributes">;
