0a4333fe9d0b5bb299ff54ad28e97559809a1b5d
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-07-10 18:08:12 +0200

    Implement GetProducerEserviceByID

--- packages/bff/src/routers/catalogRouter.ts
+++ packages/bff/src/routers/catalogRouter.ts
@@ -105,6 +105,24 @@ const catalogRouter = (
         return res.status(errorRes.status).json(errorRes).end();
       }
     })
+    .get("/producers/eservices", async (_req, res) => res.status(501).send())
+    .get("/producers/eservices/:eserviceId", async (req, res) => {
+      const ctx = fromBffAppContext(req.ctx, req.headers);
+      try {
+        const response = await catalogService.getProducerEServiceDetails(
+          req.params.eserviceId,
+          ctx
+        );
+        return res.status(200).json(response).send();
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          bffGetCatalogErrorMapper,
+          ctx.logger
+        );
+        return res.status(errorRes.status).json(errorRes).end();
+      }
+    })
     .get(
       "/producers/eservices/:eserviceId/descriptors/:descriptorId",
       async (req, res) => {
