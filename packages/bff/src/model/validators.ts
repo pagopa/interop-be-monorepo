56b865ee6ad3e5afd90f20889b59d3ebb8b6c813
Simone Camito <32327779+MalpenZibo@users.noreply.github.com>
2024-07-10 12:08:02 +0200

    IMN 646 - Migrate BFF to api-clients package (#703)

--- packages/bff/src/model/validators.ts
+++ packages/bff/src/model/validators.ts
@@ -1,14 +1,13 @@
 import { certifiedAttributesSatisfied } from "pagopa-interop-agreement-lifecycle";
-import { CatalogProcessApiEServiceDescriptor } from "./api/catalogTypes.js";
-import { TenantProcessApiTenant } from "./api/tenantTypes.js";
+import { catalogApi, tenantApi } from "pagopa-interop-api-clients";
 import {
   toDescriptorWithOnlyAttributes,
   toTenantWithOnlyAttributes,
 } from "./api/apiConverter.js";
 
 export const catalogProcessApiEServiceDescriptorCertifiedAttributesSatisfied = (
-  descriptor: CatalogProcessApiEServiceDescriptor,
-  tenant: TenantProcessApiTenant
+  descriptor: catalogApi.EServiceDescriptor,
+  tenant: tenantApi.Tenant
 ): boolean =>
   certifiedAttributesSatisfied(
     toDescriptorWithOnlyAttributes(descriptor),
