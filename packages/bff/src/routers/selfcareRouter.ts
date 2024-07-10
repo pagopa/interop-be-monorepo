5168336c02d95446e516fdb6a95bb6747911f534
Simone Camito <32327779+MalpenZibo@users.noreply.github.com>
2024-07-10 12:08:02 +0200

    IMN 646 - Migrate BFF to api-clients package (#703)

--- packages/bff/src/routers/selfcareRouter.ts
+++ packages/bff/src/routers/selfcareRouter.ts
@@ -5,18 +5,21 @@ import {
   ZodiosContext,
   zodiosValidationErrorToApiProblem,
 } from "pagopa-interop-commons";
-import { api } from "../model/generated/api.js";
+import { bffApi } from "pagopa-interop-api-clients";
 
 const selfcareRouter = (
   ctx: ZodiosContext
 ): ZodiosRouter<ZodiosEndpointDefinitions, ExpressContext> => {
-  const selfcareRouter = ctx.router(api.api, {
+  const selfcareRouter = ctx.router(bffApi.selfcareApi.api, {
     validationErrorHandler: zodiosValidationErrorToApiProblem,
   });
 
   selfcareRouter
     .get("/users/:userId", async (_req, res) => res.status(501).send())
     .get("/selfcare/institutions/products", async (_req, res) =>
+      res.status(501).send()
+    )
+    .get("/tenants/:tenantId/users", async (_req, res) =>
       res.status(501).send()
     )
     .get("/selfcare/institutions", async (_req, res) => res.status(501).send());
