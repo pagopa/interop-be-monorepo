3b9d30c171d36ecef7d5a996a8f650c0388108f7
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-07-10 17:04:40 +0200

    remove wrong changes

--- packages/bff/src/model/api/attributeTypes.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import {
-  api as attribute,
-  schemas,
-} from "../generated/attribute-process/api.js";
-
-export type AttributeProcessClientApi = typeof attribute.api;
-export type AttributeProcessApi = z.infer<typeof schemas.Attribute>;
