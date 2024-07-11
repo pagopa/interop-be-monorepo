90cffa89037755b2ebe85ec3d19da781a9ff2e09
Sandro Tajè <sandro.taje@gmail.com>
2024-07-11 15:19:44 +0200

    Add empty error mapper

--- packages/bff/src/routers/catalogRouter.ts
+++ packages/bff/src/routers/catalogRouter.ts
@@ -9,14 +9,11 @@ import {
 } from "pagopa-interop-commons";
 import { unsafeBrandId } from "pagopa-interop-models";
 import { toEserviceCatalogProcessQueryParams } from "../model/api/converters/catalogClientApiConverter.js";
-import { makeApiProblem } from "../model/domain/errors.js";
+import { emptyErrorMapper, makeApiProblem } from "../model/domain/errors.js";
 import { PagoPAInteropBeClients } from "../providers/clientProvider.js";
 import { catalogServiceBuilder } from "../services/catalogService.js";
 import { fromBffAppContext } from "../utilities/context.js";
-import {
-  bffGetCatalogErrorMapper,
-  emptyErrorMapper,
-} from "../utilities/errorMappers.js";
+import { bffGetCatalogErrorMapper } from "../utilities/errorMappers.js";
 
 const catalogRouter = (
   ctx: ZodiosContext,
