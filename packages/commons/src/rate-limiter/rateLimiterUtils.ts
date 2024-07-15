86af92bc2e9caecbc7bd406cb93ddee20d1b28a9
Eric Camellini <eric.camellini@gmail.com>
2024-07-15 10:40:20 +0200

    Using headers as in the API spec

--- packages/commons/src/rate-limiter/rateLimiterUtils.ts
+++ packages/commons/src/rate-limiter/rateLimiterUtils.ts
@@ -4,12 +4,7 @@ import { RateLimiterStatus } from "./rateLimiterModel.js";
 export const rateLimiterHeadersFromStatus = (
   rateLimiterStatus: RateLimiterStatus
 ): OutgoingHttpHeaders => ({
-  "X-RateLimit-Limit": rateLimiterStatus.maxRequests,
-  "X-RateLimit-Interval": rateLimiterStatus.rateInterval,
-  "X-RateLimit-Remaining": rateLimiterStatus.remainingRequests,
+  "X-Rate-Limit-Limit": rateLimiterStatus.maxRequests,
+  "X-Rate-Limit-Interval": rateLimiterStatus.rateInterval,
+  "X-Rate-Limit-Remaining": rateLimiterStatus.remainingRequests,
 });
-// TODO why are these different from the ones in the openapi spec?
-// The spec specifies the headers with a dash, Rate-Limit instead of RateLimit
-// "X-Rate-Limit-Limit
-// "X-Rate-Limit-Remaining"
-// "X-Rate-Limit-Interval"
