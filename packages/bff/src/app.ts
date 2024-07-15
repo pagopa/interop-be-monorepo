f01e56ecb9274305f36129757e463bd1bc9c4ee6
Eric Camellini <eric.camellini@gmail.com>
2024-07-15 10:57:37 +0200

    Adding comment

--- packages/bff/src/app.ts
+++ packages/bff/src/app.ts
@@ -26,6 +26,7 @@ const redisRateLimiter = initRedisRateLimiter({
   limiterGroup: "BFF",
   maxRequests: config.rateLimiterMaxRequests,
   rateInterval: config.rateLimiterRateInterval,
+  burstPercentage: config.rateLimiterBurstPercentage,
   redisHost: config.rateLimiterRedisHost,
   redisPort: config.rateLimiterRedisPort,
   timeout: config.rateLimiterTimeout,
