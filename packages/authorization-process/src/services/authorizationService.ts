45808576e5e8a62027e100254944a3b4acf6ad24
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-07-18 16:32:22 +0200

    Fix

--- packages/authorization-process/src/services/authorizationService.ts
+++ packages/authorization-process/src/services/authorizationService.ts
@@ -680,7 +680,7 @@ export function authorizationServiceBuilder(
       clientId: ClientId;
       kid: string;
       logger: Logger;
-    }): Promise<authorizationApi.ApiKeyWithClient> {
+    }): Promise<authorizationApi.ClientWithKeys> {
       logger.info(`Getting client ${clientId} and key ${kid}`);
       const client = await retrieveClient(clientId, readModelService);
       const key = client.data.keys.find((key) => key.kid === kid);
