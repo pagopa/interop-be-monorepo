bad03976461eb16fd65d46c553cd4cac0f00d8e4
Sandro Tajè <sandro.taje@gmail.com>
2024-07-18 08:24:29 +0200

    Remove useless destructuring

--- packages/bff/src/services/catalogService.ts
+++ packages/bff/src/services/catalogService.ts
@@ -297,38 +297,30 @@ export function catalogServiceBuilder(
     createEService: async (
       eServiceSeed: bffApi.EServiceSeed,
       { headers }: WithLogger<BffAppContext>
-    ): Promise<bffApi.CreatedResource> => {
-      const { id } = await catalogProcessClient.createEService(eServiceSeed, {
+    ): Promise<bffApi.CreatedResource> =>
+      await catalogProcessClient.createEService(eServiceSeed, {
         headers,
-      });
-      return { id };
-    },
+      }),
     updateEServiceById: async (
       eServiceId: string,
       updateEServiceSeed: bffApi.UpdateEServiceSeed,
       { headers }: WithLogger<BffAppContext>
-    ): Promise<bffApi.CreatedResource> => {
-      const { id } = await catalogProcessClient.updateEServiceById(
-        updateEServiceSeed,
-        {
-          headers,
-          params: {
-            eServiceId,
-          },
-        }
-      );
-      return { id };
-    },
+    ): Promise<bffApi.CreatedResource> =>
+      await catalogProcessClient.updateEServiceById(updateEServiceSeed, {
+        headers,
+        params: {
+          eServiceId,
+        },
+      }),
     deleteEService: async (
       eServiceId: string,
       { headers }: WithLogger<BffAppContext>
-    ): Promise<void> => {
+    ): Promise<void> =>
       await catalogProcessClient.deleteEService(undefined, {
         headers,
         params: {
           eServiceId,
         },
-      });
-    },
+      }),
   };
 }
