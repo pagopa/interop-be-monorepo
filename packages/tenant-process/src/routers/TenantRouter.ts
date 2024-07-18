135aee962c4eee288c2fa58d936c74ad720a29d5
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-07-18 15:27:27 +0200

    refactor

--- packages/tenant-process/src/routers/TenantRouter.ts
+++ packages/tenant-process/src/routers/TenantRouter.ts
@@ -339,31 +339,6 @@ const tenantsRouter = (
         }
       }
     )
-    .post(
-      "/tenants/attributes/declared",
-      authorizationMiddleware([ADMIN_ROLE]),
-      async (req, res) => {
-        const ctx = fromAppContext(req.ctx);
-        try {
-          const tenant = await tenantService.addDeclaredAttribute(
-            {
-              tenantAttributeSeed: req.body,
-              organizationId: req.ctx.authData.organizationId,
-              correlationId: req.ctx.correlationId,
-            },
-            ctx.logger
-          );
-          return res.status(200).json(toApiTenant(tenant)).end();
-        } catch (error) {
-          const errorRes = makeApiProblem(
-            error,
-            addDeclaredAttributeErrorMapper,
-            ctx.logger
-          );
-          return res.status(errorRes.status).json(errorRes).end();
-        }
-      }
-    )
     .delete(
       "/tenants/:tenantId/attributes/verified/:attributeId",
       authorizationMiddleware([ADMIN_ROLE]),
@@ -485,34 +460,59 @@ const tenantsRouter = (
   const tenantsAttributeRouter = ctx.router(tenantApi.tenantAttributeApi.api, {
     validationErrorHandler: zodiosValidationErrorToApiProblem,
   });
-  tenantsAttributeRouter.post(
-    "/tenants/:tenantId/attributes/certified",
-    authorizationMiddleware([ADMIN_ROLE, M2M_ROLE]),
-    async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
-      try {
-        const { tenantId } = req.params;
-        const tenant = await tenantService.addCertifiedAttribute(
-          {
-            tenantId: unsafeBrandId(tenantId),
-            tenantAttributeSeed: req.body,
-            organizationId: req.ctx.authData.organizationId,
-            correlationId: req.ctx.correlationId,
-          },
-          ctx.logger
-        );
-        return res.status(200).json(toApiTenant(tenant)).end();
-      } catch (error) {
-        const errorRes = makeApiProblem(
-          error,
-          addCertifiedAttributeErrorMapper,
-          ctx.logger
-        );
-        return res.status(errorRes.status).json(errorRes).end();
+  tenantsAttributeRouter
+    .post(
+      "/tenants/:tenantId/attributes/certified",
+      authorizationMiddleware([ADMIN_ROLE, M2M_ROLE]),
+      async (req, res) => {
+        const ctx = fromAppContext(req.ctx);
+        try {
+          const { tenantId } = req.params;
+          const tenant = await tenantService.addCertifiedAttribute(
+            {
+              tenantId: unsafeBrandId(tenantId),
+              tenantAttributeSeed: req.body,
+              organizationId: req.ctx.authData.organizationId,
+              correlationId: req.ctx.correlationId,
+            },
+            ctx.logger
+          );
+          return res.status(200).json(toApiTenant(tenant)).end();
+        } catch (error) {
+          const errorRes = makeApiProblem(
+            error,
+            addCertifiedAttributeErrorMapper,
+            ctx.logger
+          );
+          return res.status(errorRes.status).json(errorRes).end();
+        }
       }
-    }
-  );
-
+    )
+    .post(
+      "/tenants/attributes/declared",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (req, res) => {
+        const ctx = fromAppContext(req.ctx);
+        try {
+          const tenant = await tenantService.addDeclaredAttribute(
+            {
+              tenantAttributeSeed: req.body,
+              organizationId: req.ctx.authData.organizationId,
+              correlationId: req.ctx.correlationId,
+            },
+            ctx.logger
+          );
+          return res.status(200).json(toApiTenant(tenant)).end();
+        } catch (error) {
+          const errorRes = makeApiProblem(
+            error,
+            addDeclaredAttributeErrorMapper,
+            ctx.logger
+          );
+          return res.status(errorRes.status).json(errorRes).end();
+        }
+      }
+    );
   return [
     tenantsRouter,
     tenantsAttributeRouter,
