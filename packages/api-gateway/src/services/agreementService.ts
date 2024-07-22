c3b9e3f9d509c3cf9efa7febfe527951feff0ee2
Eric Camellini <eric.camellini@gmail.com>
2024-07-22 17:25:24 +0200

    Fixing tenant API converter

--- packages/api-gateway/src/services/agreementService.ts
+++ packages/api-gateway/src/services/agreementService.ts
@@ -8,11 +8,7 @@ import { ApiGatewayAppContext } from "../utilities/context.js";
 import { toApiGatewayAgreementIfNotDraft } from "../api/agreementApiConverter.js";
 import { producerAndConsumerParamMissing } from "../models/errors.js";
 import { toAgreementProcessGetAgreementsQueryParams } from "../api/agreementApiConverter.js";
-import {
-  certifiedAttributeToAttributeValidityState,
-  declaredAttributeToAttributeValidityState,
-  verifiedAttributeToAttributeValidityState,
-} from "../api/tenantApiConverter.js";
+import { toApiGatewayAgreementAttributes } from "../api/attributesApiConverter.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function agreementServiceBuilder(
@@ -73,7 +69,7 @@ export function agreementServiceBuilder(
     ): Promise<apiGatewayApi.Attributes> => {
       logger.info(`Retrieving Attributes for Agreement ${agreementId}`);
 
-      // TODO is it correct that in this case we succeed even if the agreement is in draft state?
+      // TODO Doubt: is it correct that in this case we succeed even if the agreement is in draft state?
       // In the other two cases we never return info about draft agreements.
       const agreement = await agreementProcessClient.getAgreementById({
         headers,
@@ -89,52 +85,7 @@ export function agreementServiceBuilder(
         },
       });
 
-      const attributes: apiGatewayApi.Attributes = {
-        verified: Array.from(
-          new Set(
-            agreement.verifiedAttributes.flatMap((attr) =>
-              tenant.attributes
-                .filter(
-                  (a) => a.verified !== undefined && a.verified.id === attr.id
-                )
-                .map((v) => v.verified!) // TODO fix this usage of non-null assertion
-                .map((v: tenantApi.VerifiedTenantAttribute) =>
-                  verifiedAttributeToAttributeValidityState(v)
-                )
-            )
-          )
-        ),
-        certified: Array.from(
-          new Set(
-            agreement.certifiedAttributes.flatMap((attr) =>
-              tenant.attributes
-                .filter(
-                  (a) => a.certified !== undefined && a.certified.id === attr.id
-                )
-                .map((c) => c.certified!) // TODO fix this usage of non-null assertion
-                .map((c: tenantApi.CertifiedTenantAttribute) =>
-                  certifiedAttributeToAttributeValidityState(c)
-                )
-            )
-          )
-        ),
-        declared: Array.from(
-          new Set(
-            agreement.declaredAttributes.flatMap((attr) =>
-              tenant.attributes
-                .filter(
-                  (a) => a.declared !== undefined && a.declared.id === attr.id
-                )
-                .map((d) => d.declared!) // TODO fix this usage of non-null assertion
-                .map((d: tenantApi.DeclaredTenantAttribute) =>
-                  declaredAttributeToAttributeValidityState(d)
-                )
-            )
-          )
-        ),
-      };
-
-      return attributes;
+      return toApiGatewayAgreementAttributes(agreement, tenant);
     },
   };
 }
