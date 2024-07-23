7361a0f6da75fabceb9387d22676d6ec85e4d859
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-07-23 17:58:42 +0200

    Implement get eservices consumers

--- packages/bff/src/model/api/apiTypes.ts
+++ packages/bff/src/model/api/apiTypes.ts
@@ -1,6 +1,16 @@
 import { agreementApi, catalogApi } from "pagopa-interop-api-clients";
+import { z } from "zod";
 
 export const catalogApiDescriptorState =
   catalogApi.EServiceDescriptorState.Values;
 
 export const agreementApiState = agreementApi.AgreementState.Values;
+
+export const EserviceConsumer = z.object({
+  descriptorVersion: z.number(),
+  descriptorState: catalogApi.EServiceDescriptorState,
+  agreementState: agreementApi.AgreementState,
+  consumerName: z.string(),
+  consumerExternalId: z.string(),
+});
+
