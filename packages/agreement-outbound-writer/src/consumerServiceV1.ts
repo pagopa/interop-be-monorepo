2cb652120d28ded2dc66b5b50b3f6be75e1acd31
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-07-23 15:18:11 +0200

    Removed event wrapper

--- packages/agreement-outbound-writer/src/consumerServiceV1.ts
+++ packages/agreement-outbound-writer/src/consumerServiceV1.ts
@@ -1,16 +1,14 @@
 import { match } from "ts-pattern";
 import { AgreementEventEnvelopeV1 } from "pagopa-interop-models";
 import { AgreementEvent as OutboundAgreementEvent } from "@pagopa/interop-outbound-models";
-import { CreateEvent } from "pagopa-interop-commons";
 import {
   toOutboundAgreementDocumentV1,
   toOutboundAgreementV1,
 } from "./converters/outboundDataV1Converters.js";
-import { createAgreementOutboundEvent } from "./utils.js";
 
 export function handleMessageV1(
   message: AgreementEventEnvelopeV1
-): CreateEvent<OutboundAgreementEvent> {
+): OutboundAgreementEvent {
   return match(message)
     .with(
       { type: "AgreementAdded" },
@@ -19,59 +17,50 @@ export function handleMessageV1(
       { type: "AgreementDeactivated" },
       { type: "AgreementUpdated" },
       { type: "VerifiedAttributeUpdated" },
-      (message) =>
-        createAgreementOutboundEvent({
-          eventVersion: 1,
-          message,
-          outboundData: {
-            agreement:
-              message.data.agreement &&
-              toOutboundAgreementV1(message.data.agreement),
-          },
-        })
-    )
-    .with({ type: "AgreementDeleted" }, (message) =>
-      createAgreementOutboundEvent({
-        eventVersion: 1,
-        message,
-        outboundData: {
-          agreementId: message.data.agreementId,
+      (message) => ({
+        type: message.type,
+        event_version: message.event_version,
+        data: {
+          agreement:
+            message.data.agreement &&
+            toOutboundAgreementV1(message.data.agreement),
         },
       })
     )
-    .with({ type: "AgreementConsumerDocumentAdded" }, (message) =>
-      createAgreementOutboundEvent({
-        eventVersion: 1,
-        message,
-        outboundData: {
-          agreementId: message.data.agreementId,
-          document:
-            message.data.document &&
-            toOutboundAgreementDocumentV1(message.data.document),
-        },
-      })
-    )
-    .with({ type: "AgreementConsumerDocumentRemoved" }, (message) =>
-      createAgreementOutboundEvent({
-        eventVersion: 1,
-        message,
-        outboundData: {
-          agreementId: message.data.agreementId,
-          documentId: message.data.documentId,
-        },
-      })
-    )
-    .with({ type: "AgreementContractAdded" }, (message) =>
-      createAgreementOutboundEvent({
-        eventVersion: 1,
-        message,
-        outboundData: {
-          agreementId: message.data.agreementId,
-          contract:
-            message.data.contract &&
-            toOutboundAgreementDocumentV1(message.data.contract),
-        },
-      })
-    )
+    .with({ type: "AgreementDeleted" }, (message) => ({
+      type: message.type,
+      event_version: message.event_version,
+      data: {
+        agreementId: message.data.agreementId,
+      },
+    }))
+    .with({ type: "AgreementConsumerDocumentAdded" }, (message) => ({
+      type: message.type,
+      event_version: message.event_version,
+      data: {
+        agreementId: message.data.agreementId,
+        document:
+          message.data.document &&
+          toOutboundAgreementDocumentV1(message.data.document),
+      },
+    }))
+    .with({ type: "AgreementConsumerDocumentRemoved" }, (message) => ({
+      type: message.type,
+      event_version: message.event_version,
+      data: {
+        agreementId: message.data.agreementId,
+        documentId: message.data.documentId,
+      },
+    }))
+    .with({ type: "AgreementContractAdded" }, (message) => ({
+      type: message.type,
+      event_version: message.event_version,
+      data: {
+        agreementId: message.data.agreementId,
+        contract:
+          message.data.contract &&
+          toOutboundAgreementDocumentV1(message.data.contract),
+      },
+    }))
     .exhaustive();
 }
--- packages/agreement-outbound-writer/src/utils.ts
+++ packages/agreement-outbound-writer/src/utils.ts
@@ -1,7 +1,3 @@
-import { AgreementEventEnvelope, generateId } from "pagopa-interop-models";
-import { AgreementEvent as OutboundAgreementEvent } from "@pagopa/interop-outbound-models";
-import { CreateEvent } from "pagopa-interop-commons";
-
 export type Exact<T, U extends T> = {
   [Key in keyof U]: Key extends keyof T
     ? T[Key] extends infer TObj | undefined
@@ -10,31 +6,3 @@ export type Exact<T, U extends T> = {
       : T[Key]
     : undefined;
 };
-
-export function createAgreementOutboundEvent<
-  TVersion extends number,
-  TMessage extends Extract<AgreementEventEnvelope, { event_version: TVersion }>,
-  TOutboundEvent extends Extract<
-    OutboundAgreementEvent,
-    { type: TMessage["type"]; event_version: TVersion }
-  >
->({
-  eventVersion,
-  message,
-  outboundData,
-}: {
-  eventVersion: TVersion;
-  message: TMessage;
-  outboundData: TOutboundEvent["data"];
-}): CreateEvent<TOutboundEvent> {
-  return {
-    streamId: message.stream_id,
-    version: message.version,
-    event: {
-      type: message.type,
-      event_version: eventVersion,
-      data: outboundData,
-    },
-    correlationId: message.correlation_id ?? generateId(),
-  } as CreateEvent<TOutboundEvent>;
-}
