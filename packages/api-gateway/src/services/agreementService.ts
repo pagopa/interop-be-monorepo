bdd53e2fd22344903912ea0aac23cc9f5458d2bc
Eric Camellini <eric.camellini@gmail.com>
2024-07-23 11:11:19 +0200

    Improving file structure

--- packages/api-gateway/src/services/agreementService.ts
+++ packages/api-gateway/src/services/agreementService.ts
@@ -1,24 +1,8 @@
-/* eslint-disable functional/immutable-data */
 import { agreementApi, apiGatewayApi } from "pagopa-interop-api-clients";
 import { WithLogger } from "pagopa-interop-commons";
 import { AgreementProcessClient } from "../clients/clientsProvider.js";
 import { ApiGatewayAppContext } from "../utilities/context.js";
-import { assertAgreementStateNotDraft } from "./validators.js";
-
-function toApiGatewayAgreementIfNotDraft(
-  agreement: agreementApi.Agreement
-): apiGatewayApi.Agreement {
-  assertAgreementStateNotDraft(agreement.state, agreement.id);
-
-  return {
-    id: agreement.id,
-    eserviceId: agreement.eserviceId,
-    descriptorId: agreement.descriptorId,
-    producerId: agreement.producerId,
-    consumerId: agreement.consumerId,
-    state: agreement.state,
-  };
-}
+import { toApiGatewayAgreementIfNotDraft } from "../api/agreementApiConverter.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function agreementServiceBuilder(
@@ -26,17 +10,16 @@ export function agreementServiceBuilder(
 ) {
   return {
     getAgreementById: async (
-      context: WithLogger<ApiGatewayAppContext>,
+      { logger, headers }: WithLogger<ApiGatewayAppContext>,
       agreementId: agreementApi.Agreement["id"]
     ): Promise<apiGatewayApi.Agreement> => {
-      context.logger.info(`Retrieving agreement by id = ${agreementId}`);
-      const agreement: agreementApi.Agreement =
-        await agreementProcessClient.getAgreementById({
-          headers: context.headers,
-          params: {
-            agreementId,
-          },
-        });
+      logger.info(`Retrieving agreement by id = ${agreementId}`);
+      const agreement = await agreementProcessClient.getAgreementById({
+        headers,
+        params: {
+          agreementId,
+        },
+      });
 
       return toApiGatewayAgreementIfNotDraft(agreement);
     },
