db36767298826df352390546a15b2fb29603527b
Sandro Tajè <sandro.taje@gmail.com>
2024-07-23 11:21:24 +0200

    fix lint

--- packages/bff/src/services/catalogService.ts
+++ packages/bff/src/services/catalogService.ts
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { bffApi, catalogApi, tenantApi } from "pagopa-interop-api-clients";
 import { WithLogger } from "pagopa-interop-commons";
-import { catalogApi, tenantApi, bffApi } from "pagopa-interop-api-clients";
-import { match } from "ts-pattern";
 import {
   DescriptorId,
   EServiceDocumentId,
   EServiceId,
 } from "pagopa-interop-models";
+import { match } from "ts-pattern";
 import { toBffCatalogApiEServiceResponse } from "../model/api/apiConverter.js";
+import { catalogApiDescriptorState } from "../model/api/apiTypes.js";
+import { noDescriptorInEservice } from "../model/domain/errors.js";
 import { catalogProcessApiEServiceDescriptorCertifiedAttributesSatisfied } from "../model/validators.js";
 import {
   AgreementProcessClient,
@@ -15,9 +17,7 @@ import {
   TenantProcessClient,
 } from "../providers/clientProvider.js";
 import { BffAppContext, Headers } from "../utilities/context.js";
-import { catalogApiDescriptorState } from "../model/api/apiTypes.js";
 import { getLatestAgreement } from "./agreementService.js";
-import { noDescriptorInEservice } from "../model/domain/errors.js";
 
 function activeDescriptorStateFilter(
   descriptor: catalogApi.EServiceDescriptor
