79238e1f5e1ea61da3e34729aff0292264020959
paologaleotti <paologaleotti2002@gmail.com>
2024-07-23 17:28:42 +0200

    fix: removed unused error mapper

--- packages/bff/src/utilities/errorMappers.ts
+++ packages/bff/src/utilities/errorMappers.ts
@@ -44,7 +44,6 @@ export const sessionTokenErrorMapper = (error: ApiError<ErrorCodes>): number =>
     .with("tenantLoginNotAllowed", () => HTTP_STATUS_FORBIDDEN)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
-export const emptyErrorMapper = (): number => HTTP_STATUS_INTERNAL_SERVER_ERROR;
 export const attributeEmptyErrorMapper = (): number =>
   HTTP_STATUS_INTERNAL_SERVER_ERROR;
 
@@ -54,3 +53,4 @@ export const getClientUsersErrorMapper = (
   match(error.code)
     .with("userNotFound", () => HTTP_STATUS_NOT_FOUND)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+export const emptyErrorMapper = (): number => HTTP_STATUS_INTERNAL_SERVER_ERROR;
