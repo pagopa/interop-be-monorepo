87f4d51f806f6597f3d895176cc576a065937119
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-07-24 18:30:36 +0200

    Added purpose-outbound-writer module

---
+++ packages/purpose-outbound-writer/src/index.ts
@@ -0,0 +1,49 @@
+import { EachMessagePayload } from "kafkajs";
+import { decodeKafkaMessage, logger } from "pagopa-interop-commons";
+import { initProducer, runConsumer } from "kafka-iam-auth";
+import { match } from "ts-pattern";
+import {
+  encodeOutboundPurposeEvent,
+  PurposeEvent as PurposeOutboundEvent,
+} from "@pagopa/interop-outbound-models";
+import { PurposeEvent } from "pagopa-interop-models";
+import { config } from "./config/config.js";
+import { toOutboundEventV1 } from "./converters/toOutboundEventV1.js";
+import { toOutboundEventV2 } from "./converters/toOutboundEventV2.js";
+
+const producer = await initProducer(
+  config.producerConfig,
+  config.purposeOutboundTopic
+);
+
+async function processMessage({
+  message,
+  partition,
+}: EachMessagePayload): Promise<void> {
+  const msg = decodeKafkaMessage(message, PurposeEvent);
+
+  const loggerInstance = logger({
+    serviceName: "purpose-outbound-writer",
+    eventType: msg.type,
+    eventVersion: msg.event_version,
+    streamId: msg.stream_id,
+    correlationId: msg.correlation_id,
+  });
+
+  const outboundEvent: PurposeOutboundEvent | undefined = match(msg)
+    .with({ event_version: 1 }, (msg) => toOutboundEventV1(msg))
+    .with({ event_version: 2 }, (msg) => toOutboundEventV2(msg))
+    .exhaustive();
+
+  if (outboundEvent) {
+    await producer.send({
+      messages: [{ value: encodeOutboundPurposeEvent(outboundEvent) }],
+    });
+  }
+
+  loggerInstance.info(
+    `Outbound event sent! Partition number: ${partition}. Offset: ${message.offset}`
+  );
+}
+
+await runConsumer(config.consumerConfig, [config.purposeTopic], processMessage);
