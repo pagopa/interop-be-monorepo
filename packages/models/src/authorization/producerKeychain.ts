96ebc14996ed94932a13b5bd4d59a939b02eba28
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2024-07-26 11:39:57 +0200

    Add producer keychain JWK key model

--- packages/models/src/authorization/producerKeychain.ts
+++ packages/models/src/authorization/producerKeychain.ts
@@ -5,7 +5,7 @@ import {
   TenantId,
   UserId,
 } from "../brandedIds.js";
-import { Key } from "./key.js";
+import { JWKKey, Key } from "./key.js";
 
 export const ProducerKeychainKey = Key.extend({
   producerKeychainId: ProducerKeychainId,
@@ -23,3 +23,9 @@ export const ProducerKeychain = z.object({
   keys: z.array(ProducerKeychainKey),
 });
 export type ProducerKeychain = z.infer<typeof ProducerKeychain>;
+
+export const ProducerKeychainJWKKey = JWKKey.extend({
+  producerKeychainId: ProducerKeychainId,
+});
+
+export type ProducerKeychainJWKKey = z.infer<typeof ProducerKeychainJWKKey>;
