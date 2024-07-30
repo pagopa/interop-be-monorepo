c4145fa594793f5b556bab66805ecc578bcd6aa3
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-07-30 11:23:55 +0200

    Added test

---
+++ packages/authorization-process/test/createProducerKeychain.test.ts
@@ -0,0 +1,77 @@
+import { describe, it, vi, beforeAll, afterAll, expect } from "vitest";
+import {
+  ProducerKeychain,
+  ProducerKeychainAddedV2,
+  TenantId,
+  UserId,
+  generateId,
+  toProducerKeychainV2,
+  unsafeBrandId,
+} from "pagopa-interop-models";
+import { genericLogger } from "pagopa-interop-commons";
+import {
+  decodeProtobufPayload,
+  readLastEventByStreamId,
+} from "pagopa-interop-commons-test/index.js";
+import { authorizationApi } from "pagopa-interop-api-clients";
+import { authorizationService, postgresDB } from "./utils.js";
+
+describe("createProducerKeychain", () => {
+  const organizationId: TenantId = generateId();
+
+  beforeAll(async () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date());
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  const producerKeychainSeed: authorizationApi.ProducerKeychainSeed = {
+    name: "Seed name",
+    description: "Description",
+    members: [organizationId],
+  };
+  it("should write on event-store for the creation of a producer keychain", async () => {
+    const producerKeychain = await authorizationService.createProducerKeychain({
+      producerKeychainSeed,
+      organizationId,
+      correlationId: generateId(),
+      logger: genericLogger,
+    });
+
+    const writtenEvent = await readLastEventByStreamId(
+      producerKeychain.id,
+      '"authorization"',
+      postgresDB
+    );
+
+    expect(writtenEvent).toMatchObject({
+      stream_id: producerKeychain.id,
+      version: "0",
+      type: "ProducerKeychainAdded",
+      event_version: 2,
+    });
+
+    const writtenPayload = decodeProtobufPayload({
+      messageType: ProducerKeychainAddedV2,
+      payload: writtenEvent.data,
+    });
+
+    const expectedProducerKeychain: ProducerKeychain = {
+      id: producerKeychain.id,
+      keys: [],
+      name: producerKeychain.name,
+      createdAt: new Date(),
+      producerId: organizationId,
+      eservices: [],
+      users: producerKeychain.users.map(unsafeBrandId<UserId>),
+      description: producerKeychain.description,
+    };
+
+    expect(writtenPayload.producerKeychain).toEqual(
+      toProducerKeychainV2(expectedProducerKeychain)
+    );
+  });
+});
