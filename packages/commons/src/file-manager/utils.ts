4ddf0f17eb178ed589d70234a70b1c5544924b1c
Simone Camito <zibo.camito@gmail.com>
2024-07-30 09:23:22 +0200

    fix test

--- packages/commons/src/file-manager/utils.ts
+++ packages/commons/src/file-manager/utils.ts
@@ -1,1 +1,5 @@
-export const streamToString = (data: Uint8Array): string => data.toString();
+export const streamToString = (data: Uint8Array): string => {
+  const decoder = new TextDecoder("utf-8");
+
+  return decoder.decode(data);
+};
