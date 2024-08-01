e71be7cf1a1e492e60d18ff415688b48ded67ab3
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-08-01 15:37:14 +0200

    Remove empty line

--- packages/commons/src/auth/jwk.ts
+++ packages/commons/src/auth/jwk.ts
@@ -9,7 +9,7 @@ const decodeBase64ToPem = (base64String: string): string => {
   try {
     const cleanedBase64 = base64String.trim();
     const decodedBytes = Buffer.from(cleanedBase64, "base64");
-    return decodedBytes.toString("utf-8");
+    return decodedBytes.toString("utf-8").replaceAll("\n\n", "\n");
   } catch (error) {
     throw jwkDecodingError(error);
   }
