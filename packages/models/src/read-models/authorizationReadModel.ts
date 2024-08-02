d9b9a25a3d5add51391ceab7f22cee11f818997d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-08-02 15:42:01 +0200

    Renamed producer keychain keys models

--- packages/models/src/read-models/authorizationReadModel.ts
+++ packages/models/src/read-models/authorizationReadModel.ts
@@ -3,7 +3,7 @@ import { Client } from "../authorization/client.js";
 import { ClientKey } from "../authorization/client.js";
 import {
   ProducerKeychain,
-  ProducerKeychainKey,
+  ProducerKey,
 } from "../authorization/producerKeychain.js";
 
 export const ClientKeyReadModel = ClientKey.extend({
@@ -12,12 +12,12 @@ export const ClientKeyReadModel = ClientKey.extend({
 
 export type ClientKeyReadModel = z.infer<typeof ClientKeyReadModel>;
 
-export const ProducerKeychainKeyReadModel = ProducerKeychainKey.extend({
+export const ProducerKeyReadModel = ProducerKey.extend({
   createdAt: z.string().datetime(),
 });
 
-export type ProducerKeychainKeyReadModel = z.infer<
-  typeof ProducerKeychainKeyReadModel
+export type ProducerKeyReadModel = z.infer<
+  typeof ProducerKeyReadModel
 >;
 
 export const ClientReadModel = Client.extend({
@@ -29,7 +29,7 @@ export type ClientReadModel = z.infer<typeof ClientReadModel>;
 
 export const ProducerKeychainReadModel = ProducerKeychain.extend({
   createdAt: z.string().datetime(),
-  keys: z.array(ProducerKeychainKeyReadModel),
+  keys: z.array(ProducerKeyReadModel),
 });
 
 export type ProducerKeychainReadModel = z.infer<
