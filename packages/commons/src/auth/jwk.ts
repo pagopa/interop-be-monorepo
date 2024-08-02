895553a70f099d2da6281d2a5166bfaa110ba97e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-08-02 14:10:02 +0200

    Fix

--- packages/commons/src/auth/jwk.ts
+++ packages/commons/src/auth/jwk.ts
@@ -5,11 +5,11 @@ import {
   notAllowedPrivateKeyException,
 } from "pagopa-interop-models";
 
-const decodeBase64ToPem = (base64String: string): string => {
+export const decodeBase64ToPem = (base64String: string): string => {
   try {
     const cleanedBase64 = base64String.trim();
     const decodedBytes = Buffer.from(cleanedBase64, "base64");
-    return decodedBytes.toString("utf-8").replaceAll("\n\n", "\n");
+    return decodedBytes.toString("utf-8");
   } catch (error) {
     throw jwkDecodingError(error);
   }
