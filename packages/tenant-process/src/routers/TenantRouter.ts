b8400d0830dda2f86feff09a3f0e8edb65db15d7
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-08-02 19:00:00 +0200

    fix

--- packages/tenant-process/src/routers/TenantRouter.ts
+++ packages/tenant-process/src/routers/TenantRouter.ts
@@ -368,11 +368,6 @@ const tenantsRouter = (
       }
     )
     .delete(
-      "/tenants/:tenantId/attributes/verified/:attributeId",
-      authorizationMiddleware([ADMIN_ROLE]),
-      async (_req, res) => res.status(501).send()
-    )
-    .delete(
       "/maintenance/tenants/:tenantId",
       authorizationMiddleware([MAINTENANCE_ROLE]),
       async (req, res) => {
