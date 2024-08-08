35ab72824feb9a79e649a843cb39df2029079317
Simone Camito <zibo.camito@gmail.com>
2024-08-08 16:18:49 +0200

    add getRequesterCertifiedAttributes

--- packages/bff/src/model/domain/apiConverter.ts
+++ packages/bff/src/model/domain/apiConverter.ts
@@ -113,3 +113,12 @@ export const toBffApiCompactOrganization = (
   id: input.id,
   name: input.name,
 });
+
+export const toBffApiRequesterCertifiedAttributes = (
+  input: tenantApi.CertifiedAttribute
+): bffApi.RequesterCertifiedAttribute => ({
+  tenantId: input.id,
+  tenantName: input.name,
+  attributeId: input.attributeId,
+  attributeName: input.attributeName,
+});
