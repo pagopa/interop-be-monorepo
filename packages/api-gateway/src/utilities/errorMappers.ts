428a4fac258161fb26cd45796fac1def7c472b4a
Eric Camellini <eric.camellini@gmail.com>
2024-08-13 17:23:51 +0200

    Adding missing error in error mapper

--- packages/api-gateway/src/utilities/errorMappers.ts
+++ packages/api-gateway/src/utilities/errorMappers.ts
@@ -40,7 +40,11 @@ export const getAgreementByPurposeErrorMapper = (
 
 export const getEserviceErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
-    .with("missingAvailableDescriptor", () => HTTP_STATUS_NOT_FOUND)
+    .with(
+      "missingAvailableDescriptor",
+      "unexpectedDescriptorState",
+      () => HTTP_STATUS_NOT_FOUND
+    )
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const emptyErrorMapper = (): number => HTTP_STATUS_INTERNAL_SERVER_ERROR;
