12e7e6d3b534f9734edbc3acb6596f60df478f0f
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-08-21 17:06:15 +0200

    fix

--- packages/agreement-email-sender/test/sendAgreementActivationEmail.integration.test.ts
+++ packages/agreement-email-sender/test/sendAgreementActivationEmail.integration.test.ts
@@ -35,7 +35,7 @@ import {
 } from "./utils.js";
 
 describe("sendAgreementActivationEmail", () => {
-  it.only("should send an email", async () => {
+  it("should send an email", async () => {
     const consumer: Tenant = {
       ...getMockTenant(),
       mails: [getMockTenantMail(tenantMailKind.DigitalAddress)],
