ee09c0dee97c91d855cdb6ea59dcc8c8d53a903e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-08-23 17:05:39 +0200

    Add parsers

---
+++ packages/poc-readmodel/src/parsers.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  DocumentSQL,
+  genericInternalError,
+  EServiceSQL,
+  DescriptorSQL,
+} from "pagopa-interop-models";
+
+export const parseDocumentSQL = (data: any): DocumentSQL | undefined => {
+  if (!data) {
+    return undefined;
+  } else {
+    const result = DocumentSQL.safeParse(data);
+    if (!result.success) {
+      throw genericInternalError(
+        `Unable to parse document item: result ${JSON.stringify(
+          result
+        )} - data ${JSON.stringify(data)} `
+      );
+    }
+    return result.data;
+  }
+};
+
+export const parseEserviceSQL = (data: any): EServiceSQL | undefined => {
+  if (!data) {
+    return undefined;
+  } else {
+    const result = EServiceSQL.safeParse(data);
+    if (!result.success) {
+      throw genericInternalError(
+        `Unable to parse eService item: result ${JSON.stringify(
+          result
+        )} - data ${JSON.stringify(data)} `
+      );
+    }
+    return result.data;
+  }
+};
+
+export const parseDescriptorSQL = (data: any): DescriptorSQL | undefined => {
+  if (!data) {
+    return undefined;
+  } else {
+    const result = DescriptorSQL.safeParse(data);
+    if (!result.success) {
+      throw genericInternalError(
+        `Unable to parse descriptor item: result ${JSON.stringify(
+          result
+        )} - data ${JSON.stringify(data)} `
+      );
+    }
+    return result.data;
+  }
+};
