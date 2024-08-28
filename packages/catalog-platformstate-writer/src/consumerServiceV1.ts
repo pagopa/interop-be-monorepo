e5e896596f297a7f47b01e1d182b94823823e428
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2024-08-28 15:15:34 +0200

    Add logics for events v1

--- packages/catalog-platformstate-writer/src/consumerServiceV1.ts
+++ packages/catalog-platformstate-writer/src/consumerServiceV1.ts
@@ -1,12 +1,69 @@
 import { match } from "ts-pattern";
-import { EServiceEventEnvelopeV1 } from "pagopa-interop-models";
+import {
+  descriptorState,
+  EServiceEventEnvelopeV1,
+  fromDescriptorV1,
+  genericInternalError,
+  PlatformStatesCatalogEntry,
+} from "pagopa-interop-models";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  deleteCatalogEntry,
+  descriptorStateToClientState,
+  writeCatalogEntry,
+} from "./utils.js";
 
 export async function handleMessageV1(
   message: EServiceEventEnvelopeV1,
-  _dynamoDBClient: DynamoDBClient
+  dynamoDBClient: DynamoDBClient
 ): Promise<void> {
   await match(message)
+    // EServiceDescriptorPublished -> EServiceDescriptorUpdated
+    // EServiceDescriptorActivated,EServiceDescriptorSuspended -> EServiceDescriptorUpdated
+    // EServiceDescriptorArchived -> EServiceDescriptorUpdated
+    .with({ type: "EServiceDescriptorUpdated" }, async (msg) => {
+      const eserviceId = msg.data.eserviceId;
+      const descriptorV1 = msg.data.eserviceDescriptor;
+      if (!descriptorV1) {
+        throw genericInternalError(
+          `EServiceDescriptor not found in message data for event ${msg.type}`
+        );
+      }
+      const descriptor = fromDescriptorV1(descriptorV1);
+
+      match(descriptor.state)
+        .with(
+          descriptorState.published,
+          descriptorState.suspended,
+          async () => {
+            console.log(descriptor.state);
+            const catalogEntry: PlatformStatesCatalogEntry = {
+              // TODO: change with the PK type
+              PK: `ESERVICEDESCRIPTOR#${eserviceId}#${descriptor.id}`,
+              state: descriptorStateToClientState(descriptor.state),
+              descriptorAudience: descriptor.audience[0],
+            };
+            console.log(catalogEntry);
+            await writeCatalogEntry(catalogEntry, dynamoDBClient);
+          }
+        )
+        .with(descriptorState.archived, async () => {
+          const eserviceId = msg.data.eserviceId;
+          const descriptorV1 = msg.data.eserviceDescriptor;
+          if (!descriptorV1) {
+            throw genericInternalError(
+              `EServiceDescriptor not found in message data for event ${msg.type}`
+            );
+          }
+          const descriptor = fromDescriptorV1(descriptorV1);
+
+          const primaryKey = `ESERVICEDESCRIPTOR#${eserviceId}#${descriptor.id}`;
+          await deleteCatalogEntry(primaryKey, dynamoDBClient);
+        })
+        .with(descriptorState.draft, descriptorState.deprecated, () =>
+          Promise.resolve()
+        );
+    })
     .with(
       { type: "EServiceAdded" },
       { type: "ClonedEServiceAdded" },
@@ -20,7 +77,6 @@ export async function handleMessageV1(
       { type: "EServiceDocumentAdded" },
       { type: "EServiceDocumentDeleted" },
       { type: "EServiceDescriptorAdded" },
-      { type: "EServiceDescriptorUpdated" },
       { type: "EServiceRiskAnalysisDeleted" },
       () => Promise.resolve()
     )
