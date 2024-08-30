8c65af6c5442457d1819d84c9547a9bd39a94778
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2024-08-30 11:25:04 +0200

    Improve platform-states PK types

--- packages/models/src/token-generation-readmodel/platform-states-entry.ts
+++ packages/models/src/token-generation-readmodel/platform-states-entry.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 import {
+  AgreementId,
+  ClientId,
+  DescriptorId,
   EServiceId,
   PurposeId,
   PurposeVersionId,
@@ -17,15 +20,59 @@ export const ItemState = z.enum([
 export type ItemState = z.infer<typeof ItemState>;
 
 /*
-type PrimaryKeyPlatformStates =
-  | `ESERVICEDESCRIPTOR#${EServiceId}#${DescriptorId}`
-  | `AGREEMENT#${AgreementId}`
-  | `PURPOSE#${PurposeId}`
-  | `CLIENT#${ClientId}`;
+export const PlatformStatesEServiceDescriptorPK = z.literal(
+  `ESERVICEDESCRIPTOR#${EServiceId}#${DescriptorId}`
+);
+export type PlatformStatesEServiceDescriptorPK = z.infer<
+  typeof PlatformStatesEServiceDescriptorPK
+>;
+
+const a: PlatformStatesEServiceDescriptorPK = `ESERVICEDESCRIPTOR#${generateId<EServiceId>()}#${generateId<DescriptorId>()}`; // OK
+const b: PlatformStatesEServiceDescriptorPK = `ESERVICEDESCRIPTOR#${generateId()}#${generateId<DescriptorId>()}`; // OK
+const c: PlatformStatesEServiceDescriptorPK = `ESERVICEDESCRIPTOR#test#test`; // OK
+const d: PlatformStatesEServiceDescriptorPK = `ESERVICEDESCRIPTOR#test#`; // OK
+const e: PlatformStatesEServiceDescriptorPK = `ESERVICEDESCRIPTOR#test`; // WRONG
+const f: PlatformStatesEServiceDescriptorPK = `test#test#test`; // WRONG
+
+We don't check the structure of the ids because they are treated as strings
 */
 
+export const PlatformStatesEServiceDescriptorPK = z
+  .string()
+  .brand(`ESERVICEDESCRIPTOR#eServiceId#descriptorId`);
+export type PlatformStatesEServiceDescriptorPK = z.infer<
+  typeof PlatformStatesEServiceDescriptorPK
+>;
+
+export const makePlatformStatesEServiceDescriptorPK = (
+  eserviceId: EServiceId,
+  descriptorId: DescriptorId
+): PlatformStatesEServiceDescriptorPK =>
+  `ESERVICEDESCRIPTOR#${eserviceId}#${descriptorId}` as PlatformStatesEServiceDescriptorPK;
+export const PlatformStatesAgreementPK = z
+  .string()
+  .brand(`AGREEMENT#agreementId`);
+export type PlatformStatesAgreementPK = z.infer<
+  typeof PlatformStatesAgreementPK
+>;
+export const makePlatformStatesAgreementPK = (
+  agreementId: AgreementId
+): PlatformStatesAgreementPK =>
+  `AGREEMENT#${agreementId}` as PlatformStatesAgreementPK;
+
+export const PlatformStatesPurposePK = z.string().brand(`PURPOSE#purposeId`);
+export type PlatformStatesPurposePK = z.infer<typeof PlatformStatesPurposePK>;
+export const makePlatformStatesPurposePK = (
+  purposeId: PurposeId
+): PlatformStatesPurposePK => `PURPOSE#${purposeId}` as PlatformStatesPurposePK;
+
+export const PlatformStatesClientPK = z.string().brand(`CLIENT#clientId`);
+export type PlatformStatesClientPK = z.infer<typeof PlatformStatesClientPK>;
+export const makePlatformStatesClientPK = (
+  clientId: ClientId
+): PlatformStatesClientPK => `CLIENT#${clientId}` as PlatformStatesClientPK;
+
 const PlatformStatesBaseEntry = z.object({
-  PK: z.string(),
   state: ItemState,
   version: z.number(),
   updatedAt: z.string().datetime(),
@@ -33,6 +80,7 @@ const PlatformStatesBaseEntry = z.object({
 type PlatformStatesBaseEntry = z.infer<typeof PlatformStatesBaseEntry>;
 
 export const PlatformStatesCatalogEntry = PlatformStatesBaseEntry.extend({
+  PK: PlatformStatesEServiceDescriptorPK,
   descriptorAudience: z.string(),
 });
 export type PlatformStatesCatalogEntry = z.infer<
@@ -40,6 +88,7 @@ export type PlatformStatesCatalogEntry = z.infer<
 >;
 
 export const PlatformStatesPurposeEntry = PlatformStatesBaseEntry.extend({
+  PK: PlatformStatesPurposePK,
   purposeVersionId: PurposeVersionId,
   purposeEserviceId: EServiceId,
   purposeConsumerId: TenantId,
@@ -49,6 +98,7 @@ export type PlatformStatesPurposeEntry = z.infer<
 >;
 
 export const PlatformStatesAgreementEntry = PlatformStatesBaseEntry.extend({
+  PK: PlatformStatesAgreementPK,
   GSIPK_consumerId_eserviceId: z.string(),
   GSISK_agreementTimestamp: z.string().datetime(),
   agreementDescriptorId: z.string(),
@@ -58,6 +108,7 @@ export type PlatformStatesAgreementEntry = z.infer<
 >;
 
 export const PlatformStatesClientEntry = PlatformStatesBaseEntry.extend({
+  PK: PlatformStatesClientPK,
   clientPurposesIds: z.array(PurposeId),
 });
 export type PlatformStatesClientEntry = z.infer<
