ad4872a01577d624d08a5a3cfa9ec50e4c3a8c5c
shuyec <76391491+shuyec@users.noreply.github.com>
2024-09-03 12:07:16 +0200

    PIN-5096 - Add producer keychain readmodel writer (#888)
    Co-authored-by: Carmine Porricelli <carmine.porricelli@pagopa.it>

---
+++ packages/producer-keychain-readmodel-writer/src/index.ts
@@ -0,0 +1,40 @@
+import { EachMessagePayload } from "kafkajs";
+import {
+  logger,
+  ReadModelRepository,
+  decodeKafkaMessage,
+} from "pagopa-interop-commons";
+import { runConsumer } from "kafka-iam-auth";
+import { AuthorizationEvent } from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import { handleMessageV2 } from "./producerKeychainConsumerServiceV2.js";
+import { config } from "./config/config.js";
+
+const { producerKeychains } = ReadModelRepository.init(config);
+
+async function processMessage({
+  message,
+  partition,
+}: EachMessagePayload): Promise<void> {
+  const decodedMessage = decodeKafkaMessage(message, AuthorizationEvent);
+
+  const loggerInstance = logger({
+    serviceName: "producer-keychain-readmodel-writer",
+    eventType: decodedMessage.type,
+    eventVersion: decodedMessage.event_version,
+    streamId: decodedMessage.stream_id,
+    correlationId: decodedMessage.correlation_id,
+  });
+  await match(decodedMessage)
+    .with({ event_version: 2 }, (msg) =>
+      handleMessageV2(msg, producerKeychains)
+    )
+    .with({ event_version: 1 }, () => Promise.resolve)
+    .exhaustive();
+
+  loggerInstance.info(
+    `Read model was updated. Partition number: ${partition}. Offset: ${message.offset}`
+  );
+}
+
+await runConsumer(config, [config.authorizationTopic], processMessage);
