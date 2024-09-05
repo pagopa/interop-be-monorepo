fcd43a3f8eff410fa3834e7b752da795aab289bd
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-09-05 11:23:34 +0200

    Fix sleep

--- packages/catalog-platformstate-writer/test/utils.ts
+++ packages/catalog-platformstate-writer/test/utils.ts
@@ -1,5 +1,5 @@
 import { setupTestContainersVitest } from "pagopa-interop-commons-test/index.js";
-import { afterEach, inject } from "vitest";
+import { afterEach, inject, vi } from "vitest";
 
 export const config = inject("tokenGenerationReadModelConfig");
 export const { cleanup } = setupTestContainersVitest();
@@ -7,3 +7,11 @@ export const { cleanup } = setupTestContainersVitest();
 afterEach(cleanup);
 
 // export const eservices = readModelRepository.eservices;
+
+export const sleep = (ms: number, mockDate = new Date()): Promise<void> =>
+  new Promise((resolve) => {
+    vi.useRealTimers();
+    setTimeout(resolve, ms);
+    vi.useFakeTimers();
+    vi.setSystemTime(mockDate);
+  });
