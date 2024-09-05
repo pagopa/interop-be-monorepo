58ae56e879682bad3a3a7e26062fc6a76c1bc409
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-09-05 11:58:35 +0200

    Add util test

--- packages/catalog-platformstate-writer/test/utils.test.ts
+++ packages/catalog-platformstate-writer/test/utils.test.ts
@@ -2,6 +2,7 @@ import {
   DescriptorId,
   EServiceId,
   generateId,
+  makeGSIPKEServiceIdDescriptorId,
   makePlatformStatesEServiceDescriptorPK,
 } from "pagopa-interop-models";
 import { describe, expect, it } from "vitest";
@@ -15,5 +16,15 @@ describe("test", () => {
       descriptorId,
     });
     expect(PK).toEqual(`ESERVICEDESCRIPTOR#${eserviceId}#${descriptorId}`);
+  });
+
+  it("makeGSIPKEServiceIdDescriptorId", () => {
+    const eserviceId = generateId<EServiceId>();
+    const descriptorId = generateId<DescriptorId>();
+    const GSI = makeGSIPKEServiceIdDescriptorId({
+      eserviceId,
+      descriptorId,
+    });
+    expect(GSI).toEqual(`${eserviceId}#${descriptorId}`);
   });
 });
