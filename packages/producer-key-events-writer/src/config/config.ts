f0bc977abf4651475b4aa65dedabb5c8f9bfea6b
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-09-06 09:54:01 +0200

    Update config.ts

--- packages/producer-key-events-writer/src/config/config.ts
+++ packages/producer-key-events-writer/src/config/config.ts
@@ -1,13 +1,11 @@
 import {
   AuthorizationTopicConfig,
   EventStoreConfig,
-  ReadModelWriterConfig,
 } from "pagopa-interop-commons";
 import { z } from "zod";
 
-export const ProducerKeyReadModelWriterConfig = ReadModelWriterConfig.and(
-  AuthorizationTopicConfig
-).and(EventStoreConfig);
+export const ProducerKeyReadModelWriterConfig =
+  AuthorizationTopicConfig.and(EventStoreConfig);
 
 export type ProducerKeyReadModelWriterConfig = z.infer<
   typeof ProducerKeyReadModelWriterConfig
