2c75c509b627f42652c60aeb8efe81e5dfb8ed52
Eric Camellini <eric.camellini@gmail.com>
2024-09-09 10:33:43 +0200

    IMN 607 - BFF Tenant router - Pt2 (#933)

--- packages/bff/src/model/api/tenantApiConverter.ts
+++ packages/bff/src/model/api/tenantApiConverter.ts
@@ -1,4 +1,9 @@
-import { tenantApi, bffApi } from "pagopa-interop-api-clients";
+import {
+  tenantApi,
+  bffApi,
+  attributeRegistryApi,
+} from "pagopa-interop-api-clients";
+import { isDefined } from "pagopa-interop-commons";
 
 export const toBffApiCompactOrganization = (
   input: tenantApi.Tenant
@@ -15,3 +20,94 @@ export const toBffApiRequesterCertifiedAttributes = (
   attributeId: input.attributeId,
   attributeName: input.attributeName,
 });
+
+export type RegistryAttributesMap = Map<
+  attributeRegistryApi.Attribute["id"],
+  attributeRegistryApi.Attribute
+>;
+
+const toBffApiCertifiedTenantAttribute = (
+  tenantAttribute: tenantApi.CertifiedTenantAttribute,
+  registryAttributesMap: RegistryAttributesMap
+): bffApi.CertifiedTenantAttribute | undefined => {
+  const registryAttribute = registryAttributesMap.get(tenantAttribute.id);
+
+  return registryAttribute
+    ? {
+        id: tenantAttribute.id,
+        name: registryAttribute.name,
+        description: registryAttribute.description,
+        assignmentTimestamp: tenantAttribute.assignmentTimestamp,
+        revocationTimestamp: tenantAttribute.revocationTimestamp,
+      }
+    : undefined;
+};
+
+const toBffApiDeclaredTenantAttribute = (
+  tenantAttribute: tenantApi.DeclaredTenantAttribute,
+  registryAttributesMap: RegistryAttributesMap
+  // eslint-disable-next-line sonarjs/no-identical-functions
+): bffApi.DeclaredTenantAttribute | undefined => {
+  const registryAttribute = registryAttributesMap.get(tenantAttribute.id);
+
+  return registryAttribute
+    ? {
+        id: tenantAttribute.id,
+        name: registryAttribute.name,
+        description: registryAttribute.description,
+        assignmentTimestamp: tenantAttribute.assignmentTimestamp,
+        revocationTimestamp: tenantAttribute.revocationTimestamp,
+      }
+    : undefined;
+};
+
+const toBffApiVerifiedTenantAttribute = (
+  tenantAttribute: tenantApi.VerifiedTenantAttribute,
+  registryAttributesMap: RegistryAttributesMap
+): bffApi.VerifiedTenantAttribute | undefined => {
+  const registryAttribute = registryAttributesMap.get(tenantAttribute.id);
+
+  return registryAttribute
+    ? {
+        id: tenantAttribute.id,
+        name: registryAttribute.name,
+        description: registryAttribute.description,
+        assignmentTimestamp: tenantAttribute.assignmentTimestamp,
+        verifiedBy: tenantAttribute.verifiedBy,
+        revokedBy: tenantAttribute.revokedBy,
+      }
+    : undefined;
+};
+
+export function toBffApiCertifiedTenantAttributes(
+  certifiedAttributes: tenantApi.CertifiedTenantAttribute[],
+  registryAttributesMap: RegistryAttributesMap
+): bffApi.CertifiedTenantAttribute[] {
+  return certifiedAttributes
+    .map((tenantAttribute) =>
+      toBffApiCertifiedTenantAttribute(tenantAttribute, registryAttributesMap)
+    )
+    .filter(isDefined);
+}
+
+export function toBffApiDeclaredTenantAttributes(
+  declaredAttributes: tenantApi.DeclaredTenantAttribute[],
+  registryAttributesMap: RegistryAttributesMap
+): bffApi.DeclaredTenantAttribute[] {
+  return declaredAttributes
+    .map((tenantAttribute) =>
+      toBffApiDeclaredTenantAttribute(tenantAttribute, registryAttributesMap)
+    )
+    .filter(isDefined);
+}
+
+export function toBffApiVerifiedTenantAttributes(
+  verifiedAttributes: tenantApi.VerifiedTenantAttribute[],
+  registryAttributesMap: RegistryAttributesMap
+): bffApi.VerifiedTenantAttribute[] {
+  return verifiedAttributes
+    .map((tenantAttribute) =>
+      toBffApiVerifiedTenantAttribute(tenantAttribute, registryAttributesMap)
+    )
+    .filter(isDefined);
+}
