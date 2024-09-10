63ddae0940dd1a7d7fa2ce2105248a84c09fa1e2
Eric Camellini <eric.camellini@gmail.com>
2024-09-10 16:49:49 +0200

    Fixing config

--- packages/selfcare-onboarding-consumer/src/config/config.ts
+++ packages/selfcare-onboarding-consumer/src/config/config.ts
@@ -1,15 +1,16 @@
 import { z } from "zod";
-import {
-  KafkaConsumerConfig,
-  TokenGenerationConfig,
-} from "pagopa-interop-commons";
+import { TokenGenerationConfig } from "pagopa-interop-commons";
 
-export const SelfcareOnboardingConsumerConfig = KafkaConsumerConfig.and(
-  TokenGenerationConfig
-).and(
+export const SelfcareOnboardingConsumerConfig = TokenGenerationConfig.and(
   z
     .object({
-      SELFCARE_TOPIC: z.string(),
+      SELFCARE_BROKER_URLS: z.string(),
+      BROKER_CONNECTION_STRING: z.string(),
+      KAFKA_CLIENT_ID: z.string(),
+      KAFKA_GROUP_ID: z.string(),
+      TOPIC_NAME: z.string(),
+
+      RESET_CONSUMER_OFFSETS: z.string().default("false"),
 
       INTEROP_PRODUCT: z.string(),
       ALLOWED_ORIGINS: z.string(),
@@ -17,7 +18,12 @@ export const SelfcareOnboardingConsumerConfig = KafkaConsumerConfig.and(
       TENANT_PROCESS_URL: z.string(),
     })
     .transform((c) => ({
-      selfcareTopic: c.SELFCARE_TOPIC,
+      selfcareBrokerUrls: c.SELFCARE_BROKER_URLS.split(","),
+      brokerConnectionString: c.BROKER_CONNECTION_STRING,
+      kafkaClientId: c.KAFKA_CLIENT_ID,
+      kafkaGroupId: c.KAFKA_GROUP_ID,
+      topicName: c.TOPIC_NAME,
+      resetConsumerOffsets: c.RESET_CONSUMER_OFFSETS.toLowerCase() === "true",
       interopProduct: c.INTEROP_PRODUCT,
       allowedOrigins: c.ALLOWED_ORIGINS.split(","),
       tenantProcessUrl: c.TENANT_PROCESS_URL,
--- packages/selfcare-onboarding-consumer/src/index.ts
+++ packages/selfcare-onboarding-consumer/src/index.ts
@@ -3,6 +3,7 @@ import {
   InteropTokenGenerator,
   RefreshableInteropToken,
 } from "pagopa-interop-commons";
+import { logLevel } from "kafkajs";
 import { config } from "./config/config.js";
 import { tenantProcessClientBuilder } from "./clients/tenantProcessClient.js";
 import { selfcareOnboardingProcessorBuilder } from "./services/selfcareOnboardingProcessor.js";
@@ -20,4 +21,18 @@ const processor = selfcareOnboardingProcessorBuilder(
   config.allowedOrigins
 );
 
-await runConsumer(config, [config.selfcareTopic], processor.processMessage);
+// remember to pass starting offset earliest and also disable IAM auth
+await runConsumer(
+  {
+    kafkaBrokers: config.selfcareBrokerUrls,
+    kafkaDisableAwsIamAuth: true,
+    kafkaBrokerConnectionString: config.brokerConnectionString,
+    kafkaClientId: config.kafkaClientId,
+    kafkaGroupId: config.kafkaGroupId,
+    resetConsumerOffsets: config.resetConsumerOffsets,
+    kafkaLogLevel: logLevel.INFO,
+    topicStartingOffset: "earliest",
+  },
+  [config.topicName],
+  processor.processMessage
+);
