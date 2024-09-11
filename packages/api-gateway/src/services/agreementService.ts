9910d84e81e03615aaa1b84a907519c0259339d2
Eric Camellini <eric.camellini@gmail.com>
2024-09-11 18:04:53 +0200

    IMN-745 - API Gateway - GET Agreement Purposes (#905)

--- packages/api-gateway/src/services/agreementService.ts
+++ packages/api-gateway/src/services/agreementService.ts
@@ -1,7 +1,12 @@
-import { agreementApi, apiGatewayApi } from "pagopa-interop-api-clients";
 import { getAllFromPaginated, WithLogger } from "pagopa-interop-commons";
 import {
+  agreementApi,
+  apiGatewayApi,
+  purposeApi,
+} from "pagopa-interop-api-clients";
+import {
   AgreementProcessClient,
+  PurposeProcessClient,
   TenantProcessClient,
 } from "../clients/clientsProvider.js";
 import { ApiGatewayAppContext } from "../utilities/context.js";
@@ -9,11 +14,16 @@ import { toApiGatewayAgreementIfNotDraft } from "../api/agreementApiConverter.js
 import { producerAndConsumerParamMissing } from "../models/errors.js";
 import { toAgreementProcessGetAgreementsQueryParams } from "../api/agreementApiConverter.js";
 import { toApiGatewayAgreementAttributes } from "../api/attributesApiConverter.js";
+import {
+  toApiGatewayPurpose,
+  toPurposeProcessGetPurposesQueryParams,
+} from "../api/purposeApiConverter.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function agreementServiceBuilder(
   agreementProcessClient: AgreementProcessClient,
-  tenantProcessClient: TenantProcessClient
+  tenantProcessClient: TenantProcessClient,
+  purposeProcessClient: PurposeProcessClient
 ) {
   return {
     getAgreements: async (
@@ -84,6 +94,39 @@ export function agreementServiceBuilder(
       });
 
       return toApiGatewayAgreementAttributes(agreement, tenant);
+    },
+
+    getAgreementPurposes: async (
+      { logger, headers }: WithLogger<ApiGatewayAppContext>,
+      agreementId: agreementApi.Agreement["id"]
+    ): Promise<apiGatewayApi.Purposes> => {
+      logger.info(`Retrieving Purposes for Agreement ${agreementId}`);
+
+      const agreement = await agreementProcessClient.getAgreementById({
+        headers,
+        params: {
+          agreementId,
+        },
+      });
+
+      const getPurposesQueryParams = toPurposeProcessGetPurposesQueryParams({
+        eserviceId: agreement.eserviceId,
+        consumerId: agreement.consumerId,
+      });
+
+      const purposes = await getAllFromPaginated<purposeApi.Purpose>(
+        async (offset, limit) =>
+          await purposeProcessClient.getPurposes({
+            headers,
+            queries: {
+              ...getPurposesQueryParams,
+              offset,
+              limit,
+            },
+          })
+      );
+
+      return { purposes: purposes.map(toApiGatewayPurpose) };
     },
   };
 }
