e1bdc9535ab9f2c90e0b9a4ff946c8bad92963c3
Eric Camellini <eric.camellini@gmail.com>
2024-09-11 10:11:42 +0200

    Improving prefix

--- packages/api-clients/src/axiosLogInterceptors.ts
+++ packages/api-clients/src/axiosLogInterceptors.ts
@@ -12,14 +12,16 @@ setGlobalConfig({
   headers: false,
 });
 
-function getCorrelationIdLogString(headers?: AxiosHeaders): string {
+function getPrefix(headers?: AxiosHeaders, clientName: string = ""): string {
   const correlationId = headers?.["X-Correlation-Id"];
-  return correlationId ? `CID=${correlationId}][` : "";
+  return correlationId ? `CID=${correlationId}][${clientName}` : clientName;
+  // AxiosLogger already logs the prefix inside square brackets,
+  // adding ][ after the CID to log [CID=...][Client Name]
 }
 
 export function configureAxiosLogInterceptors(
   axiosInstance: AxiosInstance,
-  prefix: string
+  clientName: string
 ): void {
   axiosInstance.interceptors.response.use(
     (response: AxiosResponse): AxiosResponse =>
@@ -27,8 +29,7 @@ export function configureAxiosLogInterceptors(
         response as Parameters<typeof AxiosLogger.responseLogger>[0],
         {
           logger: genericLogger.info,
-          prefixText:
-            getCorrelationIdLogString(response.config.headers) + prefix,
+          prefixText: getPrefix(response.config.headers, clientName),
         }
       ) as AxiosResponse,
     (error: AxiosError): Promise<AxiosError> =>
@@ -36,7 +37,7 @@ export function configureAxiosLogInterceptors(
         error as Parameters<typeof AxiosLogger.errorLogger>[0],
         {
           logger: genericLogger.error,
-          prefixText: getCorrelationIdLogString(error.config?.headers) + prefix,
+          prefixText: getPrefix(error.config?.headers, clientName),
         }
       ) as Promise<AxiosError>
   );
