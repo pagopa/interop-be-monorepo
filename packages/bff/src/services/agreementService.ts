602c34739ace74b9ec534da40666ed8785a94177
Eric Camellini <eric.camellini@gmail.com>
2024-09-11 16:25:10 +0200

    Fixing build

--- packages/bff/src/services/agreementService.ts
+++ packages/bff/src/services/agreementService.ts
@@ -3,7 +3,7 @@
 
 import {
   getAllFromPaginated,
-  toSetToArray,
+  removeDuplicates,
   WithLogger,
 } from "pagopa-interop-commons";
 import {
@@ -379,7 +379,7 @@ export async function enrichAgreement(
   const activeDescriptorAttributes = activeDescriptor
     ? descriptorAttributesIds(activeDescriptor)
     : [];
-  const allAttributesIds = toSetToArray([
+  const allAttributesIds = removeDuplicates([
     ...activeDescriptorAttributes,
     ...tenantAttributesIds(consumer),
   ]);
