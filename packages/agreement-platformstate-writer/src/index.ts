608f19bc85557b232c61ecece6acd99a6a83d5c6
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-09-12 17:03:08 +0200

    Add scaffold for new package

---
+++ packages/agreement-platformstate-writer/src/index.ts
@@ -0,0 +1,40 @@
+import { EachMessagePayload } from "kafkajs";
+import { logger, decodeKafkaMessage } from "pagopa-interop-commons";
+import { runConsumer } from "kafka-iam-auth";
+import { AgreementEvent } from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { handleMessageV1 } from "./consumerServiceV1.js";
+import { handleMessageV2 } from "./consumerServiceV2.js";
+import { config } from "./config/config.js";
+
+const dynamoDBClient = new DynamoDBClient({
+  credentials: { accessKeyId: "key", secretAccessKey: "secret" },
+  region: "eu-central-1",
+  endpoint: `http://${config.tokenGenerationReadModelDbHost}:${config.tokenGenerationReadModelDbPort}`,
+});
+async function processMessage({
+  message,
+  partition,
+}: EachMessagePayload): Promise<void> {
+  const decodedMessage = decodeKafkaMessage(message, AgreementEvent);
+
+  const loggerInstance = logger({
+    serviceName: "agreement-platformstate-writer",
+    eventType: decodedMessage.type,
+    eventVersion: decodedMessage.event_version,
+    streamId: decodedMessage.stream_id,
+    correlationId: decodedMessage.correlation_id,
+  });
+
+  await match(decodedMessage)
+    .with({ event_version: 1 }, (msg) => handleMessageV1(msg, dynamoDBClient))
+    .with({ event_version: 2 }, (msg) => handleMessageV2(msg, dynamoDBClient))
+    .exhaustive();
+
+  loggerInstance.info(
+    `Token-generation read model was updated. Partition number: ${partition}. Offset: ${message.offset}`
+  );
+}
+
+await runConsumer(config, [config.agreementTopic], processMessage);
---
+++ packages/agreement-platformstate-writer/test/utils.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+
+describe("test", () => {
+  it("test", () => {
+    expect(1).toBe(1);
+  });
+  // TODO which utils will be tested here?
+});
