f2b2a166fa6c93706f45f7bf693a4d807e2247f3
Eric Camellini <eric.camellini@gmail.com>
2024-09-19 14:13:27 +0200

    Updating comment

--- packages/api-gateway/src/api/tenantApiConverter.ts
+++ packages/api-gateway/src/api/tenantApiConverter.ts
@@ -77,6 +77,10 @@ export function toM2MTenantSeed(
     name: "DUMMY_VALUE_TO_BE_REMOVED",
     /* ^ dummy value: no need to pass institution name anymore after migration to node.
     Tenant will be only updated, not created. In this way, we avoid retrieving the insitution
-    from the institution API through party registry proxy service, which will not be migrated. */
+    from the institution API through party registry proxy service, which will not be migrated.
+
+    TODO update M2MAttributeSeed in tenant spec and remove this dummy value
+    https://pagopa.atlassian.net/browse/IMN-822
+    */
   };
 }
