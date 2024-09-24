611d8ca60299c91064a101b9cdd4cd07ace0a97b
Eric Camellini <eric.camellini@gmail.com>
2024-09-24 14:14:10 +0200

    IMN-710 - selfcare-onboarding-consumer (#959)
    Co-authored-by: AsterITA <fabrizio.musella@gmail.com>

---
+++ packages/selfcare-onboarding-consumer/src/model/institutionEvent.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+const SubUnitType = z.enum(["AOO", "UO"]);
+type SubUnitType = z.infer<typeof SubUnitType>;
+
+const InstitutionEvent = z.object({
+  description: z.string().trim().min(1),
+  origin: z.string().trim().min(1),
+  originId: z.string().trim().min(1),
+  taxCode: z.string().trim().min(1).nullish(),
+  subUnitCode: z.string().optional().nullish(), // AOO/UO ID
+  subUnitType: SubUnitType.optional().nullish(),
+  digitalAddress: z.string().trim().min(1),
+});
+export type InstitutionEvent = z.infer<typeof InstitutionEvent>;
+
+export const InstitutionEventPayload = z.object({
+  id: z.string(),
+  internalIstitutionID: z.string().trim().min(1), // Selfcare ID
+  product: z.string().trim().min(1),
+  onboardingTokenId: z.string(),
+  institution: InstitutionEvent,
+  createdAt: z.string(),
+});
+export type InstitutionEventPayload = z.infer<typeof InstitutionEventPayload>;
