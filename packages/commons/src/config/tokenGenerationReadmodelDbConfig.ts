2ed47eb3a4a9642cdab2622a48cd32a91e882500
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-09-24 10:01:01 +0200

    Refactor

--- packages/commons/src/config/tokenGenerationReadmodelDbConfig.ts
+++ packages/commons/src/config/tokenGenerationReadmodelDbConfig.ts
@@ -6,6 +6,8 @@ export const TokenGenerationReadModelDbConfig = z
     TOKEN_GENERATION_READMODEL_PORT: z.coerce.number().min(1001),
     TOKEN_GENERATION_READMODEL_TABLE_NAME_PLATFORM: z.string(),
     TOKEN_GENERATION_READMODEL_TABLE_NAME_TOKEN_GENERATION: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
   })
   .transform((c) => ({
     tokenGenerationReadModelDbHost: c.TOKEN_GENERATION_READMODEL_HOST,
@@ -14,6 +16,8 @@ export const TokenGenerationReadModelDbConfig = z
       c.TOKEN_GENERATION_READMODEL_TABLE_NAME_PLATFORM,
     tokenGenerationReadModelTableNameTokenGeneration:
       c.TOKEN_GENERATION_READMODEL_TABLE_NAME_TOKEN_GENERATION,
+    awsAccessKeyId: c.AWS_ACCESS_KEY_ID,
+    awsSecretAccessKey: c.AWS_SECRET_ACCESS_KEY,
   }));
 
 export type TokenGenerationReadModelDbConfig = z.infer<
