3cd657c98d498c012aef3aaad9cf17882a8bc006
Eric Camellini <eric.camellini@gmail.com>
2024-09-26 11:55:27 +0200

    Fixing error mapper

--- packages/tenant-process/src/utilities/errorMappers.ts
+++ packages/tenant-process/src/utilities/errorMappers.ts
@@ -23,7 +23,7 @@ export const getTenantByExternalIdErrorMapper = (
   error: ApiError<ErrorCodes>
 ): number =>
   match(error.code)
-    .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("tenantNotFoundByExternalId", () => HTTP_STATUS_NOT_FOUND)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const getTenantBySelfcareIdErrorMapper = (
