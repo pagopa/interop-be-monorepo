5ee83a155ebe48be1497aed87be3baba3e5e8429
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2024-10-01 09:30:23 +0200

    Remove cleanup

--- packages/purpose-platformstate-writer/test/utils.ts
+++ packages/purpose-platformstate-writer/test/utils.ts
@@ -1,7 +1,3 @@
-import { setupTestContainersVitest } from "pagopa-interop-commons-test/index.js";
-import { afterEach, inject } from "vitest";
+import { inject } from "vitest";
 
 export const config = inject("tokenGenerationReadModelConfig");
-export const { cleanup } = setupTestContainersVitest();
-
-afterEach(cleanup);
