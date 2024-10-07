c287f1736b80bd8a6f9473c29c93efd406fa8e38
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-10-07 17:15:53 +0200

    Fix jwt token verification skipping invalid WELL_KNOWN_URLS (#1002)

--- packages/attribute-registry-process/src/app.ts
+++ packages/attribute-registry-process/src/app.ts
@@ -6,6 +6,7 @@ import {
 } from "pagopa-interop-commons";
 import attributeRouter from "./routers/AttributeRouter.js";
 import healthRouter from "./routers/HealthRouter.js";
+import { config } from "./config/config.js";
 
 const serviceName = "attribute-registry-process";
 
@@ -17,7 +18,7 @@ app.disable("x-powered-by");
 
 app.use(contextMiddleware(serviceName));
 app.use(healthRouter);
-app.use(authenticationMiddleware);
+app.use(authenticationMiddleware(config));
 app.use(loggerMiddleware(serviceName));
 app.use(attributeRouter(zodiosCtx));
 
