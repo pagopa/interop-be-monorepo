d6fb39a1b96cff8fb8e8e8a4b0b532438b5c7fdc
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-10-07 11:47:34 +0200

    Add test

--- packages/authorization-platformstate-writer/test/consumerServiceV2.integration.test.ts
+++ packages/authorization-platformstate-writer/test/consumerServiceV2.integration.test.ts
@@ -18,20 +18,24 @@ import {
   getMockClient,
   getMockKey,
   getMockTokenStatesClientPurposeEntry,
+  readAllPlatformStateItems,
   readAllTokenStateItems,
 } from "pagopa-interop-commons-test";
 import {
   AuthorizationEventEnvelope,
   Client,
+  ClientDeletedV2,
+  ClientId,
   ClientKeyDeletedV2,
   clientKindTokenStates,
-  ClientKindTokenStates,
   generateId,
-  GSIPKKid,
+  itemState,
   makeGSIPKClient,
   makeGSIPKKid,
+  makePlatformStatesClientPK,
   makeTokenGenerationStatesClientKidPK,
   makeTokenGenerationStatesClientKidPurposePK,
+  PlatformStatesClientEntry,
   PurposeId,
   toClientV2,
   TokenGenerationStatesClientEntry,
@@ -40,10 +44,11 @@ import {
 } from "pagopa-interop-models";
 import { handleMessageV2 } from "../src/consumerServiceV2.js";
 import {
+  writeClientEntry,
   writeTokenStateClientEntry,
   writeTokenStateClientPurposeEntry,
 } from "../src/utils.js";
-import { config, sleep } from "./utils.js";
+import { config } from "./utils.js";
 
 describe("integration tests V2 events", async () => {
   if (!config) {
@@ -167,8 +172,118 @@ describe("integration tests V2 events", async () => {
   });
 
   describe("ClientDeleted", () => {
-    it("sample", () => {
-      expect(1).toBe(1);
+    it("should delete platform-state entry and token-generation-states entries", async () => {
+      const purposeId = generateId<PurposeId>();
+      const client: Client = {
+        ...getMockClient(),
+        purposes: [purposeId],
+        keys: [getMockKey()],
+      };
+
+      const payload: ClientDeletedV2 = {
+        client: toClientV2(client),
+        clientId: client.id,
+      };
+      const message: AuthorizationEventEnvelope = {
+        sequence_num: 1,
+        stream_id: client.id,
+        version: 2,
+        type: "ClientDeleted",
+        event_version: 2,
+        data: payload,
+        log_date: new Date(),
+      };
+
+      const otherClientId = generateId<ClientId>();
+
+      const pk1PlatformStates = makePlatformStatesClientPK(client.id);
+      const clientPlatformStateEntry1: PlatformStatesClientEntry = {
+        PK: pk1PlatformStates,
+        version: 1,
+        state: itemState.active,
+        updatedAt: new Date().toISOString(),
+        clientPurposesIds: [],
+      };
+
+      const pk2PlatformStates = makePlatformStatesClientPK(otherClientId);
+      const clientPlatformStateEntry2: PlatformStatesClientEntry = {
+        PK: pk2PlatformStates,
+        version: 1,
+        state: itemState.active,
+        updatedAt: new Date().toISOString(),
+        clientPurposesIds: [],
+      };
+
+      const pkTokenStates1 = makeTokenGenerationStatesClientKidPK({
+        clientId: client.id,
+        kid: "kid",
+      });
+      const pkTokenStates2 = makeTokenGenerationStatesClientKidPurposePK({
+        clientId: client.id,
+        kid: "kid",
+        purposeId,
+      });
+
+      const pkTokenStates3 = makeTokenGenerationStatesClientKidPurposePK({
+        clientId: otherClientId,
+        kid: "kid",
+        purposeId,
+      });
+
+      const clientTokenStateEntry: TokenGenerationStatesClientEntry = {
+        PK: pkTokenStates1,
+        consumerId: generateId(),
+        updatedAt: new Date().toISOString(),
+        clientKind: clientKindTokenStates.consumer,
+        publicKey: "PEM",
+        GSIPK_clientId: client.id,
+        GSIPK_kid: makeGSIPKKid("kid"),
+      };
+
+      const clientPurposeTokenStateEntry: TokenGenerationStatesClientPurposeEntry =
+        {
+          ...getMockTokenStatesClientPurposeEntry(),
+          PK: pkTokenStates2,
+          GSIPK_kid: makeGSIPKKid("kid"),
+          GSIPK_clientId: client.id,
+        };
+
+      const otherClientPurposeTokenStateEntry: TokenGenerationStatesClientPurposeEntry =
+        {
+          ...getMockTokenStatesClientPurposeEntry(),
+          PK: pkTokenStates3,
+          GSIPK_kid: makeGSIPKKid("kid"),
+          GSIPK_clientId: otherClientId,
+        };
+
+      await writeClientEntry(clientPlatformStateEntry1, dynamoDBClient);
+      await writeClientEntry(clientPlatformStateEntry2, dynamoDBClient);
+      await writeTokenStateClientEntry(clientTokenStateEntry, dynamoDBClient);
+      await writeTokenStateClientPurposeEntry(
+        clientPurposeTokenStateEntry,
+        dynamoDBClient
+      );
+      await writeTokenStateClientPurposeEntry(
+        otherClientPurposeTokenStateEntry,
+        dynamoDBClient
+      );
+
+      await handleMessageV2(message, dynamoDBClient);
+
+      const retrievedPlatformStatesEntries = await readAllPlatformStateItems(
+        dynamoDBClient
+      );
+
+      const retrievedTokenStatesEntries = await readAllTokenStateItems(
+        dynamoDBClient
+      );
+
+      expect(retrievedPlatformStatesEntries).toEqual([
+        clientPlatformStateEntry2,
+      ]);
+      expect(retrievedTokenStatesEntries).toEqual([
+        otherClientPurposeTokenStateEntry,
+      ]);
     });
   });
 });
