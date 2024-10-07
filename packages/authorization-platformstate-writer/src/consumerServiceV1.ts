f1a283bdb6dc241a0b42ddb2fe1321563c682526
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-10-07 11:53:09 +0200

    Draft

--- packages/authorization-platformstate-writer/src/consumerServiceV1.ts
+++ packages/authorization-platformstate-writer/src/consumerServiceV1.ts
@@ -1,18 +1,43 @@
 import { match } from "ts-pattern";
-import { AuthorizationEventEnvelopeV1 } from "pagopa-interop-models";
+import {
+  AuthorizationEventEnvelopeV1,
+  ClientId,
+  makeGSIPKClient,
+  makeGSIPKKid,
+  makePlatformStatesClientPK,
+  unsafeBrandId,
+} from "pagopa-interop-models";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  deleteClientEntryFromPlatformStates,
+  deleteEntriesFromTokenStatesByClient,
+  deleteEntriesFromTokenStatesByKid,
+} from "./utils.js";
 
 export async function handleMessageV1(
   message: AuthorizationEventEnvelopeV1,
-  _dynamoDBClient: DynamoDBClient
+  dynamoDBClient: DynamoDBClient
 ): Promise<void> {
   await match(message)
+    .with({ type: "KeyDeleted" }, async (msg) => {
+      const GSIPK_kid = makeGSIPKKid(msg.data.keyId);
+      await deleteEntriesFromTokenStatesByKid(GSIPK_kid, dynamoDBClient);
+    })
+    .with({ type: "ClientDeleted" }, async (msg) => {
+      const clientId = unsafeBrandId<ClientId>(msg.data.clientId);
+      const pk = makePlatformStatesClientPK(clientId);
+      await deleteClientEntryFromPlatformStates(pk, dynamoDBClient);
+
+      const GSIPK_clientId = makeGSIPKClient(clientId);
+      await deleteEntriesFromTokenStatesByClient(
+        GSIPK_clientId,
+        dynamoDBClient
+      );
+    })
     .with(
       { type: "ClientAdded" },
-      { type: "ClientDeleted" },
       { type: "ClientPurposeAdded" },
       { type: "ClientPurposeRemoved" },
-      { type: "KeyDeleted" },
       { type: "KeyRelationshipToUserMigrated" },
       { type: "KeysAdded" },
       { type: "RelationshipAdded" },
