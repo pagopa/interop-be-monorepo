8a4fd8e6228bc1e8972d6f19134f9a7fe550dde4
Sandro Tajè <sandro.taje@gmail.com>
2024-10-08 17:31:45 +0200

    Add get delegation by id

---
+++ packages/delegation-process/src/model/domain/apiConverter.ts
@@ -0,0 +1,86 @@
+import { delegationApi } from "pagopa-interop-api-clients";
+import {
+  Delegation,
+  DelegationContractDocument,
+  DelegationKind,
+  DelegationStamp,
+  DelegationStamps,
+  DelegationState,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+
+export const delegationToApiDelegation = (
+  delegation: Delegation
+): delegationApi.Delegation => ({
+  id: delegation.id,
+  delegatorId: delegation.delegatorId,
+  delegateId: delegation.delegateId,
+  eserviceId: delegation.eserviceId,
+  createdAt: delegation.createdAt.toJSON(),
+  submittedAt: delegation.submittedAt.toJSON(),
+  approvedAt: delegation.approvedAt
+    ? delegation.approvedAt.toJSON()
+    : undefined,
+  rejectedAt: delegation.rejectedAt
+    ? delegation.rejectedAt.toJSON()
+    : undefined,
+  rejectionReason: delegation.rejectionReason || undefined,
+  revokedAt: delegation.revokedAt ? delegation.revokedAt.toJSON() : undefined,
+  state: delegationStateToApiDelegationState(delegation.state),
+  kind: delegationKindToApiDelegationKind(delegation.kind),
+  contract: delegation.contract
+    ? delegationContractToApiDelegationContract(delegation.contract)
+    : undefined,
+  stamps: delegationStampsToApiDelegationStamps(delegation.stamps),
+});
+
+export const delegationStateToApiDelegationState = (
+  state: DelegationState
+): delegationApi.DelegationState =>
+  match<DelegationState, delegationApi.DelegationState>(state)
+    .with("Active", () => "ACTIVE")
+    .with("Rejected", () => "REJECTED")
+    .with("Revoked", () => "REVOKED")
+    .with("WaitingForApproval", () => "WAITING_FOR_APPROVAL")
+    .exhaustive();
+
+export const delegationKindToApiDelegationKind = (
+  kind: DelegationKind
+): delegationApi.DelegationKind =>
+  match<DelegationKind, delegationApi.DelegationKind>(kind)
+    .with("DelegatedConsumer", () => "DELEGATED_CONSUMER")
+    .with("DelegatedProducer", () => "DELEGATED_PRODUCER")
+    .exhaustive();
+
+export const delegationContractToApiDelegationContract = (
+  contract: DelegationContractDocument
+): delegationApi.DelegationContractDocument => ({
+  id: contract.id,
+  name: contract.name,
+  prettyName: contract.prettyName,
+  contentType: contract.contentType,
+  path: contract.path,
+  createdAt: contract.createdAt.toJSON(),
+});
+
+export const delegationStampsToApiDelegationStamps = (
+  stamps: DelegationStamps
+): delegationApi.DelegationStamps => ({
+  submission: delegationStampToApiDelegationStamp(stamps.submission),
+  activation: stamps.activation
+    ? delegationStampToApiDelegationStamp(stamps.activation)
+    : undefined,
+  rejection: stamps.rejection
+    ? delegationStampToApiDelegationStamp(stamps.rejection)
+    : undefined,
+  revocation: stamps.revocation
+    ? delegationStampToApiDelegationStamp(stamps.revocation)
+    : undefined,
+});
+
+export const delegationStampToApiDelegationStamp = (
+  stamp: DelegationStamp
+): delegationApi.DelegationStamp => ({
+  who: stamp.who,
+  when: stamp.when.toJSON(),
+});
--- packages/delegation-process/src/services/delegationProducerService.ts
+++ packages/delegation-process/src/services/delegationProducerService.ts
@@ -1,8 +1,20 @@
+import { Delegation, DelegationId } from "pagopa-interop-models";
+import { delegationNotFound } from "../model/domain/errors.js";
 import { ReadModelService } from "./readModelService.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function delegationProducerServiceBuilder(
-  _readModelService: ReadModelService
+  readModelService: ReadModelService
 ) {
-  return {};
+  return {
+    async getDelegationById(delegationId: DelegationId): Promise<Delegation> {
+      const delegation = await readModelService.getDelegationById(delegationId);
+
+      if (!delegation) {
+        throw delegationNotFound(delegationId);
+      }
+
+      return delegation;
+    },
+  };
 }
