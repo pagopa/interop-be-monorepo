f153f5c9a9d9cb13bd719cbd30f5c43675c74643
Sandro Tajè <sandro.taje@gmail.com>
2024-10-08 17:31:45 +0200

    Add get delegation by id

--- packages/delegation-process/src/routers/DelegationProducerRouter.ts
+++ packages/delegation-process/src/routers/DelegationProducerRouter.ts
@@ -3,9 +3,22 @@ import { ZodiosRouter } from "@zodios/express";
 import { delegationApi } from "pagopa-interop-api-clients";
 import {
   ExpressContext,
+  ReadModelRepository,
   ZodiosContext,
+  fromAppContext,
   zodiosValidationErrorToApiProblem,
 } from "pagopa-interop-commons";
+import { unsafeBrandId } from "pagopa-interop-models";
+import { readModelServiceBuilder } from "../services/readModelService.js";
+import { config } from "../config/config.js";
+import { delegationProducerServiceBuilder } from "../services/delegationProducerService.js";
+import { delegationToApiDelegation } from "../model/domain/apiConverter.js";
+import { makeApiProblem } from "../model/domain/errors.js";
+import { getDelegationByIdErrorMapper } from "../utilites/errorMappers.js";
+
+const readModelService = readModelServiceBuilder(
+  ReadModelRepository.init(config)
+);
 
 const delegationProducerRouter = (
   ctx: ZodiosContext
@@ -14,10 +27,36 @@ const delegationProducerRouter = (
     validationErrorHandler: zodiosValidationErrorToApiProblem,
   });
 
+  const delegationProducerService =
+    delegationProducerServiceBuilder(readModelService);
+
   delegationRouter
-    .get("/producer/delegations/:delegationId", async (_req, res) =>
-      res.status(501).send()
-    )
+    .get("/producer/delegations/:delegationId", async (req, res) => {
+      const ctx = fromAppContext(req.ctx);
+      const { delegationId } = req.params;
+
+      try {
+        const delegation = await delegationProducerService.getDelegationById(
+          unsafeBrandId(delegationId)
+        );
+
+        return res
+          .status(200)
+          .send(
+            delegationApi.Delegation.parse(
+              delegationToApiDelegation(delegation)
+            )
+          );
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          getDelegationByIdErrorMapper,
+          ctx.logger
+        );
+
+        return res.status(errorRes.status).send(errorRes);
+      }
+    })
     .get("/producer/delegations", async (_req, res) => res.status(501).send())
     .post("/producer/delegations", async (_req, res) => res.status(501).send())
     .post("/producer/delegations/:delegationId/approve", async (_req, res) =>
