642fba3004e31be2f298fbf7a869bd1062131ad4
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-10-09 10:15:58 +0200

    PIN-5364 scaffolding delegation process (#1066)
    Co-authored-by: Sandro Tajè <sandro.taje@gmail.com>

---
+++ packages/delegation-process/test/getDelegationById.test.ts
@@ -0,0 +1,9 @@
+/* eslint-disable functional/no-let */
+import { expect, describe, it } from "vitest";
+
+describe("get delegation by id", () => {
+  it("should get the delegation if it exists", async () => {
+    // Write your test here
+    expect(true).toBe(true);
+  });
+});
