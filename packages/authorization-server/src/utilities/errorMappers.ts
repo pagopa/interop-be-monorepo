96faff648d54e70d472c86b915f5d0fa8d32f6eb
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-10-15 10:48:01 +0200

    Fix and refactor

--- packages/authorization-server/src/utilities/errorMappers.ts
+++ packages/authorization-server/src/utilities/errorMappers.ts
@@ -9,5 +9,5 @@ const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = constants;
 
 export const sampleErrorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
-    .with("sampleError", () => HTTP_STATUS_NOT_FOUND)
+    .with("keyNotFound", () => HTTP_STATUS_NOT_FOUND)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
