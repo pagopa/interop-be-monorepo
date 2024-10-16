15728fe9f00d28a1948d16592382902120758f95
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-10-16 14:42:27 +0200

    Add test

--- packages/token-details-persister/test/consumerService.integration.test.ts
+++ packages/token-details-persister/test/consumerService.integration.test.ts
@@ -1,7 +1,52 @@
+import { GeneratedTokenAuditDetails, generateId } from "pagopa-interop-models";
 import { describe, expect, it } from "vitest";
+import { genericLogger } from "pagopa-interop-commons";
+import { handleMessages } from ".././src/consumerService.js";
+import { config } from "../src/config/config.js";
+import { fileManager } from "./utils.js";
 
-describe("sample", () => {
-  it("test", () => {
+describe("consumerService", () => {
+  it("should write one entry on the bucket", async () => {
+    const auditDetails = getMockAuditDetails();
+
+    expect(
+      await fileManager.listFiles(config.s3Bucket, genericLogger)
+    ).toMatchObject([]);
+
+    await handleMessages([auditDetails], fileManager, genericLogger);
+  });
+
+  it("should write two entries on the bucket", () => {
     expect(1).toBe(1);
   });
+});
+
+const getMockAuditDetails = (): GeneratedTokenAuditDetails => ({
+  correlationId: generateId(),
+  eserviceId: generateId(),
+  descriptorId: generateId(),
+  agreementId: generateId(),
+  subject: generateId(),
+  audience: "uat.interop.pagopa.it",
+  purposeId: generateId(),
+  algorithm: "RS256",
+  clientId: generateId(),
+  keyId: generateId(),
+  purposeVersionId: generateId(),
+  jwtId: generateId(),
+  issuedAt: new Date().getMilliseconds(),
+  issuer: "issuer",
+  expirationTime: new Date().getMilliseconds(),
+  organizationId: generateId(),
+  notBefore: new Date().getMilliseconds(),
+  clientAssertion: {
+    subject: generateId(),
+    audience: "uat.interop.pagopa.it",
+    algorithm: "RS256",
+    keyId: generateId(),
+    jwtId: generateId(),
+    issuedAt: new Date().getMilliseconds(),
+    issuer: "issuer",
+    expirationTime: new Date().getMilliseconds(),
+  },
 });
