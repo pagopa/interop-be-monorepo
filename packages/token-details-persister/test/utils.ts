d7eb96990daf1b31e5684098ceef5cfdcc0a4cf5
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-10-16 15:08:21 +0200

    Fix

--- packages/token-details-persister/test/utils.ts
+++ packages/token-details-persister/test/utils.ts
@@ -1,11 +1,10 @@
 import { inject, afterEach } from "vitest";
 import { setupTestContainersVitest } from "pagopa-interop-commons-test";
 
-export const { cleanup, readModelRepository, postgresDB, fileManager } =
-  await setupTestContainersVitest(
-    undefined,
-    undefined,
-    inject("fileManagerConfig")
-  );
+export const { cleanup, fileManager } = await setupTestContainersVitest(
+  undefined,
+  undefined,
+  inject("fileManagerConfig")
+);
 
 afterEach(cleanup);
