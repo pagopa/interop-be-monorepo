74f14e44d5f8f1e3e87774afb2832479f5224940
Vittorio Caprio <vittorio.caprio@buildo.io>
2024-10-16 11:06:28 +0200

    Fix test for getDelegationById

--- packages/delegation-process/test/getDelegationById.test.ts
+++ packages/delegation-process/test/getDelegationById.test.ts
@@ -3,7 +3,7 @@ import { getMockDelegationProducer } from "pagopa-interop-commons-test/index.js"
 import { DelegationId, generateId } from "pagopa-interop-models";
 import { describe, expect, it } from "vitest";
 import { delegationNotFound } from "../src/model/domain/errors.js";
-import { addOneDelegation, delegationProducerService } from "./utils.js";
+import { addOneDelegation, delegationService } from "./utils.js";
 
 describe("get delegation by id", () => {
   it("should get the delegation if it exists", async () => {
@@ -11,8 +11,9 @@ describe("get delegation by id", () => {
 
     await addOneDelegation(delegation);
 
-    const expectedDelegation =
-      await delegationProducerService.getDelegationById(delegation.id);
+    const expectedDelegation = await delegationService.getDelegationById(
+      delegation.id
+    );
 
     expect(delegation).toEqual(expectedDelegation);
   });
@@ -23,8 +24,7 @@ describe("get delegation by id", () => {
     await addOneDelegation(delegation);
 
     const notFoundId = generateId<DelegationId>();
-    const expectedDelegation =
-      delegationProducerService.getDelegationById(notFoundId);
+    const expectedDelegation = delegationService.getDelegationById(notFoundId);
 
     await expect(expectedDelegation).rejects.toThrow(
       delegationNotFound(notFoundId)
