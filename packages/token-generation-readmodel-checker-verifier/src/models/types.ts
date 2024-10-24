e05d21d9816b767d28d2f62bd5409622c5ab9f0f
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2024-10-24 17:23:16 +0200

    Add purpose types

---
+++ packages/token-generation-readmodel-checker-verifier/src/models/types.ts
@@ -0,0 +1,39 @@
+import {
+  PlatformStatesPurposeEntry,
+  Purpose,
+  TokenGenerationStatesClientPurposeEntry,
+} from "pagopa-interop-models";
+import { z } from "zod";
+
+// purpose
+export const ReducedPurpose = Purpose.pick({
+  id: true,
+  consumerId: true,
+  eserviceId: true,
+  versions: true,
+});
+export type ReducedPurpose = z.infer<typeof ReducedPurpose>;
+
+export const PlatformStatesPurposeEntryDiff = PlatformStatesPurposeEntry.pick({
+  PK: true,
+  state: true,
+  purposeConsumerId: true,
+  purposeEserviceId: true,
+  purposeVersionId: true,
+});
+export type PlatformStatesPurposeEntryDiff = z.infer<
+  typeof PlatformStatesPurposeEntryDiff
+>;
+
+export const TokenGenerationStatesPurposeEntryDiff =
+  TokenGenerationStatesClientPurposeEntry.pick({
+    PK: true,
+    consumerId: true,
+    GSIPK_purposeId: true,
+    purposeState: true,
+    purposeVersionId: true,
+    GSIPK_clientId_purposeId: true,
+  });
+export type TokenGenerationStatesPurposeEntryDiff = z.infer<
+  typeof TokenGenerationStatesPurposeEntryDiff
+>;
