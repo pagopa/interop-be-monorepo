13be51843892440448f01c037e3a9fc3a3a29d30
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-10-31 18:05:38 +0100

    Refactor

--- packages/client-assertion-validation/src/types.ts
+++ packages/client-assertion-validation/src/types.ts
@@ -1,12 +1,4 @@
-import {
-  ApiError,
-  ClientId,
-  DescriptorId,
-  ItemState,
-  PurposeId,
-  PurposeVersionId,
-  TenantId,
-} from "pagopa-interop-models";
+import { ApiError, ClientId, PurposeId, TenantId } from "pagopa-interop-models";
 import { z } from "zod";
 import { ErrorCodes } from "./errors.js";
 
@@ -48,29 +40,6 @@ export const ClientAssertion = z
   })
   .strict();
 export type ClientAssertion = z.infer<typeof ClientAssertion>;
-
-const ComponentState = z.object({
-  state: ItemState,
-});
-
-export type ComponentState = z.infer<typeof ComponentState>;
-
-const AgreementComponentState = ComponentState;
-export type AgreementComponentState = z.infer<typeof AgreementComponentState>;
-
-const EServiceComponentState = ComponentState.extend({
-  descriptorId: DescriptorId,
-  audience: z.array(z.string()),
-  voucherLifespan: z.number(),
-});
-
-export type EServiceComponentState = z.infer<typeof EServiceComponentState>;
-
-const PurposeComponentState = ComponentState.extend({
-  versionId: PurposeVersionId,
-});
-
-export type PurposeComponentState = z.infer<typeof PurposeComponentState>;
 
 export const Base64Encoded = z.string().base64().min(1);
 
