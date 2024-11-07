1be2211811f157093db7a1d8af1fde794360ae4e
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-11-07 13:02:50 +0100

    Fix merge problems

--- packages/delegation-process/src/app.ts
+++ packages/delegation-process/src/app.ts
@@ -3,6 +3,7 @@ import {
   contextMiddleware,
   loggerMiddleware,
   zodiosCtx,
+  buildJwksClients,
 } from "pagopa-interop-commons";
 
 import healthRouter from "./routers/HealthRouter.js";
@@ -14,12 +15,14 @@ const serviceName = "delgation-process";
 
 const app = zodiosCtx.app();
 
+const jwksClients = buildJwksClients(config);
+
 // Disable the "X-Powered-By: Express" HTTP header for security reasons.
 // See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
 app.disable("x-powered-by");
 app.use(contextMiddleware(serviceName));
 app.use(healthRouter);
-app.use(authenticationMiddleware(config));
+app.use(authenticationMiddleware(config, jwksClients));
 app.use(loggerMiddleware(serviceName));
 app.use(delegationRouter(zodiosCtx));
 app.use(delegationProducerRouter(zodiosCtx));
