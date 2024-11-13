c713d64d955a291c10801546a7ff6a13d0fdec1a
Sandro Tajè <sandro.taje@gmail.com>
2024-11-13 09:49:20 +0100

    Fix bug wsdl

--- packages/backend-for-frontend/src/utilities/eserviceDocumentUtils.ts
+++ packages/backend-for-frontend/src/utilities/eserviceDocumentUtils.ts
@@ -9,7 +9,7 @@ import { FileManager, WithLogger } from "pagopa-interop-commons";
 import { ApiError, genericError } from "pagopa-interop-models";
 import { P, match } from "ts-pattern";
 import YAML from "yaml";
-import { z } from "zod";
+import { ZodError, z } from "zod";
 import { config } from "../config/config.js";
 import {
   ErrorCodes,
@@ -22,6 +22,27 @@ import { CatalogProcessClient } from "../clients/clientsProvider.js";
 import { BffAppContext } from "../utilities/context.js";
 import { ConfigurationDoc } from "../model/types.js";
 import { calculateChecksum } from "./fileUtils.js";
+
+const Wsdl = z.object({
+  definitions: z.object({
+    binding: z.array(
+      z.object({
+        operation: z.array(
+          z.object({
+            name: z.string(),
+          })
+        ),
+      })
+    ),
+    service: z.object({
+      port: z.array(
+        z.object({
+          address: z.object({ location: z.string() }),
+        })
+      ),
+    }),
+  }),
+});
 
 // eslint-disable-next-line max-params
 export async function verifyAndCreateEServiceDocument(
@@ -92,7 +113,7 @@ export async function verifyAndCreateEServiceDocument(
 const getFileType = (
   name: string
 ): "json" | "yaml" | "wsdl" | "xml" | undefined =>
-  match(name)
+  match(name.toLowerCase())
     .with(P.string.endsWith("json"), () => "json" as const)
     .with(
       P.string.endsWith("yaml"),
@@ -205,20 +226,31 @@ function processSoapInterface(file: string) {
     removeNSPrefix: true,
     ignoreAttributes: false,
     attributeNamePrefix: "",
-    isArray: (name: string) => ["operation"].indexOf(name) !== -1,
+    isArray: (name: string) =>
+      ["operation", "port", "binding"].indexOf(name) !== -1,
   }).parse(file);
 
-  const address = xml.definitions?.service?.port?.address?.location;
-  if (!address) {
+  const { data: parsedXml, success, error } = Wsdl.safeParse(xml);
+
+  if (!success) {
+    throw error;
+  }
+
+  const address = parsedXml.definitions.service.port.map(
+    (p) => p.address.location
+  );
+  if (address.length === 0) {
     throw interfaceExtractingInfoError();
   }
 
-  const endpoints = xml.definitions?.binding?.operation;
+  const endpoints = parsedXml.definitions.binding.flatMap((b) =>
+    b.operation.map((o) => o.name)
+  );
   if (endpoints.length === 0) {
     throw interfaceExtractingInfoError();
   }
 
-  return [address];
+  return address;
 }
 
 async function handleEServiceDocumentProcessing(
@@ -260,7 +292,11 @@ async function handleEServiceDocumentProcessing(
       });
   } catch (error) {
     throw match(error)
-      .with(P.instanceOf(ApiError<ErrorCodes>), () => error)
+      .with(
+        P.instanceOf(ApiError<ErrorCodes>),
+        P.instanceOf(ZodError),
+        () => error
+      )
       .otherwise(() => invalidInterfaceFileDetected(eServiceId));
   }
 }
