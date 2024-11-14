d6371ec6bead16f6404b73fe723faebcfbb7c3b6
Sandro Tajè <sandro.taje@gmail.com>
2024-11-14 12:31:04 +0100

    Add error in error mappert

--- packages/tenant-process/src/utilities/errorMappers.ts
+++ packages/tenant-process/src/utilities/errorMappers.ts
@@ -150,6 +150,7 @@ export const verifyVerifiedAttributeErrorMapper = (
     .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("attributeNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("agreementNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("eServiceNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("descriptorNotFoundInEservice", () => HTTP_STATUS_NOT_FOUND)
     .with(
       "verifiedAttributeSelfVerificationNotAllowed",
@@ -165,6 +166,7 @@ export const revokeVerifiedAttributeErrorMapper = (
     .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("attributeNotFound", () => HTTP_STATUS_BAD_REQUEST)
     .with("agreementNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("eServiceNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("descriptorNotFoundInEservice", () => HTTP_STATUS_NOT_FOUND)
     .with(
       "verifiedAttributeSelfRevocationNotAllowed",
