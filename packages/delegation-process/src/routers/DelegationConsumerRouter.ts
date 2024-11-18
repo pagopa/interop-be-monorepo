0fa8b0fc9a999b65af48fda85b56a971c925cac0
Eric Camellini <eric.camellini@gmail.com>
2024-11-18 15:54:56 +0100

    Adding automated tests

--- packages/delegation-process/src/routers/DelegationConsumerRouter.ts
+++ packages/delegation-process/src/routers/DelegationConsumerRouter.ts
@@ -8,19 +8,27 @@ import {
   userRoles,
   fromAppContext,
   DB,
+  PDFGenerator,
+  FileManager,
 } from "pagopa-interop-commons";
+import { unsafeBrandId } from "pagopa-interop-models";
 import { makeApiProblem } from "../model/domain/errors.js";
 import { delegationToApiDelegation } from "../model/domain/apiConverter.js";
 import { delegationConsumerServiceBuilder } from "../services/delegationConsumerService.js";
 import { ReadModelService } from "../services/readModelService.js";
-import { createConsumerDelegationErrorMapper } from "../utilities/errorMappers.js";
+import {
+  approveDelegationErrorMapper,
+  createConsumerDelegationErrorMapper,
+} from "../utilities/errorMappers.js";
 
 const { ADMIN_ROLE } = userRoles;
 
 const delegationConsumerRouter = (
   ctx: ZodiosContext,
   eventStore: DB,
-  readModelService: ReadModelService
+  readModelService: ReadModelService,
+  pdfGenerator: PDFGenerator,
+  fileManager: FileManager
 ): ZodiosRouter<typeof delegationApi.consumerApi.api, ExpressContext> => {
   const delegationConsumerRouter = ctx.router(delegationApi.consumerApi.api, {
     validationErrorHandler: zodiosValidationErrorToApiProblem,
@@ -28,40 +36,68 @@ const delegationConsumerRouter = (
 
   const delegationConsumerService = delegationConsumerServiceBuilder(
     eventStore,
-    readModelService
+    readModelService,
+    pdfGenerator,
+    fileManager
   );
 
-  delegationConsumerRouter.post(
-    "/consumer/delegations",
-    authorizationMiddleware([ADMIN_ROLE]),
-    async (req, res) => {
-      const ctx = fromAppContext(req.ctx);
+  delegationConsumerRouter
+    .post(
+      "/consumer/delegations",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (req, res) => {
+        const ctx = fromAppContext(req.ctx);
 
-      try {
-        const delegation =
-          await delegationConsumerService.createConsumerDelegation(
-            req.body,
+        try {
+          const delegation =
+            await delegationConsumerService.createConsumerDelegation(
+              req.body,
+              ctx
+            );
+          return res
+            .status(200)
+            .json(
+              delegationApi.Delegation.parse(
+                delegationToApiDelegation(delegation)
+              )
+            );
+        } catch (error) {
+          const errorRes = makeApiProblem(
+            error,
+            createConsumerDelegationErrorMapper,
+            ctx.logger,
+            ctx.correlationId
+          );
+
+          return res.status(errorRes.status).send(errorRes);
+        }
+      }
+    )
+    .post(
+      "/consumer/delegations/:delegationId/approve",
+      authorizationMiddleware([ADMIN_ROLE]),
+      async (req, res) => {
+        const ctx = fromAppContext(req.ctx);
+
+        try {
+          await delegationConsumerService.approveConsumerDelegation(
+            unsafeBrandId(req.params.delegationId),
             ctx
           );
-        return res
-          .status(200)
-          .json(
-            delegationApi.Delegation.parse(
-              delegationToApiDelegation(delegation)
-            )
-          );
-      } catch (error) {
-        const errorRes = makeApiProblem(
-          error,
-          createConsumerDelegationErrorMapper,
-          ctx.logger,
-          ctx.correlationId
-        );
 
-        return res.status(errorRes.status).send(errorRes);
+          return res.status(204).send();
+        } catch (error) {
+          const errorRes = makeApiProblem(
+            error,
+            approveDelegationErrorMapper,
+            ctx.logger,
+            ctx.correlationId
+          );
+
+          return res.status(errorRes.status).send(errorRes);
+        }
       }
-    }
-  );
+    );
 
   return delegationConsumerRouter;
 };
