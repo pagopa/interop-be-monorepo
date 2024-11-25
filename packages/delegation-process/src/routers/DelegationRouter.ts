d515405a1bd6f7b92148ac49841edc53bec58769
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-11-25 14:26:40 +0100

    Update packages/delegation-process/src/routers/DelegationRouter.ts
    Co-authored-by: AsterITA <fabrizio.musella@gmail.com>

--- packages/delegation-process/src/routers/DelegationRouter.ts
+++ packages/delegation-process/src/routers/DelegationRouter.ts
@@ -166,7 +166,7 @@ const delegationRouter = (
             getDelegationContractErrorMapper,
             ctx.logger,
             ctx.correlationId,
-            `Error retrieving delegation ${req.params.delegationId} contract ${req.params.contractId}`
+            `Error retrieving contract ${req.params.contractId} of delegation ${req.params.delegationId}`
           );
 
           return res.status(errorRes.status).send(errorRes);
