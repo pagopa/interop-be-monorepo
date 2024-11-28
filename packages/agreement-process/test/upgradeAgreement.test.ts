74bd137eacdaae86f891e9cc258e4c77ebaaf42f
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-11-28 14:48:04 +0100

    Update packages/agreement-process/test/upgradeAgreement.test.ts
    Co-authored-by: Vittorio Caprio <vittorio.caprio@buildo.io>

--- packages/agreement-process/test/upgradeAgreement.test.ts
+++ packages/agreement-process/test/upgradeAgreement.test.ts
@@ -554,7 +554,7 @@ describe("upgrade Agreement", () => {
     }
   });
 
-  it.only("should succeed with valid Verified, Certified, and Declared attributes when consumer and producer are different and requester is a delegate", async () => {
+  it("should succeed with valid Verified, Certified, and Declared attributes when consumer and producer are different and requester is a delegate", async () => {
     const producer = getMockTenant();
 
     const validVerifiedTenantAttribute = {
