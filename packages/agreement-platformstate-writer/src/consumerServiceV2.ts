4f6fe2bd560bfa6d73eafe3f1dd2685f44aa4152
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-11-28 15:54:56 +0100

    Reduce delta

--- packages/agreement-platformstate-writer/src/consumerServiceV2.ts
+++ packages/agreement-platformstate-writer/src/consumerServiceV2.ts
@@ -88,7 +88,6 @@ export async function handleMessageV2(
           eserviceId: agreement.eserviceId,
           descriptorId: agreement.descriptorId,
         });
-
         const catalogEntry = await readCatalogEntry(
           pkCatalogEntry,
           dynamoDBClient
