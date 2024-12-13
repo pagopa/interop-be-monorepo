9d6089394b27fd9a4e00cf551303a36ad11b7a23
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2024-12-13 14:19:41 +0100

    Remove description

--- packages/token-generation-readmodel-checker/src/index.ts
+++ packages/token-generation-readmodel-checker/src/index.ts
@@ -1,8 +1,3 @@
-/**
- * This script is used to compare the data of the token generation readmodel with readmodel.
- * The comparison is done by comparing the data from the read models with a deep comparison, and if any differences are found,
- * the script will log the differences and exit with a non-zero exit code.
- */
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { logger } from "pagopa-interop-commons";
 import { CorrelationId, generateId } from "pagopa-interop-models";
