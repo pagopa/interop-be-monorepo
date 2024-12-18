c26f6eef5571d78ac1726110c38f56acd4c6d864
Eric Camellini <eric.camellini@gmail.com>
2024-12-18 17:07:55 +0100

    PIN-5609 - Agreement activation contract fixes for Incaricato (#1303)

--- packages/agreement-lifecycle/src/utils/verifiedAttributes.ts
+++ packages/agreement-lifecycle/src/utils/verifiedAttributes.ts
@@ -1,4 +1,5 @@
 import {
+  DelegationId,
   TenantId,
   TenantVerifier,
   VerifiedTenantAttribute,
@@ -10,19 +11,22 @@ export const isVerificationRevoked = (
 ): boolean =>
   attribute.revokedBy.some((revocation) => revocation.id === verifierId);
 
+export function getVerifiedAttributeActiveProducerVerification(
+  verifierId: TenantId,
+  attribute: VerifiedTenantAttribute
+): TenantVerifier | undefined {
+  return attribute.verifiedBy
+    .filter((verification) => verification.id === verifierId)
+    .sort((a, b) => a.verificationDate.getTime() - b.verificationDate.getTime())
+    .find((verification) => !isVerificationRevoked(verification.id, attribute));
+}
+
 export function getVerifiedAttributeExpirationDate(
   verifierId: TenantId,
   attribute: VerifiedTenantAttribute
 ): Date | undefined {
-  const activeProducerVerification: TenantVerifier | undefined =
-    attribute.verifiedBy
-      .filter((verification) => verification.id === verifierId)
-      .sort(
-        (a, b) => a.verificationDate.getTime() - b.verificationDate.getTime()
-      )
-      .find(
-        (verification) => !isVerificationRevoked(verification.id, attribute)
-      );
+  const activeProducerVerification =
+    getVerifiedAttributeActiveProducerVerification(verifierId, attribute);
 
   // We assume that if the extensionDate is defined, it is always
   // after the expirationDate, so we return the extensionDate if it's defined
@@ -30,4 +34,13 @@ export function getVerifiedAttributeExpirationDate(
     activeProducerVerification?.extensionDate ??
     activeProducerVerification?.expirationDate
   );
+}
+
+export function getVerifiedAttributeDelegationId(
+  verifierId: TenantId,
+  attribute: VerifiedTenantAttribute
+): DelegationId | undefined {
+  const activeProducerVerification =
+    getVerifiedAttributeActiveProducerVerification(verifierId, attribute);
+  return activeProducerVerification?.delegationId;
 }
