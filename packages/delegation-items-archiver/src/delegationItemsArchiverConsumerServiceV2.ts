3bca01c538ddc1c7f1e65cac94984e58b06365a7
AsterITA <fabrizio.musella@gmail.com>
2025-01-23 12:32:26 +0100

    PIN-5961: added delegation-items-archiver scaffold (#1374)

---
+++ packages/delegation-items-archiver/src/delegationItemsArchiverConsumerServiceV2.ts
@@ -0,0 +1,78 @@
+import {
+  CorrelationId,
+  DelegationEventEnvelopeV2,
+  missingKafkaMessageDataError,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import {
+  getInteropHeaders,
+  Logger,
+  RefreshableInteropToken,
+} from "pagopa-interop-commons";
+import { PagoPAInteropBeClients } from "./clients/clientsProvider.js";
+
+export async function handleMessageV2(
+  {
+    decodedMessage,
+    refreshableToken,
+    partition,
+    offset,
+    correlationId,
+    logger,
+  }: {
+    decodedMessage: DelegationEventEnvelopeV2;
+    refreshableToken: RefreshableInteropToken;
+    partition: number;
+    offset: string;
+    correlationId: CorrelationId;
+    logger: Logger;
+  },
+  { agreementProcessClient, purposeProcessClient }: PagoPAInteropBeClients
+): Promise<void> {
+  await match(decodedMessage)
+    .with({ type: "ConsumerDelegationRevoked" }, async (delegationMsg) => {
+      logger.info(
+        `Processing ${delegationMsg.type} message - Partition number: ${partition} - Offset: ${offset}`
+      );
+
+      if (!delegationMsg.data.delegation) {
+        throw missingKafkaMessageDataError("delegation", delegationMsg.type);
+      }
+
+      const token = (await refreshableToken.get()).serialized;
+
+      // TODO: implement archiving logic
+
+      await purposeProcessClient.archivePurposeVersion(undefined, {
+        params: {
+          purposeId: "delegationMsg.data.delegation.purposeId",
+          versionId: "delegationMsg.data.delegation.versionId",
+        },
+        headers: getInteropHeaders({
+          token,
+          correlationId,
+        }),
+      });
+
+      await agreementProcessClient.archiveAgreement(undefined, {
+        params: {
+          agreementId: "delegationMsg.data.delegation.agreementId",
+        },
+        headers: getInteropHeaders({
+          token,
+          correlationId,
+        }),
+      });
+    })
+    .with(
+      { type: "ConsumerDelegationApproved" },
+      { type: "ProducerDelegationSubmitted" },
+      { type: "ProducerDelegationApproved" },
+      { type: "ProducerDelegationRejected" },
+      { type: "ProducerDelegationRevoked" },
+      { type: "ConsumerDelegationSubmitted" },
+      { type: "ConsumerDelegationRejected" },
+      () => Promise.resolve
+    )
+    .exhaustive();
+}
