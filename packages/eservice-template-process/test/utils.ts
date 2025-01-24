e19e6dd8a9f661e9c0856bcda32f3f71003a2852
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-01-24 14:54:32 +0100

    Added tests

--- packages/eservice-template-process/test/utils.ts
+++ packages/eservice-template-process/test/utils.ts
@@ -1,5 +1,18 @@
-import { setupTestContainersVitest } from "pagopa-interop-commons-test/index.js";
+import {
+  ReadEvent,
+  readLastEventByStreamId,
+  setupTestContainersVitest,
+  StoredEvent,
+  writeInEventstore,
+  writeInReadmodel,
+} from "pagopa-interop-commons-test";
 import { inject, afterEach } from "vitest";
+import {
+  EServiceTemplate,
+  EServiceTemplateEvent,
+  EServiceTemplateId,
+  toEServiceTemplateV2,
+} from "pagopa-interop-models";
 import { readModelServiceBuilder } from "../src/services/readModelService.js";
 import { eserviceTemplateServiceBuilder } from "../src/services/eserviceTemplateService.js";
 
@@ -21,3 +34,37 @@ export const eserviceTemplateService = eserviceTemplateServiceBuilder(
   readModelService,
   fileManager
 );
+
+export const writeEServiceInEventstore = async (
+  eserviceTemplate: EServiceTemplate
+): Promise<void> => {
+  const eserviceTemplateEvent: EServiceTemplateEvent = {
+    type: "EServiceTemplateAdded",
+    event_version: 2,
+    data: { eserviceTemplate: toEServiceTemplateV2(eserviceTemplate) },
+  };
+  const eventToWrite: StoredEvent<EServiceTemplateEvent> = {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    stream_id: eserviceTemplateEvent.data.eserviceTemplate!.id,
+    version: 0,
+    event: eserviceTemplateEvent,
+  };
+
+  await writeInEventstore(eventToWrite, "eservice_template", postgresDB);
+};
+
+export const addOneEServiceTemplate = async (
+  eserviceTemplate: EServiceTemplate
+): Promise<void> => {
+  await writeEServiceInEventstore(eserviceTemplate);
+  await writeInReadmodel(eserviceTemplate, eserviceTemplates);
+};
+
+export const readLastEserviceTemplateEvent = async (
+  eserviceTemplateId: EServiceTemplateId
+): Promise<ReadEvent<EServiceTemplateEvent>> =>
+  await readLastEventByStreamId(
+    eserviceTemplateId,
+    "eservice_template",
+    postgresDB
+  );
