2c405581d7f223b77476c18270987aa2df1a7530
AsterITA <fabrizio.musella@gmail.com>
2025-01-27 18:35:30 +0100

    PIN-5936: added better errors in agreement-process (#1419)

--- packages/purpose-process/src/services/purposeService.ts
+++ packages/purpose-process/src/services/purposeService.ts
@@ -1555,10 +1555,7 @@ async function generateRiskAnalysisDocument({
       retrieveTenant(eservice.producerId, readModelService),
       retrieveTenant(purpose.consumerId, readModelService),
       readModelService.getActiveProducerDelegationByEserviceId(eservice.id),
-      purpose.delegationId &&
-        readModelService.getActiveConsumerDelegationByDelegationId(
-          purpose.delegationId
-        ),
+      retrievePurposeDelegation(purpose, readModelService),
     ]);
 
   const [producerDelegate, consumerDelegate] = await Promise.all([
