d49c8db7b06aaf40341069f334834b6be46864bb
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-01-28 10:02:55 +0100

    Improve comments for catalog utils v1

--- packages/poc-readmodel/src/catalog/utilsV1.ts
+++ packages/poc-readmodel/src/catalog/utilsV1.ts
@@ -1,24 +1,18 @@
 // "EServiceAdded",
 // "ClonedEServiceAdded"
-// insert in all the tables
+// insert in all the tables (add delete at the beginning, for reprocessing)
 
 // "EServiceUpdated"
-// delete cascade from all the tables and replace
-
 // "EServiceRiskAnalysisAdded"
-// TODO
-
 // "MovedAttributesFromEserviceToDescriptors"
-// insert only in attributes table
-
 // "EServiceRiskAnalysisUpdated"
-// delete cascade from risk analysis and replace
+// delete cascade from all the tables and replace
 
 // "EServiceWithDescriptorsDeleted"
 // delete cascade from descriptor
 
 // "EServiceDocumentUpdated"
-// update only in document table? (beware of interface and docs)
+// update only in document table? (Delete and replace. Beware of interface and docs. If this events is only used for documents and never for interfaces, the insert could be simplified, otherwise we have to retrieve the kind somehow)
 // IMPORTANT: interface also brings new serverUrls value in descriptor table
 
 // "EServiceDeleted"
@@ -28,7 +22,7 @@
 // same considerations of EServiceDocumentUpdated
 
 // "EServiceDocumentDeleted"
-// delete in document table, plus what about serverUrls?
+// delete in document table, plus what about serverUrls? Maybe this is never used for interfaces
 
 // "EServiceDescriptorAdded"
 // create in all the tables from descriptor
@@ -37,4 +31,4 @@
 // replace cascade, from descriptor
 
 // "EServiceRiskAnalysisDeleted"
-// TODO
+// delete cascade from riskAnalysis
