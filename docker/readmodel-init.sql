89d4cd996d6496d94c3353b92f16df9684ca6f35
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-02-04 10:12:23 +0100

    Draft: purpose schema

--- docker/readmodel-init.sql
+++ docker/readmodel-init.sql
@@ -319,3 +319,51 @@ CREATE TABLE IF NOT EXISTS readmodel.tenant_feature_delegated_consumer(
   tenant_version INTEGER,
   availability_timestamp TIMESTAMP WITH TIME ZONE
 );
+
+
+-- PURPOSE
+
+CREATE TABLE IF NOT EXISTS readmodel.purpose
+(
+  id UUID,
+  version INTEGER,
+  eservice_id UUID NOT NULL REFERENCES readmodel.eservice (id),
+  consumer_id UUID NOT NULL REFERENCES readmodel.tenant (id),
+  delegation_id -- TODO reference delegation table
+  -- versions
+  suspended_by_consumer TIMESTAMP WITH TIME ZONE,
+  suspended_by_producer TIMESTAMP WITH TIME ZONE,
+  title VARCHAR NOT NULL,
+  description VARCHAR NOT NULL,
+  -- riskAnalysisForm
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  is_free_of_charge BOOLEAN NOT NULL,
+  free_of_charge_reason BOOLEAN,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS domains.purpose_version
+(
+  id UUID,
+  purpose_id UUID NOT NULL REFERENCES domains.purpose (id),
+  purpose_version INTEGER, -- metadata
+  state VARCHAR NOT NULL,
+  -- riskAnalysis
+  risk_analysis_id CHAR,
+  risk_analysis_content_type VARCHAR,
+  risk_analysis_path VARCHAR,
+  risk_analysis_created_at TIMESTAMP,
+
+  daily_calls INTEGER NOT NULL,
+  rejection_reason VARCHAR,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  first_activation_at TIMESTAMP WITH TIME ZONE,
+  suspended_at TIMESTAMP WITH TIME ZONE,
+
+  PRIMARY KEY (id)
+);
+
+
+-- TODO risk analysis
