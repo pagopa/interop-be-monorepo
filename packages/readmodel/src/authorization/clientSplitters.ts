7bb47c76d80fa19697ec0be08ed2a71e6402ba95
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-05 16:14:15 +0100

    Add client SQL splitters

---
+++ packages/readmodel/src/authorization/clientSplitters.ts
@@ -0,0 +1,71 @@
+import {
+  Client,
+  ClientKeySQL,
+  ClientPurposeSQL,
+  ClientSQL,
+  ClientUserSQL,
+} from "pagopa-interop-models";
+
+export const splitClientIntoObjectsSQL = (
+  {
+    id,
+    consumerId,
+    name,
+    purposes,
+    description,
+    users,
+    kind,
+    createdAt,
+    keys,
+    ...rest
+  }: Client,
+  version: number
+): {
+  clientSQL: ClientSQL;
+  clientUsersSQL: ClientUserSQL[];
+  clientPurposesSQL: ClientPurposeSQL[];
+  clientKeysSQL: ClientKeySQL[];
+} => {
+  void (rest satisfies Record<string, never>);
+
+  const clientSQL: ClientSQL = {
+    id,
+    version,
+    consumerId,
+    name,
+    description,
+    kind,
+    createdAt,
+  };
+
+  const clientUsersSQL: ClientUserSQL[] = users.map((userId) => ({
+    client_version: version,
+    client_id: id,
+    user_id: userId,
+  }));
+
+  const clientPurposesSQL: ClientPurposeSQL[] = purposes.map((purposeId) => ({
+    client_version: version,
+    client_id: id,
+    purpose_id: purposeId,
+  }));
+
+  const clientKeysSQL: ClientKeySQL[] = keys.map((key) => ({
+    client_version: version,
+    client_id: id,
+    user_id: key.userId,
+    kid: key.kid,
+    name: key.name,
+    encoded_pem: key.encodedPem,
+    algorithm: key.algorithm,
+    use: key.use,
+    created_at: key.createdAt,
+  }));
+
+  return {
+    clientSQL,
+    clientUsersSQL,
+    clientPurposesSQL,
+    clientKeysSQL,
+  };
+};
