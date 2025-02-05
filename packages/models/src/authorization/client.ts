4804c9ecea167c317eda4960d951e09b679678e7
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-05 15:56:09 +0100

    Add sql models for client

--- packages/models/src/authorization/client.ts
+++ packages/models/src/authorization/client.ts
@@ -25,3 +25,45 @@ export const Client = z.object({
 });
 
 export type Client = z.infer<typeof Client>;
+
+export const ClientSQL = z.object({
+  id: ClientId,
+  version: z.number(),
+  consumerId: TenantId,
+  name: z.string(),
+  description: z.string().optional(),
+  kind: ClientKind,
+  createdAt: z.coerce.date(),
+});
+
+export type ClientSQL = z.infer<typeof ClientSQL>;
+
+export const ClientUserSQL = z.object({
+  client_version: z.number(),
+  client_id: ClientId,
+  user_id: UserId,
+});
+
+export type ClientUserSQL = z.infer<typeof ClientUserSQL>;
+
+export const ClientPurposeSQL = z.object({
+  client_version: z.number(),
+  client_id: ClientId,
+  purpose_id: PurposeId,
+});
+
+export type ClientPurposeSQL = z.infer<typeof ClientPurposeSQL>;
+
+export const ClientKeySQL = z.object({
+  client_version: z.number(),
+  client_id: ClientId,
+  user_id: UserId,
+  kid: z.string(),
+  name: z.string(),
+  encoded_pem: z.string(),
+  algorithm: z.string(),
+  use: z.string(),
+  created_at: z.coerce.date(),
+});
+
+export type ClientKeySQL = z.infer<typeof ClientKeySQL>;
