697d611b80f945d35ce5c1f42e21773ef7ffc206
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-05 16:32:54 +0100

    Add client SQL aggregators

---
+++ packages/readmodel/src/authorization/clientAggregators.ts
@@ -0,0 +1,56 @@
+import {
+  Client,
+  ClientKeySQL,
+  ClientPurposeSQL,
+  ClientSQL,
+  ClientUserSQL,
+  Key,
+  PurposeId,
+  UserId,
+  WithMetadata,
+} from "pagopa-interop-models";
+
+export const clientSQLToClient = (
+  {
+    id,
+    version,
+    consumer_id,
+    name,
+    description,
+    kind,
+    created_at,
+    ...rest
+  }: ClientSQL,
+  clientUsersSQL: ClientUserSQL[],
+  clientPurposesSQL: ClientPurposeSQL[],
+  clientKeysSQL: ClientKeySQL[]
+): WithMetadata<Client> => {
+  void (rest satisfies Record<string, never>);
+
+  const users: UserId[] = clientUsersSQL.map((u) => u.user_id);
+  const purposes: PurposeId[] = clientPurposesSQL.map((p) => p.purpose_id);
+  const keys: Key[] = clientKeysSQL.map((k) => ({
+    userId: k.user_id,
+    kid: k.kid,
+    name: k.name,
+    encodedPem: k.encoded_pem,
+    algorithm: k.algorithm,
+    use: k.use,
+    createdAt: k.created_at,
+  }));
+
+  return {
+    data: {
+      id,
+      consumerId: consumer_id,
+      name,
+      purposes,
+      description,
+      users,
+      kind,
+      createdAt: created_at,
+      keys,
+    },
+    metadata: { version },
+  };
+};
