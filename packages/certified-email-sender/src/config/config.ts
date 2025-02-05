ceaad4204224154905392ff86af35d20bb46a8ec
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2025-02-05 15:41:27 +0100

    refactor: remove unused mail configs

--- packages/certified-email-sender/src/config/config.ts
+++ packages/certified-email-sender/src/config/config.ts
@@ -12,15 +12,11 @@ export const EmailSenderConfig = z
     INTEROP_FE_BASE_URL: z.string(),
     PEC_SENDER_MAIL: z.string().email(),
     PEC_SENDER_LABEL: z.string(),
-    SENDER_MAIL: z.string().email(),
-    SENDER_LABEL: z.string(),
   })
   .transform((c) => ({
     interopFeBaseUrl: c.INTEROP_FE_BASE_URL,
     pecSenderMail: c.PEC_SENDER_MAIL,
     pecSenderLabel: c.PEC_SENDER_LABEL,
-    senderMail: c.SENDER_MAIL,
-    senderLabel: c.SENDER_LABEL,
   }));
 export type EmailSenderConfig = z.infer<typeof EmailSenderConfig>;
 
