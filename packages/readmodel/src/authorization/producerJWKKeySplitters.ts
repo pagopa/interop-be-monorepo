a4ebab0de8a50a8756cb715231bf0ebe4cefbb47
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-05 15:43:48 +0100

    Add ProducerJWKKeySQL splitters

---
+++ packages/readmodel/src/authorization/producerJWKKeySplitters.ts
@@ -0,0 +1,18 @@
+import { ProducerJWKKey, ProducerJWKKeySQL } from "pagopa-interop-models";
+
+export const splitProducerJWKKeyIntoObjectsSQL = (
+  { producerKeychainId, alg, e, kid, kty, n, use, ...rest }: ProducerJWKKey,
+  version: number
+): ProducerJWKKeySQL => {
+  void (rest satisfies Record<string, never>);
+  return {
+    producer_keychain_id: producerKeychainId,
+    version,
+    alg,
+    e,
+    kid,
+    kty,
+    n,
+    use,
+  };
+};
