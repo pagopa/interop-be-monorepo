6e92a5dcd53777d4f1ffd999f3acf8fd9462109e
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2025-02-06 17:35:35 +0100

    PIN-6111 Certified email sender consumer (#1465)

--- packages/certified-email-sender/src/config/config.ts
+++ packages/certified-email-sender/src/config/config.ts
@@ -3,7 +3,6 @@ import {
   PecEmailManagerConfig,
   KafkaConsumerConfig,
   ReadModelDbConfig,
-  AWSSesConfig,
 } from "pagopa-interop-commons";
 import { z } from "zod";
 
@@ -12,29 +11,24 @@ export const EmailSenderConfig = z
     INTEROP_FE_BASE_URL: z.string(),
     PEC_SENDER_MAIL: z.string().email(),
     PEC_SENDER_LABEL: z.string(),
-    SENDER_MAIL: z.string().email(),
-    SENDER_LABEL: z.string(),
   })
   .transform((c) => ({
     interopFeBaseUrl: c.INTEROP_FE_BASE_URL,
     pecSenderMail: c.PEC_SENDER_MAIL,
     pecSenderLabel: c.PEC_SENDER_LABEL,
-    senderMail: c.SENDER_MAIL,
-    senderLabel: c.SENDER_LABEL,
   }));
 export type EmailSenderConfig = z.infer<typeof EmailSenderConfig>;
 
-export const AgreementEmailSenderConfig = KafkaConsumerConfig.and(
+export const CertifiedEmailSenderConfig = KafkaConsumerConfig.and(
   ReadModelDbConfig
 )
   .and(AgreementTopicConfig)
   .and(PecEmailManagerConfig)
-  .and(AWSSesConfig)
   .and(EmailSenderConfig);
 
-export type AgreementEmailSenderConfig = z.infer<
-  typeof AgreementEmailSenderConfig
+export type CertifiedEmailSenderConfig = z.infer<
+  typeof CertifiedEmailSenderConfig
 >;
 
-export const config: AgreementEmailSenderConfig =
-  AgreementEmailSenderConfig.parse(process.env);
+export const config: CertifiedEmailSenderConfig =
+  CertifiedEmailSenderConfig.parse(process.env);
--- packages/certified-email-sender/test/utils.ts
+++ packages/certified-email-sender/test/utils.ts
@@ -1,8 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import {
-  buildHTMLTemplateService,
-  EmailManagerSES,
-} from "pagopa-interop-commons";
+import { buildHTMLTemplateService } from "pagopa-interop-commons";
 import {
   setupTestContainersVitest,
   writeInReadmodel,
@@ -15,42 +12,24 @@ import {
   toReadModelEService,
   toReadModelTenant,
 } from "pagopa-interop-models";
-import { afterEach, inject, vi } from "vitest";
-import { agreementEmailSenderServiceBuilder } from "../src/services/agreementEmailSenderService.js";
+import { afterEach, inject } from "vitest";
 import { readModelServiceBuilder } from "../src/services/readModelService.js";
+import { certifiedEmailSenderServiceBuilder } from "../src/services/certifiedEmailSenderService.js";
 
 export const readModelConfig = inject("readModelConfig");
 export const emailManagerConfig = inject("emailManagerConfig");
-export const sesEmailManagerConfig = inject("sesEmailManagerConfig");
 
-export const {
-  cleanup,
-  readModelRepository,
-  pecEmailManager,
-  sesEmailManager,
-} = await setupTestContainersVitest(
-  readModelConfig,
-  undefined,
-  undefined,
-  emailManagerConfig,
-  undefined,
-  sesEmailManagerConfig
-);
+export const { cleanup, readModelRepository, pecEmailManager } =
+  await setupTestContainersVitest(
+    readModelConfig,
+    undefined,
+    undefined,
+    emailManagerConfig,
+    undefined,
+    undefined
+  );
 export const readModelService = readModelServiceBuilder(readModelRepository);
 export const templateService = buildHTMLTemplateService();
-
-export const sesEmailManagerFailure: EmailManagerSES = {
-  kind: "SES",
-  send: vi.fn().mockRejectedValue(new Error("Generic error during send email")),
-  sendWithAttachments: vi
-    .fn()
-    .mockRejectedValue(new Error("Generic error during send email")),
-};
-
-export const sesEmailsenderData = {
-  label: "ses_sender",
-  mail: "ses_sender@test.com",
-};
 
 export const pecEmailsenderData = {
   label: "pec_sender",
@@ -58,25 +37,19 @@ export const pecEmailsenderData = {
 };
 export const interopFeBaseUrl = "http://localhost/fe";
 
-export const agreementEmailSenderService = agreementEmailSenderServiceBuilder(
+export const certifiedEmailSenderService = certifiedEmailSenderServiceBuilder(
   pecEmailManager,
   pecEmailsenderData,
-  sesEmailManager,
-  sesEmailsenderData,
   readModelService,
-  templateService,
-  interopFeBaseUrl
+  templateService
 );
 
-export const agreementEmailSenderServiceFailure =
-  agreementEmailSenderServiceBuilder(
+export const certifiedEmailSenderServiceFailure =
+  certifiedEmailSenderServiceBuilder(
     pecEmailManager,
     pecEmailsenderData,
-    sesEmailManagerFailure,
-    sesEmailsenderData,
     readModelService,
-    templateService,
-    interopFeBaseUrl
+    templateService
   );
 
 export const agreements = readModelRepository.agreements;
