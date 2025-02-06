e56311b291b2672abc3b1268c19f4b53447f1b62
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-02-06 11:16:49 +0100

    Revert "WIP templatesIds filter"
    This reverts commit 5122c8068b46b79b043cb9dfa5b59db8c34fc451.

--- packages/catalog-process/src/model/domain/models.ts
+++ packages/catalog-process/src/model/domain/models.ts
@@ -11,7 +11,6 @@ import {
   EServiceMode,
   AttributeId,
   TenantId,
-  EServiceTemplateId,
 } from "pagopa-interop-models";
 
 export type ApiGetEServicesFilters = {
@@ -24,7 +23,6 @@ export type ApiGetEServicesFilters = {
   mode?: EServiceMode;
   isConsumerDelegable?: boolean;
   delegated?: boolean;
-  templatesIds: EServiceTemplateId[];
 };
 
 export type EServiceDocument = {
