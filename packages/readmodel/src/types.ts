b69caff91178d130c1171f4a648bbf44de40bf77
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-02-12 15:59:45 +0100

    Apply tenant changes

--- packages/readmodel/src/types.ts
+++ packages/readmodel/src/types.ts
@@ -13,6 +13,14 @@ import {
   agreementDocumentInReadmodel,
   agreementInReadmodel,
   agreementStampInReadmodel,
+  tenantCertifiedAttributeInReadmodel,
+  tenantDeclaredAttributeInReadmodel,
+  tenantFeatureInReadmodel,
+  tenantInReadmodel,
+  tenantMailInReadmodel,
+  tenantVerifiedAttributeInReadmodel,
+  tenantVerifiedAttributeRevokerInReadmodel,
+  tenantVerifiedAttributeVerifierInReadmodel,
 } from "./drizzle/schema.js";
 
 export type EServiceSQL = InferSelectModel<typeof eserviceInReadmodel>;
@@ -50,3 +58,24 @@ export type AgreementStampSQL = InferSelectModel<
   typeof agreementStampInReadmodel
 >;
 export type AgreementSQL = InferSelectModel<typeof agreementInReadmodel>;
+
+export type TenantSQL = InferSelectModel<typeof tenantInReadmodel>;
+export type TenantMailSQL = InferSelectModel<typeof tenantMailInReadmodel>;
+export type TenantCertifiedAttributeSQL = InferSelectModel<
+  typeof tenantCertifiedAttributeInReadmodel
+>;
+export type TenantDeclaredAttributeSQL = InferSelectModel<
+  typeof tenantDeclaredAttributeInReadmodel
+>;
+export type TenantVerifiedAttributeSQL = InferSelectModel<
+  typeof tenantVerifiedAttributeInReadmodel
+>;
+export type TenantVerifiedAttributeVerifierSQL = InferSelectModel<
+  typeof tenantVerifiedAttributeVerifierInReadmodel
+>;
+export type TenantVerifiedAttributeRevokerSQL = InferSelectModel<
+  typeof tenantVerifiedAttributeRevokerInReadmodel
+>;
+export type TenantFeatureSQL = InferSelectModel<
+  typeof tenantFeatureInReadmodel
+>;
