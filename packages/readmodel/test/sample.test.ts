bece2fb6a45b6caa5aef62f03c8af803aa2fca89
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-14 14:01:46 +0100

    WIP: add eservice SQL splitter test

--- packages/readmodel/test/sample.test.ts
+++ packages/readmodel/test/sample.test.ts
@@ -1,10 +1,194 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { describe, it, expect } from "vitest";
+import {
+  getMockDescriptor,
+  getMockDescriptorRejectionReason,
+  getMockDocument,
+  getMockEService,
+  getMockEServiceAttribute,
+  getMockValidRiskAnalysis,
+} from "pagopa-interop-commons-test";
+import {
+  agreementApprovalPolicy,
+  attributeKind,
+  Descriptor,
+  documentKind,
+  EService,
+  tenantKind,
+} from "pagopa-interop-models";
+import { describe, it, expect, afterEach, beforeEach, vi } from "vitest";
+import { splitEserviceIntoObjectsSQL } from "../src/catalog/splitters.js";
+import {
+  EServiceDescriptorAttributeSQL,
+  EServiceDescriptorDocumentSQL,
+  EServiceDescriptorRejectionReasonSQL,
+  EServiceDescriptorSQL,
+  EServiceRiskAnalysisSQL,
+  EServiceSQL,
+} from "../src/types.js";
 
 describe("", () => {
-  describe("", () => {
-    it("", () => {
-      expect(1).toBe(1);
-    });
+  beforeEach(async () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date());
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("", () => {
+    const certifiedAttribute = getMockEServiceAttribute();
+    const doc = getMockDocument();
+    const interfaceDoc = getMockDocument();
+    const rejectionReason = getMockDescriptorRejectionReason();
+    const riskAnalysis1 = getMockValidRiskAnalysis(tenantKind.PA);
+    const riskAnalysis2 = getMockValidRiskAnalysis(tenantKind.PRIVATE);
+
+    const descriptor: Descriptor = {
+      ...getMockDescriptor(),
+      attributes: {
+        certified: [[certifiedAttribute]],
+        declared: [],
+        verified: [],
+      },
+      docs: [doc],
+      interface: interfaceDoc,
+      rejectionReasons: [rejectionReason],
+      description: "description test",
+      publishedAt: new Date(),
+      suspendedAt: new Date(),
+      deprecatedAt: new Date(),
+      archivedAt: new Date(),
+      agreementApprovalPolicy: agreementApprovalPolicy.automatic,
+    };
+
+    const eservice: EService = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+      riskAnalysis: [riskAnalysis1, riskAnalysis2],
+      isSignalHubEnabled: true,
+      isClientAccessDelegable: true,
+      isConsumerDelegable: true,
+    };
+
+    const {
+      eserviceSQL,
+      riskAnalysisSQL,
+      riskAnalysisAnswersSQL,
+      descriptorsSQL,
+      attributesSQL,
+      documentsSQL,
+      rejectionReasonsSQL,
+      // TODO: add eserviceTemplateBinding
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      eserviceTemplateBindingSQL,
+    } = splitEserviceIntoObjectsSQL(eservice, 1);
+
+    const expectedEServiceSQL: EServiceSQL = {
+      id: eservice.id,
+      metadataVersion: 1,
+      name: eservice.name,
+      createdAt: eservice.createdAt.toISOString(),
+      producerId: eservice.producerId,
+      description: eservice.description,
+      technology: eservice.technology,
+      mode: eservice.mode,
+      isSignalHubEnabled: true,
+      isClientAccessDelegable: true,
+      isConsumerDelegable: true,
+    };
+
+    const expectedRiskAnalysisSQL1: EServiceRiskAnalysisSQL = {
+      id: riskAnalysis1.id,
+      metadataVersion: 1,
+      name: riskAnalysis1.name,
+      createdAt: riskAnalysis1.createdAt.toISOString(),
+      eserviceId: eservice.id,
+      riskAnalysisFormId: riskAnalysis1.riskAnalysisForm.id,
+      riskAnalysisFormVersion: riskAnalysis1.riskAnalysisForm.version,
+    };
+
+    const expectedRiskAnalysisSQL2: EServiceRiskAnalysisSQL = {
+      id: riskAnalysis2.id,
+      metadataVersion: 1,
+      name: riskAnalysis2.name,
+      createdAt: riskAnalysis2.createdAt.toISOString(),
+      eserviceId: eservice.id,
+      riskAnalysisFormId: riskAnalysis2.riskAnalysisForm.id,
+      riskAnalysisFormVersion: riskAnalysis2.riskAnalysisForm.version,
+    };
+
+    // TODO answers
+    // const expectedRiskAnalysisAnswersSQL: EServiceRiskAnalysisSQL = {}
+
+    const expectedDescriptorSQL: EServiceDescriptorSQL = {
+      metadataVersion: 1,
+      createdAt: descriptor.createdAt.toISOString(),
+      eserviceId: eservice.id,
+      description: "description test",
+      publishedAt: new Date().toISOString(),
+      suspendedAt: new Date().toISOString(),
+      deprecatedAt: new Date().toISOString(),
+      archivedAt: new Date().toISOString(),
+      agreementApprovalPolicy: agreementApprovalPolicy.automatic,
+      id: descriptor.id,
+      version: descriptor.version,
+      state: descriptor.state,
+      audience: descriptor.audience,
+      voucherLifespan: descriptor.voucherLifespan,
+      dailyCallsPerConsumer: descriptor.dailyCallsPerConsumer,
+      dailyCallsTotal: descriptor.dailyCallsTotal,
+      serverUrls: descriptor.serverUrls,
+    };
+
+    const expectedAttributeSQL: EServiceDescriptorAttributeSQL = {
+      metadataVersion: 1,
+      eserviceId: eservice.id,
+      kind: attributeKind.certified,
+      attributeId: certifiedAttribute.id,
+      descriptorId: descriptor.id,
+      groupId: 0,
+      explicitAttributeVerification:
+        certifiedAttribute.explicitAttributeVerification,
+    };
+
+    const expectedDocumentSQL: EServiceDescriptorDocumentSQL = {
+      ...doc,
+      metadataVersion: 1,
+      eserviceId: eservice.id,
+      kind: documentKind.descriptorDocument,
+      descriptorId: descriptor.id,
+      uploadDate: doc.uploadDate.toISOString(),
+    };
+
+    const expectedInterfaceDocSQL: EServiceDescriptorDocumentSQL = {
+      ...interfaceDoc,
+      metadataVersion: 1,
+      eserviceId: eservice.id,
+      kind: documentKind.descriptorInterface,
+      descriptorId: descriptor.id,
+      uploadDate: interfaceDoc.uploadDate.toISOString(),
+    };
+
+    const expectedRejectionReasonSQL: EServiceDescriptorRejectionReasonSQL = {
+      ...rejectionReason,
+      metadataVersion: 1,
+      eserviceId: eservice.id,
+      descriptorId: descriptor.id,
+      rejectedAt: rejectionReason.rejectedAt.toISOString(),
+    };
+
+    expect(eserviceSQL).toEqual(expectedEServiceSQL);
+    expect(riskAnalysisSQL).toEqual(
+      expect.arrayContaining([
+        expectedRiskAnalysisSQL1,
+        expectedRiskAnalysisSQL2,
+      ])
+    );
+    expect(descriptorsSQL).toEqual([expectedDescriptorSQL]);
+    expect(attributesSQL).toEqual([expectedAttributeSQL]);
+    expect(documentsSQL).toEqual(
+      expect.arrayContaining([expectedDocumentSQL, expectedInterfaceDocSQL])
+    );
+    expect(rejectionReasonsSQL).toEqual([expectedRejectionReasonSQL]);
   });
 });
