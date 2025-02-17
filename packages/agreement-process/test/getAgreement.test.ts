7869833aba4204865236868e2df57d423b755428
Eric Camellini <eric.camellini@gmail.com>
2025-02-17 11:47:48 +0100

    Fixing getAgreement test

--- packages/agreement-process/test/getAgreement.test.ts
+++ packages/agreement-process/test/getAgreement.test.ts
@@ -1,21 +1,131 @@
 import { genericLogger } from "pagopa-interop-commons";
-import { getMockAgreement } from "pagopa-interop-commons-test/index.js";
-import { Agreement, generateId, AgreementId } from "pagopa-interop-models";
+import {
+  addSomeRandomDelegations,
+  getMockAgreement,
+  getMockDelegation,
+  getMockDescriptorPublished,
+  getMockEService,
+  getMockTenant,
+  getRandomAuthData,
+} from "pagopa-interop-commons-test/index.js";
+import {
+  generateId,
+  delegationKind,
+  delegationState,
+  AgreementId,
+} from "pagopa-interop-models";
 import { describe, it, expect } from "vitest";
-import { agreementNotFound } from "../src/model/domain/errors.js";
-import { addOneAgreement, agreementService } from "./utils.js";
+import {
+  agreementNotFound,
+  organizationNotAllowed,
+} from "../src/model/domain/errors.js";
+import {
+  addOneAgreement,
+  addOneDelegation,
+  addOneEService,
+  addOneTenant,
+  agreementService,
+} from "./utils.js";
 
 describe("get agreement", () => {
-  it("should get an agreement", async () => {
-    const agreement: Agreement = getMockAgreement();
-    await addOneAgreement(agreement);
-    await addOneAgreement(getMockAgreement());
+  it(`should succeed when the requester is the consumer,
+    producer, consumer delegate, or producer delegate`, async () => {
+    const producer = getMockTenant();
+    const consumer = getMockTenant();
+    const producerDelegate = getMockTenant();
+    const consumerDelegate = getMockTenant();
+    const eservice = {
+      ...getMockEService(),
+      producerId: producer.id,
+      consumerId: consumer.id,
+      descriptors: [getMockDescriptorPublished()],
+    };
+    const agreement = {
+      ...getMockAgreement(eservice.id),
+      descriptorId: eservice.descriptors[0].id,
+      producerId: producer.id,
+      consumerId: consumer.id,
+    };
+    const producerDelegation = getMockDelegation({
+      kind: delegationKind.delegatedProducer,
+      eserviceId: eservice.id,
+      delegatorId: eservice.producerId,
+      state: delegationState.active,
+      delegateId: producerDelegate.id,
+    });
 
-    const result = await agreementService.getAgreementById(
+    const consumerDelegation = getMockDelegation({
+      kind: delegationKind.delegatedConsumer,
+      eserviceId: agreement.eserviceId,
+      delegatorId: agreement.consumerId,
+      state: delegationState.active,
+      delegateId: consumerDelegate.id,
+    });
+
+    await addOneTenant(producerDelegate);
+    await addOneTenant(consumerDelegate);
+    await addOneEService(eservice);
+    await addOneAgreement(agreement);
+    await addOneDelegation(producerDelegation);
+    await addOneDelegation(consumerDelegation);
+    await addSomeRandomDelegations(agreement, addOneDelegation);
+
+    const retrievedDocumentByConsumer = await agreementService.getAgreementById(
       agreement.id,
-      genericLogger
+      {
+        authData: getRandomAuthData(consumer.id),
+        serviceName: "",
+        correlationId: generateId(),
+        logger: genericLogger,
+      }
     );
-    expect(result).toEqual(agreement);
+    expect(retrievedDocumentByConsumer).toEqual(agreement);
+
+    const retrievedDocumentByProducer = await agreementService.getAgreementById(
+      agreement.id,
+      {
+        authData: getRandomAuthData(producer.id),
+        serviceName: "",
+        correlationId: generateId(),
+        logger: genericLogger,
+      }
+    );
+    expect(retrievedDocumentByProducer).toEqual(agreement);
+
+    const retrievedDocumentByProducerDelegate =
+      await agreementService.getAgreementById(agreement.id, {
+        authData: getRandomAuthData(producerDelegate.id),
+        serviceName: "",
+        correlationId: generateId(),
+        logger: genericLogger,
+      });
+    expect(retrievedDocumentByProducerDelegate).toEqual(agreement);
+
+    const retrievedDocumentByConsumerDelegate =
+      await agreementService.getAgreementById(agreement.id, {
+        authData: getRandomAuthData(consumerDelegate.id),
+        serviceName: "",
+        correlationId: generateId(),
+        logger: genericLogger,
+      });
+    expect(retrievedDocumentByConsumerDelegate).toEqual(agreement);
+  });
+
+  it(`should throw an organizationNotAllowed error when the requester is
+    not the consumer, producer, consumer delegate, or producer delegate`, async () => {
+    const agreement = getMockAgreement();
+
+    await addOneAgreement(agreement);
+
+    const authData = getRandomAuthData();
+    await expect(
+      agreementService.getAgreementById(agreement.id, {
+        authData,
+        serviceName: "",
+        correlationId: generateId(),
+        logger: genericLogger,
+      })
+    ).rejects.toThrowError(organizationNotAllowed(authData.organizationId));
   });
 
   it("should throw an agreementNotFound error when the agreement does not exist", async () => {
@@ -24,7 +134,12 @@ describe("get agreement", () => {
     await addOneAgreement(getMockAgreement());
 
     await expect(
-      agreementService.getAgreementById(agreementId, genericLogger)
+      agreementService.getAgreementById(agreementId, {
+        authData: getRandomAuthData(),
+        serviceName: "",
+        correlationId: generateId(),
+        logger: genericLogger,
+      })
     ).rejects.toThrowError(agreementNotFound(agreementId));
   });
 });
