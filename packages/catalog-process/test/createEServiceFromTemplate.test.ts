b9365a10ff3a3afee36f0cf3df772bb312c083be
Alepazz <alessio.pazzani@gmail.com>
2025-02-17 17:32:25 +0100

    Created unit test for "create EService from existing template"

---
+++ packages/catalog-process/test/createEServiceFromTemplate.test.ts
@@ -0,0 +1,275 @@
+import { afterAll, beforeAll, describe, vi, it, expect } from "vitest";
+import {
+  decodeProtobufPayload,
+  getMockEServiceTemplate,
+  getMockEServiceTemplateVersion,
+  readEventByStreamIdAndVersion,
+} from "pagopa-interop-commons-test";
+import { genericLogger } from "pagopa-interop-commons";
+import {
+  EService,
+  EServiceAddedV2,
+  EServiceDescriptorAddedV2,
+  EServiceTemplate,
+  EServiceTemplateVersion,
+  eserviceTemplateVersionState,
+  generateId,
+  toEServiceV2,
+} from "pagopa-interop-models";
+import {
+  catalogService,
+  getMockEService,
+  getMockAuthData,
+  postgresDB,
+  readLastEserviceEvent,
+  getMockDescriptor,
+  addOneEServiceTemplate,
+} from "./utils.js";
+
+describe("create eService from template", () => {
+  const mockEService = getMockEService();
+  const mockDescriptor = getMockDescriptor();
+  const mockEServiceTemplate = getMockEServiceTemplate();
+  beforeAll(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date());
+  });
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it("should write on event-store for the creation of an eService from a template", async () => {
+    const publishedVersion: EServiceTemplateVersion = {
+      ...getMockEServiceTemplateVersion(),
+      state: eserviceTemplateVersionState.published,
+    };
+    const eServiceTemplate: EServiceTemplate = {
+      ...mockEServiceTemplate,
+      versions: [publishedVersion],
+    };
+
+    await addOneEServiceTemplate(eServiceTemplate);
+
+    const eService = await catalogService.createEServiceInstanceFromTemplate(
+      eServiceTemplate.id,
+      { instanceId: undefined },
+      {
+        authData: getMockAuthData(mockEService.producerId),
+        correlationId: generateId(),
+        serviceName: "",
+        logger: genericLogger,
+      }
+    );
+
+    expect(eService).toBeDefined();
+
+    const eServiceCreationEvent = await readEventByStreamIdAndVersion(
+      eService.id,
+      0,
+      "catalog",
+      postgresDB
+    );
+    const descriptorCreationEvent = await readLastEserviceEvent(eService.id);
+
+    expect(eServiceCreationEvent).toMatchObject({
+      stream_id: eService.id,
+      version: "0",
+      type: "EServiceAdded",
+      event_version: 2,
+    });
+    expect(descriptorCreationEvent).toMatchObject({
+      stream_id: eService.id,
+      version: "1",
+      type: "EServiceDescriptorAdded",
+      event_version: 2,
+    });
+
+    const eServiceCreationPayload = decodeProtobufPayload({
+      messageType: EServiceAddedV2,
+      payload: eServiceCreationEvent.data,
+    });
+    const descriptorCreationPayload = decodeProtobufPayload({
+      messageType: EServiceDescriptorAddedV2,
+      payload: descriptorCreationEvent.data,
+    });
+
+    const expectedEService: EService = {
+      ...mockEService,
+      description: eServiceTemplate.eserviceDescription,
+      name: eServiceTemplate.name,
+      createdAt: eService.createdAt,
+      id: eService.id,
+      isSignalHubEnabled: eService.isSignalHubEnabled,
+      isConsumerDelegable: false,
+      isClientAccessDelegable: false,
+    };
+
+    const expectedEServiceWithDescriptor: EService = {
+      ...mockEService,
+      description: eServiceTemplate.eserviceDescription,
+      name: eServiceTemplate.name,
+      createdAt: new Date(),
+      id: eService.id,
+      isSignalHubEnabled: eService.isSignalHubEnabled,
+      isClientAccessDelegable: false,
+      isConsumerDelegable: false,
+      descriptors: [
+        {
+          ...mockDescriptor,
+          description: publishedVersion.description,
+          id: eService.descriptors[0].id,
+          createdAt: new Date(),
+          serverUrls: [],
+          audience: [eServiceTemplate.audienceDescription],
+          dailyCallsPerConsumer: publishedVersion?.dailyCallsPerConsumer ?? 1,
+          dailyCallsTotal: publishedVersion?.dailyCallsTotal ?? 1,
+        },
+      ],
+    };
+
+    expect(eServiceCreationPayload.eservice).toEqual(
+      toEServiceV2(expectedEService)
+    );
+    expect(descriptorCreationPayload.eservice).toEqual(
+      toEServiceV2(expectedEServiceWithDescriptor)
+    );
+  });
+  it("should write on event-store for the creation of an eService from a template with a custom instanceId", async () => {
+    const instanceId = generateId();
+
+    const publishedVersion: EServiceTemplateVersion = {
+      ...getMockEServiceTemplateVersion(),
+      state: eserviceTemplateVersionState.published,
+    };
+    const eServiceTemplate: EServiceTemplate = {
+      ...mockEServiceTemplate,
+      versions: [publishedVersion],
+    };
+
+    await addOneEServiceTemplate(eServiceTemplate);
+
+    const eService = await catalogService.createEServiceInstanceFromTemplate(
+      eServiceTemplate.id,
+      { instanceId },
+      {
+        authData: getMockAuthData(mockEService.producerId),
+        correlationId: generateId(),
+        serviceName: "",
+        logger: genericLogger,
+      }
+    );
+
+    expect(eService).toBeDefined();
+
+    const eServiceCreationEvent = await readEventByStreamIdAndVersion(
+      eService.id,
+      0,
+      "catalog",
+      postgresDB
+    );
+    const descriptorCreationEvent = await readLastEserviceEvent(eService.id);
+
+    expect(eServiceCreationEvent).toMatchObject({
+      stream_id: eService.id,
+      version: "0",
+      type: "EServiceAdded",
+      event_version: 2,
+    });
+    expect(descriptorCreationEvent).toMatchObject({
+      stream_id: eService.id,
+      version: "1",
+      type: "EServiceDescriptorAdded",
+      event_version: 2,
+    });
+
+    const eServiceCreationPayload = decodeProtobufPayload({
+      messageType: EServiceAddedV2,
+      payload: eServiceCreationEvent.data,
+    });
+    const descriptorCreationPayload = decodeProtobufPayload({
+      messageType: EServiceDescriptorAddedV2,
+      payload: descriptorCreationEvent.data,
+    });
+
+    const expectedEService: EService = {
+      ...mockEService,
+      description: eServiceTemplate.eserviceDescription,
+      name: `${eServiceTemplate.name} ${instanceId}`,
+      createdAt: eService.createdAt,
+      id: eService.id,
+      isSignalHubEnabled: eService.isSignalHubEnabled,
+      isClientAccessDelegable: false,
+      isConsumerDelegable: false,
+      descriptors: [],
+    };
+
+    const expectedEServiceWithDescriptor: EService = {
+      ...mockEService,
+      description: eServiceTemplate.eserviceDescription,
+      name: `${eServiceTemplate.name} ${instanceId}`,
+      createdAt: new Date(),
+      id: eService.id,
+      isSignalHubEnabled: eService.isSignalHubEnabled,
+      isClientAccessDelegable: false,
+      isConsumerDelegable: false,
+      descriptors: [
+        {
+          ...mockDescriptor,
+          description: publishedVersion.description,
+          id: eService.descriptors[0].id,
+          createdAt: new Date(),
+          serverUrls: [],
+          audience: [eServiceTemplate.audienceDescription],
+          dailyCallsPerConsumer: publishedVersion?.dailyCallsPerConsumer ?? 1,
+          dailyCallsTotal: publishedVersion?.dailyCallsTotal ?? 1,
+        },
+      ],
+    };
+
+    expect(eServiceCreationPayload.eservice).toEqual(
+      toEServiceV2(expectedEService)
+    );
+    expect(descriptorCreationPayload.eservice).toEqual(
+      toEServiceV2(expectedEServiceWithDescriptor)
+    );
+  });
+  it("should throw eServiceTemplateNotFound when the template does not exist", async () => {
+    await expect(
+      catalogService.createEServiceInstanceFromTemplate(
+        generateId(),
+        { instanceId: undefined },
+        {
+          authData: getMockAuthData(mockEService.producerId),
+          correlationId: generateId(),
+          serviceName: "",
+          logger: genericLogger,
+        }
+      )
+    ).rejects.toMatchObject({
+      code: "eServiceTemplateNotFound",
+    });
+  });
+  it("should throw eServiceTemplateWithoutPublishedVersion when the template does not have a published version", async () => {
+    const eServiceTemplate: EServiceTemplate = {
+      ...mockEServiceTemplate,
+      versions: [],
+    };
+
+    await addOneEServiceTemplate(eServiceTemplate);
+
+    await expect(
+      catalogService.createEServiceInstanceFromTemplate(
+        eServiceTemplate.id,
+        { instanceId: undefined },
+        {
+          authData: getMockAuthData(mockEService.producerId),
+          correlationId: generateId(),
+          serviceName: "",
+          logger: genericLogger,
+        }
+      )
+    ).rejects.toMatchObject({
+      code: "eServiceTemplateWithoutPublishedVersion",
+    });
+  });
+});
