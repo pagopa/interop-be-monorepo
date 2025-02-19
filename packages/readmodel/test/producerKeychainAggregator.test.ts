35f338cd86bb1cf0b6f8edf104500591856a9a27
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-19 18:09:38 +0100

    Remove unnecessary variables in aggregator test

--- packages/readmodel/test/producerKeychainAggregator.test.ts
+++ packages/readmodel/test/producerKeychainAggregator.test.ts
@@ -4,10 +4,8 @@ import {
 } from "pagopa-interop-commons-test";
 import { describe, it, expect } from "vitest";
 import {
-  EServiceId,
   generateId,
   ProducerKeychain,
-  UserId,
   WithMetadata,
 } from "pagopa-interop-models";
 import { splitProducerKeychainIntoObjectsSQL } from "../src/authorization/producerKeychainSplitters.js";
@@ -15,21 +13,13 @@ import { producerKeychainSQLToProducerKeychain } from "../src/authorization/prod
 
 describe("Producer keychain aggregator", () => {
   it("should convert a producer keychain SQL object into a business logic producer keychain ", () => {
-    const userId1 = generateId<UserId>();
-    const userId2 = generateId<UserId>();
-    const eserviceId1 = generateId<EServiceId>();
-    const eserviceId2 = generateId<EServiceId>();
-    const key1 = getMockKey();
-    const key2 = getMockKey();
-    const description = "test description";
-
     const producerKeychain: WithMetadata<ProducerKeychain> = {
       data: {
         ...getMockProducerKeychain(),
-        users: [userId1, userId2],
-        eservices: [eserviceId1, eserviceId2],
-        keys: [key1, key2],
-        description,
+        users: [generateId(), generateId()],
+        eservices: [generateId(), generateId()],
+        keys: [getMockKey(), getMockKey()],
+        description: "Test description",
       },
       metadata: { version: 1 },
     };
