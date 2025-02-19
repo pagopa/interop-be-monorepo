a35363ddf68e4adf4941e37b7a2fe4b4c8a693f9
Paolo Manca <paolo.manca@pagopa.it>
2025-02-19 17:18:23 +0100

    add scaffolding for agreement aggreagator and test

---
+++ packages/readmodel/test/agreement.aggregator.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("Agreement Aggregator", () => {
+  it("should convert an Agreement object as data model into an Agreement object as business model", () => {
+    expect(1).toEqual(1);
+  });
+});
