5ad530b669cc02fb96b8e1961284dee149330976
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-19 17:29:21 +0100

    Add client JWK key aggregator test

---
+++ packages/readmodel/test/clientJWKKeyAggregator.test.ts
@@ -0,0 +1,34 @@
+import crypto from "crypto";
+import {
+  getMockClient,
+  getMockKey,
+} from "pagopa-interop-commons-test/index.js";
+import { describe, it, expect } from "vitest";
+import { keyToClientJWKKey } from "pagopa-interop-commons";
+import { Key } from "pagopa-interop-models";
+import { splitClientJWKKeyIntoObjectsSQL } from "../src/authorization/clientJWKKeySplitters.js";
+import { clientJWKKeySQLToClientJWKKey } from "../src/authorization/clientJWKKeyAggregators.js";
+
+describe("Client JWK key aggregator", () => {
+  it("should convert a client JWK key SQL object into a business logic client JWK key", () => {
+    const publicKey = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+    }).publicKey;
+
+    const base64Key = Buffer.from(
+      publicKey.export({ type: "pkcs1", format: "pem" })
+    ).toString("base64url");
+
+    const client = getMockClient();
+    const key: Key = {
+      ...getMockKey(),
+      encodedPem: base64Key,
+    };
+    const clientJWKKey = keyToClientJWKKey(key, client.id);
+    const clientJWKKeySQL = splitClientJWKKeyIntoObjectsSQL(clientJWKKey, 1);
+
+    const aggregatedClientJWKKey =
+      clientJWKKeySQLToClientJWKKey(clientJWKKeySQL);
+    expect(aggregatedClientJWKKey).toEqual(aggregatedClientJWKKey);
+  });
+});
