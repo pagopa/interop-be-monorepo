16d4d166ce4fd5fd7f42855a162c512f0f5d0626
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-21 10:48:19 +0100

    Update client aggregator test

--- packages/readmodel/test/clientAggregator.test.ts
+++ packages/readmodel/test/clientAggregator.test.ts
@@ -1,100 +1,33 @@
 import { getMockClient, getMockKey } from "pagopa-interop-commons-test";
-import {
-  Client,
-  ClientId,
-  clientKind,
-  generateId,
-  PurposeId,
-  UserId,
-  WithMetadata,
-} from "pagopa-interop-models";
+import { Client, generateId, WithMetadata } from "pagopa-interop-models";
 import { describe, it, expect } from "vitest";
-import {
-  ClientKeySQL,
-  ClientPurposeSQL,
-  ClientSQL,
-  ClientUserSQL,
-} from "pagopa-interop-readmodel-models";
-import { clientSQLToClient } from "./../src/authorization/clientAggregators.js";
+import { splitClientIntoObjectsSQL } from "../src/authorization/clientSplitters.js";
+import { clientSQLToClient } from "../src/authorization/clientAggregators.js";
 
-describe("Client Aggregator", () => {
-  it("should convert a Client object as data model into a Client object as business model ", () => {
-    const clientId = generateId<ClientId>();
-    const userId1 = generateId<UserId>();
-    const userId2 = generateId<UserId>();
-    const purposeId1 = generateId<PurposeId>();
-    const purposeId2 = generateId<PurposeId>();
-    const key1 = getMockKey();
-    const key2 = getMockKey();
-    const mockClient = getMockClient();
-
-    const clientSQL: ClientSQL = {
-      ...mockClient,
-      id: clientId,
-      createdAt: mockClient.createdAt.toISOString(),
-      description: "description",
-      kind: clientKind.consumer,
-      metadataVersion: 1,
+describe("Client aggregator", () => {
+  it("should convert a client into client SQL objects", () => {
+    const client: WithMetadata<Client> = {
+      data: {
+        ...getMockClient(),
+        users: [generateId(), generateId()],
+        purposes: [generateId(), generateId()],
+        keys: [getMockKey(), getMockKey()],
+      },
+      metadata: {
+        version: 1,
+      },
     };
 
-    const clientUserSQL1: ClientUserSQL = {
-      metadataVersion: 1,
-      clientId,
-      userId: userId1,
-    };
-    const clientUserSQL2: ClientUserSQL = {
-      metadataVersion: 1,
-      clientId,
-      userId: userId2,
-    };
-    const clientPurposeSQL1: ClientPurposeSQL = {
-      metadataVersion: 1,
-      clientId,
-      purposeId: purposeId1,
-    };
-    const clientPurposeSQL2: ClientPurposeSQL = {
-      metadataVersion: 1,
-      clientId,
-      purposeId: purposeId2,
-    };
-    const clientKeySQL1: ClientKeySQL = {
-      ...key1,
-      metadataVersion: 1,
-      clientId,
-      createdAt: key1.createdAt.toISOString(),
-    };
-    const clientKeySQL2: ClientKeySQL = {
-      ...key2,
-      metadataVersion: 1,
-      clientId,
-      createdAt: key2.createdAt.toISOString(),
-    };
-    const clientUsersSQL: ClientUserSQL[] = [clientUserSQL1, clientUserSQL2];
-    const clientPurposesSQL: ClientPurposeSQL[] = [
-      clientPurposeSQL1,
-      clientPurposeSQL2,
-    ];
-    const clientKeysSQL: ClientKeySQL[] = [clientKeySQL1, clientKeySQL2];
+    const { clientSQL, clientUsersSQL, clientPurposesSQL, clientKeysSQL } =
+      splitClientIntoObjectsSQL(client.data, 1);
 
-    const client: WithMetadata<Client> = clientSQLToClient(
+    const aggregatedClient = clientSQLToClient(
       clientSQL,
       clientUsersSQL,
       clientPurposesSQL,
       clientKeysSQL
     );
 
-    const expectedClient: WithMetadata<Client> = {
-      data: {
-        ...mockClient,
-        id: clientId,
-        description: "description",
-        kind: clientKind.consumer,
-        purposes: [purposeId1, purposeId2],
-        users: [userId1, userId2],
-        keys: [key1, key2],
-      },
-      metadata: { version: 1 },
-    };
-    expect(client).toEqual(expectedClient);
+    expect(aggregatedClient).toMatchObject(client);
   });
 });
