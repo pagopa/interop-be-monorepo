4e95fa21ad46be242632645e15044c79c972c483
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-21 12:07:59 +0100

    Update optional properties logic in getMockDelegation

--- packages/commons-test/src/testUtils.ts
+++ packages/commons-test/src/testUtils.ts
@@ -397,8 +397,8 @@ export const getMockDelegation = ({
   eserviceId = generateId<EServiceId>(),
   state = "WaitingForApproval",
   submitterId = generateId<UserId>(),
-  activationContract = undefined,
-  revocationContract = undefined,
+  activationContract,
+  revocationContract,
 }: {
   kind: DelegationKind;
   id?: DelegationId;
@@ -419,8 +419,8 @@ export const getMockDelegation = ({
     eserviceId,
     createdAt: creationTime,
     state,
-    activationContract,
-    revocationContract,
+    ...(activationContract ? { activationContract } : {}),
+    ...(revocationContract ? { revocationContract } : {}),
     kind,
     stamps: {
       submission: {
