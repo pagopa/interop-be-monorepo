7f6c83152352a20bef4981c1347c0d3af5f0fa4a
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-21 11:17:28 +0100

    Update attribute aggregator test

--- packages/readmodel/test/attributeAggregator.test.ts
+++ packages/readmodel/test/attributeAggregator.test.ts
@@ -1,54 +1,35 @@
 import { getMockAttribute } from "pagopa-interop-commons-test/index.js";
 import { describe, it, expect } from "vitest";
 import { Attribute, WithMetadata } from "pagopa-interop-models";
-import { AttributeSQL } from "pagopa-interop-readmodel-models";
+import { splitAttributeIntoObjectsSQL } from "./../src/attribute/splitters.js";
 import { attributeSQLtoAttribute } from "./../src/attribute/aggregators.js";
 
-describe("Attribute Aggregator", () => {
-  it("should convert Attribute object as data model into an Attribute object as business model", () => {
-    const mockAttribute = getMockAttribute();
-    const attributeSQL: AttributeSQL = {
-      ...mockAttribute,
-      metadataVersion: 1,
-      creationTime: mockAttribute.creationTime.toISOString(),
-      code: "code",
-      origin: "origin",
-    };
-
-    const attribute = attributeSQLtoAttribute(attributeSQL);
-
-    const expectedAttribute: WithMetadata<Attribute> = {
+describe("Attribute aggregator", () => {
+  it("should convert an Attribute SQL object into a business logic Attribute", () => {
+    const mockAttribute: WithMetadata<Attribute> = {
       data: {
-        ...mockAttribute,
-        creationTime: mockAttribute.creationTime,
-        code: "code",
-        origin: "origin",
+        ...getMockAttribute(),
+        origin: "alfa",
+        code: "beta",
       },
       metadata: { version: 1 },
     };
-    expect(attribute).toEqual(expectedAttribute);
+    const attributeSQL = splitAttributeIntoObjectsSQL(mockAttribute.data, 1);
+    const aggregatedAttribute = attributeSQLtoAttribute(attributeSQL);
+
+    expect(aggregatedAttribute).toMatchObject(mockAttribute);
   });
 
-  it("should convert Attribute object with null values as data model into an Attribute object with undefined values as business model", () => {
-    const mockAttribute = getMockAttribute();
-    const attributeSQL: AttributeSQL = {
-      ...mockAttribute,
-      metadataVersion: 1,
-      creationTime: mockAttribute.creationTime.toISOString(),
-      code: null,
-      origin: null,
-    };
-
-    const attribute = attributeSQLtoAttribute(attributeSQL);
-
-    const expectedAttribute: WithMetadata<Attribute> = {
+  it("should convert an Attribute SQL object with null values into a business logic Attribute object with undefined values", () => {
+    const attribute: WithMetadata<Attribute> = {
       data: {
-        ...mockAttribute,
-        code: undefined,
-        origin: undefined,
+        ...getMockAttribute(),
       },
       metadata: { version: 1 },
     };
-    expect(attribute).toEqual(expectedAttribute);
+    const attributeSQL = splitAttributeIntoObjectsSQL(attribute.data, 1);
+    const aggregatedAttribute = attributeSQLtoAttribute(attributeSQL);
+
+    expect(aggregatedAttribute).toMatchObject(attribute);
   });
 });
