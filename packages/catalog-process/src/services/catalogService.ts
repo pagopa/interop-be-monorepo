e1a46d20b9d71ed6a4b2e709a4b293538cb64411
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-02-23 22:19:37 +0100

    Update catalogService.ts

--- packages/catalog-process/src/services/catalogService.ts
+++ packages/catalog-process/src/services/catalogService.ts
@@ -2333,12 +2333,11 @@ export function catalogServiceBuilder(
         throw operationForbidden;
       }
 
-      if (
-        descriptor.docs.some(
-          (d) =>
-            d.prettyName.toLowerCase() === document.prettyName.toLowerCase()
-        )
-      ) {
+      const alreadyHasDoc = descriptor.docs.some(
+        (d) => d.checksum === document.checksum
+      );
+
+      if (alreadyHasDoc) {
         return;
       }
 
@@ -2413,7 +2412,7 @@ export function catalogServiceBuilder(
       eserviceId: EServiceId,
       descriptorId: DescriptorId,
       documentId: EServiceDocumentId,
-      apiEServiceDescriptorDocumentUpdateSeed: catalogApi.UpdateEServiceDescriptorDocumentSeed,
+      { prettyName }: catalogApi.UpdateEServiceDescriptorDocumentSeed,
       { correlationId, logger }: WithLogger<AppContext>
     ): Promise<void> {
       logger.info(
@@ -2431,9 +2430,7 @@ export function catalogServiceBuilder(
 
       if (
         descriptor.docs.some(
-          (d) =>
-            d.prettyName.toLowerCase() ===
-            apiEServiceDescriptorDocumentUpdateSeed.prettyName.toLowerCase()
+          (d) => d.prettyName.toLowerCase() === prettyName.toLowerCase()
         )
       ) {
         return;
@@ -2441,7 +2438,7 @@ export function catalogServiceBuilder(
 
       const updatedDocument = {
         ...document,
-        prettyName: apiEServiceDescriptorDocumentUpdateSeed.prettyName,
+        prettyName,
       };
 
       const newEservice: EService = replaceDescriptor(eservice.data, {
