1e5f41e8472b1c6585ae5f1d50c6f5c88e063d56
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-02-23 13:01:13 +0100

    WIP

---
+++ packages/catalog-process/test/internalUpdateEserviceDescription.test.ts
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { genericLogger } from "pagopa-interop-commons";
+import { decodeProtobufPayload } from "pagopa-interop-commons-test/index.js";
+import {
+  Descriptor,
+  descriptorState,
+  EService,
+  toEServiceV2,
+  EServiceDescriptionUpdatedV2,
+  generateId,
+} from "pagopa-interop-models";
+import { expect, describe, it } from "vitest";
+import { eServiceNotFound } from "../src/model/domain/errors.js";
+import {
+  addOneEService,
+  catalogService,
+  getMockAuthData,
+  readLastEserviceEvent,
+  getMockDocument,
+  getMockDescriptor,
+  getMockEService,
+} from "./utils.js";
+
+describe("internalinternalUpdateEserviceDescription", () => {
+  it("should write on event-store for the internal update of the eService description", async () => {
+    const descriptor: Descriptor = {
+      ...getMockDescriptor(descriptorState.published),
+      interface: getMockDocument(),
+    };
+    const eservice: EService = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const updatedDescription = "eservice new description";
+
+    await catalogService.internalUpdateEServiceDescription(
+      eservice.id,
+      updatedDescription,
+      {
+        authData: getMockAuthData(eservice.producerId),
+        correlationId: generateId(),
+        serviceName: "",
+        logger: genericLogger,
+      }
+    );
+
+    const updatedEService: EService = {
+      ...eservice,
+      description: updatedDescription,
+    };
+
+    const writtenEvent = await readLastEserviceEvent(eservice.id);
+    expect(writtenEvent).toMatchObject({
+      stream_id: eservice.id,
+      version: "1",
+      type: "EServiceDescriptionUpdated",
+      event_version: 2,
+    });
+    const writtenPayload = decodeProtobufPayload({
+      messageType: EServiceDescriptionUpdatedV2,
+      payload: writtenEvent.data,
+    });
+
+    expect(writtenPayload.eservice).toEqual(toEServiceV2(updatedEService));
+  });
+
+  it("should throw eServiceNotFound if the eservice doesn't exist", async () => {
+    const eservice = getMockEService();
+
+    expect(
+      catalogService.internalUpdateEServiceDescription(
+        eservice.id,
+        "eservice new description",
+        {
+          authData: getMockAuthData(eservice.producerId),
+          correlationId: generateId(),
+          serviceName: "",
+          logger: genericLogger,
+        }
+      )
+    ).rejects.toThrowError(eServiceNotFound(eservice.id));
+  });
+});
