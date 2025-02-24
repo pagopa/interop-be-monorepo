3556c60fa36cb43a2bd0283b71dfe4a30813a979
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-24 09:57:32 +0100

    Squashed commit of the following:
    commit 5548e589dfdf0c1b611038814ab0d61107e651cd
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 15:18:25 2025 +0100
    
        Add aggregatePurposeArray
    
    commit f86022069c063566a71101bffb3796032763ae97
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 14:48:22 2025 +0100
    
        Rename purposeSQLToPurpose to aggregatePurpose
    
    commit d39fe58f0664b38514e911bc2956d60c822151a1
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 12:17:56 2025 +0100
    
        Fix purpose aggregator "null -> undefined" test
    
    commit f5ef2f8591f6445a463f03823ae54f39abd282d5
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 12:09:51 2025 +0100
    
        Replace toEqual with toMatchObject
    
    commit 0b01ab70290bf581d7003437fe6dd6aadeed8c8f
    Merge: 92468afac bebae24e2
    Author: shuyec <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 12:09:01 2025 +0100
    
        Merge branch 'PIN-6092_add-purpose-splitters' into PIN-6103_add-purpose-aggregators
    
    commit 92468afac808b645a1262710e8a4d81500da1dea
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 18:11:47 2025 +0100
    
        Remove unnecessary variables in aggregator test
    
    commit a886f66601064e6f382fe89b3915c9c7c4069059
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 17:01:58 2025 +0100
    
        Add purpose aggregator tests
    
    commit 468984ec71e3c6709812d149417ae6fbe739c081
    Merge: c3598a00d 991a5f2b4
    Author: shuyec <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 16:54:58 2025 +0100
    
        Merge branch 'PIN-6092_add-purpose-splitters' into PIN-6103_add-purpose-aggregators
    
    commit c3598a00db8b0eb15ac4bd58985293f25ec0930a
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 16:52:38 2025 +0100
    
        Fix optional properties in purpose aggregator
    
    commit f284536a1c7d4a2192ac7f1a2300e446ada07c25
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 15:46:35 2025 +0100
    
        Fix readmodel-models imports
    
    commit 6c86adc36e2ccb30178b4bfe0773e8f303a5ad40
    Merge: 3ab1303bf 6cc550630
    Author: shuyec <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 15:45:01 2025 +0100
    
        Merge branch 'PIN-6092_add-purpose-splitters' into PIN-6103_add-purpose-aggregators
    
    commit 3ab1303bf983108d17617afb9b56a3c1db2f281e
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 11:09:22 2025 +0100
    
        Fix missing properties in purpose aggregator
    
    commit 9e71f4aa1019a2877840282c3ec1a9a7de9851f5
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 09:51:51 2025 +0100
    
        Fix reduce in purposeRiskAnalysisFormSQLToPurposeRiskAnalysisForm
    
    commit 19c829b9157ce0136bda9a7d14a299267eed08ee
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Mon Feb 17 17:24:03 2025 +0100
    
        Fix riskAnalysisId for purposeRiskAnalysisForm
    
    commit a41eb2edef48f68ccf4291d4923ac909c50f90cf
    Merge: b97233940 2ad2be00b
    Author: shuyec <76391491+shuyec@users.noreply.github.com>
    Date:   Mon Feb 17 17:21:24 2025 +0100
    
        Merge branch 'PIN-6092_add-purpose-splitters' into PIN-6103_add-purpose-aggregators
    
    commit b97233940160ca3622153d000caed811efac7058
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Mon Feb 17 17:16:11 2025 +0100
    
        Replace .parse with explicit objects
    
    commit 87f0d5604f6cd552b9df9eabadf62f37bb2bf7c1
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Mon Feb 17 15:32:16 2025 +0100
    
        Add parse for PurposeRiskAnalysisForm
    
    commit 15d6f2a60484abf117cb45abd0e5f4a545ee7dfd
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Mon Feb 17 15:27:34 2025 +0100
    
        Add purpose aggregator logic

---
+++ packages/readmodel/src/purpose/aggregators.ts
@@ -0,0 +1,303 @@
+import {
+  DelegationId,
+  genericInternalError,
+  Purpose,
+  PurposeId,
+  PurposeRiskAnalysisForm,
+  PurposeVersion,
+  PurposeVersionDocument,
+  PurposeVersionId,
+  PurposeVersionState,
+  RiskAnalysisAnswerKind,
+  riskAnalysisAnswerKind,
+  RiskAnalysisId,
+  RiskAnalysisMultiAnswer,
+  RiskAnalysisMultiAnswerId,
+  RiskAnalysisSingleAnswer,
+  RiskAnalysisSingleAnswerId,
+  stringToDate,
+  unsafeBrandId,
+  WithMetadata,
+} from "pagopa-interop-models";
+import {
+  PurposeSQL,
+  PurposeRiskAnalysisFormSQL,
+  PurposeRiskAnalysisAnswerSQL,
+  PurposeVersionSQL,
+  PurposeVersionDocumentSQL,
+} from "pagopa-interop-readmodel-models";
+import { match } from "ts-pattern";
+
+// TODO: delete one aggregate array version
+export const aggregatePurposeArray = ({
+  purposesSQL,
+  purposeRiskAnalysisFormsSQL,
+  purposeRiskAnalysisAnswersSQL,
+  purposeVersionsSQL,
+  purposeVersionDocumentsSQL,
+}: {
+  purposesSQL: PurposeSQL[];
+  purposeRiskAnalysisFormsSQL: PurposeRiskAnalysisFormSQL[];
+  purposeRiskAnalysisAnswersSQL: PurposeRiskAnalysisAnswerSQL[];
+  purposeVersionsSQL: PurposeVersionSQL[];
+  purposeVersionDocumentsSQL: PurposeVersionDocumentSQL[];
+}): Array<WithMetadata<Purpose>> =>
+  purposesSQL.map((purposeSQL) =>
+    aggregatePurpose({
+      purposeSQL,
+      purposeRiskAnalysisFormSQL: purposeRiskAnalysisFormsSQL.find(
+        (formSQL) => formSQL.purposeId === purposeSQL.id
+      ),
+      purposeRiskAnalysisAnswersSQL: purposeRiskAnalysisAnswersSQL.filter(
+        (answerSQL) => answerSQL.purposeId === purposeSQL.id
+      ),
+      purposeVersionsSQL: purposeVersionsSQL.filter(
+        (versionSQL) => versionSQL.purposeId === purposeSQL.id
+      ),
+      purposeVersionDocumentsSQL: purposeVersionDocumentsSQL.filter(
+        (docSQL) => docSQL.purposeId === purposeSQL.id
+      ),
+    })
+  );
+
+export const aggregatePurposeArrayWithMaps = ({
+  purposesSQL,
+  purposeRiskAnalysisFormsSQL,
+  purposeRiskAnalysisAnswersSQL,
+  purposeVersionsSQL,
+  purposeVersionDocumentsSQL,
+}: {
+  purposesSQL: PurposeSQL[];
+  purposeRiskAnalysisFormsSQL: PurposeRiskAnalysisFormSQL[];
+  purposeRiskAnalysisAnswersSQL: PurposeRiskAnalysisAnswerSQL[];
+  purposeVersionsSQL: PurposeVersionSQL[];
+  purposeVersionDocumentsSQL: PurposeVersionDocumentSQL[];
+}): Array<WithMetadata<Purpose>> => {
+  const riskAnalysisFormsSQLByPurposeId = createPurposeSQLPropertyMap(
+    purposeRiskAnalysisFormsSQL
+  );
+  const riskAnalysisAnswersSQLByPurposeId = createPurposeSQLPropertyMap(
+    purposeRiskAnalysisAnswersSQL
+  );
+  const versionsSQLByPurposeId =
+    createPurposeSQLPropertyMap(purposeVersionsSQL);
+  const versionDocumentsSQLByPurposeId = createPurposeSQLPropertyMap(
+    purposeVersionDocumentsSQL
+  );
+  return purposesSQL.map((purposeSQL) => {
+    const purposeId = unsafeBrandId<PurposeId>(purposeSQL.id);
+    return aggregatePurpose({
+      purposeSQL,
+      purposeRiskAnalysisFormSQL:
+        riskAnalysisFormsSQLByPurposeId.get(purposeId)?.[0],
+      purposeRiskAnalysisAnswersSQL:
+        riskAnalysisAnswersSQLByPurposeId.get(purposeId),
+      purposeVersionsSQL: versionsSQLByPurposeId.get(purposeId) || [],
+      purposeVersionDocumentsSQL:
+        versionDocumentsSQLByPurposeId.get(purposeId) || [],
+    });
+  });
+};
+
+const createPurposeSQLPropertyMap = <
+  T extends
+    | PurposeRiskAnalysisFormSQL
+    | PurposeRiskAnalysisAnswerSQL
+    | PurposeVersionSQL
+    | PurposeVersionDocumentSQL
+>(
+  items: T[]
+): Map<PurposeId, T[]> =>
+  items.reduce((acc, item) => {
+    const purposeId = unsafeBrandId<PurposeId>(item.purposeId);
+    acc.set(purposeId, [...(acc.get(purposeId) || []), item]);
+    return acc;
+  }, new Map<PurposeId, T[]>());
+
+// TODO: ...rest
+export const aggregatePurpose = ({
+  purposeSQL,
+  purposeRiskAnalysisFormSQL,
+  purposeRiskAnalysisAnswersSQL,
+  purposeVersionsSQL,
+  purposeVersionDocumentsSQL,
+}: {
+  purposeSQL: PurposeSQL;
+  purposeRiskAnalysisFormSQL: PurposeRiskAnalysisFormSQL | undefined;
+  purposeRiskAnalysisAnswersSQL: PurposeRiskAnalysisAnswerSQL[] | undefined;
+  purposeVersionsSQL: PurposeVersionSQL[];
+  purposeVersionDocumentsSQL: PurposeVersionDocumentSQL[];
+}): WithMetadata<Purpose> => {
+  const purposeRiskAnalysisForm =
+    purposeRiskAnalysisFormSQLToPurposeRiskAnalysisForm(
+      purposeRiskAnalysisFormSQL,
+      purposeRiskAnalysisAnswersSQL
+    );
+
+  const documentsByPurposeVersionId: Map<
+    PurposeVersionId,
+    PurposeVersionDocumentSQL
+  > = purposeVersionDocumentsSQL.reduce(
+    (acc: Map<PurposeVersionId, PurposeVersionDocumentSQL>, docSQL) => {
+      acc.set(unsafeBrandId<PurposeVersionId>(docSQL.purposeVersionId), docSQL);
+      return acc;
+    },
+    new Map()
+  );
+
+  const purposeVersions = purposeVersionsSQL.reduce(
+    (acc: PurposeVersion[], purposeVersionSQL) => {
+      const purposeVersionDocumentSQL = documentsByPurposeVersionId.get(
+        unsafeBrandId(purposeVersionSQL.id)
+      );
+      const purposeVersionDocument: PurposeVersionDocument | undefined =
+        purposeVersionDocumentSQL
+          ? {
+              id: unsafeBrandId(purposeVersionDocumentSQL.id),
+              path: purposeVersionDocumentSQL.path,
+              contentType: purposeVersionDocumentSQL.contentType,
+              createdAt: stringToDate(purposeVersionDocumentSQL.createdAt),
+            }
+          : undefined;
+
+      const purposeVersion: PurposeVersion = {
+        id: unsafeBrandId(purposeVersionSQL.id),
+        state: PurposeVersionState.parse(purposeVersionSQL.state),
+        dailyCalls: purposeVersionSQL.dailyCalls,
+        createdAt: stringToDate(purposeVersionSQL.createdAt),
+        ...(purposeVersionSQL.rejectionReason
+          ? { rejectionReason: purposeVersionSQL.rejectionReason }
+          : {}),
+        firstActivationAt: stringToDate(purposeVersionSQL.firstActivationAt),
+        ...(purposeVersionSQL.suspendedAt
+          ? { suspendedAt: stringToDate(purposeVersionSQL.suspendedAt) }
+          : {}),
+        ...(purposeVersionSQL.updatedAt
+          ? {
+              updatedAt: stringToDate(purposeVersionSQL.updatedAt),
+            }
+          : {}),
+        ...(purposeVersionDocument
+          ? { riskAnalysis: purposeVersionDocument }
+          : {}),
+      };
+
+      return [...acc, purposeVersion];
+    },
+    []
+  );
+
+  const purpose: Purpose = {
+    id: unsafeBrandId(purposeSQL.id),
+    title: purposeSQL.title,
+    createdAt: stringToDate(purposeSQL.createdAt),
+    eserviceId: unsafeBrandId(purposeSQL.eserviceId),
+    consumerId: unsafeBrandId(purposeSQL.consumerId),
+    description: purposeSQL.description,
+    isFreeOfCharge: purposeSQL.isFreeOfCharge,
+    versions: purposeVersions,
+    ...(purposeRiskAnalysisForm
+      ? { riskAnalysisForm: purposeRiskAnalysisForm }
+      : {}),
+    ...(purposeSQL.suspendedByConsumer !== null
+      ? {
+          suspendedByConsumer: purposeSQL.suspendedByConsumer,
+        }
+      : {}),
+    ...(purposeSQL.suspendedByProducer !== null
+      ? {
+          suspendedByProducer: purposeSQL.suspendedByProducer,
+        }
+      : {}),
+    ...(purposeSQL.delegationId
+      ? {
+          delegationId: unsafeBrandId<DelegationId>(purposeSQL.delegationId),
+        }
+      : {}),
+    ...(purposeSQL.freeOfChargeReason
+      ? {
+          freeOfChargeReason: purposeSQL.freeOfChargeReason,
+        }
+      : {}),
+    ...(purposeSQL.updatedAt
+      ? { updatedAt: stringToDate(purposeSQL.updatedAt) }
+      : {}),
+  };
+
+  return {
+    data: purpose,
+    metadata: { version: purposeSQL.metadataVersion },
+  };
+};
+
+// TODO: improve naming
+export const purposeRiskAnalysisFormSQLToPurposeRiskAnalysisForm = (
+  purposeRiskAnalysisFormSQL: PurposeRiskAnalysisFormSQL | undefined,
+  answers: PurposeRiskAnalysisAnswerSQL[] | undefined
+): PurposeRiskAnalysisForm | undefined => {
+  if (!purposeRiskAnalysisFormSQL) {
+    return undefined;
+  }
+
+  if (!answers) {
+    throw genericInternalError(
+      `Purpose risk analysis form with id ${purposeRiskAnalysisFormSQL.id} found without answers`
+    );
+  }
+
+  const { singleAnswers, multiAnswers } = answers.reduce<{
+    singleAnswers: RiskAnalysisSingleAnswer[];
+    multiAnswers: RiskAnalysisMultiAnswer[];
+  }>(
+    (acc, answer) =>
+      match({
+        ...answer,
+        kind: RiskAnalysisAnswerKind.parse(answer.kind),
+      })
+        .with({ kind: riskAnalysisAnswerKind.single }, (a) => ({
+          singleAnswers: [
+            ...acc.singleAnswers,
+            {
+              id: unsafeBrandId<RiskAnalysisSingleAnswerId>(a.id),
+              key: a.key,
+              ...(a.value
+                ? {
+                    value: a.value[0],
+                  }
+                : undefined),
+            },
+          ],
+          multiAnswers: acc.multiAnswers,
+        }))
+        .with({ kind: riskAnalysisAnswerKind.multi }, (a) => ({
+          singleAnswers: acc.singleAnswers,
+          multiAnswers: [
+            ...acc.multiAnswers,
+            {
+              id: unsafeBrandId<RiskAnalysisMultiAnswerId>(a.id),
+              key: a.key,
+              values: a.value || [],
+            },
+          ],
+        }))
+        .exhaustive(),
+    {
+      singleAnswers: [],
+      multiAnswers: [],
+    }
+  );
+
+  return {
+    id: unsafeBrandId(purposeRiskAnalysisFormSQL.id),
+    version: purposeRiskAnalysisFormSQL.version,
+    singleAnswers,
+    multiAnswers,
+    ...(purposeRiskAnalysisFormSQL.riskAnalysisId
+      ? {
+          riskAnalysisId: unsafeBrandId<RiskAnalysisId>(
+            purposeRiskAnalysisFormSQL.riskAnalysisId
+          ),
+        }
+      : {}),
+  };
+};
