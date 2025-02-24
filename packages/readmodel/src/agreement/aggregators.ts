e603456371865cf29c2b49820af0cbbc19ab6f88
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-24 10:05:57 +0100

    Squashed commit of the following:
    commit eafedcf177fdf80423ae226de0fed02f774e5fd8
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Fri Feb 21 19:08:48 2025 +0100
    
        adding agreement array aggregator
    
    commit 825c4cf840dd20fb07fb18f8a69c54054e3f93ab
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Fri Feb 21 18:41:06 2025 +0100
    
        adding agreement test aggregator
    
    commit 226fd521a78610e0ccd06c26bc97e835983d70c3
    Merge: 4ec7ce2a2 63a68c87b
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Fri Feb 21 13:52:41 2025 +0100
    
        Merge branch 'PIN-6089_agreement-splitter' into PIN-6100_agreement-aggregator
    
    commit 4ec7ce2a2a26b0d18aadfed9d7d40bf175110135
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Fri Feb 21 12:32:29 2025 +0100
    
        agreement aggregator
    
    commit b3e7373a34c42b41aef6d29723161d3303f39d97
    Merge: b42eef001 09e668b6f
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Fri Feb 21 11:03:14 2025 +0100
    
        Merge branch 'PIN-6089_agreement-splitter' into PIN-6100_agreement-aggregator
    
    commit b42eef0012dbc1e2ffddb3591a8727664975678b
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Wed Feb 19 19:52:25 2025 +0100
    
        draft version agreement aggregator
    
    commit a35363ddf68e4adf4941e37b7a2fe4b4c8a693f9
    Author: Paolo Manca <paolo.manca@pagopa.it>
    Date:   Wed Feb 19 17:18:23 2025 +0100
    
        add scaffolding for agreement aggreagator and test

---
+++ packages/readmodel/src/agreement/aggregators.ts
@@ -0,0 +1,205 @@
+import {
+  AgreementSQL,
+  AgreementStampSQL,
+  AgreementDocumentSQL,
+  AgreementAttributeSQL,
+} from "pagopa-interop-readmodel-models";
+import {
+  Agreement,
+  AgreementId,
+  unsafeBrandId,
+  WithMetadata,
+  EServiceId,
+  TenantId,
+  DescriptorId,
+  AgreementState,
+  stringToDate,
+  attributeKind,
+  AttributeId,
+  AgreementAttribute,
+  agreementDocumentKind,
+  AgreementDocument,
+  AgreementDocumentId,
+  AgreementStamp,
+  UserId,
+  DelegationId,
+  agreementStampKind,
+} from "pagopa-interop-models";
+
+export const aggregatorAgreementArray = ({
+  agreementSQL,
+  agreementStampsSQL,
+  agreementDocumentsSQL,
+  agreementAttributesSQL,
+}: {
+  agreementSQL: AgreementSQL[];
+  agreementStampsSQL: AgreementStampSQL[];
+  agreementDocumentsSQL: AgreementDocumentSQL[];
+  agreementAttributesSQL: AgreementAttributeSQL[];
+}): Array<WithMetadata<Agreement>> =>
+  agreementSQL.map((agreementSQL) =>
+    aggregateAgreement({
+      agreementSQL,
+      agreementStampsSQL: agreementStampsSQL.filter(
+        (stampSQL) => stampSQL.agreementId === agreementSQL.id
+      ),
+      agreementDocumentsSQL: agreementDocumentsSQL.filter(
+        (documentSQL) => documentSQL.agreementId === agreementSQL.id
+      ),
+      agreementAttributesSQL: agreementAttributesSQL.filter(
+        (attributeSQL) => attributeSQL.agreementId === agreementSQL.id
+      ),
+    })
+  );
+
+export const aggregateAgreement = ({
+  agreementSQL,
+  agreementStampsSQL,
+  agreementDocumentsSQL,
+  agreementAttributesSQL,
+}: {
+  agreementSQL: AgreementSQL;
+  agreementStampsSQL: AgreementStampSQL[];
+  agreementDocumentsSQL: AgreementDocumentSQL[];
+  agreementAttributesSQL: AgreementAttributeSQL[];
+}): WithMetadata<Agreement> => {
+  const verifiedAttributes: AgreementAttribute[] = agreementAttributesSQL
+    .filter((a) => a.kind === attributeKind.verified)
+    .map((a) => ({ id: unsafeBrandId<AttributeId>(a.attributeId) }));
+  const certifiedAttributes: AgreementAttribute[] = agreementAttributesSQL
+    .filter((a) => a.kind === attributeKind.certified)
+    .map((a) => ({ id: unsafeBrandId<AttributeId>(a.attributeId) }));
+  const declaredAttributes: AgreementAttribute[] = agreementAttributesSQL
+    .filter((a) => a.kind === attributeKind.declared)
+    .map((a) => ({ id: unsafeBrandId<AttributeId>(a.attributeId) }));
+
+  const consumerDocuments: AgreementDocument[] = agreementDocumentsSQL
+    .filter((d) => d.kind === agreementDocumentKind.consumerDoc)
+    .map(documentSQLtoDocument);
+
+  const contractSQL: AgreementDocumentSQL | undefined =
+    agreementDocumentsSQL.find(
+      (d) => d.kind === agreementDocumentKind.contract
+    );
+  const contract = contractSQL ? documentSQLtoDocument(contractSQL) : undefined;
+
+  const submissionStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.submission
+  );
+  const activationStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.activation
+  );
+  const rejectionStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.rejection
+  );
+  const suspensionByProducerStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.suspensionByProducer
+  );
+  const suspensionByConsumerStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.suspensionByConsumer
+  );
+  const upgradeStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.upgrade
+  );
+  const archivingStampSQL = agreementStampsSQL.find(
+    (stamp) => stamp.kind === agreementStampKind.archiving
+  );
+  const agreement: Agreement = {
+    id: unsafeBrandId<AgreementId>(agreementSQL.id),
+    eserviceId: unsafeBrandId<EServiceId>(agreementSQL.eserviceId),
+    descriptorId: unsafeBrandId<DescriptorId>(agreementSQL.descriptorId),
+    producerId: unsafeBrandId<TenantId>(agreementSQL.producerId),
+    consumerId: unsafeBrandId<TenantId>(agreementSQL.consumerId),
+    state: AgreementState.parse(agreementSQL.state),
+    verifiedAttributes,
+    certifiedAttributes,
+    declaredAttributes,
+    ...(agreementSQL.suspendedByConsumer !== null
+      ? {
+          suspendedByConsumer: agreementSQL.suspendedByConsumer,
+        }
+      : {}),
+    ...(agreementSQL.suspendedByProducer !== null
+      ? {
+          suspendedByProducer: agreementSQL.suspendedByProducer,
+        }
+      : {}),
+    ...(agreementSQL.suspendedByPlatform !== null
+      ? {
+          suspendedByPlatform: agreementSQL.suspendedByPlatform,
+        }
+      : {}),
+    consumerDocuments,
+    createdAt: stringToDate(agreementSQL.createdAt),
+    ...(agreementSQL.updatedAt
+      ? { updatedAt: stringToDate(agreementSQL.updatedAt) }
+      : {}),
+    ...(agreementSQL.consumerNotes !== null
+      ? {
+          consumerNotes: agreementSQL.consumerNotes,
+        }
+      : {}),
+    ...(contract ? { contract } : {}),
+    stamps: {
+      ...(submissionStampSQL
+        ? { submission: stampSQLtoStamp(submissionStampSQL) }
+        : {}),
+      ...(activationStampSQL
+        ? { activation: stampSQLtoStamp(activationStampSQL) }
+        : {}),
+      ...(rejectionStampSQL
+        ? { rejection: stampSQLtoStamp(rejectionStampSQL) }
+        : {}),
+      ...(suspensionByProducerStampSQL
+        ? {
+            suspensionByProducer: stampSQLtoStamp(suspensionByProducerStampSQL),
+          }
+        : {}),
+      ...(suspensionByConsumerStampSQL
+        ? {
+            suspensionByConsumer: stampSQLtoStamp(suspensionByConsumerStampSQL),
+          }
+        : {}),
+      ...(upgradeStampSQL ? { upgrade: stampSQLtoStamp(upgradeStampSQL) } : {}),
+      ...(archivingStampSQL
+        ? { archiving: stampSQLtoStamp(archivingStampSQL) }
+        : {}),
+    },
+    ...(agreementSQL.rejectionReason !== null
+      ? {
+          rejectionReason: agreementSQL.rejectionReason,
+        }
+      : {}),
+    ...(agreementSQL.suspendedAt !== null
+      ? {
+          suspendedAt: stringToDate(agreementSQL.suspendedAt),
+        }
+      : {}),
+  };
+
+  return {
+    data: agreement,
+    metadata: { version: agreementSQL.metadataVersion },
+  };
+};
+
+const documentSQLtoDocument = (
+  document: AgreementDocumentSQL
+): AgreementDocument => ({
+  id: unsafeBrandId<AgreementDocumentId>(document.id),
+  path: document.path,
+  name: document.name,
+  prettyName: document.prettyName,
+  contentType: document.contentType,
+  createdAt: stringToDate(document.createdAt),
+});
+
+const stampSQLtoStamp = (stampSQL: AgreementStampSQL): AgreementStamp => ({
+  who: unsafeBrandId<UserId>(stampSQL.who),
+  when: stringToDate(stampSQL.when),
+  ...(stampSQL.delegationId !== null
+    ? {
+        delegationId: unsafeBrandId<DelegationId>(stampSQL.delegationId),
+      }
+    : {}),
+});
