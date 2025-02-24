9749f77c7e60b05cb6a26ac2e20749fc3cd4adc0
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-24 09:56:45 +0100

    Squashed commit of the following:
    commit 5ee25bb35c196579926d8ec55755e11c0abaa4f1
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Fri Feb 21 10:53:34 2025 +0100
    
        Update test titles
    
    commit 4021d1823c825a84b2149f5dcbee69067e2abdd2
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Fri Feb 21 10:24:24 2025 +0100
    
        Refactor null check
    
    commit 0d3e4f3718e2b7a078351bda5eb8009ca998a6ac
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Fri Feb 21 10:17:58 2025 +0100
    
        Fix aggregator
    
    commit 7634fbe117d80b99726a7a0f37b8d2d686750e8c
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Fri Feb 21 10:17:51 2025 +0100
    
        Add aggregator test
    
    commit 963ea0e067ccdc63273335ac8bb4b1e153b95960
    Merge: e79ca9ec0 6fa658178
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Fri Feb 21 09:43:51 2025 +0100
    
        Merge branch 'PIN-6090_tenant-splitters' into PIN-6101_tenant-aggregators
    
    commit e79ca9ec0667cf657378941c40ff72a4976a4920
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 15:17:15 2025 +0100
    
        Add tenant array aggregator
    
    commit fcca8e8a61c3cc99f25c3bfb271242eda6b8cda4
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:34:33 2025 +0100
    
        Fix test
    
    commit 9c722d1ffe1be40b25b3156e3a73109de74760f0
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:31:44 2025 +0100
    
        Fix
    
    commit 88e4ad96caf4f49c642154ef1b02312a063d59c7
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:30:16 2025 +0100
    
        Fix features
    
    commit 0a0146d95bb422ac5224bb7aab4afa7a51a60425
    Merge: 2c6b2a400 7e53ea4b3
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:23:20 2025 +0100
    
        Merge branch 'PIN-6090_tenant-splitters' into PIN-6101_tenant-aggregators
    
    commit 2c6b2a40074e1577bb296079571611187a758a5f
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Wed Feb 19 15:26:53 2025 +0100
    
        Fix import
    
    commit d43d356dde0a2b18cafecfc4af9926d6acfa4b96
    Merge: d2b1cc7e4 58ccee9a7
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Wed Feb 19 15:26:32 2025 +0100
    
        Merge branch 'PIN-6090_tenant-splitters' into PIN-6101_tenant-aggregators
    
    commit d2b1cc7e47a0d236f96290d4717e165b6addf61b
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Mon Feb 17 15:08:14 2025 +0100
    
        Fix version
    
    commit f67353172a102b4e7b226f05a5335f69bded50e0
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Mon Feb 17 14:38:51 2025 +0100
    
        Reorder properties
    
    commit d908c55b1b409ffe78ff43e3412037ba621c677c
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Mon Feb 17 14:37:03 2025 +0100
    
        Add tenant aggregator
    
    commit 9fc09d85fadaf10233e8ba15a8dc92109c592b32
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Mon Feb 17 12:15:29 2025 +0100
    
        Add todo

---
+++ packages/readmodel/src/tenant/aggregators.ts
@@ -0,0 +1,313 @@
+import {
+  AttributeId,
+  CertifiedTenantAttribute,
+  DeclaredTenantAttribute,
+  DelegationId,
+  genericInternalError,
+  stringToDate,
+  Tenant,
+  TenantAttribute,
+  tenantAttributeType,
+  TenantFeature,
+  TenantFeatureCertifier,
+  TenantFeatureDelegatedConsumer,
+  TenantFeatureDelegatedProducer,
+  tenantFeatureType,
+  TenantId,
+  TenantKind,
+  TenantMail,
+  TenantMailKind,
+  TenantRevoker,
+  TenantUnitType,
+  TenantVerifier,
+  unsafeBrandId,
+  VerifiedTenantAttribute,
+  WithMetadata,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import {
+  TenantCertifiedAttributeSQL,
+  TenantDeclaredAttributeSQL,
+  TenantFeatureSQL,
+  TenantMailSQL,
+  TenantSQL,
+  TenantVerifiedAttributeRevokerSQL,
+  TenantVerifiedAttributeSQL,
+  TenantVerifiedAttributeVerifierSQL,
+} from "pagopa-interop-readmodel-models";
+
+export const aggregateTenantSQL = ({
+  tenantSQL,
+  tenantMailsSQL,
+  tenantCertifiedAttributesSQL,
+  tenantDeclaredAttributesSQL,
+  tenantVerifiedAttributesSQL,
+  tenantVerifiedAttributeVerifiersSQL,
+  tenantVerifiedAttributeRevokersSQL,
+  tenantFeaturesSQL,
+}: {
+  tenantSQL: TenantSQL;
+  tenantMailsSQL: TenantMailSQL[];
+  tenantCertifiedAttributesSQL: TenantCertifiedAttributeSQL[];
+  tenantDeclaredAttributesSQL: TenantDeclaredAttributeSQL[];
+  tenantVerifiedAttributesSQL: TenantVerifiedAttributeSQL[];
+  tenantVerifiedAttributeVerifiersSQL: TenantVerifiedAttributeVerifierSQL[];
+  tenantVerifiedAttributeRevokersSQL: TenantVerifiedAttributeRevokerSQL[];
+  tenantFeaturesSQL: TenantFeatureSQL[];
+}): WithMetadata<Tenant> => {
+  const mails = tenantMailsSQL.map(tenantMailSQLToTenantMail);
+
+  const attributes = aggregateTenantAttributes({
+    tenantCertifiedAttributesSQL,
+    tenantDeclaredAttributesSQL,
+    tenantVerifiedAttributesSQL,
+    tenantVerifiedAttributeVerifiersSQL,
+    tenantVerifiedAttributeRevokersSQL,
+  });
+
+  const features: TenantFeature[] = tenantFeaturesSQL.map((feature) =>
+    match(feature.kind)
+      .with(tenantFeatureType.persistentCertifier, () => {
+        if (!feature.certifierId) {
+          throw genericInternalError(
+            "certifierId can't be missing in certifier feature"
+          );
+        }
+        return {
+          type: tenantFeatureType.persistentCertifier,
+          certifierId: feature.certifierId,
+        } satisfies TenantFeatureCertifier;
+      })
+      .with(tenantFeatureType.delegatedProducer, () => {
+        if (!feature.availabilityTimestamp) {
+          throw genericInternalError(
+            "availabilityTimestamp can't be missing in delegatedProducer feature"
+          );
+        }
+        return {
+          type: tenantFeatureType.delegatedProducer,
+          availabilityTimestamp: stringToDate(feature.availabilityTimestamp),
+        } satisfies TenantFeatureDelegatedProducer;
+      })
+      .with(tenantFeatureType.delegatedConsumer, () => {
+        if (!feature.availabilityTimestamp) {
+          throw genericInternalError(
+            "availabilityTimestamp can't be missing in delegatedConsumer feature"
+          );
+        }
+        return {
+          type: tenantFeatureType.delegatedConsumer,
+          availabilityTimestamp: stringToDate(feature.availabilityTimestamp),
+        } satisfies TenantFeatureDelegatedConsumer;
+      })
+      .otherwise(() => {
+        throw genericInternalError("Unexpected tenant feature");
+      })
+  );
+
+  const tenant: Tenant = {
+    id: unsafeBrandId<TenantId>(tenantSQL.id),
+    name: tenantSQL.name,
+    kind: tenantSQL.kind ? TenantKind.parse(tenantSQL.kind) : undefined,
+    createdAt: stringToDate(tenantSQL.createdAt),
+    onboardedAt: stringToDate(tenantSQL.onboardedAt),
+    updatedAt: stringToDate(tenantSQL.updatedAt),
+    selfcareId: tenantSQL.selfcareId || undefined,
+    subUnitType: tenantSQL.subUnitType
+      ? TenantUnitType.parse(tenantSQL.subUnitType)
+      : undefined,
+    attributes,
+    externalId: {
+      origin: tenantSQL.externalIdOrigin,
+      value: tenantSQL.externalIdValue,
+    },
+    features,
+    mails,
+  };
+  return {
+    data: tenant,
+    metadata: {
+      version: tenantSQL.metadataVersion,
+    },
+  };
+};
+
+export const aggregateTenantArray = ({
+  tenantsSQL,
+  tenantMailsSQL,
+  tenantCertifiedAttributesSQL,
+  tenantDeclaredAttributesSQL,
+  tenantVerifiedAttributesSQL,
+  tenantVerifiedAttributeVerifiersSQL,
+  tenantVerifiedAttributeRevokersSQL,
+  tenantFeaturesSQL,
+}: {
+  tenantsSQL: TenantSQL[];
+  tenantMailsSQL: TenantMailSQL[];
+  tenantCertifiedAttributesSQL: TenantCertifiedAttributeSQL[];
+  tenantDeclaredAttributesSQL: TenantDeclaredAttributeSQL[];
+  tenantVerifiedAttributesSQL: TenantVerifiedAttributeSQL[];
+  tenantVerifiedAttributeVerifiersSQL: TenantVerifiedAttributeVerifierSQL[];
+  tenantVerifiedAttributeRevokersSQL: TenantVerifiedAttributeRevokerSQL[];
+  tenantFeaturesSQL: TenantFeatureSQL[];
+}): Array<WithMetadata<Tenant>> =>
+  tenantsSQL.map((tenantSQL) => {
+    const mailsSQLOfCurrentTenant = tenantMailsSQL.filter(
+      (mailSQL) => mailSQL.tenantId === tenantSQL.id
+    );
+
+    const certifiedAttributesSQLOfCurrentTenant =
+      tenantCertifiedAttributesSQL.filter(
+        (attr) => attr.tenantId === tenantSQL.id
+      );
+
+    const declaredAttributesSQLOfCurrentTenant =
+      tenantDeclaredAttributesSQL.filter(
+        (attr) => attr.tenantId === tenantSQL.id
+      );
+
+    const verifiedAttributesSQLOfCurrentTenant =
+      tenantVerifiedAttributesSQL.filter(
+        (attr) => attr.tenantId === tenantSQL.id
+      );
+
+    const verifiersSQLOfCurrentTenant =
+      tenantVerifiedAttributeVerifiersSQL.filter(
+        (verifier) => verifier.tenantId === tenantSQL.id
+      );
+
+    const revokersSQLOfCurrentTenant =
+      tenantVerifiedAttributeRevokersSQL.filter(
+        (revoker) => revoker.tenantId === tenantSQL.id
+      );
+
+    const feautesSQLOfCurrentTenant = tenantFeaturesSQL.filter(
+      (feature) => feature.tenantId === tenantSQL.id
+    );
+
+    return aggregateTenantSQL({
+      tenantSQL,
+      tenantMailsSQL: mailsSQLOfCurrentTenant,
+      tenantCertifiedAttributesSQL: certifiedAttributesSQLOfCurrentTenant,
+      tenantDeclaredAttributesSQL: declaredAttributesSQLOfCurrentTenant,
+      tenantVerifiedAttributesSQL: verifiedAttributesSQLOfCurrentTenant,
+      tenantVerifiedAttributeVerifiersSQL: verifiersSQLOfCurrentTenant,
+      tenantVerifiedAttributeRevokersSQL: revokersSQLOfCurrentTenant,
+      tenantFeaturesSQL: feautesSQLOfCurrentTenant,
+    });
+  });
+
+const tenantMailSQLToTenantMail = (mail: TenantMailSQL): TenantMail => ({
+  id: mail.id,
+  createdAt: stringToDate(mail.createdAt),
+  description: mail.description || undefined,
+  kind: TenantMailKind.parse(mail.kind),
+  address: mail.address,
+});
+
+const aggregateTenantAttributes = ({
+  tenantCertifiedAttributesSQL,
+  tenantDeclaredAttributesSQL,
+  tenantVerifiedAttributesSQL,
+  tenantVerifiedAttributeVerifiersSQL,
+  tenantVerifiedAttributeRevokersSQL,
+}: {
+  tenantCertifiedAttributesSQL: TenantCertifiedAttributeSQL[];
+  tenantDeclaredAttributesSQL: TenantDeclaredAttributeSQL[];
+  tenantVerifiedAttributesSQL: TenantVerifiedAttributeSQL[];
+  tenantVerifiedAttributeVerifiersSQL: TenantVerifiedAttributeVerifierSQL[];
+  tenantVerifiedAttributeRevokersSQL: TenantVerifiedAttributeRevokerSQL[];
+}): TenantAttribute[] => {
+  const certifiedTenantAttributes: CertifiedTenantAttribute[] =
+    tenantCertifiedAttributesSQL.map((certifiedAttributeSQL) => ({
+      id: unsafeBrandId<AttributeId>(certifiedAttributeSQL.attributeId),
+      type: tenantAttributeType.CERTIFIED,
+      assignmentTimestamp: stringToDate(
+        certifiedAttributeSQL.assignmentTimestamp
+      ),
+      revocationTimestamp: stringToDate(
+        certifiedAttributeSQL.revocationTimestamp
+      ),
+    }));
+
+  const declaredTenantAttributes: DeclaredTenantAttribute[] =
+    tenantDeclaredAttributesSQL.map((declaredAttributeSQL) => ({
+      id: unsafeBrandId<AttributeId>(declaredAttributeSQL.attributeId),
+      type: tenantAttributeType.DECLARED,
+      assignmentTimestamp: stringToDate(
+        declaredAttributeSQL.assignmentTimestamp
+      ),
+      revocationTimestamp: stringToDate(
+        declaredAttributeSQL.revocationTimestamp
+      ),
+      ...(declaredAttributeSQL.delegationId
+        ? {
+            delegationId: unsafeBrandId<DelegationId>(
+              declaredAttributeSQL.delegationId
+            ),
+          }
+        : {}),
+    }));
+
+  const verifiedTenantAttributes: VerifiedTenantAttribute[] =
+    tenantVerifiedAttributesSQL.map((currentVerifiedAttributeSQL) => {
+      const verifiersOfCurrentAttribute: TenantVerifier[] =
+        tenantVerifiedAttributeVerifiersSQL
+          .filter(
+            (attr) =>
+              attr.tenantVerifiedAttributeId ===
+              currentVerifiedAttributeSQL.attributeId
+          )
+          .map((tenantVerifierSQL) => ({
+            id: unsafeBrandId<TenantId>(tenantVerifierSQL.tenantVerifierId),
+            verificationDate: stringToDate(tenantVerifierSQL.verificationDate),
+            expirationDate: stringToDate(tenantVerifierSQL.expirationDate),
+            extensionDate: stringToDate(tenantVerifierSQL.extensionDate),
+            ...(tenantVerifierSQL.delegationId
+              ? {
+                  delegationId: unsafeBrandId<DelegationId>(
+                    tenantVerifierSQL.delegationId
+                  ),
+                }
+              : {}),
+          }));
+
+      const revokersOfCurrentAttribute: TenantRevoker[] =
+        tenantVerifiedAttributeRevokersSQL
+          .filter(
+            (attr) =>
+              attr.tenantVerifiedAttributeId ===
+              currentVerifiedAttributeSQL.attributeId
+          )
+          .map((tenantRevokerSQL) => ({
+            id: unsafeBrandId<TenantId>(tenantRevokerSQL.tenantRevokerId),
+            verificationDate: stringToDate(tenantRevokerSQL.verificationDate),
+            expirationDate: stringToDate(tenantRevokerSQL.expirationDate),
+            extensionDate: stringToDate(tenantRevokerSQL.extensionDate),
+            revocationDate: stringToDate(tenantRevokerSQL.revocationDate),
+            ...(tenantRevokerSQL.delegationId
+              ? {
+                  delegationId: unsafeBrandId<DelegationId>(
+                    tenantRevokerSQL.delegationId
+                  ),
+                }
+              : {}),
+          }));
+
+      return {
+        id: unsafeBrandId<AttributeId>(currentVerifiedAttributeSQL.attributeId),
+        type: tenantAttributeType.VERIFIED,
+        assignmentTimestamp: stringToDate(
+          currentVerifiedAttributeSQL.assignmentTimestamp
+        ),
+        verifiedBy: verifiersOfCurrentAttribute,
+        revokedBy: revokersOfCurrentAttribute,
+      };
+    });
+
+  return [
+    ...certifiedTenantAttributes,
+    ...declaredTenantAttributes,
+    ...verifiedTenantAttributes,
+  ];
+};
