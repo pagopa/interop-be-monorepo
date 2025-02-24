01e948b1667fc2b8492fcb8fb893172511e45a5c
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-02-24 09:57:11 +0100

    Squashed commit of the following:
    commit 30b3bbba99094d12e9e40bbe0b853d64f174df70
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 15:44:14 2025 +0100
    
        Add aggregateDelegationArray
    
    commit 8fe6f60f9e8b44781ae24cf7308a48b01b0bba7d
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 12:08:15 2025 +0100
    
        Remove console.log
    
    commit 4e95fa21ad46be242632645e15044c79c972c483
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 12:07:59 2025 +0100
    
        Update optional properties logic in getMockDelegation
    
    commit 88dcae6f300702e5f2d6363ba2667a3ebfe6b266
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 11:26:23 2025 +0100
    
        Replace toEqual with toMatchObject
    
    commit 3fad9ab34d397b5383c2e838ba57310c92c82e6b
    Merge: 9f3f49245 a7904b9e4
    Author: shuyec <76391491+shuyec@users.noreply.github.com>
    Date:   Fri Feb 21 11:25:47 2025 +0100
    
        Merge branch 'PIN-6091_add-delegation-splitter' into PIN-6102_delegation-aggregator
    
    commit 9f3f49245a4a7f38e948ffa5eb3cd7feed242c31
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 15:09:00 2025 +0100
    
        Add array aggregator
    
    commit 7f4a33eb502f6e7f7eea2d6d55e9e55ac39bf147
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:52:21 2025 +0100
    
        Fix test
    
    commit 97d8de0a0bf27886c202cc6fb648f8cd8c38394c
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:51:59 2025 +0100
    
        Fix aggregator
    
    commit c525553ff72a139e68076e3d68fc704c31e23428
    Merge: ee915701f b483d786a
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:50:56 2025 +0100
    
        Merge branch 'PIN-6091_add-delegation-splitter' into PIN-6102_delegation-aggregator
    
    commit ee915701f889ffd5cd22821c018d6673ca63eb67
    Merge: 5dcb7597f 9fa0a59e0
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Thu Feb 20 12:35:51 2025 +0100
    
        Merge branch 'PIN-6091_add-delegation-splitter' into PIN-6102_delegation-aggregator
    
    commit 5dcb7597f4779b621541e40beea9f23fe281a5dc
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 18:06:42 2025 +0100
    
        Add delegation aggregator tests
    
    commit c0de07c2017bf8fa362aa0cc21b29827bf1ac015
    Author: Stefano Hu <76391491+shuyec@users.noreply.github.com>
    Date:   Wed Feb 19 18:06:19 2025 +0100
    
        Fix optional properties in delegation aggregator
    
    commit 39f3adb6ce731bbbd636ace850c7bcd7010ec807
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Wed Feb 19 16:02:19 2025 +0100
    
        Fix import
    
    commit c4c52e7bc65f8a438146a155aaa305eb7241d8b1
    Merge: e3b646e5e 73726411c
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Wed Feb 19 16:01:31 2025 +0100
    
        Merge branch 'PIN-6091_add-delegation-splitter' into PIN-6102_delegation-aggregator
    
    commit e3b646e5e69d88809898e61deea8c2a3011bda0c
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Mon Feb 17 15:29:44 2025 +0100
    
        Add delegation aggregator
    
    commit a644a8dc731b8045d90af4014371726c76ed6586
    Author: Roberto Taglioni <roberto.taglioni@pagopa.it>
    Date:   Mon Feb 17 14:57:36 2025 +0100
    
        Add todo

---
+++ packages/readmodel/src/delegation/aggregators.ts
@@ -0,0 +1,169 @@
+import {
+  Delegation,
+  DelegationContractDocument,
+  DelegationContractId,
+  delegationContractKind,
+  DelegationId,
+  DelegationKind,
+  DelegationStamp,
+  delegationStampKind,
+  DelegationState,
+  EServiceId,
+  genericInternalError,
+  stringToDate,
+  TenantId,
+  unsafeBrandId,
+  UserId,
+  WithMetadata,
+} from "pagopa-interop-models";
+import {
+  DelegationSQL,
+  DelegationStampSQL,
+  DelegationContractDocumentSQL,
+} from "pagopa-interop-readmodel-models";
+
+export const aggregateDelegationArray = ({
+  delegationSQL,
+  delegationStampsSQL,
+  delegationContractDocumentsSQL,
+}: {
+  delegationSQL: DelegationSQL[];
+  delegationStampsSQL: DelegationStampSQL[];
+  delegationContractDocumentsSQL: DelegationContractDocumentSQL[];
+}): Array<WithMetadata<Delegation>> =>
+  delegationSQL.map((delegationSQL) =>
+    aggregateDelegation({
+      delegationSQL,
+      delegationStampsSQL: delegationStampsSQL.filter(
+        (stampSQL) => stampSQL.delegationId === delegationSQL.id
+      ),
+      delegationContractDocumentsSQL: delegationContractDocumentsSQL.filter(
+        (docSQL) => docSQL.delegationId === delegationSQL.id
+      ),
+    })
+  );
+
+export const aggregateDelegation = ({
+  delegationSQL,
+  delegationStampsSQL,
+  delegationContractDocumentsSQL,
+}: {
+  delegationSQL: DelegationSQL;
+  delegationStampsSQL: DelegationStampSQL[];
+  delegationContractDocumentsSQL: DelegationContractDocumentSQL[];
+}): WithMetadata<Delegation> => {
+  const activationContractDocumentSQL = delegationContractDocumentsSQL.find(
+    (contractDoc) => contractDoc.kind === delegationContractKind.activation
+  );
+  const activationContract: DelegationContractDocument | undefined =
+    activationContractDocumentSQL
+      ? delegationContractDocumentSQLToDelegationContractDocument(
+          activationContractDocumentSQL
+        )
+      : undefined;
+
+  const revocationContractDocumentSQL = delegationContractDocumentsSQL.find(
+    (contractDoc) => contractDoc.kind === delegationContractKind.revocation
+  );
+
+  const revocationContract: DelegationContractDocument | undefined =
+    revocationContractDocumentSQL
+      ? delegationContractDocumentSQLToDelegationContractDocument(
+          revocationContractDocumentSQL
+        )
+      : undefined;
+
+  const submissionStampSQL = delegationStampsSQL.find(
+    (stamp) => stamp.kind === delegationStampKind.submission
+  );
+
+  if (!submissionStampSQL) {
+    throw genericInternalError("submissions stamp can't be missing");
+  }
+
+  const activationStampSQL = delegationStampsSQL.find(
+    (stamp) => stamp.kind === delegationStampKind.activation
+  );
+  const rejectionStampSQL = delegationStampsSQL.find(
+    (stamp) => stamp.kind === delegationStampKind.rejection
+  );
+  const revocationStampSQL = delegationStampsSQL.find(
+    (stamp) => stamp.kind === delegationStampKind.revocation
+  );
+
+  const delegation: Delegation = {
+    id: unsafeBrandId<DelegationId>(delegationSQL.id),
+    createdAt: stringToDate(delegationSQL.createdAt),
+    updatedAt: stringToDate(delegationSQL.updatedAt),
+    eserviceId: unsafeBrandId<EServiceId>(delegationSQL.eserviceId),
+    state: DelegationState.parse(delegationSQL.state),
+    kind: DelegationKind.parse(delegationSQL.kind),
+    delegatorId: unsafeBrandId<TenantId>(delegationSQL.delegatorId),
+    delegateId: unsafeBrandId<TenantId>(delegationSQL.delegateId),
+    stamps: {
+      submission: stampSQLToStamp(submissionStampSQL),
+      ...(activationStampSQL
+        ? { activation: stampSQLToStamp(activationStampSQL) }
+        : {}),
+      ...(rejectionStampSQL
+        ? { rejection: stampSQLToStamp(rejectionStampSQL) }
+        : {}),
+      ...(revocationStampSQL
+        ? { revocation: stampSQLToStamp(revocationStampSQL) }
+        : {}),
+    },
+    ...(activationContract ? { activationContract } : {}),
+    ...(revocationContract ? { revocationContract } : {}),
+    ...(delegationSQL.rejectionReason
+      ? { rejectionReason: delegationSQL.rejectionReason }
+      : {}),
+  };
+  return {
+    data: delegation,
+    metadata: {
+      version: 1,
+    },
+  };
+};
+
+export const aggregateDelegationsArray = ({
+  delegationsSQL,
+  delegationStampsSQL,
+  delegationContractDocumentsSQL,
+}: {
+  delegationsSQL: DelegationSQL[];
+  delegationStampsSQL: DelegationStampSQL[];
+  delegationContractDocumentsSQL: DelegationContractDocumentSQL[];
+}): Array<WithMetadata<Delegation>> =>
+  delegationsSQL.map((delegationSQL) => {
+    const stampsSQLOfCurrentDelegation = delegationStampsSQL.filter(
+      (stampSQL) => stampSQL.delegationId === delegationSQL.id
+    );
+
+    const contractDocumentsSQLOfCurrentDelegation =
+      delegationContractDocumentsSQL.filter(
+        (docSQL) => docSQL.delegationId === delegationSQL.id
+      );
+
+    return aggregateDelegation({
+      delegationSQL,
+      delegationStampsSQL: stampsSQLOfCurrentDelegation,
+      delegationContractDocumentsSQL: contractDocumentsSQLOfCurrentDelegation,
+    });
+  });
+
+const delegationContractDocumentSQLToDelegationContractDocument = (
+  delegationContractDocument: DelegationContractDocumentSQL
+): DelegationContractDocument => ({
+  id: unsafeBrandId<DelegationContractId>(delegationContractDocument.id),
+  path: delegationContractDocument.path,
+  name: delegationContractDocument.name,
+  prettyName: delegationContractDocument.prettyName,
+  contentType: delegationContractDocument.contentType,
+  createdAt: stringToDate(delegationContractDocument.createdAt),
+});
+
+const stampSQLToStamp = (stampSQL: DelegationStampSQL): DelegationStamp => ({
+  who: unsafeBrandId<UserId>(stampSQL.who),
+  when: stringToDate(stampSQL.when),
+});
