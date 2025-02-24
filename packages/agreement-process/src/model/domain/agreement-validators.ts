d94dd4506bba57573562a8e3f2320850fd037c04
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2025-02-24 17:47:55 +0100

    fix: remove pending as valid state for agreement document changes

--- packages/agreement-process/src/model/domain/agreement-validators.ts
+++ packages/agreement-process/src/model/domain/agreement-validators.ts
@@ -127,7 +127,6 @@ export const agreementSubmissionConflictingStates: AgreementState[] = [
 
 export const agreementConsumerDocumentChangeValidStates: AgreementState[] = [
   agreementState.draft,
-  agreementState.pending,
 ];
 
 /* ========= ASSERTIONS ========= */
