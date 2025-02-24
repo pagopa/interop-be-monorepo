44885d5775622ebedb5eda97de18e47f1b39684f
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-02-24 09:56:07 +0100

    Add client aggregator

---
+++ packages/readmodel/src/authorization/clientAggregators.ts
@@ -0,0 +1,65 @@
+import {
+  Client,
+  ClientId,
+  ClientKind,
+  Key,
+  KeyUse,
+  PurposeId,
+  stringToDate,
+  TenantId,
+  unsafeBrandId,
+  UserId,
+  WithMetadata,
+} from "pagopa-interop-models";
+import {
+  ClientKeySQL,
+  ClientPurposeSQL,
+  ClientSQL,
+  ClientUserSQL,
+} from "pagopa-interop-readmodel-models";
+
+export const clientSQLToClient = (
+  {
+    id,
+    metadataVersion,
+    consumerId,
+    name,
+    description,
+    kind,
+    createdAt,
+  }: ClientSQL,
+  clientUsersSQL: ClientUserSQL[],
+  clientPurposesSQL: ClientPurposeSQL[],
+  clientKeysSQL: ClientKeySQL[]
+): WithMetadata<Client> => {
+  const users: UserId[] = clientUsersSQL.map((u) =>
+    unsafeBrandId<UserId>(u.userId)
+  );
+  const purposes: PurposeId[] = clientPurposesSQL.map((p) =>
+    unsafeBrandId<PurposeId>(p.purposeId)
+  );
+  const keys: Key[] = clientKeysSQL.map((k) => ({
+    userId: unsafeBrandId<UserId>(k.userId),
+    kid: k.kid,
+    name: k.name,
+    encodedPem: k.encodedPem,
+    algorithm: k.algorithm,
+    use: KeyUse.parse(k.use),
+    createdAt: stringToDate(k.createdAt),
+  }));
+
+  return {
+    data: {
+      id: unsafeBrandId<ClientId>(id),
+      consumerId: unsafeBrandId<TenantId>(consumerId),
+      name,
+      purposes,
+      description: description || undefined,
+      users,
+      kind: ClientKind.parse(kind),
+      createdAt: stringToDate(createdAt),
+      keys,
+    },
+    metadata: { version: metadataVersion },
+  };
+};
