5fce14e2064a3dac791b1ed3fd9a07bf1fa5d70d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-02-24 10:03:48 +0100

    Add aggregator

---
+++ packages/readmodel/src/attribute/aggregators.ts
@@ -0,0 +1,31 @@
+import {
+  Attribute,
+  AttributeId,
+  AttributeKind,
+  stringToDate,
+  unsafeBrandId,
+  WithMetadata,
+} from "pagopa-interop-models";
+import { AttributeSQL } from "pagopa-interop-readmodel-models";
+
+export const attributeSQLtoAttribute = ({
+  id,
+  metadataVersion,
+  name,
+  kind,
+  description,
+  creationTime,
+  origin,
+  code,
+}: AttributeSQL): WithMetadata<Attribute> => ({
+  data: {
+    id: unsafeBrandId<AttributeId>(id),
+    name,
+    kind: AttributeKind.parse(kind),
+    description,
+    creationTime: stringToDate(creationTime),
+    ...(origin ? { origin } : {}),
+    ...(code ? { code } : {}),
+  },
+  metadata: { version: metadataVersion },
+});
---
+++ packages/readmodel/test/attributeAggregator.test.ts
@@ -0,0 +1,35 @@
+import { getMockAttribute } from "pagopa-interop-commons-test/index.js";
+import { describe, it, expect } from "vitest";
+import { Attribute, WithMetadata } from "pagopa-interop-models";
+import { splitAttributeIntoObjectsSQL } from "./../src/attribute/splitters.js";
+import { attributeSQLtoAttribute } from "./../src/attribute/aggregators.js";
+
+describe("Attribute aggregator", () => {
+  it("should convert an Attribute SQL object into a business logic Attribute", () => {
+    const mockAttribute: WithMetadata<Attribute> = {
+      data: {
+        ...getMockAttribute(),
+        origin: "alfa",
+        code: "beta",
+      },
+      metadata: { version: 1 },
+    };
+    const attributeSQL = splitAttributeIntoObjectsSQL(mockAttribute.data, 1);
+    const aggregatedAttribute = attributeSQLtoAttribute(attributeSQL);
+
+    expect(aggregatedAttribute).toMatchObject(mockAttribute);
+  });
+
+  it("should convert an Attribute SQL object with null values into a business logic Attribute object with undefined values", () => {
+    const attribute: WithMetadata<Attribute> = {
+      data: {
+        ...getMockAttribute(),
+      },
+      metadata: { version: 1 },
+    };
+    const attributeSQL = splitAttributeIntoObjectsSQL(attribute.data, 1);
+    const aggregatedAttribute = attributeSQLtoAttribute(attributeSQL);
+
+    expect(aggregatedAttribute).toMatchObject(attribute);
+  });
+});
