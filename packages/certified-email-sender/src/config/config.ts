c07228144c1a7733f8021f6530320fea0aeeb3b4
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2025-02-24 15:34:14 +0100

    fix

--- packages/certified-email-sender/src/config/config.ts
+++ packages/certified-email-sender/src/config/config.ts
@@ -8,12 +8,10 @@ import { z } from "zod";
 
 export const PECEmailSenderConfig = z
   .object({
-    INTEROP_FE_BASE_URL: z.string(),
     PEC_SENDER_MAIL: z.string().email(),
     PEC_SENDER_LABEL: z.string(),
   })
   .transform((c) => ({
-    interopFeBaseUrl: c.INTEROP_FE_BASE_URL,
     pecSenderMail: c.PEC_SENDER_MAIL,
     pecSenderLabel: c.PEC_SENDER_LABEL,
   }));
