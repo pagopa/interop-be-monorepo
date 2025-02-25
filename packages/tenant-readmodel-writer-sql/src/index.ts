d3215e1e28eeaf7615be143f8610463e4dc6313a
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-02-25 17:50:58 +0100

    Update service name

--- packages/tenant-readmodel-writer-sql/src/index.ts
+++ packages/tenant-readmodel-writer-sql/src/index.ts
@@ -26,7 +26,7 @@ async function processMessage({
   const decodedMessage = decodeKafkaMessage(message, TenantEvent);
 
   const loggerInstance = logger({
-    serviceName: "tenant-readmodel-writer",
+    serviceName: "tenant-readmodel-writer-sql",
     eventType: decodedMessage.type,
     eventVersion: decodedMessage.event_version,
     streamId: decodedMessage.stream_id,
