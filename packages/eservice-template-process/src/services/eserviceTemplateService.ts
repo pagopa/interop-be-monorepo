f3f5c2ac68b328cbed9ad5d1441901bf30f70387
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-02-25 15:22:18 +0100

    PIN-6209 - Get catalog e-service template creators (#1545)

--- packages/eservice-template-process/src/services/eserviceTemplateService.ts
+++ packages/eservice-template-process/src/services/eserviceTemplateService.ts
@@ -1379,6 +1379,17 @@ export function eserviceTemplateServiceBuilder(
         limit,
       });
     },
+    async getEServiceTemplateCreators(
+      creatorName: string | undefined,
+      limit: number,
+      offset: number,
+      { logger }: WithLogger<AppContext>
+    ): Promise<ListResult<eserviceTemplateApi.CompactOrganization>> {
+      logger.info(
+        `Retrieving producers from agreements with producer name ${creatorName}, limit ${limit}, offset ${offset}`
+      );
+      return await readModelService.getCreators(creatorName, limit, offset);
+    },
     async createEServiceTemplateDocument(
       eserviceTemplateId: EServiceTemplateId,
       eserviceTemplateVersionId: EServiceTemplateVersionId,
