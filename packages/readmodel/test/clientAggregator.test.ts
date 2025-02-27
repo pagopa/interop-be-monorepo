4f62392dd0e9c16b8ce4f78de022da53a6466096
Paolo Manca <paolo.manca@pagopa.it>
2025-02-27 19:13:36 +0100

    fix name aggregateClientSQL

--- packages/readmodel/test/clientAggregator.test.ts
+++ packages/readmodel/test/clientAggregator.test.ts
@@ -2,7 +2,7 @@ import { getMockClient, getMockKey } from "pagopa-interop-commons-test";
 import { Client, generateId, WithMetadata } from "pagopa-interop-models";
 import { describe, it, expect } from "vitest";
 import { splitClientIntoObjectsSQL } from "../src/authorization/clientSplitters.js";
-import { clientSQLToClient } from "../src/authorization/clientAggregators.js";
+import { aggregateClientSQL } from "../src/authorization/clientAggregators.js";
 
 describe("Client aggregator", () => {
   it("should convert client SQL objects into a business logic client", () => {
@@ -21,7 +21,7 @@ describe("Client aggregator", () => {
     const { clientSQL, usersSQL, purposesSQL, keysSQL } =
       splitClientIntoObjectsSQL(client.data, 1);
 
-    const aggregatedClient = clientSQLToClient({
+    const aggregatedClient = aggregateClientSQL({
       clientSQL,
       usersSQL,
       purposesSQL,
