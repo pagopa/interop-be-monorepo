c91534f2905661f835bb4cff7d8e467436a9d91f
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-03-03 12:06:08 +0100

    Refactor

--- packages/readmodel-checker/src/index.ts
+++ packages/readmodel-checker/src/index.ts
@@ -1,8 +1,6 @@
 /* eslint-disable no-console */
 import { z } from "zod";
-import isEqual from "lodash.isequal";
-import { diff } from "json-diff";
-import { CorrelationId, generateId, WithMetadata } from "pagopa-interop-models";
+import { CorrelationId, generateId } from "pagopa-interop-models";
 import {
   logger,
   ReadModelDbConfig,
@@ -13,18 +11,13 @@ import { readModelServiceBuilderSQL } from "pagopa-interop-readmodel";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { readModelServiceBuilder } from "./services/readModelService.js";
+import { compare } from "./utils.js";
 
 const Config = ReadModelDbConfig.and(ReadModelSQLDbConfig);
 
 type Config = z.infer<typeof Config>;
 
 const config: Config = Config.parse(process.env);
-
-function getIdentificationKey<T extends { id: string } | { kid: string }>(
-  obj: T
-): string {
-  return "id" in obj ? obj.id : obj.kid;
-}
 
 const readModelRepository = ReadModelRepository.init(config);
 
@@ -48,76 +41,16 @@ const readModelServiceSQL = readModelServiceBuilderSQL(readModelDB);
 // const attributeReadModelServiceSQL =
 //   attributeReadModelServiceBuilderSQL(readModelDB);
 
-function compare<T extends { id: string } | { kid: string }>(
-  collectionItems: Array<WithMetadata<T>>,
-  postgresItems: Array<WithMetadata<T>>,
-  schema: string
-): void {
-  if (postgresItems.length === 0) {
-    loggerInstance.info(
-      `Skipping checks for ${schema} schema because it's empty`
-    );
-  }
-
-  const allIds = new Set(
-    [...collectionItems, ...postgresItems].map((d) =>
-      getIdentificationKey(d.data)
-    )
-  );
-  const pairs = Array.from(allIds)
-    .map((id) => [
-      collectionItems.find((d) => getIdentificationKey(d.data) === id),
-      postgresItems.find((d) => getIdentificationKey(d.data) === id),
-    ])
-    .filter(([a, b]) => !isEqual(a, b));
-
-  // eslint-disable-next-line functional/no-let
-  let differencesCount = 0;
-  pairs.forEach(([itemFromCollection, itemFromSQL]) => {
-    if (itemFromCollection && !itemFromSQL) {
-      differencesCount++;
-      console.warn(
-        `Object with id ${getIdentificationKey(
-          itemFromCollection.data
-        )} not found in SQL readmodel`
-      );
-    }
-    if (!itemFromCollection && itemFromSQL) {
-      differencesCount++;
-      console.warn(
-        `Object with id ${getIdentificationKey(
-          itemFromSQL.data
-        )} not found in collection`
-      );
-    }
-    if (itemFromCollection && itemFromSQL) {
-      const objectsDiff = diff(itemFromCollection, itemFromSQL, { sort: true });
-      if (objectsDiff) {
-        differencesCount++;
-        console.warn(
-          `Differences in object with id ${getIdentificationKey(
-            itemFromCollection.data
-          )}`
-        );
-        console.warn(JSON.stringify(objectsDiff, null, 2));
-      }
-    }
-  });
-
-  if (differencesCount > 0) {
-    loggerInstance.error(
-      `Differences count for ${schema}: ${differencesCount}`
-    );
-  } else {
-    loggerInstance.info(`No differences found for ${schema}`);
-  }
-}
-
 async function main(): Promise<void> {
   const eservices = await readModelService.getAllReadModelEServices();
   const eservicesPostgres = await readModelServiceSQL.getAllEServices();
 
-  compare(eservices, eservicesPostgres, "eservices");
+  compare({
+    collectionItems: eservices,
+    postgresItems: eservicesPostgres,
+    schema: "eservices",
+    loggerInstance,
+  });
 
   // TODO
   // const attributes = await readModelService.getAllReadModelAttributes();
@@ -125,19 +58,6 @@ async function main(): Promise<void> {
   //   await attributeReadModelServiceSQL.getAllAttributes();
 
   // compare(attributes, attributesPostgres, "attributes");
-}
-
-export function zipDataById<T extends { id: string } | { kid: string }>(
-  dataA: Array<WithMetadata<T>>,
-  dataB: Array<WithMetadata<T>>
-): Array<[WithMetadata<T> | undefined, WithMetadata<T> | undefined]> {
-  const allIds = new Set(
-    [...dataA, ...dataB].map((d) => getIdentificationKey(d.data))
-  );
-  return Array.from(allIds).map((id) => [
-    dataA.find((d) => getIdentificationKey(d.data) === id),
-    dataB.find((d) => getIdentificationKey(d.data) === id),
-  ]);
 }
 
 await main();
---
+++ packages/readmodel-checker/src/utils.ts
@@ -0,0 +1,99 @@
+/* eslint-disable no-console */
+import { diff } from "json-diff";
+import isEqual from "lodash.isequal";
+import { Logger } from "pagopa-interop-commons";
+import { WithMetadata } from "pagopa-interop-models";
+
+function getIdentificationKey<T extends { id: string } | { kid: string }>(
+  obj: T
+): string {
+  return "id" in obj ? obj.id : obj.kid;
+}
+
+export function zipDataById<T extends { id: string } | { kid: string }>(
+  dataA: Array<WithMetadata<T>>,
+  dataB: Array<WithMetadata<T>>
+): Array<[WithMetadata<T> | undefined, WithMetadata<T> | undefined]> {
+  const allIds = new Set(
+    [...dataA, ...dataB].map((d) => getIdentificationKey(d.data))
+  );
+  return Array.from(allIds).map((id) => [
+    dataA.find((d) => getIdentificationKey(d.data) === id),
+    dataB.find((d) => getIdentificationKey(d.data) === id),
+  ]);
+}
+
+export function compare<T extends { id: string } | { kid: string }>({
+  collectionItems,
+  postgresItems,
+  schema,
+  loggerInstance,
+}: {
+  collectionItems: Array<WithMetadata<T>>;
+  postgresItems: Array<WithMetadata<T>>;
+  schema: string;
+  loggerInstance: Logger;
+}): number {
+  if (postgresItems.length === 0) {
+    loggerInstance.info(
+      `Skipping checks for ${schema} schema because it's empty`
+    );
+
+    return -1;
+  }
+
+  const allIds = new Set(
+    [...collectionItems, ...postgresItems].map((d) =>
+      getIdentificationKey(d.data)
+    )
+  );
+  const pairs = Array.from(allIds)
+    .map((id) => [
+      collectionItems.find((d) => getIdentificationKey(d.data) === id),
+      postgresItems.find((d) => getIdentificationKey(d.data) === id),
+    ])
+    .filter(([a, b]) => !isEqual(a, b));
+
+  // eslint-disable-next-line functional/no-let
+  let differencesCount = 0;
+  pairs.forEach(([itemFromCollection, itemFromSQL]) => {
+    if (itemFromCollection && !itemFromSQL) {
+      differencesCount++;
+      console.warn(
+        `Object with id ${getIdentificationKey(
+          itemFromCollection.data
+        )} not found in SQL readmodel`
+      );
+    }
+    if (!itemFromCollection && itemFromSQL) {
+      differencesCount++;
+      console.warn(
+        `Object with id ${getIdentificationKey(
+          itemFromSQL.data
+        )} not found in collection`
+      );
+    }
+    if (itemFromCollection && itemFromSQL) {
+      const objectsDiff = diff(itemFromCollection, itemFromSQL, { sort: true });
+      if (objectsDiff) {
+        differencesCount++;
+        console.warn(
+          `Differences in object with id ${getIdentificationKey(
+            itemFromCollection.data
+          )}`
+        );
+        console.warn(JSON.stringify(objectsDiff, null, 2));
+      }
+    }
+  });
+
+  if (differencesCount > 0) {
+    loggerInstance.error(
+      `Differences count for ${schema}: ${differencesCount}`
+    );
+  } else {
+    loggerInstance.info(`No differences found for ${schema}`);
+  }
+
+  return differencesCount;
+}
