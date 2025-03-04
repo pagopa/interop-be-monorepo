5a82a4108d345a864fc46f39148ddf9c8ac0fc87
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-03-04 17:07:24 +0100

    Remove undefined properties from aggregator test

--- packages/readmodel/test/eserviceAggregator.test.ts
+++ packages/readmodel/test/eserviceAggregator.test.ts
@@ -104,24 +104,12 @@ describe("E-service aggregator", () => {
         verified: [],
       },
       docs: [doc],
-      interface: undefined,
-      rejectionReasons: undefined,
-      description: undefined,
-      // TODO the optional fields should not be set, otherwise they are expected in the result. Instead, the result will not fill these if the entries have null in db
-      // publishedAt: undefined,
-      // suspendedAt: undefined,
-      // deprecatedAt: undefined,
-      // archivedAt: undefined,
-      agreementApprovalPolicy: undefined,
     };
 
     const eservice: EService = {
       ...getMockEService(),
       descriptors: [descriptor],
       riskAnalysis: [riskAnalysis1, riskAnalysis2],
-      isSignalHubEnabled: undefined,
-      isClientAccessDelegable: undefined,
-      isConsumerDelegable: undefined,
     };
 
     const {
