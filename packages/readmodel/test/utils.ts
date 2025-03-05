7ffe2476a05273ef58b954709c13dcd8d26ada5d
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-03-05 15:26:40 +0100

    Add attribute queries tests

--- packages/readmodel/test/utils.ts
+++ packages/readmodel/test/utils.ts
@@ -1,5 +1,7 @@
 import { setupTestContainersVitest } from "pagopa-interop-commons-test";
 import { afterEach, inject } from "vitest";
+import { stringToDate } from "pagopa-interop-models";
+import { attributeReadModelServiceBuilderSQL } from "../src/attributeReadModelServiceSQL.js";
 
 export const { cleanup, readModelDB } = await setupTestContainersVitest(
   undefined,
@@ -12,3 +14,12 @@ export const { cleanup, readModelDB } = await setupTestContainersVitest(
 );
 
 afterEach(cleanup);
+
+export const attributeReadModelService =
+  attributeReadModelServiceBuilderSQL(readModelDB);
+
+export function stringToISOString(input: string): string;
+export function stringToISOString(input: string | null): string | null;
+export function stringToISOString(input: string | null): string | null {
+  return input ? stringToDate(input).toISOString() : null;
+}
