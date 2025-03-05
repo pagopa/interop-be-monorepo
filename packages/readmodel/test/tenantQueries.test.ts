6c17a3a8ddff323846d80dcf305e3753a3100dc9
Paolo Manca <paolo.manca@pagopa.it>
2025-03-05 16:40:59 +0100

    scaffolding test for tenant queries

---
+++ packages/readmodel/test/tenantQueries.test.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+import { describe, it, expect } from "vitest";
+
+describe("Tenant Queris", () => {
+  describe("Upsert", () => {
+    it("should do something", () => {
+      expect(1).toEqual(1);
+    });
+  });
+});
