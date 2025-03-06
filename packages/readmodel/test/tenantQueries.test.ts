260a8c833f6059f24e6cf3f2de1efb378ab45aef
Paolo Manca <paolo.manca@pagopa.it>
2025-03-06 15:12:45 +0100

    test getById, tenant not found  for tenant queries

--- packages/readmodel/test/tenantQueries.test.ts
+++ packages/readmodel/test/tenantQueries.test.ts
@@ -1,38 +1,185 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
+import {
+  getMockTenantMail,
+  getMockCertifiedTenantAttribute,
+  getMockDeclaredTenantAttribute,
+  getMockVerifiedTenantAttribute,
+  getMockTenant,
+} from "pagopa-interop-commons-test/index.js";
+import {
+  TenantMail,
+  CertifiedTenantAttribute,
+  generateId,
+  DelegationId,
+  DeclaredTenantAttribute,
+  TenantVerifier,
+  TenantRevoker,
+  VerifiedTenantAttribute,
+  TenantFeatureCertifier,
+  tenantFeatureType,
+  TenantFeatureDelegatedConsumer,
+  TenantFeatureDelegatedProducer,
+  ExternalId,
+  Tenant,
+  tenantKind,
+  tenantUnitType,
+  WithMetadata,
+  TenantId,
+} from "pagopa-interop-models";
 import { describe, it, expect } from "vitest";
+import { tenantReadModelService } from "./utils.js";
 
 describe("Tenant Queries", () => {
   describe("Upsert Tenant", () => {
-    it("should add a complete (*all* fields) tenant", () => {
+    it.skip("should add a complete (*all* fields) tenant", () => {
       expect(1).toEqual(0);
     });
-    it("should add an incomplete (*only* mandatory fields) tenant", () => {
+    it.skip("should add an incomplete (*only* mandatory fields) tenant", () => {
       expect(1).toEqual(0);
     });
-    it("should update a complete (*all* fields) tenant", () => {
+    it.skip("should update a complete (*all* fields) tenant", () => {
       expect(1).toEqual(0);
     });
   });
   describe("Get a Tenant", () => {
-    it("should get a tenant from a tenantId", () => {
-      expect(1).toEqual(0);
+    it("should get a tenant from a tenantId", async () => {
+      const tenantForVerifying: WithMetadata<Tenant> = {
+        data: {
+          ...getMockTenant(),
+        },
+        metadata: { version: 1 },
+      };
+      const tenantForRevoking: WithMetadata<Tenant> = {
+        data: {
+          ...getMockTenant(),
+        },
+        metadata: { version: 1 },
+      };
+      const delegationId = generateId<DelegationId>();
+      const tenantVerifier: TenantVerifier = {
+        id: tenantForVerifying.data.id,
+        verificationDate: new Date(),
+        expirationDate: new Date(),
+        extensionDate: new Date(),
+        delegationId,
+      };
+      const tenantRevoker: TenantRevoker = {
+        id: tenantForRevoking.data.id,
+        verificationDate: new Date(),
+        revocationDate: new Date(),
+        expirationDate: new Date(),
+        extensionDate: new Date(),
+        delegationId,
+      };
+
+      const tenantMail: TenantMail = {
+        ...getMockTenantMail(),
+        description: "mail description",
+      };
+      const tenantCertifiedAttribute: CertifiedTenantAttribute = {
+        ...getMockCertifiedTenantAttribute(),
+        assignmentTimestamp: new Date(),
+        revocationTimestamp: new Date(),
+      };
+
+      const tenantDeclaredAttribute: DeclaredTenantAttribute = {
+        ...getMockDeclaredTenantAttribute(),
+        assignmentTimestamp: new Date(),
+        revocationTimestamp: new Date(),
+        delegationId,
+      };
+
+      const tenantVerifiedAttribute: VerifiedTenantAttribute = {
+        ...getMockVerifiedTenantAttribute(),
+        verifiedBy: [tenantVerifier],
+        revokedBy: [tenantRevoker],
+        assignmentTimestamp: new Date(),
+      };
+
+      const tenantFeatureCertifier: TenantFeatureCertifier = {
+        type: tenantFeatureType.persistentCertifier,
+        certifierId: generateId(),
+      };
+
+      const tenantFeatureDelegatedConsumer: TenantFeatureDelegatedConsumer = {
+        type: tenantFeatureType.delegatedConsumer,
+        availabilityTimestamp: new Date(),
+      };
+
+      const tenantFeatureDelegatedProducer: TenantFeatureDelegatedProducer = {
+        type: tenantFeatureType.delegatedProducer,
+        availabilityTimestamp: new Date(),
+      };
+
+      const selfcareId = generateId();
+
+      const externalId: ExternalId = {
+        origin: "IPA",
+        value: generateId(),
+      };
+      const tenant: WithMetadata<Tenant> = {
+        data: {
+          ...getMockTenant(),
+          selfcareId,
+          kind: tenantKind.PA,
+          subUnitType: tenantUnitType.AOO,
+          externalId,
+          updatedAt: new Date(),
+          mails: [tenantMail],
+          attributes: [
+            tenantCertifiedAttribute,
+            tenantDeclaredAttribute,
+            tenantVerifiedAttribute,
+          ],
+          features: [
+            tenantFeatureDelegatedProducer,
+            tenantFeatureDelegatedConsumer,
+            tenantFeatureCertifier,
+          ],
+        },
+        metadata: { version: 1 },
+      };
+
+      await tenantReadModelService.upsertTenant(tenantForVerifying);
+      await tenantReadModelService.upsertTenant(tenantForRevoking);
+      await tenantReadModelService.upsertTenant(tenant);
+
+      const retrievedTenant = await tenantReadModelService.getTenantById(
+        tenant.data.id
+      );
+      // checking features: doing a check on array features separately otherwise toMatchObject fails on array equality
+      expect(retrievedTenant?.data.features).toEqual(
+        expect.arrayContaining(tenant?.data.features)
+      );
+      // overwriting features in expectedTenant so toMatchObject dont fails to check array equality
+      const expectedTenant = {
+        ...tenant,
+        data: { features: retrievedTenant?.data.features },
+      };
+      expect(retrievedTenant).toMatchObject(expectedTenant);
     });
-    it("should *not* get a tenant from a tenantId", () => {
-      expect(1).toEqual(0);
+    it("should *not* get a tenant from a tenantId", async () => {
+      const tenantId = generateId<TenantId>();
+      const retrievedTenant = await tenantReadModelService.getTenantById(
+        tenantId
+      );
+
+      expect(retrievedTenant).toBeUndefined();
     });
   });
   describe("Get all Tenants", () => {
-    it("should get all tenants", () => {
+    it.skip("should get all tenants", () => {
       expect(1).toEqual(0);
     });
-    it("should *not* get any tenants", () => {
-      expect(1).toEqual(0);
+    it("should *not* get any tenants", async () => {
+      const retrievedTenants = await tenantReadModelService.getAllTenants();
+      expect(retrievedTenants).toHaveLength(0);
     });
   });
   describe("Delete a Tenant", () => {
-    it("should delete a tenant from a tenantId", () => {
+    it.skip("should delete a tenant from a tenantId", () => {
       expect(1).toEqual(0);
     });
   });
--- packages/readmodel/test/utils.ts
+++ packages/readmodel/test/utils.ts
@@ -1,5 +1,10 @@
 import { setupTestContainersVitest } from "pagopa-interop-commons-test";
 import { afterEach, inject } from "vitest";
+import {
+  tenantVerifiedAttributeVerifierInReadmodelTenant,
+  tenantVerifiedAttributeRevokerInReadmodelTenant,
+} from "pagopa-interop-readmodel-models";
+import { tenantReadModelServiceBuilderSQL } from "../src/tenantReadModelServiceSQL.js";
 
 export const { cleanup, readModelDB } = await setupTestContainersVitest(
   undefined,
@@ -11,4 +16,17 @@ export const { cleanup, readModelDB } = await setupTestContainersVitest(
   inject("readModelSQLConfig")
 );
 
-afterEach(cleanup);
+afterEach(async () => {
+  /*
+  TODO the entries in tenant_verified_attribute_verifier and tenant_verified_attribute_revoker have a reference on the tenant table,
+  but this reference donesn't have on delete cascade. During the cleanup of the test, those entries block the deletion of tenants (for example the verifier) from the tenant table because they are still referred.
+  This problem could be fixed if we add ON DELETE CASCADE but I am not sure if that could bring to an unwanted behavior
+*/
+  await readModelDB.delete(tenantVerifiedAttributeVerifierInReadmodelTenant);
+  await readModelDB.delete(tenantVerifiedAttributeRevokerInReadmodelTenant);
+
+  await cleanup();
+});
+
+export const tenantReadModelService =
+  tenantReadModelServiceBuilderSQL(readModelDB);
