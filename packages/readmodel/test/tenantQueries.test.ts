098ae020275f149350b9e6bceebc9e4cbd5050eb
Paolo Manca <paolo.manca@pagopa.it>
2025-03-06 11:43:41 +0100

    scaffolding test for tenant queries

--- packages/readmodel/test/tenantQueries.test.ts
+++ packages/readmodel/test/tenantQueries.test.ts
@@ -3,10 +3,37 @@
 
 import { describe, it, expect } from "vitest";
 
-describe("Tenant Queris", () => {
-  describe("Upsert", () => {
-    it("should do something", () => {
-      expect(1).toEqual(1);
+describe("Tenant Queries", () => {
+  describe("Upsert Tenant", () => {
+    it("should add a complete (*all* fields) tenant", () => {
+      expect(1).toEqual(0);
+    });
+    it("should add an incomplete (*only* mandatory fields) tenant", () => {
+      expect(1).toEqual(0);
+    });
+    it("should update a complete (*all* fields) tenant", () => {
+      expect(1).toEqual(0);
+    });
+  });
+  describe("Get a Tenant", () => {
+    it("should get a tenant from a tenantId", () => {
+      expect(1).toEqual(0);
+    });
+    it("should *not* get a tenant from a tenantId", () => {
+      expect(1).toEqual(0);
+    });
+  });
+  describe("Get all Tenants", () => {
+    it("should get all tenants", () => {
+      expect(1).toEqual(0);
+    });
+    it("should *not* get any tenants", () => {
+      expect(1).toEqual(0);
+    });
+  });
+  describe("Delete a Tenant", () => {
+    it("should delete a tenant from a tenantId", () => {
+      expect(1).toEqual(0);
     });
   });
 });
