17a3037ae42b169eb325c7323575f2ee26cf3a43
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-03-14 11:11:41 +0100

    Refactor

--- packages/readmodel-checker/src/services/readModelServiceSQL.ts
+++ packages/readmodel-checker/src/services/readModelServiceSQL.ts
@@ -1,7 +1,22 @@
+import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Attribute, WithMetadata } from "pagopa-interop-models";
-import { aggregateAttributeArray } from "pagopa-interop-readmodel";
-import { attributeInReadmodelAttribute } from "pagopa-interop-readmodel-models";
+import { Attribute, EService, WithMetadata } from "pagopa-interop-models";
+import {
+  aggregateAttributeArray,
+  aggregateEserviceArray,
+  toEServiceAggregatorArray,
+} from "pagopa-interop-readmodel";
+import {
+  attributeInReadmodelAttribute,
+  eserviceDescriptorAttributeInReadmodelCatalog,
+  eserviceDescriptorDocumentInReadmodelCatalog,
+  eserviceDescriptorInReadmodelCatalog,
+  eserviceDescriptorInterfaceInReadmodelCatalog,
+  eserviceDescriptorRejectionReasonInReadmodelCatalog,
+  eserviceInReadmodelCatalog,
+  eserviceRiskAnalysisAnswerInReadmodelCatalog,
+  eserviceRiskAnalysisInReadmodelCatalog,
+} from "pagopa-interop-readmodel-models";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function readModelServiceBuilderSQL(
@@ -14,6 +29,88 @@ export function readModelServiceBuilderSQL(
         .from(attributeInReadmodelAttribute);
 
       return aggregateAttributeArray(res);
+    },
+
+    async getAllEServices(): Promise<Array<WithMetadata<EService>>> {
+      const queryResult = await readModelDB
+        .select({
+          eservice: eserviceInReadmodelCatalog,
+          descriptor: eserviceDescriptorInReadmodelCatalog,
+          interface: eserviceDescriptorInterfaceInReadmodelCatalog,
+          document: eserviceDescriptorDocumentInReadmodelCatalog,
+          attribute: eserviceDescriptorAttributeInReadmodelCatalog,
+          rejection: eserviceDescriptorRejectionReasonInReadmodelCatalog,
+          riskAnalysis: eserviceRiskAnalysisInReadmodelCatalog,
+          riskAnalysisAnswer: eserviceRiskAnalysisAnswerInReadmodelCatalog,
+          // templateBinding: eserviceTemplateBindingInReadmodelCatalog,
+        })
+        .from(eserviceInReadmodelCatalog)
+        .leftJoin(
+          // 1
+          eserviceDescriptorInReadmodelCatalog,
+          eq(
+            eserviceInReadmodelCatalog.id,
+            eserviceDescriptorInReadmodelCatalog.eserviceId
+          )
+        )
+        .leftJoin(
+          // 2
+          eserviceDescriptorInterfaceInReadmodelCatalog,
+          eq(
+            eserviceDescriptorInReadmodelCatalog.id,
+            eserviceDescriptorInterfaceInReadmodelCatalog.descriptorId
+          )
+        )
+        .leftJoin(
+          // 3
+          eserviceDescriptorDocumentInReadmodelCatalog,
+          eq(
+            eserviceDescriptorInReadmodelCatalog.id,
+            eserviceDescriptorDocumentInReadmodelCatalog.descriptorId
+          )
+        )
+        .leftJoin(
+          // 4
+          eserviceDescriptorAttributeInReadmodelCatalog,
+          eq(
+            eserviceDescriptorInReadmodelCatalog.id,
+            eserviceDescriptorAttributeInReadmodelCatalog.descriptorId
+          )
+        )
+        .leftJoin(
+          // 5
+          eserviceDescriptorRejectionReasonInReadmodelCatalog,
+          eq(
+            eserviceDescriptorInReadmodelCatalog.id,
+            eserviceDescriptorRejectionReasonInReadmodelCatalog.descriptorId
+          )
+        )
+        .leftJoin(
+          // 6
+          eserviceRiskAnalysisInReadmodelCatalog,
+          eq(
+            eserviceInReadmodelCatalog.id,
+            eserviceRiskAnalysisInReadmodelCatalog.eserviceId
+          )
+        )
+        .leftJoin(
+          // 7
+          eserviceRiskAnalysisAnswerInReadmodelCatalog,
+          eq(
+            eserviceRiskAnalysisInReadmodelCatalog.riskAnalysisFormId,
+            eserviceRiskAnalysisAnswerInReadmodelCatalog.riskAnalysisFormId
+          )
+        );
+      // .leftJoin(
+      //   // 8
+      //   eserviceTemplateBindingInReadmodelCatalog,
+      //   eq(
+      //     eserviceInReadmodelCatalog.id,
+      //     eserviceTemplateBindingInReadmodelCatalog.eserviceId
+      //   )
+      // );
+
+      return aggregateEserviceArray(toEServiceAggregatorArray(queryResult));
     },
   };
 }
--- packages/readmodel/src/index.ts
+++ packages/readmodel/src/index.ts
@@ -1,7 +1,12 @@
 export * from "./utils.js";
-export * from "./catalogReadModelService.js";
+
 export * from "./catalog/splitters.js";
+export * from "./catalog/aggregators.js";
+export * from "./catalogReadModelService.js";
+
+export * from "./attribute/aggregators.js";
 export * from "./attributeReadModelServiceSQL.js";
+
 export * from "./tenantReadModelServiceSQL.js";
 export * from "./agreementReadModelServiceSQL.js";
 export * from "./purposeReadModelServiceSQL.js";
@@ -10,4 +15,3 @@ export * from "./clientJWKKeyReadModelServiceSQL.js";
 export * from "./producerKeychainReadModelService.js";
 export * from "./producerJWKKeyReadModelService.js";
 export * from "./delegationReadModelServiceSQL.js";
-export * from "./attribute/aggregators.js";
