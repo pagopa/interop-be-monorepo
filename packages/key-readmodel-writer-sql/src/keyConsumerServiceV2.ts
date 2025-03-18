ee6749e8e40df200d00805d5189e2581d39a40ab
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-03-18 10:33:14 +0100

    Add key-readmodel-writer-sql

---
+++ packages/key-readmodel-writer-sql/src/keyConsumerServiceV2.ts
@@ -0,0 +1,69 @@
+import { keyToClientJWKKey } from "pagopa-interop-commons";
+import {
+  AuthorizationEventEnvelopeV2,
+  fromClientV2,
+  unsafeBrandId,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import { CustomReadModelService } from "./readModelService.js";
+
+export async function handleMessageV2(
+  message: AuthorizationEventEnvelopeV2,
+  clientJWKKeyReadModelService: CustomReadModelService
+): Promise<void> {
+  await match(message)
+    .with({ type: "ClientKeyAdded" }, async (message) => {
+      const client = message.data.client
+        ? fromClientV2(message.data.client)
+        : undefined;
+
+      if (!client) {
+        throw Error("Client not found in event");
+      }
+      const key = client?.keys.find((key) => key.kid === message.data.kid);
+      if (!key) {
+        throw Error(`Key not found in client: ${client?.id}`);
+      }
+      await clientJWKKeyReadModelService.upsertClientJWKKey(
+        keyToClientJWKKey(key, client.id),
+        message.version
+      );
+    })
+    .with({ type: "ClientKeyDeleted" }, async (message) => {
+      const client = message.data.client
+        ? fromClientV2(message.data.client)
+        : undefined;
+
+      if (!client) {
+        throw Error("Client not found in event");
+      }
+      await clientJWKKeyReadModelService.deleteClientJWKKeyByKid(
+        client.id,
+        message.data.kid,
+        message.version
+      );
+    })
+    .with({ type: "ClientDeleted" }, async (message) => {
+      await clientJWKKeyReadModelService.deleteClientJWKKeysByClientId(
+        unsafeBrandId(message.data.clientId),
+        message.version
+      );
+    })
+    .with(
+      { type: "ClientAdded" },
+      { type: "ClientUserAdded" },
+      { type: "ClientUserDeleted" },
+      { type: "ClientPurposeAdded" },
+      { type: "ClientPurposeRemoved" },
+      { type: "ProducerKeychainAdded" },
+      { type: "ProducerKeychainDeleted" },
+      { type: "ProducerKeychainKeyAdded" },
+      { type: "ProducerKeychainKeyDeleted" },
+      { type: "ProducerKeychainUserAdded" },
+      { type: "ProducerKeychainUserDeleted" },
+      { type: "ProducerKeychainEServiceAdded" },
+      { type: "ProducerKeychainEServiceRemoved" },
+      () => Promise.resolve
+    )
+    .exhaustive();
+}
---
+++ packages/key-readmodel-writer-sql/test/consumerServiceV1.test.ts
@@ -0,0 +1,176 @@
+import crypto from "crypto";
+import {
+  getMockClient,
+  getMockKey,
+} from "pagopa-interop-commons-test/index.js";
+import {
+  Key,
+  Client,
+  KeysAddedV1,
+  toKeyV1,
+  generateId,
+  AuthorizationEventEnvelopeV1,
+  KeyDeletedV1,
+  ClientId,
+  ClientDeletedV1,
+} from "pagopa-interop-models";
+import { describe, expect, it } from "vitest";
+import { keyToClientJWKKey } from "pagopa-interop-commons";
+import { handleMessageV1 } from "../src/keyConsumerServiceV1.js";
+import { clientJWKKeyReadModelService, readModelService } from "./utils.js";
+
+describe("Events V1", async () => {
+  const key = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+  }).publicKey;
+
+  const base64Key = Buffer.from(
+    key.export({ type: "pkcs1", format: "pem" })
+  ).toString("base64url");
+
+  const key2 = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+  }).publicKey;
+
+  const base64Key2 = Buffer.from(
+    key2.export({ type: "pkcs1", format: "pem" })
+  ).toString("base64url");
+
+  describe("KeysAdded", () => {
+    it("KeysAdded - RSA", async () => {
+      const mockClient: Client = {
+        ...getMockClient(),
+        keys: [],
+      };
+      const mockKey = { ...getMockKey(), encodedPem: base64Key };
+      const jwkKey = keyToClientJWKKey(mockKey, mockClient.id);
+
+      await readModelService.upsertClientJWKKey(jwkKey, 1);
+
+      const addedKey: Key = {
+        ...getMockKey(),
+        encodedPem: base64Key2,
+      };
+
+      const payload: KeysAddedV1 = {
+        clientId: mockClient.id,
+        keys: [
+          {
+            keyId: generateId(),
+            value: toKeyV1(addedKey),
+          },
+        ],
+      };
+
+      const message: AuthorizationEventEnvelopeV1 = {
+        sequence_num: 1,
+        stream_id: mockClient.id,
+        version: 1,
+        type: "KeysAdded",
+        event_version: 1,
+        data: payload,
+        log_date: new Date(),
+      };
+
+      await handleMessageV1(message, readModelService);
+
+      const retrievedKey =
+        await clientJWKKeyReadModelService.getClientJWKKeyByKid(
+          mockClient.id,
+          addedKey.kid
+        );
+
+      expect(retrievedKey?.data).toEqual(
+        keyToClientJWKKey(addedKey, mockClient.id)
+      );
+      expect(retrievedKey?.metadata).toEqual({
+        version: 1,
+      });
+    });
+  });
+
+  it("KeyDeleted", async () => {
+    const clientId: ClientId = generateId();
+    const mockKey = { ...getMockKey(), encodedPem: base64Key };
+    const jwkKey = keyToClientJWKKey(mockKey, clientId);
+
+    const mockClient: Client = {
+      ...getMockClient(),
+      id: clientId,
+      keys: [mockKey],
+    };
+    await readModelService.upsertClientJWKKey(jwkKey, 1);
+
+    const payload: KeyDeletedV1 = {
+      clientId: mockClient.id,
+      keyId: mockKey.kid,
+      deactivationTimestamp: new Date().toISOString(),
+    };
+
+    const message: AuthorizationEventEnvelopeV1 = {
+      sequence_num: 1,
+      stream_id: mockClient.id,
+      version: 1,
+      type: "KeyDeleted",
+      event_version: 1,
+      data: payload,
+      log_date: new Date(),
+    };
+
+    await handleMessageV1(message, readModelService);
+
+    const retrievedKey =
+      await clientJWKKeyReadModelService.getClientJWKKeyByKid(
+        clientId,
+        mockKey.kid
+      );
+
+    expect(retrievedKey).toBeUndefined();
+  });
+  it("ClientDeleted", async () => {
+    const clientId: ClientId = generateId();
+    const mockKey1: Key = { ...getMockKey(), encodedPem: base64Key };
+    const mockKey2: Key = { ...getMockKey(), encodedPem: base64Key2 };
+    const jwkKey1 = keyToClientJWKKey(mockKey1, clientId);
+    const jwkKey2 = keyToClientJWKKey(mockKey2, clientId);
+
+    const mockClient: Client = {
+      ...getMockClient(),
+      id: clientId,
+      keys: [mockKey1, mockKey2],
+    };
+
+    await readModelService.upsertClientJWKKey(jwkKey1, 1);
+    await readModelService.upsertClientJWKKey(jwkKey2, 1);
+
+    const payload: ClientDeletedV1 = {
+      clientId: mockClient.id,
+    };
+
+    const message: AuthorizationEventEnvelopeV1 = {
+      sequence_num: 1,
+      stream_id: mockClient.id,
+      version: 1,
+      type: "ClientDeleted",
+      event_version: 1,
+      data: payload,
+      log_date: new Date(),
+    };
+
+    await handleMessageV1(message, readModelService);
+
+    const retrievedKey1 =
+      await clientJWKKeyReadModelService.getClientJWKKeyByKid(
+        clientId,
+        mockKey1.kid
+      );
+    const retrievedKey2 =
+      await clientJWKKeyReadModelService.getClientJWKKeyByKid(
+        clientId,
+        mockKey2.kid
+      );
+
+    expect(retrievedKey1).toBeUndefined();
+    expect(retrievedKey2).toBeUndefined();
+  });
+});
