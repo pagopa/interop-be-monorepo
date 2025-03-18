f9455d5acb78b6b3b8fc8571ddf5fbb48b8a0b93
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-03-18 15:58:51 +0100

    Update eserviceTemplate.ts

--- packages/models/src/eservice-template/eserviceTemplate.ts
+++ packages/models/src/eservice-template/eserviceTemplate.ts
@@ -37,13 +37,13 @@ export const EServiceTemplateVersion = z.object({
   deprecatedAt: z.coerce.date().optional(),
   attributes: EServiceAttributes,
 
-  // The following fields are values to be set in all the e-service instances of this template, not editable by the user
+  // The following fields are values to be set in all the eservice instances descriptors of this template, not editable by the user
   description: z.string().optional(),
   interface: Document.optional(),
   docs: z.array(Document),
   voucherLifespan: z.number().int(),
 
-  // The following fields are default values that will be set in all the e-service instances in case the user does not provide a value
+  // The following fields are default values that will be set in all the eservice instances descriptors in case the user does not provide a value
   dailyCallsPerConsumer: z.number().int().optional(),
   dailyCallsTotal: z.number().int().optional(),
   agreementApprovalPolicy: AgreementApprovalPolicy.optional(),
@@ -53,16 +53,18 @@ export type EServiceTemplateVersion = z.infer<typeof EServiceTemplateVersion>;
 export const EServiceTemplate = z.object({
   id: EServiceTemplateId,
   creatorId: TenantId,
-  name: z.string(),
   intendedTarget: z.string(),
-  description: z.string(),
-  technology: Technology,
   versions: z.array(EServiceTemplateVersion),
   createdAt: z.coerce.date(),
+
+  // The following fields are values to be set in all the e-service instances of this template, not editable by the user
+  name: z.string(),
+  description: z.string(),
+  technology: Technology,
   riskAnalysis: z.array(RiskAnalysis),
   mode: EServiceMode,
 
-  // This field acts as a default value for all the e-service instances created from this template
+  // The following fields are default values that will be set in all the eservice instances in case the user does not provide a value
   isSignalHubEnabled: z.boolean().optional(),
 });
 export type EServiceTemplate = z.infer<typeof EServiceTemplate>;
