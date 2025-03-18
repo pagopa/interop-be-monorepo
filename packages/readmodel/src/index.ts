1ecaa8f5b5c0b8e457317a4fd8d9fa69c1e8ba32
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-03-18 16:24:31 +0100

    Add exports

--- packages/readmodel/src/index.ts
+++ packages/readmodel/src/index.ts
@@ -2,4 +2,5 @@ export * from "./utils.js";
 export * from "./attributeReadModelService.js";
 
 // E-SERVICE TEMPLATE
+export * from "./eserviceTemplateReadModelService.js";
 export * from "./eservice-template/splitters.js";
