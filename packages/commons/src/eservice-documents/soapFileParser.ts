f90fcab952cfeafc50263bf5375a5146851a4b0e
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-03-19 18:13:23 +0100

    Fix parsing SOAP interface file during create instance intrface by tempalte

--- packages/commons/src/eservice-documents/soapFileParser.ts
+++ packages/commons/src/eservice-documents/soapFileParser.ts
@@ -1,77 +1,63 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { XMLBuilder, XMLParser } from "fast-xml-parser";
-import { interfaceExtractingInfoError } from "pagopa-interop-models";
-import { z } from "zod";
+import {
+  interfaceExtractingInfoError,
+  buildingSoapFileError,
+  parsingSoapFileError,
+  interfaceExtractingSoapFiledError,
+} from "pagopa-interop-models";
 import { match } from "ts-pattern";
 import {
   eserviceInterfaceAllowedFileType,
   EserviceSoapInterfaceType,
 } from "./eserviceDocumentUtils.js";
 
-export const Wsdl = z.object({
-  definitions: z.object({
-    binding: z.array(
-      z.object({
-        operation: z.array(
-          z.object({
-            name: z.string(),
-          })
-        ),
-      })
-    ),
-    service: z.object({
-      port: z.array(
-        z.object({
-          address: z.object({ location: z.string() }),
-        })
-      ),
-    }),
-  }),
-});
-export type Wsdl = z.infer<typeof Wsdl>;
-
-const soapParser = (file: string): Wsdl => {
-  const xml = new XMLParser({
-    ignoreDeclaration: true,
-    removeNSPrefix: true,
-    ignoreAttributes: false,
-    attributeNamePrefix: "",
-    isArray: (name: string) =>
-      ["operation", "port", "binding"].indexOf(name) !== -1,
-  }).parse(file);
-
-  const { data, success, error } = Wsdl.safeParse(xml);
-  if (!success) {
-    throw error;
+// Improvement: use a more specific type for parsed SOAP xml than any
+const extractAddress = (parsedXml: any): string[] => {
+  try {
+    return parsedXml["wsdl:definitions"]["wsdl:service"]["wsdl:port"].map(
+      (b: { "soap:address": { "@_location": string } }) =>
+        b["soap:address"]["@_location"]
+    );
+  } catch (e) {
+    throw interfaceExtractingSoapFiledError("soap:address");
   }
-  return data;
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const parseSoapApi = (
-  fileType: EserviceSoapInterfaceType,
-  file: string
-): Wsdl =>
-  match(fileType)
-    .with(
-      eserviceInterfaceAllowedFileType.wsdl,
-      eserviceInterfaceAllowedFileType.xml,
-      () => soapParser(file)
-    )
-    .exhaustive();
+// Improvement: use a more specific type for parsed SOAP xml than any
+const extractEndpoints = (parsedXml: any): string[] => {
+  try {
+    return parsedXml["wsdl:definitions"]["wsdl:binding"]["wsdl:operation"].map(
+      (b: { "soap:operation": { "@_soapAction": string } }) =>
+        b["soap:operation"]["@_soapAction"]
+    );
+  } catch (e) {
+    throw interfaceExtractingSoapFiledError("soap:operation");
+  }
+};
+
+export const soapParse = (file: string) => {
+  try {
+    return new XMLParser({
+      ignoreDeclaration: false,
+      removeNSPrefix: false,
+      ignoreAttributes: false,
+      isArray: (name: string) =>
+        ["operation", "port", "binding"].indexOf(name) !== -1,
+    }).parse(file);
+  } catch (e) {
+    throw parsingSoapFileError();
+  }
+};
 
 export const retrieveServerUrlsSoapAPI = (file: string): string[] => {
-  const parsedXml = soapParser(file);
-
-  const address = parsedXml.definitions.service.port.map(
-    (p) => p.address.location
-  );
+  const parsedXml = soapParse(file);
+  const address = extractAddress(parsedXml);
   if (address.length === 0) {
     throw interfaceExtractingInfoError();
   }
-
-  const endpoints = parsedXml.definitions.binding.flatMap((b) =>
-    b.operation.map((o) => o.name)
-  );
+  const endpoints = extractEndpoints(parsedXml);
   if (endpoints.length === 0) {
     throw interfaceExtractingInfoError();
   }
@@ -88,8 +74,14 @@ export const soapApiFileToBuffer: (
       eserviceInterfaceAllowedFileType.xml,
       eserviceInterfaceAllowedFileType.wsdl,
       () => {
-        const xmlDataStr: string = new XMLBuilder().build(file);
-        return Buffer.from(xmlDataStr);
+        try {
+          const xmlDataStr: string = new XMLBuilder({
+            ignoreAttributes: false,
+          }).build(file);
+          return Buffer.from(xmlDataStr);
+        } catch (e) {
+          throw buildingSoapFileError();
+        }
       }
     )
     .exhaustive();
