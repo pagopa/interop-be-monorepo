ace89a509d87306968ad35530fa7c17a48bd7f9c
Paolo Manca <paolo.manca@pagopa.it>
2025-03-19 18:17:02 +0100

    testing client query: add a client

--- packages/readmodel/test/clientQueries.test.ts
+++ packages/readmodel/test/clientQueries.test.ts
@@ -1,14 +1,36 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import {
+  clientReadModelService,
+  getCustomMockClient,
+  retrievedClientSQLObjects,
+} from "./clientUtils.js";
 
 describe("Client queries", () => {
   describe("should insert or update a client in the db", () => {
-    it("should add a complete (*all* fields) attribute", async () => {});
+    it.only("should add a complete (*all* fields) client", async () => {
+      const client = getCustomMockClient();
+      await clientReadModelService.upsertClient(
+        client.data,
+        client.metadata.version
+      );
+      const retrievedClient = await clientReadModelService.getClientById(
+        client.data.id
+      );
+      const { clientSQL, usersSQL, purposesSQL, keysSQL } =
+        await retrievedClientSQLObjects(client);
 
-    it("should add an incomplete (*only* mandatory fields) attribute", async () => {});
+      expect(retrievedClient).toStrictEqual(client);
+      expect(clientSQL).toBeDefined();
+      expect(usersSQL).toHaveLength(client.data.users.length);
+      expect(purposesSQL).toHaveLength(client.data.purposes.length);
+      expect(keysSQL).toHaveLength(client.data.keys.length);
+    });
 
-    it("should update an attribute", async () => {});
+    it("should add an incomplete (*only* mandatory fields) client", async () => {});
+
+    it("should update an client", async () => {});
   });
 
   describe("should get a client by id from the db", () => {
