7dd3eb238d152ee9fcc5523e8188b7ed79d83382
Paolo Manca <paolo.manca@pagopa.it>
2025-03-19 16:36:24 +0100

    client queries test scaffolding

---
+++ packages/readmodel/test/clientQueries.test.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { describe, it } from "vitest";
+
+describe("Client queries", () => {
+  describe("should insert or update a client in the db", () => {
+    it("should add a complete (*all* fields) attribute", async () => {});
+
+    it("should add an incomplete (*only* mandatory fields) attribute", async () => {});
+
+    it("should update an attribute", async () => {});
+  });
+
+  describe("should get a client by id from the db", () => {
+    it("client found", async () => {});
+
+    it("client not found", async () => {});
+  });
+  describe("should delete a client from the db", () => {
+    it("delete one client", async () => {});
+  });
+});
