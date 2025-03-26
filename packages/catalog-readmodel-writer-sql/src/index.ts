5cd09c3c1da3e052b35ee40170ac41ab6664346b
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-03-26 10:11:21 +0100

    Add streamVersion to loggerInstance

--- packages/catalog-readmodel-writer-sql/src/index.ts
+++ packages/catalog-readmodel-writer-sql/src/index.ts
@@ -34,6 +34,7 @@ async function processMessage({
     eventType: decodedMessage.type,
     eventVersion: decodedMessage.event_version,
     streamId: decodedMessage.stream_id,
+    streamVersion: decodedMessage.version,
     correlationId: decodedMessage.correlation_id
       ? unsafeBrandId<CorrelationId>(decodedMessage.correlation_id)
       : generateId<CorrelationId>(),
