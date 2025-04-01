6628aebaa1caa996dff89daf79fee7c6d3e6da5f
diego-lon <diego.longo@digitouch.it>
2025-04-01 16:28:11 +0200

    Draft: handling v1 events

--- packages/domains-analytics-writer/src/handlers/catalog/consumerServiceV1.ts
+++ packages/domains-analytics-writer/src/handlers/catalog/consumerServiceV1.ts
@@ -1,38 +1,56 @@
-import { EServiceEventEnvelopeV1 } from "pagopa-interop-models";
+import { EService, EServiceEventEnvelopeV1 } from "pagopa-interop-models";
 import { match, P } from "ts-pattern";
+import { catalogServiceBuilder } from "../../service/catalogService.js";
+import { DBContext } from "../../db/db.js";
 
 export async function handleCatalogMessageV1(
-  message: EServiceEventEnvelopeV1
+  message: EServiceEventEnvelopeV1,
+  dbContext: DBContext
 ): Promise<void> {
+  const catalogService = catalogServiceBuilder(dbContext);
+
   await match(message)
-    .with({ type: P.union("EServiceAdded", "ClonedEServiceAdded") }, async () =>
-      Promise.resolve()
-    )
     .with(
       {
         type: P.union(
+          "EServiceAdded",
+          "ClonedEServiceAdded",
           "EServiceUpdated",
           "EServiceRiskAnalysisAdded",
           "MovedAttributesFromEserviceToDescriptors",
           "EServiceRiskAnalysisUpdated"
         ),
       },
-      async () => Promise.resolve()
+      async (msg) => {
+        const eservice = EService.parse(msg.data);
+        await catalogService.upsertEService(eservice, msg.event_version);
+      }
     )
+    .with({ type: "EServiceDeleted" }, async (msg) => {
+      await catalogService.deleteEService(msg.data.eserviceId);
+    })
+    .with({ type: "EServiceWithDescriptorsDeleted" }, async (msg) => {
+      await catalogService.deleteDescriptor(msg.data.descriptorId);
+    })
     .with(
-      {
-        type: P.union(
-          "EServiceWithDescriptorsDeleted",
-          "EServiceDocumentUpdated",
-          "EServiceDeleted",
-          "EServiceDocumentAdded",
-          "EServiceDocumentDeleted",
-          "EServiceDescriptorAdded",
-          "EServiceDescriptorUpdated",
-          "EServiceRiskAnalysisDeleted"
-        ),
-      },
-      async () => Promise.resolve()
+      { type: P.union("EServiceDocumentUpdated", "EServiceDocumentAdded") },
+      async (msg) => {
+        await catalogService.upsertEServiceDocument(msg.data.descriptorId);
+      }
+    )
+    .with({ type: "EServiceDocumentDeleted" }, async (msg) => {
+      await catalogService.deleteEServiceDocument(msg.data.descriptorId);
+    })
+    .with({ type: "EServiceRiskAnalysisDeleted" }, async (msg) => {
+      await catalogService.deleteEserviceRiskAnalysis(msg.data.riskAnalysisId);
+    })
+    .with(
+      { type: P.union("EServiceDescriptorAdded", "EServiceDescriptorUpdated") },
+      async (msg) => {
+        await catalogService.upsertEServiceDescriptor(
+          msg.data.eserviceDescriptor
+        );
+      }
     )
     .exhaustive();
 }
