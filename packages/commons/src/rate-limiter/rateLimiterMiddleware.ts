fda5e637e4b7a8eba83e7d95f4225bb2ad7d9e3f
Eric Camellini <eric.camellini@gmail.com>
2025-04-02 17:06:02 +0200

    Handling auth data ad-hoc in rate limiter middleware

--- packages/commons/src/rate-limiter/rateLimiterMiddleware.ts
+++ packages/commons/src/rate-limiter/rateLimiterMiddleware.ts
@@ -1,11 +1,13 @@
 import { constants } from "http2";
+import { P, match } from "ts-pattern";
 import { ZodiosRouterContextRequestHandler } from "@zodios/express";
 import {
+  TenantId,
   makeApiProblemBuilder,
   tooManyRequestsError,
+  unauthorizedError,
 } from "pagopa-interop-models";
 import { ExpressContext, fromAppContext } from "../context/context.js";
-import { validateAuthorization } from "../auth/authorization.js";
 import { RateLimiter } from "./rateLimiterModel.js";
 import { rateLimiterHeadersFromStatus } from "./rateLimiterUtils.js";
 
@@ -16,10 +18,30 @@ export function rateLimiterMiddleware(
 ): ZodiosRouterContextRequestHandler<ExpressContext> {
   return async (req, res, next) => {
     const ctx = fromAppContext(req.ctx);
-    validateAuthorization(ctx, ["admin", "api", "support", "security", "m2m"]);
+
+    const organizationId: TenantId | null = match(ctx.authData)
+      .with(
+        { systemRole: P.union("m2m", undefined) },
+        ({ organizationId }) => organizationId
+      )
+      .with({ systemRole: P.union("internal", "maintenance") }, () => null)
+      .exhaustive();
+
+    if (!organizationId) {
+      const errorRes = makeApiProblem(
+        unauthorizedError(
+          `No organizationId found in authData -- cannot apply rate limiting`
+        ),
+        () => constants.HTTP_STATUS_UNAUTHORIZED,
+        ctx.logger,
+        ctx.correlationId
+      );
+
+      return res.status(errorRes.status).send(errorRes);
+    }
 
     const rateLimiterStatus = await rateLimiter.rateLimitByOrganization(
-      ctx.authData.organizationId,
+      organizationId,
       ctx.logger
     );
 
@@ -28,7 +50,7 @@ export function rateLimiterMiddleware(
 
     if (rateLimiterStatus.limitReached) {
       const errorRes = makeApiProblem(
-        tooManyRequestsError(ctx.authData.organizationId),
+        tooManyRequestsError(organizationId),
         () => constants.HTTP_STATUS_TOO_MANY_REQUESTS,
         ctx.logger,
         ctx.correlationId
