236dac53c19400f3316a0527f1054d2bd1934eee
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-04-07 16:29:16 +0200

    Remove `sortPurpose(s)` functions

--- packages/commons-test/src/testUtils.ts
+++ packages/commons-test/src/testUtils.ts
@@ -846,29 +846,3 @@ export const sortAgreement = <
     };
   }
 };
-
-export const sortPurpose = <
-  T extends Purpose | WithMetadata<Purpose> | undefined
->(
-  purpose: T
-): T => {
-  if (!purpose) {
-    return purpose;
-  } else if ("data" in purpose) {
-    return {
-      ...purpose,
-      data: sortPurpose(purpose.data),
-    };
-  } else {
-    return {
-      ...purpose,
-      versions: [...purpose.versions].sort(sortBy<PurposeVersion>((v) => v.id)),
-    };
-  }
-};
-
-export const sortPurposes = <
-  T extends Purpose | WithMetadata<Purpose> | undefined
->(
-  purposes: T[]
-): T[] => purposes.map(sortPurpose);
