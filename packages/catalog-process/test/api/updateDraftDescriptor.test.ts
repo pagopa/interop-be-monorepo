77c0dde1be7390faad9b8a47b783d643d1f06f0b
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2025-04-07 11:53:39 +0200

    WIP

---
+++ packages/catalog-process/test/api/updateDraftDescriptor.test.ts
@@ -0,0 +1,96 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { describe, it, expect, vi } from "vitest";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+import {
+  Descriptor,
+  descriptorState,
+  EService,
+  generateId,
+  tenantKind,
+} from "pagopa-interop-models";
+import {
+  createPayload,
+  getMockAttribute,
+  getMockAuthData,
+  getMockValidRiskAnalysis,
+} from "pagopa-interop-commons-test";
+import { userRoles, AuthData } from "pagopa-interop-commons";
+import { catalogApi } from "pagopa-interop-api-clients";
+import { api } from "../vitest.api.setup.js";
+import {
+  buildUpdateDescriptorSeed,
+  getMockDescriptor,
+  getMockEService,
+} from "../mockUtils.js";
+import { catalogService } from "../../src/routers/EServiceRouter.js";
+
+describe("API /eservices/{eServiceId}/descriptors/{descriptorId} authorization test", () => {
+  const mockDescriptor = getMockDescriptor();
+
+  const descriptor: Descriptor = {
+    ...mockDescriptor,
+    state: descriptorState.draft,
+  };
+
+  const mockEService: EService = {
+    ...getMockEService(),
+    descriptors: [descriptor],
+    riskAnalysis: [getMockValidRiskAnalysis(tenantKind.PA)],
+  };
+
+  const descriptorSeed: catalogApi.UpdateEServiceDescriptorSeed = {
+    ...buildUpdateDescriptorSeed(descriptor),
+    dailyCallsTotal: 200,
+    attributes: {
+      certified: [],
+      declared: [
+        [{ id: getMockAttribute().id, explicitAttributeVerification: false }],
+      ],
+      verified: [],
+    },
+  };
+
+  vi.spyOn(catalogService, "updateDraftDescriptor").mockResolvedValue(
+    mockEService
+  );
+
+  const generateToken = (authData: AuthData) =>
+    jwt.sign(createPayload(authData), "test-secret");
+
+  const makeRequest = async (
+    token: string,
+    eServiceId: string,
+    descriptorId: string
+  ) =>
+    request(api)
+      .put(`/eservices/${eServiceId}/descriptors/${descriptorId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send(descriptorSeed);
+
+  it.each([userRoles.ADMIN_ROLE, userRoles.API_ROLE])(
+    "Should return 200 for user with role %s",
+    async (role) => {
+      const token = generateToken({ ...getMockAuthData(), userRoles: [role] });
+      const res = await makeRequest(token, mockEService.id, descriptor.id);
+      expect(res.status).toBe(200);
+    }
+  );
+
+  it.each(
+    Object.values(userRoles).filter(
+      (role) => role !== userRoles.ADMIN_ROLE && role !== userRoles.API_ROLE
+    )
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken({ ...getMockAuthData(), userRoles: [role] });
+    const res = await makeRequest(token, mockEService.id, descriptor.id);
+
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 404 not found", async () => {
+    const res = await makeRequest(generateToken(getMockAuthData()), "", "");
+    expect(res.status).toBe(404);
+  });
+});
