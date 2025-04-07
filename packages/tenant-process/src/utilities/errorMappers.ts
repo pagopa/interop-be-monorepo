6d2fe4c4f77387e74d3c4d728a4da654638f0dba
Eric Camellini <eric.camellini@gmail.com>
2025-04-07 11:00:18 +0200

    Addressing PR comment

--- packages/tenant-process/src/utilities/errorMappers.ts
+++ packages/tenant-process/src/utilities/errorMappers.ts
@@ -47,10 +47,13 @@ export const updateVerifiedAttributeExtensionDateErrorMapper = (
   error: ApiError<ErrorCodes>
 ): number =>
   match(error.code)
-    .with("verifiedAttributeNotFoundInTenant", () => 404)
-    .with("organizationNotFoundInVerifiers", () => 403)
-    .with("expirationDateNotFoundInVerifier", () => 400)
-    .with("tenantNotFound", () => 404)
+    .with(
+      "tenantNotFound",
+      "verifiedAttributeNotFoundInTenant",
+      () => HTTP_STATUS_NOT_FOUND
+    )
+    .with("organizationNotFoundInVerifiers", () => HTTP_STATUS_FORBIDDEN)
+    .with("expirationDateNotFoundInVerifier", () => HTTP_STATUS_BAD_REQUEST)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const selfcareUpsertTenantErrorMapper = (
