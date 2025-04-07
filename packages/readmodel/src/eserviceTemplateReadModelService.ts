523d36d1801977362f62620a339435d5343c8cac
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-04-07 18:02:46 +0200

    Add `getEServiceTemplateByFilter` functions

--- packages/readmodel/src/eserviceTemplateReadModelService.ts
+++ packages/readmodel/src/eserviceTemplateReadModelService.ts
@@ -1,6 +1,7 @@
 import {
   EServiceTemplate,
   EServiceTemplateId,
+  genericInternalError,
   WithMetadata,
 } from "pagopa-interop-models";
 import {
@@ -13,11 +14,13 @@ import {
   eserviceTemplateVersionInReadmodelEserviceTemplate,
   eserviceTemplateVersionInterfaceInReadmodelEserviceTemplate,
 } from "pagopa-interop-readmodel-models";
-import { and, eq, lte } from "drizzle-orm";
+import { and, eq, lte, SQL } from "drizzle-orm";
 import { splitEServiceTemplateIntoObjectsSQL } from "./eservice-template/splitters.js";
 import {
   aggregateEServiceTemplate,
+  aggregateEServiceTemplateArray,
   toEServiceTemplateAggregator,
+  toEServiceTemplateAggregatorArray,
 } from "./eservice-template/aggregators.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -123,6 +126,17 @@ export function eserviceTemplateReadModelServiceBuilder(db: DrizzleReturnType) {
     async getEServiceTemplateById(
       eserviceTemplateId: EServiceTemplateId
     ): Promise<WithMetadata<EServiceTemplate> | undefined> {
+      return this.getEServiceTemplateByFilter(
+        eq(eserviceTemplateInReadmodelEserviceTemplate.id, eserviceTemplateId)
+      );
+    },
+    async getEServiceTemplateByFilter(
+      filter: SQL | undefined
+    ): Promise<WithMetadata<EServiceTemplate> | undefined> {
+      if (filter === undefined) {
+        throw genericInternalError("Filter cannot be undefined");
+      }
+
       /*
         eservice template   ->1 version ->2 interface
                                 version ->3 document
@@ -143,9 +157,7 @@ export function eserviceTemplateReadModelServiceBuilder(db: DrizzleReturnType) {
             eserviceTemplateRiskAnalysisAnswerInReadmodelEserviceTemplate,
         })
         .from(eserviceTemplateInReadmodelEserviceTemplate)
-        .where(
-          eq(eserviceTemplateInReadmodelEserviceTemplate.id, eserviceTemplateId)
-        )
+        .where(filter)
         .leftJoin(
           // 1
           eserviceTemplateVersionInReadmodelEserviceTemplate,
@@ -201,6 +213,81 @@ export function eserviceTemplateReadModelServiceBuilder(db: DrizzleReturnType) {
 
       return aggregateEServiceTemplate(
         toEServiceTemplateAggregator(queryResult)
+      );
+    },
+    async getEServiceTemplatesByFilter(
+      filter: SQL | undefined
+    ): Promise<Array<WithMetadata<EServiceTemplate>>> {
+      if (filter === undefined) {
+        throw genericInternalError("Filter cannot be undefined");
+      }
+
+      const queryResult = await db
+        .select({
+          eserviceTemplate: eserviceTemplateInReadmodelEserviceTemplate,
+          version: eserviceTemplateVersionInReadmodelEserviceTemplate,
+          interface:
+            eserviceTemplateVersionInterfaceInReadmodelEserviceTemplate,
+          document: eserviceTemplateVersionDocumentInReadmodelEserviceTemplate,
+          attribute:
+            eserviceTemplateVersionAttributeInReadmodelEserviceTemplate,
+          riskAnalysis: eserviceTemplateRiskAnalysisInReadmodelEserviceTemplate,
+          riskAnalysisAnswer:
+            eserviceTemplateRiskAnalysisAnswerInReadmodelEserviceTemplate,
+        })
+        .from(eserviceTemplateInReadmodelEserviceTemplate)
+        .where(filter)
+        .leftJoin(
+          // 1
+          eserviceTemplateVersionInReadmodelEserviceTemplate,
+          eq(
+            eserviceTemplateInReadmodelEserviceTemplate.id,
+            eserviceTemplateVersionInReadmodelEserviceTemplate.eserviceTemplateId
+          )
+        )
+        .leftJoin(
+          // 2
+          eserviceTemplateVersionInterfaceInReadmodelEserviceTemplate,
+          eq(
+            eserviceTemplateVersionInReadmodelEserviceTemplate.id,
+            eserviceTemplateVersionInterfaceInReadmodelEserviceTemplate.versionId
+          )
+        )
+        .leftJoin(
+          // 3
+          eserviceTemplateVersionDocumentInReadmodelEserviceTemplate,
+          eq(
+            eserviceTemplateVersionInReadmodelEserviceTemplate.id,
+            eserviceTemplateVersionDocumentInReadmodelEserviceTemplate.versionId
+          )
+        )
+        .leftJoin(
+          // 4
+          eserviceTemplateVersionAttributeInReadmodelEserviceTemplate,
+          eq(
+            eserviceTemplateVersionInReadmodelEserviceTemplate.id,
+            eserviceTemplateVersionAttributeInReadmodelEserviceTemplate.versionId
+          )
+        )
+        .leftJoin(
+          // 5
+          eserviceTemplateRiskAnalysisInReadmodelEserviceTemplate,
+          eq(
+            eserviceTemplateInReadmodelEserviceTemplate.id,
+            eserviceTemplateRiskAnalysisInReadmodelEserviceTemplate.eserviceTemplateId
+          )
+        )
+        .leftJoin(
+          // 6
+          eserviceTemplateRiskAnalysisAnswerInReadmodelEserviceTemplate,
+          eq(
+            eserviceTemplateRiskAnalysisInReadmodelEserviceTemplate.riskAnalysisFormId,
+            eserviceTemplateRiskAnalysisAnswerInReadmodelEserviceTemplate.riskAnalysisFormId
+          )
+        );
+
+      return aggregateEServiceTemplateArray(
+        toEServiceTemplateAggregatorArray(queryResult)
       );
     },
     async deleteEServiceTemplateById(
