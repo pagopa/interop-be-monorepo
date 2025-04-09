f67fba69b58557b88611d3d165d4eb4b58645c67
Eric Camellini <eric.camellini@gmail.com>
2025-04-09 15:49:07 +0200

    Fixing API build and refactoring auth

--- packages/api-gateway/src/utilities/context.ts
+++ packages/api-gateway/src/utilities/context.ts
@@ -1,5 +1,10 @@
 import { IncomingHttpHeaders } from "http";
-import { AppContext, WithLogger, logger } from "pagopa-interop-commons";
+import {
+  AppContext,
+  M2MAuthData,
+  WithLogger,
+  logger,
+} from "pagopa-interop-commons";
 import { CorrelationId } from "pagopa-interop-models";
 
 export type Headers = {
@@ -8,14 +13,16 @@ export type Headers = {
   "X-Forwarded-For": string | undefined;
 };
 
-export type ApiGatewayAppContext = AppContext & { headers: Headers };
+export type ApiGatewayAppContext = AppContext<M2MAuthData> & {
+  headers: Headers;
+};
 
 export function fromApiGatewayAppContext(
   ctx: AppContext,
   headers: IncomingHttpHeaders & { "x-forwarded-for"?: string }
 ): WithLogger<ApiGatewayAppContext> {
   return {
-    ...ctx,
+    ...(ctx as AppContext<M2MAuthData>), // API Gateway is called only with M2MAuthData
     headers: {
       "X-Correlation-Id": ctx.correlationId,
       Authorization: headers.authorization,
