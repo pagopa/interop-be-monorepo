af6cfc395162bb237a5ce29608146fd350e5266d
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-04-10 14:56:06 +0200

    Add queries

---
+++ packages/ivass-certified-attributes-importer/src/service/readmodelQueriesServiceSQL.ts
@@ -0,0 +1,191 @@
+import {
+  Attribute,
+  Tenant,
+  TenantId,
+  unsafeBrandId,
+  WithMetadata,
+} from "pagopa-interop-models";
+import {
+  aggregateTenantArray,
+  AttributeReadModelService,
+  TenantReadModelService,
+  toTenantAggregatorArray,
+} from "pagopa-interop-readmodel";
+import { and, eq, inArray, isNull } from "drizzle-orm";
+import {
+  attributeInReadmodelAttribute,
+  DrizzleReturnType,
+  tenantCertifiedAttributeInReadmodelTenant,
+  tenantFeatureInReadmodelTenant,
+  tenantInReadmodelTenant,
+  tenantMailInReadmodelTenant,
+} from "pagopa-interop-readmodel-models";
+import { IvassReadModelTenant } from "../model/tenant.js";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function readModelQueriesBuilderSQL(
+  readModelDB: DrizzleReturnType,
+  tenantReadModelService: TenantReadModelService,
+  attributeReadModelService: AttributeReadModelService
+) {
+  return {
+    /**
+     * Retrieve tenants that match the given tax codes, with their unrevoked certified attribute
+     */
+    async getIVASSTenants(
+      externalId: string[]
+    ): Promise<IvassReadModelTenant[]> {
+      const queryResult = await readModelDB
+        .select({
+          tenant: tenantInReadmodelTenant,
+          certifiedAttribute: tenantCertifiedAttributeInReadmodelTenant,
+          feature: tenantFeatureInReadmodelTenant,
+        })
+        .from(tenantInReadmodelTenant)
+        .leftJoin(
+          // 1
+          tenantMailInReadmodelTenant,
+          eq(tenantInReadmodelTenant.id, tenantMailInReadmodelTenant.tenantId)
+        )
+        .innerJoin(
+          tenantCertifiedAttributeInReadmodelTenant,
+          and(
+            eq(
+              tenantInReadmodelTenant.id,
+              tenantCertifiedAttributeInReadmodelTenant.tenantId
+            ),
+            isNull(
+              tenantCertifiedAttributeInReadmodelTenant.revocationTimestamp
+            )
+          )
+        )
+        .leftJoin(
+          // 7
+          tenantFeatureInReadmodelTenant,
+          eq(
+            tenantInReadmodelTenant.id,
+            tenantFeatureInReadmodelTenant.tenantId
+          )
+        )
+        .where(
+          and(
+            eq(tenantInReadmodelTenant.externalIdOrigin, "IVASS"),
+            inArray(tenantInReadmodelTenant.externalIdValue, externalId)
+          )
+        );
+      // .orderBy(sql`LOWER(${tenantInReadmodelTenant.name})`);
+
+      const tenants = aggregateTenantArray(
+        toTenantAggregatorArray(
+          queryResult.map((entry) => ({
+            ...entry,
+            mail: null,
+            declaredAttribute: null,
+            verifiedAttribute: null,
+            verifier: null,
+            revoker: null,
+          }))
+        )
+      );
+
+      return tenants.map(tenantToIVASSTenant);
+    },
+
+    async getTenantsWithAttributes(
+      attributeIds: string[]
+    ): Promise<IvassReadModelTenant[]> {
+      const queryResult = await readModelDB
+        .select({
+          tenant: tenantInReadmodelTenant,
+          certifiedAttribute: tenantCertifiedAttributeInReadmodelTenant,
+          feature: tenantFeatureInReadmodelTenant,
+        })
+        .from(tenantInReadmodelTenant)
+        .innerJoin(
+          tenantCertifiedAttributeInReadmodelTenant,
+          and(
+            eq(
+              tenantInReadmodelTenant.id,
+              tenantCertifiedAttributeInReadmodelTenant.tenantId
+            ),
+            isNull(
+              tenantCertifiedAttributeInReadmodelTenant.revocationTimestamp
+            )
+          )
+        )
+        .leftJoin(
+          // 7
+          tenantFeatureInReadmodelTenant,
+          eq(
+            tenantInReadmodelTenant.id,
+            tenantFeatureInReadmodelTenant.tenantId
+          )
+        )
+        .where(
+          inArray(
+            tenantCertifiedAttributeInReadmodelTenant.attributeId,
+            attributeIds
+          )
+        );
+      // .orderBy(sql`LOWER(${tenantInReadmodelTenant.name})`);
+
+      const tenants = aggregateTenantArray(
+        toTenantAggregatorArray(
+          // eslint-disable-next-line sonarjs/no-identical-functions
+          queryResult.map((entry) => ({
+            ...entry,
+            mail: null,
+            declaredAttribute: null,
+            verifiedAttribute: null,
+            verifier: null,
+            revoker: null,
+          }))
+        )
+      );
+
+      return tenants.map(tenantToIVASSTenant);
+    },
+
+    async getTenantById(tenantId: string): Promise<IvassReadModelTenant> {
+      const tenantWithMetadata = await tenantReadModelService.getTenantById(
+        unsafeBrandId<TenantId>(tenantId)
+      );
+
+      if (tenantWithMetadata === undefined) {
+        throw Error(`Tenant with id ${tenantId} not found`);
+      }
+
+      return tenantWithMetadata.data;
+    },
+
+    async getAttributeByExternalId(
+      origin: string,
+      code: string
+    ): Promise<Attribute> {
+      const attributeWithMetadata =
+        await attributeReadModelService.getAttributeByFilter(
+          and(
+            eq(attributeInReadmodelAttribute.origin, origin),
+            eq(attributeInReadmodelAttribute.code, code)
+          )
+        );
+
+      if (attributeWithMetadata === undefined) {
+        throw Error(
+          `Attribute with origin ${origin} and code ${code} not found`
+        );
+      }
+
+      return attributeWithMetadata.data;
+    },
+  };
+}
+
+const tenantToIVASSTenant = (
+  tenant: WithMetadata<Tenant>
+): IvassReadModelTenant => ({
+  id: tenant.data.id,
+  externalId: tenant.data.externalId,
+  features: tenant.data.features,
+  attributes: tenant.data.attributes,
+});
