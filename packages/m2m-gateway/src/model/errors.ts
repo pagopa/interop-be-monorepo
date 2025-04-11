c579eefbf14df3a2987a22e613599c80b3f7309c
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-04-11 10:54:27 +0200

    Remove error mapper

--- packages/m2m-gateway/src/model/errors.ts
+++ packages/m2m-gateway/src/model/errors.ts
@@ -1,4 +1,3 @@
-import { constants } from "http2";
 import { makeApiProblemBuilder } from "pagopa-interop-models";
 
 export const errorCodes = {};
@@ -6,6 +5,3 @@ export const errorCodes = {};
 export type ErrorCodes = keyof typeof errorCodes;
 
 export const makeApiProblem = makeApiProblemBuilder(errorCodes, true);
-
-export const emptyErrorMapper = (): number =>
-  constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
