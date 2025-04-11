a5c9786c9530655999fdadf1cba41d5d7357c591
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-04-11 10:46:06 +0200

    Add read model sql to tests

--- packages/datalake-data-export/test/utils.ts
+++ packages/datalake-data-export/test/utils.ts
@@ -13,11 +13,27 @@ import {
   PurposeCollection,
   TenantCollection,
 } from "pagopa-interop-commons";
+import {
+  agreementReadModelServiceBuilder,
+  catalogReadModelServiceBuilder,
+  purposeReadModelServiceBuilder,
+  tenantReadModelServiceBuilder,
+} from "pagopa-interop-readmodel";
+import { Agreement, EService, Purpose, Tenant } from "pagopa-interop-models";
 import { readModelServiceBuilder } from "../src/services/readModelService.js";
+import { readModelServiceBuilderSQL } from "../src/services/readModelServiceSQL.js";
+import { config } from "../src/config/config.js";
 
-export const { cleanup, readModelRepository } = await setupTestContainersVitest(
-  inject("readModelConfig")
-);
+export const { cleanup, readModelRepository, readModelDB } =
+  await setupTestContainersVitest(
+    inject("readModelConfig"),
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    inject("readModelSQLConfig")
+  );
 
 afterEach(cleanup);
 
@@ -27,7 +43,25 @@ export const tenants: TenantCollection = readModelRepository.tenants;
 export const attributes: AttributeCollection = readModelRepository.attributes;
 export const purposes: PurposeCollection = readModelRepository.purposes;
 
-export const readModelService = readModelServiceBuilder(readModelRepository);
+const oldReadModelService = readModelServiceBuilder(readModelRepository);
+
+const agreementReadModelServiceSQL =
+  agreementReadModelServiceBuilder(readModelDB);
+const catalogReadModelServiceSQL = catalogReadModelServiceBuilder(readModelDB);
+const purposeReadModelServiceSQL = purposeReadModelServiceBuilder(readModelDB);
+const tenantReadModelServiceSQL = tenantReadModelServiceBuilder(readModelDB);
+const readModelServiceSQL = readModelServiceBuilderSQL({
+  readModelDB,
+  agreementReadModelServiceSQL,
+  catalogReadModelServiceSQL,
+  tenantReadModelServiceSQL,
+});
+export const readModelService =
+  config.featureFlagSQL &&
+  config.readModelSQLDbHost &&
+  config.readModelSQLDbPort
+    ? readModelServiceSQL
+    : oldReadModelService;
 
 export async function seedCollection<T>(
   data: T[],
@@ -37,3 +71,28 @@ export async function seedCollection<T>(
     await writeInReadmodel(d, collection);
   }
 }
+export const seedTenants = async (tenants: Tenant[]): Promise<void> => {
+  for (const t of tenants) {
+    await tenantReadModelServiceSQL.upsertTenant(t, 0);
+  }
+};
+
+export const seedAgreements = async (
+  agreements: Agreement[]
+): Promise<void> => {
+  for (const a of agreements) {
+    await agreementReadModelServiceSQL.upsertAgreement(a, 0);
+  }
+};
+
+export const seedPurposes = async (purposes: Purpose[]): Promise<void> => {
+  for (const p of purposes) {
+    await purposeReadModelServiceSQL.upsertPurpose(p, 0);
+  }
+};
+
+export const seedEServices = async (eservices: EService[]): Promise<void> => {
+  for (const e of eservices) {
+    await catalogReadModelServiceSQL.upsertEService(e, 0);
+  }
+};
