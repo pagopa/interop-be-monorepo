2813c40c8ca0793a3d5253d7ab46e991801edebb
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-04-11 14:57:26 +0200

    Simplify test

--- packages/dtd-catalog-exporter/test/utils.ts
+++ packages/dtd-catalog-exporter/test/utils.ts
@@ -1,16 +1,5 @@
-import {
-  setupTestContainersVitest,
-  writeInReadmodel,
-} from "pagopa-interop-commons-test";
+import { setupTestContainersVitest } from "pagopa-interop-commons-test";
 import { afterEach, inject } from "vitest";
-import {
-  EService,
-  Tenant,
-  toReadModelEService,
-  toReadModelTenant,
-  Attribute,
-  toReadModelAttribute,
-} from "pagopa-interop-models";
 
 export const {
   cleanup,
@@ -18,25 +7,9 @@ export const {
   postgresDB: _,
   fileManager,
 } = await setupTestContainersVitest(
-  inject("readModelConfig"),
+  undefined,
   undefined,
   inject("fileManagerConfig")
 );
 
 afterEach(cleanup);
-
-const eservices = readModelRepository.eservices;
-const tenants = readModelRepository.tenants;
-const attributes = readModelRepository.attributes;
-
-export const addOneEService = async (eservice: EService): Promise<void> => {
-  await writeInReadmodel(toReadModelEService(eservice), eservices);
-};
-
-export const addOneTenant = async (tenant: Tenant): Promise<void> => {
-  await writeInReadmodel(toReadModelTenant(tenant), tenants);
-};
-
-export const addOneAttribute = async (attribute: Attribute): Promise<void> => {
-  await writeInReadmodel(toReadModelAttribute(attribute), attributes);
-};
