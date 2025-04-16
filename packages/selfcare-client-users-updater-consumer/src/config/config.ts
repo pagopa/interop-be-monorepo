b83c6d1233a53759d44a378ea655abbfb01448d2
Alepazz <alessio.pazzani@gmail.com>
2025-04-16 14:49:38 +0200

    Fixed config.ts

--- packages/selfcare-client-users-updater-consumer/src/config/config.ts
+++ packages/selfcare-client-users-updater-consumer/src/config/config.ts
@@ -1,25 +1,25 @@
 import { z } from "zod";
 import {
-  AuthorizationServerTokenGenerationConfig,
   KafkaConsumerConfig,
+  TokenGenerationConfig,
 } from "pagopa-interop-commons";
 
 export const SelfcareClientUsersUpdaterConsumerConfig = KafkaConsumerConfig.and(
-  AuthorizationServerTokenGenerationConfig
+  TokenGenerationConfig
 ).and(
   z
     .object({
       SELFCARE_TOPIC: z.string(),
       INTEROP_PRODUCT_IDENTIFIER: z.string(),
       ALLOWED_ORIGINS: z.string(),
-      ALLOWE_ORIGINS_UUID: z.string().uuid(),
+      ALLOWED_ORIGINS_UUID: z.string().uuid(),
       AUTHORIZATION_PROCESS_URL: z.string(),
     })
     .transform((c) => ({
       selfcareTopic: c.SELFCARE_TOPIC,
       interopProductId: c.INTEROP_PRODUCT_IDENTIFIER,
       allowedOrigins: c.ALLOWED_ORIGINS.split(","),
-      allowedOriginsUuid: c.ALLOWED_ORIGINS.split(","),
+      allowedOriginsUuid: c.ALLOWED_ORIGINS_UUID.split(","),
       authorizationProcessUrl: c.AUTHORIZATION_PROCESS_URL,
     }))
 );
