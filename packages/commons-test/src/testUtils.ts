954c18d5347c3fdac25e4b6ec628f790dad1c30b
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-04-18 15:04:15 +0200

    Revert `getMockAgreement` and `getMockPurpose`

--- packages/commons-test/src/testUtils.ts
+++ packages/commons-test/src/testUtils.ts
@@ -272,18 +272,11 @@ export const getMockAgreement = (
   consumerId: TenantId = generateId<TenantId>(),
   state: AgreementState = agreementState.draft
 ): Agreement => ({
-  id: generateId(),
+  ...generateMock(Agreement),
   eserviceId,
   consumerId,
   state,
   stamps: getMockAgreementStamps(),
-  createdAt: new Date(),
-  descriptorId: generateId(),
-  producerId: generateId(),
-  verifiedAttributes: [],
-  certifiedAttributes: [],
-  declaredAttributes: [],
-  consumerDocuments: [],
 });
 
 export const getMockAttribute = (
@@ -306,6 +299,7 @@ export const getMockPurpose = (versions?: PurposeVersion[]): Purpose => ({
   description: "Test purpose - description",
   createdAt: new Date(),
   isFreeOfCharge: true,
+  freeOfChargeReason: "test",
 });
 
 export const getMockPurposeVersion = (
