9dd84405e66fee43d5e5ff1996b00cc0bb62f04b
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-04-18 15:26:43 +0200

    Clean deltas

--- packages/readmodel/src/eservice-template/splitters.ts
+++ packages/readmodel/src/eservice-template/splitters.ts
@@ -218,7 +218,7 @@ const splitEServiceTemplateVersionIntoObjectsSQL = (
   };
 };
 
-export const splitEServiceTemplateRiskAnalysisIntoObjectsSQL = (
+const splitEServiceTemplateRiskAnalysisIntoObjectsSQL = (
   riskAnalysis: RiskAnalysis,
   eserviceTemplateId: EServiceTemplateId,
   metadataVersion: number
