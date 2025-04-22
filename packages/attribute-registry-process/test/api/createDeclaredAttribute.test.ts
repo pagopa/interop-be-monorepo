302c69bb90c01fc6f07dc0a621111d2f7181545c
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2025-04-22 12:45:21 +0200

    PIN-6540 Supertest in attribute registry process (#1758)
    Co-authored-by: Tommaso Petrucciani <tommaso.petrucciani@icloud.com>

---
+++ packages/attribute-registry-process/test/api/createDeclaredAttribute.test.ts
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { describe, it, expect, vi } from "vitest";
+import { Attribute, generateId } from "pagopa-interop-models";
+import { generateToken, getMockAttribute } from "pagopa-interop-commons-test";
+import { AuthRole, authRole } from "pagopa-interop-commons";
+import request from "supertest";
+import { attributeRegistryApi } from "pagopa-interop-api-clients";
+import { api, attributeRegistryService } from "../vitest.api.setup.js";
+import { toApiAttribute } from "../../src/model/domain/apiConverter.js";
+import { attributeDuplicateByName } from "../../src/model/domain/errors.js";
+
+describe("API /declaredAttributes authorization test", () => {
+  const mockDeclaredAttributeSeed: attributeRegistryApi.AttributeSeed = {
+    name: "Declared Attribute",
+    description: "This is a declared attribute",
+  };
+
+  const mockAttribute: Attribute = {
+    ...getMockAttribute(),
+    id: generateId(),
+    kind: "Declared",
+    creationTime: new Date(),
+  };
+
+  const apiAttribute = attributeRegistryApi.Attribute.parse(
+    toApiAttribute(mockAttribute)
+  );
+
+  attributeRegistryService.createDeclaredAttribute = vi
+    .fn()
+    .mockResolvedValue(mockAttribute);
+
+  const makeRequest = async (token: string) =>
+    request(api)
+      .post("/declaredAttributes")
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send(mockDeclaredAttributeSeed);
+
+  const authorizedRoles: AuthRole[] = [authRole.ADMIN_ROLE, authRole.API_ROLE];
+
+  it.each(authorizedRoles)(
+    "Should return 200 for user with role %s",
+    async (role) => {
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual(apiAttribute);
+    }
+  );
+
+  it.each(
+    Object.values(authRole).filter((role) => !authorizedRoles.includes(role))
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken(role);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 409 for conflict", async () => {
+    attributeRegistryService.createDeclaredAttribute = vi
+      .fn()
+      .mockRejectedValue(
+        attributeDuplicateByName(mockDeclaredAttributeSeed.name)
+      );
+
+    const res = await makeRequest(generateToken(authRole.ADMIN_ROLE));
+
+    expect(res.status).toBe(409);
+  });
+
+  it("Should return 400 if passed an invalid attribute seed", async () => {
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await request(api)
+      .post("/declaredAttributes")
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send({
+        name: "Declared Attribute",
+      });
+    expect(res.status).toBe(400);
+  });
+});
