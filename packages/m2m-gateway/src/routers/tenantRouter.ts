61c6713901a8be82a1eff97a7d62d919713e996d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-04-23 11:53:57 +0200

    Added M2M_ADMIN_ROLE to `getTenants` route

--- packages/m2m-gateway/src/routers/tenantRouter.ts
+++ packages/m2m-gateway/src/routers/tenantRouter.ts
@@ -18,7 +18,7 @@ const tenantRouter = (
   ctx: ZodiosContext,
   clients: PagoPAInteropBeClients
 ): ZodiosRouter<ZodiosEndpointDefinitions, ExpressContext> => {
-  const { M2M_ROLE } = authRole;
+  const { M2M_ROLE, M2M_ADMIN_ROLE } = authRole;
   const tenantRouter = ctx.router(m2mGatewayApi.tenantsApi.api, {
     validationErrorHandler: zodiosValidationErrorToApiProblem,
   });
@@ -29,7 +29,7 @@ const tenantRouter = (
     .get("/tenants", async (req, res) => {
       const ctx = fromM2MGatewayAppContext(req.ctx, req.headers);
       try {
-        validateAuthorization(ctx, [M2M_ROLE]);
+        validateAuthorization(ctx, [M2M_ROLE, M2M_ADMIN_ROLE]);
         const tenants = await tenantService.getTenants(ctx, req.query);
 
         return res.status(200).send(m2mGatewayApi.Tenants.parse(tenants));
