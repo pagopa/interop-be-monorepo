79017a44a9a4a6d65a517547f9345d12dfe6938c
Sandro Tajè <sandro.taje@gmail.com>
2025-04-29 15:42:45 +0200

    Add createEserviceTemplateDocument

---
+++ packages/eservice-template-process/test/api/createEServiceTemplateDocument.test.ts
@@ -0,0 +1,132 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { generateId, operationForbidden } from "pagopa-interop-models";
+import {
+  generateToken,
+  getMockEServiceTemplate,
+} from "pagopa-interop-commons-test";
+import { AuthRole, authRole } from "pagopa-interop-commons";
+import request from "supertest";
+import { eserviceTemplateApi } from "pagopa-interop-api-clients";
+import { api, eserviceTemplateService } from "../vitest.api.setup.js";
+import {
+  documentPrettyNameDuplicate,
+  eServiceTemplateNotFound,
+  eServiceTemplateVersionNotFound,
+  interfaceAlreadyExists,
+} from "../../src/model/domain/errors.js";
+import { eserviceTemplateToApiEServiceTemplate } from "../../src/model/domain/apiConverter.js";
+import { buildDocumentSeed } from "../mockUtils.js";
+
+describe("API POST /templates/:templateId/versions/:templateVersionId/documents", () => {
+  const mockEserviceTemplate = getMockEServiceTemplate();
+  const mockSeed: eserviceTemplateApi.CreateEServiceTemplateVersionDocumentSeed =
+    buildDocumentSeed();
+
+  const makeRequest = async (
+    token: string,
+    seed: eserviceTemplateApi.CreateEServiceTemplateVersionDocumentSeed = mockSeed,
+    templateId: string = mockEserviceTemplate.id,
+    templateVersionId: string = mockEserviceTemplate.versions[0].id
+  ) =>
+    request(api)
+      .post(`/templates/${templateId}/versions/${templateVersionId}/documents`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send(seed);
+
+  beforeEach(() => {
+    eserviceTemplateService.createEServiceTemplateDocument = vi
+      .fn()
+      .mockResolvedValue(mockEserviceTemplate);
+  });
+
+  const authorizedRoles: AuthRole[] = [authRole.ADMIN_ROLE, authRole.API_ROLE];
+  it.each(authorizedRoles)(
+    "Should return 200 for user with role %s",
+    async (role) => {
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+      expect(res.body).toEqual(
+        eserviceTemplateToApiEServiceTemplate(mockEserviceTemplate)
+      );
+      expect(res.status).toBe(200);
+    }
+  );
+
+  it.each(
+    Object.values(authRole).filter((role) => !authorizedRoles.includes(role))
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken(role);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 404 for eserviceTemplateNotFound", async () => {
+    eserviceTemplateService.createEServiceTemplateDocument = vi
+      .fn()
+      .mockRejectedValue(eServiceTemplateNotFound(mockEserviceTemplate.id));
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `EService Template ${mockEserviceTemplate.id} not found`
+    );
+    expect(res.status).toBe(404);
+  });
+
+  it("Should return 404 for eserviceTemplateVersionNotFound", async () => {
+    eserviceTemplateService.createEServiceTemplateDocument = vi
+      .fn()
+      .mockRejectedValue(
+        eServiceTemplateVersionNotFound(
+          mockEserviceTemplate.id,
+          mockEserviceTemplate.versions[0].id
+        )
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `EService Template ${mockEserviceTemplate.id} version ${mockEserviceTemplate.versions[0].id} not found`
+    );
+    expect(res.status).toBe(404);
+  });
+
+  it("Should return 403 for operationForbidden", async () => {
+    eserviceTemplateService.createEServiceTemplateDocument = vi
+      .fn()
+      .mockRejectedValue(operationForbidden);
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe("Insufficient privileges");
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 400 for interfaceAlreadyExists", async () => {
+    const interfaceName = "interfaceName";
+    eserviceTemplateService.createEServiceTemplateDocument = vi
+      .fn()
+      .mockRejectedValue(interfaceAlreadyExists(interfaceName));
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe("Interface interfaceName already exists");
+    expect(res.status).toBe(400);
+  });
+
+  it("Should return 409 for documentPrettyNameDuplicate", async () => {
+    const documentName = "documentName";
+    eserviceTemplateService.createEServiceTemplateDocument = vi
+      .fn()
+      .mockRejectedValue(
+        documentPrettyNameDuplicate(
+          documentName,
+          mockEserviceTemplate.versions[0].id
+        )
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `A document with prettyName ${documentName} already exists in version ${mockEserviceTemplate.versions[0].id}`
+    );
+    expect(res.status).toBe(409);
+  });
+});
