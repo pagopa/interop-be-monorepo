27f4337a6599aac792ff6deb45ef91a99bb716c9
Matteo Grilli <matteo.grilli@quinck.io>
2025-04-29 09:57:01 +0200

    set up services injection in app

--- packages/backend-for-frontend/src/app.ts
+++ packages/backend-for-frontend/src/app.ts
@@ -1,11 +1,9 @@
 import {
   authenticationMiddleware,
   contextMiddleware,
-  initFileManager,
   loggerMiddleware,
   zodiosCtx,
-  initRedisRateLimiter,
-  rateLimiterMiddleware,
+  rateLimiterMiddleware as rateLimiterMiddlewareBuilder,
 } from "pagopa-interop-commons";
 import express from "express";
 import {
@@ -16,7 +14,6 @@ import {
 import { serviceName as modelsServiceName } from "pagopa-interop-models";
 import { config } from "./config/config.js";
 import privacyNoticeRouter from "./routers/privacyNoticeRouter.js";
-import { getInteropBeClients } from "./clients/clientsProvider.js";
 import healthRouter from "./routers/HealthRouter.js";
 import agreementRouter from "./routers/agreementRouter.js";
 import attributeRouter from "./routers/attributeRouter.js";
@@ -27,7 +24,6 @@ import selfcareRouter from "./routers/selfcareRouter.js";
 import supportRouter from "./routers/supportRouter.js";
 import tenantRouter from "./routers/tenantRouter.js";
 import toolRouter from "./routers/toolRouter.js";
-import getAllowList from "./utilities/getAllowList.js";
 import {
   fromFilesToBodyMiddleware,
   multerMiddleware,
@@ -39,68 +35,95 @@ import delegationRouter from "./routers/delegationRouter.js";
 import producerDelegationRouter from "./routers/producerDelegationRouter.js";
 import consumerDelegationRouter from "./routers/consumerDelegationRouter.js";
 import eserviceTemplateRouter from "./routers/eserviceTemplateRouter.js";
+import { AgreementService } from "./services/agreementService.js";
+import { AttributeService } from "./services/attributeService.js";
+import { AuthorizationService } from "./services/authorizationService.js";
+import { CatalogService } from "./services/catalogService.js";
+import { ClientService } from "./services/clientService.js";
+import { DelegationService } from "./services/delegationService.js";
+import { EServiceTemplateService } from "./services/eserviceTemplateService.js";
+import { ProducerKeychainService } from "./services/producerKeychainService.js";
+import { PurposeService } from "./services/purposeService.js";
+import { SelfcareService } from "./services/selfcareService.js";
+import { TenantService } from "./services/tenantService.js";
+import { ToolsService } from "./services/toolService.js";
 
-const serviceName = modelsServiceName.BACKEND_FOR_FRONTEND;
+export type BFFServices = {
+  agreementService: AgreementService;
+  attributeService: AttributeService;
+  authorizationService: AuthorizationService;
+  authorizationServiceForSupport: AuthorizationService;
+  catalogService: CatalogService;
+  clientService: ClientService;
+  delegationService: DelegationService;
+  eServiceTemplateService: EServiceTemplateService;
+  producerKeychainService: ProducerKeychainService;
+  purposeService: PurposeService;
+  selfcareService: SelfcareService;
+  tenantService: TenantService;
+  toolsService: ToolsService;
+};
 
-const fileManager = initFileManager(config);
-const allowList = await getAllowList(serviceName, fileManager, config);
+export type RateLimiterMiddleware = ReturnType<
+  typeof rateLimiterMiddlewareBuilder
+>;
 
-const clients = getInteropBeClients();
+export const serviceName = modelsServiceName.BACKEND_FOR_FRONTEND;
 
-const app = zodiosCtx.app();
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function createApp(
+  services: BFFServices,
+  rateLimiterMiddleware: RateLimiterMiddleware
+) {
+  const app = zodiosCtx.app();
 
-const redisRateLimiter = await initRedisRateLimiter({
-  limiterGroup: "BFF",
-  maxRequests: config.rateLimiterMaxRequests,
-  rateInterval: config.rateLimiterRateInterval,
-  burstPercentage: config.rateLimiterBurstPercentage,
-  redisHost: config.rateLimiterRedisHost,
-  redisPort: config.rateLimiterRedisPort,
-  timeout: config.rateLimiterTimeout,
-});
+  // Disable the "X-Powered-By: Express" HTTP header for security reasons.
+  // See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
+  app.disable("x-powered-by");
 
-// Disable the "X-Powered-By: Express" HTTP header for security reasons.
-// See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
-app.disable("x-powered-by");
+  app.disable("etag");
 
-app.disable("etag");
+  // parse files from multipart/form-data and put them in req.body
+  app.use(multerMiddleware);
+  app.use(fromFilesToBodyMiddleware);
 
-// parse files from multipart/form-data and put them in req.body
-app.use(multerMiddleware);
-app.use(fromFilesToBodyMiddleware);
+  // parse application/x-www-form-urlencoded and put it in req.body
+  app.use(express.urlencoded({ extended: true }));
 
-// parse application/x-www-form-urlencoded and put it in req.body
-app.use(express.urlencoded({ extended: true }));
+  app.use(loggerMiddleware(serviceName));
 
-app.use(loggerMiddleware(serviceName));
+  app.use(
+    `/backend-for-frontend/${config.backendForFrontendInterfaceVersion}`,
+    healthRouter,
+    contextMiddleware(serviceName, false),
+    await applicationAuditBeginMiddleware(serviceName, config),
+    await applicationAuditEndMiddleware(serviceName, config),
+    await applicationAuditEndSessionTokenExchangeMiddleware(
+      serviceName,
+      config
+    ),
+    authenticationMiddleware(config),
+    uiAuthDataValidationMiddleware(),
+    rateLimiterMiddleware
+  );
+  app.use(
+    agreementRouter(zodiosCtx, services.agreementService),
+    attributeRouter(zodiosCtx, services.attributeService),
+    authorizationRouter(zodiosCtx, services.authorizationService),
+    catalogRouter(zodiosCtx, services.catalogService),
+    clientRouter(zodiosCtx, services.clientService),
+    consumerDelegationRouter(zodiosCtx, services.delegationService),
+    delegationRouter(zodiosCtx, services.delegationService),
+    eserviceTemplateRouter(zodiosCtx, services.eServiceTemplateService),
+    privacyNoticeRouter(zodiosCtx),
+    producerDelegationRouter(zodiosCtx, services.delegationService),
+    producerKeychainRouter(zodiosCtx, services.producerKeychainService),
+    purposeRouter(zodiosCtx, services.purposeService),
+    selfcareRouter(zodiosCtx, services.selfcareService),
+    supportRouter(zodiosCtx, services.authorizationServiceForSupport),
+    tenantRouter(zodiosCtx, services.tenantService),
+    toolRouter(zodiosCtx, services.toolsService)
+  );
 
-app.use(
-  `/backend-for-frontend/${config.backendForFrontendInterfaceVersion}`,
-  healthRouter,
-  contextMiddleware(serviceName, false),
-  await applicationAuditBeginMiddleware(serviceName, config),
-  await applicationAuditEndMiddleware(serviceName, config),
-  await applicationAuditEndSessionTokenExchangeMiddleware(serviceName, config),
-  authorizationRouter(zodiosCtx, clients, allowList, redisRateLimiter),
-  authenticationMiddleware(config),
-  // Authenticated routes (rate limiter & authorization middlewares rely on auth data to work)
-  uiAuthDataValidationMiddleware(),
-  rateLimiterMiddleware(redisRateLimiter),
-  catalogRouter(zodiosCtx, clients, fileManager),
-  attributeRouter(zodiosCtx, clients),
-  purposeRouter(zodiosCtx, clients),
-  agreementRouter(zodiosCtx, clients, fileManager),
-  selfcareRouter(clients, zodiosCtx),
-  supportRouter(zodiosCtx, clients, redisRateLimiter),
-  toolRouter(zodiosCtx, clients),
-  tenantRouter(zodiosCtx, clients),
-  clientRouter(zodiosCtx, clients),
-  privacyNoticeRouter(zodiosCtx),
-  producerKeychainRouter(zodiosCtx, clients),
-  delegationRouter(zodiosCtx, clients, fileManager),
-  producerDelegationRouter(zodiosCtx, clients, fileManager),
-  consumerDelegationRouter(zodiosCtx, clients, fileManager),
-  eserviceTemplateRouter(zodiosCtx, clients, fileManager)
-);
-
-export default app;
+  return app;
+}
