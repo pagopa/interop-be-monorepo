7d9e184181964ede1069e7187be24002f97fc6be
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-04-29 17:45:30 +0200

    Revert "Refactor checks in createJWK"
    This reverts commit 411e278f9b57bb32d1be003dac365784b7cd6887.

--- packages/key-readmodel-writer/src/keyConsumerServiceV1.ts
+++ packages/key-readmodel-writer/src/keyConsumerServiceV1.ts
@@ -1,7 +1,7 @@
 import {
+  createJWK,
   ClientKeyCollection,
   keyToClientJWKKey,
-  createJWKWithoutChecks,
 } from "pagopa-interop-commons";
 import {
   AuthorizationEventEnvelopeV1,
@@ -21,7 +21,7 @@ export async function handleMessageV1(
         .map((keyV1) => (keyV1.value ? fromKeyV1(keyV1.value) : undefined))
         .filter((k): k is Key => k !== undefined)
         .filter((k) => {
-          const jwk = createJWKWithoutChecks(k.encodedPem);
+          const jwk = createJWK(k.encodedPem, false);
           return jwk.kty !== "EC";
         });
       for (const key of keysToAdd) {
