9bee6c7791bc3684da6ff56a5cc4e3beb5c3919f
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-04-29 17:41:00 +0200

    Create getTenant.test.ts

---
+++ packages/m2m-gateway/test/api/tenants/getTenant.test.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { describe, it, expect, vi } from "vitest";
+import { generateToken } from "pagopa-interop-commons-test";
+import { AuthRole, authRole } from "pagopa-interop-commons";
+import request from "supertest";
+import { m2mGatewayApi } from "pagopa-interop-api-clients";
+import { generateId } from "pagopa-interop-models";
+import { api, mockTenantService } from "../../vitest.api.setup.js";
+import { appBasePath } from "../../../src/config/appBasePath.js";
+
+describe("GET /tenants/:tenantId route test", () => {
+  const mockResponse: m2mGatewayApi.Tenant = {
+    id: generateId(),
+    createdAt: new Date().toISOString(),
+    externalId: {
+      origin: "ORIGIN",
+      value: "VALUE",
+    },
+    name: "Test Tenant",
+    kind: "GSP",
+    onboardedAt: new Date().toISOString(),
+  };
+
+  const makeRequest = async (token: string) =>
+    request(api)
+      .get(`${appBasePath}/tenants/${mockResponse.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+  const authorizedRoles: AuthRole[] = [
+    authRole.M2M_ROLE,
+    authRole.M2M_ADMIN_ROLE,
+  ];
+
+  it.each(authorizedRoles)(
+    "Should return 200 and perform API clients calls for user with role %s",
+    async (role) => {
+      mockTenantService.getTenant = vi.fn().mockResolvedValue(mockResponse);
+
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual(mockResponse);
+    }
+  );
+});
