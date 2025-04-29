8aa12d810b7d2b160136048ea3a3540c1bbcf0d4
Sandro Tajè <sandro.taje@gmail.com>
2025-04-29 16:47:09 +0200

    Add delete document

---
+++ packages/eservice-template-process/test/api/deleteDocument.test.ts
@@ -0,0 +1,109 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import {
+  EServiceDocumentId,
+  EServiceTemplateId,
+  EServiceTemplateVersionId,
+  eserviceTemplateVersionState,
+  generateId,
+  operationForbidden,
+} from "pagopa-interop-models";
+import { generateToken } from "pagopa-interop-commons-test";
+import { AuthRole, authRole } from "pagopa-interop-commons";
+import request from "supertest";
+import { api, eserviceTemplateService } from "../vitest.api.setup.js";
+import {
+  eServiceTemplateNotFound,
+  eServiceTemplateVersionNotFound,
+  notValidEServiceTemplateVersionState,
+} from "../../src/model/domain/errors.js";
+
+describe("API DELETE /templates/:templateId/versions/:templateVersionId/documents/:documentId", () => {
+  const templateId = generateId<EServiceTemplateId>();
+  const templateVersionId = generateId<EServiceTemplateVersionId>();
+  const documentId = generateId<EServiceDocumentId>();
+
+  const makeRequest = async (token: string) =>
+    request(api)
+      .delete(
+        `/templates/${templateId}/versions/${templateVersionId}/documents/${documentId}`
+      )
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send();
+
+  beforeEach(() => {
+    eserviceTemplateService.deleteDocument = vi
+      .fn()
+      .mockResolvedValue(undefined);
+  });
+
+  const authorizedRoles: AuthRole[] = [authRole.ADMIN_ROLE, authRole.API_ROLE];
+  it.each(authorizedRoles)(
+    "Should return 200 for user with role %s",
+    async (role) => {
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+      expect(res.status).toBe(204);
+    }
+  );
+
+  it.each(
+    Object.values(authRole).filter((role) => !authorizedRoles.includes(role))
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken(role);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 404 for eserviceTemplateNotFound", async () => {
+    eserviceTemplateService.deleteDocument = vi
+      .fn()
+      .mockRejectedValue(eServiceTemplateNotFound(templateId));
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(`EService Template ${templateId} not found`);
+    expect(res.status).toBe(404);
+  });
+
+  it("Should return 404 for eserviceTemplateVersionNotFound", async () => {
+    eserviceTemplateService.deleteDocument = vi
+      .fn()
+      .mockRejectedValue(
+        eServiceTemplateVersionNotFound(templateId, templateVersionId)
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `EService Template ${templateId} version ${templateVersionId} not found`
+    );
+    expect(res.status).toBe(404);
+  });
+
+  it("Should return 403 for operationForbidden", async () => {
+    eserviceTemplateService.deleteDocument = vi
+      .fn()
+      .mockRejectedValue(operationForbidden);
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe("Insufficient privileges");
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 400 for notValidEServiceTemplateVersionState", async () => {
+    eserviceTemplateService.deleteDocument = vi
+      .fn()
+      .mockRejectedValue(
+        notValidEServiceTemplateVersionState(
+          templateVersionId,
+          eserviceTemplateVersionState.draft
+        )
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `EService template version ${templateVersionId} has a not valid status for this operation ${eserviceTemplateVersionState.draft}`
+    );
+    expect(res.status).toBe(400);
+  });
+});
