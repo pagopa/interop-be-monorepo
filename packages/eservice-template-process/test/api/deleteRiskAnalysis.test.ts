28c587080488bb0abd98ccf562b75fa775749c32
Sandro Tajè <sandro.taje@gmail.com>
2025-04-30 11:08:08 +0200

    Add delete risk analysis

--- packages/eservice-template-process/test/api/deleteRiskAnalysis.test.ts
+++ packages/eservice-template-process/test/api/deleteRiskAnalysis.test.ts
@@ -2,54 +2,38 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import {
   EServiceTemplateId,
-  TenantId,
+  RiskAnalysisId,
   generateId,
   operationForbidden,
-  tenantKind,
 } from "pagopa-interop-models";
-import {
-  generateToken,
-  getMockValidRiskAnalysis,
-} from "pagopa-interop-commons-test";
-import {
-  AuthRole,
-  RiskAnalysisValidationIssue,
-  authRole,
-} from "pagopa-interop-commons";
+import { generateToken } from "pagopa-interop-commons-test";
+import { AuthRole, authRole } from "pagopa-interop-commons";
 import request from "supertest";
-import { eserviceTemplateApi } from "pagopa-interop-api-clients";
 import { api, eserviceTemplateService } from "../vitest.api.setup.js";
-import { buildRiskAnalysisSeed } from "../mockUtils.js";
 import {
   eServiceTemplateNotFound,
   eserviceTemplateNotInDraftState,
-  riskAnalysisValidationFailed,
   templateNotInReceiveMode,
-  tenantKindNotFound,
-  tenantNotFound,
 } from "../../src/model/domain/errors.js";
 
-describe("API POST /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
+describe("API DELETE /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
   const eserviceTemplateId = generateId<EServiceTemplateId>();
 
-  const mockValidRiskAnalysis = getMockValidRiskAnalysis(tenantKind.PA);
-  const riskAnalysisSeed: eserviceTemplateApi.EServiceRiskAnalysisSeed =
-    buildRiskAnalysisSeed(mockValidRiskAnalysis);
+  const mockValidRiskAnalysisId = generateId<RiskAnalysisId>();
 
   const makeRequest = async (
     token: string,
-    body: eserviceTemplateApi.EServiceRiskAnalysisSeed = riskAnalysisSeed,
     templateId: string = eserviceTemplateId,
-    riskAnlysisId: string = mockValidRiskAnalysis.id
+    riskAnlysisId: string = mockValidRiskAnalysisId
   ) =>
     request(api)
-      .post(`/templates/${templateId}/riskAnalysis/${riskAnlysisId}`)
+      .delete(`/templates/${templateId}/riskAnalysis/${riskAnlysisId}`)
       .set("Authorization", `Bearer ${token}`)
       .set("X-Correlation-Id", generateId())
-      .send(body);
+      .send();
 
   beforeEach(() => {
-    eserviceTemplateService.updateRiskAnalysis = vi
+    eserviceTemplateService.deleteRiskAnalysis = vi
       .fn()
       .mockResolvedValue(undefined);
   });
@@ -72,17 +56,8 @@ describe("API POST /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
     expect(res.status).toBe(403);
   });
 
-  it("Should return 400 if passed a not compliant body", async () => {
-    const token = generateToken(authRole.ADMIN_ROLE);
-    const res = await makeRequest(
-      token,
-      {} as eserviceTemplateApi.EServiceRiskAnalysisSeed
-    );
-    expect(res.status).toBe(400);
-  });
-
   it("Should return 404 for eserviceTemplateNotFound", async () => {
-    eserviceTemplateService.updateRiskAnalysis = vi
+    eserviceTemplateService.deleteRiskAnalysis = vi
       .fn()
       .mockRejectedValue(eServiceTemplateNotFound(eserviceTemplateId));
     const token = generateToken(authRole.ADMIN_ROLE);
@@ -94,7 +69,7 @@ describe("API POST /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
   });
 
   it("Should return 403 for operationForbidden", async () => {
-    eserviceTemplateService.updateRiskAnalysis = vi
+    eserviceTemplateService.deleteRiskAnalysis = vi
       .fn()
       .mockRejectedValue(operationForbidden);
     const token = generateToken(authRole.ADMIN_ROLE);
@@ -104,7 +79,7 @@ describe("API POST /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
   });
 
   it("Should return 400 for eserviceTemplateNotInDraftState", async () => {
-    eserviceTemplateService.updateRiskAnalysis = vi
+    eserviceTemplateService.deleteRiskAnalysis = vi
       .fn()
       .mockRejectedValue(eserviceTemplateNotInDraftState(eserviceTemplateId));
     const token = generateToken(authRole.ADMIN_ROLE);
@@ -116,7 +91,7 @@ describe("API POST /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
   });
 
   it("Should return 400 for eserviceTemplateNotInReceiveMode", async () => {
-    eserviceTemplateService.updateRiskAnalysis = vi
+    eserviceTemplateService.deleteRiskAnalysis = vi
       .fn()
       .mockRejectedValue(templateNotInReceiveMode(eserviceTemplateId));
     const token = generateToken(authRole.ADMIN_ROLE);
@@ -125,46 +100,5 @@ describe("API POST /templates/:templateId/riskAnalysis/:riskAnalysisId", () => {
       `EService Template ${eserviceTemplateId} is not in receive mode`
     );
     expect(res.status).toBe(400);
-  });
-
-  it("Should return 400 for riskAnalysisValidationFailed", async () => {
-    eserviceTemplateService.updateRiskAnalysis = vi.fn().mockRejectedValue(
-      riskAnalysisValidationFailed([
-        new RiskAnalysisValidationIssue({
-          code: "noRulesVersionFoundError",
-          detail: "no rule",
-        }),
-      ])
-    );
-    const token = generateToken(authRole.ADMIN_ROLE);
-    const res = await makeRequest(token);
-    expect(res.body.detail).toBe(
-      `Risk analysis validation failed. Reasons: [no rule]`
-    );
-    expect(res.status).toBe(400);
-  });
-
-  it("Should return 404 for tenantNotFound", async () => {
-    const tenantId = generateId<TenantId>();
-    eserviceTemplateService.updateRiskAnalysis = vi
-      .fn()
-      .mockRejectedValue(tenantNotFound(tenantId));
-    const token = generateToken(authRole.ADMIN_ROLE);
-    const res = await makeRequest(token);
-    expect(res.body.detail).toBe(`Tenant ${tenantId} not found`);
-    expect(res.status).toBe(404);
-  });
-
-  it("Should return 404 for tenantKindNotFound", async () => {
-    const tenantId = generateId<TenantId>();
-    eserviceTemplateService.updateRiskAnalysis = vi
-      .fn()
-      .mockRejectedValue(tenantKindNotFound(tenantId));
-    const token = generateToken(authRole.ADMIN_ROLE);
-    const res = await makeRequest(token);
-    expect(res.body.detail).toBe(
-      `Tenant kind for tenant ${tenantId} not found`
-    );
-    expect(res.status).toBe(404);
   });
 });
