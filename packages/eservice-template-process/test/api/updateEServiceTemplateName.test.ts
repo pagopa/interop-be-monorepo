92f5b9b34fb1603f108e552abce94b3e3b5a2df4
Sandro Tajè <sandro.taje@gmail.com>
2025-04-30 15:12:44 +0200

    Add update eservice template name

---
+++ packages/eservice-template-process/test/api/updateEServiceTemplateName.test.ts
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { generateId, operationForbidden } from "pagopa-interop-models";
+import {
+  generateToken,
+  getMockEServiceTemplate,
+} from "pagopa-interop-commons-test";
+import { AuthRole, authRole } from "pagopa-interop-commons";
+import request from "supertest";
+import { api, eserviceTemplateService } from "../vitest.api.setup.js";
+import {
+  eServiceTemplateNotFound,
+  eserviceTemplateWithoutPublishedVersion,
+} from "../../src/model/domain/errors.js";
+import { eserviceTemplateToApiEServiceTemplate } from "../../src/model/domain/apiConverter.js";
+
+describe("API POST /templates/:templateId/description/update", () => {
+  const mockEserviceTemplate = getMockEServiceTemplate();
+
+  const name = {
+    name: "name to be updated",
+  };
+
+  const makeRequest = async (
+    token: string,
+    seed: { name: string } = name,
+    templateId: string = mockEserviceTemplate.id
+  ) =>
+    request(api)
+      .post(`/templates/${templateId}/name/update`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send(seed);
+
+  beforeEach(() => {
+    eserviceTemplateService.updateEServiceTemplateName = vi
+      .fn()
+      .mockResolvedValue(mockEserviceTemplate);
+  });
+
+  const authorizedRoles: AuthRole[] = [authRole.ADMIN_ROLE, authRole.API_ROLE];
+  it.each(authorizedRoles)(
+    "Should return 200 for user with role %s",
+    async (role) => {
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+      expect(res.body).toEqual(
+        eserviceTemplateToApiEServiceTemplate(mockEserviceTemplate)
+      );
+      expect(res.status).toBe(200);
+    }
+  );
+
+  it.each(
+    Object.values(authRole).filter((role) => !authorizedRoles.includes(role))
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken(role);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 400 for not valid body", async () => {
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token, { name: "" });
+    expect(res.status).toBe(400);
+  });
+
+  it("Should return 404 for eserviceTemplateNotFound", async () => {
+    eserviceTemplateService.updateEServiceTemplateName = vi
+      .fn()
+      .mockRejectedValue(eServiceTemplateNotFound(mockEserviceTemplate.id));
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `EService Template ${mockEserviceTemplate.id} not found`
+    );
+    expect(res.status).toBe(404);
+  });
+
+  it("Should return 403 for operationForbidden", async () => {
+    eserviceTemplateService.updateEServiceTemplateName = vi
+      .fn()
+      .mockRejectedValue(operationForbidden);
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe("Insufficient privileges");
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 409 for eserviceTemplateWithoutPublishedVersion", async () => {
+    eserviceTemplateService.updateEServiceTemplateName = vi
+      .fn()
+      .mockRejectedValue(
+        eserviceTemplateWithoutPublishedVersion(mockEserviceTemplate.id)
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.body.detail).toBe(
+      `EService Template ${mockEserviceTemplate.id} does not have a published version`
+    );
+    expect(res.status).toBe(409);
+  });
+});
