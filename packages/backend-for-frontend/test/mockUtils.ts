f67853caadc4c9e3232b6774ee6ccf67a5d242b7
Matteo Grilli <matteo.grilli@quinck.io>
2025-05-02 10:58:17 +0200

    wip tests

--- packages/backend-for-frontend/test/mockUtils.ts
+++ packages/backend-for-frontend/test/mockUtils.ts
@@ -6,6 +6,8 @@ import {
   PurposeVersionId,
   TenantId,
   generateId,
+  DescriptorId,
+  EServiceId,
 } from "pagopa-interop-models";
 
 export const getMockBffApiPurpose = (): bffApi.Purpose & { id: PurposeId } => ({
@@ -181,4 +183,37 @@ export const getMockBffApiCompactOrganization =
     name: generateMock(z.string()),
     kind: generateMock(bffApi.TenantKind.optional()),
     contactMail: generateMock(bffApi.Mail.optional()),
+  });
+
+// export const getMockApiAgreementListEntry = (): bffApi.AgreementListEntry => ({
+//   id: generateId(),
+//   consumer: getMockTenant(),
+//   eservice: getMockEService(),
+//   canBeUpgraded: false,
+//   descriptor: getMockDescriptor(),
+//   state: "DRAFT",
+// });
+
+// const descriptorToCompactDescriptor = (
+//   descriptor: Descriptor
+// ): CompactDescriptor => ({
+//   id: descriptor.id,
+//   audience: descriptor.audience,
+//   state: descriptor.state,
+//   version: descriptor.version,
+// });
+
+export const getMockApiAgreementPayload = (): bffApi.AgreementPayload => ({
+  descriptorId: generateId<DescriptorId>(),
+  eserviceId: generateId<EServiceId>(),
+});
+
+export const getMockApiCreatedResource = (): bffApi.CreatedResource => ({
+  id: generateId(),
+});
+
+export const getMockApiCompactEServiceLight =
+  (): bffApi.CompactEServiceLight => ({
+    id: generateId<EServiceId>(),
+    name: "name",
   });
