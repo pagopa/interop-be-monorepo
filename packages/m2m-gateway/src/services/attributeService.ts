c37881d0c6c3d44c77aa06e4924dc0184aa46b36
Eric Camellini <eric.camellini@gmail.com>
2025-05-03 22:43:34 +0200

    Implementing the logic to create a certified attribute

--- packages/m2m-gateway/src/services/attributeService.ts
+++ packages/m2m-gateway/src/services/attributeService.ts
@@ -1,13 +1,35 @@
 import { WithLogger } from "pagopa-interop-commons";
-import { m2mGatewayApi } from "pagopa-interop-api-clients";
+import {
+  attributeRegistryApi,
+  m2mGatewayApi,
+} from "pagopa-interop-api-clients";
 import { PagoPAInteropBeClients } from "../clients/clientsProvider.js";
-import { M2MGatewayAppContext } from "../utils/context.js";
 import { toM2MGatewayApiCertifiedAttribute } from "../api/attributeApiConverter.js";
+import {
+  isPolledVersionAtLeastResponseVersion,
+  pollResource,
+} from "../utils/polling.js";
+import { WithMaybeMetadata } from "../clients/zodiosWithMetadataPatch.js";
+import { M2MGatewayAppContext } from "../utils/context.js";
 
 export type AttributeService = ReturnType<typeof attributeServiceBuilder>;
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function attributeServiceBuilder(clients: PagoPAInteropBeClients) {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const pollAttribute = (
+    response: WithMaybeMetadata<attributeRegistryApi.Attribute>,
+    headers: M2MGatewayAppContext["headers"]
+  ) =>
+    pollResource(() =>
+      clients.attributeProcessClient.getAttributeById({
+        params: { attributeId: response.data.id },
+        headers,
+      })
+    )({
+      checkFn: isPolledVersionAtLeastResponseVersion(response),
+    });
+
   return {
     async getCertifiedAttribute(
       attributeId: string,
@@ -21,6 +43,19 @@ export function attributeServiceBuilder(clients: PagoPAInteropBeClients) {
       });
 
       return toM2MGatewayApiCertifiedAttribute(response.data);
+    },
+    async createCertifiedAttribute(
+      seed: m2mGatewayApi.CertifiedAttributeSeed,
+      { headers }: WithLogger<M2MGatewayAppContext>
+    ): Promise<m2mGatewayApi.CertifiedAttribute> {
+      const response =
+        await clients.attributeProcessClient.createCertifiedAttribute(seed, {
+          headers,
+        });
+
+      const polledResource = await pollAttribute(response, headers);
+
+      return toM2MGatewayApiCertifiedAttribute(polledResource.data);
     },
   };
 }
