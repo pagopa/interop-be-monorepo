d5e4dbe6c4499ea7e5f9d3bf24cc8139d7789afc
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-05-06 10:10:26 +0200

    Run both read model tests

--- packages/ipa-certified-attributes-importer/vitest.sql.config.ts
+++ packages/ipa-certified-attributes-importer/vitest.sql.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     hookTimeout: 60000,
     fileParallelism: false,
     pool: "forks",
-    env: { FEATURE_FLAG_SQL: "false" },
+    env: { FEATURE_FLAG_SQL: "true" },
     watch: false,
   },
 });
