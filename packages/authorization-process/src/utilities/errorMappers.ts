baaa6bb707a2de421f801b853dda0d73e857d8c2
Alepazz <alessio.pazzani@gmail.com>
2025-05-07 15:09:48 +0200

    Fixed tests

--- packages/authorization-process/src/utilities/errorMappers.ts
+++ packages/authorization-process/src/utilities/errorMappers.ts
@@ -95,7 +95,12 @@ export const addClientAdminErrorMapper = (
 ): number =>
   match(error.code)
     .with("clientNotFound", () => HTTP_STATUS_NOT_FOUND)
-    .with("organizationNotAllowedOnClient", () => HTTP_STATUS_FORBIDDEN)
+    .with(
+      "organizationNotAllowedOnClient",
+      "clientKindNotAllowed",
+      "userWithoutSecurityPrivileges",
+      () => HTTP_STATUS_FORBIDDEN
+    )
     .with("userAlreadyClientAdmin", () => HTTP_STATUS_CONFLICT)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
--- packages/authorization-process/test/integration/setAdminToClient.test.ts
+++ packages/authorization-process/test/integration/setAdminToClient.test.ts
@@ -1,8 +1,9 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import {
   Client,
   ClientAdminSetV2,
   ClientId,
+  clientKind,
   generateId,
   TenantId,
   toClientV2,
@@ -14,20 +15,46 @@ import {
   getMockClient,
   getMockContext,
 } from "pagopa-interop-commons-test";
+import { selfcareV2ClientApi } from "pagopa-interop-api-clients";
 import {
   clientAdminAlreadyAssignedToUser,
+  clientKindNotAllowed,
   clientNotFound,
   organizationNotAllowedOnClient,
-} from "../src/model/domain/errors.js";
+  userWithoutSecurityPrivileges,
+} from "../../src/model/domain/errors.js";
 import {
   addOneClient,
   authorizationService,
   readLastAuthorizationEvent,
-} from "./integrationUtils.js";
+  selfcareV2Client,
+} from "../integrationUtils.js";
+
+function mockSelfcareV2ClientCall(
+  value: Awaited<
+    ReturnType<typeof selfcareV2Client.getInstitutionUsersByProductUsingGET>
+  >
+): void {
+  selfcareV2Client.getInstitutionUsersByProductUsingGET = vi.fn(
+    async () => value
+  );
+}
+
+const mockSelfCareUsers: selfcareV2ClientApi.UserResource = {
+  id: generateId(),
+  name: "test",
+  roles: [],
+  email: "test@test.it",
+  surname: "surname_test",
+};
 
 describe("setAdminToClient", () => {
-  const mockClient: Client = getMockClient();
+  const mockClient: Client = { ...getMockClient(), kind: clientKind.api };
   const adminId: UserId = generateId<UserId>();
+
+  beforeEach(() => {
+    mockSelfcareV2ClientCall([mockSelfCareUsers]);
+  });
 
   it("should write on event-store when adding admin to a client", async () => {
     await addOneClient(mockClient);
@@ -91,6 +118,38 @@ describe("setAdminToClient", () => {
       )
     ).rejects.toThrowError(
       organizationNotAllowedOnClient(authData.organizationId, mockClient.id)
+    );
+  });
+  it("should throw clientKindNotAllowed when client kind is not allowed", async () => {
+    const client: Client = getMockClient();
+    await addOneClient(client);
+    const authData = getMockAuthData(generateId<TenantId>());
+
+    await expect(
+      authorizationService.setAdminToClient(
+        {
+          adminId,
+          clientId: client.id,
+        },
+        getMockContext({ authData })
+      )
+    ).rejects.toThrowError(clientKindNotAllowed(client.id));
+  });
+  it("should throw userWithoutSecurityPrivileges when users length is 0", async () => {
+    mockSelfcareV2ClientCall([]);
+    const authData = getMockAuthData(generateId<TenantId>());
+    await addOneClient(mockClient);
+
+    await expect(
+      authorizationService.setAdminToClient(
+        {
+          adminId,
+          clientId: mockClient.id,
+        },
+        getMockContext({ authData })
+      )
+    ).rejects.toThrowError(
+      userWithoutSecurityPrivileges(authData.organizationId, authData.userId)
     );
   });
   it("should throw userAlreadyClientAdmin if adminId is already assigned to the client as admin", async () => {
