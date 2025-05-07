65282e40528b3054fa7ff5a07dac148848b7cd73
Alepazz <alessio.pazzani@gmail.com>
2025-05-07 10:33:40 +0200

    Added supertest to authorization-process && added addOrUpdateAdminClient.test.ts

---
+++ packages/authorization-process/test/api/addOrUpdateAdminClient.test.ts
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { generateToken, getMockClient } from "pagopa-interop-commons-test";
+import { authRole } from "pagopa-interop-commons";
+import request from "supertest";
+import { generateId, UserId } from "pagopa-interop-models";
+import { authorizationApi } from "pagopa-interop-api-clients";
+import { api, authorizationService } from "../vitest.api.setup.js";
+import { clientToApiClient } from "../../src/model/domain/apiConverter.js";
+import {
+  clientAdminAlreadyAssignedToUser,
+  clientNotFound,
+  organizationNotAllowedOnClient,
+} from "../../src/model/domain/errors.js";
+
+describe("API POST /clients/{clientId}/admin test", () => {
+  const mockClient = getMockClient();
+  const body = { adminId: generateId<UserId>() };
+  const makeRequest = async (token: string, clientId: string = mockClient.id) =>
+    request(api)
+      .post(`/clients/${clientId}/admin`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId())
+      .send(body);
+
+  const apiResponse = authorizationApi.Client.parse(
+    clientToApiClient(mockClient, { showUsers: false })
+  );
+
+  beforeEach(() => {
+    authorizationService.setAdminToClient = vi
+      .fn()
+      .mockResolvedValue(mockClient);
+  });
+
+  it("Should return 200 for user with role Admin", async () => {
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(apiResponse);
+  });
+
+  it.each(
+    Object.values(authRole).filter((role) => role !== authRole.ADMIN_ROLE)
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken(role);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 404 for clientNotFound", async () => {
+    authorizationService.setAdminToClient = vi
+      .fn()
+      .mockRejectedValue(clientNotFound(generateId()));
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(404);
+  });
+
+  it("Should return 403 for organizationNotAllowedOnClient", async () => {
+    authorizationService.setAdminToClient = vi
+      .fn()
+      .mockRejectedValue(
+        organizationNotAllowedOnClient(generateId(), mockClient.id)
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it("Should return 409 for userAlreadyClientAdmin", async () => {
+    authorizationService.setAdminToClient = vi
+      .fn()
+      .mockRejectedValue(
+        clientAdminAlreadyAssignedToUser(mockClient.id, body.adminId)
+      );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(409);
+  });
+});
