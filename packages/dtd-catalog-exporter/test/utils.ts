5bc5c5e80a5f840d283cc00ab8c84162c8a97cb3
Paolo Manca <paolo.manca@pagopa.it>
2025-05-09 19:21:42 +0200

    adjust dtd utils from datalake

--- packages/dtd-catalog-exporter/test/utils.ts
+++ packages/dtd-catalog-exporter/test/utils.ts
@@ -1,5 +1,22 @@
-import { setupTestContainersVitest } from "pagopa-interop-commons-test";
+import {
+  setupTestContainersVitest,
+  writeInReadmodel,
+} from "pagopa-interop-commons-test";
 import { afterEach, inject } from "vitest";
+import {
+  EService,
+  Tenant,
+  toReadModelEService,
+  toReadModelTenant,
+  Attribute,
+  toReadModelAttribute,
+} from "pagopa-interop-models";
+import { genericLogger } from "pagopa-interop-commons";
+import { z } from "zod";
+import { PublicEService } from "../src/models/models.js";
+import { dtdCatalogExporterServiceBuilder } from "../src/services/dtdCatalogExporterService.js";
+import { readModelServiceBuilder } from "../src/services/readModelService.js";
+import { config } from "../src/config/config.js";
 
 export const {
   cleanup,
@@ -7,9 +24,47 @@ export const {
   postgresDB: _,
   fileManager,
 } = await setupTestContainersVitest(
-  undefined,
+  inject("readModelConfig"),
   undefined,
   inject("fileManagerConfig")
 );
 
 afterEach(cleanup);
+
+const eservices = readModelRepository.eservices;
+const tenants = readModelRepository.tenants;
+const attributes = readModelRepository.attributes;
+
+const readModelService = readModelServiceBuilder(readModelRepository);
+
+export const dtdCatalogExporterService = dtdCatalogExporterServiceBuilder({
+  readModelService,
+  fileManager,
+  loggerInstance: genericLogger,
+});
+
+export const getExportedDtdPublicCatalogFromJson = async (): Promise<
+  PublicEService[]
+> => {
+  const data = await fileManager.get(
+    config.s3Bucket,
+    `${config.dtdCatalogStoragePath}/${config.dtdCatalogJsonFilename}`,
+    genericLogger
+  );
+
+  return z
+    .array(PublicEService)
+    .parse(JSON.parse(Buffer.from(data).toString()));
+};
+
+export const addOneEService = async (eservice: EService): Promise<void> => {
+  await writeInReadmodel(toReadModelEService(eservice), eservices);
+};
+
+export const addOneTenant = async (tenant: Tenant): Promise<void> => {
+  await writeInReadmodel(toReadModelTenant(tenant), tenants);
+};
+
+export const addOneAttribute = async (attribute: Attribute): Promise<void> => {
+  await writeInReadmodel(toReadModelAttribute(attribute), attributes);
+};
