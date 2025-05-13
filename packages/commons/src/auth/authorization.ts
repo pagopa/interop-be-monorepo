8b679966833162bf7b9699309444a5107d99d62d
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-05-13 12:05:35 +0200

    Fix wrong circular dependencies inauthorization.ts

--- packages/commons/src/auth/authorization.ts
+++ packages/commons/src/auth/authorization.ts
@@ -1,22 +1,17 @@
 import { unauthorizedError } from "pagopa-interop-models";
-import { P, match } from "ts-pattern";
+import { match, P } from "ts-pattern";
 import { z } from "zod";
 import { AppContext } from "../context/context.js";
+import { NonEmptyArray } from "../utils/arrays.js";
 import {
-  NonEmptyArray,
-  SystemRole,
-  systemRole,
-  UserRole,
-  userRole,
-} from "../index.js";
-import {
-  UIAuthData,
-  M2MAuthData,
-  InternalAuthData,
-  MaintenanceAuthData,
   AuthData,
+  InternalAuthData,
   M2MAdminAuthData,
+  M2MAuthData,
+  MaintenanceAuthData,
+  UIAuthData,
 } from "./authData.js";
+import { userRole, UserRole, systemRole, SystemRole } from "./roles.js";
 
 export const authRole = {
   ...userRole,
