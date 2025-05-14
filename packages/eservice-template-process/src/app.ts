3cf8dbc26d4aed8dc0f0f57ad4fa0ca2fd8df056
Sandro Tajè <sandro.taje@gmail.com>
2025-05-14 16:57:56 +0200

    Improve dependency injection

--- packages/eservice-template-process/src/app.ts
+++ packages/eservice-template-process/src/app.ts
@@ -1,6 +1,9 @@
 import {
+  ReadModelRepository,
   authenticationMiddleware,
   contextMiddleware,
+  initDB,
+  initFileManager,
   loggerMiddleware,
   zodiosCtx,
 } from "pagopa-interop-commons";
@@ -9,10 +12,61 @@ import {
   applicationAuditEndMiddleware,
 } from "pagopa-interop-application-audit";
 import { serviceName as modelsServiceName } from "pagopa-interop-models";
+import {
+  attributeReadModelServiceBuilder,
+  eserviceTemplateReadModelServiceBuilder,
+  makeDrizzleConnection,
+  tenantReadModelServiceBuilder,
+} from "pagopa-interop-readmodel";
 import eserviceTemplatesRouter from "./routers/EServiceTemplateRouter.js";
 import healthRouter from "./routers/HealthRouter.js";
 import { config } from "./config/config.js";
-import { EServiceTemplateService } from "./services/eserviceTemplateService.js";
+import {
+  EServiceTemplateService,
+  eserviceTemplateServiceBuilder,
+} from "./services/eserviceTemplateService.js";
+import { readModelServiceBuilder } from "./services/readModelService.js";
+import { readModelServiceBuilderSQL } from "./services/readModelServiceSQL.js";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const createDefaultEserviceTemplateService = async () => {
+  const readModelDB = makeDrizzleConnection(config);
+  const eserviceTemplateReadModelServiceSQL =
+    eserviceTemplateReadModelServiceBuilder(readModelDB);
+  const tenantReadModelServiceSQL = tenantReadModelServiceBuilder(readModelDB);
+  const attributeReadModelServiceSQL =
+    attributeReadModelServiceBuilder(readModelDB);
+
+  const oldReadModelService = readModelServiceBuilder(
+    ReadModelRepository.init(config)
+  );
+  const readModelServiceSQL = readModelServiceBuilderSQL({
+    readModelDB,
+    eserviceTemplateReadModelServiceSQL,
+    tenantReadModelServiceSQL,
+    attributeReadModelServiceSQL,
+  });
+  const readModelService =
+    config.featureFlagSQL &&
+    config.readModelSQLDbHost &&
+    config.readModelSQLDbPort
+      ? readModelServiceSQL
+      : oldReadModelService;
+
+  return eserviceTemplateServiceBuilder(
+    initDB({
+      username: config.eventStoreDbUsername,
+      password: config.eventStoreDbPassword,
+      host: config.eventStoreDbHost,
+      port: config.eventStoreDbPort,
+      database: config.eventStoreDbName,
+      schema: config.eventStoreDbSchema,
+      useSSL: config.eventStoreDbUseSSL,
+    }),
+    readModelService,
+    initFileManager(config)
+  );
+};
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function createApp(service?: EServiceTemplateService) {
@@ -21,7 +75,10 @@ export async function createApp(service?: EServiceTemplateService) {
   const router =
     service != null
       ? eserviceTemplatesRouter(zodiosCtx, service)
-      : eserviceTemplatesRouter(zodiosCtx);
+      : eserviceTemplatesRouter(
+          zodiosCtx,
+          await createDefaultEserviceTemplateService()
+        );
 
   const app = zodiosCtx.app();
 
