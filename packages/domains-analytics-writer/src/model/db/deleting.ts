2b8ca58501e41fee6100f533eab76f720e78c7b1
diego-lon <diego.longo@digitouch.it>
2025-05-19 18:27:32 +0200

    Removed extract prop to define tables

--- packages/domains-analytics-writer/src/model/db/deleting.ts
+++ packages/domains-analytics-writer/src/model/db/deleting.ts
@@ -4,36 +4,27 @@ import {
   eserviceInReadmodelCatalog,
   eserviceRiskAnalysisInReadmodelCatalog,
 } from "pagopa-interop-readmodel-models";
+
 import { AttributeDeletingSchema } from "../attribute/attribute.js";
 import { EserviceDeletingSchema } from "../catalog/eservice.js";
-
 import { EserviceRiskAnalysisDeletingSchema } from "../catalog/eserviceRiskAnalysis.js";
-import { extractProp } from "../../db/dbModelMetadataExtractor.js";
 
-const DeletingTableMeta = {
-  attribute_deleting_table: {
-    schema: AttributeDeletingSchema,
-    readModel: attributeInReadmodelAttribute,
-  },
-  catalog_deleting_table: {
-    schema: EserviceDeletingSchema,
-    readModel: eserviceInReadmodelCatalog,
-  },
-  catalog_risk_deleting_table: {
-    schema: EserviceRiskAnalysisDeletingSchema,
-    readModel: eserviceRiskAnalysisInReadmodelCatalog,
-  },
+export const DeletingDbTableConfig = {
+  attribute_deleting_table: AttributeDeletingSchema,
+  catalog_deleting_table: EserviceDeletingSchema,
+  catalog_risk_deleting_table: EserviceRiskAnalysisDeletingSchema,
 } as const;
-export const DeletingDbTableConfig = extractProp(DeletingTableMeta, "schema");
 export type DeletingDbTableConfig = typeof DeletingDbTableConfig;
 
-export const DeletingDbTableReadModel = extractProp(
-  DeletingTableMeta,
-  "readModel"
-);
+export const DeletingDbTableReadModel = {
+  attribute_deleting_table: attributeInReadmodelAttribute,
+  catalog_deleting_table: eserviceInReadmodelCatalog,
+  catalog_risk_deleting_table: eserviceRiskAnalysisInReadmodelCatalog,
+} as const;
 export type DeletingDbTableReadModel = typeof DeletingDbTableReadModel;
 
 export type DeletingDbTable = keyof DeletingDbTableConfig;
+
 export const DeletingDbTable = Object.fromEntries(
   Object.keys(DeletingDbTableConfig).map((k) => [k, k])
 ) as { [K in DeletingDbTable]: K };
