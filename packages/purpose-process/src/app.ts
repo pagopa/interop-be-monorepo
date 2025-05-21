bfaed25ee5be0ac195e190ac8db69d4242ee1c21
Tommaso Petrucciani <tommaso.petrucciani@icloud.com>
2025-05-21 10:36:39 +0200

    Move service init to index.ts

--- packages/purpose-process/src/app.ts
+++ packages/purpose-process/src/app.ts
@@ -1,10 +1,6 @@
 import {
-  ReadModelRepository,
   authenticationMiddleware,
   contextMiddleware,
-  initDB,
-  initFileManager,
-  initPDFGenerator,
   loggerMiddleware,
   zodiosCtx,
 } from "pagopa-interop-commons";
@@ -13,82 +9,16 @@ import {
   applicationAuditEndMiddleware,
 } from "pagopa-interop-application-audit";
 import { serviceName as modelsServiceName } from "pagopa-interop-models";
-import {
-  agreementReadModelServiceBuilder,
-  catalogReadModelServiceBuilder,
-  delegationReadModelServiceBuilder,
-  makeDrizzleConnection,
-  purposeReadModelServiceBuilder,
-  tenantReadModelServiceBuilder,
-} from "pagopa-interop-readmodel";
 import purposeRouter from "./routers/PurposeRouter.js";
 import healthRouter from "./routers/HealthRouter.js";
 import { config } from "./config/config.js";
-import {
-  PurposeService,
-  purposeServiceBuilder,
-} from "./services/purposeService.js";
-import { readModelServiceBuilder } from "./services/readModelService.js";
-import { readModelServiceBuilderSQL } from "./services/readModelServiceSQL.js";
+import { PurposeService } from "./services/purposeService.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const createDefaultPurposeService = async () => {
-  const readModelDB = makeDrizzleConnection(config);
-  const purposeReadModelServiceSQL =
-    purposeReadModelServiceBuilder(readModelDB);
-  const catalogReadModelServiceSQL =
-    catalogReadModelServiceBuilder(readModelDB);
-  const tenantReadModelServiceSQL = tenantReadModelServiceBuilder(readModelDB);
-  const agreementReadModelServiceSQL =
-    agreementReadModelServiceBuilder(readModelDB);
-  const delegationReadModelServiceSQL =
-    delegationReadModelServiceBuilder(readModelDB);
-
-  const oldReadModelService = readModelServiceBuilder(
-    ReadModelRepository.init(config)
-  );
-  const readModelServiceSQL = readModelServiceBuilderSQL({
-    readModelDB,
-    purposeReadModelServiceSQL,
-    catalogReadModelServiceSQL,
-    tenantReadModelServiceSQL,
-    agreementReadModelServiceSQL,
-    delegationReadModelServiceSQL,
-  });
-  const readModelService =
-    config.featureFlagSQL &&
-    config.readModelSQLDbHost &&
-    config.readModelSQLDbPort
-      ? readModelServiceSQL
-      : oldReadModelService;
-
-  const fileManager = initFileManager(config);
-  const pdfGenerator = await initPDFGenerator();
-
-  return purposeServiceBuilder(
-    initDB({
-      username: config.eventStoreDbUsername,
-      password: config.eventStoreDbPassword,
-      host: config.eventStoreDbHost,
-      port: config.eventStoreDbPort,
-      database: config.eventStoreDbName,
-      schema: config.eventStoreDbSchema,
-      useSSL: config.eventStoreDbUseSSL,
-    }),
-    readModelService,
-    fileManager,
-    pdfGenerator
-  );
-};
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function createApp(service?: PurposeService) {
+export async function createApp(service: PurposeService) {
   const serviceName = modelsServiceName.PURPOSE_PROCESS;
 
-  const router =
-    service != null
-      ? purposeRouter(zodiosCtx, service)
-      : purposeRouter(zodiosCtx, await createDefaultPurposeService());
+  const router = purposeRouter(zodiosCtx, service);
 
   const app = zodiosCtx.app();
 
--- packages/purpose-process/src/index.ts
+++ packages/purpose-process/src/index.ts
@@ -1,5 +1,67 @@
-import { startServer } from "pagopa-interop-commons";
+import {
+  ReadModelRepository,
+  initDB,
+  initFileManager,
+  initPDFGenerator,
+  startServer,
+} from "pagopa-interop-commons";
+import {
+  agreementReadModelServiceBuilder,
+  catalogReadModelServiceBuilder,
+  delegationReadModelServiceBuilder,
+  makeDrizzleConnection,
+  purposeReadModelServiceBuilder,
+  tenantReadModelServiceBuilder,
+} from "pagopa-interop-readmodel";
 import { config } from "./config/config.js";
 import { createApp } from "./app.js";
+import { readModelServiceBuilder } from "./services/readModelService.js";
+import { readModelServiceBuilderSQL } from "./services/readModelServiceSQL.js";
+import { purposeServiceBuilder } from "./services/purposeService.js";
 
-startServer(await createApp(), config);
+const readModelDB = makeDrizzleConnection(config);
+const purposeReadModelServiceSQL = purposeReadModelServiceBuilder(readModelDB);
+const catalogReadModelServiceSQL = catalogReadModelServiceBuilder(readModelDB);
+const tenantReadModelServiceSQL = tenantReadModelServiceBuilder(readModelDB);
+const agreementReadModelServiceSQL =
+  agreementReadModelServiceBuilder(readModelDB);
+const delegationReadModelServiceSQL =
+  delegationReadModelServiceBuilder(readModelDB);
+
+const oldReadModelService = readModelServiceBuilder(
+  ReadModelRepository.init(config)
+);
+const readModelServiceSQL = readModelServiceBuilderSQL({
+  readModelDB,
+  purposeReadModelServiceSQL,
+  catalogReadModelServiceSQL,
+  tenantReadModelServiceSQL,
+  agreementReadModelServiceSQL,
+  delegationReadModelServiceSQL,
+});
+const readModelService =
+  config.featureFlagSQL &&
+  config.readModelSQLDbHost &&
+  config.readModelSQLDbPort
+    ? readModelServiceSQL
+    : oldReadModelService;
+
+const fileManager = initFileManager(config);
+const pdfGenerator = await initPDFGenerator();
+
+const service = purposeServiceBuilder(
+  initDB({
+    username: config.eventStoreDbUsername,
+    password: config.eventStoreDbPassword,
+    host: config.eventStoreDbHost,
+    port: config.eventStoreDbPort,
+    database: config.eventStoreDbName,
+    schema: config.eventStoreDbSchema,
+    useSSL: config.eventStoreDbUseSSL,
+  }),
+  readModelService,
+  fileManager,
+  pdfGenerator
+);
+
+startServer(await createApp(service), config);
