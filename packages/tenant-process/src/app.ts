5df9f8c6291eca0b6a6836c13abb3ac81c5d7044
Tommaso Petrucciani <tommaso.petrucciani@icloud.com>
2025-05-21 17:30:37 +0200

    Move service init to index.ts

--- packages/tenant-process/src/app.ts
+++ packages/tenant-process/src/app.ts
@@ -1,8 +1,6 @@
 import {
-  ReadModelRepository,
   authenticationMiddleware,
   contextMiddleware,
-  initDB,
   loggerMiddleware,
   zodiosCtx,
 } from "pagopa-interop-commons";
@@ -11,74 +9,16 @@ import {
   applicationAuditEndMiddleware,
 } from "pagopa-interop-application-audit";
 import { serviceName as modelsServiceName } from "pagopa-interop-models";
-import {
-  agreementReadModelServiceBuilder,
-  attributeReadModelServiceBuilder,
-  catalogReadModelServiceBuilder,
-  delegationReadModelServiceBuilder,
-  makeDrizzleConnection,
-  tenantReadModelServiceBuilder,
-} from "pagopa-interop-readmodel";
 import healthRouter from "./routers/HealthRouter.js";
 import tenantRouter from "./routers/TenantRouter.js";
 import { config } from "./config/config.js";
-import {
-  TenantService,
-  tenantServiceBuilder,
-} from "./services/tenantService.js";
-import { readModelServiceBuilder } from "./services/readModelService.js";
-import { readModelServiceBuilderSQL } from "./services/readModelServiceSQL.js";
-//
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const createDefaultTenantService = async () => {
-  const db = makeDrizzleConnection(config);
-  const tenantReadModelServiceSQL = tenantReadModelServiceBuilder(db);
-  const agreementReadModelServiceSQL = agreementReadModelServiceBuilder(db);
-  const attributeReadModelServiceSQL = attributeReadModelServiceBuilder(db);
-  const catalogReadModelServiceSQL = catalogReadModelServiceBuilder(db);
-  const delegationReadModelServiceSQL = delegationReadModelServiceBuilder(db);
-
-  const readModelRepository = ReadModelRepository.init(config);
-
-  const oldReadModelService = readModelServiceBuilder(readModelRepository);
-  const readModelServiceSQL = readModelServiceBuilderSQL(
-    db,
-    tenantReadModelServiceSQL,
-    agreementReadModelServiceSQL,
-    attributeReadModelServiceSQL,
-    catalogReadModelServiceSQL,
-    delegationReadModelServiceSQL
-  );
-
-  const readModelService =
-    config.featureFlagSQL &&
-    config.readModelSQLDbHost &&
-    config.readModelSQLDbPort
-      ? readModelServiceSQL
-      : oldReadModelService;
-
-  return tenantServiceBuilder(
-    initDB({
-      username: config.eventStoreDbUsername,
-      password: config.eventStoreDbPassword,
-      host: config.eventStoreDbHost,
-      port: config.eventStoreDbPort,
-      database: config.eventStoreDbName,
-      schema: config.eventStoreDbSchema,
-      useSSL: config.eventStoreDbUseSSL,
-    }),
-    readModelService
-  );
-};
+import { TenantService } from "./services/tenantService.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function createApp(service?: TenantService) {
+export async function createApp(service: TenantService) {
   const serviceName = modelsServiceName.TENANT_PROCESS;
 
-  const router =
-    service != null
-      ? tenantRouter(zodiosCtx, service)
-      : tenantRouter(zodiosCtx, await createDefaultTenantService());
+  const router = tenantRouter(zodiosCtx, service);
 
   const app = zodiosCtx.app();
 
