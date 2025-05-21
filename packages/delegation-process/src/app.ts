15eb8026f998e8b42deb1311e16a0651fce1ee05
Tommaso Petrucciani <tommaso.petrucciani@icloud.com>
2025-05-21 18:03:16 +0200

    Move service init to index.ts

--- packages/delegation-process/src/app.ts
+++ packages/delegation-process/src/app.ts
@@ -1,10 +1,6 @@
 import {
-  ReadModelRepository,
   authenticationMiddleware,
   contextMiddleware,
-  initDB,
-  initFileManager,
-  initPDFGenerator,
   loggerMiddleware,
   zodiosCtx,
 } from "pagopa-interop-commons";
@@ -12,79 +8,17 @@ import {
   applicationAuditBeginMiddleware,
   applicationAuditEndMiddleware,
 } from "pagopa-interop-application-audit";
-import {
-  agreementReadModelServiceBuilder,
-  catalogReadModelServiceBuilder,
-  delegationReadModelServiceBuilder,
-  makeDrizzleConnection,
-  tenantReadModelServiceBuilder,
-} from "pagopa-interop-readmodel";
 import { serviceName as modelsServiceName } from "pagopa-interop-models";
 import healthRouter from "./routers/HealthRouter.js";
 import delegationRouter from "./routers/DelegationRouter.js";
 import { config } from "./config/config.js";
-import {
-  DelegationService,
-  delegationServiceBuilder,
-} from "./services/delegationService.js";
-import { readModelServiceBuilder } from "./services/readModelService.js";
-import { readModelServiceBuilderSQL } from "./services/readModelServiceSQL.js";
+import { DelegationService } from "./services/delegationService.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const createDefaultDelegationService = async () => {
-  const fileManager = initFileManager(config);
-  const pdfGenerator = await initPDFGenerator();
-
-  const readModelDB = makeDrizzleConnection(config);
-  const delegationReadModelServiceSQL =
-    delegationReadModelServiceBuilder(readModelDB);
-  const catalogReadModelServiceSQL =
-    catalogReadModelServiceBuilder(readModelDB);
-  const tenantReadModelServiceSQL = tenantReadModelServiceBuilder(readModelDB);
-  const agreementReadModelServiceSQL =
-    agreementReadModelServiceBuilder(readModelDB);
-
-  const oldReadModelService = readModelServiceBuilder(
-    ReadModelRepository.init(config)
-  );
-  const readModelServiceSQL = readModelServiceBuilderSQL({
-    readModelDB,
-    delegationReadModelServiceSQL,
-    catalogReadModelServiceSQL,
-    tenantReadModelServiceSQL,
-    agreementReadModelServiceSQL,
-  });
-  const readModelService =
-    config.featureFlagSQL &&
-    config.readModelSQLDbHost &&
-    config.readModelSQLDbPort
-      ? readModelServiceSQL
-      : oldReadModelService;
-
-  return delegationServiceBuilder(
-    initDB({
-      username: config.eventStoreDbUsername,
-      password: config.eventStoreDbPassword,
-      host: config.eventStoreDbHost,
-      port: config.eventStoreDbPort,
-      database: config.eventStoreDbName,
-      schema: config.eventStoreDbSchema,
-      useSSL: config.eventStoreDbUseSSL,
-    }),
-    readModelService,
-    pdfGenerator,
-    fileManager
-  );
-};
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function createApp(service?: DelegationService) {
+export async function createApp(service: DelegationService) {
   const serviceName = modelsServiceName.DELEGATION_PROCESS;
 
-  const router =
-    service != null
-      ? delegationRouter(zodiosCtx, service)
-      : delegationRouter(zodiosCtx, await createDefaultDelegationService());
+  const router = delegationRouter(zodiosCtx, service);
 
   const app = zodiosCtx.app();
 
