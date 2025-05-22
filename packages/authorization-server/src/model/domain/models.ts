082feb024d07ac73b1200e94e80c5723ab1a3481
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-05-22 12:10:11 +0200

    Add DPoP token type

--- packages/authorization-server/src/model/domain/models.ts
+++ packages/authorization-server/src/model/domain/models.ts
@@ -1,5 +1,15 @@
+import { z } from "zod";
 export interface InteropTokenResponse {
   access_token: string;
   token_type: string;
   expires_in: number;
 }
+export const tokenType = {
+  bearer: "Bearer",
+  dPoP: "DPoP",
+} as const;
+export const TokenType = z.enum([
+  Object.values(tokenType)[0],
+  ...Object.values(tokenType).slice(1),
+]);
+export type TokenType = z.infer<typeof TokenType>;
