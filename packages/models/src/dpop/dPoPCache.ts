0ba5d6cba1c5d775d17739de54f4f8dd8f3020e2
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-05-28 15:18:15 +0200

    Delete DPoP files

--- packages/models/src/dpop/dPoPCache.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const DPoPCache = z.object({
-  jti: z.string(),
-  iat: z.number().int().min(0),
-  ttl: z.number().int().min(0),
-});
-export type DPoPCache = z.infer<typeof DPoPCache>;
