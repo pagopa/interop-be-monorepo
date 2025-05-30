2fbc9df160da47aa700e9883e92de792187018dd
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-05-30 12:14:32 +0200

    Add AWS test credentials for DynamoDBClient

--- packages/dpop-validation/test/utils.ts
+++ packages/dpop-validation/test/utils.ts
@@ -3,6 +3,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 export const dynamoDBClient = new DynamoDBClient({
   endpoint: `http://localhost:8085`,
   region: "eu-south-1",
+  credentials: {
+    accessKeyId: "key",
+    secretAccessKey: "secret",
+  },
 });
 
 export const dpopCacheTable = "dpop-cache";
