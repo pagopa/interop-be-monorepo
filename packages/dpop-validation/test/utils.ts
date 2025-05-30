1d61b714b889dbf2515d21185e8d0f98fe9e2a46
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-05-30 22:22:04 +0200

    Fix test configs

--- packages/dpop-validation/test/utils.ts
+++ packages/dpop-validation/test/utils.ts
@@ -1,12 +1,22 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { genericInternalError } from "pagopa-interop-models";
+import { inject } from "vitest";
 import { DPoPConfig } from "../src/config.js";
 
-const config = DPoPConfig.safeParse(process.env);
-if (!config.success) {
-  throw genericInternalError("Invalid config");
+const tokenGenReadModelConfig = inject("tokenGenerationReadModelConfig");
+if (!tokenGenReadModelConfig) {
+  throw genericInternalError(
+    "Token generation read model config is not defined"
+  );
 }
 
-export const dynamoDBClient = new DynamoDBClient();
+export const dynamoDBClient = new DynamoDBClient({
+  endpoint: `http://localhost:${tokenGenReadModelConfig.tokenGenerationReadModelDbPort}`,
+});
 
-export const dpopCacheTable = config.data.dpopCacheTable;
+const dpopConfig = DPoPConfig.safeParse(process.env);
+if (!dpopConfig.success) {
+  throw genericInternalError("Invalid DPoP config");
+}
+
+export const dpopCacheTable = dpopConfig.data.dpopCacheTable;
