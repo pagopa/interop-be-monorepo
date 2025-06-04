8400dc3e8f814642ec963cb4cbeb4110fcc0da78
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-06-04 10:09:18 +0200

    Delete `TokenRequest` type

--- packages/authorization-server/src/model/domain/models.ts
+++ packages/authorization-server/src/model/domain/models.ts
@@ -1,13 +1,5 @@
-import { IncomingHttpHeaders } from "http";
-import { authorizationServerApi } from "pagopa-interop-api-clients";
-
 export interface InteropTokenResponse {
   access_token: string;
   token_type: string;
   expires_in: number;
 }
-
-export type TokenRequest = {
-  headers: IncomingHttpHeaders & { DPoP?: string };
-  body: authorizationServerApi.AccessTokenRequest;
-};
