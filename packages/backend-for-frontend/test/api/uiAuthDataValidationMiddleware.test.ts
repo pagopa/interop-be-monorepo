d5cb8cc1c76ca229a52cdf14acaabb2012bbfaac
Matteo Grilli <matteo.grilli@quinck.io>
2025-06-09 16:00:12 +0200

    reverted changes to test uiAuthDataValidationMiddleware

--- packages/backend-for-frontend/test/api/uiAuthDataValidationMiddleware.test.ts
+++ packages/backend-for-frontend/test/api/uiAuthDataValidationMiddleware.test.ts
@@ -2,7 +2,7 @@
 import { describe, it, expect, vi } from "vitest";
 import { generateId } from "pagopa-interop-models";
 import { generateToken } from "pagopa-interop-commons-test";
-import { AuthRole, authRole } from "pagopa-interop-commons";
+import { systemRole, userRole } from "pagopa-interop-commons";
 import request from "supertest";
 import { api, services } from "../vitest.api.setup.js";
 import { appBasePath } from "../../src/config/appBasePath.js";
@@ -20,14 +20,7 @@ describe("uiAuthDataValidationMiddleware", () => {
     .fn()
     .mockResolvedValue(getMockBffApiPurpose());
 
-  const authorizedRoles: AuthRole[] = [
-    authRole.ADMIN_ROLE,
-    authRole.SECURITY_ROLE,
-    authRole.API_ROLE,
-    authRole.SUPPORT_ROLE,
-  ];
-
-  it.each(authorizedRoles)(
+  it.each(Object.values(userRole))(
     "Should correctly accept tokens with role %s",
     async (role) => {
       const token = generateToken(role);
@@ -36,11 +29,12 @@ describe("uiAuthDataValidationMiddleware", () => {
     }
   );
 
-  it.each(
-    Object.values(authRole).filter((role) => !authorizedRoles.includes(role))
-  )("Should return 403 for user with role %s", async (role) => {
-    const token = generateToken(role);
-    const res = await makeRequest(token);
-    expect(res.status).toBe(403);
-  });
+  it.each(Object.values(systemRole))(
+    "Should return 403 for user with role %s",
+    async (role) => {
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+      expect(res.status).toBe(403);
+    }
+  );
 });
