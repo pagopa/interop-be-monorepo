a71f7d51b3a9634821722985616425b12e4a833b
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-06-11 15:56:49 +0200

    Improve mockedPayloadForToken functions

--- packages/commons-test/src/mockedPayloadForToken.ts
+++ packages/commons-test/src/mockedPayloadForToken.ts
@@ -1,15 +1,22 @@
+import jwt from "jsonwebtoken";
 import {
   AuthRole,
-  userRole,
   InteropJwtMaintenancePayload,
   SerializedAuthTokenPayload,
-  SerializedInteropJwtInternalPayload,
   SerializedInteropJwtApiPayload,
+  SerializedInteropJwtInternalPayload,
   SerializedInteropJwtUIPayload,
+  systemRole,
+  userRole,
 } from "pagopa-interop-commons";
-import { ClientId, TenantId, UserId, generateId } from "pagopa-interop-models";
+import {
+  ClientId,
+  TenantId,
+  UserId,
+  generateId,
+  unsafeBrandId,
+} from "pagopa-interop-models";
 import { match } from "ts-pattern";
-import jwt from "jsonwebtoken";
 
 export const mockTokenOrganizationId = generateId<TenantId>();
 
@@ -18,7 +25,7 @@ function createUserPayload(
 ): SerializedInteropJwtUIPayload {
   return {
     iss: "dev.interop.pagopa.it",
-    aud: "dev.interop.pagopa.it/ui",
+    aud: "dev.interop.pagopa.it/ui,interop.pagopa.it/ui",
     exp: Math.floor(Date.now() / 1000) + 3600,
     nbf: Math.floor(Date.now() / 1000),
     iat: Math.floor(Date.now() / 1000),
@@ -52,12 +59,12 @@ function createUserPayload(
 function createMaintenancePayload(): InteropJwtMaintenancePayload {
   return {
     iss: "dev.interop.pagopa.it",
-    aud: ["dev.interop.pagopa.it/ui"],
+    aud: ["dev.interop.pagopa.it/maintenance", "interop.pagopa.it/maintenance"],
     exp: Math.floor(Date.now() / 1000) + 3600,
     nbf: Math.floor(Date.now() / 1000),
     iat: Math.floor(Date.now() / 1000),
     jti: "1bca86f5-e913-4fce-bc47-2803bde44d2b",
-    role: "maintenance",
+    role: systemRole.MAINTENANCE_ROLE,
     sub: "interop.testing",
   };
 }
@@ -65,12 +72,12 @@ function createMaintenancePayload(): InteropJwtMaintenancePayload {
 function createM2MPayload(): SerializedInteropJwtApiPayload {
   return {
     iss: "dev.interop.pagopa.it",
-    aud: "dev.interop.pagopa.it/ui",
+    aud: "dev.interop.pagopa.it/m2m,interop.pagopa.it/m2m",
     exp: Math.floor(Date.now() / 1000) + 3600,
     nbf: Math.floor(Date.now() / 1000),
     iat: Math.floor(Date.now() / 1000),
     jti: "1bca86f5-e913-4fce-bc47-2803bde44d2b",
-    role: "m2m",
+    role: systemRole.M2M_ROLE,
     organizationId: mockTokenOrganizationId,
     client_id: generateId(),
     sub: generateId(),
@@ -85,8 +92,42 @@ function createInternalPayload(): SerializedInteropJwtInternalPayload {
     nbf: Math.floor(Date.now() / 1000),
     iat: Math.floor(Date.now() / 1000),
     jti: "1bca86f5-e913-4fce-bc47-2803bde44d2b",
-    role: "internal",
+    role: systemRole.INTERNAL_ROLE,
     sub: "interop.testing",
+  };
+}
+
+export function createUIPayload(): SerializedInteropJwtUIPayload {
+  return {
+    iss: "dev.interop.pagopa.it",
+    externalId: {
+      origin: "IPA",
+      value: "5N2TR557",
+    },
+    "user-roles": [userRole.SECURITY_ROLE, userRole.API_ROLE].join(","),
+    selfcareId: unsafeBrandId("1962d21c-c701-4805-93f6-53a877898756"),
+    organizationId: unsafeBrandId("69e2865e-65ab-4e48-a638-2037a9ee2ee7"),
+    aud: "dev.interop.pagopa.it/ui,interop.pagopa.it/ui",
+    uid: unsafeBrandId("f07ddb8f-17f9-47d4-b31e-35d1ac10e521"),
+    nbf: 1710841859,
+    organization: {
+      id: unsafeBrandId("1962d21c-c701-4805-93f6-53a877898756"),
+      name: "PagoPA S.p.A.",
+      roles: [
+        {
+          partyRole: "MANAGER",
+          role: userRole.ADMIN_ROLE,
+        },
+      ],
+      fiscal_code: "15376371009",
+      ipaCode: "5N2TR557",
+    },
+    name: "Mario",
+    exp: 1710928259,
+    iat: 1710841859,
+    family_name: "Rossi",
+    jti: "e82bd774-9cac-4885-931b-015b2eb4e9a5",
+    email: "m.rossi@psp.it",
   };
 }
 
@@ -98,12 +139,12 @@ export const mockM2MAdminUserId: UserId = generateId();
 function createM2MAdminPayload(): SerializedInteropJwtApiPayload {
   return {
     iss: "dev.interop.pagopa.it",
-    aud: "dev.interop.pagopa.it/ui",
+    aud: "dev.interop.pagopa.it/m2m,interop.pagopa.it/m2m",
     exp: Math.floor(Date.now() / 1000) + 3600,
     nbf: Math.floor(Date.now() / 1000),
     iat: Math.floor(Date.now() / 1000),
     jti: "1bca86f5-e913-4fce-bc47-2803bde44d2b",
-    role: "m2m-admin",
+    role: systemRole.M2M_ADMIN_ROLE,
     organizationId: mockTokenOrganizationId,
     client_id: mockM2MAdminClientId,
     sub: mockM2MAdminClientId,
@@ -127,3 +168,7 @@ export const createPayload = (
 
 export const generateToken = (role: AuthRole): string =>
   jwt.sign(createPayload(role), "test-secret");
+
+export const signPayload = (
+  payload: object // for testing purposes, allowing signature of any payload, including invalid ones
+): string => jwt.sign(payload, "test-secret");
