0081c9462cb534702054d73fedeb9e00ab8a9168
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-06-13 09:43:28 +0200

    Fix risk analysis answers joins in purpose queries

--- packages/readmodel/src/purposeReadModelService.ts
+++ packages/readmodel/src/purposeReadModelService.ts
@@ -120,9 +120,15 @@ export function purposeReadModelServiceBuilder(db: DrizzleReturnType) {
         .leftJoin(
           // 2
           purposeRiskAnalysisAnswerInReadmodelPurpose,
-          eq(
-            purposeRiskAnalysisFormInReadmodelPurpose.id,
-            purposeRiskAnalysisAnswerInReadmodelPurpose.riskAnalysisFormId
+          and(
+            eq(
+              purposeInReadmodelPurpose.id,
+              purposeRiskAnalysisAnswerInReadmodelPurpose.purposeId
+            ),
+            eq(
+              purposeRiskAnalysisFormInReadmodelPurpose.id,
+              purposeRiskAnalysisAnswerInReadmodelPurpose.riskAnalysisFormId
+            )
           )
         )
         .leftJoin(
@@ -175,9 +181,15 @@ export function purposeReadModelServiceBuilder(db: DrizzleReturnType) {
         )
         .leftJoin(
           purposeRiskAnalysisAnswerInReadmodelPurpose,
-          eq(
-            purposeRiskAnalysisFormInReadmodelPurpose.id,
-            purposeRiskAnalysisAnswerInReadmodelPurpose.riskAnalysisFormId
+          and(
+            eq(
+              purposeInReadmodelPurpose.id,
+              purposeRiskAnalysisAnswerInReadmodelPurpose.purposeId
+            ),
+            eq(
+              purposeRiskAnalysisFormInReadmodelPurpose.id,
+              purposeRiskAnalysisAnswerInReadmodelPurpose.riskAnalysisFormId
+            )
           )
         )
         .leftJoin(
