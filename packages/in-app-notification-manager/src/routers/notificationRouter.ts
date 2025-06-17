e5b1506122fb930908ee961643266492d9f2a4fd
Sandro Tajè <sandro.taje@gmail.com>
2025-06-17 17:08:25 +0200

    Add offset,limit and filters

--- packages/in-app-notification-manager/src/routers/notificationRouter.ts
+++ packages/in-app-notification-manager/src/routers/notificationRouter.ts
@@ -30,8 +30,14 @@ export const notificationRouter = (
     try {
       validateAuthorization(ctx, [ADMIN_ROLE, API_ROLE]);
 
-      const notifications = await service.getNotifications(ctx);
-      return res.status(200).json(notifications);
+      const { limit, offset, q } = req.query;
+      const notifications = await service.getNotifications(
+        q,
+        limit,
+        offset,
+        ctx
+      );
+      return res.status(200).send(notifications);
     } catch (error) {
       const errorRes = makeApiProblem(
         error,
--- packages/in-app-notification-manager/src/services/inAppNotificationService.ts
+++ packages/in-app-notification-manager/src/services/inAppNotificationService.ts
@@ -1,27 +1,39 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { AppContext, UIAuthData, WithLogger } from "pagopa-interop-commons";
-import { and, eq, desc } from "drizzle-orm";
+import { and, eq, ilike, desc } from "drizzle-orm";
+import { ListResult } from "pagopa-interop-models";
 import { notification, Notification } from "../db/schema.js";
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function inAppNotificationServiceBuilder(
   db: ReturnType<typeof drizzle>
 ) {
   return {
-    getNotifications: ({
-      logger,
-      authData: { userId, organizationId },
-    }: WithLogger<AppContext<UIAuthData>>): Promise<Notification[]> => {
+    getNotifications: async (
+      q: string | undefined,
+      limit: number,
+      offset: number,
+      {
+        logger,
+        authData: { userId, organizationId },
+      }: WithLogger<AppContext<UIAuthData>>
+    ): Promise<ListResult<Notification>> => {
       logger.info("Getting notifications");
-      return db
+      const notifications = await db
         .select()
         .from(notification)
         .where(
           and(
             eq(notification.userId, userId),
-            eq(notification.tenantId, organizationId)
+            eq(notification.tenantId, organizationId),
+            ilike(notification.body, `%${q ?? ""}%`)
           )
         )
         .orderBy(desc(notification.createdAt));
+
+      return {
+        results: notifications.slice(offset, offset + limit),
+        totalCount: notifications.length,
+      };
     },
   };
 }
