ada96fa485b8f5419e9f7cdab67cfb72d362396c
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-06-19 10:18:23 +0200

    Fix `dpopProofInvalidClaims` in tests

--- packages/dpop-validation/test/validation.test.ts
+++ packages/dpop-validation/test/validation.test.ts
@@ -62,7 +62,9 @@ describe("DPoP validation tests", async () => {
 
       expect(errors).toBeDefined();
       expect(errors).toHaveLength(1);
-      expect(errors?.[0].code).toBe(dpopProofInvalidClaims("{}").code);
+      expect(errors?.[0].code).toBe(
+        dpopProofInvalidClaims("{}", "header").code
+      );
     });
 
     it("should add error if there are invalid claims in the DPoP proof payload", async () => {
@@ -80,7 +82,9 @@ describe("DPoP validation tests", async () => {
 
       expect(errors).toBeDefined();
       expect(errors).toHaveLength(1);
-      expect(errors?.[0].code).toBe(dpopProofInvalidClaims("{}").code);
+      expect(errors?.[0].code).toBe(
+        dpopProofInvalidClaims("{}", "payload").code
+      );
     });
 
     it("should not add error if the DPoP proof signature is wrong", async () => {
