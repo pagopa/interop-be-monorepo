f89cc3f1647fc3ac190afd72101850cff6f6f203
Sandro Tajè <sandro.taje@gmail.com>
2025-06-19 09:58:02 +0200

    Add in app notification db config

--- packages/in-app-notification-manager/src/config/config.ts
+++ packages/in-app-notification-manager/src/config/config.ts
@@ -3,26 +3,11 @@ import {
   CommonHTTPServiceConfig,
 } from "pagopa-interop-commons";
 import { z } from "zod";
+import { InAppNotificationDBConfig } from "pagopa-interop-commons";
 
 const InAppNotificationManagerConfig = CommonHTTPServiceConfig.and(
   ApplicationAuditProducerConfig
-).and(
-  z
-    .object({
-      DB_HOST: z.string(),
-      DB_NAME: z.string(),
-      DB_USERNAME: z.string(),
-      DB_PASSWORD: z.string(),
-      DB_PORT: z.coerce.number().min(1001),
-    })
-    .transform((c) => ({
-      dbHost: c.DB_HOST,
-      dbName: c.DB_NAME,
-      dbUsername: c.DB_USERNAME,
-      dbPassword: c.DB_PASSWORD,
-      dbPort: c.DB_PORT,
-    }))
-);
+).and(InAppNotificationDBConfig);
 
 export type InAppNotificationManagerConfig = z.infer<
   typeof InAppNotificationManagerConfig
