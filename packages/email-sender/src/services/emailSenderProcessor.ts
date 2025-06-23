31e104bffb6cfde27744e4dac5e60e330d9d667f
Matteo Grilli <matteo.grilli@quinck.io>
2025-06-23 16:48:35 +0200

    added test
    still has issues

--- packages/email-sender/src/services/emailSenderProcessor.ts
+++ packages/email-sender/src/services/emailSenderProcessor.ts
@@ -1,8 +1,9 @@
 import { genericInternalError } from "pagopa-interop-models";
 import { EachMessagePayload } from "kafkajs";
-import { EmailManagerSES, Logger } from "pagopa-interop-commons";
+import { delay, EmailManagerSES, Logger } from "pagopa-interop-commons";
 import Mail from "nodemailer/lib/mailer/index.js";
 import { TooManyRequestsException } from "@aws-sdk/client-sesv2";
+import { config } from "../config/config.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function emailSenderProcessorBuilder(
@@ -15,41 +16,41 @@ export function emailSenderProcessorBuilder(
       message,
       partition,
     }: EachMessagePayload): Promise<void> {
-      try {
-        loggerInstance.info(
-          `Consuming message for partition ${partition} with offset ${message.offset}`
-        );
-
-        if (!message.value) {
-          loggerInstance.warn(
-            `Empty message for partition ${partition} with offset ${message.offset}`
+      const attempts = 0;
+      while (attempts < config.maxAttempts) {
+        try {
+          loggerInstance.info(
+            `Consuming message for partition ${partition} with offset ${message.offset}`
           );
-          return;
-        }
 
-        const jsonPayload = JSON.parse(message.value.toString());
+          if (!message.value) {
+            loggerInstance.warn(
+              `Empty message for partition ${partition} with offset ${message.offset}`
+            );
+            return;
+          }
 
-        // TODO: compose mail options. jsonPayload fields are still tentative, as
-        // I don't know/remember what the message from the dispatcher looks like
-        const mailOptions: Mail.Options = {
-          from: { name: sesSenderData.label, address: sesSenderData.mail },
-          subject: jsonPayload.subject,
-          to: [jsonPayload.address],
-          html: jsonPayload.html,
-        };
+          const jsonPayload = JSON.parse(message.value.toString());
 
-        // TODO: try sending email <-- this requires some more info. It's too generic like this
-        loggerInstance.info(`Sending email`);
-        await sesEmailManager.send(mailOptions);
-        loggerInstance.info(`Email sent`);
-      } catch (err) {
-        if (err instanceof TooManyRequestsException) {
-          // Manage reaching rate limit...
-          // Not sure how consuming an event works
-        } else {
-          throw genericInternalError(
-            `Error consuming message in partition ${partition} with offset ${message.offset}. Reson: ${err}`
-          );
+          const mailOptions: Mail.Options = {
+            from: { name: sesSenderData.label, address: sesSenderData.mail },
+            subject: jsonPayload.subject,
+            to: [jsonPayload.address],
+            html: jsonPayload.body,
+          };
+
+          // TODO: try sending email <-- this requires some more info. It's too generic like this
+          loggerInstance.info(`Sending email`);
+          await sesEmailManager.send(mailOptions);
+          loggerInstance.info(`Email sent`);
+        } catch (err) {
+          if (err instanceof TooManyRequestsException) {
+            await delay(config.retryDelay);
+          } else {
+            throw genericInternalError(
+              `Error consuming message in partition ${partition} with offset ${message.offset}. Reson: ${err}`
+            );
+          }
         }
       }
     },
