cca6b67c7551332db2799cafd2a8fc157b113633
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-07-02 15:42:44 +0200

    Revert change

--- packages/purpose-process/test/api/updateReversePurpose.test.ts
+++ packages/purpose-process/test/api/updateReversePurpose.test.ts
@@ -64,7 +64,10 @@ describe("API POST /reverse/purposes/{purposeId} test", () => {
       .set("X-Correlation-Id", generateId())
       .send(body);
 
-  const authorizedRoles: AuthRole[] = [authRole.ADMIN_ROLE];
+  const authorizedRoles: AuthRole[] = [
+    authRole.ADMIN_ROLE,
+    authRole.M2M_ADMIN_ROLE,
+  ];
 
   it.each(authorizedRoles)(
     "Should return 200 for user with role %s",
