aa36395dee62655abfc84ac175135747fe43bfc1
Matteo Grilli <matteo.grilli@quinck.io>
2025-07-03 10:49:19 +0200

    rimosso SendingPausedException dai casi gestiti con retry

--- packages/email-sender/src/services/emailSenderProcessor.ts
+++ packages/email-sender/src/services/emailSenderProcessor.ts
@@ -124,7 +124,6 @@ export function emailSenderProcessorBuilder(
           switch (true) {
             case err instanceof LimitExceededException:
             case err instanceof TooManyRequestsException:
-            case err instanceof SendingPausedException:
               await delay(config.retryDelayInMillis);
               break;
             default:
