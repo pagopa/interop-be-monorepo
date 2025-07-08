e5410a811671435e1197aaf07ef5abc150f47b76
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-07-08 18:11:37 +0200

    PIN-7088 - APIV2 `DELETE /purposes/:purposeId` (#2075)
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- packages/m2m-gateway/src/routers/purposeRouter.ts
+++ packages/m2m-gateway/src/routers/purposeRouter.ts
@@ -313,6 +313,27 @@ const purposeRouter = (
         );
         return res.status(errorRes.status).send(errorRes);
       }
+    })
+    .delete("/purposes/:purposeId", async (req, res) => {
+      const ctx = fromM2MGatewayAppContext(req.ctx, req.headers);
+      try {
+        validateAuthorization(ctx, [M2M_ADMIN_ROLE]);
+
+        await purposeService.deletePurpose(
+          unsafeBrandId(req.params.purposeId),
+          ctx
+        );
+
+        return res.status(204).send();
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          emptyErrorMapper,
+          ctx,
+          `Error deleting purpose with id ${req.params.purposeId}`
+        );
+        return res.status(errorRes.status).send(errorRes);
+      }
     });
 
   return purposeRouter;
