5ff27e089e8a31c7aa4752fee72121d8bc33738d
Matteo Grilli <matteo.grilli@quinck.io>
2025-07-09 15:54:00 +0200

    setup email-notification-dispatcher

---
+++ packages/email-notification-dispatcher/src/config/config.ts
@@ -0,0 +1,46 @@
+import {
+  AgreementTopicConfig,
+  KafkaConsumerConfig,
+  ReadModelDbConfig,
+  AWSSesConfig,
+  PurposeTopicConfig,
+  CatalogTopicConfig,
+  FeatureFlagSQLConfig,
+  ReadModelSQLDbConfig,
+  KafkaProducerConfig,
+  EmailSenderTopicConfig,
+} from "pagopa-interop-commons";
+import { z } from "zod";
+
+export const SESEmailSenderConfig = z
+  .object({
+    INTEROP_FE_BASE_URL: z.string(),
+    SENDER_MAIL: z.string().email(),
+    SENDER_LABEL: z.string(),
+  })
+  .transform((c) => ({
+    interopFeBaseUrl: c.INTEROP_FE_BASE_URL,
+    senderMail: c.SENDER_MAIL,
+    senderLabel: c.SENDER_LABEL,
+  }));
+export type SESEmailSenderConfig = z.infer<typeof SESEmailSenderConfig>;
+
+export const NotificationEmailSenderConfig = KafkaConsumerConfig.and(
+  KafkaProducerConfig
+)
+  .and(ReadModelDbConfig)
+  .and(AgreementTopicConfig)
+  .and(AWSSesConfig)
+  .and(SESEmailSenderConfig)
+  .and(PurposeTopicConfig)
+  .and(CatalogTopicConfig)
+  .and(FeatureFlagSQLConfig)
+  .and(ReadModelSQLDbConfig.optional()) // TODO: something to fix here. We keep the SQL config for sure, but did we rename it or something?
+  .and(EmailSenderTopicConfig);
+
+export type NotificationEmailSenderConfig = z.infer<
+  typeof NotificationEmailSenderConfig
+>;
+
+export const config: NotificationEmailSenderConfig =
+  NotificationEmailSenderConfig.parse(process.env);
