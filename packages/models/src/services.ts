f7b2314211b6895f0e6b9498f6d32664d4a14c9c
Sandro Tajè <sandro.taje@gmail.com>
2025-07-10 12:45:22 +0200

    PIN-7006 in app notification manager (#2065)

--- packages/models/src/services.ts
+++ packages/models/src/services.ts
@@ -13,6 +13,7 @@ export const serviceName = {
   DELEGATION_PROCESS: "delegation-process",
   ESERVICE_TEMPLATE_PROCESS: "eservice-template-process",
   M2M_GATEWAY: "m2m-gateway",
+  IN_APP_NOTIFICATION_MANAGER: "in-app-notification-manager",
   NOTIFICATION_CONFIG_PROCESS: "notification-config-process",
 } as const;
 
@@ -35,5 +36,6 @@ export const serviceErrorCode: Record<ServiceName, string> = {
   [serviceName.DELEGATION_PROCESS]: "010",
   [serviceName.ESERVICE_TEMPLATE_PROCESS]: "011",
   [serviceName.M2M_GATEWAY]: "012",
+  [serviceName.IN_APP_NOTIFICATION_MANAGER]: "013",
   [serviceName.NOTIFICATION_CONFIG_PROCESS]: "014",
 };
