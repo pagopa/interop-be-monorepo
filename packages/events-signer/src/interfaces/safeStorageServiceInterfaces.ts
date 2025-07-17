f50da879594c27012f42609856d4ee43e4377ba9
Giorgio Ceccherini <giorgio.ceccherini@digitouch.it>
2025-07-17 10:09:02 +0200

    [SafeStorageService] WIP: created service, tests.

---
+++ packages/events-signer/src/interfaces/safeStorageServiceInterfaces.ts
@@ -0,0 +1,50 @@
+export interface FileCreationRequest {
+  contentType: string;
+  documentType: string;
+  status: string;
+  checksumValue?: string;
+}
+
+export interface FileCreationResponse {
+  uploadMethod: "PUT" | "POST";
+  uploadUrl: string;
+  secret: string;
+  key: string;
+}
+
+export interface FileDownloadResponse {
+  key: string;
+  versionId: string;
+  contentType: string;
+  contentLength: number;
+  checksum: string;
+  documentType: string;
+  documentStatus: string;
+  retentionUntil: string;
+  download?: {
+    url?: string;
+    retryAfter?: number;
+  };
+}
+
+export interface SafeStorageApiClient {
+  createFile: (request: FileCreationRequest) => Promise<FileCreationResponse>;
+  uploadFileContent: (
+    uploadUrl: string,
+    fileContent: Buffer,
+    contentType: string,
+    secret: string,
+    checksumValue: string
+  ) => Promise<void>;
+  getFile: (
+    fileKey: string,
+    metadataOnly?: boolean
+  ) => Promise<FileDownloadResponse>;
+  downloadFileContent: (downloadUrl: string) => Promise<Buffer>;
+}
+
+export interface SafeStorageApiConfig {
+  safeStorageBaseUrl: string;
+  safeStorageApiKey: string;
+  safeStorageClientId: string;
+}
