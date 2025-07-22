5d8758463887350a953e1da939fc1e0cb56f30ec
Matteo Grilli <matteo.grilli@quinck.io>
2025-07-22 10:53:39 +0200

    Now handling AgreeementActivated

---
+++ packages/email-notification-dispatcher/test/handleAgreementActivated.test.ts
@@ -0,0 +1,429 @@
+/* eslint-disable sonarjs/no-identical-functions */
+import {
+  getMockAgreement,
+  getMockContext,
+  getMockDescriptor,
+  getMockDescriptorPublished,
+  getMockEService,
+  getMockTenant,
+  getMockTenantMail,
+} from "pagopa-interop-commons-test";
+import {
+  CorrelationId,
+  EServiceId,
+  generateId,
+  missingKafkaMessageDataError,
+  TenantId,
+  toAgreementV2,
+  UserId,
+} from "pagopa-interop-models";
+import { describe, expect, it } from "vitest";
+import {
+  agreementStampDateNotFound,
+  descriptorNotFound,
+  eServiceNotFound,
+  tenantNotFound,
+} from "../src/models/errors.js";
+import { handleAgreementActivated } from "../src/handlers/handleAgreementActivated.js";
+import {
+  addOneAgreement,
+  addOneEService,
+  addOneTenant,
+  interopFeBaseUrl,
+  readModelService,
+  templateService,
+} from "./utils.js";
+
+describe("handleAgreementActivated", async () => {
+  const agreement = {
+    ...getMockAgreement(),
+    producerId: generateId<TenantId>(),
+    descriptors: [getMockDescriptorPublished()],
+  };
+
+  const { logger } = getMockContext({});
+
+  await addOneAgreement(agreement);
+
+  it("should throw missingKafkaMessageDataError when agreement is undefined", async () => {
+    await expect(() =>
+      handleAgreementActivated({
+        agreementV2Msg: undefined,
+        logger,
+        interopFeBaseUrl,
+        templateService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(
+      missingKafkaMessageDataError("eservice", "AgreementActivated")
+    );
+  });
+
+  it("should throw tenantNotFound when consumer is not found", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const consumerId = generateId<TenantId>();
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementActivated({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        interopFeBaseUrl,
+        templateService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(tenantNotFound(consumerId));
+  });
+
+  it("should throw tenantNotFound when producer is not found", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerId = generateId<TenantId>();
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementActivated({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        interopFeBaseUrl,
+        templateService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(tenantNotFound(producerId));
+  });
+
+  it("should throw agreementStampDateNotFound when activation date is not found", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: {},
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementActivated({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        interopFeBaseUrl,
+        templateService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(agreementStampDateNotFound("activation", agreement.id));
+  });
+
+  it("should throw eServiceNotFound when eservice is not found", async () => {
+    const descriptor = getMockDescriptor();
+    const eServiceId = generateId<EServiceId>();
+
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: {},
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eServiceId,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementActivated({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        interopFeBaseUrl,
+        templateService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(eServiceNotFound(eServiceId));
+  });
+
+  it("should throw descriptorNotFound when descriptor is not found", async () => {
+    const eservice = getMockEService();
+    await addOneEService(eservice);
+
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId: producerTenant.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementActivated({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        interopFeBaseUrl,
+        templateService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(
+      descriptorNotFound(agreement.eserviceId, agreement.descriptorId)
+    );
+  });
+
+  it("should skip tenant when no emails exist for the tenant", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+    const messages = await handleAgreementActivated({
+      agreementV2Msg: toAgreementV2(agreement),
+      logger,
+      interopFeBaseUrl,
+      templateService,
+      readModelService,
+      correlationId: generateId<CorrelationId>(),
+    });
+    expect(messages).toEqual([]);
+  });
+
+  it("should generate one message to the tenant that published the eservice", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    const messages = await handleAgreementActivated({
+      agreementV2Msg: toAgreementV2(agreement),
+      logger,
+      interopFeBaseUrl,
+      templateService,
+      readModelService,
+      correlationId: generateId<CorrelationId>(),
+    });
+    expect(messages.length).toEqual(1);
+  });
+
+  it("should generate a message using the latest mail registered per tenant", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const oldMail = { ...getMockTenantMail(), createdAt: new Date(1999) };
+    const newMail = getMockTenantMail();
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [oldMail, newMail],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    const messages = await handleAgreementActivated({
+      agreementV2Msg: toAgreementV2(agreement),
+      logger,
+      interopFeBaseUrl,
+      templateService,
+      readModelService,
+      correlationId: generateId<CorrelationId>(),
+    });
+    expect(messages.length).toEqual(1);
+    expect(messages[0].address).toEqual(newMail.address);
+  });
+
+  it("should generate a message that correctly imports the specified header and footer", async () => {
+    const descriptor = getMockDescriptor();
+    const eservice = {
+      ...getMockEService(),
+      descriptors: [descriptor],
+    };
+    await addOneEService(eservice);
+
+    const oldMail = { ...getMockTenantMail(), createdAt: new Date(1999) };
+    const newMail = getMockTenantMail();
+    const consumerTenant = {
+      ...getMockTenant(),
+      mails: [oldMail, newMail],
+    };
+    await addOneTenant(consumerTenant);
+
+    const producerTenant = {
+      ...getMockTenant(),
+      mails: [getMockTenantMail()],
+    };
+    await addOneTenant(producerTenant);
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: { activation: { when: new Date(), who: generateId<UserId>() } },
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+
+    const messages = await handleAgreementActivated({
+      agreementV2Msg: toAgreementV2(agreement),
+      logger,
+      interopFeBaseUrl,
+      templateService,
+      readModelService,
+      correlationId: generateId<CorrelationId>(),
+    });
+    expect(messages.length).toBe(1);
+    expect(messages[0].email.body).toContain("<head>");
+    expect(messages[0].email.body).toContain(`Nuova richiesta di fruizione`);
+    expect(messages[0].email.body).toContain("<body");
+    expect(messages[0].email.body).toContain("<!-- Footer -->");
+  });
+});
