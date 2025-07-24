4ff3cc91f426eeac21abd858e0f663faec58ce4b
Sandro Tajè <sandro.taje@gmail.com>
2025-07-24 12:03:33 +0200

    refactor: differantiate topic event handler

--- packages/in-app-notification-dispatcher/src/index.ts
+++ packages/in-app-notification-dispatcher/src/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable sonarjs/no-identical-functions */
 import { runConsumer } from "kafka-iam-auth";
 import { EachMessagePayload } from "kafkajs";
-import { decodeKafkaMessage, logger } from "pagopa-interop-commons";
+import { decodeKafkaMessage, Logger, logger } from "pagopa-interop-commons";
 import {
   AgreementEventV2,
   AttributeEvent,
@@ -9,8 +9,10 @@ import {
   CorrelationId,
   DelegationEventV2,
   EServiceEventV2,
+  EventEnvelope,
   generateId,
   genericInternalError,
+  Notification,
   PurposeEventV2,
   unsafeBrandId,
 } from "pagopa-interop-models";
@@ -24,10 +26,19 @@ import {
 } from "pagopa-interop-readmodel";
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
+import { z } from "zod";
 import { config } from "./config/config.js";
-import { readModelServiceBuilderSQL } from "./services/readModelServiceSQL.js";
+import {
+  readModelServiceBuilderSQL,
+  ReadModelServiceSQL,
+} from "./services/readModelServiceSQL.js";
 import { inAppNotificationServiceBuilderSQL } from "./services/inAppNotificationServiceSQL.js";
-import { handleEvent } from "./handlers/eventHandler.js";
+import { handleEServiceEvent } from "./handlers/eservices/handleEserviceEvent.js";
+import { handleAgreementEvent } from "./handlers/agreements/handleAgreementEvent.js";
+import { handlePurposeEvent } from "./handlers/purposes/handlePurposeEvent.js";
+import { handleDelegationEvent } from "./handlers/delegations/handleDelegationEvent.js";
+import { handleAuthorizationEvent } from "./handlers/authorizations/handleAuthorizationEvent.js";
+import { handleAttributeEvent } from "./handlers/attributes/handleAttributeEvent.js";
 
 interface TopicNames {
   catalogTopic: string;
@@ -77,42 +88,58 @@ function processMessage(topicNames: TopicNames) {
       attributeTopic,
     } = topicNames;
 
-    const eventType = match(messagePayload.topic)
-      .with(catalogTopic, () => EServiceEventV2)
-      .with(agreementTopic, () => AgreementEventV2)
-      .with(purposeTopic, () => PurposeEventV2)
-      .with(delegationTopic, () => DelegationEventV2)
-      .with(authorizationTopic, () => AuthorizationEventV2)
-      .with(attributeTopic, () => AttributeEvent)
+    const handleWith = <T extends z.ZodType>(
+      eventType: T,
+      handler: (
+        decodedMessage: EventEnvelope<z.infer<T>>,
+        logger: Logger,
+        readModelService: ReadModelServiceSQL
+      ) => Promise<Notification[]>
+    ): Promise<Notification[]> => {
+      const decodedMessage = decodeKafkaMessage(
+        messagePayload.message,
+        eventType
+      );
+      const loggerInstance = logger({
+        serviceName: "in-app-notification-dispatcher",
+        eventType: decodedMessage.type,
+        eventVersion: decodedMessage.event_version,
+        streamId: decodedMessage.stream_id,
+        streamVersion: decodedMessage.version,
+        correlationId: decodedMessage.correlation_id
+          ? unsafeBrandId<CorrelationId>(decodedMessage.correlation_id)
+          : generateId<CorrelationId>(),
+      });
+
+      loggerInstance.info(
+        `Processing ${decodedMessage.type} message - Partition number: ${messagePayload.partition} - Offset: ${messagePayload.message.offset}`
+      );
+      return handler(decodedMessage, loggerInstance, readModelService);
+    };
+
+    const notifications = await match(messagePayload.topic)
+      .with(catalogTopic, async () =>
+        handleWith(EServiceEventV2, handleEServiceEvent)
+      )
+      .with(agreementTopic, async () =>
+        handleWith(AgreementEventV2, handleAgreementEvent)
+      )
+      .with(purposeTopic, async () =>
+        handleWith(PurposeEventV2, handlePurposeEvent)
+      )
+      .with(delegationTopic, async () =>
+        handleWith(DelegationEventV2, handleDelegationEvent)
+      )
+      .with(authorizationTopic, async () =>
+        handleWith(AuthorizationEventV2, handleAuthorizationEvent)
+      )
+      .with(attributeTopic, async () =>
+        handleWith(AttributeEvent, handleAttributeEvent)
+      )
       .otherwise(() => {
         throw genericInternalError(`Unknown topic: ${messagePayload.topic}`);
       });
 
-    const decodedMessage = decodeKafkaMessage(
-      messagePayload.message,
-      eventType
-    );
-
-    const loggerInstance = logger({
-      serviceName: "in-app-notification-dispatcher",
-      eventType: decodedMessage.type,
-      eventVersion: decodedMessage.event_version,
-      streamId: decodedMessage.stream_id,
-      streamVersion: decodedMessage.version,
-      correlationId: decodedMessage.correlation_id
-        ? unsafeBrandId<CorrelationId>(decodedMessage.correlation_id)
-        : generateId<CorrelationId>(),
-    });
-
-    loggerInstance.info(
-      `Processing ${decodedMessage.type} message - Partition number: ${messagePayload.partition} - Offset: ${messagePayload.message.offset}`
-    );
-
-    const notifications = await handleEvent(
-      decodedMessage,
-      loggerInstance,
-      readModelService
-    );
     await inAppNotificationService.insertNotifications(notifications);
   };
 }
