0380b183e2cdc234f86f087b09f4ef27881a7d84
Matteo Grilli <matteo.grilli@quinck.io>
2025-07-30 17:51:17 +0200

    messages are now produced in a transaction

--- packages/email-notification-dispatcher/src/services/emailNotificationDispatcherService.ts
+++ packages/email-notification-dispatcher/src/services/emailNotificationDispatcherService.ts
@@ -1,5 +1,6 @@
 import { EmailNotificationMessagePayload } from "pagopa-interop-models";
 import { initProducer } from "kafka-iam-auth";
+import { logger } from "pagopa-interop-commons";
 import { config } from "../config/config.js";
 import { encodeEmailEvent } from "../handlers/utils.js";
 
@@ -11,11 +12,21 @@ export function emailNotificationDispatcherServiceBuilder() {
     sendMessages: async (
       messagePayloads: EmailNotificationMessagePayload[]
     ): Promise<void> => {
-      await producer.send({
-        messages: messagePayloads.map((payload) => ({
-          value: encodeEmailEvent(payload),
-        })),
-      });
+      const transaction = await producer.transaction();
+      try {
+        await producer.send({
+          messages: messagePayloads.map((payload) => ({
+            value: encodeEmailEvent(payload),
+          })),
+        });
+        await transaction.commit();
+      } catch (e) {
+        logger({ serviceName: "email-notification-dispatcher" }).info(
+          `Error while sending messages. Transaction will be aborted. Affected correlationIds: ` +
+          messagePayloads.map((payload) => payload.correlationId).join(", ")
+        );
+        await transaction.abort();
+      }
     },
   };
 }
