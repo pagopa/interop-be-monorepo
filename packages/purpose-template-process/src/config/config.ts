7119418d8b4cf828fb29589db9a309dab23d072c
Alepazz <alessio.pazzani@gmail.com>
2025-07-31 09:47:26 +0200

    Refactor purpose template API schema by removing unused properties

--- packages/purpose-template-process/src/config/config.ts
+++ packages/purpose-template-process/src/config/config.ts
@@ -2,18 +2,19 @@ import {
   CommonHTTPServiceConfig,
   EventStoreConfig,
   ApplicationAuditProducerConfig,
-  
   ReadModelSQLDbConfig,
 } from "pagopa-interop-commons";
 import { z } from "zod";
 
-const PurposeTemplateProcessConfig = CommonHTTPServiceConfig
-  .and(EventStoreConfig)
+const PurposeTemplateProcessConfig = CommonHTTPServiceConfig.and(
+  EventStoreConfig
+)
   .and(ApplicationAuditProducerConfig)
   .and(ReadModelSQLDbConfig);
 
-export type PurposeTemplateProcessConfig = z.infer<typeof PurposeTemplateProcessConfig>;
+export type PurposeTemplateProcessConfig = z.infer<
+  typeof PurposeTemplateProcessConfig
+>;
 
-export const config: PurposeTemplateProcessConfig = PurposeTemplateProcessConfig.parse(
-  process.env
-);
+export const config: PurposeTemplateProcessConfig =
+  PurposeTemplateProcessConfig.parse(process.env);
