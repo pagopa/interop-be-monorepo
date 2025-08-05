2605cc7b84c6e64e2a553f97df9940ce6a272dab
Eric Camellini <eric.camellini@gmail.com>
2025-08-05 18:03:44 +0200

    Refactor EService API documentation references and improve authorization validation comments

--- packages/catalog-process/src/routers/EServiceRouter.ts
+++ packages/catalog-process/src/routers/EServiceRouter.ts
@@ -374,12 +374,13 @@ const eservicesRouter = (
         const ctx = fromAppContext(req.ctx);
 
         try {
-          // The same check is done in the backend-for-frontend, if you change this check, change it there too
-          validateAuthorization(ctx, [ADMIN_ROLE, API_ROLE, M2M_ADMIN_ROLE]);
-          // TODO find a different way to handle the fix implemented in
-          // https://github.com/pagopa/interop-be-monorepo/pull/2177/files
-          // Maybe catch and rethrow the visibility error in uploadDocument and avoid replicating
-          // the visibility check in the BFF and M2M gateway?
+          validateAuthorization(ctx, [
+            ADMIN_ROLE,
+            API_ROLE,
+            // ^^ The same check for the roles above is done in the backend-for-frontend
+            // If you change this check, change it there too
+            M2M_ADMIN_ROLE,
+          ]);
 
           const { data: document, metadata } =
             await catalogService.uploadDocument(
