49b1b4c05452376107cf1349dd2249699f25cd71
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-08-06 12:39:01 +0200

    Add purpose template upsert/delete tests

--- packages/purpose-template-readmodel-writer-sql/test/utils.ts
+++ packages/purpose-template-readmodel-writer-sql/test/utils.ts
@@ -1,6 +1,36 @@
-import { setupTestContainersVitest } from "pagopa-interop-commons-test";
-import { inject, afterEach } from "vitest";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import {
+  getMockPurposeTemplate,
+  getMockRiskAnalysisFormTemplate,
+  getMockRiskAnalysisTemplateAnswerAnnotation,
+  getMockRiskAnalysisTemplateAnswerAnnotationDocument,
+  setupTestContainersVitest,
+} from "pagopa-interop-commons-test";
+import { inject, afterEach, expect } from "vitest";
 import { purposeTemplateReadModelServiceBuilder } from "pagopa-interop-readmodel";
+import {
+  PurposeTemplate,
+  PurposeTemplateId,
+  RiskAnalysisFormTemplate,
+  RiskAnalysisTemplateMultiAnswer,
+  RiskAnalysisTemplateSingleAnswer,
+  tenantKind,
+} from "pagopa-interop-models";
+import {
+  DrizzleReturnType,
+  purposeTemplateInReadmodelPurposeTemplate,
+  PurposeTemplateItemsSQL,
+  purposeTemplateRiskAnalysisAnswerAnnotationDocumentInReadmodelPurposeTemplate,
+  PurposeTemplateRiskAnalysisAnswerAnnotationDocumentSQL,
+  purposeTemplateRiskAnalysisAnswerAnnotationInReadmodelPurposeTemplate,
+  PurposeTemplateRiskAnalysisAnswerAnnotationSQL,
+  purposeTemplateRiskAnalysisAnswerInReadmodelPurposeTemplate,
+  PurposeTemplateRiskAnalysisAnswerSQL,
+  purposeTemplateRiskAnalysisFormInReadmodelPurposeTemplate,
+  PurposeTemplateRiskAnalysisFormSQL,
+  PurposeTemplateSQL,
+} from "pagopa-interop-readmodel-models";
+import { eq } from "drizzle-orm";
 import { purposeTemplateWriterServiceBuilder } from "../src/purposeTemplateWriterService.js";
 
 export const { cleanup, readModelDB } = await setupTestContainersVitest(
@@ -19,3 +49,168 @@ export const purposeTemplateReadModelService =
   purposeTemplateReadModelServiceBuilder(readModelDB);
 export const purposeTemplateWriterService =
   purposeTemplateWriterServiceBuilder(readModelDB);
+
+export const getCompleteMockPurposeTemplate = (): PurposeTemplate => {
+  const incompleteRiskAnalysisFormTemplate = getMockRiskAnalysisFormTemplate(
+    tenantKind.PA
+  );
+  const riskAnalysisFormTemplate: RiskAnalysisFormTemplate = {
+    ...incompleteRiskAnalysisFormTemplate,
+    singleAnswers: incompleteRiskAnalysisFormTemplate.singleAnswers.map(
+      (a): RiskAnalysisTemplateSingleAnswer => ({
+        ...a,
+        annotation: {
+          ...getMockRiskAnalysisTemplateAnswerAnnotation(),
+          docs: [getMockRiskAnalysisTemplateAnswerAnnotationDocument()],
+        },
+        suggestedValues: ["a", "b"],
+      })
+    ),
+    multiAnswers: incompleteRiskAnalysisFormTemplate.multiAnswers.map(
+      (a): RiskAnalysisTemplateMultiAnswer => ({
+        ...a,
+        annotation: {
+          ...getMockRiskAnalysisTemplateAnswerAnnotation(),
+          docs: [getMockRiskAnalysisTemplateAnswerAnnotationDocument()],
+        },
+      })
+    ),
+  };
+
+  return {
+    ...getMockPurposeTemplate(),
+    updatedAt: new Date(),
+    purposeRiskAnalysisForm: riskAnalysisFormTemplate,
+    purposeFreeOfChargeReason: "Free of charge reason",
+    purposeDailyCalls: 100,
+  };
+};
+
+export const checkCompletePurposeTemplate = async (
+  purposeTemplate: PurposeTemplate
+): Promise<PurposeTemplateItemsSQL> => {
+  const retrievedPurposeTemplateSQL = await retrievePurposeTemplateSQLById(
+    readModelDB,
+    purposeTemplate.id
+  );
+  const retrievedRiskAnalysisFormTemplateSQL =
+    await retrievePurposeTemplateRiskAnalysisFormSQLById(
+      readModelDB,
+      purposeTemplate.id
+    );
+  const retrievedRiskAnalysisTemplateAnswersSQL =
+    await retrievePurposeTemplateRiskAnalysisFormAnswersSQLById(
+      readModelDB,
+      purposeTemplate.id
+    );
+  const retrievedRiskAnalysisTemplateAnswersAnnotationsSQL =
+    await retrievePurposeTemplateRiskAnalysisFormAnswersAnnotationsSQLById(
+      readModelDB,
+      purposeTemplate.id
+    );
+  const retrievedRiskAnalysisTemplateAnswersAnnotationsDocumentsSQL =
+    await retrievePurposeTemplateRiskAnalysisFormAnswersAnnotationsDocumentsSQLById(
+      readModelDB,
+      purposeTemplate.id
+    );
+
+  const answersLength =
+    purposeTemplate.purposeRiskAnalysisForm!.singleAnswers.length +
+    purposeTemplate.purposeRiskAnalysisForm!.multiAnswers.length;
+  expect(retrievedPurposeTemplateSQL).toBeDefined();
+  expect(retrievedRiskAnalysisFormTemplateSQL).toBeDefined();
+  expect(retrievedRiskAnalysisTemplateAnswersSQL).toHaveLength(answersLength);
+  expect(retrievedRiskAnalysisTemplateAnswersAnnotationsSQL).toHaveLength(
+    answersLength
+  );
+  expect(
+    retrievedRiskAnalysisTemplateAnswersAnnotationsDocumentsSQL
+  ).toHaveLength(answersLength);
+
+  return {
+    purposeTemplateSQL: retrievedPurposeTemplateSQL!,
+    riskAnalysisFormTemplateSQL: retrievedRiskAnalysisFormTemplateSQL,
+    riskAnalysisTemplateAnswersSQL: retrievedRiskAnalysisTemplateAnswersSQL,
+    riskAnalysisTemplateAnswersAnnotationsSQL:
+      retrievedRiskAnalysisTemplateAnswersAnnotationsSQL,
+    riskAnalysisTemplateAnswersAnnotationsDocumentsSQL:
+      retrievedRiskAnalysisTemplateAnswersAnnotationsDocumentsSQL,
+  };
+};
+
+export const retrievePurposeTemplateSQLById = async (
+  readModelDB: DrizzleReturnType,
+  purposeTemplateId: PurposeTemplateId
+): Promise<PurposeTemplateSQL | undefined> => {
+  const queryResult = await readModelDB
+    .select()
+    .from(purposeTemplateInReadmodelPurposeTemplate)
+    .where(eq(purposeTemplateInReadmodelPurposeTemplate.id, purposeTemplateId));
+
+  return queryResult[0];
+};
+
+export const retrievePurposeTemplateRiskAnalysisFormSQLById = async (
+  readModelDB: DrizzleReturnType,
+  purposeTemplateId: PurposeTemplateId
+): Promise<PurposeTemplateRiskAnalysisFormSQL | undefined> => {
+  const queryResult = await readModelDB
+    .select()
+    .from(purposeTemplateRiskAnalysisFormInReadmodelPurposeTemplate)
+    .where(
+      eq(
+        purposeTemplateRiskAnalysisFormInReadmodelPurposeTemplate.purposeTemplateId,
+        purposeTemplateId
+      )
+    );
+
+  return queryResult[0];
+};
+
+export const retrievePurposeTemplateRiskAnalysisFormAnswersSQLById = async (
+  readModelDB: DrizzleReturnType,
+  purposeTemplateId: PurposeTemplateId
+): Promise<PurposeTemplateRiskAnalysisAnswerSQL[]> =>
+  await readModelDB
+    .select()
+    .from(purposeTemplateRiskAnalysisAnswerInReadmodelPurposeTemplate)
+    .where(
+      eq(
+        purposeTemplateRiskAnalysisAnswerInReadmodelPurposeTemplate.purposeTemplateId,
+        purposeTemplateId
+      )
+    );
+
+export const retrievePurposeTemplateRiskAnalysisFormAnswersAnnotationsSQLById =
+  async (
+    readModelDB: DrizzleReturnType,
+    purposeTemplateId: PurposeTemplateId
+  ): Promise<PurposeTemplateRiskAnalysisAnswerAnnotationSQL[]> =>
+    await readModelDB
+      .select()
+      .from(
+        purposeTemplateRiskAnalysisAnswerAnnotationInReadmodelPurposeTemplate
+      )
+      .where(
+        eq(
+          purposeTemplateRiskAnalysisAnswerAnnotationInReadmodelPurposeTemplate.purposeTemplateId,
+          purposeTemplateId
+        )
+      );
+
+export const retrievePurposeTemplateRiskAnalysisFormAnswersAnnotationsDocumentsSQLById =
+  async (
+    readModelDB: DrizzleReturnType,
+    purposeTemplateId: PurposeTemplateId
+  ): Promise<PurposeTemplateRiskAnalysisAnswerAnnotationDocumentSQL[]> =>
+    await readModelDB
+      .select()
+      .from(
+        purposeTemplateRiskAnalysisAnswerAnnotationDocumentInReadmodelPurposeTemplate
+      )
+      .where(
+        eq(
+          purposeTemplateRiskAnalysisAnswerAnnotationDocumentInReadmodelPurposeTemplate.purposeTemplateId,
+          purposeTemplateId
+        )
+      );
