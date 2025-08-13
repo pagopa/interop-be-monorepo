9fe3b153fbbdd86e8a3d8ce164b30dcc941acb1c
Eric Camellini <eric.camellini@gmail.com>
2025-08-13 09:38:35 +0200

    Updating api tests

--- packages/catalog-process/test/api/deleteDraftDescriptor.test.ts
+++ packages/catalog-process/test/api/deleteDraftDescriptor.test.ts
@@ -17,14 +17,12 @@ import {
   getMockWithMetadata,
 } from "pagopa-interop-commons-test";
 import { AuthRole, authRole } from "pagopa-interop-commons";
-import { catalogApi } from "pagopa-interop-api-clients";
 import { api, catalogService } from "../vitest.api.setup.js";
 import {
   eServiceDescriptorNotFound,
   eServiceNotFound,
   notValidDescriptorState,
 } from "../../src/model/domain/errors.js";
-import { eServiceToApiEService } from "../../src/model/domain/apiConverter.js";
 
 describe("API /eservices/{eServiceId}/descriptors/{descriptorId} authorization test", () => {
   const descriptor: Descriptor = {
@@ -34,6 +32,12 @@ describe("API /eservices/{eServiceId}/descriptors/{descriptorId} authorization t
     ...getMockEService(),
     descriptors: [descriptor],
   };
+
+  const serviceResponse = getMockWithMetadata(eservice);
+
+  catalogService.deleteDraftDescriptor = vi
+    .fn()
+    .mockResolvedValue(serviceResponse);
 
   const makeRequest = async (
     token: string,
@@ -51,39 +55,16 @@ describe("API /eservices/{eServiceId}/descriptors/{descriptorId} authorization t
     authRole.API_ROLE,
     authRole.M2M_ADMIN_ROLE,
   ];
-  it.each(authorizedRoles)(
-    "Should return 200 for user with role %s",
-    async (role) => {
-      const eserviceWithoutDescriptor = {
-        ...getMockEService(),
-        descriptors: [],
-      };
-      const eserviceWithMetadata = getMockWithMetadata(
-        eserviceWithoutDescriptor
-      );
-      const apiEservice = catalogApi.EService.parse(
-        eServiceToApiEService(eserviceWithoutDescriptor)
-      );
-      catalogService.deleteDraftDescriptor = vi
-        .fn()
-        .mockResolvedValue(eserviceWithMetadata);
-      const token = generateToken(role);
-      const res = await makeRequest(token, eservice.id, descriptor.id);
-      expect(res.status).toBe(200);
-      expect(res.body).toEqual(apiEservice);
-      expect(res.headers["x-metadata-version"]).toBe(
-        eserviceWithMetadata.metadata.version.toString()
-      );
-    }
-  );
 
   it.each(authorizedRoles)(
     "Should return 204 for user with role %s",
     async (role) => {
-      catalogService.deleteDraftDescriptor = vi.fn().mockResolvedValue({});
       const token = generateToken(role);
       const res = await makeRequest(token, eservice.id, descriptor.id);
       expect(res.status).toBe(204);
+      expect(res.headers["x-metadata-version"]).toEqual(
+        serviceResponse.metadata.version.toString()
+      );
     }
   );
 
@@ -94,6 +75,16 @@ describe("API /eservices/{eServiceId}/descriptors/{descriptorId} authorization t
     const res = await makeRequest(token, eservice.id, descriptor.id);
 
     expect(res.status).toBe(403);
+  });
+
+  it("Should return 204 and not set metadata when the entire e-service is deleted", async () => {
+    catalogService.deleteDraftDescriptor = vi.fn().mockResolvedValueOnce(
+      undefined // when the entire e-service is deleted, the service returns undefined
+    );
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token, eservice.id, descriptor.id);
+    expect(res.status).toBe(204);
+    expect(res.headers["x-metadata-version"]).toBeUndefined();
   });
 
   it.each([
