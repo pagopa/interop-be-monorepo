7991879a40048546522334f9f321a46abf50ea4e
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-08-21 19:59:36 +0200

    Refactor risk analysis template validation: move in a seperated file

--- packages/commons/src/risk-analysis/index.ts
+++ packages/commons/src/risk-analysis/index.ts
@@ -3,4 +3,5 @@ export * from "./riskAnalysisValidationErrors.js";
 export * from "./riskAnalysisTemplateValidationErrors.js";
 export * from "./riskAnalysisTemplateErrors.js";
 export * from "./riskAnalysisValidation.js";
+export * from "./riskAnalysisTemplateValidation.js";
 export * from "./rules/index.js";
