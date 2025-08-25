b14adc1e0d08f5ede86c796ec040991abeb8d9ab
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-08-25 16:27:21 +0200

    Align risk analysis template mock functions

--- packages/commons/src/risk-analysis/models.ts
+++ packages/commons/src/risk-analysis/models.ts
@@ -3,9 +3,11 @@ import {
   RiskAnalysis,
   RiskAnalysisForm,
   RiskAnalysisFormId,
+  RiskAnalysisFormTemplate,
   RiskAnalysisId,
   RiskAnalysisMultiAnswerId,
   RiskAnalysisSingleAnswerId,
+  RiskAnalysisTemplateAnswerAnnotation,
   TenantKind,
   generateId,
 } from "pagopa-interop-models";
@@ -70,6 +72,65 @@ export type ValidationRule = {
   allowedValues: Set<string> | undefined;
 };
 
+/* ===============================
+  Purpose Template Risk Analysis 
+================================== */
+
+export type RiskAnalysisFormTemplateToValidate = {
+  version: string;
+  answers: Record<string, RiskAnalysisTemplateAnswerToValidate>;
+};
+
+export type RiskAnalysisTemplateAnswerToValidate = {
+  values: string[];
+  editable: boolean;
+  annotation?: RiskAnalysisTemplateValidatedAnswerAnnotation;
+  suggestedValues: string[];
+};
+
+export type RiskAnalysisTemplateValidatedAnswerAnnotation = {
+  text: string;
+  docs: RiskAnalysisTemplateAnswerAnnotationDocument[];
+};
+
+export type RiskAnalysisTemplateAnswerAnnotationDocument = {
+  name: string;
+  contentType: string;
+  prettyName: string;
+  path: string;
+};
+
+export type RiskAnalysisTemplateValidatedForm = {
+  version: string;
+  singleAnswers: RiskAnalysisTemplateValidatedSingleAnswer[];
+  multiAnswers: RiskAnalysisTemplateValidatedMultiAnswer[];
+};
+
+export type RiskAnalysisTemplateValidatedSingleOrMultiAnswer =
+  | {
+      type: "single";
+      answer: RiskAnalysisTemplateValidatedSingleAnswer;
+    }
+  | {
+      type: "multi";
+      answer: RiskAnalysisTemplateValidatedMultiAnswer;
+    };
+
+export type RiskAnalysisTemplateValidatedSingleAnswer = {
+  key: string;
+  value?: string;
+  editable: boolean;
+  annotation?: RiskAnalysisTemplateValidatedAnswerAnnotation;
+  suggestedValues: string[];
+};
+
+export type RiskAnalysisTemplateValidatedMultiAnswer = {
+  key: string;
+  values: string[];
+  editable: boolean;
+  annotation?: RiskAnalysisTemplateValidatedAnswerAnnotation;
+};
+
 export function riskAnalysisValidatedFormToNewRiskAnalysis(
   validatedForm: RiskAnalysisValidatedForm,
   name: RiskAnalysis["name"]
@@ -115,6 +176,30 @@ export function riskAnalysisValidatedFormToNewRiskAnalysisForm(
   };
 }
 
+export function riskAnalysisValidatedFormTemplateToNewRiskAnalysisFormTemplate(
+  validatedForm: RiskAnalysisTemplateValidatedForm
+): RiskAnalysisFormTemplate {
+  return {
+    id: generateId(),
+    version: validatedForm.version,
+    singleAnswers: validatedForm.singleAnswers.map((a) => ({
+      id: generateId(),
+      key: a.key,
+      value: a.value,
+      editable: a.editable,
+      suggestedValues: a.suggestedValues,
+      ...(a.annotation ? { annotation: mapAnnotation(a.annotation) } : {}),
+    })),
+    multiAnswers: validatedForm.multiAnswers.map((a) => ({
+      id: generateId(),
+      key: a.key,
+      values: a.values,
+      editable: a.editable,
+      ...(a.annotation ? { annotation: mapAnnotation(a.annotation) } : {}),
+    })),
+  };
+}
+
 export function riskAnalysisFormToRiskAnalysisFormToValidate(
   form: RiskAnalysisForm
 ): RiskAnalysisFormToValidate {
@@ -136,5 +221,19 @@ export function riskAnalysisFormToRiskAnalysisFormToValidate(
         {}
       ),
     },
+  };
+}
+
+function mapAnnotation(
+  annotation: RiskAnalysisTemplateValidatedAnswerAnnotation
+): RiskAnalysisTemplateAnswerAnnotation {
+  return {
+    id: generateId(),
+    text: annotation.text,
+    docs: annotation.docs.map((d) => ({
+      id: generateId(),
+      ...d,
+      createdAt: new Date(),
+    })),
   };
 }
