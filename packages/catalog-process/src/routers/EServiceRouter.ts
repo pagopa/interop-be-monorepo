9a329e20ad60aa3232b732cfffe92893026cc5f7
Eric Camellini <eric.camellini@gmail.com>
2025-08-25 14:48:07 +0200

    PIN-7248 - M2M patch draft E-Service (#2271)

--- packages/catalog-process/src/routers/EServiceRouter.ts
+++ packages/catalog-process/src/routers/EServiceRouter.ts
@@ -238,11 +238,35 @@ const eservicesRouter = (
       try {
         validateAuthorization(ctx, [ADMIN_ROLE, API_ROLE]);
 
-        const updatedEService = await catalogService.updateEService(
+        const { data: updatedEService } = await catalogService.updateEService(
           unsafeBrandId(req.params.eServiceId),
           req.body,
           ctx
         );
+        return res
+          .status(200)
+          .send(
+            catalogApi.EService.parse(eServiceToApiEService(updatedEService))
+          );
+      } catch (error) {
+        const errorRes = makeApiProblem(error, updateEServiceErrorMapper, ctx);
+        return res.status(errorRes.status).send(errorRes);
+      }
+    })
+    .patch("/eservices/:eServiceId", async (req, res) => {
+      const ctx = fromAppContext(req.ctx);
+
+      try {
+        validateAuthorization(ctx, [M2M_ADMIN_ROLE]);
+
+        const { data: updatedEService, metadata } =
+          await catalogService.patchUpdateEService(
+            unsafeBrandId(req.params.eServiceId),
+            req.body,
+            ctx
+          );
+
+        setMetadataVersionHeader(res, metadata);
         return res
           .status(200)
           .send(
