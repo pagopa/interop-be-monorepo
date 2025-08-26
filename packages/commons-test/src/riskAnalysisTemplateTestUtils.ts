0bed0ca6b6d1c8fd393e542d66ee24a667a364bc
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-08-26 18:33:41 +0200

    Move risk-analisys-template to commons package

---
+++ packages/commons-test/src/riskAnalysisTemplateTestUtils.ts
@@ -0,0 +1,276 @@
+import {
+  RiskAnalysisTemplateValidatedForm,
+  riskAnalysisValidatedFormTemplateToNewRiskAnalysisFormTemplate,
+} from "pagopa-interop-commons";
+import {
+  RiskAnalysisFormTemplate,
+  TenantKind,
+  tenantKind,
+} from "pagopa-interop-models";
+import { match } from "ts-pattern";
+import {
+  validRiskAnalysis2_0_Private,
+  validRiskAnalysis3_0_Pa,
+} from "./riskAnalysisTestUtils.js";
+
+export const validatedRiskAnalysisTemplate3_0_Pa: RiskAnalysisTemplateValidatedForm =
+  {
+    version: validRiskAnalysis3_0_Pa.version,
+    singleAnswers: [
+      {
+        key: "purpose",
+        value: "INSTITUTIONAL",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "institutionalPurpose",
+        value: "MyPurpose",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "otherPersonalDataTypes",
+        value: "MyDataTypes",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "legalObligationReference",
+        value: "somethingLegal",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "knowsDataQuantity",
+        value: "NO",
+        editable: false,
+        suggestedValues: ["YES", "NO"],
+      },
+      {
+        key: "deliveryMethod",
+        value: "ANONYMOUS",
+        editable: false,
+        suggestedValues: ["ANONYMOUS", "PSEUDONYMOUS", "IDENTIFIED"],
+      },
+      {
+        key: "policyProvided",
+        value: "NO",
+        editable: false,
+        suggestedValues: ["YES", "NO"],
+      },
+      {
+        key: "confirmPricipleIntegrityAndDiscretion",
+        value: "true",
+        editable: false,
+        suggestedValues: ["true", "false"],
+      },
+      {
+        key: "reasonPolicyNotProvided",
+        value: "Because",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "doneDpia",
+        value: "NO",
+        editable: false,
+        suggestedValues: ["YES", "NO"],
+      },
+      {
+        key: "dataDownload",
+        value: "YES",
+        editable: false,
+        suggestedValues: ["YES", "NO"],
+      },
+      {
+        key: "confirmDataRetentionPeriod",
+        value: "true",
+        editable: false,
+        suggestedValues: ["true", "false"],
+      },
+      {
+        key: "purposePursuit",
+        value: "MERE_CORRECTNESS",
+        editable: false,
+        suggestedValues: ["MERE_CORRECTNESS", "QUALITY_IMPROVEMENT"],
+      },
+      {
+        key: "checkedExistenceMereCorrectnessInteropCatalogue",
+        value: "true",
+        editable: false,
+        suggestedValues: ["true", "false"],
+      },
+      {
+        key: "usesThirdPartyData",
+        value: "NO",
+        editable: false,
+        suggestedValues: ["YES", "NO"],
+      },
+      {
+        key: "declarationConfirmGDPR",
+        value: "true",
+        editable: false,
+        suggestedValues: ["true", "false"],
+      },
+      {
+        key: "publicInterestTaskText",
+        value: undefined,
+        editable: true,
+        suggestedValues: [],
+      },
+      {
+        key: "legalBasisPublicInterest",
+        value: "PUBLIC_INTEREST_TASK",
+        editable: false,
+        suggestedValues: [],
+      },
+    ],
+    multiAnswers: [
+      { key: "personalDataTypes", values: ["OTHER"], editable: false },
+      {
+        key: "legalBasis",
+        values: ["LEGAL_OBLIGATION", "PUBLIC_INTEREST"],
+        editable: false,
+      },
+    ],
+  };
+
+export const validatedRiskAnalysisTemplate2_0_Private: RiskAnalysisTemplateValidatedForm =
+  {
+    version: validRiskAnalysis2_0_Private.version,
+    singleAnswers: [
+      {
+        key: "purpose",
+        value: "INSTITUTIONAL",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "institutionalPurpose",
+        value: "MyPurpose",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "usesPersonalData",
+        value: "YES",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "otherPersonalDataTypes",
+        value: "MyDataTypes",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "legalObligationReference",
+        value: "YES",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "legalBasisPublicInterest",
+        value: "RULE_OF_LAW",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "ruleOfLawText",
+        value: "TheLaw",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "knowsDataQuantity",
+        value: "NO",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "dataDownload",
+        value: "YES",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "deliveryMethod",
+        value: "CLEARTEXT",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "policyProvided",
+        value: "NO",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "confirmPricipleIntegrityAndDiscretion",
+        value: "true",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "reasonPolicyNotProvided",
+        value: "Because",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "doneDpia",
+        value: "NO",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "dataRetentionPeriod",
+        value: "10",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "purposePursuit",
+        value: "MERE_CORRECTNESS",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "checkedExistenceMereCorrectnessInteropCatalogue",
+        value: "true",
+        editable: false,
+        suggestedValues: [],
+      },
+      {
+        key: "declarationConfirmGDPR",
+        value: "true",
+        editable: false,
+        suggestedValues: [],
+      },
+    ],
+    multiAnswers: [
+      { key: "personalDataTypes", values: ["OTHER"], editable: false },
+      {
+        key: "legalBasis",
+        values: ["LEGAL_OBLIGATION", "PUBLIC_INTEREST"],
+        editable: false,
+      },
+    ],
+  };
+
+export const getMockValidRiskAnalysisFormTemplate = (
+  producerTenantKind: TenantKind
+): RiskAnalysisFormTemplate =>
+  match(producerTenantKind)
+    .with(tenantKind.PA, () =>
+      riskAnalysisValidatedFormTemplateToNewRiskAnalysisFormTemplate(
+        validatedRiskAnalysisTemplate3_0_Pa
+      )
+    )
+    .with(tenantKind.PRIVATE, tenantKind.GSP, tenantKind.SCP, () =>
+      riskAnalysisValidatedFormTemplateToNewRiskAnalysisFormTemplate(
+        validatedRiskAnalysisTemplate2_0_Private
+      )
+    )
+    .exhaustive();
---
+++ packages/commons/src/risk-analysis-template/riskAnalysisTemplateValidation.ts
@@ -0,0 +1,303 @@
+import { TenantKind } from "pagopa-interop-models";
+import { match, P } from "ts-pattern";
+import {
+  ValidationRule,
+  ValidationRuleDependency,
+} from "../risk-analysis/models.js";
+import {
+  buildValidationRules,
+  getLatestVersionFormRules,
+} from "../risk-analysis/riskAnalysisValidation.js";
+import {
+  RiskAnalysisFormTemplateToValidate,
+  RiskAnalysisTemplateAnswerToValidate,
+  RiskAnalysisTemplateValidatedForm,
+  RiskAnalysisTemplateValidatedSingleOrMultiAnswer,
+} from "./riskAnalysisFormTemplate.js";
+import {
+  invalidTemplateResult,
+  malformedTemplateFieldValueOrSuggestion,
+  missingExpectedTemplateFieldError,
+  noRulesVersionTemplateFoundError,
+  RiskAnalysisTemplateValidationIssue,
+  RiskAnalysisTemplateValidationResult,
+  templateDependencyNotFoundError,
+  unexpectedTemplateDependencyEditableError,
+  unexpectedTemplateDependencyValueError,
+  unexpectedTemplateFieldError,
+  unexpectedTemplateFieldValueError,
+  unexpectedTemplateFieldValueOrSuggestion,
+  unexpectedTemplateRulesVersionError,
+  validTemplateResult,
+} from "./riskAnalysisTemplateValidationErrors.js";
+
+export function validatePurposeTemplateRiskAnalysis(
+  riskAnalysisFormTemplate: RiskAnalysisFormTemplateToValidate,
+  tenantKind: TenantKind
+): RiskAnalysisTemplateValidationResult<RiskAnalysisTemplateValidatedForm> {
+  const latestVersionFormRules = getLatestVersionFormRules(tenantKind);
+
+  if (latestVersionFormRules === undefined) {
+    return invalidTemplateResult([
+      noRulesVersionTemplateFoundError(tenantKind),
+    ]);
+  }
+
+  if (latestVersionFormRules.version !== riskAnalysisFormTemplate.version) {
+    return invalidTemplateResult([
+      unexpectedTemplateRulesVersionError(riskAnalysisFormTemplate.version),
+    ]);
+  }
+
+  const validationRules = buildValidationRules(latestVersionFormRules);
+
+  const results = validateTemplateFormAnswers(
+    riskAnalysisFormTemplate.answers,
+    validationRules
+  );
+
+  if (results.some((r) => r.type === "invalid")) {
+    return invalidTemplateResult(
+      results.flatMap((r) => (r.type === "invalid" ? r.issues : []))
+    );
+  } else {
+    const validatedAnswers = results.flatMap((r) =>
+      r.type === "valid" ? [r.value] : []
+    );
+
+    const { singleAnswers, multiAnswers } = validatedAnswers.reduce<
+      Omit<RiskAnalysisTemplateValidatedForm, "version">
+    >(
+      (validatedForm, answer) =>
+        match(answer)
+          .with({ type: "single" }, (a) => ({
+            ...validatedForm,
+            singleAnswers: [...validatedForm.singleAnswers, a.answer],
+          }))
+          .with({ type: "multi" }, (a) => ({
+            ...validatedForm,
+            multiAnswers: [...validatedForm.multiAnswers, a.answer],
+          }))
+          .exhaustive(),
+      {
+        singleAnswers: [],
+        multiAnswers: [],
+      }
+    );
+
+    return validTemplateResult({
+      version: latestVersionFormRules.version,
+      singleAnswers,
+      multiAnswers,
+    });
+  }
+}
+
+function validateTemplateFormAnswers(
+  answers: RiskAnalysisFormTemplateToValidate["answers"],
+  validationRules: ValidationRule[]
+): Array<
+  RiskAnalysisTemplateValidationResult<RiskAnalysisTemplateValidatedSingleOrMultiAnswer>
+> {
+  const requiredFieldValidationIssues = validateTemplateRequiredFields(
+    answers,
+    validationRules
+  );
+
+  return Object.entries(answers)
+    .map(([answerKey, answerValue]) => {
+      const validationRule = validationRules.find(
+        (r) => r.fieldName === answerKey
+      );
+
+      return match(validationRule)
+        .with(P.nullish, () =>
+          invalidTemplateResult([unexpectedTemplateFieldError(answerKey)])
+        )
+        .with(P.not(P.nullish), (rule) => {
+          const errors = validateFormAnswer(answerValue, rule, answers);
+
+          if (errors.length > 0) {
+            return invalidTemplateResult(errors);
+          } else {
+            return answerToValidatedSingleOrMultiAnswer(
+              answerKey,
+              answerValue,
+              rule
+            );
+          }
+        })
+        .exhaustive();
+    })
+    .concat(
+      requiredFieldValidationIssues.length > 0
+        ? [invalidTemplateResult(requiredFieldValidationIssues)]
+        : []
+    );
+}
+
+function validateFormAnswer(
+  templateAnswer: RiskAnalysisTemplateAnswerToValidate,
+  validationRule: ValidationRule,
+  answers: RiskAnalysisFormTemplateToValidate["answers"]
+): RiskAnalysisTemplateValidationIssue[] {
+  return [
+    ...validateAnswerValue(templateAnswer, validationRule),
+    ...validationRule.dependencies.flatMap((dependencyRule) =>
+      validateAnswerDependency(
+        answers,
+        dependencyRule,
+        validationRule.fieldName
+      )
+    ),
+  ];
+}
+
+function validateAnswerValue(
+  answer: RiskAnalysisTemplateAnswerToValidate,
+  rule: ValidationRule
+): RiskAnalysisTemplateValidationIssue[] {
+  if (answer.editable) {
+    return answer.values.length > 0 || answer.suggestedValues.length > 0
+      ? [unexpectedTemplateFieldValueOrSuggestion(rule.fieldName)]
+      : [];
+  }
+
+  const hasSuggestions = answer.suggestedValues.length > 0;
+  const hasValue = answer.values.length > 0;
+  return match(rule)
+    .with(P.nullish, () => [])
+    .with({ dataType: "freeText" }, (r) => {
+      if (!hasValue && !hasSuggestions) {
+        return [unexpectedTemplateFieldValueOrSuggestion(r.fieldName)];
+      }
+
+      if (hasValue && hasSuggestions) {
+        return [malformedTemplateFieldValueOrSuggestion(r.fieldName)];
+      }
+
+      return [];
+    })
+    .with({ dataType: P.not("freeText") }, () =>
+      rule.allowedValues &&
+      answer.values.some((e) => !rule.allowedValues?.has(e))
+        ? [
+            unexpectedTemplateFieldValueError(
+              rule.fieldName,
+              rule.allowedValues
+            ),
+          ]
+        : []
+    )
+    .exhaustive();
+}
+
+function validateAnswerDependency(
+  answers: RiskAnalysisFormTemplateToValidate["answers"],
+  dependency: ValidationRuleDependency,
+  dependentField: string
+): RiskAnalysisTemplateValidationIssue[] {
+  return match(answers[dependency.fieldName])
+    .with(P.nullish, () => [
+      templateDependencyNotFoundError(dependentField, dependency.fieldName),
+    ])
+    .with({ editable: true }, () => [
+      unexpectedTemplateDependencyEditableError(
+        dependentField,
+        dependency.fieldName
+      ),
+    ])
+    .with({ values: P.when((v) => !v.includes(dependency.fieldValue)) }, () => [
+      unexpectedTemplateDependencyValueError(
+        dependentField,
+        dependency.fieldName,
+        dependency.fieldValue
+      ),
+    ])
+    .otherwise(() => []);
+}
+
+function answerToValidatedSingleOrMultiAnswer(
+  answerKey: string,
+  answerValue: RiskAnalysisTemplateAnswerToValidate,
+  validationRule: ValidationRule
+): RiskAnalysisTemplateValidationResult<RiskAnalysisTemplateValidatedSingleOrMultiAnswer> {
+  return match(validationRule.dataType)
+    .with("single", "freeText", () =>
+      validTemplateResult<RiskAnalysisTemplateValidatedSingleOrMultiAnswer>({
+        type: "single",
+        answer: {
+          key: answerKey,
+          value: answerValue.values[0] ?? undefined,
+          editable: answerValue.editable,
+          suggestedValues: answerValue.suggestedValues,
+        },
+      })
+    )
+    .with("multi", () =>
+      validTemplateResult<RiskAnalysisTemplateValidatedSingleOrMultiAnswer>({
+        type: "multi",
+        answer: {
+          key: answerKey,
+          values: answerValue.values,
+          editable: answerValue.editable,
+        },
+      })
+    )
+    .exhaustive();
+}
+
+function formContainsDependency(
+  answers: RiskAnalysisFormTemplateToValidate["answers"],
+  dependency: ValidationRuleDependency
+): boolean {
+  const field = answers[dependency.fieldName];
+  return match(field)
+    .with(P.not(P.nullish), (answerToValidate) =>
+      answerToValidate.values.some((v) => v === dependency.fieldValue)
+    )
+    .with(P.nullish, () => false)
+    .exhaustive();
+}
+
+function validateTemplateRequiredFields(
+  answers: RiskAnalysisFormTemplateToValidate["answers"],
+  validationRules: ValidationRule[]
+): RiskAnalysisTemplateValidationIssue[] {
+  return validationRules
+    .filter((r) => r.required)
+    .flatMap((rule) => {
+      const templateAnswer = answers[rule.fieldName];
+      const depsSatisfied = rule.dependencies.every((dependency) =>
+        formContainsDependency(answers, dependency)
+      );
+
+      if (templateAnswer === undefined && depsSatisfied) {
+        return [missingExpectedTemplateFieldError(rule.fieldName)];
+      }
+
+      if (templateAnswer === undefined) {
+        return [];
+      }
+
+      // if the field is editable, require fields are not checked
+      if (templateAnswer.editable) {
+        return [];
+      }
+
+      if (
+        rule.dataType === "freeText" &&
+        !templateAnswer.editable &&
+        !templateAnswer.values.length &&
+        !templateAnswer.suggestedValues.length
+      ) {
+        return [unexpectedTemplateFieldValueOrSuggestion(rule.fieldName)];
+      }
+
+      if (!templateAnswer.editable && !templateAnswer.values.length) {
+        return [unexpectedTemplateFieldValueOrSuggestion(rule.fieldName)];
+      }
+
+      return [];
+    });
+}
