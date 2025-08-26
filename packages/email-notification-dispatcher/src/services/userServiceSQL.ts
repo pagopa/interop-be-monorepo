86c1ba68b8e2dfffd8477510cce64a4669fff953
Matteo Grilli <matteo.grilli@quinck.io>
2025-08-26 17:56:31 +0200

    readUser refactor

--- packages/email-notification-dispatcher/src/services/userServiceSQL.ts
+++ packages/email-notification-dispatcher/src/services/userServiceSQL.ts
@@ -1,17 +1,22 @@
-import { eq } from "drizzle-orm";
-import { user, UserDB } from "pagopa-interop-selfcare-user-db-models";
+import { inArray } from "drizzle-orm";
+import { user } from "pagopa-interop-selfcare-user-db-models";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { UserId } from "pagopa-interop-models";
+import { unsafeBrandId, UserId } from "pagopa-interop-models";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function userServiceBuilderSQL(userDB: ReturnType<typeof drizzle>) {
   return {
-    readUser: async (userId: UserId): Promise<UserDB | undefined> => {
+    readUsers: async (
+      userIds: UserId[]
+    ): Promise<Array<{ userId: UserId; email: string }>> => {
       const result = await userDB
         .select()
         .from(user)
-        .where(eq(user.userId, userId));
-      return result.length === 0 ? undefined : result[0];
+        .where(inArray(user.userId, userIds));
+      return result.map(({ userId, email }) => ({
+        userId: unsafeBrandId(userId),
+        email,
+      }));
     },
   };
 }
