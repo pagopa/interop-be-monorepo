caaee6c149e3ecfdf2d89cc4ae8316fd8cf5dc5f
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-08-27 10:00:22 +0200

    Fix typo

--- packages/commons/src/risk-analysis-template/riskAnalysisTemplateValidationErrors.ts
+++ packages/commons/src/risk-analysis-template/riskAnalysisTemplateValidationErrors.ts
@@ -116,12 +116,12 @@ export function unexpectedTemplateRulesVersionError(
 }
 export function unexpectedTemplateDependencyValueError(
   dependentField: string,
-  depencencyField: string,
+  dependencyField: string,
   expectedValue: string
 ): RiskAnalysisTemplateValidationIssue {
   return new RiskAnalysisTemplateValidationIssue({
     code: "unexpectedTemplateDependencyValueError",
-    detail: `Field ${dependentField} requires field ${depencencyField} value to be ${expectedValue}`,
+    detail: `Field ${dependentField} requires field ${dependencyField} value to be ${expectedValue}`,
   });
 }
 
