0aae8d5d4cebd621655987f3c9aa0ddbaa02baa5
Matteo Grilli <matteo.grilli@quinck.io>
2025-08-28 17:32:57 +0200

    refactored tests using userDB

--- packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementActivatedToConsumer.ts
+++ packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementActivatedToConsumer.ts
@@ -14,6 +14,7 @@ import {
   AgreementHandlerParams,
   getRecipientsForTenants,
   retrieveAgreementEservice,
+  getRecipientsForTenant,
 } from "../handlerCommons.js";
 
 const notificationType: NotificationType =
