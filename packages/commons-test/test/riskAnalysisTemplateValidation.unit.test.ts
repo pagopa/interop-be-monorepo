4b6949caa914583c3c20a5e3dd2595cec6bc77a3
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-08-28 16:33:42 +0200

    Fix imports

--- packages/commons-test/test/riskAnalysisTemplateValidation.unit.test.ts
+++ packages/commons-test/test/riskAnalysisTemplateValidation.unit.test.ts
@@ -1,5 +1,14 @@
 import { describe, expect, it } from "vitest";
 import { tenantKind } from "pagopa-interop-models";
+import {
+  riskAnalysisFormTemplateToRiskAnalysisFormTemplateToValidate,
+  validatePurposeTemplateRiskAnalysis,
+} from "pagopa-interop-commons";
+import {
+  getMockValidRiskAnalysisFormTemplate,
+  validatedRiskAnalysisTemplate3_0_Pa,
+  validatedRiskAnalysisTemplate2_0_Private,
+} from "../src/riskAnalysisTemplateTestUtils.js";
 
 describe("Risk Analysis Template Validation", () => {
   it("should succeed on correct form 3.0 on tenant kind PA", () => {
