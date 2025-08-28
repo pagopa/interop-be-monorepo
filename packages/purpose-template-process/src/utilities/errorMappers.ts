46bf3f4cc1710b2af31671219e70a47cf172f5e0
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-08-28 16:52:42 +0200

    Fix `getPurposeTemplatesErrorMapper`

--- packages/purpose-template-process/src/utilities/errorMappers.ts
+++ packages/purpose-template-process/src/utilities/errorMappers.ts
@@ -9,7 +9,6 @@ const {
   HTTP_STATUS_INTERNAL_SERVER_ERROR,
   HTTP_STATUS_BAD_REQUEST,
   HTTP_STATUS_CONFLICT,
-  HTTP_STATUS_NOT_FOUND,
 } = constants;
 
 export const createPurposeTemplateErrorMapper = (
@@ -27,9 +26,5 @@ export const createPurposeTemplateErrorMapper = (
     .with("purposeTemplateNameConflict", () => HTTP_STATUS_CONFLICT)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
-export const getPurposeTemplatesErrorMapper = (
-  error: ApiError<ErrorCodes>
-): number =>
-  match(error.code)
-    .with("purposeTemplateNotFound", () => HTTP_STATUS_NOT_FOUND)
-    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+export const getPurposeTemplatesErrorMapper = (): number =>
+  HTTP_STATUS_INTERNAL_SERVER_ERROR;
