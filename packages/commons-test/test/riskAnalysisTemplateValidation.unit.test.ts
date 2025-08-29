c1a4941221b274216c7ad0c1f452eb0bdaed13d2
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-08-29 18:00:33 +0200

    Add risk analysis template validation tests

---
+++ packages/commons-test/test/riskAnalysisTemplateValidation.unit.test.ts
@@ -0,0 +1,52 @@
+import { describe, expect, it } from "vitest";
+import { tenantKind } from "pagopa-interop-models";
+import {
+  riskAnalysisFormTemplateToRiskAnalysisFormTemplateToValidate,
+  validatePurposeTemplateRiskAnalysis,
+} from "pagopa-interop-commons";
+import {
+  getMockValidRiskAnalysisFormTemplate,
+  validatedRiskAnalysisTemplate2_0_Private,
+  validatedRiskAnalysisTemplate3_0_Pa,
+} from "../src/riskAnalysisTemplateTestUtils.js";
+
+describe("Risk Analysis Template Validation", () => {
+  it("should succeed on correct form 3.0 on tenant kind PA", () => {
+    const mockValidRiskAnalysisTemplateForm =
+      getMockValidRiskAnalysisFormTemplate(tenantKind.PA);
+
+    const riskAnalysisFormTemplateToValidate =
+      riskAnalysisFormTemplateToRiskAnalysisFormTemplateToValidate(
+        mockValidRiskAnalysisTemplateForm
+      );
+
+    const result = validatePurposeTemplateRiskAnalysis(
+      riskAnalysisFormTemplateToValidate,
+      tenantKind.PA
+    );
+
+    expect(result).toEqual({
+      type: "valid",
+      value: validatedRiskAnalysisTemplate3_0_Pa,
+    });
+  });
+  it("should succeed on correct form 2.0 on tenant kind PRIVATE", () => {
+    const mockValidRiskAnalysisTemplateForm =
+      getMockValidRiskAnalysisFormTemplate(tenantKind.PRIVATE);
+
+    const riskAnalysisFormTemplateToValidate =
+      riskAnalysisFormTemplateToRiskAnalysisFormTemplateToValidate(
+        mockValidRiskAnalysisTemplateForm
+      );
+
+    const result = validatePurposeTemplateRiskAnalysis(
+      riskAnalysisFormTemplateToValidate,
+      tenantKind.PRIVATE
+    );
+
+    expect(result).toEqual({
+      type: "valid",
+      value: validatedRiskAnalysisTemplate2_0_Private,
+    });
+  });
+});
