b3a86316634937d72bf6fdb3c58602e3cc3cf26f
Matteo Grilli <matteo.grilli@quinck.io>
2025-09-01 15:39:17 +0200

    updated tests after rebase

--- packages/email-notification-dispatcher/test/handleAgreementSuspendedByPlatform.test.ts
+++ packages/email-notification-dispatcher/test/handleAgreementSuspendedByPlatform.test.ts
@@ -3,7 +3,6 @@
 import {
   getMockAgreement,
   getMockContext,
-  getMockDescriptor,
   getMockDescriptorPublished,
   getMockEService,
   getMockTenant,
@@ -25,9 +24,11 @@ import {
   addOneAgreement,
   addOneEService,
   addOneTenant,
+  addOneUser,
   getMockUser,
   readModelService,
   templateService,
+  userService,
 } from "./utils.js";
 
 describe("handleAgreementSuspendedByPlatform", async () => {
@@ -53,23 +54,20 @@ describe("handleAgreementSuspendedByPlatform", async () => {
     getMockUser(consumerTenant.id),
   ];
 
-  const userService = {
-    readUser: vi.fn(),
-  };
   const { logger } = getMockContext({});
 
   beforeEach(async () => {
     await addOneEService(eservice);
     await addOneTenant(producerTenant);
     await addOneTenant(consumerTenant);
+    for (const user of users) {
+      await addOneUser(user);
+    }
     readModelService.getTenantUsersWithNotificationEnabled = vi
       .fn()
       .mockReturnValueOnce(
-        users.map((user) => ({ userId: user.userId, tenantId: user.tenantId }))
+        users.map((user) => ({ userId: user.id, tenantId: user.tenantId }))
       );
-    userService.readUser.mockImplementation((userId) =>
-      users.find((user) => user.userId === userId)
-    );
   });
 
   it("should throw missingKafkaMessageDataError when agreement is undefined", async () => {
@@ -88,107 +86,62 @@ describe("handleAgreementSuspendedByPlatform", async () => {
   });
 
   it("should throw tenantNotFound when consumer is not found", async () => {
-    const descriptor = getMockDescriptor();
-    const eservice = {
-      ...getMockEService(),
-      descriptors: [descriptor],
-    };
-    await addOneEService(eservice);
-
-    const consumerId = generateId<TenantId>();
-
-    const producerTenant = {
-      ...getMockTenant(),
-      mails: [getMockTenantMail()],
-    };
-    await addOneTenant(producerTenant);
-
-    const agreement = {
-      ...getMockAgreement(),
-      stamps: {
-        suspensionByProducer: { when: new Date(), who: generateId<UserId>() },
-      },
-      producerId: producerTenant.id,
-      descriptorId: descriptor.id,
-      eserviceId: eservice.id,
-      consumerId,
-    };
-    await addOneAgreement(agreement);
-
-    await expect(() =>
-      handleAgreementSuspendedByPlatform({
-        agreementV2Msg: toAgreementV2(agreement),
-        logger,
-        templateService,
-        userService,
-        readModelService,
-        correlationId: generateId<CorrelationId>(),
-      })
-    ).rejects.toThrow(tenantNotFound(consumerId));
-  });
-
-  it("should throw tenantNotFound when producer is not found", async () => {
-    const descriptor = getMockDescriptor();
-    const eservice = {
-      ...getMockEService(),
-      descriptors: [descriptor],
-    };
-    await addOneEService(eservice);
-
-    const consumerTenant = {
-      ...getMockTenant(),
-      mails: [getMockTenantMail()],
-    };
-    await addOneTenant(consumerTenant);
-
-    const producerId = generateId<TenantId>();
-
-    const agreement = {
-      ...getMockAgreement(),
-      stamps: {
-        suspensionByProducer: { when: new Date(), who: generateId<UserId>() },
-      },
-      producerId,
-      descriptorId: descriptor.id,
-      eserviceId: eservice.id,
-      consumerId: consumerTenant.id,
-    };
-    await addOneAgreement(agreement);
-
-    await expect(() =>
-      handleAgreementSuspendedByPlatform({
-        agreementV2Msg: toAgreementV2(agreement),
-        logger,
-        templateService,
-        userService,
-        readModelService,
-        correlationId: generateId<CorrelationId>(),
-      })
-    ).rejects.toThrow(tenantNotFound(producerId));
-  });
-
-  it("should throw eServiceNotFound when eservice is not found", async () => {
-    const descriptor = getMockDescriptor();
-    const eServiceId = generateId<EServiceId>();
-
-    const consumerTenant = {
-      ...getMockTenant(),
-      mails: [getMockTenantMail()],
-    };
-    await addOneTenant(consumerTenant);
-
-    const producerTenant = {
-      ...getMockTenant(),
-      mails: [getMockTenantMail()],
-    };
-    await addOneTenant(producerTenant);
+    const unknownConsumerId = generateId<TenantId>();
 
     const agreement = {
       ...getMockAgreement(),
       stamps: {},
       producerId: producerTenant.id,
       descriptorId: descriptor.id,
-      eserviceId: eServiceId,
+      eserviceId: eservice.id,
+      consumerId: unknownConsumerId,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementSuspendedByPlatform({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        templateService,
+        userService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(tenantNotFound(unknownConsumerId));
+  });
+
+  it("should throw tenantNotFound when producer is not found", async () => {
+    const unknownProducerId = generateId<TenantId>();
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: {},
+      producerId: unknownProducerId,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: consumerTenant.id,
+    };
+    await addOneAgreement(agreement);
+    await expect(() =>
+      handleAgreementSuspendedByPlatform({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        templateService,
+        userService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(tenantNotFound(unknownProducerId));
+  });
+
+  it("should throw eServiceNotFound when eservice is not found", async () => {
+    const unknownEServiceId = generateId<EServiceId>();
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: {},
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: unknownEServiceId,
       consumerId: consumerTenant.id,
     };
     await addOneAgreement(agreement);
@@ -202,36 +155,10 @@ describe("handleAgreementSuspendedByPlatform", async () => {
         readModelService,
         correlationId: generateId<CorrelationId>(),
       })
-    ).rejects.toThrow(eServiceNotFound(eServiceId));
+    ).rejects.toThrow(eServiceNotFound(unknownEServiceId));
   });
 
   it("should generate one message per user of the tenant that consumed the eservice", async () => {
-    const descriptor = getMockDescriptor();
-    const eservice = {
-      ...getMockEService(),
-      descriptors: [descriptor],
-    };
-    await addOneEService(eservice);
-
-    const consumerTenant = getMockTenant();
-    await addOneTenant(consumerTenant);
-
-    const producerTenant = getMockTenant();
-    await addOneTenant(producerTenant);
-
-    const users = [
-      getMockUser(consumerTenant.id),
-      getMockUser(consumerTenant.id),
-    ];
-    readModelService.getTenantUsersWithNotificationEnabled = vi
-      .fn()
-      .mockReturnValueOnce(
-        users.map((user) => ({ userId: user.userId, tenantId: user.tenantId }))
-      );
-    userService.readUser.mockImplementation((userId) =>
-      users.find((user) => user.userId === userId)
-    );
-
     const agreement = {
       ...getMockAgreement(),
       stamps: {
@@ -260,7 +187,9 @@ describe("handleAgreementSuspendedByPlatform", async () => {
   it("should not generate a message if the user disabled this email notification", async () => {
     readModelService.getTenantUsersWithNotificationEnabled = vi
       .fn()
-      .mockResolvedValue([users[0]]);
+      .mockResolvedValue([
+        { userId: users[0].id, tenantId: users[0].tenantId },
+      ]);
 
     const agreement = {
       ...getMockAgreement(),
@@ -291,28 +220,6 @@ describe("handleAgreementSuspendedByPlatform", async () => {
   });
 
   it("should generate a complete and correct message", async () => {
-    const descriptor = getMockDescriptor();
-    const eservice = {
-      ...getMockEService(),
-      descriptors: [descriptor],
-    };
-    await addOneEService(eservice);
-
-    const consumerTenant = {
-      ...getMockTenant(),
-      mails: [getMockTenantMail()],
-    };
-    await addOneTenant(consumerTenant);
-
-    const producerTenant = getMockTenant();
-    await addOneTenant(producerTenant);
-
-    const user = getMockUser(consumerTenant.id);
-    readModelService.getTenantUsersWithNotificationEnabled = vi
-      .fn()
-      .mockReturnValueOnce([{ userId: user.userId, tenantId: user.tenantId }]);
-    userService.readUser.mockImplementation((_) => user);
-
     const activationDate = new Date();
     const agreement = {
       ...getMockAgreement(),
@@ -337,11 +244,13 @@ describe("handleAgreementSuspendedByPlatform", async () => {
       readModelService,
       correlationId: generateId<CorrelationId>(),
     });
-    expect(messages.length).toBe(1);
+    expect(messages.length).toBe(2);
     messages.forEach((message) => {
       expect(message.email.body).toContain("<!-- Footer -->");
       expect(message.email.body).toContain("<!-- Title & Main Message -->");
-      expect(message.email.body).toContain(`Nuova richiesta di fruizione`);
+      expect(message.email.body).toContain(
+        `Sospensione richiesta di fruizione da parte della Piattaforma`
+      );
     });
   });
 });
