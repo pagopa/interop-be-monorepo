7ac37151e6d40660f34e27549c9823748ae211c5
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-09-03 10:05:17 +0200

    Align `getMockPurposeTemplate` function with #2285

--- packages/commons-test/src/testUtils.ts
+++ packages/commons-test/src/testUtils.ts
@@ -111,8 +111,9 @@ import {
   UserNotificationConfig,
   DelegationStamps,
   PurposeTemplate,
-  purposeTemplateState,
   tenantKind,
+  purposeTemplateState,
+  PurposeTemplateState,
 } from "pagopa-interop-models";
 import {
   AppContext,
@@ -337,6 +338,23 @@ export const getMockPurpose = (versions?: PurposeVersion[]): Purpose => ({
   createdAt: new Date(),
   isFreeOfCharge: true,
   freeOfChargeReason: "test",
+});
+
+export const getMockPurposeTemplate = (
+  creatorId: TenantId = generateId<TenantId>(),
+  state: PurposeTemplateState = purposeTemplateState.draft
+): PurposeTemplate => ({
+  id: generateId(),
+  targetDescription: "Target description",
+  targetTenantKind: tenantKind.PA,
+  creatorId,
+  state,
+  createdAt: new Date(),
+  purposeTitle: "Purpose title",
+  purposeDescription: "Purpose description",
+
+  purposeIsFreeOfCharge: false,
+  purposeDailyCalls: 100,
 });
 
 export const getMockPurposeVersion = (
@@ -1298,16 +1316,4 @@ export const getMockUserNotificationConfig = (): UserNotificationConfig => ({
   emailConfig: getMockNotificationConfig(),
   createdAt: generateMock(z.coerce.date()),
   updatedAt: generateMock(z.coerce.date().optional()),
-});
-
-export const getMockPurposeTemplate = (): PurposeTemplate => ({
-  id: generateId(),
-  targetDescription: "Mock purpose template target",
-  targetTenantKind: tenantKind.PA,
-  creatorId: generateId(),
-  state: purposeTemplateState.draft,
-  createdAt: new Date(),
-  purposeTitle: "Mock purpose template title",
-  purposeDescription: "Mock purpose template description",
-  purposeIsFreeOfCharge: false,
 });
