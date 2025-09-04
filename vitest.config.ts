9a11a08029f8d5f8b42e1e659441c0825fdbca9b
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2025-09-04 15:46:17 +0200

    test19

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,9 +9,20 @@ export default defineConfig({
     pool: "forks",
   },
   resolve: {
-    alias: {
-      "pagopa-interop-commons-test": resolve(__dirname, "./packages/commons-test/src"),
-      "pagopa-interop-models": resolve(__dirname, "./packages/models/src"),
-    },
+    // alias: {
+    //   "pagopa-interop-commons-test": resolve(__dirname, "./packages/commons-test/src"),
+    //   "pagopa-interop-models": resolve(__dirname, "./packages/models/src"),
+    // },
+
+    alias: [
+      {
+        find: "pagopa-interop-commons-test",
+        replacement: resolve(__dirname, "./packages/commons-test/src"),
+      },
+      {
+        find: /^pagopa-interop-models(\/.*)?$/,
+        replacement: resolve(__dirname, "./packages/models/src$1"),
+      },
+    ],
   },
 });
