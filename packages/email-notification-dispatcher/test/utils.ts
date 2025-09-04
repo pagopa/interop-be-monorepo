87e3aa4b8ace2b230db3ac09f9ca95ac763b3134
Matteo Grilli <matteo.grilli@quinck.io>
2025-09-04 17:41:33 +0200

    fix check and formatting

--- packages/email-notification-dispatcher/test/utils.ts
+++ packages/email-notification-dispatcher/test/utils.ts
@@ -14,7 +14,7 @@ import {
   UserId,
   UserRole,
 } from "pagopa-interop-models";
-import { afterEach, inject, vi } from "vitest";
+import { afterEach, inject } from "vitest";
 import {
   agreementReadModelServiceBuilder,
   catalogReadModelServiceBuilder,
