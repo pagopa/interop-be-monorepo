7f958f34c5cf041a8893c45cddeca2fe021bb56d
Matteo Grilli <matteo.grilli@quinck.io>
2025-09-11 17:01:28 +0200

    template fixes

--- packages/email-notification-dispatcher/test/handleAgreementUnsuspendedByConsumer.test.ts
+++ packages/email-notification-dispatcher/test/handleAgreementUnsuspendedByConsumer.test.ts
@@ -81,6 +81,33 @@ describe("handleAgreementUnsuspendedByConsumer", async () => {
     );
   });
 
+  it("should throw tenantNotFound when consumer is not found", async () => {
+    const unknownConsumerId = generateId<TenantId>();
+
+    const agreement = {
+      ...getMockAgreement(),
+      stamps: {
+        suspensionByConsumer: { when: new Date(), who: generateId<UserId>() },
+      },
+      producerId: producerTenant.id,
+      descriptorId: descriptor.id,
+      eserviceId: eservice.id,
+      consumerId: unknownConsumerId,
+    };
+    await addOneAgreement(agreement);
+
+    await expect(() =>
+      handleAgreementUnsuspendedByConsumer({
+        agreementV2Msg: toAgreementV2(agreement),
+        logger,
+        templateService,
+        userService,
+        readModelService,
+        correlationId: generateId<CorrelationId>(),
+      })
+    ).rejects.toThrow(tenantNotFound(unknownConsumerId));
+  });
+
   it("should throw tenantNotFound when producer is not found", async () => {
     const unknownProducerId = generateId<TenantId>();
 
