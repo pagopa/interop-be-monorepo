40336bf94ab0d094a62f4d01f909c035313be2ba
Matteo Grilli <matteo.grilli@quinck.io>
2025-09-11 17:01:28 +0200

    template fixes

--- packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementSuspendedByConsumer.ts
+++ packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementSuspendedByConsumer.ts
@@ -40,12 +40,13 @@ export async function handleAgreementSuspendedByConsumer(
 
   const agreement = fromAgreementV2(agreementV2Msg);
 
-  const [htmlTemplate, eservice, producer] = await Promise.all([
+  const [htmlTemplate, eservice, producer, consumer] = await Promise.all([
     retrieveHTMLTemplate(
       eventMailTemplateType.agreementSuspendedByConsumerMailTemplate
     ),
     retrieveAgreementEservice(agreement, readModelService),
     retrieveTenant(agreement.producerId, readModelService),
+    retrieveTenant(agreement.consumerId, readModelService),
   ]);
 
   const targets = await getRecipientsForTenants({
@@ -72,6 +73,7 @@ export async function handleAgreementSuspendedByConsumer(
         title: `Sospensione richiesta di fruizione da parte del fruitore`,
         notificationType,
         entityId: agreement.id,
+        consumerName: consumer.name,
         producerName: producer.name,
         eserviceName: eservice.name,
         ctaLabel: `Visualizza richiesta`,
