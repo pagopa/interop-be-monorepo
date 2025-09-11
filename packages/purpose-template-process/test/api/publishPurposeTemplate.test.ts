c27ca3ede2c45fd70bb7cfe1c011b94f3f5ae182
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-09-11 15:08:05 +0200

    Add purpose template publish API tests

---
+++ packages/purpose-template-process/test/api/publishPurposeTemplate.test.ts
@@ -0,0 +1,110 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { purposeTemplateApi } from "pagopa-interop-api-clients";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import {
+  generateToken,
+  getMockPurposeTemplate,
+  getMockWithMetadata,
+} from "pagopa-interop-commons-test";
+import request from "supertest";
+import { AuthRole, authRole } from "pagopa-interop-commons";
+import {
+  generateId,
+  operationForbidden,
+  PurposeTemplateId,
+  purposeTemplateState,
+} from "pagopa-interop-models";
+import { purposeTemplateToApiPurposeTemplate } from "../../src/model/domain/apiConverter.js";
+import { api, purposeTemplateService } from "../vitest.api.setup.js";
+import {
+  missingRiskAnalysisFormTemplate,
+  purposeTemplateNotFound,
+  purposeTemplateNotInExpectedState,
+  riskAnalysisTemplateValidationFailed,
+} from "../../src/model/domain/errors.js";
+
+describe("API POST /purposeTemplates/{id}/publish", () => {
+  const purposeTemplate = getMockPurposeTemplate();
+  const serviceResponse = getMockWithMetadata(purposeTemplate);
+
+  const apiResponse = purposeTemplateApi.PurposeTemplate.parse(
+    purposeTemplateToApiPurposeTemplate(purposeTemplate)
+  );
+
+  beforeEach(() => {
+    purposeTemplateService.publishPurposeTemplate = vi
+      .fn()
+      .mockResolvedValue(serviceResponse);
+  });
+
+  const makeRequest = async (
+    token: string,
+    purposeTemplateId: PurposeTemplateId = purposeTemplate.id
+  ) =>
+    request(api)
+      .post(`/purposeTemplates/${purposeTemplateId}/publish`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("X-Correlation-Id", generateId());
+
+  const authorizedRoles: AuthRole[] = [
+    authRole.ADMIN_ROLE,
+    authRole.M2M_ADMIN_ROLE,
+  ];
+
+  it.each(authorizedRoles)(
+    "Should return 200 for user with role %s",
+    async (role) => {
+      const token = generateToken(role);
+      const res = await makeRequest(token);
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual(apiResponse);
+      expect(res.headers["x-metadata-version"]).toBe(
+        serviceResponse.metadata.version.toString()
+      );
+    }
+  );
+
+  it.each(
+    Object.values(authRole).filter((role) => !authorizedRoles.includes(role))
+  )("Should return 403 for user with role %s", async (role) => {
+    const token = generateToken(role);
+    const res = await makeRequest(token);
+    expect(res.status).toBe(403);
+  });
+
+  it.each([
+    {
+      error: missingRiskAnalysisFormTemplate(purposeTemplate.id),
+      expectedStatus: 400,
+    },
+    { error: riskAnalysisTemplateValidationFailed([]), expectedStatus: 400 },
+    {
+      error: purposeTemplateNotInExpectedState(
+        generateId(),
+        purposeTemplateState.active
+      ),
+      expectedStatus: 400,
+    },
+    { error: operationForbidden, expectedStatus: 403 },
+    {
+      error: purposeTemplateNotFound(generateId()),
+      expectedStatus: 404,
+    },
+  ])(
+    "Should return $expectedStatus for $error.code",
+    async ({ error, expectedStatus }) => {
+      purposeTemplateService.publishPurposeTemplate = vi
+        .fn()
+        .mockRejectedValue(error);
+      const token = generateToken(authRole.ADMIN_ROLE);
+      const res = await makeRequest(token);
+      expect(res.status).toBe(expectedStatus);
+    }
+  );
+
+  it("Should return 400 if passed invalid data: %s", async () => {
+    const token = generateToken(authRole.ADMIN_ROLE);
+    const res = await makeRequest(token, "invalid" as PurposeTemplateId);
+    expect(res.status).toBe(400);
+  });
+});
