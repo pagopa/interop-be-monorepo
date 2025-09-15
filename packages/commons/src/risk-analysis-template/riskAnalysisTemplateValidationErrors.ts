bf361e160c3fa886d5e67d2dd364d775c945621e
shuyec <76391491+shuyec@users.noreply.github.com>
2025-09-15 17:09:28 +0200

    feature: add purpose template splitters, aggregators, queries (PIN-7335) (#2244)
    Co-authored-by: Vittorio Caprio <vittorio.caprio@buildo.io>
    Co-authored-by: Paolo Manca <paolo.manca@pagopa.it>
    Co-authored-by: Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>
    Co-authored-by: Alepazz <alessio.pazzani@gmail.com>

---
+++ packages/commons/src/risk-analysis-template/riskAnalysisTemplateValidationErrors.ts
@@ -0,0 +1,153 @@
+import { InternalError, TenantKind } from "pagopa-interop-models";
+
+type RiskAnalysisTemplateValidationIssueCode =
+  | "unexpectedTemplateFieldValueOrSuggestionError"
+  | "malformedTemplateFieldValueOrSuggestion"
+  | "templateDependencyNotFound"
+  | "templateDependencyNotBeingEditable"
+  | "unexpectedTemplateFieldValueError"
+  | "unexpectedTemplateFieldError"
+  | "noRulesVersionTemplateFoundError"
+  | "unexpectedTemplateRulesVersionError"
+  | "unexpectedTemplateDependencyValueError"
+  | "missingExpectedFieldError";
+
+export class RiskAnalysisTemplateValidationIssue extends InternalError<RiskAnalysisTemplateValidationIssueCode> {
+  constructor({
+    code,
+    detail,
+  }: {
+    code: RiskAnalysisTemplateValidationIssueCode;
+    detail: string;
+  }) {
+    super({ code, detail });
+  }
+}
+
+export type RiskAnalysisTemplateValidationInvalid = {
+  type: "invalid";
+  issues: RiskAnalysisTemplateValidationIssue[];
+};
+
+export type RiskAnalysisTemplateValidationValid<T> = {
+  type: "valid";
+  value: T;
+};
+
+export type RiskAnalysisTemplateValidationResult<T> =
+  | RiskAnalysisTemplateValidationValid<T>
+  | RiskAnalysisTemplateValidationInvalid;
+
+export function unexpectedTemplateFieldValueOrSuggestionError(
+  fieldName: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "unexpectedTemplateFieldValueOrSuggestionError",
+    detail: `Field ${fieldName} value or suggestion not allowed`,
+  });
+}
+
+export function unexpectedTemplateFieldValueError(
+  fieldName: string,
+  allowedValues: Set<string>
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "unexpectedTemplateFieldValueError",
+    detail: `Field ${fieldName} should be one of [${Array.from(
+      allowedValues
+    ).join(",")}]`,
+  });
+}
+
+export function unexpectedTemplateFieldError(
+  fieldName: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "unexpectedTemplateFieldError",
+    detail: `Unexpected field ${fieldName}`,
+  });
+}
+
+export function malformedTemplateFieldValueOrSuggestionError(
+  fieldName: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "malformedTemplateFieldValueOrSuggestion",
+    detail: `Field ${fieldName} has conflicting or invalid value and suggestion configuration`,
+  });
+}
+
+export function templateDependencyNotFoundError(
+  dependentField: string,
+  dependencyField: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "templateDependencyNotFound",
+    detail: `Field ${dependentField} depends on ${dependencyField} which is missing`,
+  });
+}
+
+export function unexpectedTemplateDependencyEditableError(
+  dependentField: string,
+  dependencyField: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "templateDependencyNotBeingEditable",
+    detail: `Field ${dependentField} depends on ${dependencyField} which is editable`,
+  });
+}
+
+export function noRulesVersionTemplateFoundError(
+  kind: TenantKind
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "noRulesVersionTemplateFoundError",
+    detail: `Ruleset version for tenant kind ${kind} not found`,
+  });
+}
+
+export function unexpectedTemplateRulesVersionError(
+  version: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "unexpectedTemplateRulesVersionError",
+    detail: `Unexpected ruleset version ${version}`,
+  });
+}
+export function unexpectedTemplateDependencyValueError(
+  dependentField: string,
+  dependencyField: string,
+  expectedValue: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "unexpectedTemplateDependencyValueError",
+    detail: `Field ${dependentField} requires field ${dependencyField} value to be ${expectedValue}`,
+  });
+}
+
+export function missingExpectedTemplateFieldError(
+  fieldName: string
+): RiskAnalysisTemplateValidationIssue {
+  return new RiskAnalysisTemplateValidationIssue({
+    code: "missingExpectedFieldError",
+    detail: `Expected field ${fieldName} not found in form`,
+  });
+}
+
+export function invalidTemplateResult(
+  issues: RiskAnalysisTemplateValidationIssue[]
+): RiskAnalysisTemplateValidationInvalid {
+  return {
+    type: "invalid",
+    issues,
+  };
+}
+
+export function validTemplateResult<T>(
+  value: T
+): RiskAnalysisTemplateValidationResult<T> {
+  return {
+    type: "valid",
+    value,
+  };
+}
