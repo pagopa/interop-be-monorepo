06ff315c099d33f3788922ed0930be914608394a
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-09-16 12:00:26 +0200

    Refactor

--- packages/purpose-process/src/services/purposeService.ts
+++ packages/purpose-process/src/services/purposeService.ts
@@ -100,7 +100,7 @@ import {
 } from "../model/domain/toEvent.js";
 import { config } from "../config/config.js";
 import { PurposeDocumentEServiceInfo } from "../model/domain/models.js";
-import { GetPurposesFilters, ReadModelService } from "./readModelService.js";
+import { GetPurposesFilters } from "./readModelService.js";
 import {
   assertEserviceMode,
   assertConsistentFreeOfCharge,
@@ -125,10 +125,11 @@ import {
   getOrganizationRole,
 } from "./validators.js";
 import { riskAnalysisDocumentBuilder } from "./riskAnalysisDocumentBuilder.js";
+import { ReadModelServiceSQL } from "./readModelServiceSQL.js";
 
 const retrievePurpose = async (
   purposeId: PurposeId,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<WithMetadata<Purpose>> => {
   const purpose = await readModelService.getPurposeById(purposeId);
   if (purpose === undefined) {
@@ -172,7 +173,7 @@ const retrievePurposeVersionDocument = (
 
 const retrieveEService = async (
   eserviceId: EServiceId,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<EService> => {
   const eservice = await readModelService.getEServiceById(eserviceId);
   if (eservice === undefined) {
@@ -183,7 +184,7 @@ const retrieveEService = async (
 
 const retrieveTenant = async (
   tenantId: TenantId,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<Tenant> => {
   const tenant = await readModelService.getTenantById(tenantId);
   if (tenant === undefined) {
@@ -195,7 +196,7 @@ const retrieveTenant = async (
 export const retrieveActiveAgreement = async (
   eserviceId: EServiceId,
   consumerId: TenantId,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<Agreement> => {
   const activeAgreement = await readModelService.getActiveAgreement(
     eserviceId,
@@ -224,7 +225,7 @@ const retrieveRiskAnalysis = (
 
 async function retrieveTenantKind(
   tenantId: TenantId,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<TenantKind> {
   const tenant = await retrieveTenant(tenantId, readModelService);
   if (!tenant.kind) {
@@ -235,7 +236,7 @@ async function retrieveTenantKind(
 
 export const retrievePurposeDelegation = async (
   purpose: Purpose,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<Delegation | undefined> => {
   if (!purpose.delegationId) {
     return undefined;
@@ -253,7 +254,7 @@ export const retrievePurposeDelegation = async (
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function purposeServiceBuilder(
   dbInstance: DB,
-  readModelService: ReadModelService,
+  readModelService: ReadModelServiceSQL,
   fileManager: FileManager,
   pdfGenerator: PDFGenerator
 ) {
@@ -1568,7 +1569,7 @@ const replacePurposeVersion = (
 const retrieveKindOfInvolvedTenantByEServiceMode = async (
   eservice: EService,
   consumerId: TenantId,
-  readModelService: ReadModelService
+  readModelService: ReadModelServiceSQL
 ): Promise<TenantKind> => {
   if (eservice.mode === eserviceMode.deliver) {
     return retrieveTenantKind(consumerId, readModelService);
@@ -1608,7 +1609,7 @@ const performUpdatePurpose = async (
         updateContent: purposeApi.ReversePurposeUpdateContent;
       },
   authData: UIAuthData | M2MAdminAuthData,
-  readModelService: ReadModelService,
+  readModelService: ReadModelServiceSQL,
   correlationId: CorrelationId,
   repository: ReturnType<typeof eventRepository<PurposeEvent>>
   // eslint-disable-next-line max-params
@@ -1727,7 +1728,7 @@ async function generateRiskAnalysisDocument({
   eservice: EService;
   purpose: Purpose;
   dailyCalls: number;
-  readModelService: ReadModelService;
+  readModelService: ReadModelServiceSQL;
   fileManager: FileManager;
   pdfGenerator: PDFGenerator;
   logger: Logger;
@@ -1887,7 +1888,7 @@ async function activatePurposeLogic({
   purpose: WithMetadata<Purpose>;
   purposeVersion: PurposeVersion;
   eservice: EService;
-  readModelService: ReadModelService;
+  readModelService: ReadModelServiceSQL;
   fileManager: FileManager;
   pdfGenerator: PDFGenerator;
   correlationId: CorrelationId;
