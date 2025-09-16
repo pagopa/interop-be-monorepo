7d2d1f6826ca27cc9a10ec9f657011f957097bc1
Eric Camellini <eric.camellini@gmail.com>
2025-09-16 12:43:13 +0200

    Finalizing test setup

--- packages/m2m-event-dispatcher/test/utils.ts
+++ packages/m2m-event-dispatcher/test/utils.ts
@@ -1,14 +1,35 @@
+import { desc } from "drizzle-orm";
 import { setupTestContainersVitest } from "pagopa-interop-commons-test";
+import { attributeM2MEventInM2MEvent } from "pagopa-interop-m2m-event-db-models";
 import { afterEach, inject } from "vitest";
+import { AttributeM2MEvent } from "pagopa-interop-models";
+import { m2mEventWriterServiceSQLBuilder } from "../src/services/m2mEventWriterServiceSQL.js";
 
-export const { cleanup, readModelDB } = await setupTestContainersVitest(
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  inject("readModelSQLConfig")
-);
+export const { cleanup, readModelDB, m2mEventDB } =
+  await setupTestContainersVitest(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    inject("readModelSQLConfig"),
+    undefined,
+    undefined,
+    inject("m2mEventDbConfig")
+  );
 
 afterEach(cleanup);
+
+export const testM2mEventWriterService =
+  m2mEventWriterServiceSQLBuilder(m2mEventDB);
+
+export async function retrieveLastAttributeM2MEvent(): Promise<AttributeM2MEvent> {
+  const sqlEvents = await m2mEventDB
+    .select()
+    .from(attributeM2MEventInM2MEvent)
+    .orderBy(desc(attributeM2MEventInM2MEvent.id))
+    .limit(1);
+
+  return AttributeM2MEvent.parse(sqlEvents[0]);
+}
