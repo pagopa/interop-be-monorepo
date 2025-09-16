0898c3b22f5a65be1d2771fdeef987bfeb6e7eda
Matteo Grilli <matteo.grilli@quinck.io>
2025-09-16 16:06:24 +0200

    fix post rebase

--- packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementUnsuspendedByProducer.ts
+++ packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementUnsuspendedByProducer.ts
@@ -41,7 +41,7 @@ export async function handleAgreementUnsuspendedByProducer(
   const agreement = fromAgreementV2(agreementV2Msg);
 
   const [htmlTemplate, eservice, producer, consumer] = await Promise.all([
-    retrieveHTMLTemplate(eventMailTemplateType.agreementUnsuspendedByProducer),
+    retrieveHTMLTemplate(eventMailTemplateType.agreementUnsuspendedByProducerMailTemplate),
     retrieveAgreementEservice(agreement, readModelService),
     retrieveTenant(agreement.producerId, readModelService),
     retrieveTenant(agreement.consumerId, readModelService),
