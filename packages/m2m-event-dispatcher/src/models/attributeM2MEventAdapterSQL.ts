a5a2249a1557d40393198d06506bad90b5d4762d
Eric Camellini <eric.camellini@gmail.com>
2025-09-17 12:38:28 +0200

    Removing useless branding

--- packages/m2m-event-dispatcher/src/models/attributeM2MEventAdapterSQL.ts
+++ packages/m2m-event-dispatcher/src/models/attributeM2MEventAdapterSQL.ts
@@ -1,8 +1,4 @@
-import {
-  AttributeEventEnvelope,
-  dateToString,
-  unsafeBrandId,
-} from "pagopa-interop-models";
+import { AttributeEventEnvelope, dateToString } from "pagopa-interop-models";
 import { AttributeM2MEventSQL } from "pagopa-interop-m2m-event-db-models";
 import { generateM2MEventId } from "../utils/uuidv7.js";
 
@@ -14,6 +10,6 @@ export function toNewAttributeM2MEventSQL(
     id: generateM2MEventId(),
     eventType: event.type,
     eventTimestamp: dateToString(eventTimestamp),
-    attributeId: unsafeBrandId(event.stream_id),
+    attributeId: event.stream_id,
   };
 }
