b3d134c1cb0ab6b46af99c512491e4e9c9453900
Eric Camellini <eric.camellini@gmail.com>
2025-09-17 12:54:22 +0200

    Adding integration tests

--- packages/m2m-event-manager/test/integrationUtils.ts
+++ packages/m2m-event-manager/test/integrationUtils.ts
@@ -1,5 +1,7 @@
 import { setupTestContainersVitest } from "pagopa-interop-commons-test";
 import { inject, afterEach } from "vitest";
+import { AttributeM2MEvent, dateToString } from "pagopa-interop-models";
+import { attributeM2MEventInM2MEvent } from "pagopa-interop-m2m-event-db-models";
 import { m2mEventServiceBuilder } from "../src/services/m2mEventService.js";
 import { m2mEventReaderServiceSQLBuilder } from "../src/services/m2mEventReaderServiceSQL.js";
 
@@ -20,3 +22,14 @@ afterEach(cleanup);
 
 const m2mEventReaderServiceSQL = m2mEventReaderServiceSQLBuilder(m2mEventDB);
 export const m2mEventService = m2mEventServiceBuilder(m2mEventReaderServiceSQL);
+
+export async function writeAttributeM2MEvent(
+  event: AttributeM2MEvent
+): Promise<void> {
+  await m2mEventDB.insert(attributeM2MEventInM2MEvent).values([
+    {
+      ...event,
+      eventTimestamp: dateToString(event.eventTimestamp),
+    },
+  ]);
+}
