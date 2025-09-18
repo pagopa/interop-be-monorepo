780e3f08c35c58660cb3c826794bbab238e7f712
Eric Camellini <eric.camellini@gmail.com>
2025-09-18 18:41:44 +0200

    Improving typecheck

--- packages/models/src/m2m-event/attributeM2MEvent.ts
+++ packages/models/src/m2m-event/attributeM2MEvent.ts
@@ -2,13 +2,14 @@ import { z } from "zod";
 import { AttributeId, AttributeM2MEventId } from "../brandedIds.js";
 import { AttributeEvent } from "../attribute/attributeEvents.js";
 
-const AttributeM2MEventType = z.union([
-  z.literal("AttributeAdded"),
-  z.literal("MaintenanceAttributeDeleted"),
-]);
-export type AttributeM2MEventType = z.infer<typeof AttributeM2MEventType> &
-  AttributeEvent["type"];
-// ^ make sure it's compatible with AttributeEvent types
+const AttributeM2MEventType = z.literal("AttributeAdded");
+// ^ Convert into a z.union([...]) when supporting more event types
+export type AttributeM2MEventType = z.infer<typeof AttributeM2MEventType>;
+
+const _: AttributeEvent["type"] = {} as AttributeM2MEventType;
+// ^ Type check: ensure AttributeM2MEventType options are a subset of AttributeEvent["type"]
+
+void _; // avoid unused variable TS error, cannot use ts-ignore for a type check
 
 export const AttributeM2MEvent = z.object({
   id: AttributeM2MEventId,
