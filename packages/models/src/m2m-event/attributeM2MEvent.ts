f956640b3a25836fb70bba1bcfd21fc57cdac4a6
Eric Camellini <eric.camellini@gmail.com>
2025-09-18 18:46:53 +0200

    Improve comment

--- packages/models/src/m2m-event/attributeM2MEvent.ts
+++ packages/models/src/m2m-event/attributeM2MEvent.ts
@@ -7,7 +7,8 @@ const AttributeM2MEventType = z.literal("AttributeAdded");
 export type AttributeM2MEventType = z.infer<typeof AttributeM2MEventType>;
 
 const _: AttributeEvent["type"] = {} as AttributeM2MEventType;
-// ^ Type check: ensure AttributeM2MEventType options are a subset of AttributeEvent["type"]
+// ^ Type check: ensure AttributeM2MEventType options are a subset of AttributeEvent["type"].
+//   This is required because Zod does not have an equivalent of TS Extract<...>.
 
 void _; // avoid unused variable TS error, cannot use ts-ignore for a type check
 
