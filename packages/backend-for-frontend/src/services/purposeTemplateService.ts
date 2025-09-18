32dfcb05553c9d232a70d9fbaa678ffb8c6b8d20
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-09-18 15:55:34 +0200

    feature: add targetTenantKind to GET /catalog/purposeTemplates (BFF)

--- packages/backend-for-frontend/src/services/purposeTemplateService.ts
+++ packages/backend-for-frontend/src/services/purposeTemplateService.ts
@@ -4,6 +4,7 @@ import {
   tenantApi,
 } from "pagopa-interop-api-clients";
 import { WithLogger } from "pagopa-interop-commons";
+import { TenantKind } from "pagopa-interop-models";
 import {
   PurposeTemplateProcessClient,
   TenantProcessClient,
@@ -82,13 +83,23 @@ export function purposeTemplateServiceBuilder(
         },
       };
     },
-    async getCatalogPurposeTemplates(
-      purposeTitle: string | undefined,
-      eserviceIds: string[],
-      offset: number,
-      limit: number,
-      { headers, logger }: WithLogger<BffAppContext>
-    ): Promise<bffApi.CatalogPurposeTemplates> {
+    async getCatalogPurposeTemplates({
+      purposeTitle,
+      targetTenantKind,
+      eserviceIds,
+      offset,
+      limit,
+      ctx,
+    }: {
+      purposeTitle: string | undefined;
+      targetTenantKind: TenantKind | undefined;
+      eserviceIds: string[];
+      offset: number;
+      limit: number;
+      ctx: WithLogger<BffAppContext>;
+    }): Promise<bffApi.CatalogPurposeTemplates> {
+      const { headers, logger } = ctx;
+
       logger.info(
         `Retrieving catalog purpose templates with title ${purposeTitle}, eserviceIds ${eserviceIds.toString()} offset ${offset}, limit ${limit}`
       );
@@ -98,6 +109,7 @@ export function purposeTemplateServiceBuilder(
           headers,
           queries: {
             purposeTitle,
+            targetTenantKind,
             eserviceIds,
             states: [purposeTemplateApi.PurposeTemplateState.Enum.ACTIVE],
             limit,
