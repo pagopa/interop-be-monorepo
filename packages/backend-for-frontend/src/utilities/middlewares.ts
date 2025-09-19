5e0a94897a70a6d6cc14f1522387d383160a5f2e
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-09-19 14:44:39 +0200

    feature: add feature flag (PIN-7588)

--- packages/backend-for-frontend/src/utilities/middlewares.ts
+++ packages/backend-for-frontend/src/utilities/middlewares.ts
@@ -3,10 +3,15 @@ import { ZodiosRouterContextRequestHandler } from "@zodios/express";
 import {
   ExpressContext,
   fromAppContext,
+  isFeatureFlagEnabled,
   isUiAuthData,
 } from "pagopa-interop-commons";
-import { unauthorizedError } from "pagopa-interop-models";
+import {
+  featureFlagNotEnabled,
+  unauthorizedError,
+} from "pagopa-interop-models";
 import { makeApiProblem } from "../model/errors.js";
+import { config } from "../config/config.js";
 
 export function uiAuthDataValidationMiddleware(): ZodiosRouterContextRequestHandler<ExpressContext> {
   return async (req, res, next) => {
@@ -21,6 +26,23 @@ export function uiAuthDataValidationMiddleware(): ZodiosRouterContextRequestHand
         unauthorizedError(
           `Invalid role ${ctx.authData.systemRole} for this operation`
         ),
+        () => constants.HTTP_STATUS_FORBIDDEN,
+        ctx
+      );
+      return res.status(errorRes.status).send(errorRes);
+    }
+
+    return next();
+  };
+}
+
+export function purposeTemplateConfigFeatureFlagMiddleware(): ZodiosRouterContextRequestHandler<ExpressContext> {
+  return async (req, res, next) => {
+    const ctx = fromAppContext(req.ctx);
+
+    if (!isFeatureFlagEnabled(config, "featureFlagNotificationConfig")) {
+      const errorRes = makeApiProblem(
+        featureFlagNotEnabled("featureFlagPurposeTemplateConfig"),
         () => constants.HTTP_STATUS_FORBIDDEN,
         ctx
       );
