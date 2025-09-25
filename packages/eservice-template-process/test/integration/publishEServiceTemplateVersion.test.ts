9aa4eabceea2727ebd71197393431f773cf8592d
Crasherbit <sagripantimatteo@gmail.com>
2025-09-25 09:25:36 +0200

    test

--- packages/eservice-template-process/test/integration/publishEServiceTemplateVersion.test.ts
+++ packages/eservice-template-process/test/integration/publishEServiceTemplateVersion.test.ts
@@ -411,13 +411,15 @@ describe("publishEServiceTemplateVersion", () => {
     await addOneEServiceTemplate(eserviceTemplate);
 
     expect(
-      await eserviceTemplateService.publishEServiceTemplateVersion(
-        eserviceTemplate.id,
-        eserviceTemplateVersion.id,
-        getMockContext({
-          authData: getMockAuthData(eserviceTemplate.creatorId),
-        })
-      )
+      (
+        await eserviceTemplateService.publishEServiceTemplateVersion(
+          eserviceTemplate.id,
+          eserviceTemplateVersion.id,
+          getMockContext({
+            authData: getMockAuthData(eserviceTemplate.creatorId),
+          })
+        )
+      ).data
     ).equal(undefined);
   });
 });
