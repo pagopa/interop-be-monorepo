51f78e9c9f688a1538dadcf1013dbd004da41faa
Alepazz <alessio.pazzani@gmail.com>
2025-09-25 14:17:52 +0200

    Refactor: Refactor addEServiceInterfaceByTemplateErrorMapper and removed createPurposeTemplateRiskAnalysisAnswerErrorMapper (PIN-7734)

--- packages/backend-for-frontend/src/utilities/errorMappers.ts
+++ packages/backend-for-frontend/src/utilities/errorMappers.ts
@@ -238,14 +238,7 @@ export const addEServiceInterfaceByTemplateErrorMapper = (
     .with(
       "eserviceTemplateIsNotPublished",
       "eserviceIsNotDraft",
-      "eserviceDescriptorDraftNotFound",
       "eserviceTemplateNotFound",
-      "eserviceTemplateInterfaceNotFound",
-      "eserviceTemplateInterfaceDataNotValid",
-      "invalidContentTypeDetected",
-      "invalidEserviceInterfaceFileDetected",
-      "interfaceExtractingInfoError",
-      "templateDataNotFound",
       () => HTTP_STATUS_BAD_REQUEST
     )
     .with("invalidEserviceRequester", () => HTTP_STATUS_FORBIDDEN)
@@ -265,21 +258,4 @@ export const getEServiceTemplateInstancesErrorMapper = (
 ): number =>
   match(error.code)
     .with("eserviceTemplateNotFound", () => HTTP_STATUS_NOT_FOUND)
-    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
-
-export const createPurposeTemplateRiskAnalysisAnswerErrorMapper = (
-  error: ApiError<ErrorCodes>
-): number =>
-  match(error.code)
-    .with("purposeTemplateNotFound", () => HTTP_STATUS_NOT_FOUND)
-    .with(
-      "riskAnalysisTemplateValidationFailed",
-      "annotationTextLengthError",
-      "hyperlinkDetectionError",
-      () => HTTP_STATUS_BAD_REQUEST
-    )
-    .with(
-      "purposeTemplateRiskAnalysisFormNotFound",
-      () => HTTP_STATUS_INTERNAL_SERVER_ERROR
-    )
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
