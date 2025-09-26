624e0061d8e5521f8d8351dd4f5ecd46b05bb41d
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-09-26 12:09:34 +0200

    feature: add purpose template suspend to BFF

--- packages/backend-for-frontend/src/routers/purposeTemplateRouter.ts
+++ packages/backend-for-frontend/src/routers/purposeTemplateRouter.ts
@@ -164,7 +164,27 @@ const purposeTemplateRouter = (
           return res.status(errorRes.status).send(errorRes);
         }
       }
-    );
+    )
+    .post("/purposeTemplates/:purposeTemplateId/suspend", async (req, res) => {
+      const ctx = fromBffAppContext(req.ctx, req.headers);
+
+      try {
+        const response = await purposeTemplateService.suspendPurposeTemplate(
+          unsafeBrandId(req.params.purposeTemplateId),
+          ctx
+        );
+
+        return res.status(200).send(bffApi.PurposeTemplate.parse(response));
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          emptyErrorMapper,
+          ctx,
+          `Error suspending purpose template ${req.params.purposeTemplateId}`
+        );
+        return res.status(errorRes.status).send(errorRes);
+      }
+    });
 
   return purposeTemplateRouter;
 };
