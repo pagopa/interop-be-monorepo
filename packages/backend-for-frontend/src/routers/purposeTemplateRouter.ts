83677842e3045443784e47e3c3dfcf43dc04a531
Paolo Manca <paolo.manca@pagopa.it>
2025-09-30 15:24:29 +0200

    purpose template: fix import and error mapper (PIN-7394)

--- packages/backend-for-frontend/src/routers/purposeTemplateRouter.ts
+++ packages/backend-for-frontend/src/routers/purposeTemplateRouter.ts
@@ -12,7 +12,6 @@ import { PurposeTemplateService } from "../services/purposeTemplateService.js";
 import { fromBffAppContext } from "../utilities/context.js";
 import {
   getCatalogPurposeTemplatesErrorMapper,
-  getPurposeTemplateErrorMapper,
   linkEServiceToPurposeTemplateErrorMapper,
 } from "../utilities/errorMappers.js";
 
--- packages/backend-for-frontend/src/utilities/errorMappers.ts
+++ packages/backend-for-frontend/src/utilities/errorMappers.ts
@@ -282,7 +282,6 @@ export const linkEServiceToPurposeTemplateErrorMapper = (
       "associationBetweenEServiceAndPurposeTemplateAlreadyExists",
       () => HTTP_STATUS_CONFLICT
     )
-    .with("featureFlagNotEnabled", () => HTTP_STATUS_NOT_IMPLEMENTED)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
 
 export const getCatalogPurposeTemplatesErrorMapper = (
