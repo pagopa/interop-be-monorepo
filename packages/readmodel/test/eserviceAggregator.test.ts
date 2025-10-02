f4e27e768b8d48044c4e0727244e1d61c5fa02a8
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-02 15:50:34 +0200

    feat: added group id e-service attribute group order consistency tests

--- packages/readmodel/test/eserviceAggregator.test.ts
+++ packages/readmodel/test/eserviceAggregator.test.ts
@@ -10,10 +10,14 @@ import {
   agreementApprovalPolicy,
   Descriptor,
   EService,
+  EServiceAddedV2,
+  EserviceAttributes,
   EServiceTemplateId,
   EServiceTemplateVersionRef,
+  fromEServiceV2,
   generateId,
   tenantKind,
+  toEServiceV2,
 } from "pagopa-interop-models";
 import { describe, it, expect } from "vitest";
 import { splitEserviceIntoObjectsSQL } from "../src/catalog/splitters.js";
@@ -135,5 +139,128 @@ describe("E-service aggregator", () => {
       data: eservice,
       metadata: { version: 1 },
     });
+  });
+
+  /**
+   * !! IMPORTANT !!
+   * This test ensures that the order of attribute groups are consistent with the original order
+   * in the descriptor, even after protobuf deserialization, splitting,
+   * aggregating a reserialization of the eservice object.
+   *
+   * This is important because some functionalities in APIV2 (e.g. in-add attributes endpoints)
+   * rely on the order of these groups.
+   */
+  it("should keep the descriptor attributes group order by groupIndex", () => {
+    const descriptorCertifiedAttributeGroups: EserviceAttributes["certified"] =
+      [
+        [getMockEServiceAttribute()],
+        [getMockEServiceAttribute(), getMockEServiceAttribute()],
+        [getMockEServiceAttribute(), getMockEServiceAttribute()],
+        [
+          getMockEServiceAttribute(),
+          getMockEServiceAttribute(),
+          getMockEServiceAttribute(),
+        ],
+      ];
+
+    const descriptorVerifiedAttributeGroups: EserviceAttributes["verified"] = [
+      [getMockEServiceAttribute()],
+      [getMockEServiceAttribute(), getMockEServiceAttribute()],
+      [getMockEServiceAttribute(), getMockEServiceAttribute()],
+      [
+        getMockEServiceAttribute(),
+        getMockEServiceAttribute(),
+        getMockEServiceAttribute(),
+      ],
+    ];
+
+    const descriptorDeclaredAttributeGroups: EserviceAttributes["declared"] = [
+      [getMockEServiceAttribute()],
+      [getMockEServiceAttribute(), getMockEServiceAttribute()],
+      [getMockEServiceAttribute(), getMockEServiceAttribute()],
+      [
+        getMockEServiceAttribute(),
+        getMockEServiceAttribute(),
+        getMockEServiceAttribute(),
+      ],
+    ];
+
+    const descriptor: Descriptor = {
+      ...getMockDescriptor(),
+      attributes: {
+        certified: descriptorCertifiedAttributeGroups,
+        verified: descriptorVerifiedAttributeGroups,
+        declared: descriptorDeclaredAttributeGroups,
+      },
+    };
+
+    const eservice = getMockEService(undefined, undefined, [descriptor]);
+    const serialized = EServiceAddedV2.toBinary({
+      eservice: toEServiceV2(eservice),
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    for (const _ of Array(100)) {
+      const deserialized = EServiceAddedV2.fromBinary(serialized).eservice;
+
+      if (!deserialized) {
+        throw new Error("Deserialized eservice is undefined");
+      }
+
+      const deserializedEService = fromEServiceV2(deserialized);
+
+      const {
+        eserviceSQL,
+        riskAnalysesSQL,
+        riskAnalysisAnswersSQL,
+        descriptorsSQL,
+        attributesSQL,
+        interfacesSQL,
+        documentsSQL,
+        rejectionReasonsSQL,
+        templateVersionRefsSQL,
+      } = splitEserviceIntoObjectsSQL(deserializedEService, 1);
+
+      const aggregatedEservice = aggregateEservice({
+        eserviceSQL,
+        riskAnalysesSQL,
+        riskAnalysisAnswersSQL,
+        descriptorsSQL,
+        attributesSQL,
+        interfacesSQL,
+        documentsSQL,
+        rejectionReasonsSQL,
+        templateVersionRefsSQL,
+      }).data;
+
+      const aggregatedDescriptor = aggregatedEservice.descriptors[0];
+
+      expect(aggregatedDescriptor.attributes.certified).toHaveLength(
+        descriptorCertifiedAttributeGroups.length
+      );
+      aggregatedDescriptor.attributes.certified.forEach((group, index) => {
+        expect(group).toHaveLength(
+          descriptorCertifiedAttributeGroups[index].length
+        );
+      });
+
+      expect(aggregatedDescriptor.attributes.verified).toHaveLength(
+        descriptorVerifiedAttributeGroups.length
+      );
+      aggregatedDescriptor.attributes.verified.forEach((group, index) => {
+        expect(group).toHaveLength(
+          descriptorVerifiedAttributeGroups[index].length
+        );
+      });
+
+      expect(aggregatedDescriptor.attributes.declared).toHaveLength(
+        descriptorDeclaredAttributeGroups.length
+      );
+      aggregatedDescriptor.attributes.declared.forEach((group, index) => {
+        expect(group).toHaveLength(
+          descriptorDeclaredAttributeGroups[index].length
+        );
+      });
+    }
   });
 });
