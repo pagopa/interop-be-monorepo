5851cb82f4a37eb871f54061fc24d7b38b7f87b3
Eric Camellini <eric.camellini@gmail.com>
2025-10-03 18:27:04 +0200

    feature: retrieving E-Service M2M events (PIN-7685) (#2422)

--- packages/m2m-event-manager/test/integrationUtils.ts
+++ packages/m2m-event-manager/test/integrationUtils.ts
@@ -1,7 +1,14 @@
 import { setupTestContainersVitest } from "pagopa-interop-commons-test";
 import { inject, afterEach } from "vitest";
-import { AttributeM2MEvent, dateToString } from "pagopa-interop-models";
-import { attributeInM2MEvent } from "pagopa-interop-m2m-event-db-models";
+import {
+  AttributeM2MEvent,
+  EServiceM2MEvent,
+  dateToString,
+} from "pagopa-interop-models";
+import {
+  attributeInM2MEvent,
+  eserviceInM2MEvent,
+} from "pagopa-interop-m2m-event-db-models";
 import { m2mEventServiceBuilder } from "../src/services/m2mEventService.js";
 import { m2mEventReaderServiceSQLBuilder } from "../src/services/m2mEventReaderServiceSQL.js";
 
@@ -29,6 +36,18 @@ export async function writeAttributeM2MEvent(
     {
       ...event,
       eventTimestamp: dateToString(event.eventTimestamp),
+    },
+  ]);
+}
+
+export async function writeEServiceM2MEvent(event: EServiceM2MEvent) {
+  await m2mEventDB.insert(eserviceInM2MEvent).values([
+    {
+      ...event,
+      eventTimestamp: dateToString(event.eventTimestamp),
+      descriptorId: event.descriptorId ?? null,
+      producerDelegateId: event.producerDelegateId ?? null,
+      producerDelegationId: event.producerDelegationId ?? null,
     },
   ]);
 }
