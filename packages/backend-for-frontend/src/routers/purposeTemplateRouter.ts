388fe00bd126d664250d2fdcb4d4f0667c2e18e8
shuyec <76391491+shuyec@users.noreply.github.com>
2025-10-03 11:06:21 +0200

    feature: purpose-template-process `GET /purposeTemplates` (PIN-7471) (#2303)

--- packages/backend-for-frontend/src/routers/purposeTemplateRouter.ts
+++ packages/backend-for-frontend/src/routers/purposeTemplateRouter.ts
@@ -6,10 +6,11 @@ import {
   ZodiosContext,
   zodiosValidationErrorToApiProblem,
 } from "pagopa-interop-commons";
+import { emptyErrorMapper } from "pagopa-interop-models";
 import { makeApiProblem } from "../model/errors.js";
 import { PurposeTemplateService } from "../services/purposeTemplateService.js";
 import { fromBffAppContext } from "../utilities/context.js";
-import { getPurposeTemplateErrorMapper } from "../utilities/errorMappers.js";
+import { getCatalogPurposeTemplatesErrorMapper } from "../utilities/errorMappers.js";
 
 const purposeTemplateRouter = (
   ctx: ZodiosContext,
@@ -19,25 +20,82 @@ const purposeTemplateRouter = (
     validationErrorHandler: zodiosValidationErrorToApiProblem,
   });
 
-  purposeTemplateRouter.post("/purposeTemplates", async (req, res) => {
-    const ctx = fromBffAppContext(req.ctx, req.headers);
+  purposeTemplateRouter
+    .post("/purposeTemplates", async (req, res) => {
+      const ctx = fromBffAppContext(req.ctx, req.headers);
 
-    try {
-      const result = await purposeTemplateService.createPurposeTemplate(
-        req.body,
-        ctx
-      );
-      return res.status(201).send(bffApi.CreatedResource.parse(result));
-    } catch (error) {
-      const errorRes = makeApiProblem(
-        error,
-        getPurposeTemplateErrorMapper,
-        ctx,
-        "Error creating purpose template"
-      );
-      return res.status(errorRes.status).send(errorRes);
-    }
-  });
+      try {
+        const result = await purposeTemplateService.createPurposeTemplate(
+          req.body,
+          ctx
+        );
+        return res.status(201).send(bffApi.CreatedResource.parse(result));
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          emptyErrorMapper,
+          ctx,
+          "Error creating purpose template"
+        );
+        return res.status(errorRes.status).send(errorRes);
+      }
+    })
+    .get("/creators/purposeTemplates", async (req, res) => {
+      const ctx = fromBffAppContext(req.ctx, req.headers);
+
+      try {
+        const response =
+          await purposeTemplateService.getCreatorPurposeTemplates({
+            purposeTitle: req.query.q,
+            states: req.query.states,
+            eserviceIds: req.query.eserviceIds,
+            offset: req.query.offset,
+            limit: req.query.limit,
+            ctx,
+          });
+
+        return res
+          .status(200)
+          .send(bffApi.CreatorPurposeTemplates.parse(response));
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          emptyErrorMapper,
+          ctx,
+          "Error retrieving creator's purpose templates"
+        );
+        return res.status(errorRes.status).send(errorRes);
+      }
+    })
+    .get("/catalog/purposeTemplates", async (req, res) => {
+      const ctx = fromBffAppContext(req.ctx, req.headers);
+
+      try {
+        const response =
+          await purposeTemplateService.getCatalogPurposeTemplates({
+            purposeTitle: req.query.q,
+            targetTenantKind: req.query.targetTenantKind,
+            creatorIds: req.query.creatorIds,
+            eserviceIds: req.query.eserviceIds,
+            excludeExpiredRiskAnalysis: req.query.excludeExpiredRiskAnalysis,
+            offset: req.query.offset,
+            limit: req.query.limit,
+            ctx,
+          });
+
+        return res
+          .status(200)
+          .send(bffApi.CatalogPurposeTemplates.parse(response));
+      } catch (error) {
+        const errorRes = makeApiProblem(
+          error,
+          getCatalogPurposeTemplatesErrorMapper,
+          ctx,
+          "Error retrieving catalog purpose templates"
+        );
+        return res.status(errorRes.status).send(errorRes);
+      }
+    });
 
   return purposeTemplateRouter;
 };
