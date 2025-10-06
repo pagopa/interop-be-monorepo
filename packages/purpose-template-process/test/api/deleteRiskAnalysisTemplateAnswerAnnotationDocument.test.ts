68a1c409ce03428b0b2373fdf825fc0969722285
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-10-06 12:50:15 +0200

    fix: update purposeTemplateRiskAnalysisFormNotFound mapping to 500

--- packages/purpose-template-process/test/api/deleteRiskAnalysisTemplateAnswerAnnotationDocument.test.ts
+++ packages/purpose-template-process/test/api/deleteRiskAnalysisTemplateAnswerAnnotationDocument.test.ts
@@ -99,7 +99,7 @@ describe("API /purposeTemplates/{id}/riskAnalysis/answers/{answerId}/annotation/
     },
     {
       error: purposeTemplateRiskAnalysisFormNotFound(purposeTemplateId),
-      expectedStatus: 404,
+      expectedStatus: 500,
     },
     {
       error: riskAnalysisTemplateAnswerAnnotationDocumentNotFound(
