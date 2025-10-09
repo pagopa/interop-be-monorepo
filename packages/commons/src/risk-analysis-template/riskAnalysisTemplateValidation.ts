97c5ec1a8044139eef5270dee13f303e06ecdd50
Alessio Pazzani <37593191+Alepazz@users.noreply.github.com>
2025-10-09 21:34:57 +0200

    feature: create answer for purpose template (PIN-7734) (#2396)
    Co-authored-by: Vittorio Caprio <vittorio.caprio@buildo.io>
    Co-authored-by: Paolo Manca <paolo.manca@pagopa.it>
    Co-authored-by: shuyec <76391491+shuyec@users.noreply.github.com>

--- packages/commons/src/risk-analysis-template/riskAnalysisTemplateValidation.ts
+++ packages/commons/src/risk-analysis-template/riskAnalysisTemplateValidation.ts
@@ -114,6 +114,47 @@ export function validatePurposeTemplateRiskAnalysis(
   });
 }
 
+export function validateRiskAnalysisAnswer(
+  riskAnalysisAnswerKey: string,
+  riskAnalysisAnswerValue: RiskAnalysisTemplateAnswerToValidate,
+  tenantKind: TenantKind
+): RiskAnalysisTemplateValidationResult<RiskAnalysisTemplateValidatedSingleOrMultiAnswer> {
+  const latestVersionFormRules = getLatestVersionFormRules(tenantKind);
+
+  if (latestVersionFormRules === undefined) {
+    return invalidTemplateResult([
+      noRiskAnalysisTemplateRulesVersionFoundError(tenantKind),
+    ]);
+  }
+
+  const validationRules = buildValidationRules(latestVersionFormRules);
+  const validationRule = validationRules.find(
+    (rule) => rule.fieldName === riskAnalysisAnswerKey
+  );
+
+  if (!validationRule) {
+    return invalidTemplateResult([
+      unexpectedRiskAnalysisTemplateFieldError(riskAnalysisAnswerKey),
+    ]);
+  }
+
+  // Validate only the value, NOT the dependencies
+  const valueValidationErrors = validateAnswerValue(
+    riskAnalysisAnswerValue,
+    validationRule
+  );
+
+  if (valueValidationErrors.length > 0) {
+    return invalidTemplateResult(valueValidationErrors);
+  }
+
+  return buildValidResultAnswer(
+    riskAnalysisAnswerKey,
+    riskAnalysisAnswerValue,
+    validationRule
+  );
+}
+
 function validateTemplateFormAnswers(
   answers: Record<string, RiskAnalysisTemplateAnswerToValidate>,
   validationRules: ValidationRule[]
