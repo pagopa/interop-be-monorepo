6a4647e68855e126202bf530ecafe7aa7b8e5a81
MatteoGrilli-quinck <matteo.grilli@quinck.io>
2025-10-09 15:01:03 +0200

    feature: email notification implementation switch 13 (PIN-7450)

---
+++ packages/email-notification-dispatcher/src/handlers/agreements/handleAgreementUnsuspendedByPlatformToConsumer.ts
@@ -0,0 +1,82 @@
+import {
+  EmailNotificationMessagePayload,
+  generateId,
+  missingKafkaMessageDataError,
+  fromAgreementV2,
+  NotificationType,
+} from "pagopa-interop-models";
+import {
+  eventMailTemplateType,
+  retrieveHTMLTemplate,
+  retrieveTenant,
+} from "../../services/utils.js";
+import {
+  AgreementHandlerParams,
+  getRecipientsForTenants,
+  retrieveAgreementEservice,
+} from "../handlerCommons.js";
+
+const notificationType: NotificationType =
+  "agreementSuspendedUnsuspendedToConsumer";
+
+export async function handleAgreementUnsuspendedByPlatformToConsumer(
+  data: AgreementHandlerParams
+): Promise<EmailNotificationMessagePayload[]> {
+  const {
+    agreementV2Msg,
+    readModelService,
+    logger,
+    templateService,
+    userService,
+    correlationId,
+  } = data;
+
+  if (!agreementV2Msg) {
+    throw missingKafkaMessageDataError(
+      "agreement",
+      "AgreementUnsuspendedByPlatform"
+    );
+  }
+
+  const agreement = fromAgreementV2(agreementV2Msg);
+
+  const [htmlTemplate, eservice, consumer] = await Promise.all([
+    retrieveHTMLTemplate(
+      eventMailTemplateType.agreementUnsuspendedByPlatformToConsumerMailTemplate
+    ),
+    retrieveAgreementEservice(agreement, readModelService),
+    retrieveTenant(agreement.producerId, readModelService),
+    retrieveTenant(agreement.consumerId, readModelService),
+  ]);
+
+  const targets = await getRecipientsForTenants({
+    tenants: [consumer],
+    notificationType,
+    readModelService,
+    userService,
+    logger,
+    includeTenantContactEmails: false,
+  });
+
+  if (targets.length === 0) {
+    logger.info(
+      `No targets found for tenant. Agreement ${agreement.id}, no emails to dispatch.`
+    );
+    return [];
+  }
+
+  return targets.map(({ address }) => ({
+    correlationId: correlationId ?? generateId(),
+    email: {
+      subject: `Riattivazione richiesta da parte della Piattaforma`,
+      body: templateService.compileHtml(htmlTemplate, {
+        title: `Riattivazione richiesta da parte della Piattaforma`,
+        notificationType,
+        entityId: agreement.id,
+        consumerName: consumer.name,
+        eserviceName: eservice.name,
+      }),
+    },
+    address,
+  }));
+}
