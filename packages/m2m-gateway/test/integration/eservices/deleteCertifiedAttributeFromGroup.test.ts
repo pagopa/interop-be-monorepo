2e8ad080c1ed819a7e13db02ad655cc2244a3798
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-17 15:23:00 +0200

    feat: added tests for delete attributes from group routes in api v2 (PIN-7516)

---
+++ packages/m2m-gateway/test/integration/eservices/deleteCertifiedAttributeFromGroup.test.ts
@@ -0,0 +1,259 @@
+import { describe, it, vi, beforeEach, expect } from "vitest";
+import {
+  generateId,
+  pollingMaxRetriesExceeded,
+  unsafeBrandId,
+} from "pagopa-interop-models";
+import {
+  getMockWithMetadata,
+  getMockedApiEServiceAttribute,
+  getMockedApiEservice,
+  getMockedApiEserviceDescriptor,
+} from "pagopa-interop-commons-test";
+import {
+  eserviceService,
+  expectApiClientPostToHaveBeenCalledWith,
+  expectApiClientGetToHaveBeenCalledWith,
+  mockInteropBeClients,
+  mockPollingResponse,
+} from "../../integrationUtils.js";
+import { PagoPAInteropBeClients } from "../../../src/clients/clientsProvider.js";
+import { getMockM2MAdminAppContext } from "../../mockUtils.js";
+import {
+  eserviceDescriptorAttributeNotFound,
+  eserviceDescriptorGroupNotFound,
+  eserviceDescriptorNotFound,
+  missingMetadata,
+} from "../../../src/model/errors.js";
+import { config } from "../../../src/config/config.js";
+
+describe("deleteCertifiedAttributeFromGroup", () => {
+  const mockAttribute = getMockedApiEServiceAttribute();
+  const mockCertifiedAttributes = [
+    [getMockedApiEServiceAttribute(), getMockedApiEServiceAttribute()],
+    [getMockedApiEServiceAttribute(), mockAttribute],
+    [mockAttribute],
+    [
+      getMockedApiEServiceAttribute(),
+      getMockedApiEServiceAttribute(),
+      getMockedApiEServiceAttribute(),
+    ],
+  ];
+  const mockDescriptor = getMockedApiEserviceDescriptor({
+    attributes: {
+      certified: mockCertifiedAttributes,
+      declared: [],
+      verified: [],
+    },
+  });
+  const mockEService = getMockedApiEservice({
+    descriptors: [mockDescriptor],
+  });
+
+  const mockGetEServiceResponse = getMockWithMetadata(mockEService);
+
+  const mockGetEService = vi.fn(
+    mockPollingResponse(mockGetEServiceResponse, 2)
+  );
+
+  const mockPatchUpdateDescriptor = vi
+    .fn()
+    .mockResolvedValue(mockGetEServiceResponse);
+
+  mockInteropBeClients.catalogProcessClient = {
+    patchUpdateDraftDescriptor: mockPatchUpdateDescriptor,
+    getEServiceById: mockGetEService,
+  } as unknown as PagoPAInteropBeClients["catalogProcessClient"];
+
+  beforeEach(() => {
+    mockPatchUpdateDescriptor.mockClear();
+    mockGetEService.mockClear();
+  });
+
+  it("Should succeed and perform API clients calls", async () => {
+    mockGetEService.mockResolvedValueOnce(mockGetEServiceResponse);
+
+    const groupIndex = 1;
+
+    await eserviceService.deleteCertifiedAttributeFromGroup(
+      unsafeBrandId(mockEService.id),
+      unsafeBrandId(mockDescriptor.id),
+      groupIndex,
+      unsafeBrandId(mockAttribute.id),
+      getMockM2MAdminAppContext()
+    );
+
+    expectApiClientPostToHaveBeenCalledWith({
+      mockPost:
+        mockInteropBeClients.catalogProcessClient.patchUpdateDraftDescriptor,
+      params: {
+        eServiceId: mockEService.id,
+        descriptorId: mockDescriptor.id,
+      },
+      body: {
+        attributes: {
+          certified: mockCertifiedAttributes.map((group, index) => {
+            if (index === groupIndex) {
+              return group.filter((attr) => attr.id !== mockAttribute.id);
+            }
+            return group;
+          }),
+          declared: [],
+          verified: [],
+        },
+      },
+    });
+    expectApiClientGetToHaveBeenCalledWith({
+      mockGet: mockInteropBeClients.catalogProcessClient.getEServiceById,
+      params: { eServiceId: mockEService.id },
+    });
+    expect(
+      mockInteropBeClients.catalogProcessClient.getEServiceById
+    ).toHaveBeenCalledTimes(3);
+  });
+
+  it("Should delete the whole group if the last attribute is removed", async () => {
+    mockGetEService.mockResolvedValueOnce(mockGetEServiceResponse);
+
+    const groupIndex = 2;
+
+    await eserviceService.deleteCertifiedAttributeFromGroup(
+      unsafeBrandId(mockEService.id),
+      unsafeBrandId(mockDescriptor.id),
+      groupIndex,
+      unsafeBrandId(mockAttribute.id),
+      getMockM2MAdminAppContext()
+    );
+
+    expectApiClientPostToHaveBeenCalledWith({
+      mockPost:
+        mockInteropBeClients.catalogProcessClient.patchUpdateDraftDescriptor,
+      params: {
+        eServiceId: mockEService.id,
+        descriptorId: mockDescriptor.id,
+      },
+      body: {
+        attributes: {
+          certified: mockCertifiedAttributes.filter(
+            (_, index) => index !== groupIndex
+          ),
+          declared: [],
+          verified: [],
+        },
+      },
+    });
+  });
+
+  it("Should throw missingMetadata in case the eservice returned by the update PATCH call has no metadata", async () => {
+    mockPatchUpdateDescriptor.mockResolvedValueOnce({
+      ...mockGetEServiceResponse,
+      metadata: undefined,
+    });
+
+    await expect(
+      eserviceService.deleteCertifiedAttributeFromGroup(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(mockDescriptor.id),
+        1,
+        unsafeBrandId(mockAttribute.id),
+        getMockM2MAdminAppContext()
+      )
+    ).rejects.toThrowError(missingMetadata());
+  });
+
+  it("Should throw missingMetadata in case the eservice returned by the polling GET call has no metadata", async () => {
+    mockGetEService.mockResolvedValueOnce(mockGetEServiceResponse);
+    mockGetEService.mockResolvedValueOnce({
+      ...mockGetEServiceResponse,
+      metadata: undefined,
+    });
+
+    await expect(
+      eserviceService.deleteCertifiedAttributeFromGroup(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(mockDescriptor.id),
+        1,
+        unsafeBrandId(mockAttribute.id),
+        getMockM2MAdminAppContext()
+      )
+    ).rejects.toThrowError(missingMetadata());
+  });
+
+  it("Should throw pollingMaxRetriesExceeded in case of polling max attempts", async () => {
+    mockGetEService.mockResolvedValueOnce(mockGetEServiceResponse);
+    mockGetEService.mockImplementation(
+      mockPollingResponse(
+        mockGetEServiceResponse,
+        config.defaultPollingMaxRetries + 1
+      )
+    );
+
+    await expect(
+      eserviceService.deleteCertifiedAttributeFromGroup(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(mockDescriptor.id),
+        1,
+        unsafeBrandId(mockAttribute.id),
+        getMockM2MAdminAppContext()
+      )
+    ).rejects.toThrowError(
+      pollingMaxRetriesExceeded(
+        config.defaultPollingMaxRetries,
+        config.defaultPollingRetryDelay
+      )
+    );
+    expect(mockGetEService).toHaveBeenCalledTimes(
+      config.defaultPollingMaxRetries + 1
+    );
+  });
+
+  it("Should throw eserviceDescriptorGroupNotFound in case of missing group for the specified group index", async () => {
+    await expect(
+      eserviceService.deleteCertifiedAttributeFromGroup(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(mockDescriptor.id),
+        mockCertifiedAttributes.length + 1,
+        unsafeBrandId(mockAttribute.id),
+        getMockM2MAdminAppContext()
+      )
+    ).rejects.toThrowError(
+      eserviceDescriptorGroupNotFound(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(mockDescriptor.id),
+        mockCertifiedAttributes.length + 1
+      )
+    );
+  });
+
+  it("Should throw eserviceDescriptorAttributeNotFound in case of attribute not found", async () => {
+    await expect(
+      eserviceService.deleteCertifiedAttributeFromGroup(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(mockDescriptor.id),
+        1,
+        unsafeBrandId(generateId()),
+        getMockM2MAdminAppContext()
+      )
+    ).rejects.toThrowError(
+      eserviceDescriptorAttributeNotFound(unsafeBrandId(mockDescriptor.id))
+    );
+  });
+
+  it("Should throw eserviceDescriptorNotFound in case of eservice descriptor not found", async () => {
+    const descriptorId = generateId();
+    await expect(
+      eserviceService.deleteCertifiedAttributeFromGroup(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(descriptorId),
+        1,
+        unsafeBrandId(mockAttribute.id),
+        getMockM2MAdminAppContext()
+      )
+    ).rejects.toThrowError(
+      eserviceDescriptorNotFound(
+        unsafeBrandId(mockEService.id),
+        unsafeBrandId(descriptorId)
+      )
+    );
+  });
+});
