d2e95316d52e90c591d586b132dafd73150983a4
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-10-22 17:06:52 +0200

    PIN-7343 Add purpose template in kpi-domains-readmodel-checker (#2490)
    Co-authored-by: Eduardo Mihalache <eduardo.mihalache@digitouch.it>
    Co-authored-by: lorenzo-pa <lorenzo.giorgi@pagopa.it>
    Co-authored-by: Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>

--- packages/kpi-domains-readmodel-checker/src/services/readModelServiceKPI.ts
+++ packages/kpi-domains-readmodel-checker/src/services/readModelServiceKPI.ts
@@ -7,6 +7,7 @@ import {
   EServiceTemplate,
   ProducerKeychain,
   Purpose,
+  PurposeTemplate,
   Tenant,
   WithMetadata,
 } from "pagopa-interop-models";
@@ -20,6 +21,7 @@ import {
   aggregateProducerKeychainArray,
   aggregateDelegationsArray,
   aggregateEServiceTemplateArray,
+  aggregatePurposeTemplateArray,
 } from "pagopa-interop-readmodel";
 import { z } from "zod";
 import { IConnected, IMain } from "pg-promise";
@@ -38,6 +40,7 @@ import { DelegationDbTable } from "../model/db/delegation.js";
 import { EserviceTemplateDbTable } from "../model/db/eserviceTemplate.js";
 import { PurposeDbTable } from "../model/db/purpose.js";
 import { DomainDbTable, DomainDbTableSchemas } from "../model/db/index.js";
+import { PurposeTemplateDbTable } from "../model/db/purposeTemplate.js";
 
 export type DBConnection = IConnected<unknown, IClient>;
 export type DBContext = {
@@ -347,6 +350,48 @@ export function readModelServiceBuilderKPI(dbContext: DBContext) {
         delegationsSQL,
         contractDocumentsSQL,
         stampsSQL,
+      });
+    },
+
+    async getAllPurposeTemplates(): Promise<
+      Array<WithMetadata<PurposeTemplate>>
+    > {
+      const purposeTemplatesSQL = await getManyFromDb(
+        dbContext,
+        PurposeTemplateDbTable.purpose_template
+      );
+      const riskAnalysisFormTemplatesSQL = await getManyFromDb(
+        dbContext,
+        PurposeTemplateDbTable.purpose_template_risk_analysis_form
+      );
+      const riskAnalysisTemplateAnswersSQL = await getManyFromDb(
+        dbContext,
+        PurposeTemplateDbTable.purpose_template_risk_analysis_answer
+      );
+      const riskAnalysisTemplateAnswersAnnotationsSQL = await getManyFromDb(
+        dbContext,
+        PurposeTemplateDbTable.purpose_template_risk_analysis_answer_annotation
+      );
+      const riskAnalysisTemplateAnswersAnnotationsDocumentsSQL =
+        await getManyFromDb(
+          dbContext,
+          PurposeTemplateDbTable.purpose_template_risk_analysis_answer_annotation_document
+        );
+
+      return aggregatePurposeTemplateArray({
+        purposeTemplatesSQL,
+        riskAnalysisFormTemplatesSQL,
+        riskAnalysisTemplateAnswersSQL: riskAnalysisTemplateAnswersSQL.map(
+          (ra) => ({
+            ...ra,
+            value: JSON.parse(ra.value),
+            suggestedValues: ra.suggestedValues
+              ? JSON.parse(ra.suggestedValues)
+              : null,
+          })
+        ),
+        riskAnalysisTemplateAnswersAnnotationsSQL,
+        riskAnalysisTemplateAnswersAnnotationsDocumentsSQL,
       });
     },
   };
