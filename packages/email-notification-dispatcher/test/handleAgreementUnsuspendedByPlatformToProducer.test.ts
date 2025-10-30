189a4bdab7cb9f9cd992187ed3b217fba4014c97
Tommaso Petrucciani <tommaso.petrucciani@icloud.com>
2025-10-30 14:12:33 +0100

    Update newly merged handlers

--- packages/email-notification-dispatcher/test/handleAgreementUnsuspendedByPlatformToProducer.test.ts
+++ packages/email-notification-dispatcher/test/handleAgreementUnsuspendedByPlatformToProducer.test.ts
@@ -7,6 +7,7 @@ import {
   getMockEService,
   getMockTenant,
 } from "pagopa-interop-commons-test";
+import { authRole } from "pagopa-interop-commons";
 import {
   Agreement,
   CorrelationId,
@@ -63,7 +64,12 @@ describe("handleAgreementUnsuspendedByPlatformToProducerToProducer", async () =>
     readModelService.getTenantUsersWithNotificationEnabled = vi
       .fn()
       .mockReturnValueOnce(
-        users.map((user) => ({ userId: user.id, tenantId: user.tenantId }))
+        users.map((user) => ({
+          userId: user.id,
+          tenantId: user.tenantId,
+          // Only consider ADMIN_ROLE since role restrictions are tested separately in getRecipientsForTenants.test.ts
+          userRoles: [authRole.ADMIN_ROLE],
+        }))
       );
   });
 
@@ -201,7 +207,12 @@ describe("handleAgreementUnsuspendedByPlatformToProducerToProducer", async () =>
     readModelService.getTenantUsersWithNotificationEnabled = vi
       .fn()
       .mockResolvedValue([
-        { userId: users[0].id, tenantId: users[0].tenantId },
+        {
+          userId: users[0].id,
+          tenantId: users[0].tenantId,
+          // Only consider ADMIN_ROLE since role restrictions are tested separately in getRecipientsForTenants.test.ts
+          userRoles: [authRole.ADMIN_ROLE],
+        },
       ]);
 
     const agreement: Agreement = {
--- packages/email-notification-dispatcher/test/handleEserviceTemplateNameUpdated.test.ts
+++ packages/email-notification-dispatcher/test/handleEserviceTemplateNameUpdated.test.ts
@@ -7,6 +7,7 @@ import {
   getMockEServiceTemplate,
   getMockTenant,
 } from "pagopa-interop-commons-test";
+import { authRole } from "pagopa-interop-commons";
 import {
   CorrelationId,
   EService,
@@ -82,7 +83,12 @@ describe("handleEServiceTemplateNameUpdated", async () => {
           .filter((user) =>
             tenantIds.includes(unsafeBrandId<TenantId>(user.tenantId))
           )
-          .map((user) => ({ userId: user.id, tenantId: user.tenantId }))
+          .map((user) => ({
+            userId: user.id,
+            tenantId: user.tenantId,
+            // Only consider ADMIN_ROLE since role restrictions are tested separately in getRecipientsForTenants.test.ts
+            userRoles: [authRole.ADMIN_ROLE],
+          }))
       );
   });
 
@@ -133,7 +139,12 @@ describe("handleEServiceTemplateNameUpdated", async () => {
     readModelService.getTenantUsersWithNotificationEnabled = vi
       .fn()
       .mockResolvedValue([
-        { userId: users[0].id, tenantId: users[0].tenantId },
+        {
+          userId: users[0].id,
+          tenantId: users[0].tenantId,
+          // Only consider ADMIN_ROLE since role restrictions are tested separately in getRecipientsForTenants.test.ts
+          userRoles: [authRole.ADMIN_ROLE],
+        },
       ]);
 
     const messages = await handleEServiceTemplateNameUpdated({
