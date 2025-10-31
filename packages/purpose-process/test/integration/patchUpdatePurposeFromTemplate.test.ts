4ef2011877952dea9a0151b7212550ce2b85bc3d
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-10-31 15:50:50 +0100

    fix: fix patchUpdatePurposeFromTemplate test

--- packages/purpose-process/test/integration/patchUpdatePurposeFromTemplate.test.ts
+++ packages/purpose-process/test/integration/patchUpdatePurposeFromTemplate.test.ts
@@ -454,9 +454,9 @@ describe("patchUpdatePurposeFromTemplate", () => {
     ).rejects.toThrowError(purposeDraftVersionNotFound(draftPurpose.id));
   });
 
-  it("Should throw purposeNotInDraftState if have one purpose version not in draft state", async () => {
+  it("Should throw purposeDraftVersionNotFound if there's a purpose version not in draft state", async () => {
     const nonDraftState: PurposeVersionState = getRandomNonPurposeStateWithout(
-      purposeVersionState.active
+      purposeVersionState.draft
     );
 
     await addOnePurpose({
