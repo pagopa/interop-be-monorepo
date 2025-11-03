dc3a0a0c462159192f5782fa9ba721f60ff623bd
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-11-03 13:10:31 +0100

    Revert "fix: add mock date to uploadEServiceTemplateVersionDocument.test.ts to prevent invalid lastModified in expected uploaded file"
    This reverts commit 65ed4e153c41acc0f5331f02bf205adedd78d4df.

--- packages/m2m-gateway/test/api/eServiceTemplates/uploadEServiceTemplateVersionDocument.test.ts
+++ packages/m2m-gateway/test/api/eServiceTemplates/uploadEServiceTemplateVersionDocument.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { generateToken } from "pagopa-interop-commons-test";
 import { generateId, pollingMaxRetriesExceeded } from "pagopa-interop-models";
 import { AuthRole, authRole } from "pagopa-interop-commons";
@@ -15,15 +15,6 @@ import {
 import { config } from "../../../src/config/config.js";
 
 describe("POST /eserviceTemplates/:templateId/versions/:versionId/documents router test", () => {
-  const mockDate = new Date();
-  beforeEach(() => {
-    vi.useFakeTimers();
-    vi.setSystemTime(mockDate);
-  });
-  afterEach(() => {
-    vi.useRealTimers();
-  });
-
   const mockFileUpload = {
     fileContent: Buffer.from("test content"),
     filename: "test_document.pdf",
@@ -80,7 +71,7 @@ describe("POST /eserviceTemplates/:templateId/versions/:versionId/documents rout
         templateId,
         versionId,
         {
-          file: fileFromTestMultipartFileUpload(mockFileUpload, mockDate),
+          file: fileFromTestMultipartFileUpload(mockFileUpload),
           prettyName: mockFileUpload.prettyName,
         },
         expect.any(Object) // Context object
