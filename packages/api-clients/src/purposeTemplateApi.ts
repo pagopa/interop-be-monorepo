9b7979c5bbc64946e3e9cb4921df92fb0246e9ed
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-11-05 11:53:27 +0100

    feature: add GET "/purposeTemplates/:id/riskAnalysis/answers/:answerId/annotation/documents" to purpose template api

--- packages/api-clients/src/purposeTemplateApi.ts
+++ packages/api-clients/src/purposeTemplateApi.ts
@@ -8,4 +8,10 @@ export type GetPurposeTemplatesQueryParams = QueryParametersByAlias<
   "getPurposeTemplates"
 >;
 
+export type GetRiskAnalysisTemplateAnswerAnnotationDocumentsQueryParams =
+  QueryParametersByAlias<
+    Api,
+    "getRiskAnalysisTemplateAnswerAnnotationDocuments"
+  >;
+
 export * from "./generated/purposeTemplateApi.js";
