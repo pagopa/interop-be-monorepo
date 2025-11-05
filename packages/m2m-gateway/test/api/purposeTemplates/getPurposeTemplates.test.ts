9d26a2dba8c43d4b2989f6be19d3f6a5fdf18375
Stefano Hu <76391491+shuyec@users.noreply.github.com>
2025-11-05 18:17:21 +0100

    fix: fix getPurposeTemplates m2m tests

--- packages/m2m-gateway/test/api/purposeTemplates/getPurposeTemplates.test.ts
+++ packages/m2m-gateway/test/api/purposeTemplates/getPurposeTemplates.test.ts
@@ -137,7 +137,7 @@ describe("API GET /purposeTemplates", () => {
       },
     },
   ])(
-    "Should return 500 when API model parsing fails for response",
+    "Should return 500 when API model parsing fails for response: %s",
     async (resp) => {
       mockPurposeTemplateService.getPurposeTemplates = vi
         .fn()
