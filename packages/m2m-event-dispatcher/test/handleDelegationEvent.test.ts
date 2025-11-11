42410f0051aff8af6bd2ad8742aafd96cf70da87
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-11-11 16:24:13 +0100

    fix: tests

--- packages/m2m-event-dispatcher/test/handleDelegationEvent.test.ts
+++ packages/m2m-event-dispatcher/test/handleDelegationEvent.test.ts
@@ -6,6 +6,7 @@ import {
   toDelegationV2,
   DelegationEventEnvelopeV2,
   delegationKind,
+  DelegationKind,
 } from "pagopa-interop-models";
 import { genericLogger } from "pagopa-interop-commons";
 import { match, P } from "ts-pattern";
@@ -27,16 +28,144 @@ describe("handleDelegationEvent test", async () => {
     vi.clearAllMocks();
   });
 
-  it.each(DelegationEventV2.options.map((o) => o.shape.type.value))(
-    "should write %s M2M event with the right visibility (kind: delegatedConsumer)",
-    async (eventType: DelegationEvent["type"]) => {
+  describe.each(Object.values(delegationKind))("with kind %s", (kind) => {
+    it.each(DelegationEventV2.options.map((o) => o.shape.type.value))(
+      "should write %s M2M event with the right visibility",
+      async (eventType: DelegationEvent["type"]) => {
+        const delegation = getMockDelegation({
+          kind,
+        });
+        const message = {
+          ...getMockEventEnvelopeCommons(),
+          stream_id: delegation.id,
+          type: eventType,
+          data: {
+            delegation: toDelegationV2(delegation),
+          },
+        } as DelegationEventEnvelopeV2;
+
+        const eventTimestamp = new Date();
+
+        const expectedM2MEvent = await match(kind)
+          .with(delegationKind.delegatedProducer, () =>
+            match(eventType)
+              .with(
+                P.union(
+                  "ProducerDelegationSubmitted",
+                  "ProducerDelegationApproved",
+                  "ProducerDelegationRejected",
+                  "ProducerDelegationRevoked",
+                  "DelegationSignedContractGenerated"
+                ),
+                async () => ({
+                  id: expect.any(String),
+                  eventType,
+                  eventTimestamp,
+                  resourceVersion: message.version,
+                  delegationId: delegation.id,
+                })
+              )
+              .with(
+                P.union(
+                  "DelegationContractGenerated",
+                  "ConsumerDelegationSubmitted",
+                  "ConsumerDelegationApproved",
+                  "ConsumerDelegationRejected",
+                  "ConsumerDelegationRevoked"
+                ),
+                () => undefined
+              )
+              .exhaustive()
+          )
+          .with(delegationKind.delegatedConsumer, () =>
+            match(eventType)
+              .with(
+                P.union(
+                  "ConsumerDelegationSubmitted",
+                  "ConsumerDelegationApproved",
+                  "ConsumerDelegationRejected",
+                  "ConsumerDelegationRevoked",
+                  "DelegationSignedContractGenerated"
+                ),
+                async () => ({
+                  id: expect.any(String),
+                  eventType,
+                  eventTimestamp,
+                  resourceVersion: message.version,
+                  delegationId: delegation.id,
+                })
+              )
+              .with(
+                P.union(
+                  "DelegationContractGenerated",
+                  "ProducerDelegationSubmitted",
+                  "ProducerDelegationApproved",
+                  "ProducerDelegationRejected",
+                  "ProducerDelegationRevoked"
+                ),
+                () => undefined
+              )
+              .exhaustive()
+          )
+          .exhaustive();
+
+        await handleDelegationEvent(
+          message,
+          eventTimestamp,
+          genericLogger,
+          testM2mEventWriterService
+        );
+
+        await match(kind)
+          .with(delegationKind.delegatedConsumer, async () => {
+            if (!expectedM2MEvent) {
+              expect(
+                testM2mEventWriterService.insertConsumerDelegationM2MEvent
+              ).not.toHaveBeenCalled();
+            } else {
+              expect(
+                testM2mEventWriterService.insertConsumerDelegationM2MEvent
+              ).toHaveBeenCalledTimes(1);
+              const actualM2MEvent =
+                await retrieveLastConsumerDelegationM2MEvent();
+              expect(actualM2MEvent).toEqual(expectedM2MEvent);
+            }
+          })
+          .with(delegationKind.delegatedProducer, async () => {
+            if (!expectedM2MEvent) {
+              expect(
+                testM2mEventWriterService.insertProducerDelegationM2MEvent
+              ).not.toHaveBeenCalled();
+            } else {
+              expect(
+                testM2mEventWriterService.insertProducerDelegationM2MEvent
+              ).toHaveBeenCalledTimes(1);
+              const actualM2MEvent =
+                await retrieveLastProducerDelegationM2MEvent();
+              expect(actualM2MEvent).toEqual(expectedM2MEvent);
+            }
+          })
+          .exhaustive();
+      }
+    );
+
+    it("should not write the event if the same resource version is already present", async () => {
       const delegation = getMockDelegation({
-        kind: delegationKind.delegatedConsumer,
+        kind,
       });
       const message = {
         ...getMockEventEnvelopeCommons(),
         stream_id: delegation.id,
-        type: eventType,
+        type: match(kind)
+          .with(
+            delegationKind.delegatedConsumer,
+            () => "ConsumerDelegationSubmitted"
+          )
+          .with(
+            delegationKind.delegatedProducer,
+            () => "ProducerDelegationSubmitted"
+          )
+          .exhaustive(),
         data: {
           delegation: toDelegationV2(delegation),
         },
@@ -44,30 +173,7 @@ describe("handleDelegationEvent test", async () => {
 
       const eventTimestamp = new Date();
 
-      const expectedM2MEvent = await match(eventType)
-        .with(
-          P.union(
-            "ConsumerDelegationSubmitted",
-            "ConsumerDelegationApproved",
-            "ConsumerDelegationRejected",
-            "ConsumerDelegationRevoked",
-            "DelegationSignedContractGenerated",
-            "ProducerDelegationSubmitted",
-            "ProducerDelegationApproved",
-            "ProducerDelegationRejected",
-            "ProducerDelegationRevoked"
-          ),
-          async () => ({
-            id: expect.any(String),
-            eventType,
-            eventTimestamp,
-            resourceVersion: message.version,
-            delegationId: delegation.id,
-          })
-        )
-        .with(P.union("DelegationContractGenerated"), () => undefined)
-        .exhaustive();
-
+      // Insert the event for the first time
       await handleDelegationEvent(
         message,
         eventTimestamp,
@@ -75,61 +181,7 @@ describe("handleDelegationEvent test", async () => {
         testM2mEventWriterService
       );
 
-      if (!expectedM2MEvent) {
-        expect(
-          testM2mEventWriterService.insertConsumerDelegationM2MEvent
-        ).not.toHaveBeenCalled();
-      } else {
-        expect(
-          testM2mEventWriterService.insertConsumerDelegationM2MEvent
-        ).toHaveBeenCalledTimes(1);
-        const actualM2MEvent = await retrieveLastConsumerDelegationM2MEvent();
-        expect(actualM2MEvent).toEqual(expectedM2MEvent);
-      }
-    }
-  );
-
-  it.each(DelegationEventV2.options.map((o) => o.shape.type.value))(
-    "should write %s M2M event with the right visibility (kind: delegatedProducer)",
-    async (eventType: DelegationEvent["type"]) => {
-      const delegation = getMockDelegation({
-        kind: delegationKind.delegatedProducer,
-      });
-      const message = {
-        ...getMockEventEnvelopeCommons(),
-        stream_id: delegation.id,
-        type: eventType,
-        data: {
-          delegation: toDelegationV2(delegation),
-        },
-      } as DelegationEventEnvelopeV2;
-
-      const eventTimestamp = new Date();
-
-      const expectedM2MEvent = await match(eventType)
-        .with(
-          P.union(
-            "ProducerDelegationSubmitted",
-            "ProducerDelegationApproved",
-            "ProducerDelegationRejected",
-            "ProducerDelegationRevoked",
-            "DelegationSignedContractGenerated",
-            "ConsumerDelegationSubmitted",
-            "ConsumerDelegationApproved",
-            "ConsumerDelegationRejected",
-            "ConsumerDelegationRevoked"
-          ),
-          async () => ({
-            id: expect.any(String),
-            eventType,
-            eventTimestamp,
-            resourceVersion: message.version,
-            delegationId: delegation.id,
-          })
-        )
-        .with(P.union("DelegationContractGenerated"), () => undefined)
-        .exhaustive();
-
+      // Try to insert the same event again: should be skipped
       await handleDelegationEvent(
         message,
         eventTimestamp,
@@ -137,109 +189,34 @@ describe("handleDelegationEvent test", async () => {
         testM2mEventWriterService
       );
 
-      if (!expectedM2MEvent) {
-        expect(
-          testM2mEventWriterService.insertProducerDelegationM2MEvent
-        ).not.toHaveBeenCalled();
-      } else {
-        expect(
-          testM2mEventWriterService.insertProducerDelegationM2MEvent
-        ).toHaveBeenCalledTimes(1);
-        const actualM2MEvent = await retrieveLastProducerDelegationM2MEvent();
-        expect(actualM2MEvent).toEqual(expectedM2MEvent);
-      }
-    }
-  );
+      // Try to insert one with a further resource version: should be inserted
+      await handleDelegationEvent(
+        { ...message, version: message.version + 1 },
+        eventTimestamp,
+        genericLogger,
+        testM2mEventWriterService
+      );
 
-  it("should not write the event if the same resource version is already present (consumer)", async () => {
-    const delegation = getMockDelegation({
-      kind: delegationKind.delegatedConsumer,
+      await match(kind)
+        .with(delegationKind.delegatedConsumer, async () => {
+          expect(
+            testM2mEventWriterService.insertConsumerDelegationM2MEvent
+          ).toHaveBeenCalledTimes(3);
+
+          expect(await retrieveAllConsumerDelegationM2MEvents()).toHaveLength(
+            2
+          );
+        })
+        .with(delegationKind.delegatedProducer, async () => {
+          expect(
+            testM2mEventWriterService.insertProducerDelegationM2MEvent
+          ).toHaveBeenCalledTimes(3);
+
+          expect(await retrieveAllProducerDelegationM2MEvents()).toHaveLength(
+            2
+          );
+        })
+        .exhaustive();
     });
-    const message = {
-      ...getMockEventEnvelopeCommons(),
-      stream_id: delegation.id,
-      type: "ConsumerDelegationSubmitted",
-      data: {
-        delegation: toDelegationV2(delegation),
-      },
-    } as DelegationEventEnvelopeV2;
-
-    const eventTimestamp = new Date();
-
-    // Insert the event for the first time
-    await handleDelegationEvent(
-      message,
-      eventTimestamp,
-      genericLogger,
-      testM2mEventWriterService
-    );
-
-    // Try to insert the same event again: should be skipped
-    await handleDelegationEvent(
-      message,
-      eventTimestamp,
-      genericLogger,
-      testM2mEventWriterService
-    );
-
-    // Try to insert one with a further resource version: should be inserted
-    await handleDelegationEvent(
-      { ...message, version: message.version + 1 },
-      eventTimestamp,
-      genericLogger,
-      testM2mEventWriterService
-    );
-
-    expect(
-      testM2mEventWriterService.insertConsumerDelegationM2MEvent
-    ).toHaveBeenCalledTimes(3);
-
-    expect(await retrieveAllConsumerDelegationM2MEvents()).toHaveLength(2);
-  });
-
-  it("should not write the event if the same resource version is already present (producer)", async () => {
-    const delegation = getMockDelegation({
-      kind: delegationKind.delegatedProducer,
-    });
-    const message = {
-      ...getMockEventEnvelopeCommons(),
-      stream_id: delegation.id,
-      type: "ProducerDelegationSubmitted",
-      data: {
-        delegation: toDelegationV2(delegation),
-      },
-    } as DelegationEventEnvelopeV2;
-
-    const eventTimestamp = new Date();
-
-    // Insert the event for the first time
-    await handleDelegationEvent(
-      message,
-      eventTimestamp,
-      genericLogger,
-      testM2mEventWriterService
-    );
-
-    // Try to insert the same event again: should be skipped
-    await handleDelegationEvent(
-      message,
-      eventTimestamp,
-      genericLogger,
-      testM2mEventWriterService
-    );
-
-    // Try to insert one with a further resource version: should be inserted
-    await handleDelegationEvent(
-      { ...message, version: message.version + 1 },
-      eventTimestamp,
-      genericLogger,
-      testM2mEventWriterService
-    );
-
-    expect(
-      testM2mEventWriterService.insertProducerDelegationM2MEvent
-    ).toHaveBeenCalledTimes(3);
-
-    expect(await retrieveAllProducerDelegationM2MEvents()).toHaveLength(2);
   });
 });
