62dd15b1b133d9d1468761ba633e2a771567c261
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2025-11-12 17:17:06 +0100

    linted

--- packages/m2m-event-manager/test/mockUtils.ts
+++ packages/m2m-event-manager/test/mockUtils.ts
@@ -24,10 +24,10 @@ import { v7 as uuidv7 } from "uuid";
 
 export function generateM2MEventId<
   ID extends
-  | AttributeM2MEventId
-  | EServiceM2MEventId
-  | AgreementM2MEventId
-  | PurposeM2MEventId
+    | AttributeM2MEventId
+    | EServiceM2MEventId
+    | AgreementM2MEventId
+    | PurposeM2MEventId
 >(): ID {
   return unsafeBrandId<ID>(uuidv7());
 }
