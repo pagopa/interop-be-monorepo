b9847a74296b850f264d4338c273f5401c2f089f
shuyec <76391491+shuyec@users.noreply.github.com>
2025-11-14 09:48:24 +0100

    feature: m2m-gateway `GET /purposeTemplates/:purposeTemplateId` (PIN-7984) (#2546)
    Co-authored-by: Paolo Manca <paolo.manca@pagopa.it>
    Co-authored-by: Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>

--- packages/m2m-gateway/src/services/purposeTemplateService.ts
+++ packages/m2m-gateway/src/services/purposeTemplateService.ts
@@ -1,7 +1,9 @@
-import { m2mGatewayApi } from "pagopa-interop-api-clients";
+import { m2mGatewayApi, purposeTemplateApi } from "pagopa-interop-api-clients";
 import { WithLogger } from "pagopa-interop-commons";
+import { PurposeTemplateId } from "pagopa-interop-models";
 import { PagoPAInteropBeClients } from "../clients/clientsProvider.js";
 import { M2MGatewayAppContext } from "../utils/context.js";
+import { WithMaybeMetadata } from "../clients/zodiosWithMetadataPatch.js";
 import {
   toGetPurposeTemplatesApiQueryParams,
   toM2MGatewayApiPurposeTemplate,
@@ -13,6 +15,17 @@ export type PurposeTemplateService = ReturnType<
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function purposeTemplateServiceBuilder(clients: PagoPAInteropBeClients) {
+  const retrievePurposeTemplateById = async (
+    purposeTemplateId: PurposeTemplateId,
+    headers: M2MGatewayAppContext["headers"]
+  ): Promise<WithMaybeMetadata<purposeTemplateApi.PurposeTemplate>> =>
+    await clients.purposeTemplateProcessClient.getPurposeTemplate({
+      params: {
+        id: purposeTemplateId,
+      },
+      headers,
+    });
+
   return {
     async getPurposeTemplates(
       queryParams: m2mGatewayApi.GetPurposeTemplatesQueryParams,
@@ -48,6 +61,19 @@ export function purposeTemplateServiceBuilder(clients: PagoPAInteropBeClients) {
           totalCount,
         },
       };
+    },
+    async getPurposeTemplate(
+      purposeTemplateId: PurposeTemplateId,
+      { logger, headers }: WithLogger<M2MGatewayAppContext>
+    ): Promise<m2mGatewayApi.PurposeTemplate> {
+      logger.info(`Retrieving purpose template with id ${purposeTemplateId}`);
+
+      const { data } = await retrievePurposeTemplateById(
+        purposeTemplateId,
+        headers
+      );
+
+      return toM2MGatewayApiPurposeTemplate(data);
     },
   };
 }
