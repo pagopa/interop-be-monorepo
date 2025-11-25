fae6894e9c511cca74c8ff87962fe20a52fe9439
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-11-25 15:08:22 +0100

    fix: merge problem (PIN-8228)

--- packages/m2m-gateway/src/routers/eventRouter.ts
+++ packages/m2m-gateway/src/routers/eventRouter.ts
@@ -74,7 +74,7 @@ const eventRouter = (
     }
   });
 
-  eventRouter.get("/producerDelegationEvents", async (req, res) => {
+  eventRouter.get("/keyEvents", async (req, res) => {
     const ctx = fromM2MGatewayAppContext(req.ctx, req.headers);
     try {
       validateAuthorization(ctx, [M2M_ROLE, M2M_ADMIN_ROLE]);
