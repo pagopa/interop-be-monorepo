f62f80d819b88395ad4ff1c74083b8a92c254555
crasherbit <70882451+crasherbit@users.noreply.github.com>
2025-11-25 11:02:23 +0100

    Pin 8151 m 2 m gw api eventi m 2 m attributes (#2680)
    Co-authored-by: Crasherbit <sagripantimatteo@gmail.com>
    Co-authored-by: Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
    Co-authored-by: Carmine Porricelli <carmine.porricelli@pagopa.it>

--- packages/api-clients/src/m2mGatewayApi.ts
+++ packages/api-clients/src/m2mGatewayApi.ts
@@ -10,6 +10,7 @@ type DelegationApi = typeof m2mGatewayApi.delegationsApi.api;
 type EServiceTemplateApi = typeof m2mGatewayApi.eserviceTemplatesApi.api;
 type ClientApi = typeof m2mGatewayApi.clientsApi.api;
 type ProducerKeychainApi = typeof m2mGatewayApi.producerKeychainsApi.api;
+type EventManagerApi = typeof m2mGatewayApi.eventsApi.api;
 type PurposeTemplateApi = typeof m2mGatewayApi.purposeTemplatesApi.api;
 
 export type GetAgreementsQueryParams = QueryParametersByAlias<
@@ -178,6 +179,10 @@ export type GetEServiceTemplateVersionVerifiedAttributesQueryParams =
     "getEServiceTemplateVersionVerifiedAttributes"
   >;
 
+export type GetEventManagerAttributesQueryParams = QueryParametersByAlias<
+  EventManagerApi,
+  "getAttributesEvents"
+>;
 export type GetPurposeTemplatesQueryParams = QueryParametersByAlias<
   PurposeTemplateApi,
   "getPurposeTemplates"
