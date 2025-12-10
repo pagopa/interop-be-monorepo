2dfcff3029e549c620086aab02d91401435b86ab
Sandro Tajè <sandro.taje@gmail.com>
2025-12-10 15:27:17 +0100

    refactor: remove unused index from digest_email_sent table
    - Removed idx_digest_email_sent_sent_at index on sent_at column as it's not needed for current queries

--- docker/digest-tracking-db/digest-tracking.sql
+++ docker/digest-tracking-db/digest-tracking.sql
@@ -6,6 +6,3 @@ CREATE TABLE IF NOT EXISTS digest_tracking.digest_email_sent (
     sent_at TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY (user_id, tenant_id)
 );
-
-CREATE INDEX IF NOT EXISTS idx_digest_email_sent_sent_at 
-ON digest_tracking.digest_email_sent (sent_at);
