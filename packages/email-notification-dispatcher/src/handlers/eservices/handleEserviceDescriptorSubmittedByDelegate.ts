0426ef653e2ee530337847a7642d43668c5c06e1
Sandro Tajè <sandro.taje@gmail.com>
2025-12-15 09:48:49 +0100

    fix: add missing bffUrl to template context (PIN-8754) (#2786)

--- packages/email-notification-dispatcher/src/handlers/eservices/handleEserviceDescriptorSubmittedByDelegate.ts
+++ packages/email-notification-dispatcher/src/handlers/eservices/handleEserviceDescriptorSubmittedByDelegate.ts
@@ -16,6 +16,7 @@ import {
   getRecipientsForTenants,
   mapRecipientToEmailPayload,
 } from "../handlerCommons.js";
+import { config } from "../../config/config.js";
 
 const notificationType: NotificationType =
   "eserviceNewVersionSubmittedToDelegator";
@@ -79,6 +80,7 @@ export async function handleEserviceDescriptorSubmittedByDelegate(
         delegateName: delegate.name,
         eserviceName: eservice.name,
         ctaLabel: "Valuta la richiesta",
+        bffUrl: config.bffUrl,
       }),
     },
     tenantId: t.tenantId,
