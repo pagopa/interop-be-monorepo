3522a208c7bbb8adfe2b355e3e38f021be964c15
Davide Giunzioni <giunzioni.dp@gmail.com>
2025-12-18 17:24:00 +0100

    hotfix: integration test in catalog-process (PIN-7793)

--- packages/catalog-process/test/api/addEserviceTemplateinstanceInterface.test.ts
+++ packages/catalog-process/test/api/addEserviceTemplateinstanceInterface.test.ts
@@ -17,13 +17,10 @@ import {
   EServiceId,
   generateId,
   interfaceExtractingInfoError,
-  interfaceExtractingSoapFiledError,
   invalidContentTypeDetected,
+  invalidInterfaceData,
   invalidInterfaceFileDetected,
-  invalidServerUrl,
-  openapiVersionNotRecognized,
   operationForbidden,
-  parsingSoapFileError,
   technology,
   Technology,
   TenantId,
@@ -213,20 +210,8 @@ describe("addEServiceTemplateInstanceInterface", () => {
           expectedStatus: 400,
         },
         {
-          error: interfaceExtractingSoapFiledError("field-name"),
-          expectedStatus: 400,
-        },
-        {
-          error: parsingSoapFileError(),
-          expectedStatus: 400,
-        },
-        {
-          error: openapiVersionNotRecognized("invalid-version"),
-          expectedStatus: 400,
-        },
-        {
-          error: invalidServerUrl({
-            id: "invalid",
+          error: invalidInterfaceData({
+            id: eservice.id,
             isEserviceTemplate: true,
           }),
           expectedStatus: 400,
