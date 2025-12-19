dee8ec0439ec917b0196b5d7d67388da035f4f1e
diego-lon <diego.longo@digitouch.it>
2025-12-19 11:56:35 +0100

    Added more verbosity logging on processing files upload on ss

--- packages/audit-signer/src/handlers/sqsMessageHandler.ts
+++ packages/audit-signer/src/handlers/sqsMessageHandler.ts
@@ -29,6 +29,7 @@ async function processMessage(
   correlationId: CorrelationId
 ): Promise<void> {
   try {
+    logger.info(`Processing and signing file ${s3Key}`);
     const file: Uint8Array = await fileManager.get(
       config.s3Bucket,
       s3Key,
@@ -50,6 +51,7 @@ async function processMessage(
       safeStorageRequest,
       logger
     );
+    logger.info(`Created file on safe storage with key: ${key}`);
 
     await safeStorageService.uploadFileContent(
       uploadUrl,
