3ac8b49c2f050b4b66aab9e9fb87de258ec70d18
Alepazz <alessio.pazzani@gmail.com>
2026-01-13 12:23:21 +0100

    feature: refactor types in signed-objects-persister and remove ts-node dependency from package.json (PIN-8938)

--- packages/signed-objects-persister/src/clients/clientProvider.ts
+++ packages/signed-objects-persister/src/clients/clientProvider.ts
@@ -5,19 +5,19 @@ import {
 } from "pagopa-interop-api-clients";
 import { config } from "../config/config.js";
 
-export type AgreementProcessClient = ReturnType<
+type AgreementProcessClient = ReturnType<
   typeof agreementApi.createAgreementApiClient
 >;
 
-export type PurposeProcessClient = ReturnType<
+type PurposeProcessClient = ReturnType<
   typeof purposeApi.createPurposeApiClient
 >;
 
-export type DelegationProcessClient = {
+type DelegationProcessClient = {
   delegation: ReturnType<typeof delegationApi.createDelegationApiClient>;
 };
 
-export type PagoPAInteropBeClients = {
+type PagoPAInteropBeClients = {
   agreementProcessClient: AgreementProcessClient;
   purposeProcessClient: PurposeProcessClient;
   delegationProcessClient: DelegationProcessClient;
