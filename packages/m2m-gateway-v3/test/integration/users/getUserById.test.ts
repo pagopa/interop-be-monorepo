cfc0aa3960ab9729daece65f8b521fbaa29ef2b6
FredPirani <federico.pirani@bidcompany.it>
2026-01-21 12:29:53 +0100

    feature: Added structure for integration test (PIN-8996)

---
+++ packages/m2m-gateway-v3/test/integration/users/getUserById.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, beforeEach } from "vitest";
+// import { m2mGatewayApiV3 } from "pagopa-interop-api-clients";
+// import { unsafeBrandId } from "pagopa-interop-models";
+// import {
+//   getMockedApiTenant,
+//   getMockWithMetadata,
+// } from "pagopa-interop-commons-test";
+// import {
+//   userService,
+// } from "../../integrationUtils.js";
+// import { PagoPAInteropBeClients } from "../../../src/clients/clientsProvider.js";
+// import { getMockM2MAdminAppContext } from "../../mockUtils.js";
+
+describe("getTenant", () => {
+  beforeEach(() => {
+    // Clear mock counters and call information before each test
+  });
+
+  it("Should succeed and perform API clients calls", async () => {
+    // TODO
+  });
+});
