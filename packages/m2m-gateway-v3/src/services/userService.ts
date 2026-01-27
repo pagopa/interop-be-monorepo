8b674e49cbba938f2109b959c798691b4adc80ea
Javier Jara <jarai.antonio@gmail.com>
2026-01-27 10:43:46 +0100

    feature: Add assertTenantSelfcareIdExists (PIN-8995)

--- packages/m2m-gateway-v3/src/services/userService.ts
+++ packages/m2m-gateway-v3/src/services/userService.ts
@@ -3,7 +3,7 @@ import { m2mGatewayApiV3 } from "pagopa-interop-api-clients";
 import { unauthorizedError } from "pagopa-interop-models";
 import { toM2MGatewayApiUser } from "../api/usersApiConverter.js";
 import { PagoPAInteropBeClients } from "../clients/clientsProvider.js";
-import { M2MGatewayAppContext } from "../utils/context.js";
+import { Headers, M2MGatewayAppContext } from "../utils/context.js";
 
 export type GetUsersQueryParams = {
   roles: string[];
@@ -12,6 +12,25 @@ export type GetUsersQueryParams = {
 };
 
 export type UserService = ReturnType<typeof userServiceBuilder>;
+
+async function assertTenantSelfcareIdExists(
+  clients: PagoPAInteropBeClients,
+  organizationId: string,
+  headers: Headers
+): Promise<string> {
+  const { data: tenant } = await clients.tenantProcessClient.tenant.getTenant({
+    params: { id: organizationId },
+    headers,
+  });
+
+  if (!tenant.selfcareId) {
+    throw unauthorizedError(
+      `Tenant ${organizationId} does not have a SelfCare ID`
+    );
+  }
+
+  return tenant.selfcareId;
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function userServiceBuilder(clients: PagoPAInteropBeClients) {
@@ -33,19 +52,11 @@ export function userServiceBuilder(clients: PagoPAInteropBeClients) {
       );
 
       // Get tenant to resolve institutionId (selfcareId)
-      const { data: tenant } =
-        await clients.tenantProcessClient.tenant.getTenant({
-          params: { id: authData.organizationId },
-          headers,
-        });
-
-      if (!tenant.selfcareId) {
-        throw unauthorizedError(
-          `Tenant ${authData.organizationId} does not have a SelfCare ID`
-        );
-      }
-
-      const selfcareId = tenant.selfcareId;
+      const selfcareId = await assertTenantSelfcareIdExists(
+        clients,
+        authData.organizationId,
+        headers
+      );
 
       // Fetch users from SelfCare (API already returns only active users)
       const { data: users } =
