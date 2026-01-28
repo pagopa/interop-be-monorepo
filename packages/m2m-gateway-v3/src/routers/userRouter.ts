a15c3a8858af9caf45ccb7228b6a41eb0d53fef8
FredPirani <federico.pirani@bidcompany.it>
2026-01-28 09:25:26 +0100

    fix: Post Merge (PIN-8996)

--- packages/m2m-gateway-v3/src/routers/userRouter.ts
+++ packages/m2m-gateway-v3/src/routers/userRouter.ts
@@ -8,11 +8,14 @@ import {
   validateAuthorization,
   authRole,
 } from "pagopa-interop-commons";
-import { UserId, emptyErrorMapper, unsafeBrandId } from "pagopa-interop-models";
+import { UserId, unsafeBrandId } from "pagopa-interop-models";
 import { makeApiProblem } from "../model/errors.js";
 import { UserService } from "../services/userService.js";
 import { fromM2MGatewayAppContext } from "../utils/context.js";
-import { getUserErrorMapper, getSelfcareErrorMapper } from "../utils/errorMappers.js";
+import {
+  getUserErrorMapper,
+  getSelfcareErrorMapper,
+} from "../utils/errorMappers.js";
 
 const userRouter = (
   ctx: ZodiosContext,
