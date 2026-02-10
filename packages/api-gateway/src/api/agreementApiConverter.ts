57177988b7ed79593b063a8874b895b04d0a21be
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-02-10 10:05:37 +0100

    Fix api-gateway

--- packages/api-gateway/src/api/agreementApiConverter.ts
+++ packages/api-gateway/src/api/agreementApiConverter.ts
@@ -35,6 +35,7 @@ export function toAgreementProcessGetAgreementsQueryParams(
   return {
     producersIds: producerId ? [producerId] : [],
     consumersIds: consumerId ? [consumerId] : [],
+    excludeDelegations: false,
     eservicesIds: eserviceId ? [eserviceId] : [],
     descriptorsIds: descriptorId ? [descriptorId] : [],
     showOnlyUpgradeable: false,
