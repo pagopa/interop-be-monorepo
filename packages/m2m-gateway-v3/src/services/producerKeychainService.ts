fa489b2ba8cc90c31a6877a2d12c7d4c665ff4a6
Roberto Gregnanin <128478646+rGregnanin@users.noreply.github.com>
2026-02-10 12:45:05 +0100

    Fix/Fix lint (PIN-9142)

--- packages/m2m-gateway-v3/src/services/producerKeychainService.ts
+++ packages/m2m-gateway-v3/src/services/producerKeychainService.ts
@@ -313,13 +313,6 @@ export function producerKeychainServiceBuilder(
 
       assertTenantHasSelfcareId(tenant);
 
-      console.log(
-        "Getting producer keychain users for producerKeychainId",
-        producerKeychainId,
-        "with tenant selfcareId",
-        tenant.selfcareId
-      );
-
       const producerKeychainUsers =
         await clients.authorizationClient.producerKeychain.getProducerKeychainUsers(
           {
@@ -327,14 +320,6 @@ export function producerKeychainServiceBuilder(
             headers: ctx.headers,
           }
         );
-      console.log(
-        "Retrieved producer keychain users for producerKeychainId",
-        producerKeychainId,
-        "with tenant selfcareId",
-        tenant.selfcareId,
-        "users",
-        producerKeychainUsers.data
-      );
 
       const users = await Promise.all(
         producerKeychainUsers.data.map(async (id) =>
@@ -346,27 +331,10 @@ export function producerKeychainServiceBuilder(
           )
         )
       );
-      console.log(
-        "Retrieved selfcare users for producerKeychainId",
-        producerKeychainId,
-        "with tenant selfcareId",
-        tenant.selfcareId,
-        "users",
-        users
-      );
 
       const results: m2mGatewayApiV3.User[] = users.slice(
         offset,
         offset + limit
-      );
-
-      console.log(
-        "Returning paginated users for producerKeychainId",
-        producerKeychainId,
-        "with tenant selfcareId",
-        tenant.selfcareId,
-        "results",
-        results
       );
 
       return {
