68937e7d242b45589cc8fd28cefdf91ad33af525
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-02-11 10:14:22 +0100

    Draft tests

--- packages/agreement-process/test/integration/getAgreements.test.ts
+++ packages/agreement-process/test/integration/getAgreements.test.ts
@@ -22,7 +22,7 @@ import {
   delegationState,
   TenantId,
 } from "pagopa-interop-models";
-import { describe, beforeEach, it, expect } from "vitest";
+import { describe, beforeEach, it, expect, beforeAll } from "vitest";
 import {
   addOneTenant,
   addOneEService,
@@ -654,7 +654,7 @@ describe("get agreements", () => {
     expectSinglePageListResult(agreements, [agreement1, agreement2]);
   });
 
-  it("should get agreements for a delegated eservice with filters: consumerId and requester is consumer delegate", async () => {
+  it.only("should get agreements for a delegated eservice with filters: consumerId and requester is consumer delegate", async () => {
     const agreements = await agreementService.getAgreements(
       {
         consumerId: tenant1.id,
@@ -665,4 +665,70 @@ describe("get agreements", () => {
     );
     expectSinglePageListResult(agreements, [agreement5]);
   });
+
+  // describe("strictConsumer", () => {
+  //   const tenantA = getMockTenant();
+  //   const tenantB = getMockTenant();
+
+  //   const eserviceA: EService = {
+  //     ...getMockEService(),
+  //     descriptors: [getMockDescriptorPublished()],
+  //   };
+
+  //   const agreement11: Agreement = {
+  //     ...getMockAgreement(),
+  //     eserviceId: eserviceA.id,
+  //     descriptorId: eserviceA.descriptors[0].id,
+  //     producerId: eserviceA.producerId,
+  //     consumerId: tenantA.id,
+  //   };
+
+  //   const agreement12: Agreement = {
+  //     ...getMockAgreement(),
+  //     eserviceId: eserviceA.id,
+  //     descriptorId: eserviceA.descriptors[0].id,
+  //     producerId: eserviceA.producerId,
+  //     consumerId: tenantB.id,
+  //   };
+
+  //   const consumerDelegation = getMockDelegation({
+  //     kind: delegationKind.delegatedConsumer,
+  //     delegateId: tenantA.id,
+  //     delegatorId: tenantB.id,
+  //     eserviceId: eserviceA.id,
+  //     state: delegationState.active,
+  //   });
+
+  //   beforeAll(async () => {
+  //     await addOneTenant(tenantA);
+  //     await addOneTenant(tenantB);
+  //     await addOneEService(eserviceA);
+  //     await addOneAgreement(agreement11);
+  //     await addOneAgreement(agreement12);
+  //     await addOneDelegation(consumerDelegation);
+  //   });
+
+  //   it.only("should return agreements for tenantA with strictConsumer true", async () => {
+  //     const agreements = await agreementService.getAgreements(
+  //       {
+  //         consumerId: [tenantA.id],
+  //         strictConsumer: true,
+  //       },
+  //       10,
+  //       0,
+  //       getMockContext({ authData: getMockAuthData(tenantA.id) })
+  //     );
+  //     expectSinglePageListResult(agreements, [agreement11]);
+  //   });
+
+  //   it("should return agreements for tenantA with strictConsumer false", async () => {
+  //     const agreements = await agreementService.getAgreements(
+  //       { consumerId: [tenantA.id], strictConsumer: false },
+  //       10,
+  //       0,
+  //       getMockContext({ authData: getMockAuthData(tenantA.id) })
+  //     );
+  //     expectSinglePageListResult(agreements, [agreement11, agreement12]);
+  //   });
+  // });
 });
