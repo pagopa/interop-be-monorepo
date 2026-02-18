8e341e8016775e89ba68e9221684c10ff6c48237
diego-lon <diego.longo@digitouch.it>
2026-02-18 09:55:28 +0100

    Handle any non-PA tenant kinds on usePAFallback

--- packages/documents-generator/src/service/purpose/purposeContractBuilder.ts
+++ packages/documents-generator/src/service/purpose/purposeContractBuilder.ts
@@ -92,10 +92,9 @@ export const riskAnalysisDocumentBuilder = (
       const documentCreatedAt = messageTimestamp;
       const riskAnalysisVersion = purpose.riskAnalysisForm.version;
 
-      // Handle GSP or PRIVATE that were previously PA and have access to PA risk analysis versions (3.0, 3.1)
+      // Handle any non-PA tenant kinds that were previously PA and have access to PA risk analysis versions (3.0, 3.1)
       const usePAFallback =
-        (tenantKind === TenantKind.Enum.GSP ||
-          tenantKind === TenantKind.Enum.PRIVATE) &&
+        tenantKind !== TenantKind.Enum.PA &&
         ["3.0", "3.1"].includes(riskAnalysisVersion);
 
       const riskAnalysisFormConfig =
