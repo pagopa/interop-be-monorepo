51cb5772fb647dd587a3caf80a832018ec32d2af
Crasherbit <sagripantimatteo@gmail.com>
2026-02-24 12:12:51 +0100

    changed sleep to delay in redisRateLimiter.integration.test.ts

--- packages/commons-test/test/redisRateLimiter.integration.test.ts
+++ packages/commons-test/test/redisRateLimiter.integration.test.ts
@@ -35,7 +35,7 @@ describe("Redis rate limiter tests", () => {
   it("should rate limit requests by organizationId", async () => {
     const organizationId: TenantId = generateId();
     // wait 500 ms to ensure the redis is connected
-    await sleep(500);
+    await delay(500);
 
     expect(await redisRateLimiter.getCountByOrganization(organizationId)).toBe(
       0
