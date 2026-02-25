b8684dfcf1ab042bd4dd777aa69d387eb16ea47b
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-02-25 10:07:47 +0100

    Update comment

--- packages/commons/src/utils/instanceLabel.ts
+++ packages/commons/src/utils/instanceLabel.ts
@@ -1,8 +1,8 @@
 /**
  * Builds the instance name from the template name and optional instance label.
- * - instanceLabel: maxLength 12
  * - templateName: maxLength 45
  * - separator " - ": 3 characters
+ * - instanceLabel: maxLength 12
  * - eservice name (result): maxLength 60
  */
 export const buildInstanceName = (
