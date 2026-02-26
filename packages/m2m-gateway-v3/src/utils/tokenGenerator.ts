ffde516025e84a4989c5f00e7dc9fd74ddf1fded
FredPirani <federico.pirani@bidcompany.it>
2026-02-26 07:59:28 +0100

    test: Added test to check signature and digest (PIN-9333)

--- packages/m2m-gateway-v3/src/utils/tokenGenerator.ts
+++ packages/m2m-gateway-v3/src/utils/tokenGenerator.ts
@@ -1,6 +1,19 @@
 import { InteropTokenGenerator } from "pagopa-interop-commons";
+import { KMSClient } from "@aws-sdk/client-kms";
 import { config } from "../config/config.js";
 
+const kmsClient = new KMSClient();
+
+/**
+ * Note: This function is left to its own file to enable mocking
+ * or to override the KmsClient to use a local KmsClient like so:
+ *
+ * ```
+ * const kmsClient = new KMSClient({
+ *   endpoint: "http://localhost:4566",
+ * });
+ * ```
+ */
 export function getIntoropTokenGenerator(): InteropTokenGenerator {
-  return new InteropTokenGenerator(config);
+  return new InteropTokenGenerator(config, kmsClient);
 }
