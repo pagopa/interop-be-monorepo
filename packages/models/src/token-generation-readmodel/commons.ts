d697a64e5777271a881df2fc24fed0f10559daea
DenisLantero <denis.lantero@gmail.com>
2026-03-03 17:32:41 +0100

    feat(models): add interactions read model (PIN-9313)

--- packages/models/src/token-generation-readmodel/commons.ts
+++ packages/models/src/token-generation-readmodel/commons.ts
@@ -3,6 +3,9 @@ import {
   ClientId,
   DescriptorId,
   EServiceId,
+  GSIPKPurposeIdEServiceId,
+  InteractionId,
+  InteractionsPK,
   GSIPKClientIdKid,
   GSIPKClientIdPurposeId,
   GSIPKConsumerIdEServiceId,
@@ -97,6 +100,20 @@ export const makeTokenGenerationStatesClientKidPK = ({
   unsafeBrandId<TokenGenerationStatesClientKidPK>(
     `CLIENTKID#${clientId}#${kid}`
   );
+
+export const makeInteractionPK = (
+  interactionId: InteractionId
+): InteractionsPK =>
+  unsafeBrandId<InteractionsPK>(`INTERACTION#${interactionId}`);
+
+export const makeGSIPKPurposeIdEServiceId = ({
+  purposeId,
+  eServiceId,
+}: {
+  purposeId: PurposeId;
+  eServiceId: EServiceId;
+}): GSIPKPurposeIdEServiceId =>
+  unsafeBrandId<GSIPKPurposeIdEServiceId>(`${purposeId}#${eServiceId}`);
 
 export const makeGSIPKEServiceIdDescriptorId = ({
   eserviceId,
