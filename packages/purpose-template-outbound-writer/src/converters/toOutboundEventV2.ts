a6df1f9742a30603017f40a385575f9cf4e7daf2
Sandro Tajè <sandro.taje@gmail.com>
2026-03-03 15:24:45 +0100

    revert: keep async exchange descriptor fields ignored in outbound models (PIN-9237)

--- packages/purpose-template-outbound-writer/src/converters/toOutboundEventV2.ts
+++ packages/purpose-template-outbound-writer/src/converters/toOutboundEventV2.ts
@@ -67,6 +67,12 @@ function toOutboundDescriptorV2(
     templateVersionRef:
       descriptor.templateVersionRef &&
       toOutboundEServiceTemplateVersionRefV2(descriptor.templateVersionRef),
+    asyncExchangeResponseTime: undefined,
+    asyncExchangeResourceAvailableTime: undefined,
+    asyncExchangeConfirmation: undefined,
+    asyncExchangeBulk: undefined,
+    asyncExchangeMaxResultSet: undefined,
+    asyncExchangeOnlyMultiEntityMode: undefined,
   };
 }
 
