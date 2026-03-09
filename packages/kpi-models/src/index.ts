055c4d80333dada82162a7ea929c53c918fdb74e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-03-09 13:16:23 +0100

    Draft new package

---
+++ packages/kpi-models/src/index.ts
@@ -0,0 +1,11 @@
+// Agreement models
+export * from "./agreement/agreement.js";
+export * from "./agreement/agreementAttribute.js";
+export * from "./agreement/agreementConsumerDocument.js";
+export * from "./agreement/agreementContract.js";
+export * from "./agreement/agreementSignedContract.js";
+export * from "./agreement/agreementStamp.js";
+export * from "./agreement/agreementItems.js";
+
+// DB models
+export * from "./db/agreement.js";
