16942212ed07ebff59496f6278ee833ab5dd3856
Sandro Tajè <sandro.taje@gmail.com>
2026-03-12 09:21:14 +0100

    refactor(authorization-server): extract shared router helpers (PIN-9421)
    Extract buildCtxHelpers and handleTokenError into routerUtils to remove
    duplication between the sync and async token endpoint routers.

--- packages/authorization-server/src/routers/AsyncAuthorizationServerRouter.ts
+++ packages/authorization-server/src/routers/AsyncAuthorizationServerRouter.ts
@@ -1,24 +1,13 @@
-import { constants } from "http2";
 import {
-  AuthServerAppContext,
   ExpressContext,
-  logger,
-  WithLogger,
   ZodiosContext,
   zodiosValidationErrorToApiProblem,
 } from "pagopa-interop-commons";
-import {
-  ClientId,
-  ClientKindTokenGenStates,
-  Problem,
-  TenantId,
-} from "pagopa-interop-models";
 import { authorizationServerApi } from "pagopa-interop-api-clients";
 import { ZodiosEndpointDefinitions } from "@zodios/core";
 import { ZodiosRouter } from "@zodios/express";
-import { makeApiProblem } from "../model/domain/errors.js";
-import { authorizationServerErrorMapper } from "../utilities/errorMappers.js";
 import { AsyncTokenService } from "../services/asyncTokenService.js";
+import { buildCtxHelpers, handleTokenError } from "../utilities/routerUtils.js";
 
 const asyncAuthorizationServerRouter = (
   ctx: ZodiosContext,
@@ -29,27 +18,8 @@ const asyncAuthorizationServerRouter = (
   });
 
   router.post("/token.oauth2.async", async (req, res) => {
-    const setCtxClientId = (clientId: ClientId): void => {
-      // eslint-disable-next-line functional/immutable-data
-      req.ctx.clientId = clientId;
-    };
-
-    const setCtxOrganizationId = (organizationId: TenantId): void => {
-      // eslint-disable-next-line functional/immutable-data
-      req.ctx.organizationId = organizationId;
-    };
-
-    const setCtxClientKind = (
-      tokenGenClientKind: ClientKindTokenGenStates
-    ): void => {
-      // eslint-disable-next-line functional/immutable-data
-      req.ctx.clientKind = tokenGenClientKind;
-    };
-
-    const getCtx = (): WithLogger<AuthServerAppContext> => ({
-      ...req.ctx,
-      logger: logger({ ...req.ctx }),
-    });
+    const { getCtx, setCtxClientId, setCtxClientKind, setCtxOrganizationId } =
+      buildCtxHelpers(req.ctx);
 
     try {
       await asyncTokenService.generateAsyncToken(
@@ -65,44 +35,8 @@ const asyncAuthorizationServerRouter = (
       // Each scope handler will return the appropriate response.
       return res.status(501).send();
     } catch (err) {
-      const errorRes = makeApiProblem(
-        err,
-        authorizationServerErrorMapper,
-        getCtx()
-      );
-      if (errorRes.status === constants.HTTP_STATUS_BAD_REQUEST) {
-        const cleanedError: Problem = {
-          title: "The request contains bad syntax or cannot be fulfilled.",
-          type: "about:blank",
-          status: constants.HTTP_STATUS_BAD_REQUEST,
-          detail: "Bad request",
-          errors: [
-            {
-              code: "015-0008",
-              detail: "Unable to generate a token for the given request",
-            },
-          ],
-          correlationId: req.ctx.correlationId,
-        };
-
-        return res.status(cleanedError.status).send(cleanedError);
-      } else {
-        const cleanedError: Problem = {
-          title: "The request couldn't be fulfilled due to an internal error",
-          type: "internalServerError",
-          status: constants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
-          detail: "Internal server error",
-          errors: [
-            {
-              code: "015-0000",
-              detail:
-                "Unable to generate a token for the given request due to an internal error",
-            },
-          ],
-          correlationId: req.ctx.correlationId,
-        };
-        return res.status(cleanedError.status).send(cleanedError);
-      }
+      const { status, body } = handleTokenError(err, getCtx());
+      return res.status(status).send(body);
     }
   });
 
