7efdefa8e890fdbe9a0a1cd700ffd88fafce7b8d
Eduardo Mihalache <eduardo.mihalache@digitouch.it>
2026-03-12 19:12:43 +0100

    chore: Add schema creation for tenant_kind_history

--- docker/tenant-kind-history-db/tenant-kind-history.sql
+++ docker/tenant-kind-history-db/tenant-kind-history.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS tenant_kind_history;
+
 CREATE TABLE IF NOT EXISTS tenant_kind_history.tenant_kind_history (
   tenant_id UUID NOT NULL,
   metadata_version INTEGER NOT NULL,
