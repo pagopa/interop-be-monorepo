72c63104faa9e5c9d9d290cc2194f0ff4c9eafce
Denis Lantero <83024157+DenisLantero@users.noreply.github.com>
2026-03-16 15:29:09 +0100

    feat(interactions): add interactions DynamoDB table and utils (PIN-9313) (#3103)

---
+++ packages/models/src/token-generation-readmodel/interactions-entry.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import {
+  DescriptorId,
+  EServiceId,
+  InteractionId,
+  InteractionsPK,
+  PurposeId,
+} from "../brandedIds.js";
+
+export const interactionState = {
+  startInteraction: "start_interaction",
+  callbackInvocation: "callback_invocation",
+  getResource: "get_resource",
+  confirmation: "confirmation",
+} as const;
+
+export const InteractionState = z.enum([
+  Object.values(interactionState)[0],
+  ...Object.values(interactionState).slice(1),
+]);
+export type InteractionState = z.infer<typeof InteractionState>;
+
+export const Interaction = z.object({
+  PK: InteractionsPK,
+  interactionId: InteractionId,
+  purposeId: PurposeId,
+  eServiceId: EServiceId,
+  descriptorId: DescriptorId,
+  state: InteractionState,
+  startInteractionTokenIssuedAt: z.string().datetime().optional(),
+  callbackInvocationTokenIssuedAt: z.string().datetime().optional(),
+  updatedAt: z.string().datetime(),
+});
+export type Interaction = z.infer<typeof Interaction>;
