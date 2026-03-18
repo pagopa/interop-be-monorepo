728ed8bd314692014bf0eea120d2e0ea50b4baf8
Alessandro Vinci <alessandro.vinci@digitouch.it>
2026-03-18 09:49:57 +0100

    refactor(tenant-kind-history-consumer): simplify consumer, tests

--- packages/tenant-kind-history-consumer/src/tenantKindHistoryConsumerService.ts
+++ packages/tenant-kind-history-consumer/src/tenantKindHistoryConsumerService.ts
@@ -1,6 +1,5 @@
 import { Logger } from "pagopa-interop-commons";
 import {
-  Tenant,
   TenantEventEnvelopeV1,
   TenantEventEnvelopeV2,
   fromTenantV1,
@@ -10,110 +9,96 @@ import {
 import { match, P } from "ts-pattern";
 import { TenantKindHistoryWriterService } from "./tenantKindHistoryWriterService.js";
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function tenantKindhistoryConsumerServiceBuilder(
-  tenantKindHistoryWriterService: TenantKindHistoryWriterService
-) {
-  const createTenantKindHistoryEntry = async (
-    tenant: Tenant,
-    metadataVersion: number,
-    messageTimestamp: Date
-  ): Promise<void> => {
-    await tenantKindHistoryWriterService.createTenantKindHistory(
-      tenant.id,
-      metadataVersion,
-      tenant.kind,
-      messageTimestamp
-    );
-  };
+export async function handleMessageV1(
+  message: TenantEventEnvelopeV1,
+  tenantKindHistoryWriterService: TenantKindHistoryWriterService,
+  logger: Logger
+): Promise<void> {
+  await match(message)
+    .with(
+      { type: P.union("TenantCreated", "TenantUpdated") },
+      async (message) => {
+        if (!message.data.tenant) {
+          throw missingKafkaMessageDataError("tenant", "TenantOnboarded");
+        }
+        const tenant = fromTenantV1(message.data.tenant);
+        await tenantKindHistoryWriterService.createTenantKindHistoryEntry(
+          tenant.id,
+          message.version,
+          tenant.kind,
+          message.log_date
+        );
+      }
+    )
+    .with(
+      {
+        type: P.union(
+          "TenantDeleted",
+          "SelfcareMappingCreated",
+          "SelfcareMappingDeleted",
+          "TenantMailAdded",
+          "TenantMailDeleted"
+        ),
+      },
+      async (message) => {
+        logger.info(`Ignoring ${message.type} message`);
+      }
+    )
+    .exhaustive();
+}
 
-  return {
-    async handleMessageV1(
-      message: TenantEventEnvelopeV1,
-      logger: Logger
-    ): Promise<void> {
-      await match(message)
-        .with(
-          { type: P.union("TenantCreated", "TenantUpdated") },
-          async (message) => {
-            if (!message.data.tenant) {
-              throw missingKafkaMessageDataError("tenant", "TenantOnboarded");
-            }
-            await createTenantKindHistoryEntry(
-              fromTenantV1(message.data.tenant),
-              message.version,
-              message.log_date
-            );
-          }
-        )
-        .with(
-          {
-            type: P.union(
-              "TenantDeleted",
-              "SelfcareMappingCreated",
-              "SelfcareMappingDeleted",
-              "TenantMailAdded",
-              "TenantMailDeleted"
-            ),
-          },
-          async (message) => {
-            logger.info(`Ignoring ${message.type} message`);
-          }
-        )
-        .exhaustive();
-    },
-
-    async handleMessageV2(
-      message: TenantEventEnvelopeV2,
-      logger: Logger
-    ): Promise<void> {
-      await match(message)
-        .with(
-          {
-            type: P.union(
-              "TenantOnboarded",
-              "TenantOnboardDetailsUpdated",
-              "MaintenanceTenantUpdated",
-              "TenantKindUpdated"
-            ),
-          },
-          async (message) => {
-            if (!message.data.tenant) {
-              throw missingKafkaMessageDataError("tenant", message.type);
-            }
-            await createTenantKindHistoryEntry(
-              fromTenantV2(message.data.tenant),
-              message.version,
-              message.log_date
-            );
-          }
-        )
-        .with(
-          {
-            type: P.union(
-              "MaintenanceTenantDeleted",
-              "TenantCertifiedAttributeAssigned",
-              "TenantCertifiedAttributeRevoked",
-              "TenantDeclaredAttributeAssigned",
-              "TenantDeclaredAttributeRevoked",
-              "TenantVerifiedAttributeAssigned",
-              "TenantVerifiedAttributeRevoked",
-              "TenantVerifiedAttributeExpirationUpdated",
-              "TenantVerifiedAttributeExtensionUpdated",
-              "TenantMailAdded",
-              "MaintenanceTenantPromotedToCertifier",
-              "TenantMailDeleted",
-              "TenantDelegatedProducerFeatureAdded",
-              "TenantDelegatedProducerFeatureRemoved",
-              "TenantDelegatedConsumerFeatureAdded",
-              "TenantDelegatedConsumerFeatureRemoved"
-            ),
-          },
-          async (message) => {
-            logger.info(`Ignoring ${message.type} message`);
-          }
-        )
-        .exhaustive();
-    },
-  };
+export async function handleMessageV2(
+  message: TenantEventEnvelopeV2,
+  tenantKindHistoryWriterService: TenantKindHistoryWriterService,
+  logger: Logger
+): Promise<void> {
+  await match(message)
+    .with(
+      {
+        type: P.union(
+          "TenantOnboarded",
+          "TenantOnboardDetailsUpdated",
+          "MaintenanceTenantUpdated",
+          "TenantKindUpdated"
+        ),
+      },
+      async (message) => {
+        if (!message.data.tenant) {
+          throw missingKafkaMessageDataError("tenant", message.type);
+        }
+        const tenant = fromTenantV2(message.data.tenant);
+        await tenantKindHistoryWriterService.createTenantKindHistoryEntry(
+          tenant.id,
+          message.version,
+          tenant.kind,
+          message.log_date
+        );
+      }
+    )
+    .with(
+      {
+        type: P.union(
+          "MaintenanceTenantDeleted",
+          "TenantCertifiedAttributeAssigned",
+          "TenantCertifiedAttributeRevoked",
+          "TenantDeclaredAttributeAssigned",
+          "TenantDeclaredAttributeRevoked",
+          "TenantVerifiedAttributeAssigned",
+          "TenantVerifiedAttributeRevoked",
+          "TenantVerifiedAttributeExpirationUpdated",
+          "TenantVerifiedAttributeExtensionUpdated",
+          "TenantMailAdded",
+          "MaintenanceTenantPromotedToCertifier",
+          "TenantMailDeleted",
+          "TenantDelegatedProducerFeatureAdded",
+          "TenantDelegatedProducerFeatureRemoved",
+          "TenantDelegatedConsumerFeatureAdded",
+          "TenantDelegatedConsumerFeatureRemoved"
+        ),
+      },
+      async (message) => {
+        logger.info(`Ignoring ${message.type} message`);
+      }
+    )
+    .exhaustive();
 }
