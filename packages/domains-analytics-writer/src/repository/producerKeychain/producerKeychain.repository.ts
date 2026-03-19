c2c0089f9f072a91aca75ae2b60133f5e04d994e
Andrea Gorletta <andrea.alice.gorletta@gmail.com>
2026-03-19 15:06:15 +0100

    chore: extract generic repository factory in domains-analytics-writer

--- packages/domains-analytics-writer/src/repository/producerKeychain/producerKeychain.repository.ts
+++ packages/domains-analytics-writer/src/repository/producerKeychain/producerKeychain.repository.ts
@@ -1,116 +1,22 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { genericInternalError } from "pagopa-interop-models";
-import { ITask, IMain } from "pg-promise";
-import { config } from "../../config/config.js";
 import { DBConnection } from "../../db/db.js";
+import { createRepository } from "../createRepository.js";
 import {
-  buildColumnSet,
-  generateMergeDeleteQuery,
-  generateMergeQuery,
-  generateStagingDeleteQuery,
-} from "../../utils/sqlQueryHelper.js";
-import {
-  DeletingDbTable,
   ProducerKeychainDbTable,
+  DeletingDbTable,
 } from "../../model/db/index.js";
 import { ProducerKeychainSchema } from "pagopa-interop-kpi-models";
 import { ProducerKeychainDeletingSchema } from "../../model/authorization/producerKeychain.js";
 
-export function producerKeychainRepository(conn: DBConnection) {
-  const schemaName = config.dbSchemaName;
-  const tableName = ProducerKeychainDbTable.producer_keychain;
-  const stagingTableName = `${tableName}_${config.mergeTableSuffix}`;
-  const deletingTableName = DeletingDbTable.producer_keychain_deleting_table;
-  const stagingDeletingTableName = `${deletingTableName}_${config.mergeTableSuffix}`;
-
-  return {
-    async insert(
-      t: ITask<unknown>,
-      pgp: IMain,
-      records: ProducerKeychainSchema[]
-    ): Promise<void> {
-      try {
-        const cs = buildColumnSet(pgp, tableName, ProducerKeychainSchema);
-        await t.none(pgp.helpers.insert(records, cs));
-        await t.none(generateStagingDeleteQuery(tableName, ["id"]));
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error inserting into staging table ${stagingTableName}: ${error}`
-        );
-      }
+export const producerKeychainRepository = (conn: DBConnection) =>
+  createRepository(conn, {
+    tableName: ProducerKeychainDbTable.producer_keychain,
+    schema: ProducerKeychainSchema,
+    keyColumns: ["id"],
+    deleting: {
+      deletingTableName: DeletingDbTable.producer_keychain_deleting_table,
+      deletingSchema: ProducerKeychainDeletingSchema,
+      useIdAsSourceDeleteKey: false,
+      physicalDelete: false,
     },
-
-    async merge(t: ITask<unknown>): Promise<void> {
-      try {
-        const mergeQuery = generateMergeQuery(
-          ProducerKeychainSchema,
-          schemaName,
-          tableName,
-          ["id"]
-        );
-        await t.none(mergeQuery);
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error merging staging table ${stagingTableName} into ${schemaName}.${tableName}: ${error}`
-        );
-      }
-    },
-
-    async clean(): Promise<void> {
-      try {
-        await conn.none(`TRUNCATE TABLE ${stagingTableName};`);
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error cleaning staging table ${stagingTableName}: ${error}`
-        );
-      }
-    },
-
-    async insertDeleting(
-      t: ITask<unknown>,
-      pgp: IMain,
-      records: ProducerKeychainDeletingSchema[]
-    ): Promise<void> {
-      try {
-        const cs = buildColumnSet(
-          pgp,
-          deletingTableName,
-          ProducerKeychainDeletingSchema
-        );
-        await t.none(pgp.helpers.insert(records, cs));
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error inserting into deleting table ${stagingDeletingTableName}: ${error}`
-        );
-      }
-    },
-
-    async mergeDeleting(t: ITask<unknown>): Promise<void> {
-      try {
-        const mergeQuery = generateMergeDeleteQuery(
-          schemaName,
-          tableName,
-          deletingTableName,
-          ["id"],
-          false,
-          false
-        );
-        await t.none(mergeQuery);
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error merging deletion flag from ${stagingDeletingTableName} into ${schemaName}.${tableName}: ${error}`
-        );
-      }
-    },
-
-    async cleanDeleting(): Promise<void> {
-      try {
-        await conn.none(`TRUNCATE TABLE ${stagingDeletingTableName};`);
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error cleaning deleting staging table ${stagingDeletingTableName}: ${error}`
-        );
-      }
-    },
-  };
-}
+  });
