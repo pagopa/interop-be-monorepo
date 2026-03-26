48fa07bcc37bb79f38d4d350fc501f154521dee5
Fred Pirani BID <85495913+FredPirani@users.noreply.github.com>
2026-03-26 17:09:28 +0100

    feature: create producer keychain (PIN-9499) (#3155)

--- packages/authorization-process/src/routers/AuthorizationRouter.ts
+++ packages/authorization-process/src/routers/AuthorizationRouter.ts
@@ -649,9 +649,9 @@ const authorizationRouter = (
       const ctx = fromAppContext(req.ctx);
 
       try {
-        validateAuthorization(ctx, [ADMIN_ROLE]);
+        validateAuthorization(ctx, [ADMIN_ROLE, M2M_ADMIN_ROLE]);
 
-        const producerKeychain =
+        const { data: producerKeychain, metadata } =
           await authorizationService.createProducerKeychain(
             {
               producerKeychainSeed: req.body,
@@ -659,6 +659,7 @@ const authorizationRouter = (
             ctx
           );
 
+        setMetadataVersionHeader(res, metadata);
         return res
           .status(200)
           .send(
