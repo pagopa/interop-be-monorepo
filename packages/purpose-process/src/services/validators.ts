14f1551325e0411e537ccf4e1ec1fec23c52152f
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-04-02 11:41:19 +0200

    Remove unused import

--- packages/purpose-process/src/services/validators.ts
+++ packages/purpose-process/src/services/validators.ts
@@ -43,7 +43,6 @@ import {
   riskAnalysisAnswerNotInSuggestValues,
   riskAnalysisContainsNotEditableAnswers,
   riskAnalysisMissingExpectedFieldError,
-  riskAnalysisTenantKindMismatch,
   riskAnalysisValidationFailed,
   riskAnalysisVersionMismatch,
   tenantIsNotTheConsumer,
