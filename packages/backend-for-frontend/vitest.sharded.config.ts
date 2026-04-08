014307b4fddab50f345bd71ac7e89382cc68982c
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-04-08 11:39:29 +0200

    Refactor bff sharding

---
+++ packages/backend-for-frontend/vitest.sharded.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+import "dotenv-flow/config";
+
+export default defineConfig({
+  test: {
+    globalSetup: ["./test/vitestGlobalSetup.ts"],
+    setupFiles: "./test/vitest.api.setup.ts",
+    include: ["./test/*.test.ts", "./test/api/**/*.test.ts"],
+    testTimeout: 60000,
+    hookTimeout: 60000,
+    fileParallelism: false,
+    pool: "forks",
+    reporters: ["verbose"],
+  },
+});
