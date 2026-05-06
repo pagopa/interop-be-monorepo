34b4ed3c029f15bde57088b2e167091b78d1a0d9
FredPirani <federico.pirani@bidcompany.it>
2026-05-06 15:36:27 +0200

    fix: added eserviceDescriptorArchivingScheduleInReadmodelCatalog to catalogTables (PIN-9746)

--- packages/catalog-readmodel-writer-sql/src/catalogWriterService.ts
+++ packages/catalog-readmodel-writer-sql/src/catalogWriterService.ts
@@ -44,6 +44,7 @@ export function catalogWriterServiceBuilder(db: DrizzleReturnType) {
       eserviceDescriptorAttributeInReadmodelCatalog,
       eserviceRiskAnalysisInReadmodelCatalog,
       eserviceRiskAnalysisAnswerInReadmodelCatalog,
+      eserviceDescriptorArchivingScheduleInReadmodelCatalog,
     ];
 
     for (const table of catalogTables) {
