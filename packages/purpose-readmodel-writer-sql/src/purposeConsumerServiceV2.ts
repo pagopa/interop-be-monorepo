48472fc14ae5931ac0f9c2ff171fe836383aaf51
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-05-15 18:56:33 +0200

    Handle new event

--- packages/purpose-readmodel-writer-sql/src/purposeConsumerServiceV2.ts
+++ packages/purpose-readmodel-writer-sql/src/purposeConsumerServiceV2.ts
@@ -49,6 +49,7 @@ export async function handleMessageV2(
       { type: "PurposeVersionArchivedByRevokedDelegation" },
       { type: "RiskAnalysisDocumentGenerated" },
       { type: "RiskAnalysisSignedDocumentGenerated" },
+      { type: "PurposeRiskAnalysisAssigned" },
       async (message) => {
         await purposeWriterService.upsertPurpose(purpose, message.version);
       }
