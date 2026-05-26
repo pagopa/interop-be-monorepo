58bb709b37bf3298e57c49bbd42319bfa9acd3e9
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-05-26 10:35:46 +0200

    PIN-10018 Fix api interpolation (#3328)

--- packages/commons/src/eservice-documents/eserviceDocumentUtils.ts
+++ packages/commons/src/eservice-documents/eserviceDocumentUtils.ts
@@ -192,7 +192,7 @@ export const interpolateTemplateRestApiSpec = async (
   /* eslint-enable */
 
   try {
-    await SwaggerParser.validate(jsonApi);
+    await SwaggerParser.validate(structuredClone(jsonApi));
     const updatedInterfaceBuffer = restApiFileToBuffer(
       concreteFileType,
       jsonApi
