677954c1107a840a164124f3a3f1e3f92dcce18c
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-06-01 17:25:08 +0200

    Handle new events

--- packages/notification-email-sender/src/index.ts
+++ packages/notification-email-sender/src/index.ts
@@ -248,7 +248,9 @@ export async function handlePurposeMessage(
           "PurposeVersionArchivedByRevokedDelegation",
           "RiskAnalysisDocumentGenerated",
           "RiskAnalysisSignedDocumentGenerated",
-          "MaintenancePurposeRiskAnalysisSetTenantKind"
+          "MaintenancePurposeRiskAnalysisSetTenantKind",
+          "PurposeRiskAnalysisWorkflowCreated",
+          "PurposeRiskAnalysisAssigned"
         ),
       },
       () => {
