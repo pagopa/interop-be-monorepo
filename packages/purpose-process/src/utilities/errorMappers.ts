0d7be24ee08f5d334b6615b57e8d62a6930cc07b
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-06-05 14:20:30 +0200

    Handle missing error

--- packages/purpose-process/src/utilities/errorMappers.ts
+++ packages/purpose-process/src/utilities/errorMappers.ts
@@ -368,5 +368,6 @@ export const assignRiskAnalysisReviewerErrorMapper = (
     .with("purposeNotFound", () => HTTP_STATUS_NOT_FOUND)
     .with("tenantIsNotTheConsumer", () => HTTP_STATUS_FORBIDDEN)
     .with("reviewerWorkflowConflict", () => HTTP_STATUS_CONFLICT)
+    .with("multipleReviewersNotAllowed", () => HTTP_STATUS_BAD_REQUEST)
     .with("featureFlagNotEnabled", () => HTTP_STATUS_NOT_IMPLEMENTED)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
