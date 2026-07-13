106f53b48642884681e20a2b9d9e00717e52bfae
GorlemZ <andrea.alice.gorletta@gmail.com>
2026-07-13 14:35:26 +0200

    chore: extract generic repository factory in domains-analytics-writer (#3176)
    Co-authored-by: Eduardo <eduardo.mihalache@digitouch.it>

--- packages/domains-analytics-writer/src/repository/tenant/tenantVerifiedAttributeRevoker.repository.ts
+++ packages/domains-analytics-writer/src/repository/tenant/tenantVerifiedAttributeRevoker.repository.ts
@@ -1,72 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { genericInternalError } from "pagopa-interop-models";
-import { IMain, ITask } from "pg-promise";
 import { DBConnection } from "../../db/db.js";
-import {
-  buildColumnSet,
-  generateMergeQuery,
-  generateStagingDeleteQuery,
-} from "../../utils/sqlQueryHelper.js";
-import { config } from "../../config/config.js";
-
-import { TenantVerifiedAttributeRevokerSchema } from "pagopa-interop-kpi-models";
+import { createRepository } from "../createRepository.js";
 import { TenantDbTable } from "../../model/db/index.js";
+import { TenantVerifiedAttributeRevokerSchema } from "pagopa-interop-kpi-models";
 
-export function tenantVerifiedAttributeRevokerRepository(conn: DBConnection) {
-  const schemaName = config.dbSchemaName;
-  const tableName = TenantDbTable.tenant_verified_attribute_revoker;
-  const stagingTableName = `${tableName}_${config.mergeTableSuffix}`;
-
-  return {
-    async insert(
-      t: ITask<unknown>,
-      pgp: IMain,
-      records: TenantVerifiedAttributeRevokerSchema[]
-    ): Promise<void> {
-      try {
-        const cs = buildColumnSet(
-          pgp,
-          tableName,
-          TenantVerifiedAttributeRevokerSchema
-        );
-        await t.none(pgp.helpers.insert(records, cs));
-        await t.none(
-          generateStagingDeleteQuery(tableName, [
-            "tenantId",
-            "tenantVerifiedAttributeId",
-          ])
-        );
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error inserting into staging table ${stagingTableName}: ${error}`
-        );
-      }
-    },
-
-    async merge(t: ITask<unknown>): Promise<void> {
-      try {
-        const mergeQuery = generateMergeQuery(
-          TenantVerifiedAttributeRevokerSchema,
-          schemaName,
-          tableName,
-          ["tenantId", "tenantVerifiedAttributeId"]
-        );
-        await t.none(mergeQuery);
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error merging staging table ${stagingTableName} into ${schemaName}.${tableName}: ${error}`
-        );
-      }
-    },
-
-    async clean(): Promise<void> {
-      try {
-        await conn.none(`TRUNCATE TABLE ${stagingTableName};`);
-      } catch (error: unknown) {
-        throw genericInternalError(
-          `Error cleaning staging table ${stagingTableName}: ${error}`
-        );
-      }
-    },
-  };
-}
+export const tenantVerifiedAttributeRevokerRepository = (conn: DBConnection) =>
+  createRepository(conn, {
+    tableName: TenantDbTable.tenant_verified_attribute_revoker,
+    schema: TenantVerifiedAttributeRevokerSchema,
+    keyColumns: ["tenantId", "tenantVerifiedAttributeId"],
+  });
