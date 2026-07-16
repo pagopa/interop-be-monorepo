3742fd5a5a4a3ece9177050aafb024652f9e18a9
Davide Lombardi <davidelombardi130p@gmail.com>
2026-07-16 16:29:04 +0200

    feat: new split files for Certified Discrete Attribute events

---
+++ packages/email-notification-dispatcher/src/handlers/tenants/handleTenantCertifiedDiscreteAttributeAssigned.ts
@@ -0,0 +1,97 @@
+import {
+  EmailNotificationMessagePayload,
+  generateId,
+  missingKafkaMessageDataError,
+  NotificationType,
+  fromTenantV2,
+} from "pagopa-interop-models";
+import {
+  eventMailTemplateType,
+  retrieveHTMLTemplate,
+  getRecipientsForTenants,
+  mapRecipientToEmailPayload,
+  retrieveAttribute,
+  retrieveTenantByCertifierId,
+} from "pagopa-interop-notification-commons";
+import { TenantHandlerParams } from "../../models/handlerParams.js";
+
+import { certifierDatabaseOriginNames } from "../../config/constants.js";
+import { config } from "../../config/config.js";
+
+const notificationType: NotificationType =
+  "certifiedVerifiedAttributeAssignedRevokedToAssignee";
+
+export async function handleTenantCertifiedDiscreteAttributeAssigned(
+  data: TenantHandlerParams
+): Promise<EmailNotificationMessagePayload[]> {
+  const {
+    tenantV2Msg,
+    attributeId,
+    readModelService,
+    logger,
+    templateService,
+    correlationId,
+  } = data;
+
+  if (!tenantV2Msg) {
+    throw missingKafkaMessageDataError(
+      "tenant",
+      "TenantCertifiedDiscreteAttributeAssigned"
+    );
+  }
+
+  const tenant = fromTenantV2(tenantV2Msg);
+
+  const [htmlTemplate, attribute] = await Promise.all([
+    retrieveHTMLTemplate(
+      eventMailTemplateType.tenantCertifiedAttributeAssignedMailTemplate
+    ),
+    retrieveAttribute(attributeId, readModelService),
+  ]);
+
+  if (!attribute.origin) {
+    logger.error(
+      `Origin of certified discrete attribute ${attribute.id} cannot be undefined.`
+    );
+    return [];
+  }
+
+  const targets = await getRecipientsForTenants({
+    tenants: [tenant],
+    notificationType,
+    readModelService,
+    logger,
+    includeTenantContactEmails: false,
+  });
+
+  if (targets.length === 0) {
+    logger.info(
+      `No users with email notifications enabled for handleTenantCertifiedDiscreteAttributeAssigned - entityId: ${tenant.id}, eventType: ${notificationType}`
+    );
+    return [];
+  }
+
+  const certifierName = certifierDatabaseOriginNames.includes(attribute.origin)
+    ? attribute.origin
+    : (await retrieveTenantByCertifierId(attribute.origin, readModelService))
+        .name;
+
+  return targets.map((t) => ({
+    correlationId: correlationId ?? generateId(),
+    email: {
+      subject: `Hai ricevuto un nuovo attributo certificato`,
+      body: templateService.compileHtml(htmlTemplate, {
+        title: `Hai ricevuto un nuovo attributo certificato`,
+        notificationType,
+        entityId: tenant.id,
+        ...(t.type === "Tenant" ? { recipientName: tenant.name } : {}),
+        certifierName,
+        attributeName: attribute.name,
+        selfcareId: t.selfcareId,
+        bffUrl: config.bffUrl,
+      }),
+    },
+    tenantId: t.tenantId,
+    ...mapRecipientToEmailPayload(t),
+  }));
+}
---
+++ packages/in-app-notification-dispatcher/src/handlers/tenants/handleCertifiedDiscreteAttributeRevokedToAssignee.ts
@@ -0,0 +1,76 @@
+import {
+  AttributeId,
+  fromTenantV2,
+  missingKafkaMessageDataError,
+  NewNotification,
+  TenantV2,
+} from "pagopa-interop-models";
+import { Logger } from "pagopa-interop-commons";
+import {
+  getNotificationRecipients,
+  retrieveAttribute,
+  retrieveTenantByCertifierId,
+  inAppTemplates,
+  attributeOriginUndefined,
+} from "pagopa-interop-notification-commons";
+import { ReadModelServiceSQL } from "../../services/readModelServiceSQL.js";
+
+const IMPORTED_ATTRIBUTE_ORIGINS = ["ISTAT"];
+
+export async function handleCertifiedDiscreteAttributeRevokedToAssignee(
+  tenantV2Msg: TenantV2 | undefined,
+  attributeId: AttributeId,
+  logger: Logger,
+  readModelService: ReadModelServiceSQL
+): Promise<NewNotification[]> {
+  if (!tenantV2Msg) {
+    throw missingKafkaMessageDataError(
+      "tenant",
+      "TenantCertifiedDiscreteAttributeRevoked"
+    );
+  }
+  logger.info(
+    `Sending in-app notification for handleCertifiedDiscreteAttributeRevokedToAssignee - entityId: ${tenantV2Msg.id}, eventType: TenantCertifiedDiscreteAttributeRevoked`
+  );
+
+  const tenant = fromTenantV2(tenantV2Msg);
+
+  const usersWithNotifications = await getNotificationRecipients(
+    [tenant.id],
+    "certifiedVerifiedAttributeAssignedRevokedToAssignee",
+    readModelService,
+    logger
+  );
+
+  if (usersWithNotifications.length === 0) {
+    logger.info(
+      `No users with notifications enabled for handleCertifiedDiscreteAttributeRevokedToAssignee - entityId: ${tenant.id}, eventType: TenantCertifiedDiscreteAttributeRevoked`
+    );
+    return [];
+  }
+
+  const attribute = await retrieveAttribute(attributeId, readModelService);
+
+  if (!attribute.origin) {
+    throw attributeOriginUndefined(attribute.id);
+  }
+
+  const body = IMPORTED_ATTRIBUTE_ORIGINS.includes(attribute.origin)
+    ? inAppTemplates.certifiedAttributeRevokedToAssigneeFromImport(
+        attribute.name
+      )
+    : inAppTemplates.certifiedVerifiedAttributeRevokedToAssignee(
+        attribute.name,
+        "certificato",
+        (await retrieveTenantByCertifierId(attribute.origin, readModelService))
+          .name
+      );
+
+  return usersWithNotifications.map(({ userId, tenantId }) => ({
+    userId,
+    tenantId,
+    body,
+    notificationType: "certifiedVerifiedAttributeAssignedRevokedToAssignee",
+    entityId: attribute.id,
+  }));
+}
