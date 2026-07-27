6e310b8245548696d9006d1206e5a93184dd91df
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-07-27 15:15:45 +0200

    Add new event

--- packages/in-app-notification-dispatcher/src/handlers/purposes/handlePurposeEvent.ts
+++ packages/in-app-notification-dispatcher/src/handlers/purposes/handlePurposeEvent.ts
@@ -111,6 +111,7 @@ export async function handlePurposeEvent(
           "MaintenancePurposeRiskAnalysisSetTenantKind",
           "PurposeRiskAnalysisWorkflowCreated",
           "PurposeRiskAnalysisAssigned",
+          "PurposeRiskAnalysisSelfAssigned",
           "PurposeRiskAnalysisSubmitted",
           "PurposeRiskAnalysisSigned",
           "PurposeRiskAnalysisRejected",
