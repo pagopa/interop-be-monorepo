11490ee267510aaed3d79588cca6717e9a0d052e
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2026-08-05 18:53:23 +0200

    Fix lint (PIN-10690)

--- packages/readmodel/src/catalog/aggregators.ts
+++ packages/readmodel/src/catalog/aggregators.ts
@@ -414,22 +414,24 @@ export const aggregateEservice = ({
       : {}),
     ...(archivingRequestsSQLWithNullDescriptorId.length > 0
       ? {
-          delegatedArchivingRequest: archivingRequestsSQLWithNullDescriptorId
-            .map<DelegatedEServiceArchivingRequest>((archivingRequest) => ({
-              gracePeriodDays: GracePeriodDays.parse(
-                archivingRequest.gracePeriodDays
-              ),
-              archivingReason: archivingRequest.archivingReason ?? "",
-              requestedAt: stringToDate(archivingRequest.requestedAt),
-              requesterId: unsafeBrandId(archivingRequest.requesterId),
-              rejectedAt: archivingRequest.rejectedAt
-                ? stringToDate(archivingRequest.rejectedAt)
-                : undefined,
-              acceptedAt: archivingRequest.acceptedAt
-                ? stringToDate(archivingRequest.acceptedAt)
-                : undefined,
-              rejectionReason: archivingRequest.rejectionReason ?? undefined,
-            })),
+          delegatedArchivingRequest:
+            archivingRequestsSQLWithNullDescriptorId.map<DelegatedEServiceArchivingRequest>(
+              (archivingRequest) => ({
+                gracePeriodDays: GracePeriodDays.parse(
+                  archivingRequest.gracePeriodDays
+                ),
+                archivingReason: archivingRequest.archivingReason ?? "",
+                requestedAt: stringToDate(archivingRequest.requestedAt),
+                requesterId: unsafeBrandId(archivingRequest.requesterId),
+                rejectedAt: archivingRequest.rejectedAt
+                  ? stringToDate(archivingRequest.rejectedAt)
+                  : undefined,
+                acceptedAt: archivingRequest.acceptedAt
+                  ? stringToDate(archivingRequest.acceptedAt)
+                  : undefined,
+                rejectionReason: archivingRequest.rejectionReason ?? undefined,
+              })
+            ),
         }
       : {}),
   };
