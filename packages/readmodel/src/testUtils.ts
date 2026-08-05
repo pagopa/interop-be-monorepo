6ee24a3472897042cfe298ef52bf926d57081165
Javier Jara <jarai.antonio@gmail.com>
2026-08-05 16:52:14 +0200

    fix: reorder (PIN-10693)

--- packages/readmodel/src/testUtils.ts
+++ packages/readmodel/src/testUtils.ts
@@ -349,16 +349,16 @@ export const upsertEService = async (
         .values(archivingScheduleSQL);
     }
 
-    for (const archivingRequestSQL of archivingRequestsSQL) {
-      await tx
-        .insert(eserviceDescriptorArchivingRequestInReadmodelCatalog)
-        .values(archivingRequestSQL);
-    }
-
     for (const asyncExchangePropsSQL of asyncExchangePropertiesSQL) {
       await tx
         .insert(eserviceDescriptorAsyncExchangePropertiesInReadmodelCatalog)
         .values(asyncExchangePropsSQL);
+    }
+
+    for (const archivingRequestSQL of archivingRequestsSQL) {
+      await tx
+        .insert(eserviceDescriptorArchivingRequestInReadmodelCatalog)
+        .values(archivingRequestSQL);
     }
   });
 };
