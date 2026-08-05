a51be1be8b8f255ccc3cb0dc804690dfd7ae9fa1
Davide Giunzioni <giunzioni.dp@gmail.com>
2026-08-05 14:06:10 +0200

    fix: knip (PIN-5051)

--- packages/catalog-process/src/services/validators.ts
+++ packages/catalog-process/src/services/validators.ts
@@ -497,7 +497,7 @@ export async function assertEServiceNameAvailableForProducer(
   }
 }
 
-export async function assertEServiceNameNotConflictingWithTemplate(
+async function assertEServiceNameNotConflictingWithTemplate(
   name: string,
   readModelService: ReadModelServiceSQL
 ): Promise<void> {
