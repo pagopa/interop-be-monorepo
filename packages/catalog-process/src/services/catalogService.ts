f034cc711034d97e9cb831fc155b44fb8a0b6139
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2026-08-27 09:28:09 +0200

    Fix/ Fixed archivingSchedule in cloneDescriptor (PIN-10822)

--- packages/catalog-process/src/services/catalogService.ts
+++ packages/catalog-process/src/services/catalogService.ts
@@ -2273,6 +2273,7 @@ export function catalogServiceBuilder(
             suspendedAt: undefined,
             deprecatedAt: undefined,
             archivedAt: undefined,
+            archivingSchedule: undefined,
           },
         ],
         personalData: eservice.data.personalData,
