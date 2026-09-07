7b0b344762397755185fc37c9b28285906a80dd9
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-09-07 18:02:09 +0200

    Reduce diff

--- packages/purpose-process/test/integration/assignRiskAnalysisReviewer.test.ts
+++ packages/purpose-process/test/integration/assignRiskAnalysisReviewer.test.ts
@@ -553,6 +553,32 @@ describe("assignRiskAnalysisReviewer", () => {
     ).rejects.toThrow(tenantIsNotTheConsumer(otherOrganizationId));
   });
 
+  it("should throw reviewerWorkflowConflict if the risk analysis has already been signed", async () => {
+    const reviewerId = generateId<UserId>();
+    const mockPurpose: Purpose = {
+      ...getMockPurpose([getMockPurposeVersion()]),
+      reviewMode: riskAnalysisReviewMode.reviewerWritesReviewerSigns,
+      reviewerWorkflow: {
+        reviewers: [{ id: reviewerId, sentToReviewerAt: undefined }],
+        signingState: RiskAnalysisSigningState.Values.Signed,
+        signedBy: generateId<UserId>(),
+      },
+    };
+
+    await addOnePurpose(mockPurpose);
+
+    expect(
+      purposeService.assignRiskAnalysisReviewer(
+        mockPurpose.id,
+        {
+          reviewMode: riskAnalysisReviewMode.reviewerWritesReviewerSigns,
+          reviewerIds: [generateId()],
+        },
+        getMockContext({ authData: getMockAuthData(mockPurpose.consumerId) })
+      )
+    ).rejects.toThrow(reviewerWorkflowConflict(mockPurpose.id));
+  });
+
   it.each([
     {
       description: "A: adminWritesAdminSigns -> adminWritesReviewerSigns",
@@ -1174,31 +1200,5 @@ describe("assignRiskAnalysisReviewer", () => {
         getMockContext({ authData: getMockAuthData(mockPurpose.consumerId) })
       )
     ).rejects.toThrow(purposeNotInDraftState(mockPurpose.id));
-  });
-
-  it("should throw reviewerWorkflowConflict if the risk analysis has already been signed", async () => {
-    const reviewerId = generateId<UserId>();
-    const mockPurpose: Purpose = {
-      ...getMockPurpose([getMockPurposeVersion()]),
-      reviewMode: riskAnalysisReviewMode.reviewerWritesReviewerSigns,
-      reviewerWorkflow: {
-        reviewers: [{ id: reviewerId, sentToReviewerAt: undefined }],
-        signingState: RiskAnalysisSigningState.Values.Signed,
-        signedBy: generateId<UserId>(),
-      },
-    };
-
-    await addOnePurpose(mockPurpose);
-
-    expect(
-      purposeService.assignRiskAnalysisReviewer(
-        mockPurpose.id,
-        {
-          reviewMode: riskAnalysisReviewMode.reviewerWritesReviewerSigns,
-          reviewerIds: [generateId()],
-        },
-        getMockContext({ authData: getMockAuthData(mockPurpose.consumerId) })
-      )
-    ).rejects.toThrow(reviewerWorkflowConflict(mockPurpose.id));
   });
 });
