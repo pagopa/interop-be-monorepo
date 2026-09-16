85dd9f4f9bd9a928876ce5a63d1eecf4968adf7c
Roberto Taglioni <roberto.taglioni@pagopa.it>
2026-09-16 18:59:31 +0200

    Remove to do

--- packages/purpose-outbound-writer/src/converters/toOutboundEventV2.ts
+++ packages/purpose-outbound-writer/src/converters/toOutboundEventV2.ts
@@ -54,8 +54,8 @@ function toOutboundPurposeV2(
     versions: purpose.versions.map(toOutboundPurposeVersionV2),
     riskAnalysisForm: undefined,
     purposeTemplateId: purpose.purposeTemplateId,
-    riskAnalysisReviewMode: undefined, // TODO
-    reviewerWorkflow: undefined, // TODO
+    riskAnalysisReviewMode: undefined,
+    reviewerWorkflow: undefined,
   };
 }
 
