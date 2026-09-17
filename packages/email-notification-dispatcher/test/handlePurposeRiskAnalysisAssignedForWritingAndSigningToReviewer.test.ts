9f1da907c3eaa3274fbabdf0c6a22d37309a2abc
Denis Lantero <denis.lantero@dev.buildo.io>
2026-09-17 16:07:31 +0200

    style: format renamed notification fields (PIN-10857)

--- packages/email-notification-dispatcher/test/handlePurposeRiskAnalysisAssignedForWritingAndSigningToReviewer.test.ts
+++ packages/email-notification-dispatcher/test/handlePurposeRiskAnalysisAssignedForWritingAndSigningToReviewer.test.ts
@@ -126,7 +126,8 @@ describe("handlePurposeRiskAnalysisAssignedForWritingAndSigningToReviewer", () =
       data: {
         purpose: toPurposeV2({
           ...purpose,
-          riskAnalysisReviewMode: riskAnalysisReviewMode.reviewerWritesReviewerSigns,
+          riskAnalysisReviewMode:
+            riskAnalysisReviewMode.reviewerWritesReviewerSigns,
           reviewerWorkflow: {
             reviewers: [...reviewerIds, unchangedReviewer.id].map((id) => ({
               id,
