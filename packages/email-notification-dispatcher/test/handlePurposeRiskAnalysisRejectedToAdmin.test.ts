04a97c6a8e667354c5fa7ecdec165e950abc1d29
Denis Lantero <denis.lantero@dev.buildo.io>
2026-09-17 16:05:25 +0200

    fix: align notification fixtures with renamed review mode (PIN-10863)

--- packages/email-notification-dispatcher/test/handlePurposeRiskAnalysisRejectedToAdmin.test.ts
+++ packages/email-notification-dispatcher/test/handlePurposeRiskAnalysisRejectedToAdmin.test.ts
@@ -64,7 +64,7 @@ describe("purposeRiskAnalysisRejectedToAdmin", () => {
     title: "Finalità test",
     consumerId: consumer.id,
     eserviceId: eservice.id,
-    reviewMode: "AdminWritesReviewerSigns",
+    riskAnalysisReviewMode: "AdminWritesReviewerSigns",
     reviewerWorkflow: {
       reviewers: [{ id: reviewerConfig.userId }],
       signingState: riskAnalysisSigningState.rejected,
