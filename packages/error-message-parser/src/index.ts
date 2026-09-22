496950f0aa72e3c5f872a9ec8d0d100da01df9b7
FredPirani <federico.pirani@bidcompany.it>
2026-09-22 14:40:34 +0200

    fix: Removed things from index (PIN-10970)

--- packages/error-message-parser/src/index.ts
+++ packages/error-message-parser/src/index.ts
@@ -1,8 +1,2 @@
 export * from "./models/index.js";
 export * from "./utils/index.js";
-
-import { readCsvErrorFile } from "./utils/index.js";
-
-const errors = readCsvErrorFile("input.csv", ["it", "en"]);
-
-console.log(JSON.stringify(errors, null, 2));
